fb512f34669cc9ccfb41185433d68be8551b8dfd
Sebastian Rindom <skrindom@gmail.com>
2020-01-28 16:25:28 +0100

    Adds cart service skeleton

---
+++ packages/medusa/src/services/__mocks__/region.js
@@ -0,0 +1,31 @@
+import IdMap from "../../helpers/id-map"
+
+export const regions = {
+  testRegion: {
+    _id: IdMap.getId("testRegion"),
+    name: "Test Region",
+    countries: ["DK", "US", "DE"],
+    tax_rate: 0.25,
+    payment_providers: ["default_provider"],
+    shipping_providers: ["test_shipper"],
+    currency_code: "usd",
+  },
+}
+
+export const RegionServiceMock = {
+  retrieve: jest.fn().mockImplementation(regionId => {
+    if (regionId === IdMap.getId("testRegion")) {
+      return Promise.resolve(regions.testRegion)
+    }
+    return Promise.resolve(undefined)
+  }),
+  list: jest.fn().mockImplementation(data => {
+    return Promise.resolve([regions.testRegion])
+  }),
+}
+
+const mock = jest.fn().mockImplementation(() => {
+  return RegionServiceMock
+})
+
+export default mock
---
+++ packages/medusa/src/services/__tests__/cart.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose"
+import CartService from "../cart"
+import { RegionServiceMock } from "../__mocks__/region"
+import { CartModelMock, carts } from "../../models/__mocks__/cart"
+import IdMap from "../../helpers/id-map"
+
+describe("CartService", () => {
+  describe("retrieve", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const cartService = new CartService({
+        cartModel: CartModelMock,
+      })
+      result = await cartService.retrieve(IdMap.getId("emptyCart"))
+    })
+
+    it("calls cart model functions", () => {
+      expect(CartModelMock.findOne).toHaveBeenCalledTimes(1)
+      expect(CartModelMock.findOne).toHaveBeenCalledWith({
+        _id: IdMap.getId("emptyCart"),
+      })
+    })
+
+    it("returns the cart", () => {
+      expect(result).toEqual(carts.emptyCart)
+    })
+  })
+
+  describe("setMetadata", () => {
+    const cartService = new CartService({
+      cartModel: CartModelMock,
+    })
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it("calls updateOne with correct params", async () => {
+      const id = mongoose.Types.ObjectId()
+      await cartService.setMetadata(`${id}`, "metadata", "testMetadata")
+
+      expect(CartModelMock.updateOne).toBeCalledTimes(1)
+      expect(CartModelMock.updateOne).toBeCalledWith(
+        { _id: `${id}` },
+        { $set: { "metadata.metadata": "testMetadata" } }
+      )
+    })
+
+    it("throw error on invalid key type", async () => {
+      const id = mongoose.Types.ObjectId()
+
+      try {
+        await cartService.setMetadata(`${id}`, 1234, "nono")
+      } catch (err) {
+        expect(err.message).toEqual(
+          "Key type is invalid. Metadata keys must be strings"
+        )
+      }
+    })
+  })
+})
