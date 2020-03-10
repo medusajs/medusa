7828340607c2e1d42b6bba2c94158e2fb763552b
olivermrbl <oliver@mrbltech.com>
2020-03-10 12:36:51 +0100

    Adds revokeRole

--- packages/medusa-plugin-permissions/src/services/permission.js
+++ packages/medusa-plugin-permissions/src/services/permission.js
@@ -40,16 +40,24 @@ class PermissionService extends BaseService {
     return value
   }
 
-  retrieveRole(name) {
-    return this.roleModel_.findOne({ name }).catch(err => {
+  async retrieveRole(name) {
+    const role = await this.roleModel_.findOne({ name }).catch(err => {
       throw new MedusaError(MedusaError.Types.DB_ERROR, err.message)
     })
+
+    if (!role) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_ARGUMENT,
+        `${role} does not exist. Use method createRole to create it.`
+      )
+    }
+    return role
   }
 
   async hasPermission(user, method, endpoint) {
     for (let i = 0; i < user.metadata.roles.length; i++) {
       const role = user.metadata.roles[i]
-      const permissions = await this.roleModel_.findOne({ name: role })
+      const permissions = await this.retrieveRole(role)
       return permissions.permissions.some(action => {
         return action.method === method && action.endpoint === endpoint
       })
@@ -57,95 +65,79 @@ class PermissionService extends BaseService {
     return false
   }
 
-  async createRole(role, permissions) {
+  async createRole(roleName, permissions) {
     const validatedPermissions = permissions.map(permission =>
       this.validatePermission_(permission)
     )
-    const exists = await this.roleModel_.findOne({ name: role })
-    if (exists) {
+    const role = await this.retrieveRole(roleName)
+    if (role) {
       throw new MedusaError(
         MedusaError.Types.INVALID_ARGUMENT,
-        `${role} already exists`
+        `${roleName} already exists`
       )
     }
 
     return this.roleModel_.create({
-      name: role,
+      name: roleName,
       permissions: validatedPermissions,
     })
   }
 
-  async deleteRole(role) {
+  async deleteRole(roleName) {
     return this.roleModel_.deleteOne({
-      name: role,
+      name: roleName,
       permissions: validatedPermissions,
     })
   }
 
-  async addPermission(role, permission) {
-    const roleToUpdate = await this.roleModel_.findOne({ name: role })
-
-    if (!roleToUpdate) {
-      throw new MedusaError(
-        MedusaError.Types.INVALID_ARGUMENT,
-        `${role} does not exist. Use method createRole to create it.`
-      )
-    }
-
+  async addPermission(roleName, permission) {
+    const role = await this.retrieveRole(roleName)
     const validatedPermission = this.validatePermission_(permission)
 
     return this.roleModel_.updateOne(
-      { _id: roleToUpdate._id },
+      { _id: role._id },
       { $push: { permissions: validatedPermission } }
     )
   }
 
-  async removePermission(role, permission) {
-    const roleToUpdate = await this.roleModel_.findOne({ name: role })
-
-    if (!roleToUpdate) {
-      throw new MedusaError(
-        MedusaError.Types.INVALID_ARGUMENT,
-        `${role} does not exist. Use method createRole to create it.`
-      )
-    }
-
+  async removePermission(roleName, permission) {
+    const role = await this.retrieveRole(roleName)
     const validatedPermission = this.validatePermission_(permission)
 
     return this.roleModel_.updateOne(
-      { _id: roleToUpdate._id },
+      { _id: role._id },
       { $pull: { permissions: validatedPermission } }
     )
   }
 
-  async grantRole(userId, role) {
+  async grantRole(userId, roleName) {
+    const role = await this.retrieve(roleName)
     const user = await this.userService_.retrieve(userId)
-    if (!user) {
+    if (user.metadata.roles.includes(role.name)) {
       throw new MedusaError(
         MedusaError.Types.DB_ERROR,
-        `Could not find user with id: ${userId}`
+        `User already has role: ${role.name}`
       )
     }
 
-    if (user.metadata.roles.includes(role)) {
-      throw new MedusaError(
-        MedusaError.Types.DB_ERROR,
-        `User already has role: ${role}`
-      )
-    }
+    const newRoles = user.metadata.roles.push(roleName)
 
-    const exists = await this.roleModel_.findOne({ name: role })
-    if (!exists) {
-      throw new MedusaError(
-        MedusaError.Types.INVALID_ARGUMENT,
-        `${role} does not exist`
-      )
-    }
-
-    return this.userService_.setMetadata(userId, role)
+    return this.userService_.setMetadata(userId, "roles", newRoles)
   }
 
-  async revokeRole(userId, role) {}
+  async revokeRole(userId, roleName) {
+    const user = await this.userService_.retrieve(userId)
+    if (!user.metadata.roles.includes(roleName)) {
+      // revokeRole is idempotent, we return a promise to allow then-chaining
+      return Promise.resolve()
+    }
+    // remove role from metadata.roles
+    const newRoles = (user.metadata.roles = user.metadata.roles.filter(
+      r => r !== roleName
+    ))
+
+    return this.userService_.setMetadata(userId, "roles", newRoles)
+  }
 }
 
 export default PermissionService
