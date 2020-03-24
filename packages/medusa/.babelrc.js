0aae68189e5c3e9b96992f1e519bd0a3b8c50a5c
Sebastian Rindom <skrindom@gmail.com>
2020-03-24 14:23:07 +0100

    Ignore tests in build

---
+++ packages/medusa/.babelrc.js
@@ -0,0 +1,18 @@
+let ignore = [`**/dist`]
+
+// Jest needs to compile this code, but generally we don't want this copied
+// to output folders
+if (process.env.NODE_ENV !== `test`) {
+  ignore.push(`**/__tests__`)
+}
+
+module.exports = {
+  plugins: ["@babel/plugin-proposal-class-properties"],
+  presets: ["@babel/preset-env"],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-runtime"],
+    },
+  },
+  ignore,
+}
