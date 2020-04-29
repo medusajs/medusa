d88b9277613e5bef416ee3201eeca9a4c9735a69
Sebastian Rindom <skrindom@gmail.com>
2020-04-29 11:00:25 +0200

    Adds decorator functionality to BaseService

--- packages/medusa-interfaces/src/base-service.js
+++ packages/medusa-interfaces/src/base-service.js
@@ -2,5 +2,23 @@
  * Common functionality for Services
  * @interface
  */
-class BaseService {}
+class BaseService {
+  constructor() {
+    this.decorators_ = []
+  }
+
+  addDecorator(fn) {
+    if (typeof fn !== "function") {
+      throw Error("Decorators must be of type function")
+    }
+
+    this.decorators_.push(fn)
+  }
+
+  runDecorators_(obj) {
+    return this.decorators_.reduce(async (acc, next) => {
+      return acc.then(res => next(res)).catch(() => acc)
+    }, Promise.resolve(obj))
+  }
+}
 export default BaseService
