eb5115f2c580111c7df890c017787b46be202517
Sebastian Rindom <skrindom@gmail.com>
2020-04-29 11:06:58 +0200

    Adds documentation

--- packages/medusa-interfaces/src/base-service.js
+++ packages/medusa-interfaces/src/base-service.js
@@ -7,6 +7,11 @@ class BaseService {
     this.decorators_ = []
   }
 
+  /**
+   * Adds a decorator to a service. The decorator must be a function and should
+   * return a decorated object.
+   * @param {function} fn - the decorator to add to the service
+   */
   addDecorator(fn) {
     if (typeof fn !== "function") {
       throw Error("Decorators must be of type function")
@@ -15,6 +20,13 @@ class BaseService {
     this.decorators_.push(fn)
   }
 
+  /**
+   * Runs the decorators registered on the service. The decorators are run in
+   * the order they have been registered in. Failing decorators will be skipped
+   * in order to ensure deliverability in spite of breaking code.
+   * @param {object} obj - the object to decorate.
+   * @return {object} the decorated object.
+   */
   runDecorators_(obj) {
     return this.decorators_.reduce(async (acc, next) => {
       return acc.then(res => next(res)).catch(() => acc)
