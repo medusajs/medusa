9fbb9b5df9ede6b0cfe4df3fb11cd42ec2cbf0cb
Sebastian Rindom <skrindom@gmail.com>
2020-05-05 09:59:29 +0200

    Adds todo comment in customer

--- packages/medusa/src/services/customer.js
+++ packages/medusa/src/services/customer.js
@@ -92,6 +92,10 @@ class CustomerService extends BaseService {
     const expiry = Math.floor(Date.now() / 1000) + 60 * 15 // 15 minutes ahead
     const payload = { customer_id: customer._id, exp: expiry }
     const token = jwt.sign(payload, secret)
+
+    // TODO: Call event layer to ensure that there is an email service that
+    // sends the token.
+
     return token
   }
 
