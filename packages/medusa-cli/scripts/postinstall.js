07edf423a14581add113f75352f766fe5f62ef6a
Sebastian Rindom <skrindom@gmail.com>
2020-05-07 13:21:19 +0200

    Postinstall fix

--- packages/medusa-cli/scripts/postinstall.js
+++ packages/medusa-cli/scripts/postinstall.js
@@ -2,18 +2,14 @@ const chalk = require(`chalk`)
 
 const showSuccessMessage = () => {
   console.log(chalk.green(`Success!\n`))
-  console.log(
-    chalk.cyan(`Welcome to the Gatsby CLI! Please visit `) +
-      chalk.underline(`https://www.gatsbyjs.org/docs/gatsby-cli/`) +
-      chalk.cyan(` for more information.\n`)
-  )
+  console.log(chalk.cyan(`Welcome to the Medusa CLI!`))
 }
 
 try {
-  // check if it's a global installation of gatsby-cli
+  // check if it's a global installation of medusa-cli
   const npmArgs = JSON.parse(process.env[`npm_config_argv`])
   if (npmArgs.cooked && npmArgs.cooked.includes(`--global`)) {
-    const createCli = require(`../lib/create-cli`)
+    const createCli = require(`../dist/create-cli`)
     showSuccessMessage()
     createCli(`--help`)
   }
