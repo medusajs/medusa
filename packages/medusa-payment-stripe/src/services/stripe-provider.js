194e8fb2c0fc1a8089774d6f0f7772d87f35358f
olivermrbl <oliver@mrbltech.com>
2020-05-11 13:51:00 +0200

    Starts work on stripe payment provider

--- packages/medusa-payment-stripe/src/services/stripe-provider.js
+++ packages/medusa-payment-stripe/src/services/stripe-provider.js
@@ -1,4 +1,5 @@
 import _ from "lodash"
+import Stripe from "stripe"
 import { PaymentService } from "medusa-interfaces"
 
 class StripeProviderService extends PaymentService {
@@ -7,6 +8,42 @@ class StripeProviderService extends PaymentService {
   constructor(appScope, options) {
     super()
     console.log(options)
+
+    this.stripe_ = Stripe(options.api_key)
+  }
+
+  async createPayment(object) {
+    const { customer_id } = object
+
+    let customer = {}
+
+    if (!customer_id) {
+      customer = await this.stripe_.customers.create({
+        email: object.email,
+      })
+    } else {
+      customer = await this.stripe_.customer.retrieve(customer_id)
+    }
+  }
+
+  async retrievePayment(object) {
+    const { provider_id } = object.payment_method
+
+    try {
+      return this.stripe_.paymentIntents.retrieve(provider_id)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async updatePayment(object, data) {
+    const { provider_id } = object.payment_method
+
+    try {
+      return this.stripe_.paymentIntents.retrieve(provider_id)
+    } catch (error) {
+      throw error
+    }
   }
 }
 
