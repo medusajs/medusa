3debd7e1081473ec17ab4848bde29f4dff1252b7
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2020-05-25 21:08:16 +0200

    Stripe payment provider plugin (#65)
    Adds Stripe payment provider plugin
    
    Closes #44

---
+++ packages/medusa-payment-stripe/src/api/routes/hooks/stripe.js
@@ -0,0 +1,35 @@
+export default async (req, res) => {
+  const signature = req.headers["stripe-signature"]
+
+  let event
+  try {
+    const stripeProviderService = req.resolve("pp_stripe")
+    event = stripeProviderService.constructWebhookEvent(req.body, signature)
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`)
+    return
+  }
+
+  const paymentIntent = event.data.object
+
+  // handle payment intent events
+  switch (event.type) {
+    case "payment_intent.succeeded":
+      break
+    case "payment_intent.canceled":
+      break
+    case "payment_intent.created":
+      break
+    case "payment_intent.payment_failed":
+      break
+    case "payment_intent.amount_capturable_updated":
+      break
+    case "payment_intent.processing":
+      break
+    default:
+      res.status(400)
+      return
+  }
+
+  res.sendStatus(200)
+}
