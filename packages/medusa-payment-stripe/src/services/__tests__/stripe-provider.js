3b4bc5eda38fc2caf77a78c13645bd7edc11f7e4
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2020-05-25 21:08:16 +0200

    Stripe payment provider plugin (#65)
    Adds Stripe payment provider plugin
    
    Closes #44

---
+++ packages/medusa-payment-stripe/src/services/__tests__/stripe-provider.js
@@ -0,0 +1,243 @@
+import { IdMap } from "medusa-test-utils"
+import StripeProviderService from "../stripe-provider"
+import { CustomerServiceMock } from "../../__mocks__/customer"
+import { carts } from "../../__mocks__/cart"
+import { TotalsServiceMock } from "../../__mocks__/totals"
+
+describe("StripeProviderService", () => {
+  describe("createCustomer", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const stripeProviderService = new StripeProviderService(
+        {
+          customerService: CustomerServiceMock,
+          totalsService: TotalsServiceMock,
+        },
+        {
+          api_key: "test",
+        }
+      )
+
+      result = await stripeProviderService.createCustomer({
+        _id: IdMap.getId("vvd"),
+        first_name: "Virgil",
+        last_name: "Van Dijk",
+        email: "virg@vvd.com",
+        password_hash: "1234",
+        metadata: {},
+      })
+    })
+
+    it("returns created stripe customer", () => {
+      expect(result).toEqual({
+        id: "cus_vvd",
+        email: "virg@vvd.com",
+      })
+    })
+  })
+
+  describe("createPayment", () => {
+    let result
+    const stripeProviderService = new StripeProviderService(
+      {
+        customerService: CustomerServiceMock,
+        totalsService: TotalsServiceMock,
+      },
+      {
+        api_key: "test",
+      }
+    )
+
+    beforeEach(async () => {
+      jest.clearAllMocks()
+    })
+
+    it("returns created stripe payment intent for cart with existing customer", async () => {
+      result = await stripeProviderService.createPayment(carts.frCart)
+      expect(result).toEqual({
+        id: "pi_lebron",
+        customer: "cus_123456789_new",
+        amount: 100,
+      })
+    })
+
+    it("returns created stripe payment intent for cart with no customer", async () => {
+      carts.frCart.customer_id = ""
+      result = await stripeProviderService.createPayment(carts.frCart)
+      expect(result).toEqual({
+        id: "pi_lebron",
+        customer: "cus_lebron",
+        amount: 100,
+      })
+    })
+  })
+
+  describe("retrievePayment", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const stripeProviderService = new StripeProviderService(
+        {
+          customerService: CustomerServiceMock,
+          totalsService: TotalsServiceMock,
+        },
+        {
+          api_key: "test",
+        }
+      )
+
+      result = await stripeProviderService.retrievePayment({
+        payment_method: {
+          data: {
+            id: "pi_lebron",
+          },
+        },
+      })
+    })
+
+    it("returns stripe payment intent", () => {
+      expect(result).toEqual({
+        id: "pi_lebron",
+        customer: "cus_lebron",
+      })
+    })
+  })
+
+  describe("updatePayment", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const stripeProviderService = new StripeProviderService(
+        {
+          customerService: CustomerServiceMock,
+          totalsService: TotalsServiceMock,
+        },
+        {
+          api_key: "test",
+        }
+      )
+
+      result = await stripeProviderService.updatePayment(
+        {
+          payment_method: {
+            data: {
+              id: "pi_lebron",
+            },
+          },
+        },
+        {
+          amount: 1000,
+        }
+      )
+    })
+
+    it("returns cancelled stripe payment intent", () => {
+      expect(result).toEqual({
+        id: "pi_lebron",
+        customer: "cus_lebron",
+        amount: 1000,
+      })
+    })
+  })
+
+  describe("updatePaymentIntentCustomer", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const stripeProviderService = new StripeProviderService(
+        {
+          customerService: CustomerServiceMock,
+          totalsService: TotalsServiceMock,
+        },
+        {
+          api_key: "test",
+        }
+      )
+
+      result = await stripeProviderService.updatePaymentIntentCustomer(
+        "pi_lebron",
+        "cus_lebron_2"
+      )
+    })
+
+    it("returns update stripe payment intent", () => {
+      expect(result).toEqual({
+        id: "pi_lebron",
+        customer: "cus_lebron_2",
+        amount: 1000,
+      })
+    })
+  })
+
+  describe("capturePayment", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const stripeProviderService = new StripeProviderService(
+        {},
+        {
+          api_key: "test",
+        }
+      )
+
+      result = await stripeProviderService.capturePayment("pi_lebron")
+    })
+
+    it("returns captured stripe payment intent", () => {
+      expect(result).toEqual({
+        id: "pi_lebron",
+        customer: "cus_lebron",
+        amount: 1000,
+        status: "succeeded",
+      })
+    })
+  })
+
+  describe("refundPayment", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const stripeProviderService = new StripeProviderService(
+        {},
+        {
+          api_key: "test",
+        }
+      )
+
+      result = await stripeProviderService.refundPayment("pi_lebron", 1000)
+    })
+
+    it("returns refunded stripe payment intent", () => {
+      expect(result).toEqual({
+        id: "re_123",
+        payment_intent: "pi_lebron",
+        amount: 1000,
+        status: "succeeded",
+      })
+    })
+  })
+
+  describe("cancelPayment", () => {
+    let result
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      const stripeProviderService = new StripeProviderService(
+        {},
+        {
+          api_key: "test",
+        }
+      )
+
+      result = await stripeProviderService.cancelPayment("pi_lebron")
+    })
+
+    it("returns cancelled stripe payment intent", () => {
+      expect(result).toEqual({
+        id: "pi_lebron",
+        customer: "cus_lebron",
+        status: "cancelled",
+      })
+    })
+  })
+})
