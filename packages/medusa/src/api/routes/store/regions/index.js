c8d4a5536c4b9c49b600712f4ab5b891a7387f81
Sebastian Rindom <skrindom@gmail.com>
2020-05-27 20:03:21 +0200

    Adds store/regions endpoints; fixes payment provider service

---
+++ packages/medusa/src/api/routes/store/regions/index.js
@@ -0,0 +1,13 @@
+import { Router } from "express"
+import middlewares from "../../../middlewares"
+
+const route = Router()
+
+export default app => {
+  app.use("/regions", route)
+
+  route.get("/", middlewares.wrap(require("./list-regions").default))
+  route.get("/:region_id", middlewares.wrap(require("./get-region").default))
+
+  return app
+}
