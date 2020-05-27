2b4399fcd983d6c421d77640588dd4e9910cf7eb
Sebastian Rindom <skrindom@gmail.com>
2020-05-27 23:03:32 +0200

    Adds createSession and updateSession

--- packages/medusa/src/services/__tests__/payment-provider.js
+++ packages/medusa/src/services/__tests__/payment-provider.js
@@ -26,4 +26,60 @@ describe("ProductService", () => {
       }
     })
   })
+
+  describe("createSession", () => {
+    const createSession = jest.fn().mockReturnValue(Promise.resolve())
+    const container = {
+      pp_default_provider: {
+        createSession,
+      },
+    }
+
+    const providerService = new PaymentProviderService(container)
+
+    it("successfully creates session", async () => {
+      await providerService.createSession("default_provider", {
+        total: 100,
+      })
+
+      expect(createSession).toBeCalledTimes(1)
+      expect(createSession).toBeCalledWith({
+        total: 100,
+      })
+    })
+  })
+
+  describe("updateSession", () => {
+    const updateSession = jest.fn().mockReturnValue(Promise.resolve())
+
+    const container = {
+      pp_default_provider: {
+        updateSession,
+      },
+    }
+
+    const providerService = new PaymentProviderService(container)
+
+    it("successfully creates session", async () => {
+      await providerService.updateSession(
+        {
+          provider_id: "default_provider",
+          data: {
+            id: "1234",
+          },
+        },
+        {
+          total: 100,
+        }
+      )
+
+      expect(updateSession).toBeCalledTimes(1)
+      expect(updateSession).toBeCalledWith(
+        { id: "1234" },
+        {
+          total: 100,
+        }
+      )
+    })
+  })
 })
