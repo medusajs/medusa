934baa1ffde6ad81099b57df8c050d1f1ec2475a
olivermrbl <oliver@mrbltech.com>
2020-06-07 21:21:54 +0200

    Removes commented code

--- packages/medusa-payment-klarna/src/api/routes/hooks/push.js
+++ packages/medusa-payment-klarna/src/api/routes/hooks/push.js
@@ -32,29 +32,3 @@ export default async (req, res) => {
     throw error
   }
 }
-
-// app.post("/order-completed", async (req, res) => {
-//   const klarnaOrderId = req.query.klarna_order_id
-//   const klarnaOrder = await Klarna.get(
-//     `/ordermanagement/v1/orders/${klarnaOrderId}`
-//   )
-//     .then(({ data }) => {
-//       return data
-//     })
-//     .catch(err => {
-//       console.log(err)
-//       return {}
-//     })
-
-//   const cartId = klarnaOrder.merchant_data
-//   if (klarnaOrder) {
-//     const order = await Order.findOne({ cartId })
-//     if (order) {
-//       await acknowledgeKlarnaOrder(klarnaOrderId, order._id)
-//     } else {
-//       await Hooks.run("onCreateOrder", cartId)
-//     }
-//   }
-
-//   res.sendStatus(200)
-// })
