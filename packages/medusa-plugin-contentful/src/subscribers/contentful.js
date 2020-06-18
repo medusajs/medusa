002859192f21b65e5a6d262b35868168fecf93f2
olivermrbl <oliver@mrbltech.com>
2020-06-18 14:47:48 +0200

    Adds plugin for contentful synchronization

---
+++ packages/medusa-plugin-contentful/src/subscribers/contentful.js
@@ -0,0 +1,16 @@
+class ContentfulSubscriber {
+  constructor({ contentfulService, eventBusService }) {
+    this.contentfulService_ = contentfulService
+    this.eventBus_ = eventBusService
+
+    this.eventBus_.subscribe("product-variant.updated", async (data) => {
+      await this.contentfulService_.updateProductVariantInContentful(data)
+    })
+
+    this.eventBus_.subscribe("product.updated", async (data) => {
+      await this.contentfulService_.updateProductInContentful(data)
+    })
+  }
+}
+
+export default ContentfulSubscriber
