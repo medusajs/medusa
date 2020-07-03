d6fc477636870f1db8de2cc6415444af24da648c
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2020-07-03 18:00:54 +0200

    SendGrid plugin (#77)
    Closes #73 and #77

--- packages/medusa/src/services/order.js
+++ packages/medusa/src/services/order.js
@@ -3,6 +3,12 @@ import { Validator, MedusaError } from "medusa-core-utils"
 import { BaseService } from "medusa-interfaces"
 
 class OrderService extends BaseService {
+  static Events = {
+    PLACED: "order.placed",
+    UPDATED: "order.updated",
+    CANCELLED: "order.cancelled",
+  }
+
   constructor({
     orderModel,
     paymentProviderService,
@@ -153,9 +159,16 @@ class OrderService extends BaseService {
    * @return {Promise} resolves to the creation result.
    */
   async create(order) {
-    return this.orderModel_.create(order).catch(err => {
-      throw new MedusaError(MedusaError.Types.DB_ERROR, err.message)
-    })
+    return this.orderModel_
+      .create(order)
+      .then(result => {
+        // Notify subscribers
+        this.eventBus_.emit(OrderService.Events.PLACED, result)
+        return result
+      })
+      .catch(err => {
+        throw new MedusaError(MedusaError.Types.DB_ERROR, err.message)
+      })
   }
 
   /**
@@ -225,6 +238,11 @@ class OrderService extends BaseService {
         { $set: updateFields },
         { runValidators: true }
       )
+      .then(result => {
+        // Notify subscribers
+        this.eventBus_.emit(OrderService.Events.UPDATED, result)
+        return result
+      })
       .catch(err => {
         throw new MedusaError(MedusaError.Types.DB_ERROR, err.message)
       })
@@ -256,14 +274,23 @@ class OrderService extends BaseService {
 
     // TODO: cancel payment method
 
-    return this.orderModel_.updateOne(
-      {
-        _id: orderId,
-      },
-      {
-        $set: { status: "cancelled" },
-      }
-    )
+    return this.orderModel_
+      .updateOne(
+        {
+          _id: orderId,
+        },
+        {
+          $set: { status: "cancelled" },
+        }
+      )
+      .then(result => {
+        // Notify subscribers
+        this.eventBus_.emit(OrderService.Events.CANCELLED, result)
+        return result
+      })
+      .catch(err => {
+        throw new MedusaError(MedusaError.Types.DB_ERROR, err.message)
+      })
   }
 
   /**
@@ -344,14 +371,23 @@ class OrderService extends BaseService {
       })
     )
 
-    return this.orderModel_.updateOne(
-      {
-        _id: orderId,
-      },
-      {
-        $set: updateFields,
-      }
-    )
+    return this.orderModel_
+      .updateOne(
+        {
+          _id: orderId,
+        },
+        {
+          $set: updateFields,
+        }
+      )
+      .then(result => {
+        // Notify subscribers
+        this.eventBus_.emit(OrderService.Events.UPDATED, result)
+        return result
+      })
+      .catch(err => {
+        throw new MedusaError(MedusaError.Types.DB_ERROR, err.message)
+      })
   }
 
   /**
