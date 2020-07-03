517f6baff06f083a5d36687a6bc4ddacd0c44eec
olivermrbl <oliver@mrbltech.com>
2020-07-03 17:01:10 +0200

    Adds missing folders

---
+++ packages/medusa-fulfillment-manual/dist/services/manual-fulfillment.js
@@ -0,0 +1,94 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _medusaInterfaces = _interopRequireDefault(require("medusa-interfaces"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var ManualFulfillmentService = /*#__PURE__*/function (_BaseFulfillmentServi) {
+  _inherits(ManualFulfillmentService, _BaseFulfillmentServi);
+
+  var _super = _createSuper(ManualFulfillmentService);
+
+  function ManualFulfillmentService() {
+    _classCallCheck(this, ManualFulfillmentService);
+
+    return _super.call(this);
+  }
+
+  _createClass(ManualFulfillmentService, [{
+    key: "getFulfillmentOptions",
+    value: function getFulfillmentOptions() {
+      return [{
+        id: "manual-fulfillment"
+      }];
+    }
+  }, {
+    key: "validateFulfillmentData",
+    value: function validateFulfillmentData(data, cart) {
+      return data;
+    }
+  }, {
+    key: "validateOption",
+    value: function validateOption(data) {
+      if (data.id === "manual-fulfillment") {
+        return true;
+      }
+
+      return false;
+    }
+  }, {
+    key: "canCalculate",
+    value: function canCalculate() {
+      return false;
+    }
+  }, {
+    key: "calculatePrice",
+    value: function calculatePrice() {
+      throw Error("Manual Fulfillment service cannot calculatePrice");
+    }
+  }, {
+    key: "createOrder",
+    value: function createOrder() {
+      // No data is being sent anywhere
+      return;
+    }
+  }]);
+
+  return ManualFulfillmentService;
+}(_medusaInterfaces["default"]);
+
+_defineProperty(ManualFulfillmentService, "identifier", "manual");
+
+var _default = ManualFulfillmentService;
+exports["default"] = _default;
