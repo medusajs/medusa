24dbfc3dc73804750c91b4da8a8fbf0143a5367e
olivermrbl <oliver@mrbltech.com>
2020-07-04 18:11:18 +0200

    Fixes creating discount with rule

--- packages/medusa/src/api/routes/admin/discounts/create-discount.js
+++ packages/medusa/src/api/routes/admin/discounts/create-discount.js
@@ -7,10 +7,13 @@ export default async (req, res) => {
       .keys({
         description: Validator.string().optional(),
         type: Validator.string().required(),
-        value: Validator.number().required(),
+        value: Validator.number()
+          .positive()
+          .required(),
         allocation: Validator.string().required(),
         valid_for: Validator.array().items(Validator.string()),
         usage_limit: Validator.number().optional(),
+        total_limit: Validator.number().optional(),
       })
       .required(),
     usage_count: Validator.number().optional(),
