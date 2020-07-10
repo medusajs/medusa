3bcff97dcb6e11dcd2ee0326cddc69798d8ca68c
Sebastian Rindom <skrindom@gmail.com>
2020-07-10 15:41:42 +0200

    Klarna orders

--- packages/medusa-payment-klarna/src/api/routes/hooks/address.js
+++ packages/medusa-payment-klarna/src/api/routes/hooks/address.js
@@ -3,30 +3,29 @@ export default async (req, res) => {
   const { shipping_address, merchant_data } = req.body
 
   try {
-    const cartService = req.resolve("cartService")
-    const klarnaProviderService = req.resolve("pp_klarna")
+    const cartService = req.scope.resolve("cartService")
+    const klarnaProviderService = req.scope.resolve("pp_klarna")
 
     const cart = await cartService.retrieve(merchant_data)
 
     if (shipping_address) {
       const updatedAddress = {
-        email: shipping_address.email,
-        street_address: shipping_address.address_1,
-        street_address2: shipping_address.address_2,
-        postal_code: shipping_address.postal_code,
+        first_name: shipping_address.given_name,
+        last_name: shipping_address.family_name,
+        address_1: shipping_address.street_address,
+        address_2: shipping_address.street_address2,
         city: shipping_address.city,
-        country: shipping_address.country_code,
+        country_code: shipping_address.country,
+        postal_code: shipping_address.postal_code,
       }
-
-      await cartService.update(cart._id, {
-        email: shipping_address.email,
-        shipping_address: updatedAddress,
-        billing_address: updatedAddress,
-      })
+      
+      await cartService.updateShippingAddress(cart._id, updatedAddress)
+      await cartService.updateBillingAddress(cart._id, updatedAddress)
+      await cartService.updateEmail(cart._id, shipping_address.email)
 
       // Fetch and return updated Klarna order
       const updatedCart = await cartService.retrieve(cart._id)
-      const order = klarnaProviderService.cartToKlarnaOrder(updatedCart)
+      const order = await klarnaProviderService.cartToKlarnaOrder(updatedCart)
       res.json(order)
       return
     } else {
--- packages/medusa-payment-klarna/src/api/routes/hooks/index.js
+++ packages/medusa-payment-klarna/src/api/routes/hooks/index.js
@@ -4,10 +4,10 @@ import middlewares from "../../middlewares"
 const route = Router()
 
 export default (app) => {
-  app.use("/hooks", route)
+  app.use("/klarna", route)
 
-  route.post("/shipping", middlewares.wrap(require("./shippping").default))
   route.post("/address", middlewares.wrap(require("./address").default))
+  route.post("/shipping", middlewares.wrap(require("./shipping").default))
   route.post("/push", middlewares.wrap(require("./push").default))
 
   return app
