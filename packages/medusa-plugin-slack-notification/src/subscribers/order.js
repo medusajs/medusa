2a54ec231bb335be643fdf9aef1d4317d7d55fbc
olivermrbl <oliver@mrbltech.com>
2020-07-12 11:26:01 +0200

    Adds order subscriber for slack

--- packages/medusa-plugin-slack-notification/src/subscribers/order.js
+++ packages/medusa-plugin-slack-notification/src/subscribers/order.js
@@ -1,15 +1,11 @@
 class OrderSubscriber {
-  constructor({ economicService, eventBusService }) {
-    this.economicService_ = economicService
+  constructor({ slackService, eventBusService }) {
+    this.slackService_ = slackService
 
     this.eventBus_ = eventBusService
 
     this.eventBus_.subscribe("order.placed", async (order) => {
-      await this.economicService_.draftEconomicInvoice(order)
-    })
-
-    this.eventBus_.subscribe("order.completed", async (order) => {
-      await this.economicService_.bookEconomicInvoice(order._id)
+      await this.slackService_.orderNotification(order._id)
     })
   }
 }
