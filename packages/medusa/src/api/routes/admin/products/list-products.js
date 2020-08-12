1c8fd2c4d9215ae6104663cf022f7d4abdfe2cb0
olivermrbl <oliver@mrbltech.com>
2020-08-12 20:55:31 +0200

    Adds query builder

--- packages/medusa/src/api/routes/admin/products/list-products.js
+++ packages/medusa/src/api/routes/admin/products/list-products.js
@@ -1,7 +1,17 @@
+import _ from "lodash"
+
 export default async (req, res) => {
   try {
     const productService = req.scope.resolve("productService")
-    let products = await productService.list({})
+    const queryBuilderService = req.scope.resolve("queryBuilderService")
+
+    const query = queryBuilderService.buildQuery(req.query, [
+      "title",
+      "description",
+    ])
+
+    let products = await productService.list(query)
+
     products = await Promise.all(
       products.map(
         async product =>
