014354a16dd695d9bae0af5565457b696a04a8a1
olivermrbl <oliver@mrbltech.com>
2020-08-28 11:36:35 +0200

    Adds discount decoration

--- packages/medusa/src/api/routes/admin/discounts/get-discount.js
+++ packages/medusa/src/api/routes/admin/discounts/get-discount.js
@@ -4,7 +4,22 @@ export default async (req, res) => {
     const discountService = req.scope.resolve("discountService")
     const data = await discountService.retrieve(discount_id)
 
-    res.status(200).json({ discount: data })
+    const discount = await discountService.decorate(
+      data,
+      [
+        "code",
+        "is_dynamic",
+        "discount_rule",
+        "usage_count",
+        "disabled",
+        "starts_at",
+        "ends_at",
+        "regions",
+      ],
+      ["valid_for"]
+    )
+
+    res.status(200).json({ discount })
   } catch (err) {
     throw err
   }
