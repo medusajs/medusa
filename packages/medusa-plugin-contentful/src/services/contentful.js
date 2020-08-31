7b6a6afe3cb7400fbd5c9d5bab9a9e7ab966908c
Sebastian Rindom <skrindom@gmail.com>
2020-08-31 20:45:51 +0200

    pull thumbs from contentful

--- packages/medusa-plugin-contentful/src/services/contentful.js
+++ packages/medusa-plugin-contentful/src/services/contentful.js
@@ -267,8 +267,14 @@ class ContentfulService extends BaseService {
         this.redis_.set("product_ignore_ids", JSON.stringify(ignoreIds))
       }
 
+      // Get the thumbnail
+      const thumb = await environment.getAsset(
+        entry.fields.thumbnail["en-US"].sys.id
+      )
+
       const updatedProduct = await this.productService_.update(productId, {
         title: productEntry.fields.title["en-US"],
+        thumbnail: thumb.fields.file["en-US"].url,
       })
 
       return updatedProduct
