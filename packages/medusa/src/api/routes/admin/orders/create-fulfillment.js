e12ab2be32191b72d6db10a388c56fc71beecc63
Sebastian Rindom <skrindom@gmail.com>
2020-09-07 17:20:49 +0200

    fix: updates admin fulfillment endpoint

--- packages/medusa/src/api/routes/admin/orders/create-fulfillment.js
+++ packages/medusa/src/api/routes/admin/orders/create-fulfillment.js
@@ -1,9 +1,31 @@
+import { MedusaError, Validator } from "medusa-core-utils"
+
 export default async (req, res) => {
   const { id } = req.params
 
+  const schema = Validator.object().keys({
+    items: Validator.array()
+      .items({
+        item_id: Validator.string().required(),
+        quantity: Validator.number().required(),
+      })
+      .required(),
+    metadata: Validator.object().optional(),
+  })
+
+  const { value, error } = schema.validate(req.body)
+  if (error) {
+    throw new MedusaError(MedusaError.Types.INVALID_DATA, error.details)
+  }
+
   try {
     const orderService = req.scope.resolve("orderService")
-    let order = await orderService.createFulfillment(id)
+
+    let order = await orderService.createFulfillment(
+      id,
+      value.items,
+      value.metadata
+    )
     order = await orderService.decorate(order, [], ["region"])
     res.json({ order })
   } catch (error) {
