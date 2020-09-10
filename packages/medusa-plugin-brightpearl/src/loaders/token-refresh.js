f964d22593b598eb87d60b095a2cbce085b24ede
Sebastian Rindom <skrindom@gmail.com>
2020-09-10 11:55:57 +0200

    chore get coorection

--- packages/medusa-plugin-brightpearl/src/loaders/token-refresh.js
+++ packages/medusa-plugin-brightpearl/src/loaders/token-refresh.js
@@ -11,8 +11,10 @@ const refreshToken = async (container) => {
       console.log(`running refresh token ${REFRESH_CRON}`)
       const appData = await oauthService.retrieveByName("brightpearl")
       const data = appData.data
+      console.log(appData)
+      console.log(data)
       if (!data || !data.access_token) {
-        await oauthService
+        return oauthService
           .refreshToken("brightpearl", data.refresh_token)
           .catch((err) => {
             console.log(err)
