658bfddbc9692726e1b05f9f32ac3c73f9b4620f
Sebastian Rindom <skrindom@gmail.com>
2020-09-10 10:56:25 +0200

    chore: debug

--- packages/medusa-plugin-brightpearl/src/loaders/token-refresh.js
+++ packages/medusa-plugin-brightpearl/src/loaders/token-refresh.js
@@ -1,11 +1,13 @@
 const REFRESH_CRON = process.env.BP_REFRESH_CRON || "5 4 * * */6"
+
 const refreshToken = async (container) => {
+  const logger = container.resolve("logger")
   const oauthService = container.resolve("oauthService")
   const eventBus = container.resolve("eventBusService")
 
   try {
-    const pattern = REFRESH_CRON
-    eventBus.createCronJob("refresh-token-bp", {}, pattern, async () => {
+    logger.info("registering refresh cron job BP")
+    eventBus.createCronJob("refresh-token-bp", {}, REFRESH_CRON, async () => {
       const data = await oauthService.retrieveByName("brightpearl")
       if (!data || !data.access_token) {
         await oauthService.refreshToken("brightpearl", data.refresh_token)
