10af77498b24cf03c69ee893f8fe4381da026d55
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2020-09-23 09:51:47 +0200

    fix(medusa-plugin-addon): Fixes admin endpoints, Adds flag to avoid merging add-on line-items

--- packages/medusa-plugin-add-ons/src/api/routes/admin/index.js
+++ packages/medusa-plugin-add-ons/src/api/routes/admin/index.js
@@ -1,10 +1,24 @@
 import { Router } from "express"
 import bodyParser from "body-parser"
+import cors from "cors"
 import middlewares from "../../middlewares"
+import { getConfigFile } from "medusa-core-utils"
 
 const route = Router()
 
-export default (app) => {
+export default (app, rootDirectory) => {
+  const { configModule } = getConfigFile(rootDirectory, `medusa-config`)
+  const config = (configModule && configModule.projectConfig) || {}
+
+  const adminCors = config.admin_cors || ""
+
+  route.use(
+    cors({
+      origin: adminCors.split(","),
+      credentials: true,
+    })
+  )
+
   app.use("/admin", route)
 
   route.post(
@@ -17,6 +31,24 @@ export default (app) => {
     "/add-ons/:id",
     bodyParser.json(),
     middlewares.wrap(require("./update-add-on").default)
+  )
+
+  route.get(
+    "/add-ons",
+    bodyParser.json(),
+    middlewares.wrap(require("./list-add-ons").default)
+  )
+
+  route.get(
+    "/add-ons/:id",
+    bodyParser.json(),
+    middlewares.wrap(require("./get-add-on").default)
+  )
+
+  route.delete(
+    "/add-ons/:id",
+    bodyParser.json(),
+    middlewares.wrap(require("./delete-add-on").default)
   )
 
   return app
