a3a3d8692e4932c286e7775cf8731843185f8d5d
olivermrbl <oliver@mrbltech.com>
2020-09-30 09:27:26 +0200

    hotfix(medusa-plugin-add-ons): adds metadata to endpoints

--- packages/medusa-plugin-add-ons/src/api/routes/store/create-line-item.js
+++ packages/medusa-plugin-add-ons/src/api/routes/store/create-line-item.js
@@ -7,6 +7,7 @@ export default async (req, res) => {
     variant_id: Validator.string().required(),
     quantity: Validator.number().required(),
     add_ons: Validator.array().items(Validator.string()).optional(),
+    metadata: Validator.object().optional(),
   })
 
   const { value, error } = schema.validate(req.body)
@@ -24,12 +25,13 @@ export default async (req, res) => {
       value.variant_id,
       cart.region_id,
       value.quantity,
-      value.add_ons
+      value.add_ons,
+      value.metadata
     )
 
     cart = await cartService.addLineItem(cart._id, lineItem)
     cart = await cartService.decorate(cart, [], ["region"])
-    
+
     cart.items = await Promise.all(
       cart.items.map((item) =>
         lineItemService.decorate(
