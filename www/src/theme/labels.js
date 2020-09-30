26a86d2a8dd105b1b3b38fbef5ee2fb3f874a945
Sebastian Rindom <skrindom@gmail.com>
2020-09-30 21:16:46 +0200

    docs: adds first version of REST docs

---
+++ www/src/theme/labels.js
@@ -0,0 +1,17 @@
+export const labels = {
+  required: {
+    color: "danger",
+    fontSize: 0,
+  },
+  GET: {
+    color: "green",
+  },
+  POST: {
+    color: "blue",
+  },
+  DELETE: {
+    color: "danger",
+  },
+}
+
+export default labels
