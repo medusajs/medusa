eedad6e6e5483ce2176732a57317ce103922b11e
olivermrbl <oliver@mrbltech.com>
2020-10-19 09:35:42 +0200

    Adds provider_id to request params

--- packages/medusa/src/api/routes/store/carts/update-payment-method.js
+++ packages/medusa/src/api/routes/store/carts/update-payment-method.js
@@ -1,10 +1,9 @@
 import { Validator, MedusaError } from "medusa-core-utils"
 
 export default async (req, res) => {
-  const { id } = req.params
+  const { id, provider_id } = req.params
 
   const schema = Validator.object().keys({
-    provider_id: Validator.string().required(),
     data: Validator.object().optional(),
   })
 
@@ -16,7 +15,10 @@ export default async (req, res) => {
   try {
     const cartService = req.scope.resolve("cartService")
 
-    let cart = await cartService.setPaymentMethod(id, value)
+    let cart = await cartService.setPaymentMethod(id, {
+      provider_id,
+      data: value.data,
+    })
     cart = await cartService.decorate(cart, [], ["region"])
 
     res.status(200).json({ cart })
