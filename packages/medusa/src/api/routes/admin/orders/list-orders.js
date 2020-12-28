d0fafe01d10eb1155cab31cde1048aff92e299f9
olivermrbl <oliver@mrbltech.com>
2020-12-28 15:33:48 +0100

    feat(medusa): Adds filtering to order listing for use in admin

--- packages/medusa/src/api/routes/admin/orders/list-orders.js
+++ packages/medusa/src/api/routes/admin/orders/list-orders.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
     const orderService = req.scope.resolve("orderService")
     const queryBuilderService = req.scope.resolve("queryBuilderService")
 
-    const query = queryBuilderService.buildQuery(req.query, [
+    let query = queryBuilderService.buildQuery(req.query, [
       "display_id",
       "email",
       "status",
@@ -13,10 +13,55 @@ export default async (req, res) => {
       "payment_status",
     ])
 
-    const limit = parseInt(req.query.limit) || 0
+    const limit = parseInt(req.query.limit) || 50
     const offset = parseInt(req.query.offset) || 0
 
-    let orders = await orderService.list(query, offset, limit)
+    let orders
+
+    // Temporary solution for admin tabs filtering
+    // Will be replaced in Mongo -> Postgres migration
+    if (req.query["status"]) {
+      if (req.query["status"] === "returns") {
+        query = {
+          "returns.status": "requested",
+        }
+      }
+
+      if (req.query["status"] === "new") {
+        query = {
+          $or: [
+            { fulfillment_status: { $ne: "shipped" } },
+            { payment_status: { $ne: "captured" } },
+          ],
+        }
+      }
+
+      if (req.query["status"] === "requires_action") {
+        query = {
+          $or: [
+            { status: "requires_action" },
+            { payment_status: "requires_action" },
+            { fulfillment_status: "requires_action" },
+          ],
+        }
+      }
+
+      if (req.query["status"] === "swaps") {
+        const swapService = req.scope.resolve("swapService")
+
+        const swapsInProgress = await swapService.list({
+          fulfillment_status: { $ne: "shipped" },
+        })
+
+        if (swapsInProgress.length) {
+          query = {
+            swaps: { $in: swapsInProgress.map(s => s._id.toString()) },
+          }
+        }
+      }
+    }
+
+    orders = await orderService.list(query, offset, limit)
 
     let includeFields = []
     if ("fields" in req.query) {
