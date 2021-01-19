9e4810e2c2978fd6e4942b360522a4928d9062e4
olivermrbl <oliver@mrbltech.com>
2021-01-19 13:56:49 +0100

    fix migration

--- packages/medusa/src/migrations/1611060475941-discount_rule_rename.ts
+++ packages/medusa/src/migrations/1611060475941-discount_rule_rename.ts
@@ -3,17 +3,11 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class discountRuleRename1611060475941 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "discount" RENAME COLUMN "discount_rule" TO "rule"`
-    )
-    await queryRunner.query(
       `ALTER TABLE "discount" RENAME COLUMN "discount_rule_id" TO "rule_id"`
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "discount" RENAME COLUMN "rule" TO "discount_rule"`
-    )
     await queryRunner.query(
       `ALTER TABLE "discount" RENAME COLUMN "rule_id" TO "discount_rule_id"`
     )
