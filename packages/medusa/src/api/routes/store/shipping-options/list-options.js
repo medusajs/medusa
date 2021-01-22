ce1edc33616aeccc868d4232bda2c1119a875597
Sebastian Rindom <skrindom@gmail.com>
2021-01-22 15:49:42 +0100

    fix: only get relevant shipping options

--- packages/medusa/src/api/routes/store/shipping-options/list-options.js
+++ packages/medusa/src/api/routes/store/shipping-options/list-options.js
@@ -1,17 +1,26 @@
 export default async (req, res) => {
+  const productIds =
+    (req.query.product_ids && req.query.product_ids.split(",")) || []
   const regionId = req.query.region_id
 
   try {
+    const productService = req.scope.resolve("productService")
     const shippingOptionService = req.scope.resolve("shippingOptionService")
 
-    const options = await shippingOptionService.list(
-      {
-        region_id: regionId,
-      },
-      {
-        relations: ["requirements"],
-      }
-    )
+    const query = {}
+
+    if (regionId) {
+      query.region_id = regionId
+    }
+
+    if (productIds.length) {
+      const prods = await productService.list({ id: productIds })
+      query.profile_id = prods.map(p => p.profile_id)
+    }
+
+    const options = await shippingOptionService.list(query, {
+      relations: ["requirements"],
+    })
 
     res.status(200).json({ shipping_options: options })
   } catch (err) {
