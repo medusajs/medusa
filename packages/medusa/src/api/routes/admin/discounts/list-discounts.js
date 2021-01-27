aa56920052f9298066a6c7ce6213a24bcddadec0
olivermrbl <oliver@mrbltech.com>
2021-01-27 09:35:32 +0100

    feat: adds discount code search functionality

--- packages/medusa/src/api/routes/admin/discounts/list-discounts.js
+++ packages/medusa/src/api/routes/admin/discounts/list-discounts.js
@@ -2,23 +2,35 @@ import { defaultFields, defaultRelations } from "./"
 
 export default async (req, res) => {
   try {
-    const selector = {}
+    const discountService = req.scope.resolve("discountService")
 
     const limit = parseInt(req.query.limit) || 20
     const offset = parseInt(req.query.offset) || 0
 
-    const discountService = req.scope.resolve("discountService")
+    let selector = {}
+
+    if ("q" in req.query) {
+      selector.q = req.query.q
+    }
+
+    if ("is_dynamic" in req.query) {
+      selector.is_dynamic = req.query.is_dynamic === "true"
+    }
 
     const listConfig = {
       select: defaultFields,
       relations: defaultRelations,
       skip: offset,
       take: limit,
+      order: { created_at: "DESC" },
     }
 
-    const discounts = await discountService.list(selector, listConfig)
+    const [discounts, count] = await discountService.listAndCount(
+      selector,
+      listConfig
+    )
 
-    res.status(200).json({ discounts })
+    res.status(200).json({ discounts, count, offset, limit })
   } catch (err) {
     throw err
   }
