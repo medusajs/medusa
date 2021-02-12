6676c8ef962e23ac72eefe5b4496b5cac12a57ab
Sebastian Rindom <skrindom@gmail.com>
2021-02-12 17:09:26 +0100

    fix: tests

--- packages/medusa/src/services/__mocks__/event-bus.js
+++ packages/medusa/src/services/__mocks__/event-bus.js
@@ -1,6 +1,9 @@
 export const EventBusServiceMock = {
   emit: jest.fn(),
   subscribe: jest.fn(),
+  withTransaction: function() {
+    return this
+  },
 }
 
 const mock = jest.fn().mockImplementation(() => {
