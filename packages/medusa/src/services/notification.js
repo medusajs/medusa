eacf54f3e60a906bb43f5978280be9bf2b9a6bba
Sebastian Rindom <skrindom@gmail.com>
2021-02-12 17:44:03 +0100

    docs: method descriptions

--- packages/medusa/src/services/notification.js
+++ packages/medusa/src/services/notification.js
@@ -31,10 +31,19 @@ class NotificationService extends BaseService {
     this.attachmentGenerator_ = null
   }
 
+  /**
+   * Registers an attachment generator to the service. The generator can be
+   * used to generate on demand invoices or other documents.
+   */
   registerAttachmentGenerator(service) {
     this.attachmentGenerator_ = service
   }
 
+  /**
+   * Sets the service's manager to a given transaction manager.
+   * @parma {EntityManager} transactionManager - the manager to use
+   * return {NotificationService} a cloned notification service
+   */
   withTransaction(transactionManager) {
     if (!transactionManager) {
       return this
@@ -50,6 +59,10 @@ class NotificationService extends BaseService {
     return cloned
   }
 
+  /**
+   * Takes a list of notification provider ids and persists them in the database.
+   * @param {Array<string>} providers - a list of provider ids
+   */
   async registerInstalledProviders(providers) {
     const { manager, notificationProviderRepository } = this.container_
     const model = manager.getCustomRepository(notificationProviderRepository)
@@ -60,6 +73,12 @@ class NotificationService extends BaseService {
     }
   }
 
+  /**
+   * Retrieves a list of notifications.
+   * @param {object} selector - the params to select the notifications by.
+   * @param {object} config - the configuration to apply to the query
+   * @return {Array<Notification>} the notifications that satisfy the query.
+   */
   async list(
     selector,
     config = { skip: 0, take: 50, order: { created_at: "DESC" } }
@@ -72,9 +91,9 @@ class NotificationService extends BaseService {
   }
 
   /**
-   * Retrieves a line item by its id.
-   * @param {string} id - the id of the line item to retrieve
-   * @return {LineItem} the line item
+   * Retrieves a notification with a given id
+   * @param {string} id - the id of the notification
+   * @return {Notification} the notification
    */
   async retrieve(id, config = {}) {
     const notiRepository = this.manager_.getCustomRepository(
@@ -96,6 +115,11 @@ class NotificationService extends BaseService {
     return notification
   }
 
+  /**
+   * Subscribes a given provider to an event.
+   * @param {string} eventName - the event to subscribe to
+   * @param {string} providerId - the provider that the event will be sent to
+   */
   subscribe(eventName, providerId) {
     if (typeof providerId !== "string") {
       throw new MedusaError(
@@ -111,6 +135,12 @@ class NotificationService extends BaseService {
     }
   }
 
+  /**
+   * Finds a provider with a given id. Will throw a NOT_FOUND error if the
+   * resolution fails.
+   * @param {string} id - the id of the provider
+   * @return {NotificationProvider} the notification provider
+   */
   retrieveProvider_(id) {
     try {
       return this.container_[`noti_${id}`]
@@ -122,6 +152,13 @@ class NotificationService extends BaseService {
     }
   }
 
+  /**
+   * Handles an event by relaying the event data to the subscribing providers.
+   * The result of the notification send will be persisted in the database in
+   * order to allow for resends. Will log any errors that are encountered.
+   * @param {string} eventName - the event to handle
+   * @param {object} data - the data the event was sent with
+   */
   handleEvent(eventName, data) {
     const subs = this.subscribers_[eventName]
     if (!subs) {
@@ -140,6 +177,14 @@ class NotificationService extends BaseService {
     )
   }
 
+  /**
+   * Sends a notification, by calling the given provider's sendNotification
+   * method. Persists the Notification in the database.
+   * @param {string} event - the name of the event
+   * @param {object} eventData - the data the event was sent with
+   * @param {string} providerId - the provider that should hande the event.
+   * @return {Notification} the created notification
+   */
   async send(event, eventData, providerId) {
     const provider = this.retrieveProvider_(providerId)
     const result = await provider.sendNotification(
@@ -175,6 +220,14 @@ class NotificationService extends BaseService {
     return notiRepo.save(created)
   }
 
+  /**
+   * Resends a notification by retrieving a prior notification and calling the
+   * underlying provider's resendNotification method.
+   * @param {string} id - the id of the notification
+   * @param {object} config - any configuration that might override the previous
+   *  send
+   * @return {Notification} the newly created notification
+   */
   async resend(id, config = {}) {
     const notification = await this.retrieve(id)
 
