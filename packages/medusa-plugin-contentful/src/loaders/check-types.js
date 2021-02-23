cf6a029b53c50bac9145da894049ed36b18e0182
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-02-23 07:49:01 +0100

    fix(medusa-plugin-contentful): Allow custom fields in plugin options (#180)

--- packages/medusa-plugin-contentful/src/loaders/check-types.js
+++ packages/medusa-plugin-contentful/src/loaders/check-types.js
@@ -19,8 +19,16 @@ const checkContentTypes = async (container) => {
   if (product && product.fields) {
     const productFields = product.fields
 
+    const customProductFields = Object.keys(
+      contentfulService.options_.custom_product_fields || {}
+    )
     const keys = Object.values(productFields).map((f) => f.id)
-    if (!requiredProductFields.every((f) => keys.includes(f))) {
+
+    const missingKeys = requiredProductFields.filter(
+      (rpf) => !keys.includes(rpf) && !customProductFields.includes(rpf)
+    )
+
+    if (missingKeys.length) {
       throw Error(
         `Contentful: Content type ${`product`} is missing some required key(s). Required: ${requiredProductFields.join(
           ", "
@@ -32,8 +40,16 @@ const checkContentTypes = async (container) => {
   if (variant && variant.fields) {
     const variantFields = variant.fields
 
+    const customVariantFields = Object.keys(
+      contentfulService.options_.custom_variant_fields || {}
+    )
     const keys = Object.values(variantFields).map((f) => f.id)
-    if (!requiredVariantFields.every((f) => keys.includes(f))) {
+
+    const missingKeys = requiredVariantFields.filter(
+      (rpf) => !keys.includes(rpf) && !customVariantFields.includes(rpf)
+    )
+
+    if (missingKeys.length) {
       throw Error(
         `Contentful: Content type ${`productVariant`} is missing some required key(s). Required: ${requiredVariantFields.join(
           ", "
@@ -47,13 +63,13 @@ const requiredProductFields = [
   "title",
   "variants",
   "options",
-  "objectId",
+  "medusaId",
   "type",
   "collection",
   "tags",
   "handle",
 ]
 
-const requiredVariantFields = ["title", "sku", "prices", "options", "objectId"]
+const requiredVariantFields = ["title", "sku", "prices", "options", "medusaId"]
 
 export default checkContentTypes
