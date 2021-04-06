2b2555004e52e97c15bfca59e030fdfc3d86ae49
Sebastian Rindom <skrindom@gmail.com>
2021-04-06 12:22:19 +0200

    fix: creates restock functionality

--- packages/medusa-plugin-restock-notification/src/subscribers/index.js
+++ packages/medusa-plugin-restock-notification/src/subscribers/index.js
@@ -1,81 +1,19 @@
 class VariantSubscriber {
-  constructor({
-    eventBusService,
-    restockNotificationService,
-    productVariantService,
-  }) {
+  constructor({ eventBusService, restockNotificationService }) {
     this.restockNotificationService_ = restockNotificationService
 
-    eventBusService.subscribe("product_variant.updated", this.handleVariantUpdate)
+    eventBusService.subscribe(
+      "product_variant.updated",
+      this.handleVariantUpdate
+    )
   }
 
-  registerClaim = async (data) => {
-    const { id } = data
-    const fromClaim = await this.claimService_.retrieve(id, {
-      relations: [
-        "order",
-        "order.payments",
-        "order.region",
-        "order.claims",
-        "order.discounts",
-        "claim_items",
-        "return_order",
-        "return_order.items",
-        "return_order.shipping_method",
-        "additional_items",
-        "shipping_address",
-        "shipping_methods",
-      ],
-    })
-
-    const fromOrder = fromClaim.order
-
-    if (fromClaim.type === "replace") {
-      await this.brightpearlService_.createClaim(fromOrder, fromClaim)
-    } else if (fromClaim.type === "refund") {
-      await this.brightpearlService_.createClaimCredit(fromOrder, fromClaim)
+  handleVariantUpdate = async (data) => {
+    const { id, fields } = data
+    if (fields.includes("inventory_quantity")) {
+      return this.restockNotificationService_.triggerRestock(id)
     }
-  }
-
-  registerShipment = async (data) => {
-    const { fulfillment_id } = data
-    const shipment = await this.fulfillmentService_.retrieve(fulfillment_id)
-    const noteId = shipment.metadata.goods_out_note
-    if (noteId) {
-      await this.brightpearlService_.registerGoodsOutTrackingNumber(
-        noteId,
-        shipment
-      )
-      await this.brightpearlService_.registerGoodsOutShipped(noteId, shipment)
-    }
-  }
-
-  registerReturn = async (data) => {
-    const { id, return_id } = data
-
-    const order = await this.orderService_.retrieve(id, {
-      relations: ["region", "payments"],
-    })
-
-    const fromReturn = await this.returnService_.retrieve(return_id, {
-      relations: ["items"],
-    })
-
-    return this.brightpearlService_
-      .createSalesCredit(order, fromReturn)
-      .catch((err) => console.log(err))
-  }
-
-  registerRefund = async (data) => {
-    const { id, refund_id } = data
-    const order = await this.orderService_.retrieve(id, {
-      relations: ["region", "payments"],
-    })
-    const refund = await this.paymentProviderService_.retrieveRefund(refund_id)
-    return this.brightpearlService_
-      .createRefundCredit(order, refund)
-      .catch((err) => console.log(err))
   }
 }
 
-export default OrderSubscriber
+export default VariantSubscriber
--- packages/medusa-plugin-sendgrid/src/subscribers/order.js
+++ packages/medusa-plugin-sendgrid/src/subscribers/order.js
@@ -1,16 +1,6 @@
 class OrderSubscriber {
-  constructor({
-    totalsService,
-    orderService,
-    sendgridService,
-    notificationService,
-    fulfillmentService,
-  }) {
-    this.orderService_ = orderService
-    this.totalsService_ = totalsService
-    this.sendgridService_ = sendgridService
+  constructor({ notificationService }) {
     this.notificationService_ = notificationService
-    this.fulfillmentService_ = fulfillmentService
 
     this.notificationService_.subscribe("order.shipment_created", "sendgrid")
     this.notificationService_.subscribe("order.gift_card_created", "sendgrid")
@@ -23,6 +13,10 @@ class OrderSubscriber {
     this.notificationService_.subscribe("swap.created", "sendgrid")
     this.notificationService_.subscribe("order.items_returned", "sendgrid")
     this.notificationService_.subscribe("order.return_requested", "sendgrid")
+    this.notificationService_.subscribe(
+      "restock_notification.restocked",
+      "sendgrid"
+    )
   }
 }
 
