c6fedeccbb1d8a04cf822a12c70a08c93baa5c85
Sebastian Rindom <skrindom@gmail.com>
2021-04-07 14:21:04 +0200

    fix: cors

--- packages/medusa-plugin-restock-notification/src/api/routes/index.js
+++ packages/medusa-plugin-restock-notification/src/api/routes/index.js
@@ -1,12 +1,24 @@
 import { Router } from "express"
 import bodyParser from "body-parser"
+import cors from "cors"
+import { getConfigFile } from "medusa-core-utils"
+
 import middlewares from "../middleware"
 
 const route = Router()
 
-export default (app) => {
+export default (app, rootDirectory) => {
   app.use("/restock-notifications", route)
 
+  const { configModule } = getConfigFile(rootDirectory, "medusa-config")
+  const { projectConfig } = configModule
+
+  const corsOptions = {
+    origin: projectConfig.store_cors.split(","),
+    credentials: true,
+  }
+
+  route.options("/variants/:variant_id", cors(corsOptions))
   route.post(
     "/variants/:variant_id",
     bodyParser.json(),
