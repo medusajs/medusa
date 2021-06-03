df359dee70675f2424ed7b61eb8acb6e79d6dac6
--list <sebastian.m.nicolajsen@gmail.com>
2021-06-03 12:07:03 +0200

    began impl. of no_notification

--- packages/medusa/src/models/swap.ts
+++ packages/medusa/src/models/swap.ts
@@ -130,6 +130,9 @@ export class Swap {
   @DeleteDateColumn({ type: "timestamptz" })
   deleted_at: Date
 
+  @Column({ type: "boolean"})
+  no_notification: Boolean
+
   @Column({ type: "jsonb", nullable: true })
   metadata: any
 
