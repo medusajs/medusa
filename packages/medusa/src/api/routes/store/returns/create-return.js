2ecdb90a7b6806f5b8ec444b76643cb27f796c9f
--list <sebastian.m.nicolajsen@gmail.com>
2021-06-03 12:07:03 +0200

    began impl. of no_notification

--- packages/medusa/src/api/routes/store/returns/create-return.js
+++ packages/medusa/src/api/routes/store/returns/create-return.js
@@ -31,9 +31,6 @@ import { MedusaError, Validator } from "medusa-core-utils"
  *               option_id:
  *                 type: string
  *                 description: The id of the Shipping Option to create the Shipping Method from.
- *           no_notification:
- *             description: If set to true no notification will be send
- *             type: boolean
  *             
  * tags:
  *   - Return
@@ -62,8 +59,7 @@ export default async (req, res) => {
       .keys({
         option_id: Validator.string().optional(),
       })
-      .optional(),
-    no_notification: Validator.boolean().optional()
+      .optional()
   })
 
   const { value, error } = schema.validate(req.body)
@@ -136,8 +132,7 @@ export default async (req, res) => {
                 .withTransaction(manager)
                 .emit("order.return_requested", {
                   id: value.order_id,
-                  return_id: createdReturn.id,
-                  no_notification: no_notification
+                  return_id: createdReturn.id
                 })
 
               return {
