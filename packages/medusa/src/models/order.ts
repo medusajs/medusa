03644b6ebf7c7c45ec9bfbe0d537370fb3497007
--list <sebastian.m.nicolajsen@gmail.com>
2021-06-03 12:07:03 +0200

    began impl. of no_notification

--- packages/medusa/src/models/order.ts
+++ packages/medusa/src/models/order.ts
@@ -245,6 +245,9 @@ export class Order {
   @Column({ type: "jsonb", nullable: true })
   metadata: any
 
+  @Column({ type: "boolean"})
+  no_notification: Boolean
+
   @Column({ nullable: true })
   idempotency_key: string
 
