afaf601c931c57ff86830ae3979f68d3f97c09e4
--list <sebastian.m.nicolajsen@gmail.com>
2021-06-17 08:39:03 +0200

    removed conflict

--- packages/medusa/src/services/order.js
+++ packages/medusa/src/services/order.js
@@ -1136,11 +1136,7 @@ class OrderService extends BaseService {
 
       const result = await this.retrieve(orderId)
 
-<<<<<<< HEAD
-      const evaluatedNoNotification = noNotification !== undefined && noNotification !== null ? noNotification : order.no_notification
-=======
       const evaluatedNoNotification = noNotification !== undefined ? noNotification : order.no_notification
->>>>>>> 04fe5292f7e9dcd14cb1a4ea17db8978f9b52c03
 
       this.eventBus_.emit(OrderService.Events.REFUND_CREATED, {
         id: result.id,
