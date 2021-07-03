fb045f5cbdaa82017ed556bfc512744ca61abff7
--list <sebastian.m.nicolajsen@gmail.com>
2021-07-03 12:18:08 +0200

    began impl. tests

--- packages/medusa/src/services/__tests__/gift-card.js
+++ packages/medusa/src/services/__tests__/gift-card.js
@@ -1,40 +1,101 @@
-import { Identifier, MockManager, MockRepository } from "@babel/types";
-import { describe, fail } from "yargs";
+import { IdMap, MockManager, MockRepository } from "medusa-test-utils"
+import idMap from "medusa-test-utils/dist/id-map"
+import mockRepository from "medusa-test-utils/dist/mock-repository"
+
 import GiftCardService from "../gift-card"
 
 describe("GiftCardService", () => {
-  
-    const eventBusService = {
-        emit: jest.fn(),
-        withTransaction: function(){
-            return this
-        },
+  const eventBusService = {
+    emit: jest.fn(),
+    withTransaction: function() {
+      return this
+    },
+  }
+
+  describe("create", () => {
+    const giftCardRepo = mockRepository({
+      create: s => {
+        return Promise.resolve(s)
+      },
+      save: s => {
+        return Promise.resolve(s)
+      },
+    })
+
+    const regionService = {
+      withTransaction: function() {
+        return this
+      },
+      retrieve: () => {
+        return Promise.resolve({
+          id: IdMap.getId("region-id"),
+        })
+      },
     }
 
-    describe("list", async () => {  
-      fail("create list tests")  
+    const giftCardService = new GiftCardService({
+      manager: MockManager,
+      giftCardRepository: giftCardRepo,
+      regionService: regionService,
+      eventBusService: eventBusService,
     })
 
-    describe("create", async () => {
-        fail("create create tests")
+    const giftCard = {
+      region_id: IdMap.getId("region-id"),
+      order_id: "order-id",
+      is_disabled: true,
+    }
+
+    beforeEach(async () => {
+      jest.clearAllMocks()
     })
 
-    describe("retrieve", async () => {
-        fail("create retrieve tests")
+    it("correctly creates a giftcard", async () => {
+      await giftCardService.create(giftCard)
+
+      expect(giftCardRepo.create).toHaveBeenCalledTimes(1)
+      expect(giftCardRepo.create).toHaveBeenCalledWith({
+        region_id: IdMap.getId("region-id"),
+        order_id: "order-id",
+        is_disabled: true,
+        code: expect.any(String),
+      })
     })
 
-    describe("retrieveByCode", async () => {
-        fail("create retrieveByCode tests")
+    it("fails to create giftcard if no region is provided", async () => {
+      const card = {
+        ...giftCard,
+      }
+
+      card.region_id = undefined
+
+      await expect(giftCardService.create(card)).rejects.toThrow(
+        "Gift card is missing region_id"
+      )
     })
+  })
 
-    describe("update", async () => {
-        fail("create update tests")
+  describe("retrieve", () => {
+    it("test1", async () => {
+      fail("impl")
     })
+  })
 
-    describe("delete", async () => {
-        fail("create delete tests")
+  describe("retrieveByCode", () => {
+    it("test1", async () => {
+      fail("impl")
     })
+  })
 
+  describe("update", () => {
+    it("test1", async () => {
+      fail("impl")
+    })
+  })
 
-    
+  describe("delete", () => {
+    it("test1", async () => {
+      fail("impl")
+    })
+  })
 })
