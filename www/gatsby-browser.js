836cec0ed5b69bc8f998d5986cd3973325ee65d2
Vadim Smirnov <smirnou.vadzim@gmail.com>
2021-07-09 12:32:03 +0300

    Add site reload if updated

--- www/gatsby-browser.js
+++ www/gatsby-browser.js
@@ -2,6 +2,16 @@ import React from "react"
 import { ThemeProvider as Provider } from "./src/theme"
 import { Global } from "@emotion/react"
 
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    "We have updated the docs site. Reload to display the latest version?"
+  )
+
+  if (answer) {
+    window.location.reload()
+  }
+}
+
 export const wrapPageElement = ({ element }) => {
   return (
     <>
