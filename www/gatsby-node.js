a6b3adb2e3398a5aac07125c63d1511fba5b3824
zakariasaad <zakaria.elas@gmail.com>
2021-07-12 19:16:31 +0100

    add: theme-ui

--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -3,9 +3,10 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({ node, getNode })
+
+    console.log({ slug })
 
     createNodeField({
       node,
