ccaf01a6fed5dc4054402ff48bbeb55bbc4cfe96
Vilfred Sikker <vilfredsikker@gmail.com>
2021-07-16 16:47:07 +0200

    cleanup request body and respons data. Also added fragments

--- www/reference/src/utils/fragments.js
+++ www/reference/src/utils/fragments.js
@@ -34,23 +34,80 @@ export const method = graphql`
     method
     operationId
     responses {
-      description
-      status
+      ...response
     }
     requestBody {
-      content {
-        application_json {
-          schema {
-            type
-          }
-        }
-      }
+      ...requestBody
     }
+
     parameters {
       description
       in
       name
       required
+      schema {
+        type
+      }
+    }
+  }
+`
+
+export const response = graphql`
+  fragment response on ApiNodeSectionsSectionPathsMethodsResponses {
+    status
+    description
+    content {
+      _ref
+      type
+      property
+      description
+      items {
+        type
+        _ref
+      }
+    }
+  }
+`
+
+export const requestBody = graphql`
+  fragment requestBody on ApiNodeSectionsSectionPathsMethodsRequestBody {
+    type
+    required
+    properties {
+      description
+      enum
+      format
+      oneOf {
+        _ref
+      }
+      property
+      type
+      items {
+        type
+        properties {
+          type
+          property
+          enum
+          description
+          items {
+            type
+          }
+        }
+      }
+      properties {
+        price {
+          type
+          description
+        }
+        option_id {
+          type
+          description
+        }
+        value {
+          type
+          description
+        }
+      }
     }
   }
 `
