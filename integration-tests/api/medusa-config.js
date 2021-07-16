1fa05b94032e0899b5f561ee9e55bd2e38d93109
zakariasaad <zakaria.elas@gmail.com>
2021-07-16 13:40:05 +0100

    add: read db credentials from .env

--- integration-tests/api/medusa-config.js
+++ integration-tests/api/medusa-config.js
@@ -1,8 +1,13 @@
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+console.log(DB_USERNAME, DB_PASSWORD);
+
 module.exports = {
   plugins: [],
   projectConfig: {
     // redis_url: REDIS_URL,
-    database_url: "postgres://localhost/medusa-integration",
+    database_url: `postgres://${DB_USERNAME}:${DB_PASSWORD}@localhost/medusa-integration`,
     database_type: "postgres",
   },
 };
