e3d1502f8f480a0be9ca0197ac35f93749813c40
Vadim Smirnov <smirnou.vadzim@gmail.com>
2021-07-16 10:53:40 +0300

    API data pull (WIP)

--- www/reference/gatsby-node.js
+++ www/reference/gatsby-node.js
@@ -1,158 +1,65 @@
 const path = require(`path`)
+const fs = require("fs")
+const util = require("util")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const adminDoc = require("../../docs/api/admin-spec3.json")
 const storeFront = require("../../docs/api/store-spec3.json")
-const rawSpec = require("../../docs/api/admin-spec3.json")
-const convertToKebabCase = string => {
-  return string.replace(/\s+/g, "-").toLowerCase()
-}
-const memo = {}
-const followRef = ref => {
-  if (ref in memo) {
-    return memo[ref]
-  }
-  const [, ...path] = ref.split("/")
-  let cleanDets = rawSpec
-  for (const part of path) {
-    if (part === "#") continue
-    cleanDets = cleanDets[part]
-  }
-  memo[ref] = cleanDets
-  return cleanDets
-}
-const dereffed = {}
-const derefSchema = (schema, spec) => {
-  if (!schema) {
-    return {}
-  }
-  if ("x-resourceId" in schema && schema["x-resourceId"] in dereffed) {
-    return dereffed[schema["x-resourceId"]]
-  }
-  const dereffedProps = {}
-  if (schema.properties) {
-    for (let [key, details] of Object.entries(schema.properties)) {
-      if (details.type === "array") {
-        details.items = derefSchema(details.items, spec)
-      }
-      if (details.anyOf) {
-        details.anyOf = details.anyOf.map(ao => derefSchema(ao, spec))
-      }
-      dereffedProps[key] = details
-    }
-  }
-  if (schema.$ref) {
-    return followRef(schema.$ref)
-  }
-  schema.properties = dereffedProps
-  return schema
-}
-const createTagNode = (tag, createContentDigest) => {
-  const resourceId = tag["x-resourceId"]
-  const schema = rawSpec.components.schemas[tag["x-resourceId"]]
-  return Object.assign(
-    {},
-    {
-      id: convertToKebabCase(tag.name),
-      title: tag.name,
-      resourceId,
-      schema: JSON.stringify(derefSchema(schema)),
-      parent: null,
-      children: [],
-      internal: {
-        type: "APITag",
-        content: JSON.stringify(tag),
-        contentDigest: createContentDigest(JSON.stringify(tag)),
-      },
-    }
-  )
-}
-const createEndpointNode = (endpoint, createContentDigest) => {
-  const tagKey = endpoint.tags[0]
-  return Object.assign(
-    {},
-    {
-      id: endpoint.operationId,
-      method: endpoint.method,
-      path: endpoint.path,
-      summary: endpoint.summary,
-      description: endpoint.description,
-      parent: convertToKebabCase(tagKey),
-      internal: {
-        type: "APIEndpoint",
-        content: JSON.stringify(endpoint),
-        contentDigest: createContentDigest(JSON.stringify(endpoint)),
-      },
-    }
-  )
-}
-exports.sourceNodes = async ({ actions, createContentDigest }) => {
-  const { createNode } = actions
-  for (const tag of rawSpec.tags) {
-    const tagNode = createTagNode(tag, createContentDigest)
-    createNode(tagNode)
-  }
-  for (const [path, methods] of Object.entries(rawSpec.paths)) {
+
+const useSpec = raw => {
+  let tags = {}
+
+  for (const [path, methods] of Object.entries(raw.paths)) {
     for (const [method, specification] of Object.entries(methods)) {
-      const endpointNode = createEndpointNode(
-        {
-          method: method.toUpperCase(),
-          path,
-          ...specification,
-        },
-        createContentDigest
-      )
-      createNode(endpointNode)
-    }
-  }
-}
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
-
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-            }
+      for (const t of specification.tags) {
+        if (t in tags) {
+          tags = {
+            ...tags,
+            [t]: [
+              ...tags[t],
+              {
+                method: method.toUpperCase(),
+                path,
+                ...specification,
+              },
+            ],
+          }
+        } else {
+          tags = {
+            ...tags,
+            [t]: [
+              {
+                method: method.toUpperCase(),
+                path,
+                ...specification,
+              },
+            ],
           }
         }
       }
     }
-  `)
+  }
 
-  const posts = result.data.allMarkdownRemark.edges
-
-  posts.forEach(({ node }, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/docs.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-        previous,
-        next,
-      },
-    })
-  })
+  return { tags, spec: raw }
 }
+
+fs.writeFile(
+  "./data/admin-api.json",
+  JSON.stringify(useSpec(adminDoc)),
+  function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log("THE JSON FILE WAS CREATED!!!!")
+  }
+)
+
+fs.writeFile(
+  "./data/storefront-api.json",
+  JSON.stringify(useSpec(storeFront)),
+  function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log("THE JSON FILE WAS CREATED!!!!")
+  }
+)
--- www/reference/src/pages/index.js
+++ www/reference/src/pages/index.js
@@ -1,23 +1,51 @@
 import React from "react"
 import { Flex } from "rebass"
 import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SideBar from "../components/sidebar"
 import DocsReader from "../components/docs-reader"
 
-import AdminApi from "../../data/admin-api.json"
+import Storefront from "../../data/storefront-api.json"
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
+  console.log(Storefront.tags)
   return (
     <Layout>
       <Helmet>
         <title>API Docs | Medusa Commerce</title>
       </Helmet>
       <Flex>
-        <SideBar tags={AdminApi.tags} />
-        <DocsReader tags={AdminApi.tags} spec={AdminApi.spec} />
+        <SideBar tags={Storefront.tags} />
+        <DocsReader tags={Storefront.tags} spec={Storefront.spec} />
       </Flex>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query MyQuery {
+    allFile(
+      filter: {
+        name: { eq: "store-spec3" }
+        internal: { mediaType: { eq: "application/json" } }
+      }
+    ) {
+      nodes {
+        name
+        sourceInstanceName
+        internal {
+          type
+          mediaType
+        }
+        childApiJson {
+          tags {
+            name
+          }
+        }
+      }
+    }
+  }
+`
