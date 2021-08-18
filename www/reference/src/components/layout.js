1abaf6b0295d24c9fcaae094c888bd21bac5cd23
Kasper <kasper@medusa-commerce.com>
2021-08-18 15:16:37 +0200

    fix: cleaned up layout of unused code

--- www/reference/src/components/layout.js
+++ www/reference/src/components/layout.js
@@ -3,31 +3,8 @@ import { Flex, Box } from "theme-ui"
 import Sidebar from "./sidebar"
 
 const Layout = ({ data, api, children }) => {
-  // const [visible, setVisible] = useState(false)
-
-  //SEARCH ON CMD/CTRL + F
-  // useEffect(() => {
-  //   function handleKeyPress(e) {
-  //     if (!visible && e.metaKey && e.key === "f") {
-  //       e.preventDefault()
-  //       setVisible(true)
-  //     } else if (visible && e.metaKey && e.key === "f") {
-  //       e.preventDefault()
-  //       setVisible(false)
-  //     }
-  //   }
-  //   window.addEventListener("keydown", handleKeyPress)
-  //   return () => window.removeEventListener("keydown", handleKeyPress)
-  // }, [visible])
-
   return (
     <Flex sx={{ p: "0", m: "0", overflow: "hidden" }}>
-      {/* IMPLEMENT: Algolia Docsearch */}
-      {/* <Flex
-        sx={{
-          display: visible ? "flex" : "none",
-        }}
-      ></Flex> */}
       <Flex
         sx={{
           position: "absolute",
@@ -35,7 +12,6 @@ const Layout = ({ data, api, children }) => {
           left: 0,
           right: 0,
           bottom: 0,
-          overflowY: "scroll",
           fontFamily: "body",
           flexGrow: "1",
         }}
