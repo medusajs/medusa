9ed259bb4ddecf4bc69e264936345588f39b50a8
olivermrbl <oliver@mrbltech.com>
2021-08-20 07:50:49 +0200

    add babel optional chaining

--- www/reference/gatsby-node.js
+++ www/reference/gatsby-node.js
@@ -551,3 +551,9 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateBabelConfig = ({ actions }) => {
+  actions.setBabelPlugin({
+    name: require.resolve("@babel/plugin-proposal-optional-chaining"),
+  })
+}
