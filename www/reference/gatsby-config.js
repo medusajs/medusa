17230e1332523385c03c569c87ab97fdb6d245ba
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2021-08-20 10:26:29 +0200

    feat: Update to API references look and feel (#343)
    Co-authored-by: Vadim Smirnov <smirnou.vadzim@gmail.com>
    Co-authored-by: zakariasaad <zakaria.elas@gmail.com>
    Co-authored-by: Vilfred Sikker <vilfredsikker@gmail.com>
    Co-authored-by: olivermrbl <oliver@mrbltech.com>

---
+++ www/reference/gatsby-config.js
@@ -0,0 +1,86 @@
+module.exports = {
+  siteMetadata: {
+    title: "Medusa",
+    description: "Open-source headless commerce engine",
+    author: "Medusa core team",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-anchor-links`,
+      options: {
+        offset: -100,
+        duration: 1000,
+      },
+    },
+    `gatsby-transformer-json`,
+    `gatsby-plugin-emotion`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "store-api",
+        path: `${__dirname}/../../docs/api/store-spec3.json`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "admin-api",
+        path: `${__dirname}/../../docs/api/admin-spec3.json`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `docs`,
+        path: `${__dirname}/../../docs/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/../../docs/pages`,
+        name: `pages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              elements: [`h2`, `h3`, `h4`],
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-theme-ui`,
+    {
+      resolve: `gatsby-plugin-algolia-docsearch`,
+      options: {
+        apiKey: process.env.ALGOLIA_DOCSEARCH_API_KEY, // required
+        indexName: process.env.ALGOLIA_DOCSEARCH_INDEX_NAME, // required
+        inputSelector: "#algolia-doc-search", // required
+        debug: false, // (bool) Optional. Default `false`
+      },
+    },
+    // `gatsby-plugin-preact`,
+    // {
+    //   resolve: `gatsby-source-openapi-aggregate`,
+    //   options: {
+    //     specs: [
+    //       {
+    //         name: "admin-spec",
+    //         resolve: () =>
+    //           fromJson(
+    //             path.resolve(__dirname, "../../docs/api/admin-spec3.json")
+    //           ),
+    //       },
+    //     ],
+    //   },
+    // },
+  ],
+}
