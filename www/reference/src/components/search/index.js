485c065ba4acba8a26087cfcbeca11dd7f8997aa
Kasper <kasper@medusa-commerce.com>
2021-08-24 11:07:21 +0200

    added docsearch

---
+++ www/reference/src/components/search/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { DocSearch } from "@docsearch/react"
+
+const Search = () => {
+  return (
+    <DocSearch
+      apiKey="25626fae796133dc1e734c6bcaaeac3c"
+      indexName="docsearch"
+    />
+  )
+}
+
+export default Search
