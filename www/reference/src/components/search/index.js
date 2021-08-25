aee10dd69c058711f3851ecbd6844875b440c1e5
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2021-08-25 15:51:00 +0200

    feat: Enable API search (#353)
    * enabled search in ref and changed from section -> main so Algolia will index ref content
    
    * fix: algolia apu key

--- www/reference/src/components/search/index.js
+++ www/reference/src/components/search/index.js
@@ -2,12 +2,10 @@ import React from "react"
 import { DocSearch } from "@docsearch/react"
 import "../../medusa-plugin-themes/docsearch/theme.css"
 
-const algoliaApiKey =
-  process.env.ALGOLIA_API_KEY || "25626fae796133dc1e734c6bcaaeac3c" //second opt is for testing purposes
-const algoliaIndexName = process.env.ALGOLIA_API_KEY || "docsearch"
+const algoliaApiKey = process.env.ALGOLIA_API_KEY || "temp"
 
 const Search = () => {
-  return <DocSearch apiKey={algoliaApiKey} indexName={algoliaIndexName} />
+  return <DocSearch apiKey={algoliaApiKey} indexName="medusa-commerce" />
 }
 
 export default Search
