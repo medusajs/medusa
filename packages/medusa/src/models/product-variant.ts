ab02634ee91966a5685650c3892458aa82e4526c
olivermrbl <oliver@mrbltech.com>
2021-09-05 21:39:00 +0200

    adds money amount migration

--- packages/medusa/src/models/product-variant.ts
+++ packages/medusa/src/models/product-variant.ts
@@ -43,7 +43,7 @@ export class ProductVariant {
   @OneToMany(
     () => MoneyAmount,
     ma => ma.variant,
-    { cascade: ["remove"] }
+    { cascade: true, onDelete: "CASCADE" }
   )
   prices: MoneyAmount[]
 
