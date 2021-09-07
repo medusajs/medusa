fbccf3423169f488e12d1d7296351b6197adfb3e
--list <sebastian.m.nicolajsen@gmail.com>
2021-09-07 15:17:06 +0200

    ran integration tests

--- integration-tests/api/medusa-config.js
+++ integration-tests/api/medusa-config.js
@@ -1,5 +1,5 @@
-const DB_USERNAME = process.env.DB_USERNAME || "postgres";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_USERNAME = process.env.DB_USERNAME || "postgres"
+const DB_PASSWORD = process.env.DB_PASSWORD || ""
 
 module.exports = {
   plugins: [],
@@ -8,4 +8,4 @@ module.exports = {
     database_url: `postgres://${DB_USERNAME}:${DB_PASSWORD}@localhost/medusa-integration`,
     database_type: "postgres",
   },
-};
+}
