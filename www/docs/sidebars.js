2af746faa9f878a9245c98ae8f49f85799e382fb
Vadim Smirnov <62517920+FuzzyReason@users.noreply.github.com>
2021-09-07 18:24:35 +0300

    change target branch (#375)

--- www/docs/sidebars.js
+++ www/docs/sidebars.js
@@ -71,6 +71,10 @@ module.exports = {
             },
           ],
         },
+        {
+          type: "doc",
+          id: "how-to/setting-up-a-nextjs-storefront-for-your-medusa-project",
+        },
       ],
     },
   ],
