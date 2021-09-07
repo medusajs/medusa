427beb30fcfd410f165cb82fbddda98da0c8bdb0
Sebastian Mateos Nicolajsen <80953876+sebastiannicolajsen@users.noreply.github.com>
2021-09-07 11:15:43 +0200

    Fix/create customer in draft order (#285)
    * draft order now creates customer when creating cart
    
    * corrected to update instead of create
    
    * corrected tests
    
    * added creation of customer to create of cart
    
    * removed update function from mock
    
    * added test
    
    * corrected update to update email properly
    
    * corrected tests
    
    * removed unused func
    
    * updated core properly
    
    * removed manual-payment service

--- packages/medusa/src/services/__tests__/cart.js
+++ packages/medusa/src/services/__tests__/cart.js
@@ -202,11 +202,23 @@ describe("CartService", () => {
 
     const addressRepository = MockRepository({ create: c => c })
     const cartRepository = MockRepository()
+    const customerService = {
+      retrieveByEmail: jest.fn().mockReturnValue(
+        Promise.resolve({
+          id: IdMap.getId("customer"),
+          email: "email@test.com",
+        })
+      ),
+      withTransaction: function() {
+        return this
+      },
+    }
     const cartService = new CartService({
       manager: MockManager,
       addressRepository,
       totalsService,
       cartRepository,
+      customerService,
       regionService,
       eventBusService,
     })
@@ -218,6 +230,7 @@ describe("CartService", () => {
     it("successfully creates a cart", async () => {
       await cartService.create({
         region_id: IdMap.getId("testRegion"),
+        email: "email@test.com",
       })
 
       expect(eventBusService.emit).toHaveBeenCalledTimes(1)
@@ -237,6 +250,9 @@ describe("CartService", () => {
         shipping_address: {
           country_code: "us",
         },
+        customer_id: IdMap.getId("customer"),
+        email: "email@test.com",
+        customer: expect.any(Object),
       })
 
       expect(cartRepository.save).toHaveBeenCalledTimes(1)
@@ -734,11 +750,17 @@ describe("CartService", () => {
         if (email === "no@mail.com") {
           return Promise.reject()
         }
-        return Promise.resolve({ id: IdMap.getId("existing") })
+        return Promise.resolve({
+          id: IdMap.getId("existing"),
+          email,
+        })
       }),
-      create: jest
-        .fn()
-        .mockReturnValue(Promise.resolve({ id: IdMap.getId("newCus") })),
+      create: jest.fn().mockImplementation(data =>
+        Promise.resolve({
+          id: IdMap.getId("newCus"),
+          email: data.email,
+        })
+      ),
       withTransaction: function() {
         return this
       },
@@ -774,6 +796,7 @@ describe("CartService", () => {
         customer_id: IdMap.getId("existing"),
         customer: {
           id: IdMap.getId("existing"),
+          email: "test@testdom.com",
         },
         email: "test@testdom.com",
         discount_total: 0,
@@ -798,7 +821,7 @@ describe("CartService", () => {
       expect(cartRepository.save).toHaveBeenCalledTimes(1)
       expect(cartRepository.save).toHaveBeenCalledWith({
         customer_id: IdMap.getId("newCus"),
-        customer: { id: IdMap.getId("newCus") },
+        customer: { id: IdMap.getId("newCus"), email: "no@mail.com" },
         email: "no@mail.com",
         discount_total: 0,
         shipping_total: 0,
