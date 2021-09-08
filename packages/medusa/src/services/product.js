545eeca64bb2c854237c52211a8c62179023e513
pKorsholm <philip.korsholm@hotmail.com>
2021-09-08 14:14:28 +0200

    moved variant sorting from service to the repository

--- packages/medusa/src/services/product.js
+++ packages/medusa/src/services/product.js
@@ -185,12 +185,6 @@ class ProductService extends BaseService {
       )
     }
 
-    if (product.variants) {
-      product.variants.sort(
-        (variant1, variant2) => variant1.variant_rank - variant2.variant_rank
-      )
-    }
-
     return product
   }
 
@@ -298,10 +292,6 @@ class ProductService extends BaseService {
       if (rest.is_giftcard) {
         rest.discountable = false
       }
-
-      if (rest.variants)
-        for (const [i, variant] of rest.variants.entries())
-          variant.variant_rank = i
 
       let product = productRepo.create(rest)
 
