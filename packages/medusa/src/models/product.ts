27150959ffa70819cf3c4ad176288a1c70287942
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-09-10 16:03:44 +0200

    fix: Ensure uniqueness for products, variants, collections and discounts (#382)

--- packages/medusa/src/models/product.ts
+++ packages/medusa/src/models/product.ts
@@ -40,7 +40,7 @@ export class Product {
   @Column({ nullable: true })
   description: string
 
-  @Index({ unique: true, where: "deleted_at IS NOT NULL" })
+  @Index({ unique: true, where: "deleted_at IS NULL" })
   @Column({ nullable: true })
   handle: string
 
