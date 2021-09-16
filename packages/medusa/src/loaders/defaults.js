8095deb7e630fc4a9a8c057f15ee2618cd82f74f
zakariaelas <zakaria.elas@gmail.com>
2021-09-16 16:49:49 +0100

    fix: meiliesearch README.md + remove: searchService from silentResolver

--- packages/medusa/src/loaders/defaults.js
+++ packages/medusa/src/loaders/defaults.js
@@ -20,9 +20,6 @@ const silentResolution = (container, name, logger) => {
         case "fulfillmentProviders":
           identifier = "fulfillment"
           break
-        case "searchService":
-          identifier = "search"
-          break
         default:
           identifier = name
       }
