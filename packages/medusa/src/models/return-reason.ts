01eade0ed50bdb8bfbcdc441c595909de31d96bc
pKorsholm <philip.korsholm@hotmail.com>
2021-09-17 12:15:26 +0200

    add nested return reasons to database

--- packages/medusa/src/models/return-reason.ts
+++ packages/medusa/src/models/return-reason.ts
@@ -7,6 +7,8 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   PrimaryColumn,
+  ManyToOne, 
+  JoinColumn
 } from "typeorm"
 import { ulid } from "ulid"
 import { resolveDbType, DbAwareColumn } from "../utils/db-aware-column"
@@ -25,6 +27,13 @@ export class ReturnReason {
 
   @Column({ nullable: true })
   description: string
+
+  @Column({ nullable: true })
+  parent_return_reason_id: string
+
+  @ManyToOne(() => ReturnReason)
+  @JoinColumn({ name: "parent_return_reason_id" })
+  parent_return_reason: ReturnReason
 
   @CreateDateColumn({ type: resolveDbType("timestamptz") })
   created_at: Date
