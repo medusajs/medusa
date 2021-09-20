7195916e493df76722dd769933497812e3d67d58
pKorsholm <philip.korsholm@hotmail.com>
2021-09-20 15:38:46 +0200

    nested return reasons migration

---
+++ packages/medusa/src/migrations/1631875269665-return_reasons_with_children.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class returnReasonsWithChildren1631875269665 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
