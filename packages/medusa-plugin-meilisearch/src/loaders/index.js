820d6c529fe06c322f9f6a730216a3a29c00751d
zakariaelas <zakaria.elas@gmail.com>
2021-09-21 13:14:08 +0100

    more adjustments
    move indexing logic to plugin
    remove loadToSearchEngine from ProductService
    adjust medusa core accordingly

--- packages/medusa-plugin-meilisearch/src/loaders/index.js
+++ packages/medusa-plugin-meilisearch/src/loaders/index.js
@@ -1,4 +1,53 @@
+import { transformProduct } from "../utils/transform-products"
+
 const INDEX_NS = "medusa-commerce"
+
+async function loadIntoSearchEngine(container) {
+  const meilisearchService = container.resolve("meilisearchService")
+  const productService = container.resolve("productService")
+
+  const TAKE = 20
+  const totalCount = await productService.count()
+  let iterCount = 0,
+    lastSeenId = "prod"
+
+  while (iterCount < totalCount) {
+    const products = await productService.list(
+      { id: { gt: lastSeenId } },
+      {
+        select: [
+          "id",
+          "title",
+          "subtitle",
+          "description",
+          "handle",
+          "is_giftcard",
+          "discountable",
+          "thumbnail",
+          "profile_id",
+          "collection_id",
+          "type_id",
+          "origin_country",
+          "created_at",
+          "updated_at",
+        ],
+        relations: ["variants", "tags", "type", "collection"],
+        take: TAKE,
+        order: { id: "ASC" },
+      }
+    )
+
+    const transformedProducts = products.map(transformProduct)
+
+    await meilisearchService.addDocuments(
+      `medusa-commerce_products`,
+      transformedProducts
+    )
+
+    iterCount += products.length
+    lastSeenId = products.at(-1).id
+  }
+}
 
 export default async (container, options) => {
   try {
@@ -9,6 +58,8 @@ export default async (container, options) => {
         meilisearchService.updateSettings(`${INDEX_NS}_${key}`, value)
       )
     )
+
+    await loadIntoSearchEngine(container)
   } catch (err) {
     console.log(err)
   }
