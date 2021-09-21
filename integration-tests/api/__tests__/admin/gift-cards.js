fb6a2278a8b4aa88931a75c93749b940aacdca40
Zakaria El Asri <33696020+zakariaelas@users.noreply.github.com>
2021-09-21 10:15:33 +0100

    fix: ILIKE operator not supported in sqlite (#393)
    * fix: replace ILIKE operator with ILike function where possible
    
    * remove: unused import
    
    * fix: remove alias from test case as it is not needed
    
    * fix: product variant query
    
    * add: integration tests + fallback to original query for all queries searching on the display_id field
    
    * remove: console.log

--- integration-tests/api/__tests__/admin/gift-cards.js
+++ integration-tests/api/__tests__/admin/gift-cards.js
@@ -1,54 +1,162 @@
-const path = require("path");
-const { Region } = require("@medusajs/medusa");
+const path = require("path")
+const { Region, GiftCard } = require("@medusajs/medusa")
 
-const setupServer = require("../../../helpers/setup-server");
-const { useApi } = require("../../../helpers/use-api");
-const { initDb, useDb } = require("../../../helpers/use-db");
-const adminSeeder = require("../../helpers/admin-seeder");
+const setupServer = require("../../../helpers/setup-server")
+const { useApi } = require("../../../helpers/use-api")
+const { initDb, useDb } = require("../../../helpers/use-db")
+const adminSeeder = require("../../helpers/admin-seeder")
 
-jest.setTimeout(30000);
+jest.setTimeout(30000)
 
 describe("/admin/gift-cards", () => {
-  let medusaProcess;
-  let dbConnection;
+  let medusaProcess
+  let dbConnection
 
   beforeAll(async () => {
-    const cwd = path.resolve(path.join(__dirname, "..", ".."));
-    dbConnection = await initDb({ cwd });
-    medusaProcess = await setupServer({ cwd });
-  });
+    const cwd = path.resolve(path.join(__dirname, "..", ".."))
+    dbConnection = await initDb({ cwd })
+    medusaProcess = await setupServer({ cwd })
+  })
 
   afterAll(async () => {
-    const db = useDb();
-    await db.shutdown();
+    const db = useDb()
+    await db.shutdown()
 
-    medusaProcess.kill();
-  });
+    medusaProcess.kill()
+  })
+
+  describe("GET /admin/gift-cards", () => {
+    beforeEach(async () => {
+      const manager = dbConnection.manager
+      try {
+        await adminSeeder(dbConnection)
+        await manager.insert(Region, {
+          id: "test-region",
+          name: "Test Region",
+          currency_code: "usd",
+          tax_rate: 0,
+        })
+        await manager.insert(GiftCard, {
+          id: "gift_test",
+          code: "GC_TEST",
+          value: 20000,
+          balance: 20000,
+          region_id: "test-region",
+        })
+        await manager.insert(GiftCard, {
+          id: "another_gift_test",
+          code: "CARD_TEST",
+          value: 200000,
+          balance: 200000,
+          region_id: "test-region",
+        })
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    })
+
+    afterEach(async () => {
+      const db = useDb()
+      await db.teardown()
+    })
+
+    it("lists gift cards and query count", async () => {
+      const api = useApi()
+
+      const response = await api
+        .get("/admin/gift-cards", {
+          headers: {
+            Authorization: "Bearer test_token",
+          },
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+
+      expect(response.status).toEqual(200)
+      expect(response.data.gift_cards).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: "gift_test",
+            code: "GC_TEST",
+          }),
+          expect.objectContaining({
+            id: "another_gift_test",
+            code: "CARD_TEST",
+          }),
+        ])
+      )
+    })
+
+    it("lists gift cards with specific query", async () => {
+      const api = useApi()
+
+      const response = await api
+        .get("/admin/gift-cards?q=gc", {
+          headers: {
+            Authorization: "Bearer test_token",
+          },
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+
+      expect(response.status).toEqual(200)
+      expect(response.data.gift_cards.length).toEqual(1)
+      expect(response.data.gift_cards).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: "gift_test",
+            code: "GC_TEST",
+          }),
+        ])
+      )
+    })
+
+    it("lists no gift cards on query for non-existing gift card code", async () => {
+      const api = useApi()
+
+      const response = await api
+        .get("/admin/gift-cards?q=bla", {
+          headers: {
+            Authorization: "Bearer test_token",
+          },
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+
+      expect(response.status).toEqual(200)
+      expect(response.data.gift_cards.length).toEqual(0)
+      expect(response.data.gift_cards).toEqual([])
+    })
+  })
 
   describe("POST /admin/gift-cards", () => {
     beforeEach(async () => {
-      const manager = dbConnection.manager;
+      const manager = dbConnection.manager
       try {
-        await adminSeeder(dbConnection);
+        await adminSeeder(dbConnection)
         await manager.insert(Region, {
           id: "region",
           name: "Test Region",
           currency_code: "usd",
           tax_rate: 0,
-        });
+        })
       } catch (err) {
-        console.log(err);
-        throw err;
+        console.log(err)
+        throw err
       }
-    });
+    })
 
     afterEach(async () => {
-      const db = useDb();
-      await db.teardown();
-    });
+      const db = useDb()
+      await db.teardown()
+    })
 
     it("creates a gift card", async () => {
-      const api = useApi();
+      const api = useApi()
 
       const response = await api
         .post(
@@ -64,13 +172,13 @@ describe("/admin/gift-cards", () => {
           }
         )
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
 
-      expect(response.status).toEqual(200);
-      expect(response.data.gift_card.value).toEqual(1000);
-      expect(response.data.gift_card.balance).toEqual(1000);
-      expect(response.data.gift_card.region_id).toEqual("region");
-    });
-  });
-});
+      expect(response.status).toEqual(200)
+      expect(response.data.gift_card.value).toEqual(1000)
+      expect(response.data.gift_card.balance).toEqual(1000)
+      expect(response.data.gift_card.region_id).toEqual("region")
+    })
+  })
+})
