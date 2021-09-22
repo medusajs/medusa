897ccf475aa417ec61bdd534b0d6d40b4ee2ea33
Sebastian Mateos Nicolajsen <80953876+sebastiannicolajsen@users.noreply.github.com>
2021-09-22 15:19:35 +0200

    Feat/note on order (#399)
    * added NoteService and related endpoints && tests
    
    * removed snapshots
    
    * corrected error in service
    
    * removed snapshot
    
    * added the ability to note down author using a string
    
    * updated model for note
    
    * refactored to access logged in user
    
    * added other user id option
    
    * removed snapshot
    
    * updated according to feedback
    
    * removed snapshots
    
    * reintroduced snapshots
    
    * updated to snake case
    
    * removed try catch from use-db

--- integration-tests/api/helpers/admin-seeder.js
+++ integration-tests/api/helpers/admin-seeder.js
@@ -1,11 +1,11 @@
-const Scrypt = require("scrypt-kdf");
-const { User } = require("@medusajs/medusa");
+const Scrypt = require("scrypt-kdf")
+const { User } = require("@medusajs/medusa")
 
 module.exports = async (connection, data = {}) => {
-  const manager = connection.manager;
+  const manager = connection.manager
 
-  const buf = await Scrypt.kdf("secret_password", { logN: 1, r: 1, p: 1 });
-  const password_hash = buf.toString("base64");
+  const buf = await Scrypt.kdf("secret_password", { logN: 1, r: 1, p: 1 })
+  const password_hash = buf.toString("base64")
 
   await manager.insert(User, {
     id: "admin_user",
@@ -13,5 +13,5 @@ module.exports = async (connection, data = {}) => {
     api_token: "test_token",
     password_hash,
     ...data,
-  });
-};
+  })
+}
