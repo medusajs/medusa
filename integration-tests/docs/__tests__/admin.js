d82cfabfbc58fb457a7fa9e76865e1df5f52b790
Philip Korsholm <philip.korsholm@hotmail.com>
2021-09-28 15:57:25 +0200

    note fixture

--- integration-tests/docs/__tests__/admin.js
+++ integration-tests/docs/__tests__/admin.js
@@ -12,6 +12,7 @@ const orderTest = require("../test-input/admin/order")
 const productTest = require("../test-input/admin/product")
 const rrTest = require("../test-input/admin/return-reason")
 const swapTest = require("../test-input/admin/swap")
+const noteTest = require("../test-input/admin/note")
 
 const toTest = [
   customerTest,
@@ -22,6 +23,7 @@ const toTest = [
   productTest,
   rrTest,
   swapTest,
+  noteTest,
 ]
 
 jest.setTimeout(30000)
--- integration-tests/docs/test-input/admin/note.js
+++ integration-tests/docs/test-input/admin/note.js
@@ -1,1 +1,36 @@
 // TODO
+module.exports = {
+  operationId: "GetNote",
+  buildEndpoint: (id) => `/admin/notes/${id}`,
+  setup: async (manager, api) => {
+    const payload = {
+      resource_id: "order_id",
+      resource_type: "order",
+      value: "note value",
+    }
+
+    const response = await api.post("/admin/notes", payload, {
+      headers: {
+        Authorization: "Bearer test_token",
+      },
+    })
+
+    return response.data.note.id
+  },
+  snapshotMatch: {
+    note: {
+      id: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+      author: {
+        id: expect.any(String),
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+        password_hash: expect.any(String),
+      },
+      resource_id: "order_id",
+      resource_type: "order",
+      value: "note value",
+    },
+  },
+}
