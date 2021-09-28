7d56385d1ddecc5a860b8cc36673d62273453be2
Philip Korsholm <philip.korsholm@hotmail.com>
2021-09-28 17:52:13 +0200

    user auth test

--- integration-tests/api/__tests__/admin/auth.js
+++ integration-tests/api/__tests__/admin/auth.js
@@ -51,41 +51,4 @@ describe("/admin/discounts", () => {
       updated_at: expect.any(String),
     })
   })
-
-  it("creates store session correctly", async () => {
-    const api = useApi()
-
-    await api
-      .post("/store/customers", {
-        email: "test@testesen.dk",
-        password: "secret_password",
-        first_name: "test",
-        last_name: "testesen",
-        phone: "12345678",
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-
-    const response = await api
-      .post("/store/auth", {
-        email: "test@testesen.dk",
-        password: "secret_password",
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-
-    expect(response.status).toEqual(200)
-    expect(response.data.customer.password_hash).toEqual(undefined)
-    expect(response.data.customer).toMatchSnapshot({
-      id: expect.any(String),
-      created_at: expect.any(String),
-      updated_at: expect.any(String),
-      first_name: "test",
-      last_name: "testesen",
-      phone: "12345678",
-      email: "test@testesen.dk",
-    })
-  })
 })
