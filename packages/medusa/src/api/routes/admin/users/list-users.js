46c5c46d9a4e88f0ad59d713d1e901fad42ed231
Philip Korsholm <philip.korsholm@hotmail.com>
2021-10-06 11:44:51 +0200

    conditional inclusion of invites in "list-users"

--- packages/medusa/src/api/routes/admin/users/list-users.js
+++ packages/medusa/src/api/routes/admin/users/list-users.js
@@ -1,18 +1,30 @@
+import { MedusaError, Validator } from "medusa-core-utils"
 export default async (req, res) => {
+  const schema = Validator.object().keys({
+    includeInvites: Validator.boolean().optional(),
+  })
+
+  const { value, error } = schema.validate(req.query)
+  if (error) {
+    throw new MedusaError(MedusaError.Types.INVALID_DATA, error.details)
+  }
+
   try {
     const userService = req.scope.resolve("userService")
-    const users = await userService.list({})
+    let users = await userService.list({})
 
-    const inviteService = req.scope.resolve("inviteService")
-    const invites = await inviteService.list({ accepted: false })
+    if (value.includeInvites) {
+      const inviteService = req.scope.resolve("inviteService")
+      const invites = await inviteService.list({ accepted: false })
 
-    const concat_res = users.concat(
-      invites.map(invite => {
-        return { ...invite, email: invite.user_email, isInvite: true }
-      })
-    )
+      users = users.concat(
+        invites.map(invite => {
+          return { ...invite, email: invite.user_email, isInvite: true }
+        })
+      )
+    }
 
-    res.status(200).json({ users: concat_res })
+    res.status(200).json({ users })
   } catch (err) {
     throw err
   }
