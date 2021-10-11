9e4dc163b344bbb8109883505d903d9552ba855d
Philip Korsholm <philip.korsholm@hotmail.com>
2021-10-11 14:43:20 +0200

    db aware columns

--- packages/medusa/src/models/user.ts
+++ packages/medusa/src/models/user.ts
@@ -22,7 +22,7 @@ export class User {
   @PrimaryColumn()
   id: string
 
-  @Column({type:'enum', enum: UserRoles, nullable: true, default: UserRoles.MEMBER})
+  @DbAwareColumn({type:'enum', enum: UserRoles, nullable: true, default: UserRoles.MEMBER})
   role: UserRoles
 
   @Index({ unique: true })
