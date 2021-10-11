87617dca05b8c154a443ddb74037777fe9a9a06b
Philip Korsholm <philip.korsholm@hotmail.com>
2021-10-11 13:26:41 +0200

    change name to verified token for the verified token

--- packages/medusa/src/api/routes/admin/users/reset-password.js
+++ packages/medusa/src/api/routes/admin/users/reset-password.js
@@ -23,8 +23,8 @@ export default async (req, res) => {
       select: ["id", "password_hash"],
     })
 
-    const decodedToken = await jwt.verify(value.token, user.password_hash)
-    if (!decodedToken || decodedToken.user_id !== user.id) {
+    const verifiedToken = await jwt.verify(value.token, user.password_hash)
+    if (!verifiedToken || verifiedToken.user_id !== user.id) {
       res.status(401).send("Invalid or expired password reset token")
       return
     }
