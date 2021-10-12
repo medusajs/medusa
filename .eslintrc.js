d4ba243185debd378d940caf4578426f05549f86
Sebastian Rindom <skrindom@gmail.com>
2021-10-12 22:09:46 +0200

    chore: add lint-staged + husky

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: `@babel/eslint-parser`,
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaFeatures: {
+      experimentalDecorators: true,
+    },
+  },
+  plugins: [`eslint-plugin-prettier`],
+  extends: [`eslint:recommended`, `google`, `eslint-config-prettier`],
+  rules: {
+    curly: [2, `all`],
+    "new-cap": `off`,
+    "require-jsdoc": `off`,
+    semi: `off`,
+    "no-unused-expressions": `off`,
+    camelcase: `off`,
+  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  overrides: [
+    {
+      files: [`*.ts`],
+      parser: `@typescript-eslint/parser`,
+      plugins: [`@typescript-eslint/eslint-plugin`],
+      extends: [`plugin:@typescript-eslint/recommended`],
+    },
+  ],
+}
