3a426f0fb8dc0daf39e142cb24fb1e42538b57ee
Diego Giuliani <giuliani.diego@gmail.com>
2021-10-12 04:07:01 -0300

    docs: store fixture for products (#490)

--- integration-tests/docs/__tests__/store.js
+++ integration-tests/docs/__tests__/store.js
@@ -5,8 +5,9 @@ const { useDb } = require("../../helpers/use-db")
 const cartTest = require("../test-input/store/cart")
 const customerTest = require("../test-input/store/customer")
 const giftCardTest = require("../test-input/store/gift-card")
+const productTest = require("../test-input/store/product")
 
-const toTest = [cartTest, customerTest, giftCardTest]
+const toTest = [cartTest, customerTest, giftCardTest, productTest]
 
 jest.setTimeout(30000)
 
--- integration-tests/docs/test-input/store/product.js
+++ integration-tests/docs/test-input/store/product.js
@@ -1,1 +1,96 @@
-// TODO
+const {
+  ShippingProfile,
+  MoneyAmount,
+  ProductOption,
+  Product,
+  ProductVariant,
+} = require("@medusajs/medusa")
+
+module.exports = {
+  operationId: "GetProductsProduct",
+  buildEndpoint: (id) => `/store/products/${id}`,
+  setup: async (manager) => {
+    const defaultProfile = await manager.findOne(ShippingProfile, {
+      type: "default",
+    })
+
+    const product = await manager.create(Product, {
+      title: "Basic Tee",
+      profile_id: defaultProfile.id,
+    })
+
+    await manager.save(product)
+
+    const productOption = await manager.create(ProductOption, {
+      title: "Size",
+      product_id: product.id,
+    })
+    await manager.save(productOption)
+
+    const variant = await manager.create(ProductVariant, {
+      title: "One Size",
+      product_id: product.id,
+      inventory_quantity: 1,
+      options: [
+        {
+          option_id: productOption.id,
+          value: "Size",
+        },
+      ],
+    })
+    await manager.save(variant)
+
+    const ma = manager.create(MoneyAmount, {
+      variant_id: variant.id,
+      currency_code: "usd",
+      amount: 8000,
+    })
+    await manager.save(ma)
+
+    return product.id
+  },
+  snapshotMatch: {
+    product: {
+      id: expect.stringMatching(/^prod_*/),
+      options: [
+        {
+          id: expect.stringMatching(/^opt_*/),
+          product_id: expect.stringMatching(/^prod_*/),
+          created_at: expect.any(String),
+          updated_at: expect.any(String),
+          values: [
+            {
+              id: expect.stringMatching(/^optval_*/),
+              value: "Size",
+              option_id: expect.stringMatching(/^opt_*/),
+              variant_id: expect.stringMatching(/^variant_*/),
+              created_at: expect.any(String),
+              updated_at: expect.any(String),
+              deleted_at: null,
+              metadata: null,
+            },
+          ],
+        },
+      ],
+      variants: [
+        {
+          id: expect.stringMatching(/^variant_*/),
+          created_at: expect.any(String),
+          updated_at: expect.any(String),
+          product_id: expect.stringMatching(/^prod_*/),
+          prices: [
+            {
+              id: expect.stringMatching(/^ma_*/),
+              variant_id: expect.stringMatching(/^variant_*/),
+              created_at: expect.any(String),
+              updated_at: expect.any(String),
+            },
+          ],
+        },
+      ],
+      profile_id: expect.stringMatching(/^sp_*/),
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+    },
+  },
+}
