10d1596c16ed0319e6f60e043861143960edc318
Minh Quan Nguyen <mqnguyen5@myseneca.ca>
2021-10-12 23:31:58 -0400

    Generating admin API fixtures for Users

--- integration-tests/docs/test-input/admin/user.js
+++ integration-tests/docs/test-input/admin/user.js
@@ -1,1 +1,22 @@
-// TODO
+const { User } = require("@medusajs/medusa")
+
+module.exports = {
+  operationId: "GetAdminUser",
+  buildEndpoint: (id) => `/admin/users/${id}`,
+  setup: async (manager, api) => {
+    const adminUser = await manager.create(User, {
+      email: "testAdmin@mail.com",
+      password: "test",
+      api_token: "test_token",
+    })
+    await manager.save(adminUser)
+    return adminUser.id
+  },
+  snapshotMatch: {
+    user: {
+      id: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+    },
+  },
+}
