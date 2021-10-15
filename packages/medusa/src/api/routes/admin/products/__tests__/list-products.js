6ac78662e82fb9e6db762e1457b6076745f41527
RafaAudibert <rafaeelaudibert@gmail.com>
2021-10-15 23:36:15 +0200

    tests(): Add unit tests for products listing

--- packages/medusa/src/api/routes/admin/products/__tests__/list-products.js
+++ packages/medusa/src/api/routes/admin/products/__tests__/list-products.js
@@ -6,16 +6,18 @@ import {
 } from "../../../../../services/__mocks__/product"
 
 describe("GET /admin/products", () => {
+  const adminSession = {
+    jwt: {
+      userId: IdMap.getId("admin_user"),
+    },
+  }
+
   describe("successfully lists products", () => {
     let subject
 
     beforeAll(async () => {
       subject = await request("GET", `/admin/products`, {
-        adminSession: {
-          jwt: {
-            userId: IdMap.getId("admin_user"),
-          },
-        },
+        adminSession
       })
     })
 
@@ -29,6 +31,49 @@ describe("GET /admin/products", () => {
 
     it("calls update", () => {
       expect(ProductServiceMock.list).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe("successfully sorts products", () => {
+    it("sorts ascending title field properly", async () => {
+      await request("GET", `/admin/products?order=title`, {
+        adminSession
+      })
+
+      expect(ProductServiceMock.list).toHaveBeenCalledWith({}, { order: { title: "ASC" } })
+    })
+
+    it("sorts descending title field properly", async () => {
+      await request("GET", `/admin/products?order=-title`, {
+        adminSession
+      })
+
+      expect(ProductServiceMock.list).toHaveBeenCalledWith({}, { order: { title: "DESC" } })
+    })
+
+    it("sorts ascending date field properly", async () => {
+      await request("GET", `/admin/products?order=created_at`, {
+        adminSession
+      })
+
+      expect(ProductServiceMock.list).toHaveBeenCalledWith({}, { order: { created_at: "ASC" } })
+    })
+
+    it("sorts descending date field properly", async () => {
+      await request("GET", `/admin/products?order=-updated_at`, {
+        adminSession
+      })
+
+      expect(ProductServiceMock.list).toHaveBeenCalledWith({}, { order: { updated_at: "DESC" } })
+    })
+
+    it("doesn't support different order parameters", async () => {
+      await request("GET", `/admin/products?order=other_field`, {
+        adminSession
+      })
+
+      expect(ProductServiceMock.list).not.toHaveBeenCalledWith({}, { order: { other_field: "ASC" } })
+      expect(ProductServiceMock.list).not.toHaveBeenCalledWith({}, { order: { other_field: "DESC" } })
     })
   })
 })
