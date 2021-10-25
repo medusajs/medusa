546e13c6759e591e34686622b4d026c41c18bfbb
timothy22000 <timothy22000@gmail.com>
2021-10-25 07:55:37 +0100

    fix: /api/routes/store/customers pass linting (#646)

--- packages/medusa/src/api/routes/store/customers/update-customer.js
+++ packages/medusa/src/api/routes/store/customers/update-customer.js
@@ -1,4 +1,3 @@
-import { optional } from "joi"
 import { Validator, MedusaError } from "medusa-core-utils"
 import { defaultRelations, defaultFields } from "./"
 
@@ -64,17 +63,13 @@ export default async (req, res) => {
     throw new MedusaError(MedusaError.Types.INVALID_DATA, error.details)
   }
 
-  try {
-    const customerService = req.scope.resolve("customerService")
-    await customerService.update(id, value)
+  const customerService = req.scope.resolve("customerService")
+  await customerService.update(id, value)
 
-    const customer = await customerService.retrieve(id, {
-      relations: defaultRelations,
-      select: defaultFields,
-    })
+  const customer = await customerService.retrieve(id, {
+    relations: defaultRelations,
+    select: defaultFields,
+  })
 
-    res.status(200).json({ customer })
-  } catch (err) {
-    throw err
-  }
+  res.status(200).json({ customer })
 }
