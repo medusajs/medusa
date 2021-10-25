e688411b3492136342f25a0a7e58bd6b74f63c35
audreyaudz <audz@kon.sg>
2021-10-25 22:00:05 +0800

    fix: make packages/medusa/src/api/routes/admin/regions pass eslint.

--- packages/medusa/src/api/routes/admin/regions/delete-metadata.js
+++ packages/medusa/src/api/routes/admin/regions/delete-metadata.js
@@ -23,17 +23,13 @@ import { defaultRelations, defaultFields } from "./"
 export default async (req, res) => {
   const { id, key } = req.params
 
-  try {
-    const regionService = req.scope.resolve("regionService")
-    await regionService.deleteMetadata(id, key)
+  const regionService = req.scope.resolve("regionService")
+  await regionService.deleteMetadata(id, key)
 
-    const region = await regionService.retrieve(id, {
-      select: defaultFields,
-      relations: defaultRelations,
-    })
+  const region = await regionService.retrieve(id, {
+    select: defaultFields,
+    relations: defaultRelations,
+  })
 
-    res.status(200).json({ region })
-  } catch (err) {
-    throw err
-  }
+  res.status(200).json({ region })
 }
