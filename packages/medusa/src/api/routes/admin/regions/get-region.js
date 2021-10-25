9047134e65a8c18d91e557029040349acc4bee26
Audrey Kon <audz@kon.sg>
2021-10-26 02:17:42 +0800

    fix: make packages/medusa/src/api/routes/admin/auth and 3 others pass eslint (#666)

--- packages/medusa/src/api/routes/admin/regions/get-region.js
+++ packages/medusa/src/api/routes/admin/regions/get-region.js
@@ -1,4 +1,3 @@
-import { MedusaError, Validator } from "medusa-core-utils"
 import { defaultRelations, defaultFields } from "./"
 
 /**
@@ -22,15 +21,11 @@ import { defaultRelations, defaultFields } from "./"
  */
 export default async (req, res) => {
   const { region_id } = req.params
-  try {
-    const regionService = req.scope.resolve("regionService")
-    const data = await regionService.retrieve(region_id, {
-      select: defaultFields,
-      relations: defaultRelations,
-    })
+  const regionService = req.scope.resolve("regionService")
+  const data = await regionService.retrieve(region_id, {
+    select: defaultFields,
+    relations: defaultRelations,
+  })
 
-    res.status(200).json({ region: data })
-  } catch (err) {
-    throw err
-  }
+  res.status(200).json({ region: data })
 }
