c1a450fe26c57a141bdc02454f047b3bbfbffc33
Daniel Tiringer <53534182+danielTiringer@users.noreply.github.com>
2021-10-26 17:59:36 +0000

    Fix linting errors in admin/draft-orders (#676)

--- packages/medusa/src/api/routes/admin/draft-orders/get-draft-order.js
+++ packages/medusa/src/api/routes/admin/draft-orders/get-draft-order.js
@@ -28,22 +28,18 @@ import {
 export default async (req, res) => {
   const { id } = req.params
 
-  try {
-    const draftOrderService = req.scope.resolve("draftOrderService")
-    const cartService = req.scope.resolve("cartService")
+  const draftOrderService = req.scope.resolve("draftOrderService")
+  const cartService = req.scope.resolve("cartService")
 
-    const draftOrder = await draftOrderService.retrieve(id, {
-      select: defaultFields,
-      relations: defaultRelations,
-    })
+  const draftOrder = await draftOrderService.retrieve(id, {
+    select: defaultFields,
+    relations: defaultRelations,
+  })
 
-    draftOrder.cart = await cartService.retrieve(draftOrder.cart_id, {
-      relations: defaultCartRelations,
-      select: defaultCartFields,
-    })
+  draftOrder.cart = await cartService.retrieve(draftOrder.cart_id, {
+    relations: defaultCartRelations,
+    select: defaultCartFields,
+  })
 
-    res.json({ draft_order: draftOrder })
-  } catch (error) {
-    throw error
-  }
+  res.json({ draft_order: draftOrder })
 }
