a1516006d3f504c0d312e505e4b8e9bf3c8d0a43
Michael Zhou <myz227@nyu.edu>
2021-10-26 12:16:10 -0700

    Remove try/catch blocks

--- packages/medusa/src/api/routes/admin/shipping-profiles/get-shipping-profile.js
+++ packages/medusa/src/api/routes/admin/shipping-profiles/get-shipping-profile.js
@@ -21,16 +21,12 @@ import { defaultFields, defaultRelations } from "./"
  */
 export default async (req, res) => {
   const { profile_id } = req.params
-  try {
-    const profileService = req.scope.resolve("shippingProfileService")
+  const profileService = req.scope.resolve("shippingProfileService")
 
-    const profile = await profileService.retrieve(profile_id, {
-      select: defaultFields,
-      relations: defaultRelations,
-    })
+  const profile = await profileService.retrieve(profile_id, {
+    select: defaultFields,
+    relations: defaultRelations,
+  })
 
-    res.status(200).json({ shipping_profile: profile })
-  } catch (err) {
-    return { error: err }
-  }
+  res.status(200).json({ shipping_profile: profile })
 }
