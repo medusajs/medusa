30c40bbb82ff6d7f0db383d025fc169981849b86
Woramat Ngamkham <30778789+Icyscools@users.noreply.github.com>
2021-10-28 00:49:59 +0700

    fix: make packages/medusa/src/api/routes/admin/orders pass eslint (#649)

--- packages/medusa/src/api/routes/admin/orders/cancel-order.js
+++ packages/medusa/src/api/routes/admin/orders/cancel-order.js
@@ -22,17 +22,13 @@ import { defaultFields, defaultRelations } from "."
 export default async (req, res) => {
   const { id } = req.params
 
-  try {
-    const orderService = req.scope.resolve("orderService")
-    await orderService.cancel(id)
+  const orderService = req.scope.resolve("orderService")
+  await orderService.cancel(id)
 
-    const order = await orderService.retrieve(id, {
-      select: defaultFields,
-      relations: defaultRelations,
-    })
+  const order = await orderService.retrieve(id, {
+    select: defaultFields,
+    relations: defaultRelations,
+  })
 
-    res.json({ order })
-  } catch (error) {
-    throw error
-  }
+  res.json({ order })
 }
