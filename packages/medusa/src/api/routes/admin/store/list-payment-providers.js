16b81dcbf4021b9c59d4e551e0f27ea23a1670d3
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-10-28 22:57:39 +0530

    fix: make packages/medusa/src/api/routes/admin/store pass eslint

--- packages/medusa/src/api/routes/admin/store/list-payment-providers.js
+++ packages/medusa/src/api/routes/admin/store/list-payment-providers.js
@@ -18,11 +18,7 @@
  *                 $ref: "#/components/schemas/store"
  */
 export default async (req, res) => {
-  try {
-    const paymentProviderService = container.resolve("paymentProviderService")
-    const paymentProviders = await paymentProviderService.list()
-    res.status(200).json({ payment_providers: paymentProviders })
-  } catch (err) {
-    throw err
-  }
+  const paymentProviderService = req.scope.resolve("paymentProviderService")
+  const paymentProviders = await paymentProviderService.list()
+  res.status(200).json({ payment_providers: paymentProviders })
 }
