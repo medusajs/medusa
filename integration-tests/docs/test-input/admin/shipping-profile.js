ae401dbb8b5868d67e9d16f2a507254635fedfa0
Leo Lou <louyuhong@gmail.com>
2021-10-31 21:01:39 +0800

    Add admin API fixture for Shipping Profile #476

--- integration-tests/docs/test-input/admin/shipping-profile.js
+++ integration-tests/docs/test-input/admin/shipping-profile.js
@@ -1,1 +1,28 @@
-// TODO
+const {
+  ShippingOption,
+  ShippingProfile,
+  Region,
+  FulfillmentProvider,
+} = require("@medusajs/medusa")
+
+module.exports = {
+  operationId: "GetShippingProfilesShippingProfile",
+  buildEndpoint: (id) => `/admin/shipping-profiles/${id}`,
+  setup: async (manager) => {
+    const newProfile = await manager.create(ShippingProfile, {
+      name: "shipping_profile_name",
+      type: "default"
+    })
+    await manager.save(newProfile)
+    return newProfile.id
+  },
+  snapshotMatch: {
+    shipping_profile: {
+      id: expect.stringMatching(/^sp_*/),
+      type: expect.stringMatching("default"),
+      name: expect.stringMatching("shipping_profile_name"),
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+    },
+  },
+}
