ea36b81eb12130c7f2be252bd606ea07fd66c2ab
Emme Bravo <75594869+emmebravo@users.noreply.github.com>
2021-10-31 01:32:17 -0700

    fix: make api/routes/store/swaps pass eslint (#678)

--- packages/medusa/src/api/routes/store/swaps/get-swap-by-cart.js
+++ packages/medusa/src/api/routes/store/swaps/get-swap-by-cart.js
@@ -20,11 +20,7 @@
 export default async (req, res) => {
   const { cart_id } = req.params
 
-  try {
-    const swapService = req.scope.resolve("swapService")
-    const swap = await swapService.retrieveByCartId(cart_id)
-    res.json({ swap })
-  } catch (error) {
-    throw error
-  }
+  const swapService = req.scope.resolve("swapService")
+  const swap = await swapService.retrieveByCartId(cart_id)
+  res.json({ swap })
 }
