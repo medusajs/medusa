628e88d71ff5612a686c2203d83b9e4fe6de5f60
Abdullah Pathan <aonepathan@gmail.com>
2021-10-31 03:55:58 -0400

    fix: make api/routes/admin/apps pass eslint (#641)

--- packages/medusa/src/api/routes/store/products/search.js
+++ packages/medusa/src/api/routes/store/products/search.js
@@ -4,9 +4,7 @@ import ProductService from "../../../../services/product"
 export default async (req, res) => {
   const schema = Validator.object()
     .keys({
-      q: Validator.string()
-        .required()
-        .allow(""),
+      q: Validator.string().required().allow(""),
       offset: Validator.number().optional(),
       limit: Validator.number().optional(),
       filter: Validator.any(),
