f01166272db7dc2d1b783223a7ea5c9e662a028b
Asian Cat <0xsapphir3@gmail.com>
2021-10-31 14:04:47 +0530

    fix: make packages/medusa/src/api/routes/admin/notes pass eslint (#690)

--- packages/medusa/src/api/routes/admin/notes/index.js
+++ packages/medusa/src/api/routes/admin/notes/index.js
@@ -3,7 +3,7 @@ import middlewares from "../../../middlewares"
 
 const route = Router()
 
-export default app => {
+export default (app) => {
   app.use("/notes", route)
 
   route.get("/:id", middlewares.wrap(require("./get-note").default))
