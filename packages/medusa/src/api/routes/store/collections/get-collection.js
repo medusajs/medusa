855fdeb36dde120058eafd1c7f36c796b1f62e44
Sebastian Rindom <skrindom@gmail.com>
2021-11-03 11:24:45 +0100

    feat(medusa): adds collection endpoints to storefront (#711)
    Adds:
    - `/store/collections/:id`
    - `/store/collections`

---
+++ packages/medusa/src/api/routes/store/collections/get-collection.js
@@ -0,0 +1,33 @@
+/**
+ * @oas [get] /collections/{id}
+ * operationId: "GetCollectionsCollection"
+ * summary: "Retrieve a Product Collection"
+ * description: "Retrieves a Product Collection."
+ * parameters:
+ *   - (path) id=* {string} The id of the Product Collection
+ * tags:
+ *   - Collection
+ * responses:
+ *  "200":
+ *    description: OK
+ *    content:
+ *      application/json:
+ *        schema:
+ *          properties:
+ *            collection:
+ *              $ref: "#/components/schemas/product_collection"
+ */
+
+export default async (req, res) => {
+  const { id } = req.params
+  try {
+    const productCollectionService = req.scope.resolve(
+      "productCollectionService"
+    )
+
+    const collection = await productCollectionService.retrieve(id)
+    res.status(200).json({ collection })
+  } catch (err) {
+    throw err
+  }
+}
