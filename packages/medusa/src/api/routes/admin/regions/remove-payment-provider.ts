4c962c74b41075d2bb8800a19e3efef8c0d3bc98
Philip Korsholm <philip.korsholm@hotmail.com>
2021-11-08 15:18:50 +0100

    set-metadata

--- packages/medusa/src/api/routes/admin/regions/remove-payment-provider.ts
+++ packages/medusa/src/api/routes/admin/regions/remove-payment-provider.ts
@@ -1,10 +1,14 @@
-import { defaultRelations, defaultFields } from "./"
+import { defaultRelations, defaultFields } from "."
+import { validator } from "medusa-core-utils"
+import Region from "../../../.."
+import RegionService from "../../../../services/region"
 
 /**
  * @oas [delete] /regions/{id}/payment-providers/{provider_id}
  * operationId: "PostRegionsRegionPaymentProvidersProvider"
  * summary: "Remove Payment Provider"
  * description: "Removes a Payment Provider."
+ * x-authenticated: true
  * parameters:
  *   - (path) id=* {string} The id of the Region.
  *   - (path) provider_id=* {string} The id of the Payment Provider.
@@ -21,14 +25,28 @@ import { defaultRelations, defaultFields } from "./"
  *               $ref: "#/components/schemas/region"
  */
 export default async (req, res) => {
-  const { region_id, provider_id } = req.params
-  const regionService = req.scope.resolve("regionService")
+  const validated = await validator(
+    AdminRemovePaymentProviderFromRegionRequest,
+    req.query
+  )
+
+  const { region_id, provider_id } = validated
+  const regionService = req.scope.resolve("regionService") as RegionService
   await regionService.removePaymentProvider(region_id, provider_id)
 
-  const region = await regionService.retrieve(region_id, {
+  const region: Region = await regionService.retrieve(region_id, {
     select: defaultFields,
     relations: defaultRelations,
   })
 
   res.json({ region })
+}
+
+export class AdminRemovePaymentProviderFromRegionRequest {
+  region_id: string
+  provider_id: string
+}
+
+export class AdminRemovePaymentProviderFromRegionResponse {
+  region: Region
 }
---
+++ packages/medusa/src/api/routes/admin/regions/set-metadata.ts
@@ -0,0 +1,59 @@
+import { defaultFields, defaultRelations } from "."
+import { validator } from "medusa-core-utils"
+import Region from "../../../.."
+import RegionService from "../../../../services/region"
+
+/**
+ * @oas [delete] /regions/{id}/payment-providers/{provider_id}
+ * operationId: "PostRegionsRegionPaymentProvidersProvider"
+ * summary: "Set the metadata of a Region"
+ * description: "Sets the metadata of a Region"
+ * x-authenticated: true
+ * parameters:
+ *   - (path) id=* {string} The id of the Region.
+ * tags:
+ *   - Region
+ * requestBody:
+ *    application/json:
+ *      schema:
+ *        properties:
+ *          key:
+ *            type: string
+ *            description: Key for the metadata value
+ *          value:
+ *            type: string
+ *            description: The value that the key relates to.
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             region:
+ *               $ref: "#/components/schemas/region"
+ */
+export default async (req, res) => {
+  const { id } = req.params
+
+  const validated = await validator(AdminRegionSetMetadataRequest, req.body)
+
+  const regionService = req.scope.resolve("regionService") as RegionService
+  await regionService.setMetadata(id, validated.key, validated.value)
+
+  const region: Region = await regionService.retrieve(id, {
+    select: defaultFields,
+    relations: defaultRelations,
+  })
+
+  res.status(200).json({ region })
+}
+
+export class AdminRegionSetMetadataRequest {
+  key: string
+  value: string
+}
+
+export class AdminRegionSetMetadataResponse {
+  region: Region
+}
