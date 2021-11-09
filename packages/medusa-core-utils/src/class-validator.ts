002a05082a0c86aa8a741814ab55da6bc1442f8c
Philip Korsholm <philip.korsholm@hotmail.com>
2021-11-09 13:11:37 +0100

    class validator nested

--- packages/medusa-core-utils/src/class-validator.ts
+++ packages/medusa-core-utils/src/class-validator.ts
@@ -1,23 +1,29 @@
 import { ClassConstructor, plainToClass } from "class-transformer"
-import { validate } from "class-validator"
+import { validate, ValidationError } from "class-validator"
 import { MedusaError } from "."
 
 async function validator<T extends object, V>(
   typedClass: ClassConstructor<T>,
   plain: V
 ): Promise<T> {
-  const toValidate = plainToClass(typedClass, plain)
+  const toValidate = plainToClass(typedClass, plain, {
+    ignoreDecorators: true,
+    enableImplicitConversion: true,
+  })
+
+  console.log(toValidate)
 
   const errors = await validate(toValidate)
 
-  const errorMessages = errors.reduce((acc: string[], next) => {
-    if (next.constraints) {
-        for (const [_, msg] of Object.entries(next.constraints)) {
-          acc.push(msg)
-        }
-    }
-    return acc
-  }, [])
+  const errorMessages = getErrorMessages(errors)
+  // errors.reduce((acc: string[], next) => {
+  //   if (next.constraints) {
+  //     for (const [_, msg] of Object.entries(next.constraints)) {
+  //       acc.push(msg)
+  //     }
+  //   }
+  //   return acc
+  // }, [])
 
   if (errors?.length) {
     throw new MedusaError(
@@ -27,6 +33,20 @@ async function validator<T extends object, V>(
   }
 
   return toValidate
+}
+
+const getErrorMessages = (errors: ValidationError[]): string[] => {
+  return errors.reduce((acc: string[], next) => {
+    if (next.constraints) {
+      for (const [_, msg] of Object.entries(next.constraints)) {
+        acc.push(msg)
+      }
+    }
+    if (next.children) {
+      acc.push(...getErrorMessages(next.children))
+    }
+    return acc
+  }, [])
 }
 
 export default validator
