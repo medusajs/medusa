c29a9780ab1ceb91706e9b3a7a16a23533da1648
olivermrbl <oliver@mrbltech.com>
2021-11-10 17:19:32 +0100

    fix: add typed client for store/carts

--- packages/medusa-js/src/resources/carts.ts
+++ packages/medusa-js/src/resources/carts.ts
@@ -1,20 +1,31 @@
-import BaseResource from './base';
-import LineItemsResource from './line-items';
-import * as Types from '../types';
-import { CompleteCartResponse } from '../types';
+import {
+  StoreAddCartShippingMethodRequest,
+  StoreCartResponse,
+  StoreCreateCartRequest,
+  StoreSetCartPaymentSession,
+  StoreUpdateCartPaymentSessionRequest,
+  StoreUpdateCartRequest,
+} from "@medusajs/medusa"
+import { AxiosPromise } from "axios"
+import { CompleteCartResponse } from "../types"
+import BaseResource from "./base"
+import LineItemsResource from "./line-items"
 
 class CartsResource extends BaseResource {
-  public lineItems = new LineItemsResource(this.client);
+  public lineItems = new LineItemsResource(this.client)
 
   /**
    * Adds a shipping method to cart
-   * @param cart_id Id of cart
-   * @param payload Containg id of shipping option and optional data
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id Id of cart
+   * @param {StoreAddCartShippingMethodRequest} payload Containg id of shipping option and optional data
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  addShippingMethod(cart_id: string, payload: Types.ShippingOption): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}/shipping-methods`;
-    return this.client.request('POST', path, payload);
+  addShippingMethod(
+    cart_id: string,
+    payload: StoreAddCartShippingMethodRequest
+  ): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}/shipping-methods`
+    return this.client.request("POST", path, payload)
   }
 
   /**
@@ -23,118 +34,131 @@ class CartsResource extends BaseResource {
    * If payment is authorized and order is not yet created, we make sure to do so.
    * The completion of a cart can be performed idempotently with a provided header Idempotency-Key.
    * If not provided, we will generate one for the request.
-   * @param cart_id is required
-   * @returns AsyncResult<CompleteCartResponse>
+   * @param {string} cart_id is required
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  complete(cart_id: string): Types.AsyncResult<CompleteCartResponse> {
-    const path = `/store/carts/${cart_id}/complete`;
-    return this.client.request('POST', path);
+  complete(cart_id: string): AxiosPromise<CompleteCartResponse> {
+    const path = `/store/carts/${cart_id}/complete`
+    return this.client.request("POST", path)
   }
 
   /**
    * Creates a cart
-   * @param payload is optional and can contain a region_id and items.
+   * @param {StoreCreateCartRequest} payload is optional and can contain a region_id and items.
    * The cart will contain the payload, if provided. Otherwise it will be empty
-   * @returns AsyncResult<{ cart: Cart }>
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  create(payload?: Types.CartCreateResource): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts`;
-    return this.client.request('POST', path, payload);
+  create(payload?: StoreCreateCartRequest): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts`
+    return this.client.request("POST", path, payload)
   }
 
   /**
    * Creates payment sessions.
    * Initializes the payment sessions that can be used to pay for the items of the cart.
    * This is usually called when a customer proceeds to checkout.
-   * @param cart_id is required
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id is required
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  createPaymentSessions(cart_id: string): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}/payment-sessions`;
-    return this.client.request('POST', path);
+  createPaymentSessions(cart_id: string): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}/payment-sessions`
+    return this.client.request("POST", path)
   }
 
   /**
    * Removes a discount from cart.
-   * @param cart_id is required
-   * @param code discount code to remove
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id is required
+   * @param {string} code discount code to remove
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  deleteDiscount(cart_id: string, code: string): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}/discounts/${code}`;
-    return this.client.request('DELETE', path);
+  deleteDiscount(
+    cart_id: string,
+    code: string
+  ): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}/discounts/${code}`
+    return this.client.request("DELETE", path)
   }
 
   /**
    * Removes a payment session from a cart.
    * Can be useful in case a payment has failed
-   * @param cart_id is required
-   * @param provider_id the provider id of the session e.g. "stripe"
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id is required
+   * @param {string} provider_id the provider id of the session e.g. "stripe"
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  deletePaymentSession(cart_id: string, provider_id: string): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}/payment-sessions/${provider_id}`;
-    return this.client.request('DELETE', path);
+  deletePaymentSession(
+    cart_id: string,
+    provider_id: string
+  ): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}/payment-sessions/${provider_id}`
+    return this.client.request("DELETE", path)
   }
 
   /**
    * Refreshes a payment session.
-   * @param cart_id is required
-   * @param provider_id the provider id of the session e.g. "stripe"
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id is required
+   * @param {string} provider_id the provider id of the session e.g. "stripe"
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  refreshPaymentSession(cart_id: string, provider_id: string): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}/payment-sessions/${provider_id}/refresh`;
-    return this.client.request('POST', path);
+  refreshPaymentSession(
+    cart_id: string,
+    provider_id: string
+  ): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}/payment-sessions/${provider_id}/refresh`
+    return this.client.request("POST", path)
   }
 
   /**
    * Retrieves a cart
-   * @param cart_id is required
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id is required
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  retrieve(cart_id: string): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}`;
-    return this.client.request('GET', path);
+  retrieve(cart_id: string): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}`
+    return this.client.request("GET", path)
   }
 
   /**
    * Refreshes a payment session.
-   * @param cart_id is required
-   * @param payload the provider id of the session e.g. "stripe"
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id is required
+   * @param {StoreSetCartPaymentSession} payload the provider id of the session e.g. "stripe"
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  setPaymentSession(cart_id: string, payload: { provider_id: string }): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}/payment-session`;
-    return this.client.request('POST', path, payload);
+  setPaymentSession(
+    cart_id: string,
+    payload: StoreSetCartPaymentSession
+  ): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}/payment-session`
+    return this.client.request("POST", path, payload)
   }
 
   /**
    * Updates a cart
-   * @param cart_id is required
-   * @param payload is required and can contain region_id, email, billing and shipping address
-   * @returns AsyncResult<{ cart: Cart }>
+   * @param {string} cart_id is required
+   * @param {StoreUpdateCartRequest} payload is required and can contain region_id, email, billing and shipping address
+   * @return {AxiosPromise<StoreCartResponse>}
    */
-  update(cart_id: string, payload: Types.CartUpdateResource): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}`;
-    return this.client.request('POST', path, payload);
+  update(
+    cart_id: string,
+    payload: StoreUpdateCartRequest
+  ): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}`
+    return this.client.request("POST", path, payload)
   }
 
   /**
    * Updates the payment method
-   * @param cart_id is required
-   * @param provider_id is required
-   * @param data is optional and contains TODO:
-   * Used to hold any data that the shipping method may need to process the fulfillment of the order.
-   * Look at the documentation for your installed fulfillment providers to find out what to send.
+   * @param {string} cart_id is required
+   * @param {StoreUpdateCartPaymentSessionRequest} payload is required
+   * @return {AxiosPromise<StoreCartResponse>}
    */
   updatePaymentSession(
     cart_id: string,
-    payload: { provider_id: string; data: object },
-  ): Types.AsyncResult<{ cart: Types.Cart }> {
-    const path = `/store/carts/${cart_id}/payment-session/update`;
-    return this.client.request('POST', path, payload);
+    payload: StoreUpdateCartPaymentSessionRequest
+  ): AxiosPromise<StoreCartResponse> {
+    const path = `/store/carts/${cart_id}/payment-session/update`
+    return this.client.request("POST", path, payload)
   }
 }
 
-export default CartsResource;
+export default CartsResource
