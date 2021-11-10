34a5314b15e39b6250fb830bae9487703151b34c
Philip Korsholm <philip.korsholm@hotmail.com>
2021-11-10 09:56:48 +0100

    add array types and number types

--- packages/medusa/src/utils/is-type.ts
+++ packages/medusa/src/utils/is-type.ts
@@ -1,4 +1,6 @@
 import {
+  isArray,
+  isNumber,
   isString,
   registerDecorator,
   ValidationArguments,
@@ -12,8 +14,20 @@ async function typeValidator(
 ): Promise<boolean> {
   switch (typedClass) {
     case String:
-      return Promise.resolve(isString(plain))
+      return isString(plain)
+    case Number:
+      return isNumber(Number(plain))
     default:
+      if (isArray(typedClass) && isArray(plain)) {
+        return (
+          await Promise.all(
+            (plain as any[]).map(
+              async (p) =>
+                await typeValidator(typedClass[0], p).catch(() => false)
+            )
+          )
+        ).some(Boolean)
+      }
       return (
         (await validator(typedClass, plain).then(() => true)) &&
         typeof plain === "object"
@@ -39,12 +53,25 @@ export function IsType(types: any[], validationOptions?: ValidationOptions) {
           return results.some(Boolean)
         },
         defaultMessage(validationArguments?: ValidationArguments) {
-          const names = types.map((t) => t.name)
+          const names = types.map(
+            (t) => t.name || (isArray(t) ? `${t[0].name}[]` : "")
+          )
           return `${validationArguments?.property} must be one of ${names
             .join(", ")
             .replace(/, ([^,]*)$/, " or $1")}`
         },
       },
     })
+  }
+}
+
+class MyArray<T> extends Array<T> {
+  private constructor(items: Array<T>) {
+    super(...items)
+  }
+
+  static test<T>(): MyArray<T> {
+    MyArray.name
+    return Object.create(MyArray.prototype)
   }
 }
