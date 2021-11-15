ba5a8c2b4424198b14bbb2254335c8aaa9702b3b
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2021-11-15 12:04:08 +0100

    feat: typed admin/shipping-profiles (#786)

--- packages/medusa/src/api/routes/admin/shipping-profiles/list-shipping-profiles.ts
+++ packages/medusa/src/api/routes/admin/shipping-profiles/list-shipping-profiles.ts
@@ -1,8 +1,11 @@
+import { ShippingProfileService } from "../../../../services"
+
 /**
  * @oas [get] /shipping-profiles
  * operationId: "GetShippingProfiles"
  * summary: "List Shipping Profiles"
  * description: "Retrieves a list of Shipping Profile."
+ * x-authenticated: true
  * tags:
  *   - Shipping Profile
  * responses:
@@ -18,7 +21,9 @@
  *                 $ref: "#/components/schemas/shipping_profile"
  */
 export default async (req, res) => {
-  const profileService = req.scope.resolve("shippingProfileService")
+  const profileService: ShippingProfileService = req.scope.resolve(
+    "shippingProfileService"
+  )
 
   const data = await profileService.list()
 
