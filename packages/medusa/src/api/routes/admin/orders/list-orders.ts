26294e1d2f1c4bff416439c9484675f2852e0987
Philip Korsholm <philip.korsholm@hotmail.com>
2021-11-15 20:26:02 +0100

    refactor selector into types

--- packages/medusa/src/api/routes/admin/orders/list-orders.ts
+++ packages/medusa/src/api/routes/admin/orders/list-orders.ts
@@ -1,17 +1,9 @@
 import { defaultAdminOrdersRelations, defaultAdminOrdersFields } from "."
 import { validator } from "../../../../utils/validator"
-import {
-  IsArray,
-  IsEnum,
-  IsOptional,
-  IsString,
-  ValidateNested,
-} from "class-validator"
-import { DateComparisonOperator } from "../../../../types/common"
-import { Type } from "class-transformer"
+import { IsOptional, IsString } from "class-validator"
 import _, { identity } from "lodash"
 import { OrderService } from "../../../../services"
-import { MedusaError } from "medusa-core-utils"
+import { Selector } from "../../../../types/orders"
 
 /**
  * @oas [get] /orders
@@ -34,24 +26,12 @@ import { MedusaError } from "medusa-core-utils"
  *                 $ref: "#/components/schemas/order"
  */
 export default async (req, res) => {
-  const value = await validator(AdminGetOrdersReq, req.query)
+  const value = await validator(AdminGetOrdersParams, req.query)
 
   const orderService: OrderService = req.scope.resolve("orderService")
 
   const limit = parseInt(value.limit) || 50
   const offset = parseInt(value.offset) || 0
-
-  // const selector: Selector = {}
-
-  // for (const [key, val] of Object.entries(value)) {
-  //   if (val && Object.keys(new Selector()).indexOf(key) !== -1) {
-  //     selector[key] = val
-  //   }
-  // }
-
-  // if (value.q) {
-  //   selector.q = value.q
-  // }
 
   let includeFields: string[] = []
   if (value.fields) {
@@ -96,105 +76,7 @@ export default async (req, res) => {
   res.json({ orders: data, count, offset, limit })
 }
 
-enum OrderStatus {
-  pending = "pending",
-  completed = "completed",
-  archived = "archived",
-  canceled = "canceled",
-  requires_action = "requires_action",
-}
-
-enum FulfillmentStatus {
-  not_fulfilled = "not_fulfilled",
-  fulfilled = "fulfilled",
-  partially_fulfilled = "partially_fulfilled",
-  shipped = "shipped",
-  partially_shipped = "partially_shipped",
-  canceled = "canceled",
-  returned = "returned",
-  partially_returned = "partially_returned",
-  requires_action = "requires_action",
-}
-
-enum PaymentStatus {
-  captured = "captured",
-  awaiting = "awaiting",
-  not_paid = "not_paid",
-  refunded = "refunded",
-  partially_refunded = "partially_refunded",
-  canceled = "canceled",
-  requires_action = "requires_action",
-}
-
-export class Selector {
-  @IsString()
-  @IsOptional()
-  q?: string
-
-  @IsString()
-  @IsOptional()
-  id?: string
-
-  @IsArray()
-  @IsEnum(OrderStatus, { each: true })
-  @IsOptional()
-  status?: string
-
-  @IsArray()
-  @IsEnum(FulfillmentStatus, { each: true })
-  @IsOptional()
-  fulfillment_status?: string
-
-  @IsArray()
-  @IsEnum(PaymentStatus)
-  @IsOptional()
-  payment_status?: string
-
-  @IsString()
-  @IsOptional()
-  display_id?: string
-
-  @IsString()
-  @IsOptional()
-  cart_id?: string
-
-  @IsString()
-  @IsOptional()
-  customer_id?: string
-
-  @IsString()
-  @IsOptional()
-  email?: string
-
-  @IsString()
-  @IsOptional()
-  region_id?: string
-
-  @IsString()
-  @IsOptional()
-  currency_code?: string
-
-  @IsString()
-  @IsOptional()
-  tax_rate?: string
-
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => DateComparisonOperator)
-  canceled_at?: DateComparisonOperator
-
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => DateComparisonOperator)
-  created_at?: DateComparisonOperator
-
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => DateComparisonOperator)
-  updated_at?: DateComparisonOperator
-}
-
-export class AdminGetOrdersReq extends Selector {
+export class AdminGetOrdersParams extends Selector {
   @IsString()
   @IsOptional()
   offset: string
