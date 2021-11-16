654ce318e9bffba864cbfd384711b939a7231cf6
zakariaelas <zakaria.elas@gmail.com>
2021-11-16 11:42:47 +0100

    fix: compilation error

--- packages/medusa/src/api/routes/store/auth/create-session.ts
+++ packages/medusa/src/api/routes/store/auth/create-session.ts
@@ -42,14 +42,14 @@ export default async (req, res) => {
 
   // Add JWT to cookie
   req.session.jwt = jwt.sign(
-    { customer_id: result.customer.id },
+    { customer_id: result.customer?.id },
     (config as any).jwtSecret,
     {
       expiresIn: "30d",
     }
   )
 
-  const customer = await customerService.retrieve(result.customer.id, {
+  const customer = await customerService.retrieve(result.customer?.id || "", {
     relations: ["orders", "orders.items"],
   })
 
