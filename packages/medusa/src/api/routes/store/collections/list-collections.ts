cd1db07c0cbf28f94ad5232f2c434da793c67625
zakariaelas <zakaria.elas@gmail.com>
2021-11-16 15:27:01 +0100

    add: @IsInt() extra validator

--- packages/medusa/src/api/routes/store/collections/list-collections.ts
+++ packages/medusa/src/api/routes/store/collections/list-collections.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer"
-import { IsOptional } from "class-validator"
+import { IsOptional, IsInt } from "class-validator"
 import ProductCollectionService from "../../../../services/product-collection"
 import { validator } from "../../../../utils/validator"
 /**
@@ -23,35 +23,40 @@ import { validator } from "../../../../utils/validator"
  *              $ref: "#/components/schemas/product_collection"
  */
 export default async (req, res) => {
-  const { limit, offset } = await validator(
-    StoreGetCollectionsParams,
-    req.query
-  )
-  const selector = {}
+  try {
+    const { limit, offset } = await validator(
+      StoreGetCollectionsParams,
+      req.query
+    )
+    const selector = {}
 
-  const productCollectionService: ProductCollectionService = req.scope.resolve(
-    "productCollectionService"
-  )
+    const productCollectionService: ProductCollectionService =
+      req.scope.resolve("productCollectionService")
 
-  const listConfig = {
-    skip: offset,
-    take: limit,
+    const listConfig = {
+      skip: offset,
+      take: limit,
+    }
+
+    const [collections, count] = await productCollectionService.listAndCount(
+      selector,
+      listConfig
+    )
+
+    res.status(200).json({ collections, count, limit, offset })
+  } catch (err) {
+    console.log(err)
   }
-
-  const [collections, count] = await productCollectionService.listAndCount(
-    selector,
-    listConfig
-  )
-
-  res.status(200).json({ collections, count, limit, offset })
 }
 
 export class StoreGetCollectionsParams {
   @IsOptional()
+  @IsInt()
   @Type(() => Number)
   limit?: number = 10
 
   @IsOptional()
+  @IsInt()
   @Type(() => Number)
   offset?: number = 0
 }
