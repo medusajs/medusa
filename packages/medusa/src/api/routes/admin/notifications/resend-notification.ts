d9b66d8ce192f3e124deb1d2b3789c0bda605c85
Zakaria El Asri <33696020+zakariaelas@users.noreply.github.com>
2021-11-17 09:32:36 +0100

    feat: migrate admin/notifications to typescript  (#809)

---
+++ packages/medusa/src/api/routes/admin/notifications/resend-notification.ts
@@ -0,0 +1,70 @@
+import { IsString } from "class-validator"
+import { IsOptional } from "class-validator"
+import {
+  defaultAdminNotificationsFields,
+  defaultAdminNotificationsRelations,
+} from "."
+import { validator } from "../../../../utils/validator"
+import { NotificationService } from "../../../../services"
+
+/**
+ * @oas [post] /notifications/{id}/resend
+ * operationId: "PostNotificationsNotificationResend"
+ * summary: "Resend Notification"
+ * description: "Resends a previously sent notifications, with the same data but optionally to a different address"
+ * x-authenticated: true
+ * parameters:
+ *   - (path) id=* {string} The id of the Notification
+ * requestBody:
+ *   content:
+ *     application/json:
+ *       schema:
+ *         properties:
+ *           to:
+ *             description: "The address or user identifier that the Notification was sent to"
+ *             type: string
+ * tags:
+ *   - Notification
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             notification:
+ *               $ref: "#/components/schemas/notification"
+ */
+export default async (req, res) => {
+  const { id } = req.params
+
+  const validatedBody = await validator(
+    AdminPostNotificationsNotificationResendReq,
+    req.body
+  )
+
+  const notificationService: NotificationService = req.scope.resolve(
+    "notificationService"
+  )
+
+  const config: any = {}
+
+  if (validatedBody.to) {
+    config.to = validatedBody.to
+  }
+
+  await notificationService.resend(id, config)
+
+  const notification = await notificationService.retrieve(id, {
+    select: defaultAdminNotificationsFields,
+    relations: defaultAdminNotificationsRelations,
+  })
+
+  res.json({ notification })
+}
+
+export class AdminPostNotificationsNotificationResendReq {
+  @IsOptional()
+  @IsString()
+  to?: string
+}
