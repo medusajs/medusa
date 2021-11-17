b155deff4c82c1d596102099596958a985fc70f8
Philip Korsholm <philip.korsholm@hotmail.com>
2021-11-17 16:01:21 +0100

    add types to list-variants

--- packages/medusa/src/api/routes/admin/variants/list-variants.ts
+++ packages/medusa/src/api/routes/admin/variants/list-variants.ts
@@ -4,7 +4,9 @@ import { FilterableProductVariantProps } from "../../../../types/product-variant
 import { FindConfig } from "../../../../types/common"
 import { ProductVariant } from "../../../../models/product-variant"
 import ProductVariantService from "../../../../services/product-variant"
-
+import { validator } from "../../../../utils/validator"
+import { Type } from "class-transformer"
+import { IsInt, IsOptional } from "class-validator"
 /**
  * @oas [get] /variants
  * operationId: "GetVariants"
@@ -30,8 +32,7 @@ export default async (req, res) => {
     "productVariantService"
   )
 
-  const limit = parseInt(req.query.limit) || 20
-  const offset = parseInt(req.query.offset) || 0
+  const { offset, limit } = await validator(AdminGetVariantsParams, req.query)
 
   const selector: FilterableProductVariantProps = {}
 
@@ -49,4 +50,16 @@ export default async (req, res) => {
   const variants = await variantService.list(selector, listConfig)
 
   res.json({ variants, count: variants.length, offset, limit })
+}
+
+export class AdminGetVariantsParams {
+  @IsInt()
+  @IsOptional()
+  @Type(() => Number)
+  limit?: number = 20
+
+  @IsInt()
+  @IsOptional()
+  @Type(() => Number)
+  offset?: number = 0
 }
