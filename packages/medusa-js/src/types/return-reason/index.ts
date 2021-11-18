2c9945689ba8024088d3adba76aaef76b27fec50
olivermrbl <oliver@mrbltech.com>
2021-11-18 09:54:46 +0100

    remove legacy types from medusa-js

--- packages/medusa-js/src/types/return-reason/index.ts
+++
@@ -1,8 +0,0 @@
-export interface ReturnReason {
-  id: string;
-  value: string;
-  label: string;
-  created_at: Date;
-  updated_at: Date;
-  deleted_at?: Date;
-}
--- packages/medusa-js/src/types/shipping-option/index.ts
+++
@@ -1,6 +0,0 @@
-
-import { StoreGetShippingOptionsParams } from '@medusajs/medusa';
-
-export type StoreGetShippingOptionsParamsObject = Omit<StoreGetShippingOptionsParams, 'product_ids'> & {
-  product_ids?: string[]
-}
