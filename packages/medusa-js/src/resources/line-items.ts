076ee105856f4364912de2a80967b59985c6e123
zakariaelas <zakaria.elas@gmail.com>
2021-11-22 11:53:56 +0100

    fix: wrong import

--- packages/medusa-js/src/resources/line-items.ts
+++ packages/medusa-js/src/resources/line-items.ts
@@ -1,5 +1,5 @@
 import {
-  StoreCartsCartRes,
+  StoreCartsRes,
   StorePostCartsCartLineItemsItemReq,
   StorePostCartsCartLineItemsReq,
 } from "@medusajs/medusa"
@@ -11,12 +11,12 @@ class LineItemsResource extends BaseResource {
    * Creates a line-item for a cart
    * @param {string} cart_id id of cart
    * @param {StorePostCartsCartLineItemsReq} payload details needed to create a line-item
-   * @return {ResponsePromise<StoreCartsCartRes>}
+   * @return {ResponsePromise<StoreCartsRes>}
    */
   create(
     cart_id: string,
     payload: StorePostCartsCartLineItemsReq
-  ): ResponsePromise<StoreCartsCartRes> {
+  ): ResponsePromise<StoreCartsRes> {
     const path = `/store/carts/${cart_id}/line-items`
     return this.client.request("POST", path, payload)
   }
@@ -27,13 +27,13 @@ class LineItemsResource extends BaseResource {
    * @param {string} cart_id id of cart
    * @param {string} line_id id of item to update
    * @param {StorePostCartsCartLineItemsItemReq} payload details needed to update a line-item
-   * @return {ResponsePromise<StoreCartsCartRes>}
+   * @return {ResponsePromise<StoreCartsRes>}
    */
   update(
     cart_id: string,
     line_id: string,
     payload: StorePostCartsCartLineItemsItemReq
-  ): ResponsePromise<StoreCartsCartRes> {
+  ): ResponsePromise<StoreCartsRes> {
     const path = `/store/carts/${cart_id}/line-items/${line_id}`
     return this.client.request("POST", path, payload)
   }
@@ -44,7 +44,7 @@ class LineItemsResource extends BaseResource {
    * @param {string} line_id id of item to remove
    * @return {ResponsePromise<StoreCartsDeleteRes>}
    */
-  delete(cart_id: string, line_id: string): ResponsePromise<StoreCartsCartRes> {
+  delete(cart_id: string, line_id: string): ResponsePromise<StoreCartsRes> {
     const path = `/store/carts/${cart_id}/line-items/${line_id}`
     return this.client.request("DELETE", path)
   }
