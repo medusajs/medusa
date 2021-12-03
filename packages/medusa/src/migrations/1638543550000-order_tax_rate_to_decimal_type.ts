7d2d1bca5e91dd352653097272cc64a2ef2b213e
Derek Wene <derek.wene@gmail.com>
2021-12-03 09:08:13 -0600

    chore: cleanup

--- packages/medusa/src/migrations/1638543550000-order_tax_rate_to_decimal_type.ts
+++ packages/medusa/src/migrations/1638543550000-order_tax_rate_to_decimal_type.ts
@@ -1,8 +1,8 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class addCustomShippingOptions1633614437919
+export class orderTaxRateToDecimalType1638543550000
   implements MigrationInterface {
-  name = "addCustomShippingOptions1633614437919"
+  name = "orderTaxRateToDecimalType1638543550000"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
