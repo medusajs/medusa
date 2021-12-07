6da97ec5c8e5a98644c199f68d19b98694aaf005
Philip Korsholm <philip.korsholm@hotmail.com>
2021-12-07 14:26:54 +0100

    remove "public"

--- packages/medusa/src/migrations/1638458215844-unique_constraint_shipping_option.ts
+++ packages/medusa/src/migrations/1638458215844-unique_constraint_shipping_option.ts
@@ -1,18 +1,19 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class uniqueConstraintShippingOption1638458215844
-  implements MigrationInterface {
+  implements MigrationInterface
+{
   name = "uniqueConstraintShippingOption1638458215844"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "public"."shipping_method" ADD CONSTRAINT "unique shipping option for cart" UNIQUE ("cart_id", "shipping_option_id")`
+      `ALTER TABLE "shipping_method" ADD CONSTRAINT "unique shipping option for cart" UNIQUE ("cart_id", "shipping_option_id")`
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "public"."shipping_method" DROP CONSTRAINT "unique shipping option for cart"`
+      `ALTER TABLE "shipping_method" DROP CONSTRAINT "unique shipping option for cart"`
     )
   }
 }
