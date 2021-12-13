25fe224a10842a7ac93ed496a6724ef113b41916
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-12-13 21:32:47 +0100

    feat: Adds Customer Admin routes to JS client (#918)

---
+++ packages/medusa-js/src/resources/admin/customers.ts
@@ -0,0 +1,63 @@
+import {
+  AdminCustomersListRes,
+  AdminCustomersRes,
+  AdminGetCustomersParams,
+  AdminPostCustomersReq,
+} from "@medusajs/medusa"
+import { ResponsePromise } from "../.."
+import BaseResource from "../base"
+
+class AdminCustomersResource extends BaseResource {
+  /**
+   * Creates a customer
+   * @param payload information of customer
+   */
+  create(payload: AdminPostCustomersReq): ResponsePromise<AdminCustomersRes> {
+    const path = `/admin/customers`
+    return this.client.request("POST", path, payload)
+  }
+
+  /**
+   * Updates a customer
+   * @param id customer id
+   * @param payload data to update customer with
+   */
+  update(
+    id: string,
+    payload: AdminPostCustomersReq
+  ): ResponsePromise<AdminCustomersRes> {
+    const path = `/admin/customers/${id}`
+    return this.client.request("POST", path, payload)
+  }
+
+  /**
+   * Retrieves a customer
+   * @param id customer id
+   */
+  retrieve(id: string): ResponsePromise<AdminCustomersRes> {
+    const path = `/admin/customers/${id}`
+    return this.client.request("GET", path)
+  }
+
+  /**
+   * Lists customers
+   * @param query optional
+   */
+  list(
+    query?: AdminGetCustomersParams
+  ): ResponsePromise<AdminCustomersListRes> {
+    let path = `/admin/customers`
+
+    if (query) {
+      const queryString = Object.entries(query).map(([key, value]) => {
+        return `${key}=${value}`
+      })
+
+      path = `/admin/customers?${queryString.join("&")}`
+    }
+
+    return this.client.request("GET", path)
+  }
+}
+
+export default AdminCustomersResource
