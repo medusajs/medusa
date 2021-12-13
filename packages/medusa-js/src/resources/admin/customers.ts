aae44bd634a6e53d23d92d83878a1508b65bf5cb
olivermrbl <oliver@mrbltech.com>
2021-12-13 18:35:48 +0100

    feat: Add Customer Admin routes to JS client

---
+++ packages/medusa-js/src/resources/admin/customers.ts
@@ -0,0 +1,67 @@
+import {
+  AdminCustomersListRes,
+  AdminCustomersRes,
+  AdminGetCustomersParams,
+  AdminPostCustomersReq,
+} from "@medusajs/medusa"
+import { ResponsePromise } from "../.."
+import BaseResource from "../base"
+
+class AdminCustomerResource extends BaseResource {
+  /**
+   * Creates a customer
+   * @param {AdminPostCustomersReq} payload information of customer
+   * @return { ResponsePromise<AdminCustomersRes>}
+   */
+  create(payload: AdminPostCustomersReq): ResponsePromise<AdminCustomersRes> {
+    const path = `/admin/customers`
+    return this.client.request("POST", path, payload)
+  }
+
+  /**
+   * Updates a customer
+   * @param {string} id customer id
+   * @param {AdminPostCustomersReq} payload data to update customer with
+   * @return { ResponsePromise<AdminCustomersRes>}
+   */
+  update(
+    id: string,
+    payload: AdminPostCustomersReq
+  ): ResponsePromise<AdminCustomersRes> {
+    const path = `/admin/customers/${id}`
+    return this.client.request("POST", path, payload)
+  }
+
+  /**
+   * Retrieves a customer
+   * @param {string} id customer id
+   * @return { ResponsePromise<AdminCustomersRes>}
+   */
+  retrieve(id: string): ResponsePromise<AdminCustomersRes> {
+    const path = `/admin/customers/${id}`
+    return this.client.request("GET", path)
+  }
+
+  /**
+   * Lists customers
+   * @param {AdminGetCustomersParams} query optional
+   * @return { ResponsePromise<AdminCustomersRes>}
+   */
+  list(
+    query?: AdminGetCustomersParams
+  ): ResponsePromise<AdminCustomersListRes> {
+    let path = `/admin/customers`
+
+    if (query) {
+      const queryString = Object.entries(query).map(([key, value]) => {
+        return `${key}=${value}`
+      })
+
+      path = `/admin/customers?${queryString.join("&")}`
+    }
+
+    return this.client.request("GET", path)
+  }
+}
+
+export default AdminCustomerResource
--- packages/medusa-js/src/resources/admin/index.ts
+++ packages/medusa-js/src/resources/admin/index.ts
@@ -1,8 +1,10 @@
 import BaseResource from "../base"
 import AdminAuthResource from "./auth"
+import AdminCustomerResource from "./customers"
 
 class Admin extends BaseResource {
   public auth = new AdminAuthResource(this.client)
+  public customers = new AdminCustomerResource(this.client)
 }
 
 export default Admin
