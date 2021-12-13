dced9dd558e005425f82aac588794dc0d643c756
Sebastian Rindom <skrindom@gmail.com>
2021-12-13 21:51:10 +0100

    chore: rm tax-line repo

--- packages/medusa/src/repositories/tax-line.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm"
-import { TaxLine } from "../models/tax-line"
-
-@EntityRepository(TaxLine)
-export class TaxLineRepository extends Repository<TaxLine> {}
