f1a5fe7106bf05b5d5e4a3fd702c5e73415dd376
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-12-13 21:32:47 +0100

    feat: Adds Customer Admin routes to JS client (#918)

--- packages/medusa-js/src/resources/admin/index.ts
+++ packages/medusa-js/src/resources/admin/index.ts
@@ -1,8 +1,10 @@
 import BaseResource from "../base"
 import AdminAuthResource from "./auth"
+import AdminCustomersResource from "./customers"
 
 class Admin extends BaseResource {
   public auth = new AdminAuthResource(this.client)
+  public customers = new AdminCustomersResource(this.client)
 }
 
 export default Admin
