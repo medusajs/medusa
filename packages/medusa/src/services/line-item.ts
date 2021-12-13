defbc0d19d46b4df4ba150223615922a541d569b
Philip Korsholm <philip.korsholm@hotmail.com>
2021-12-13 13:03:14 +0100

    convert line item service to typescript

---
+++ packages/medusa/src/services/line-item.ts
@@ -0,0 +1,266 @@
+import { MedusaError } from "medusa-core-utils"
+import { BaseService } from "medusa-interfaces"
+import { EntityManager } from "typeorm"
+import { ProductService, ProductVariantService, RegionService } from "."
+import { LineItem, ShippingMethod } from ".."
+import { CartRepository } from "../repositories/cart"
+import { LineItemRepository } from "../repositories/line-item"
+import { GenerateConfig } from "../types/line-item"
+
+/**
+ * Provides layer to manipulate line items.
+ * @extends BaseService
+ */
+
+export type LineItemServiceProps = {
+  manager: EntityManager
+  lineItemRepository: typeof LineItemRepository
+  productVariantService: ProductVariantService
+  productService: ProductService
+  regionService: RegionService
+  cartRepository: typeof CartRepository
+}
+class LineItemService extends BaseService {
+  private manager_: EntityManager
+  private lineItemRepository_: typeof LineItemRepository
+  private productVariantService_: ProductVariantService
+  private productService_: ProductService
+  private regionService_: RegionService
+  private cartRepository_: typeof CartRepository
+
+  constructor({
+    manager,
+    lineItemRepository,
+    productVariantService,
+    productService,
+    regionService,
+    cartRepository,
+  }: LineItemServiceProps) {
+    super()
+
+    /** @private @const {EntityManager} */
+    this.manager_ = manager
+
+    /** @private @const {LineItemRepository} */
+    this.lineItemRepository_ = lineItemRepository
+
+    /** @private @const {ProductVariantService} */
+    this.productVariantService_ = productVariantService
+
+    /** @private @const {ProductService} */
+    this.productService_ = productService
+
+    /** @private @const {RegionService} */
+    this.regionService_ = regionService
+
+    /** @private @const {CartRepository} */
+    this.cartRepository_ = cartRepository
+  }
+
+  withTransaction(transactionManager): LineItemService {
+    if (!transactionManager) {
+      return this
+    }
+
+    const cloned = new LineItemService({
+      manager: transactionManager,
+      lineItemRepository: this.lineItemRepository_,
+      productVariantService: this.productVariantService_,
+      productService: this.productService_,
+      regionService: this.regionService_,
+      cartRepository: this.cartRepository_,
+    })
+
+    cloned.transactionManager_ = transactionManager
+
+    return cloned
+  }
+
+  async list(
+    selector,
+    config = { skip: 0, take: 50, order: { created_at: "DESC" } }
+  ): Promise<LineItem[]> {
+    const liRepo = this.manager_.getCustomRepository(this.lineItemRepository_)
+    const query = this.buildQuery_(selector, config)
+    return liRepo.find(query)
+  }
+
+  /**
+   * Retrieves a line item by its id.
+   * @param {string} id - the id of the line item to retrieve
+   * @param {object} config - the config to be used at query building
+   * @return {LineItem} the line item
+   */
+  async retrieve(id, config = {}): Promise<LineItem> {
+    const lineItemRepository = this.manager_.getCustomRepository(
+      this.lineItemRepository_
+    )
+
+    const validatedId = this.validateId_(id)
+    const query = this.buildQuery_({ id: validatedId }, config)
+
+    const lineItem = await lineItemRepository.findOne(query)
+
+    if (!lineItem) {
+      throw new MedusaError(
+        MedusaError.Types.NOT_FOUND,
+        `Line item with ${id} was not found`
+      )
+    }
+
+    return lineItem
+  }
+
+  async generate(
+    variantId,
+    regionId,
+    quantity,
+    config: GenerateConfig = {}
+  ): Promise<LineItem> {
+    return this.atomicPhase_(async (manager) => {
+      const variant = await this.productVariantService_
+        .withTransaction(manager)
+        .retrieve(variantId, {
+          relations: ["product"],
+        })
+
+      const region = await this.regionService_
+        .withTransaction(manager)
+        .retrieve(regionId)
+
+      let price
+      let shouldMerge = true
+
+      if (config.unit_price !== undefined && config.unit_price !== null) {
+        // if custom unit_price, we ensure positive values
+        // and we choose to not merge the items
+        shouldMerge = false
+        if (config.unit_price < 0) {
+          price = 0
+        } else {
+          price = config.unit_price
+        }
+      } else {
+        price = await this.productVariantService_
+          .withTransaction(manager)
+          .getRegionPrice(variant.id, region.id)
+      }
+
+      const toCreate = {
+        unit_price: price,
+        title: variant.product.title,
+        description: variant.title,
+        thumbnail: variant.product.thumbnail,
+        variant_id: variant.id,
+        quantity: quantity || 1,
+        allow_discounts: variant.product.discountable,
+        is_giftcard: variant.product.is_giftcard,
+        metadata: config?.metadata || {},
+        should_merge: shouldMerge,
+      }
+
+      return toCreate
+    })
+  }
+
+  /**
+   * Create a line item
+   * @param {LineItem} lineItem - the line item object to create
+   * @return {LineItem} the created line item
+   */
+  async create(lineItem): Promise<LineItem> {
+    return this.atomicPhase_(async (manager) => {
+      const lineItemRepository = manager.getCustomRepository(
+        this.lineItemRepository_
+      )
+
+      const created = await lineItemRepository.create(lineItem)
+      const result = await lineItemRepository.save(created)
+      return result
+    })
+  }
+
+  /**
+   * Updates a line item
+   * @param {string} id - the id of the line item to update
+   * @param {object} update - the properties to update on line item
+   * @return {LineItem} the update line item
+   */
+  async update(id, update): Promise<LineItem> {
+    return this.atomicPhase_(async (manager) => {
+      const lineItemRepository = manager.getCustomRepository(
+        this.lineItemRepository_
+      )
+
+      const lineItem = await this.retrieve(id)
+
+      const { metadata, ...rest } = update
+
+      if (metadata) {
+        lineItem.metadata = this.setMetadata_(lineItem, metadata)
+      }
+
+      for (const [key, value] of Object.entries(rest)) {
+        lineItem[key] = value
+      }
+
+      const result = await lineItemRepository.save(lineItem)
+      return result
+    })
+  }
+
+  /**
+   * Updates a line item
+   * @param {LineItem[]} lineItems - the id of the line item to update
+   * @param {ShippingMethod[]} shippingMethods - the properties to update on line item
+   * @return {Promise<LineItem[]>} the update line item
+   */
+  async updateHasShipping(
+    lineItems: LineItem[],
+    shippingMethods: ShippingMethod[]
+  ): Promise<LineItem[]> {
+    return this.atomicPhase_(async (manager) => {
+      const lineItemRepository: LineItemRepository =
+        manager.getCustomRepository(this.lineItemRepository_)
+
+      const shippingProfiles = shippingMethods.map(
+        (sm) => sm.shipping_option.profile_id
+      )
+
+      lineItems.forEach((li) => {
+        li.has_shipping =
+          shippingProfiles.find(
+            (sp) => sp === li.variant?.product?.profile_id
+          ) !== undefined
+      })
+
+      const result = await lineItemRepository.save(lineItems)
+      return result
+    })
+  }
+
+  /**
+   * Deletes a line item.
+   * @param {string} id - the id of the line item to delete
+   * @return {Promise} the result of the delete operation
+   */
+  async delete(id): Promise<void> {
+    return this.atomicPhase_(async (manager) => {
+      const lineItemRepository = manager.getCustomRepository(
+        this.lineItemRepository_
+      )
+
+      const lineItem = await lineItemRepository.findOne({ where: { id } })
+
+      if (!lineItem) {
+        return Promise.resolve()
+      }
+
+      await lineItemRepository.remove(lineItem)
+
+      return Promise.resolve()
+    })
+  }
+}
+
+export default LineItemService
