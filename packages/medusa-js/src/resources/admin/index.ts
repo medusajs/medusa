77eab33f1a748c148324d1f6e579924286752d41
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-12-14 15:27:31 +0100

    Adds admin notes API to medusa-js (#923)

--- packages/medusa-js/src/resources/admin/index.ts
+++ packages/medusa-js/src/resources/admin/index.ts
@@ -2,11 +2,13 @@ import BaseResource from "../base"
 import AdminAuthResource from "./auth"
 import AdminCustomersResource from "./customers"
 import AdminDiscountsResource from "./discounts"
+import AdminNotesResource from "./notes"
 
 class Admin extends BaseResource {
   public auth = new AdminAuthResource(this.client)
   public customers = new AdminCustomersResource(this.client)
   public discounts = new AdminDiscountsResource(this.client)
+  public notes = new AdminNotesResource(this.client)
 }
 
 export default Admin
