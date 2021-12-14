4f3d9775dfe7bba78ccfb9340bce5f8ae0b69c40
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-12-14 16:06:39 +0100

    Adds admin giftcard API to medusa-js (#921)

--- packages/medusa-js/src/resources/admin/index.ts
+++ packages/medusa-js/src/resources/admin/index.ts
@@ -2,6 +2,8 @@ import BaseResource from "../base"
 import AdminAuthResource from "./auth"
 import AdminCustomersResource from "./customers"
 import AdminDiscountsResource from "./discounts"
+import AdminDraftOrdersResource from "./draft-orders"
+import AdminGiftCardsResource from "./gift-cards"
 import AdminInvitesResource from "./invites"
 import AdminNotesResource from "./notes"
 
@@ -9,6 +11,8 @@ class Admin extends BaseResource {
   public auth = new AdminAuthResource(this.client)
   public customers = new AdminCustomersResource(this.client)
   public discounts = new AdminDiscountsResource(this.client)
+  public draftOrders = new AdminDraftOrdersResource(this.client)
+  public giftCards = new AdminGiftCardsResource(this.client)
   public invites = new AdminInvitesResource(this.client)
   public notes = new AdminNotesResource(this.client)
 }
