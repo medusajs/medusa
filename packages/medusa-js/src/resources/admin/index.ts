006e67eea1e471e78f34f3db6e7735bbb9fca653
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-12-14 15:34:23 +0100

    Adds admin invites API to medusa-js (#922)

--- packages/medusa-js/src/resources/admin/index.ts
+++ packages/medusa-js/src/resources/admin/index.ts
@@ -2,12 +2,14 @@ import BaseResource from "../base"
 import AdminAuthResource from "./auth"
 import AdminCustomersResource from "./customers"
 import AdminDiscountsResource from "./discounts"
+import AdminInvitesResource from "./invites"
 import AdminNotesResource from "./notes"
 
 class Admin extends BaseResource {
   public auth = new AdminAuthResource(this.client)
   public customers = new AdminCustomersResource(this.client)
   public discounts = new AdminDiscountsResource(this.client)
+  public invites = new AdminInvitesResource(this.client)
   public notes = new AdminNotesResource(this.client)
 }
 
