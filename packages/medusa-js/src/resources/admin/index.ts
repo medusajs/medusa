ee43f08a54cec7a0435dfe8f97c8779e5fcfa481
Zakaria El Asri <33696020+zakariaelas@users.noreply.github.com>
2021-12-16 14:55:03 +0100

    feat: medusa js admin shipping options (#934)
    * add: shipping-options admin endpoint to medusa-js
    
    * fix: retrieve return type for Collections
    
    * fix typo in routes
    
    Co-authored-by: Sebastian Rindom <seb@medusajs.com>

--- packages/medusa-js/src/resources/admin/index.ts
+++ packages/medusa-js/src/resources/admin/index.ts
@@ -12,6 +12,7 @@ import AdminReturnReasonsResource from "./return-reasons"
 import AdminVariantsResource from "./variants"
 import AdminSwapsResource from "./swaps"
 import AdminShippingProfilesResource from "./shipping-profiles"
+import AdminShippingOptionsResource from "./shipping-options"
 import AdminRegionsResource from "./regions"
 
 class Admin extends BaseResource {
@@ -28,6 +29,7 @@ class Admin extends BaseResource {
   public variants = new AdminVariantsResource(this.client)
   public swaps = new AdminSwapsResource(this.client)
   public shippingProfiles = new AdminShippingProfilesResource(this.client)
+  public shippingOptions = new AdminShippingOptionsResource(this.client)
   public regions = new AdminRegionsResource(this.client)
 }
 
