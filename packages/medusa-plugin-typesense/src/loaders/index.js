67b494304bacb895185115da584ab998bca2ec33
Jason Bosco <mail@jasonbos.co>
2022-01-17 23:40:45 -0600

    First pass at adding Typesense specific API calls

--- packages/medusa-plugin-typesense/src/loaders/index.js
+++ packages/medusa-plugin-typesense/src/loaders/index.js
@@ -2,11 +2,12 @@ export default async (container, options) => {
   try {
     const typesenseService = container.resolve("typesenseService")
 
-    await Promise.all(
-      Object.entries(options.settings).map(([key, value]) =>
-        typesenseService.updateSettings(key, value)
-      )
-    )
+    // Not needed in Typesense
+    // await Promise.all(
+    //   Object.entries(options.settings).map(([key, value]) =>
+    //     typesenseService.updateSettings(key, value)
+    //   )
+    // )
   } catch (err) {
     // ignore
     console.log(err)
--- packages/medusa-plugin-typesense/src/services/typesense.js
+++ packages/medusa-plugin-typesense/src/services/typesense.js
@@ -1,43 +1,66 @@
 import { indexTypes } from "medusa-core-utils"
 import { SearchService } from "medusa-interfaces"
 import { transformProduct } from "../utils/transform-product"
+import { Client as Typesense } from "typesense"
 
 class TypesenseSearchService extends SearchService {
   constructor(container, options) {
     super()
 
     this.options_ = options
+
+    // options.config should be the same format used to initialize the Typesense Client
+    //  as described here: https://typesense.org/docs/0.22.1/api/authentication.html
+    this.client_ = new Typesense(options.config)
   }
 
   createIndex(indexName, options) {
-    return Promise.resolve()
+    // options.settings.{indexName} should be the schema of the collection
+    //  as described here: https://typesense.org/docs/0.22.1/api/collections.html#create-a-collection
+
+    return this.client_.collections().create(options.settings[indexName])
   }
 
   getIndex(indexName) {
-    return Promise.resolve()
+    return this.client_.collections(indexName).retrieve()
   }
 
   addDocuments(indexName, documents, type) {
-    return Promise.resolve()
+    const transformedDocuments = this.getTransformedDocuments(type, documents)
+    return this.client_.collections(indexName).documents().import(transformedDocuments)
   }
 
   replaceDocuments(indexName, documents, type) {
-    return Promise.resolve()
+    const transformedDocuments = this.getTransformedDocuments(type, documents)
+    return this.client_.collections(indexName).documents().import(transformedDocuments, {action: "upsert"})
   }
 
-  deleteDocument(indexName, document_id) {
-    return Promise.resolve()
+  deleteDocument(indexName, documentId) {
+    return this.client_.collections(indexName).documents(documentId).delete()
   }
 
-  deleteAllDocuments(indexName) {
-    return Promise.resolve()
+  // TODO: Can we access "options" here?
+  async deleteAllDocuments(indexName, options) {
+    await this.client_.collections(indexName).delete();
+    return this.createIndex(indexName, options)
   }
 
   search(indexName, query, options) {
-    return Promise.resolve()
+    // TODO: What is the format of `filter` below?
+    // Need to convert that into Typesense's filter_by format
+
+    const { paginationOptions, filter, additionalOptions } = options
+
+    return this.client_.collections(indexName).documents().search({
+      per_page: paginationOptions.limit,
+      page: Math.floor(paginationOptions.offset / paginationOptions.limit),
+      filter_by: filter,
+      ...additionalOptions
+    })
   }
 
   updateSettings(indexName, settings) {
+    // No-op in Typesense, since settings are controlled at search time
     return Promise.resolve()
   }
 
