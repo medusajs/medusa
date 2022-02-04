c49414ae162784a34a9c12768e05bf38c4901314
Kasper <kasper@medusajs.com>
2022-02-04 10:37:27 +0100

    updated integration tests

--- integration-tests/api/__tests__/admin/colllections.js
+++ integration-tests/api/__tests__/admin/colllections.js
@@ -165,6 +165,8 @@ describe("/admin/collections", () => {
             id: "test-collection",
             handle: "test-collection",
             title: "Test collection",
+            created_at: expect.any(String),
+            updated_at: expect.any(String),
             products: [
               {
                 collection_id: "test-collection",
@@ -184,6 +186,8 @@ describe("/admin/collections", () => {
             id: "test-collection1",
             handle: "test-collection1",
             title: "Test collection 1",
+            created_at: expect.any(String),
+            updated_at: expect.any(String),
             products: [
               {
                 collection_id: "test-collection1",
@@ -203,6 +207,8 @@ describe("/admin/collections", () => {
             id: "test-collection2",
             handle: "test-collection2",
             title: "Test collection 2",
+            created_at: expect.any(String),
+            updated_at: expect.any(String),
             products: [
               {
                 collection_id: "test-collection2",
@@ -232,6 +238,8 @@ describe("/admin/collections", () => {
             id: "test-collection",
             handle: "test-collection",
             title: "Test collection",
+            created_at: expect.any(String),
+            updated_at: expect.any(String),
             products: [
               {
                 collection_id: "test-collection",
