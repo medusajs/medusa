ae08127c7a964eb45439bed2aa103a9923fb7faf
adrien2p <adrien.deperetti@gmail.com>
2022-02-13 14:43:42 +0100

    feat(payments): Refactor core Payment related

--- packages/medusa/src/services/__mocks__/test-pay.js
+++ packages/medusa/src/services/__mocks__/test-pay.js
@@ -1,9 +1,6 @@
 export const testPayServiceMock = {
   identifier: "test-pay",
   getIdentifier: "test-pay",
-  withTransaction: function () {
-    return this
-  },
   getStatus: jest.fn().mockResolvedValue(Promise.resolve("authorised")),
   retrieveSavedMethods: jest.fn().mockResolvedValue(Promise.resolve([])),
   getPaymentData: jest.fn().mockResolvedValue(Promise.resolve({})),
