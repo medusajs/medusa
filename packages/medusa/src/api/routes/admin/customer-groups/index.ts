ad1a700edc63d89f87aa6839014d0667b507b259
fPolic <frane@medusajs.com>
2022-02-16 11:55:21 +0100

    feat: GET customer group endpoint

--- packages/medusa/src/api/routes/admin/customer-groups/index.ts
+++ packages/medusa/src/api/routes/admin/customer-groups/index.ts
@@ -8,6 +8,7 @@ const route = Router()
 export default (app) => {
   app.use("/customer-groups", route)
 
+  route.get("/:id", middlewares.wrap(require("./get-customer-group").default))
   route.post("/", middlewares.wrap(require("./create-customer-group").default))
   return app
 }
