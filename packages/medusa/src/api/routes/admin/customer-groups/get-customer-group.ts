72367c85c21af570f1d59867497a5c7deeb4a914
fPolic <frane@medusajs.com>
2022-02-16 11:55:21 +0100

    feat: GET customer group endpoint

---
+++ packages/medusa/src/api/routes/admin/customer-groups/get-customer-group.ts
@@ -0,0 +1,32 @@
+import { CustomerGroupService } from "../../../../services"
+
+/**
+ * @oas [get] /customer-group/{id}
+ * operationId: "GetCustomerGroups"
+ * summary: "Retrieve a CustomerGroup"
+ * description: "Retrieves a Customer Group."
+ * x-authenticated: true
+ * parameters:
+ *   - (path) id=* {string} The id of the Customer Group.
+ * tags:
+ *   - CustomerGroup
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             customerGroup:
+ *               $ref: "#/components/schemas/customer-group"
+ */
+export default async (req, res) => {
+  const { id } = req.params
+  const customerGroupService: CustomerGroupService = req.scope.resolve(
+    "customerGroupService"
+  )
+
+  const customerGroup = await customerGroupService.retrieve(id)
+
+  res.json({ customerGroup })
+}
