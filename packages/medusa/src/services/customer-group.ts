e14645a29f6f5d0c7fda9d9723e970386db93ab6
Philip Korsholm <philip.korsholm@hotmail.com>
2022-02-17 16:01:44 +0100

    add jsdoc

--- packages/medusa/src/services/customer-group.ts
+++ packages/medusa/src/services/customer-group.ts
@@ -39,6 +39,11 @@ class CustomerGroupService extends BaseService {
     return cloned
   }
 
+  /**
+   * Creates a customer group with the provided data.
+   * @param {DeepPartial<CustomerGroup>} group - the customergroup to create
+   * @return {Promise} the result of the create operation
+   */
   async create(group: DeepPartial<CustomerGroup>): Promise<CustomerGroup> {
     return this.atomicPhase_(async (manager) => {
       try {
