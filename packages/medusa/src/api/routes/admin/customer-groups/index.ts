3b878bc5dddd70a389b2993ebfb86c96c62d3130
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-02-18 09:58:54 +0100

    Feat: Create customer group (#1074)
    * fix babel transform-runtime regenerator required for migrations
    
    * add customer group model
    
    * add migration for customer group
    
    * add customer group model export
    
    * add customer group repository
    
    * add customer group service
    
    * add CustomerGroupRepository to "withTransaction" in CustomerGroupService
    
    * remove unnecessary argument to runtime plugin
    
    * service export ordering
    
    * add create customer group endpoint
    
    * add customergroup to route index in admin
    
    * add customer group service
    
    * add customer groups test
    
    * cleanup
    
    * duplicate error handling
    
    * ducplicate name integration test
    
    * add jsdoc
    
    * customergroup not customer
    
    * pr feedback
    
    * pipeline test
    
    * fix weird merge
    
    Co-authored-by: Sebastian Rindom <skrindom@gmail.com>

---
+++ packages/medusa/src/api/routes/admin/customer-groups/index.ts
@@ -0,0 +1,25 @@
+import { Router } from "express"
+import { CustomerGroup } from "../../../.."
+import { DeleteResponse, PaginatedResponse } from "../../../../types/common"
+import middlewares from "../../../middlewares"
+
+const route = Router()
+
+export default (app) => {
+  app.use("/customer-groups", route)
+
+  route.post("/", middlewares.wrap(require("./create-customer-group").default))
+  return app
+}
+
+export type AdminCustomerGroupsRes = {
+  customer_group: CustomerGroup
+}
+
+export type AdminCustomerGroupsDeleteRes = DeleteResponse
+
+export type AdminCustomerGroupsListRes = PaginatedResponse & {
+  customer_groups: CustomerGroup[]
+}
+
+export * from "./create-customer-group"
