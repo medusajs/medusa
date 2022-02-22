728c201dbe322cfc25265b5edc21870742f21ef8
fPolic <frane@medusajs.com>
2022-02-22 16:24:30 +0100

    update customer tests

--- integration-tests/api/__tests__/admin/customer.js
+++ integration-tests/api/__tests__/admin/customer.js
@@ -250,7 +250,7 @@ describe("/admin/customers", () => {
       expect(response.status).toEqual(200)
       expect(response.data.customer.groups).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ id: "test-group-4", name: "test-group-4" }),
+          expect.objectContaining({ id: "test-group-4", name: "Test group 4" }),
         ])
       )
 
@@ -276,7 +276,7 @@ describe("/admin/customers", () => {
       expect(response.data.customer.groups.length).toEqual(1)
       expect(response.data.customer.groups).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ id: "test-group-4", name: "test-group-4" }),
+          expect.objectContaining({ id: "test-group-4", name: "Test group 4" }),
         ])
       )
 
@@ -323,8 +323,8 @@ describe("/admin/customers", () => {
       expect(response.data.customer.groups.length).toEqual(2)
       expect(response.data.customer.groups).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ id: "test-group-4", name: "test-group-4" }),
-          expect.objectContaining({ id: "test-group-5", name: "test-group-5" }),
+          expect.objectContaining({ id: "test-group-4", name: "Test group 4" }),
+          expect.objectContaining({ id: "test-group-5", name: "Test group 5" }),
         ])
       )
     })
