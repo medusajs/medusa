dd192a736bece47f452bdf87a0add2dbd2215deb
Philip Korsholm <philip.korsholm@hotmail.com>
2022-02-22 10:36:33 +0100

    delete fix

--- packages/medusa/src/repositories/customer-group.ts
+++ packages/medusa/src/repositories/customer-group.ts
@@ -9,7 +9,6 @@ export class CustomerGroupRepository extends Repository<CustomerGroup> {
     groupId: string,
     customerIds: CustomerBatchIds[]
   ): Promise<CustomerGroup | undefined> {
-    // try {
     const customerGroup = await this.findOne(groupId)
     if (!customerGroup) {
       throw new MedusaError(
@@ -26,34 +25,7 @@ export class CustomerGroupRepository extends Repository<CustomerGroup> {
         customer_id: In(customerIds.map(({ id }) => id)),
       })
       .execute()
-    // customerIds.map(({ id }) => ({
-    //   customer_id: id,
-    //   customer_group_id: groupId,
-    // }))
 
     return customerGroup
-    // } catch (error) {
-    //   if (error.code === "23503") {
-    //     const existingCustomers = await getConnection()
-    //       .getRepository(Customer)
-    //       .createQueryBuilder("customer")
-    //       .where("customer.id IN (:...customer_ids)", {
-    //         customer_ids: customerIds.map(({ id }) => id),
-    //       })
-    //       .getMany()
-
-    //     const nonExistingCustomers = customerIds.filter(
-    //       (cId) => existingCustomers.findIndex((el) => el.id === cId.id) === -1
-    //     )
-
-    //     throw new MedusaError(
-    //       MedusaError.Types.INVALID_DATA,
-    //       `The following customer ids do not exist: ${JSON.stringify(
-    //         nonExistingCustomers.map(({ id }) => id).join(", ")
-    //       )}`
-    //     )
-    //   }
-    //   throw error
-    // }
   }
 }
