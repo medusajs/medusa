11b02f5cc5fb2a8be1267919ed8b00b2f5d2eb92
Philip Korsholm <philip.korsholm@hotmail.com>
2022-02-23 09:58:23 +0100

    duplicate exception hadnling

--- packages/medusa/src/utils/exception-formatter.ts
+++ packages/medusa/src/utils/exception-formatter.ts
@@ -2,13 +2,20 @@ import { MedusaError } from "medusa-core-utils"
 
 export enum PostgresError {
   DUPLICATE_ERROR = "23505",
-  FOREIGN_KEY_ERROR = "23503"
+  FOREIGN_KEY_ERROR = "23503",
 }
 export const formatException = (err): Error => {
   switch (err.code) {
-    case POSTGRES_DUPLICATE_ERROR:
-      return new MedusaError(MedusaError.Types.DUPLICATE_ERROR, err.detail)
-    case POSTGRES_FOREIGN_KEY_ERROR:
+    case PostgresError.DUPLICATE_ERROR:
+      return new MedusaError(
+        MedusaError.Types.DUPLICATE_ERROR,
+        `${err.table.charAt(0).toUpperCase()}${err.table.slice(
+          1
+        )} with ${err.detail
+          .slice(4)
+          .replace(/[()=]/g, (s) => (s === "=" ? " " : ""))}`
+      )
+    case PostgresError.FOREIGN_KEY_ERROR:
       return new MedusaError(MedusaError.Types.INVALID_DATA, err.detail)
     default:
       return err
