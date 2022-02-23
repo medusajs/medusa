b943d25dc262c4fdbebe6ce507e45f2ba71b3880
Philip Korsholm <philip.korsholm@hotmail.com>
2022-02-23 10:48:17 +0100

    rename err to error in exception formatter

--- packages/medusa/src/utils/exception-formatter.ts
+++ packages/medusa/src/utils/exception-formatter.ts
@@ -4,25 +4,27 @@ export enum PostgresError {
   DUPLICATE_ERROR = "23505",
   FOREIGN_KEY_ERROR = "23503",
 }
-export const formatException = (err): Error => {
-  switch (err.code) {
+export const formatException = (error): Error => {
+  switch (error.code) {
     case PostgresError.DUPLICATE_ERROR:
+      // example error.detail: Key (handle)=(test-collection) already exists
       return new MedusaError(
         MedusaError.Types.DUPLICATE_ERROR,
-        `${err.table.charAt(0).toUpperCase()}${err.table.slice(
+        `${error.table.charAt(0).toUpperCase()}${error.table.slice(
           1
-        )} with ${err.detail
+        )} with ${error.detail
           .slice(4)
           .replace(/[()=]/g, (s) => (s === "=" ? " " : ""))}`
       )
     case PostgresError.FOREIGN_KEY_ERROR: {
+      // example error.detail: Key (customer_id)=(test-customer-27) is not present in table "customer".
       const matches =
         /Key \(([\w-\d]+)\)=\(([\w-\d]+)\) is not present in table "(\w+)"/g.exec(
-          err.detail
+          error.detail
         )
 
       if (matches?.length !== 4) {
-        return new MedusaError(MedusaError.Types.NOT_FOUND, err.detail)
+        return new MedusaError(MedusaError.Types.NOT_FOUND, error.detail)
       }
 
       return new MedusaError(
@@ -33,6 +35,6 @@ export const formatException = (err): Error => {
       )
     }
     default:
-      return err
+      return error
   }
 }
