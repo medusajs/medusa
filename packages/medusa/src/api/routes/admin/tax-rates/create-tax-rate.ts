11d2eaff848471368e9f5232fdd85811ca89ec10
Sebastian Rindom <skrindom@gmail.com>
2022-02-23 13:27:46 +0100

    fix: cleanup api

--- packages/medusa/src/api/routes/admin/tax-rates/create-tax-rate.ts
+++ packages/medusa/src/api/routes/admin/tax-rates/create-tax-rate.ts
@@ -5,12 +5,7 @@ import { MedusaError } from "medusa-core-utils"
 
 import { pickByConfig, getRetrieveConfig } from "./utils/get-query-config"
 import { TaxRate } from "../../../.."
-import {
-  ProductService,
-  ProductTypeService,
-  ShippingOptionService,
-  TaxRateService,
-} from "../../../../services"
+import { TaxRateService } from "../../../../services"
 import { validator } from "../../../../utils/validator"
 import { IsType } from "../../../../utils/validators/is-type"
 
@@ -41,12 +36,6 @@ export default async (req, res) => {
 
   const manager: EntityManager = req.scope.resolve("manager")
   const rateService: TaxRateService = req.scope.resolve("taxRateService")
-  const productService: ProductService = req.scope.resolve("productService")
-  const productTypeService: ProductTypeService =
-    req.scope.resolve("productTypeService")
-  const shippingOptionService: ShippingOptionService = req.scope.resolve(
-    "shippingOptionService"
-  )
 
   let id: string | undefined
   await manager.transaction(async (tx) => {
@@ -57,69 +46,15 @@ export default async (req, res) => {
     id = created.id
 
     if (typeof value.products !== "undefined") {
-      try {
-        await txRateService.addToProduct(id, value.products)
-      } catch (err) {
-        if (err.code === "23503") {
-          // A foreign key constraint failed meaning some thing doesn't exist
-          // either it is a product or the tax rate itself. Using Promise.all
-          // will try to retrieve all of the resources and will fail when
-          // something is not found.
-          await Promise.all([
-            txRateService.retrieve(id, { select: ["id"] }),
-            ...value.products.map((pId) =>
-              productService.retrieve(pId, { select: ["id"] })
-            ),
-          ])
-        }
-
-        throw err
-      }
+      await txRateService.addToProduct(id, value.products)
     }
 
     if (typeof value.product_types !== "undefined") {
-      try {
-        await txRateService.addToProductType(id, value.product_types)
-      } catch (err) {
-        if (err.code === "23503") {
-          // A foreign key constraint failed meaning some thing doesn't exist
-          // either it is a product or the tax rate itself. Using Promise.all
-          // will try to retrieve all of the resources and will fail when
-          // something is not found.
-          await Promise.all([
-            txRateService.retrieve(id, { select: ["id"] }) as Promise<unknown>,
-            ...value.product_types.map(
-              (pId) =>
-                productTypeService.retrieve(pId, {
-                  select: ["id"],
-                }) as Promise<unknown>
-            ),
-          ])
-        }
-
-        throw err
-      }
+      await txRateService.addToProductType(id, value.product_types)
     }
 
     if (typeof value.shipping_options !== "undefined") {
-      try {
-        await txRateService.addToShippingOption(id, value.shipping_options)
-      } catch (err) {
-        if (err.code === "23503") {
-          // A foreign key constraint failed meaning some thing doesn't exist
-          // either it is a product or the tax rate itself. Using Promise.all
-          // will try to retrieve all of the resources and will fail when
-          // something is not found.
-          await Promise.all([
-            txRateService.retrieve(id, { select: ["id"] }),
-            ...value.shipping_options.map((sId) =>
-              shippingOptionService.retrieve(sId, { select: ["id"] })
-            ),
-          ])
-        }
-
-        throw err
-      }
+      await txRateService.addToShippingOption(id, value.shipping_options)
     }
   })
 
