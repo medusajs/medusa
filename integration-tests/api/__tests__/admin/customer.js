ee2db7b335dc09af86ae542307d1291ec86e8a7f
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-02-25 15:29:13 +0100

    Feat: bulk delete customers from customer group (#1097)
    * integration testing
    
    * customer seeder
    
    * initial bulk removal
    
    * integraiton testing of deletes
    
    * delete fix
    
    * not found test
    
    * remove unused code
    
    * Apply suggestions from code review
    
    Co-authored-by: Sebastian Rindom <skrindom@gmail.com>
    
    * update integration tests
    
    * pr review fixes
    
    * update migration
    
    * formatting
    
    * integration tests for deletion
    
    * pr feedback
    
    * fix failing integration tests
    
    * remove integration tests before merging
    
    Co-authored-by: Sebastian Rindom <skrindom@gmail.com>

--- integration-tests/api/__tests__/admin/customer.js
+++ integration-tests/api/__tests__/admin/customer.js
@@ -56,7 +56,7 @@ describe("/admin/customers", () => {
         })
 
       expect(response.status).toEqual(200)
-      expect(response.data.count).toEqual(6)
+      expect(response.data.count).toEqual(8)
       expect(response.data.customers).toEqual(
         expect.arrayContaining([
           expect.objectContaining({
