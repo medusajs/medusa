1f77ff54951a3372bd53e1f5b232295997db0ec2
Sebastian Rindom <skrindom@gmail.com>
2022-02-26 20:33:32 +0100

    fix: dedupe type

--- packages/medusa/src/types/customer-groups.ts
+++ packages/medusa/src/types/customer-groups.ts
@@ -3,10 +3,6 @@ import { IsType } from "../utils/validators/is-type"
 
 import { StringComparisonOperator } from "./common"
 
-export class CustomerGroupsBatchCustomer {
-  @IsString()
-  id: string
-}
 export class FilterableCustomerGroupProps {
   @ValidateNested()
   @IsType([String, [String], StringComparisonOperator])
@@ -17,6 +13,7 @@ export class CustomerGroupsBatchCustomer {
   @IsString()
   id: string
 }
+
 export class CustomerGroupUpdate {
   name?: string
   metadata?: object
