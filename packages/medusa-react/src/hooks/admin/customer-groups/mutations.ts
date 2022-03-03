d88c2bd5a8ae9d9e678ec78b0fae66abcca38cd4
fPolic <frane@medusajs.com>
2022-03-03 11:18:42 +0100

    feat: add create/update customer groups hooks

--- packages/medusa-react/src/hooks/admin/customer-groups/mutations.ts
+++ packages/medusa-react/src/hooks/admin/customer-groups/mutations.ts
@@ -1,1 +1,49 @@
-export const v = () => {}
+import {
+  AdminCustomerGroupsRes,
+  AdminPostCustomerGroupsGroupReq,
+  AdminPostCustomerGroupsReq,
+} from "@medusajs/medusa"
+import { useMutation, UseMutationOptions, useQueryClient } from "react-query"
+import { Response } from "@medusajs/medusa-js"
+
+import { useMedusa } from "../../../contexts/medusa"
+import { buildOptions } from "../../utils/buildOptions"
+import { adminCustomerGroupKeys } from "./queries"
+
+export const useAdminCreateCustomerGroup = (
+  options?: UseMutationOptions<
+    Response<AdminCustomerGroupsRes>,
+    Error,
+    AdminPostCustomerGroupsReq
+  >
+) => {
+  const { client } = useMedusa()
+  const queryClient = useQueryClient()
+
+  return useMutation(
+    (payload: AdminPostCustomerGroupsReq) =>
+      client.admin.customerGroups.create(payload),
+    buildOptions(queryClient, adminCustomerGroupKeys.lists(), options)
+  )
+}
+
+export const useAdminUpdateCustomerGroup = (
+  id: string,
+  options?: UseMutationOptions<
+    Response<AdminCustomerGroupsRes>,
+    Error,
+    AdminPostCustomerGroupsGroupReq
+  >
+) => {
+  const { client } = useMedusa()
+  const queryClient = useQueryClient()
+  return useMutation(
+    (payload: AdminPostCustomerGroupsGroupReq) =>
+      client.admin.customerGroups.update(id, payload),
+    buildOptions(
+      queryClient,
+      [adminCustomerGroupKeys.lists(), adminCustomerGroupKeys.detail(id)],
+      options
+    )
+  )
+}
