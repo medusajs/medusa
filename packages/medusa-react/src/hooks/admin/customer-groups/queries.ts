79cd335dae68dad2708d9fe95a899336d9b8883f
fPolic <frane@medusajs.com>
2022-03-03 11:08:07 +0100

    feat: add customer groups query hooks

---
+++ packages/medusa-react/src/hooks/admin/customer-groups/queries.ts
@@ -0,0 +1,53 @@
+import {
+  AdminCustomerGroupsListRes,
+  AdminCustomerGroupsRes,
+  AdminGetCustomerGroupsParams,
+} from "@medusajs/medusa"
+import { Response } from "@medusajs/medusa-js"
+import { useQuery } from "react-query"
+
+import { useMedusa } from "../../../contexts"
+import { UseQueryOptionsWrapper } from "../../../types"
+import { queryKeysFactory } from "../../utils/index"
+
+const ADMIN_CUSTOMER_GROUPS_QUERY_KEY = `admin_customer_groups` as const
+
+export const adminCustomerGroupKeys = queryKeysFactory(
+  ADMIN_CUSTOMER_GROUPS_QUERY_KEY
+)
+
+type CustomerGroupQueryKeys = typeof adminCustomerGroupKeys
+
+export const useAdminCustomerGroup = (
+  id: string,
+  options?: UseQueryOptionsWrapper<
+    Response<AdminCustomerGroupsRes>,
+    Error,
+    ReturnType<CustomerGroupQueryKeys["detail"]>
+  >
+) => {
+  const { client } = useMedusa()
+  const { data, ...rest } = useQuery(
+    adminCustomerGroupKeys.detail(id),
+    () => client.admin.customerGroups.retrieve(id),
+    options
+  )
+  return { ...data, ...rest } as const
+}
+
+export const useAdminCustomerGroups = (
+  query?: AdminGetCustomerGroupsParams,
+  options?: UseQueryOptionsWrapper<
+    Response<AdminCustomerGroupsListRes>,
+    Error,
+    ReturnType<CustomerGroupQueryKeys["list"]>
+  >
+) => {
+  const { client } = useMedusa()
+  const { data, ...rest } = useQuery(
+    adminCustomerGroupKeys.list(query),
+    () => client.admin.customerGroups.list(query),
+    options
+  )
+  return { ...data, ...rest } as const
+}
