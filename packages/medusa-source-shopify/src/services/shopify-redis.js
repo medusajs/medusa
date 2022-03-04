f1932c159f6d9b44b781f9aa018ea49a0042a30b
Kasper <kasper@medusajs.com>
2022-03-04 11:27:37 +0100

    clear ignored keys in redis

--- packages/medusa-source-shopify/src/services/shopify-redis.js
+++ packages/medusa-source-shopify/src/services/shopify-redis.js
@@ -1,4 +1,3 @@
-// shopify-redis
 import { BaseService } from "medusa-interfaces"
 import { IGNORE_THRESHOLD } from "../utils/const"
 
@@ -13,7 +12,7 @@ class shopifyRedisService extends BaseService {
   }
 
   async addIgnore(id, side) {
-    const key = `${id}_ignore_${side}`
+    const key = `sh_${id}_ignore_${side}`
     return await this.redis_.set(
       key,
       1,
@@ -23,18 +22,34 @@ class shopifyRedisService extends BaseService {
   }
 
   async shouldIgnore(id, action) {
-    const key = `${id}_ignore_${action}`
+    const key = `sh_${id}_ignore_${action}`
     return await this.redis_.get(key)
   }
 
-  async addUniqueValue(uniqueVal, type) {
-    const key = `${uniqueVal}_${type}`
-    return await this.redis_.set(key, 1, "EX", 60 * 5)
-  }
+  async clearIgnores() {
+    let ignoredKeys = 0
 
-  async getUniqueValue(uniqueVal, type) {
-    const key = `${uniqueVal}_${type}`
-    return await this.redis_.get(key)
+    const stream = this.redis_.scanStream({
+      match: "sh_*_ignore_*",
+    })
+
+    stream.on("data", async (keys) => {
+      // `keys` is an array of strings representing key names
+      if (keys.length) {
+        ignoredKeys = keys.length
+        const pipeline = this.redis_.pipeline()
+        keys.forEach(function (key) {
+          pipeline.del(key)
+        })
+        pipeline.exec()
+      }
+    })
+
+    stream.on("end", function () {
+      console.log(
+        `medusa-source-sopify: Redis scan stream ended, and ${ignoredKeys} ignored keys was cleared`
+      )
+    })
   }
 }
 
