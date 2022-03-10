60c70fb12c65966227a661ed9dd2ab9c65771980
olivermrbl <oliver@mrbltech.com>
2022-03-10 13:52:36 +0100

    Fix commented code

--- packages/medusa/src/api/middlewares/error-handler.js
+++ packages/medusa/src/api/middlewares/error-handler.js
@@ -50,10 +50,9 @@ export default () => {
       case MedusaError.Types.INVALID_ARGUMENT:
         break
       default:
-        // console.error(">>>>: ", errObj)
-        // errObj.code = "unknown_error"
-        // errObj.message = "An unknown error occurred."
-        // errObj.type = "unknown_error"
+        errObj.code = "unknown_error"
+        errObj.message = "An unknown error occurred."
+        errObj.type = "unknown_error"
         break
     }
 
