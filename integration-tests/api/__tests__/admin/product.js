cf2892a30badd3d8c271398347881f031a3d48bc
Philip Korsholm <philip.korsholm@hotmail.com>
2022-03-10 18:29:34 +0100

    run with verbose

--- integration-tests/api/__tests__/admin/product.js
+++ integration-tests/api/__tests__/admin/product.js
@@ -17,7 +17,7 @@ describe("/admin/products", () => {
   beforeAll(async () => {
     const cwd = path.resolve(path.join(__dirname, "..", ".."))
     dbConnection = await initDb({ cwd })
-    medusaProcess = await setupServer({ cwd })
+    medusaProcess = await setupServer({ cwd, verbose: true })
   })
 
   afterAll(async () => {
