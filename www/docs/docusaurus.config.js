f931a5b0b82a6588a0844b773e147105b8e37639
Shahed Nasser <shahednasser@gmail.com>
2022-03-25 10:12:25 +0200

    updated docsaurus

--- www/docs/docusaurus.config.js
+++ www/docs/docusaurus.config.js
@@ -1,12 +1,14 @@
 const path = require("path")
 const docsPath = path.join(__dirname, "../../docs/content")
 
+console.log(docsPath);
+
 const algoliaAppId = process.env.ALGOLIA_APP_ID || "temp"
 const algoliaApiKey = process.env.ALGOLIA_API_KEY || "temp"
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "Medusa Commerce",
+  title: "Medusa",
   tagline: "Explore and learn how to use Medusa",
   url: "https://docs.medusajs.com",
   baseUrl: "/",
@@ -25,7 +27,10 @@ module.exports = {
     ],
   ],
   themeConfig: {
-    disableSwitch: true,
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+    },
     algolia: {
       apiKey: algoliaApiKey,
       indexName: "medusa-commerce",
@@ -35,8 +40,8 @@ module.exports = {
     prism: {
       defaultLanguage: "js",
       plugins: ["line-numbers", "show-language"],
-      theme: require("@kiwicopple/prism-react-renderer/themes/vsDark"),
-      darkTheme: require("@kiwicopple/prism-react-renderer/themes/vsDark"),
+      theme: require("prism-react-renderer/themes/vsDark"),
+      darkTheme: require("prism-react-renderer/themes/vsDark"),
     },
     navbar: {
       hideOnScroll: true,
@@ -44,6 +49,7 @@ module.exports = {
         alt: "Medusa Commerce",
         src: "img/logo.svg",
         srcDark: "img/logo.svg",
+        width: 100
       },
       items: [
         {
@@ -98,12 +104,12 @@ module.exports = {
           title: "More",
           items: [
             {
-              label: "Contact",
-              href: "https://medusa-commere.com",
+              label: "Medusa Home",
+              href: "https://medusajs.com",
             },
             {
-              label: "Privacy & Terms",
-              href: "https://medusa-commere.com",
+              label: "Contact",
+              href: "https://ky5eo2x1u81.typeform.com/get-in-touch",
             },
             {
               label: "GitHub",
@@ -112,7 +118,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `© ${new Date().getFullYear()} Medusa Commerce`,
+      copyright: `© ${new Date().getFullYear()} Medusa`,
     },
   },
   presets: [
