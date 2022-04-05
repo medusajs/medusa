5e25ba6805320c8d4b8f3bdbdda61db65687a617
Zakaria El Asri <zakaria.elas@gmail.com>
2022-04-05 12:25:10 +0000

    remove: verbose option

--- integration-tests/api/__tests__/store/cart.js
+++ integration-tests/api/__tests__/store/cart.js
@@ -42,7 +42,7 @@ describe("/store/carts", () => {
     const cwd = path.resolve(path.join(__dirname, "..", ".."))
     try {
       dbConnection = await initDb({ cwd })
-      medusaProcess = await setupServer({ cwd, verbose: true })
+      medusaProcess = await setupServer({ cwd })
     } catch (error) {
       console.log(error)
     }
