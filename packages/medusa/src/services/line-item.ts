af6475bcd3f0c026b44023ec0ad6b42933eee9e7
adrien2p <adrien.deperetti@gmail.com>
2022-04-11 15:09:27 +0200

    feat(medusa): Cleanup line-item service

--- packages/medusa/src/services/line-item.ts
+++ packages/medusa/src/services/line-item.ts
@@ -8,13 +8,13 @@ import { CartRepository } from "../repositories/cart"
 import { LineItem } from "../models/line-item"
 
 type InjectedDependencies = {
-  manager: EntityManager;
-  lineItemRepository: typeof LineItemRepository;
-  lineItemTaxLineRepository: typeof LineItemTaxLineRepository;
-  cartRepository: typeof CartRepository;
-  productVariantService: ProductVariantService;
-  productService: ProductService;
-  regionService: RegionService;
+  manager: EntityManager
+  lineItemRepository: typeof LineItemRepository
+  lineItemTaxLineRepository: typeof LineItemTaxLineRepository
+  cartRepository: typeof CartRepository
+  productVariantService: ProductVariantService
+  productService: ProductService
+  regionService: RegionService
 }
 
 /**
@@ -30,17 +30,15 @@ class LineItemService extends BaseService {
   protected readonly productService_: ProductService
   protected readonly regionService_: RegionService
 
-  constructor(
-    {
-      manager,
-      lineItemRepository,
-      lineItemTaxLineRepository,
-      productVariantService,
-      productService,
-      regionService,
-      cartRepository
-    }: InjectedDependencies
-  ) {
+  constructor({
+    manager,
+    lineItemRepository,
+    lineItemTaxLineRepository,
+    productVariantService,
+    productService,
+    regionService,
+    cartRepository,
+  }: InjectedDependencies) {
     super()
 
     this.manager_ = manager
@@ -64,7 +62,7 @@ class LineItemService extends BaseService {
       productVariantService: this.productVariantService_,
       productService: this.productService_,
       regionService: this.regionService_,
-      cartRepository: this.cartRepository_
+      cartRepository: this.cartRepository_,
     })
 
     cloned.transactionManager_ = transactionManager
@@ -74,11 +72,17 @@ class LineItemService extends BaseService {
 
   async list(
     selector,
-    config = { skip: 0, take: 50, order: { created_at: "DESC" } },
+    config = { skip: 0, take: 50, order: { created_at: "DESC" } }
   ): Promise<LineItem[]> {
-    const lineItemRepo = this.manager_.getCustomRepository(this.lineItemRepository_)
-    const query = this.buildQuery_(selector, config)
-    return lineItemRepo.find(query)
+    return await this.atomicPhase_(
+      async (transactionManager: EntityManager) => {
+        const lineItemRepo = transactionManager.getCustomRepository(
+          this.lineItemRepository_
+        )
+        const query = this.buildQuery_(selector, config)
+        return await lineItemRepo.find(query)
+      }
+    )
   }
 
   /**
@@ -88,23 +92,27 @@ class LineItemService extends BaseService {
    * @return {Promise<LineItem | never>} the line item
    */
   async retrieve(id: string, config = {}): Promise<LineItem | never> {
-    const lineItemRepository = this.manager_.getCustomRepository(
-      this.lineItemRepository_
+    return await this.atomicPhase_(
+      async (transactionManager: EntityManager) => {
+        const lineItemRepository = transactionManager.getCustomRepository(
+          this.lineItemRepository_
+        )
+
+        const validatedId = this.validateId_(id)
+        const query = this.buildQuery_({ id: validatedId }, config)
+
+        const lineItem = await lineItemRepository.findOne(query)
+
+        if (!lineItem) {
+          throw new MedusaError(
+            MedusaError.Types.NOT_FOUND,
+            `Line item with ${id} was not found`
+          )
+        }
+
+        return lineItem
+      }
     )
-
-    const validatedId = this.validateId_(id)
-    const query = this.buildQuery_({ id: validatedId }, config)
-
-    const lineItem = await lineItemRepository.findOne(query)
-
-    if (!lineItem) {
-      throw new MedusaError(
-        MedusaError.Types.NOT_FOUND,
-        `Line item with ${id} was not found`
-      )
-    }
-
-    return lineItem
   }
 
   /**
@@ -114,41 +122,49 @@ class LineItemService extends BaseService {
    * @param {string} cartId - the cart to assign the return line items to.
    * @return {Promise<LineItem[]>} the created line items
    */
-  async createReturnLines(returnId: string, cartId: string): Promise<LineItem[]> {
-    const lineItemRepo = this.manager_.getCustomRepository(
-      this.lineItemRepository_
-    )
-
-    const itemTaxLineRepo = this.manager_.getCustomRepository(
-      this.itemTaxLineRepo_
-    )
-
-    const returnLineItems = await lineItemRepo.findByReturn(returnId)
-      .then(lineItems => {
-        return lineItems.map(lineItem =>
-          lineItemRepo.create({
-            cart_id: cartId,
-            thumbnail: lineItem.thumbnail,
-            is_return: true,
-            title: lineItem.title,
-            variant_id: lineItem.variant_id,
-            unit_price: -1 * lineItem.unit_price,
-            quantity: lineItem.return_item.quantity,
-            allow_discounts: lineItem.allow_discounts,
-            tax_lines: lineItem.tax_lines.map((taxLine) => {
-              return itemTaxLineRepo.create({
-                name: taxLine.name,
-                code: taxLine.code,
-                rate: taxLine.rate,
-                metadata: taxLine.metadata,
-              })
-            }),
-            metadata: lineItem.metadata,
-          })
+  async createReturnLines(
+    returnId: string,
+    cartId: string
+  ): Promise<LineItem[]> {
+    return await this.atomicPhase_(
+      async (transactionManager: EntityManager) => {
+        const lineItemRepo = transactionManager.getCustomRepository(
+          this.lineItemRepository_
         )
-      })
 
-    return await lineItemRepo.save(returnLineItems)
+        const itemTaxLineRepo = transactionManager.getCustomRepository(
+          this.itemTaxLineRepo_
+        )
+
+        const returnLineItems = await lineItemRepo
+          .findByReturn(returnId)
+          .then((lineItems) => {
+            return lineItems.map((lineItem) =>
+              lineItemRepo.create({
+                cart_id: cartId,
+                thumbnail: lineItem.thumbnail,
+                is_return: true,
+                title: lineItem.title,
+                variant_id: lineItem.variant_id,
+                unit_price: -1 * lineItem.unit_price,
+                quantity: lineItem.return_item.quantity,
+                allow_discounts: lineItem.allow_discounts,
+                tax_lines: lineItem.tax_lines.map((taxLine) => {
+                  return itemTaxLineRepo.create({
+                    name: taxLine.name,
+                    code: taxLine.code,
+                    rate: taxLine.rate,
+                    metadata: taxLine.metadata,
+                  })
+                }),
+                metadata: lineItem.metadata,
+              })
+            )
+          })
+
+        return await lineItemRepo.save(returnLineItems)
+      }
+    )
   }
 
   async generate(
@@ -156,48 +172,54 @@ class LineItemService extends BaseService {
     regionId: string,
     quantity: number,
     config: {
-      unit_price?: number;
-      metadata?: Record<string, unknown>;
+      unit_price?: number
+      metadata?: Record<string, unknown>
       customer_id?: string
     } = {}
   ): Promise<LineItem> {
-    return this.atomicPhase_(async (manager: EntityManager) => {
-      const [variant, region] = await Promise.all([
-        this.productVariantService_.withTransaction(manager).retrieve(variantId, {
-          relations: ["product"],
-          include_discount_prices: true,
-        }),
-        this.regionService_.withTransaction(manager).retrieve(regionId),
-      ])
+    return await this.atomicPhase_(
+      async (transactionManager: EntityManager) => {
+        const [variant, region] = await Promise.all([
+          this.productVariantService_
+            .withTransaction(transactionManager)
+            .retrieve(variantId, {
+              relations: ["product"],
+              include_discount_prices: true,
+            }),
+          this.regionService_
+            .withTransaction(transactionManager)
+            .retrieve(regionId),
+        ])
 
-      let unit_price = Number(config.unit_price) < 0 ? 0 : config.unit_price
-      let shouldMerge = false
+        let unit_price = Number(config.unit_price) < 0 ? 0 : config.unit_price
+        let shouldMerge = false
 
-      if (config.unit_price === undefined || config.unit_price === null) {
-        shouldMerge = true;
-        unit_price = await this.productVariantService_
-          .withTransaction(manager)
-          .getRegionPrice(variant.id, {
-            regionId: region.id,
-            quantity: quantity,
-            customer_id: config?.customer_id,
-            include_discount_prices: true,
-          })
+        if (config.unit_price === undefined || config.unit_price === null) {
+          shouldMerge = true
+          unit_price = await this.productVariantService_
+            .withTransaction(transactionManager)
+            .getRegionPrice(variant.id, {
+              regionId: region.id,
+              quantity: quantity,
+              customer_id: config?.customer_id,
+              include_discount_prices: true,
+            })
+        }
+
+        return {
+          unit_price,
+          title: variant.product.title,
+          description: variant.title,
+          thumbnail: variant.product.thumbnail,
+          variant_id: variant.id,
+          quantity: quantity || 1,
+          allow_discounts: variant.product.discountable,
+          is_giftcard: variant.product.is_giftcard,
+          metadata: config?.metadata || {},
+          should_merge: shouldMerge,
+        }
       }
-
-      return {
-        unit_price,
-        title: variant.product.title,
-        description: variant.title,
-        thumbnail: variant.product.thumbnail,
-        variant_id: variant.id,
-        quantity: quantity || 1,
-        allow_discounts: variant.product.discountable,
-        is_giftcard: variant.product.is_giftcard,
-        metadata: config?.metadata || {},
-        should_merge: shouldMerge,
-      }
-    })
+    )
   }
 
   /**
@@ -206,14 +228,16 @@ class LineItemService extends BaseService {
    * @return {Promise<LineItem>} the created line item
    */
   async create(data: Partial<LineItem>): Promise<LineItem> {
-    return this.atomicPhase_(async (manager: EntityManager) => {
-      const lineItemRepository = manager.getCustomRepository(
-        this.lineItemRepository_
-      )
+    return await this.atomicPhase_(
+      async (transactionManager: EntityManager) => {
+        const lineItemRepository = transactionManager.getCustomRepository(
+          this.lineItemRepository_
+        )
 
-      const lineItem = lineItemRepository.create(data)
-      return await lineItemRepository.save(lineItem)
-    })
+        const lineItem = lineItemRepository.create(data)
+        return await lineItemRepository.save(lineItem)
+      }
+    )
   }
 
   /**
@@ -225,24 +249,25 @@ class LineItemService extends BaseService {
   async update(id: string, data: Partial<LineItem>): Promise<LineItem> {
     const { metadata, ...rest } = data
 
-    return this.atomicPhase_(async (manager: EntityManager) => {
-      const lineItemRepository = manager.getCustomRepository(
-        this.lineItemRepository_
-      )
+    return await this.atomicPhase_(
+      async (transactionManager: EntityManager) => {
+        const lineItemRepository = transactionManager.getCustomRepository(
+          this.lineItemRepository_
+        )
 
-      const lineItem = await this.retrieve(id)
-        .then(lineItem => {
-          const lineItemMetadata = !!metadata
-              ? this.setMetadata_(lineItem, metadata)
-              : lineItem.metadata
+        const lineItem = await this.retrieve(id).then((lineItem) => {
+          const lineItemMetadata = metadata
+            ? this.setMetadata_(lineItem, metadata)
+            : lineItem.metadata
 
           return Object.assign(lineItem, {
             ...rest,
-            metadata: lineItemMetadata
-          });
+            metadata: lineItemMetadata,
+          })
         })
-      return await lineItemRepository.save(lineItem)
-    })
+        return await lineItemRepository.save(lineItem)
+      }
+    )
   }
 
   /**
@@ -251,15 +276,17 @@ class LineItemService extends BaseService {
    * @return {Promise<LineItem | undefined>} the result of the delete operation
    */
   async delete(id: string): Promise<LineItem | undefined> {
-    return this.atomicPhase_(async (manager: EntityManager) => {
-      const lineItemRepository = manager.getCustomRepository(
-        this.lineItemRepository_
-      )
+    return await this.atomicPhase_(
+      async (transactionManager: EntityManager) => {
+        const lineItemRepository = transactionManager.getCustomRepository(
+          this.lineItemRepository_
+        )
 
-      return await lineItemRepository
-        .findOne({ where: { id } })
-        .then(lineItem => lineItem && lineItemRepository.remove(lineItem))
-    })
+        return await lineItemRepository
+          .findOne({ where: { id } })
+          .then((lineItem) => lineItem && lineItemRepository.remove(lineItem))
+      }
+    )
   }
 }
 
