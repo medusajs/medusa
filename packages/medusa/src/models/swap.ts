d0b2a99b77794bd2c10910d16300924297cae222
adrien2p <adrien.deperetti@gmail.com>
2022-04-13 16:06:55 +0200

    feat(medusa): Improve exported medusa API Avoid to always have to import from @medusajs/medusa/dist/*

--- packages/medusa/src/models/swap.ts
+++ packages/medusa/src/models/swap.ts
@@ -26,7 +26,7 @@ import { Cart } from "./cart"
 import { Payment } from "./payment"
 import { ShippingMethod } from "./shipping-method"
 
-export enum FulfillmentStatus {
+export enum SwapFulfillmentStatus {
   NOT_FULFILLED = "not_fulfilled",
   FULFILLED = "fulfilled",
   SHIPPED = "shipped",
@@ -34,11 +34,11 @@ export enum FulfillmentStatus {
   REQUIRES_ACTION = "requires_action",
 }
 
-export enum PaymentStatus {
+export enum SwapPaymentStatus {
   NOT_PAID = "not_paid",
   AWAITING = "awaiting",
   CAPTURED = "captured",
-  CONFIRMED = "confirmed",
+  CONFIRMED = "confqirmed",
   CANCELED = "canceled",
   DIFFERENCE_REFUNDED = "difference_refunded",
   PARTIALLY_REFUNDED = "partially_refunded",
@@ -51,11 +51,11 @@ export class Swap {
   @PrimaryColumn()
   id: string
 
-  @DbAwareColumn({ type: "enum", enum: FulfillmentStatus })
-  fulfillment_status: FulfillmentStatus
+  @DbAwareColumn({ type: "enum", enum: SwapFulfillmentStatus })
+  fulfillment_status: SwapFulfillmentStatus
 
-  @DbAwareColumn({ type: "enum", enum: PaymentStatus })
-  payment_status: PaymentStatus
+  @DbAwareColumn({ type: "enum", enum: SwapPaymentStatus })
+  payment_status: SwapPaymentStatus
 
   @Index()
   @Column({ type: "string" })
