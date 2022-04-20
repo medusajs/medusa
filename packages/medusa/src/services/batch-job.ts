eb9b583c7d8b4a723f86ef4d0904bcc102d9df22
Philip Korsholm <philip.korsholm@hotmail.com>
2022-04-20 11:35:17 +0700

    add completed event

--- packages/medusa/src/services/batch-job.ts
+++ packages/medusa/src/services/batch-job.ts
@@ -22,6 +22,7 @@ class BatchJobService extends BaseService<BatchJobService> {
   static Events = {
     CREATED: "batch.created",
     UPDATED: "batch.updated",
+    COMPLETED: "batch.completed",
     CANCELED: "batch.canceled",
   }
 
@@ -55,8 +56,6 @@ class BatchJobService extends BaseService<BatchJobService> {
    */
   async complete(batchJobId: string, userId: string): Promise<BatchJob> {
     return await this.atomicPhase_(async (manager) => {
-      // logic...
-
       const batchJobRepo: BatchJobRepository = manager.getCustomRepository(
         this.batchJobRepository_
       )
@@ -64,7 +63,6 @@ class BatchJobService extends BaseService<BatchJobService> {
       const batchJob = await batchJobRepo.findOne(batchJobId)
 
       if (!batchJob || batchJob.created_by !== userId) {
-        // TODO: check if user is admin
         throw new MedusaError(
           MedusaError.Types.NOT_ALLOWED,
           "Cannot complete batch jobs created by other users"
@@ -88,7 +86,7 @@ class BatchJobService extends BaseService<BatchJobService> {
 
       await this.eventBus_
         .withTransaction(manager)
-        .emit(BatchJobService.Events.UPDATED, {
+        .emit(BatchJobService.Events.COMPLETED, {
           id: result.id,
         })
 
