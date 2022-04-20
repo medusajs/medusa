622c510b19689ce74c03fe81cadbc9c505fed9a3
Philip Korsholm <philip.korsholm@hotmail.com>
2022-04-20 14:57:36 +0700

    remove comment

--- packages/medusa/src/migrations/1649775522087-add_batch_job_model.ts
+++ packages/medusa/src/migrations/1649775522087-add_batch_job_model.ts
@@ -24,18 +24,4 @@ export class addBatchJobModel1649775522087 implements MigrationInterface {
     await queryRunner.query(`DROP TABLE "batch_job"`)
     await queryRunner.query(`DROP TYPE "batch_job_status_enum"`)
   }
-
-  // public async up(queryRunner: QueryRunner): Promise<void> {
-  //   await queryRunner.query(
-  //     `CREATE TYPE "batch_job_status_enum" AS ENUM('created', 'processing', 'awaiting_confirmation', 'completed')`
-  //   )
-  //   await queryRunner.query(
-  //     `CREATE TABLE "batch_job" ("id" character varying NOT NULL, "type" text NOT NULL, "status" "public"."batch_job_status_enum" NOT NULL, "created_by" text, "context" jsonb, "result" jsonb, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, CONSTRAINT "PK_e57f84d485145d5be96bc6d871e" PRIMARY KEY ("id"))`
-  //   )
-  // }
-
-  // public async down(queryRunner: QueryRunner): Promise<void> {
-  //   await queryRunner.query(`DROP TABLE "batch_job"`)
-  //   await queryRunner.query(`DROP TYPE "batch_job_status_enum"`)
-  // }
 }
