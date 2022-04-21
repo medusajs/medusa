ed068fe7b131006ab4c165b91f27dc96c25562b5
Shahed Nasser <shahednasser@gmail.com>
2022-04-21 17:13:01 +0300

    make yarn the default tab

---
+++ www/docs/src/theme/Tabs/index.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+import Tabs from '@theme-original/Tabs';
+
+export default function TabsWrapper(props) {
+
+  useEffect(() => {
+    if (!window.localStorage.getItem('docusaurus.tab.npm2yarn')) {
+      //set the default
+      window.localStorage.setItem('docusaurus.tab.npm2yarn', 'yarn')
+    }
+  }, [])
+
+  return (
+    <>
+      <Tabs {...props} />
+    </>
+  );
+}
