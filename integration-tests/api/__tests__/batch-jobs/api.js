fd2b69d9cf359b19b060e13fad0bece0632225e2
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-04-22 10:23:11 +0700

    Feat: Retrieve batch job (#1386)
    * add tests for getting batch job
    
    * add get-batch endpoint

--- integration-tests/api/__tests__/batch-jobs/api.js
+++ integration-tests/api/__tests__/batch-jobs/api.js
@@ -122,7 +122,7 @@ describe("/admin/batch", () => {
   })
 
   describe("POST /admin/batch/", () => {
-    beforeEach(async () => {
+    beforeEach(async() => {
       try {
         await adminSeeder(dbConnection)
       } catch (err) {
@@ -131,12 +131,7 @@ describe("/admin/batch", () => {
       }
     })
 
-    afterEach(async () => {
-      const db = useDb()
-      await db.teardown()
-    })
-
-    it("Creates a batch job", async () => {
+    it("Creates a batch job", async() => {
       const api = useApi()
 
       const response = await api.post(
@@ -156,6 +151,56 @@ describe("/admin/batch", () => {
         created_at: expect.any(String),
         updated_at: expect.any(String),
       })
+    })
+  })
+
+  describe("GET /admin/batch/id", () => {
+    beforeEach(async () => {
+      try {
+        await adminSeeder(dbConnection)
+
+        await simpleBatchJobFactory(dbConnection, {
+          id: "job_1",
+          type: "batch_1",
+          created_by: "admin_user",
+        })
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    })
+
+    afterEach(async () => {
+      const db = useDb()
+      await db.teardown()
+    })
+
+    it("Retrieves batch job with id", async () => {
+      const api = useApi()
+
+      const response = await api.get(`/admin/batch/job_1`, adminReqConfig)
+
+      expect(response.status).toEqual(200)
+      expect(response.data.batch_job).toMatchSnapshot({
+        id: "job_1",
+        created_by: "admin_user",
+      })
+    })
+
+    it("Fails with a descriptive error when retrieving batch job that doesn't exist", async () => {
+      expect.assertions(3)
+
+      const api = useApi()
+
+      try {
+        await api.get(`/admin/batch/job_2`, adminReqConfig)
+      } catch (error) {
+        expect(error.response.status).toEqual(404)
+        expect(error.response.data.type).toEqual("not_found")
+        expect(error.response.data.message).toEqual(
+          "Batch job with id job_2 was not found"
+        )
+      }
     })
   })
 
