f6e8740f913ca581e6683ec88152f9127e8d551f
Philip Korsholm <philip.korsholm@hotmail.com>
2022-04-23 13:59:26 +0700

    update cancel tests

--- integration-tests/api/__tests__/batch-jobs/api.js
+++ integration-tests/api/__tests__/batch-jobs/api.js
@@ -147,8 +147,8 @@ describe("/admin/batch", () => {
       expect(response.data.batch_job).toMatchSnapshot({
         created_at: expect.any(String),
         updated_at: expect.any(String),
-        cancelled_at: expect.any(String),
-        status: "canceled",
+        cancelled_at: null,
+        status: "created",
       })
     })
 
