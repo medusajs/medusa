17e3dcd72a57e94a643b54acfe36cfde47228f35
Philip Korsholm <philip.korsholm@hotmail.com>
2022-04-26 12:54:02 +0700

    remove verbose flag

--- integration-tests/api/__tests__/admin/price-list.js
+++ integration-tests/api/__tests__/admin/price-list.js
@@ -26,7 +26,7 @@ describe("/admin/price-lists", () => {
   beforeAll(async () => {
     const cwd = path.resolve(path.join(__dirname, "..", ".."))
     dbConnection = await initDb({ cwd })
-    medusaProcess = await setupServer({ cwd, verbose: true })
+    medusaProcess = await setupServer({ cwd })
   })
 
   afterAll(async () => {
