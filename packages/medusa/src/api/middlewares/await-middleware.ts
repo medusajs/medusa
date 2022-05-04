e7612020eb7a5c9fc059dbec48d68fd0689169db
adrien2p <adrien.deperetti@gmail.com>
2022-05-04 11:23:30 +0200

    refactor(medusa): Migrate middlewares to ts

---
+++ packages/medusa/src/api/middlewares/await-middleware.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, RequestHandler, Response } from "express"
+
+type handler = (req: Request, res: Response) => Promise<void>
+
+export default (fn: handler): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction) =>
+    fn(req, res).catch(next)
+}
---
+++ packages/medusa/src/api/middlewares/normalized-query.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express"
+
+export default (): ((
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    req.query = Object.entries(req.query).reduce((acc, [key, val]) => {
+      if (Array.isArray(val) && val.length === 1) {
+        acc[key] = (val as string[])[0].split(",")
+      } else {
+        acc[key] = val
+      }
+      return acc
+    }, {})
+
+    next()
+  }
+}
