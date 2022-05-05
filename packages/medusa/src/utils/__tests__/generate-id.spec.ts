10db9cfcc48fc234772f8f8577b8378c217a2b09
adrien2p <adrien.deperetti@gmail.com>
2022-05-05 11:09:57 +0200

    feat(medusa): Create generateEntityId standalong function utility and update config to properly supprt ts spec files

---
+++ packages/medusa/src/utils/__tests__/generate-id.spec.ts
@@ -0,0 +1,40 @@
+import { generateEntityId } from "../generate-entity-id"
+import { Entity, PrimaryColumn } from "typeorm"
+
+@Entity()
+class GenerateIdSpecEntity {
+  @PrimaryColumn()
+  id: string
+}
+
+describe("generateEntityId", () => {
+  it('should throw if the target id column name is nor part of the given entity', () => {
+    const entity = new GenerateIdSpecEntity()
+
+    let err
+    try {
+      generateEntityId(entity, "fakeId", "prefix")
+    } catch (e) {
+      err = e
+    }
+
+    expect(err).toBeTruthy()
+    expect(err.message).toBe(`Unable to generate the id for the entity ${GenerateIdSpecEntity.name} based on the id column name fakeId. This column is not part of that entity.`)
+  })
+
+  it('should return false if nothing happened', () => {
+    const entity = new GenerateIdSpecEntity()
+    entity.id = "fakeId"
+
+    const actionPerformed = generateEntityId(entity, "id", "prefix")
+    expect(actionPerformed).toBe(false)
+  })
+
+  it('should update the target id column on the entity with the expected generated id', () => {
+    const entity = new GenerateIdSpecEntity()
+
+    const actionPerformed = generateEntityId(entity, "id", "prefix")
+    expect(actionPerformed).toBeUndefined()
+    expect(entity.id).toEqual(expect.stringMatching(/prefix_*/))
+  })
+})
---
+++ packages/medusa/src/utils/generate-entity-id.ts
@@ -0,0 +1,21 @@
+import { ulid } from "ulid"
+
+/**
+ * Generate a composed id based on the input parameters and return false only if the id already exists which means no action to do.
+ * @param entity
+ * @param targetIdColumnName
+ * @param prefix
+ */
+export function generateEntityId<TEntity extends Object>(entity: TEntity, targetIdColumnName: string, prefix?: string): void | false {
+  if (!entity.hasOwnProperty(targetIdColumnName)) {
+    throw new Error(`Unable to generate the id for the entity ${entity.constructor.name} based on the id column name ${targetIdColumnName}. This column is not part of that entity.`)
+  }
+
+  if (entity[targetIdColumnName]) {
+    return false
+  }
+
+  const id = ulid()
+  prefix = prefix ? `${prefix}_` : ''
+  entity[targetIdColumnName] = `${prefix}${id}`
+}
