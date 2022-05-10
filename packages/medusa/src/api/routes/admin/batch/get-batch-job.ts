d5bbe264dac02c8b4e979cada3bd94265b565033
Philip Korsholm <philip.korsholm@hotmail.com>
2022-05-10 16:54:53 +0700

    add error when retrieving batch job created by another user

--- packages/medusa/src/api/routes/admin/batch/get-batch-job.ts
+++ packages/medusa/src/api/routes/admin/batch/get-batch-job.ts
@@ -1,3 +1,4 @@
+import { MedusaError } from "medusa-core-utils"
 import { BatchJobService } from "../../../../services"
 /**
  * @oas [get] /batch/{id}
@@ -22,8 +23,18 @@ import { BatchJobService } from "../../../../services"
 export default async (req, res) => {
   const { id } = req.params
 
+  const userId: string = req.user.id || req.user.userId
+
   const batchJobService: BatchJobService = req.scope.resolve("batchJobService")
 
   const batch_job = await batchJobService.retrieve(id)
+
+  if (userId !== batch_job.created_by) {
+    throw new MedusaError(
+      MedusaError.Types.NOT_ALLOWED,
+      "Cannot retrieve batch jobs created by other users"
+    )
+  }
+
   res.status(200).json({ batch_job })
 }
