134c1979d3d2c3f02f0f07465244e96e27cad460
adrien2p <adrien.deperetti@gmail.com>
2022-05-12 17:27:26 +0200

    test(medusa-payment-klarna): Fix typo

--- packages/medusa-payment-klarna/src/services/__tests__/klarna-provider.js
+++ packages/medusa-payment-klarna/src/services/__tests__/klarna-provider.js
@@ -20,7 +20,7 @@ describe("KlarnaProviderService", () => {
           terms: "terms",
           checkout: "checkout",
           confirmation: "confirmation",
-        },refundPayment
+        },
       }
     )
 
