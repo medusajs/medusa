037862f4a10cabf76e8eab6289d0fbaf43837362
Adrien de Peretti <adrien.deperetti@gmail.com>
2022-05-15 07:50:15 +0200

    fix(medusa): CartService fix constraint check

--- packages/medusa/src/services/cart.ts
+++ packages/medusa/src/services/cart.ts
@@ -779,7 +779,7 @@ class CartService extends TransactionBaseService<CartService> {
           await this.updateShippingAddress_(cart, shippingAddress, addrRepo)
         }
 
-        if (data.discounts?.length) {
+        if (typeof data.discounts !== "undefined") {
           const previousDiscounts = [...cart.discounts]
           cart.discounts.length = 0
 
@@ -796,9 +796,7 @@ class CartService extends TransactionBaseService<CartService> {
           // if we previously had a free shipping discount and then removed it,
           // we need to update shipping methods to original price
           if (
-            previousDiscounts.some(
-              ({ rule }) => rule?.type === "free_shipping"
-            ) &&
+            previousDiscounts.some(({ rule }) => rule.type === "free_shipping") &&
             !hasFreeShipping
           ) {
             await this.adjustFreeShipping_(cart, false)
