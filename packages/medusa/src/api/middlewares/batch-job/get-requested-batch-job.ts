75829799c2d9f4d0eec6017ded623cf5a109bb8e
adrien2p <adrien.deperetti@gmail.com>
2022-05-16 15:32:00 +0200

    feat(medusa): Improve getRequestedBatchJob middleware

--- packages/medusa/src/api/middlewares/batch-job/get-requested-batch-job.ts
+++ packages/medusa/src/api/middlewares/batch-job/get-requested-batch-job.ts
@@ -3,6 +3,14 @@ import BatchJobService from "../../../services/batch-job"
 export async function getRequestedBatchJob(req, res, next) {
   const id = req.params.id
   const batchJobService: BatchJobService = req.scope.resolve("batchJobService")
-  req.batch_job = await batchJobService.retrieve(id)
+
+  let error
+  req.batch_job = await batchJobService
+    .retrieve(id)
+    .catch((err) => (error = err))
+
+  if (error) {
+    return next(error)
+  }
   next()
 }
