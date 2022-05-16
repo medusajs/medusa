ac2786ed45fb8bde544a1a7dedcbe56340b77d63
Kasper <kasper@medusajs.com>
2022-05-16 11:50:32 +0200

    remove use of Joi from CustomerService, remove dead code from order service, remove tests for Joi validation

--- packages/medusa/src/types/common.ts
+++ packages/medusa/src/types/common.ts
@@ -178,28 +178,38 @@ export class AddressPayload {
 export class AddressCreatePayload {
   @IsString()
   first_name: string
+
   @IsString()
   last_name: string
+
   @IsOptional()
   @IsString()
   phone: string
+
   @IsOptional()
   metadata: object
+
   @IsOptional()
   @IsString()
   company: string
+
   @IsString()
   address_1: string
+
   @IsOptional()
   @IsString()
   address_2: string
+
   @IsString()
   city: string
+
   @IsString()
   country_code: string
+
   @IsOptional()
   @IsString()
   province: string
+
   @IsString()
   postal_code: string
 }
