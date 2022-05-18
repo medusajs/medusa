3b8a8de7b671d85347f2c495ea446566449f307a
Philip Korsholm <philip.korsholm@hotmail.com>
2022-05-18 11:53:56 +0200

    use try catch instead of let err

--- packages/medusa/src/api/middlewares/batch-job/get-requested-batch-job.ts
+++ packages/medusa/src/api/middlewares/batch-job/get-requested-batch-job.ts
@@ -4,13 +4,11 @@ export async function getRequestedBatchJob(req, res, next) {
   const id = req.params.id
   const batchJobService: BatchJobService = req.scope.resolve("batchJobService")
 
-  let error
-  req.batch_job = await batchJobService
-    .retrieve(id)
-    .catch((err) => (error = err))
-
-  if (error) {
+  try {
+    req.batch_job = await batchJobService.retrieve(id)
+  } catch (error) {
     return next(error)
   }
+
   next()
 }
