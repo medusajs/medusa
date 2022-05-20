1be2774052ecf94e58046fcc323806f39e82fe42
adrien2p <adrien.deperetti@gmail.com>
2022-05-20 14:39:51 +0200

    feat(medusa): Start batch job end point

--- packages/medusa/src/migrations/1649775522087-add_batch_job_model.ts
+++ packages/medusa/src/migrations/1649775522087-add_batch_job_model.ts
@@ -5,25 +5,42 @@ export class addBatchJobModel1649775522087 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TYPE "batch_job_status_enum" AS ENUM('created', 'processing', 'awaiting_confirmation', 'canceled', 'completed')`
+      `CREATE TYPE "batch_job_status_enum" AS ENUM('created', 'processing', 'awaiting_confirmation', 'cancelled', 'completed')`
     )
 
     await queryRunner.query(
-      `CREATE TABLE "batch_job" ("id" character varying NOT NULL, "type" text NOT NULL, "status" "public"."batch_job_status_enum" NOT NULL, "created_by" character varying, "context" jsonb, "result" jsonb, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "processing_at" TIMESTAMP WITH TIME ZONE, "awaiting_confirmation_at" TIMESTAMP WITH TIME ZONE, "confirmed_at" TIMESTAMP WITH TIME ZONE, "canceled_at" TIMESTAMP WITH TIME ZONE, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, CONSTRAINT "PK_e57f84d485145d5be96bc6d871e" PRIMARY KEY ("id"))`
+        `CREATE TABLE "batch_job"
+         (
+             "id"                       character varying                NOT NULL,
+             "type"                     text                             NOT NULL,
+             "status"                   "public"."batch_job_status_enum" NOT NULL,
+             "created_by"               character varying,
+             "context"                  jsonb,
+             "result"                   jsonb,
+             "created_at"               TIMESTAMP WITH TIME ZONE         NOT NULL DEFAULT now(),
+             "processing_at"            TIMESTAMP WITH TIME ZONE,
+             "awaiting_confirmation_at" TIMESTAMP WITH TIME ZONE,
+             "confirmed_at"             TIMESTAMP WITH TIME ZONE,
+             "completed_at"             TIMESTAMP WITH TIME ZONE,
+             "canceled_at"              TIMESTAMP WITH TIME ZONE,
+             "updated_at"               TIMESTAMP WITH TIME ZONE         NOT NULL DEFAULT now(),
+             "deleted_at"               TIMESTAMP WITH TIME ZONE,
+             CONSTRAINT "PK_e57f84d485145d5be96bc6d871e" PRIMARY KEY ("id")
+         )`
     )
 
     await queryRunner.query(
-      `ALTER TABLE "batch_job" ADD CONSTRAINT "FK_fa53ca4f5fd90605b532802a626" FOREIGN KEY ("created_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
-    )
-    await queryRunner.query(
-    `ALTER TABLE "batch_job" ADD CONSTRAINT "FK_fa53ca4f5fd90605b532802a626" FOREIGN KEY ("created_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        `ALTER TABLE "batch_job"
+                ADD CONSTRAINT "FK_fa53ca4f5fd90605b532802a626" FOREIGN KEY ("created_by") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "batch_job" DROP CONSTRAINT "FK_fa53ca4f5fd90605b532802a626"`
+        `ALTER TABLE "batch_job"
+                DROP CONSTRAINT "FK_fa53ca4f5fd90605b532802a626"`
     )
     await queryRunner.query(`DROP TABLE "batch_job"`)
     await queryRunner.query(`DROP TYPE "batch_job_status_enum"`)
   }
+}
