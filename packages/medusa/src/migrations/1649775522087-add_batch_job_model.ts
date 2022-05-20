21a34d2a3ba4330539095f79cf43ec7aa0f7d2a2
adrien2p <adrien.deperetti@gmail.com>
2022-05-20 13:19:15 +0200

    feat(medusa): Update migration

--- packages/medusa/src/migrations/1649775522087-add_batch_job_model.ts
+++ packages/medusa/src/migrations/1649775522087-add_batch_job_model.ts
@@ -11,16 +11,13 @@ export class addBatchJobModel1649775522087 implements MigrationInterface {
     `CREATE TABLE "batch_job" ("id" character varying NOT NULL, "type" text NOT NULL, "status" "public"."batch_job_status_enum" NOT NULL, "created_by" character varying, "context" jsonb, "result" jsonb, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "processing_at" TIMESTAMP WITH TIME ZONE, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, CONSTRAINT "PK_e57f84d485145d5be96bc6d871e" PRIMARY KEY ("id"))`
     )
     await queryRunner.query(
-    `ALTER TABLE "batch_job" ADD CONSTRAINT "FK_cdf30493ba1c9ef207e1e80c10a" FOREIGN KEY ("created_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    `ALTER TABLE "batch_job" ADD CONSTRAINT "FK_fa53ca4f5fd90605b532802a626" FOREIGN KEY ("created_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "batch_job" DROP CONSTRAINT "FK_fa53ca4f5fd90605b532802a626"`
-    )
-    await queryRunner.query(
-      `ALTER TABLE "batch_job" DROP CONSTRAINT "FK_cdf30493ba1c9ef207e1e80c10a"`
     )
     await queryRunner.query(`DROP TABLE "batch_job"`)
     await queryRunner.query(`DROP TYPE "batch_job_status_enum"`)
