ee95fef620e62ca2441785e6cea9a599329d669e
Zakaria El Asri <zakaria.elas@gmail.com>
2022-05-25 16:18:23 +0100

    fix: change file location

--- packages/medusa/src/interfaces/csv-parser.ts
+++ packages/medusa/src/interfaces/csv-parser.ts
@@ -1,12 +1,4 @@
-import {
-  AbstractParser,
-  IParser,
-  LineContext,
-  ParserOptions,
-} from "./abstract-parser"
 import { AwilixContainer } from "awilix"
-import Papa, { ParseConfig } from "papaparse"
-import fs from "fs"
 
 export type CsvParserContext = {
   line: number
@@ -27,7 +19,7 @@ export abstract class AbstractValidator<TLine> implements IValidator<TLine> {
   abstract validate(value: string, line: TLine, context: CsvParserContext)
 }
 
-type CsvSchema<TLine = any> = {
+export type CsvSchema<TLine = any> = {
   columns: {
     name: string
     required?: boolean
@@ -36,81 +28,6 @@ type CsvSchema<TLine = any> = {
   }[]
 }
 
-interface ICsvParser<TResult> extends IParser<TResult> {
+export interface ICsvParser<TResult> extends IParser<TResult> {
   $$delimiter: string
-}
-
-export abstract class CsvParser<
-    TSchema extends CsvSchema = CsvSchema,
-    TParserResult = any,
-    TOutputResult = any
-  >
-  extends AbstractParser<TSchema, TParserResult, TOutputResult>
-  implements ICsvParser<TParserResult>
-{
-  $$delimiter = ";"
-
-  constructor(
-    protected readonly container: AwilixContainer,
-    schema: TSchema,
-    delimiter?: string
-  ) {
-    super(schema)
-    if (delimiter) {
-      this.$$delimiter = delimiter
-    }
-  }
-
-  public async parse(
-    path: string,
-    options: ParserOptions = {
-      dynamicTyping: true,
-      header: true,
-    }
-  ): Promise<TParserResult[]> {
-    const fileStream = fs.createReadStream(path)
-    const csvStream = Papa.parse(Papa.NODE_STREAM_INPUT, {
-      ...(options as ParseConfig),
-    })
-
-    const parsedContent: TParserResult[] = []
-    fileStream.pipe(csvStream)
-    for await (const chunk of csvStream) {
-      parsedContent.push(chunk)
-    }
-
-    return parsedContent
-  }
-
-  async validateSchema(
-    line: TParserResult,
-    context: LineContext
-  ): Promise<TOutputResult> {
-    const outputTuple = {} as TOutputResult
-
-    const tupleKeys = Object.keys(line)
-
-    for (const tupleKey of tupleKeys) {
-      const column = this.$$schema.columns.find((column) => {
-        return column.name === tupleKey
-      })
-
-      if (!column) {
-        throw new Error(
-          `Unable to to treat column ${tupleKey} from the csv file. No target column found in the provided schema`
-        )
-      }
-
-      if (column.validator) {
-        await column.validator.validate(line[tupleKey], line, {
-          line: context.lineNumber,
-          column: tupleKey,
-        })
-      }
-
-      outputTuple[column.mapTo ?? tupleKey] = line[tupleKey]
-    }
-
-    return outputTuple
-  }
 }
---
+++ packages/medusa/src/services/csv-parser.ts
@@ -0,0 +1,86 @@
+import { AwilixContainer } from "awilix"
+import {
+  AbstractParser,
+  LineContext,
+  ParserOptions,
+} from "../interfaces/abstract-parser"
+import { CsvSchema, ICsvParser } from "../interfaces/csv-parser"
+import Papa, { ParseConfig } from "papaparse"
+import fs from "fs"
+
+class CsvParser<
+    TSchema extends CsvSchema = CsvSchema,
+    TParserResult = any,
+    TOutputResult = any
+  >
+  extends AbstractParser<TSchema, TParserResult, TOutputResult>
+  implements ICsvParser<TParserResult>
+{
+  $$delimiter = ";"
+
+  constructor(
+    protected readonly container: AwilixContainer,
+    schema: TSchema,
+    delimiter?: string
+  ) {
+    super(schema)
+    if (delimiter) {
+      this.$$delimiter = delimiter
+    }
+  }
+
+  public async parse(
+    path: string,
+    options: ParserOptions = {
+      dynamicTyping: true,
+      header: true,
+    }
+  ): Promise<TParserResult[]> {
+    const fileStream = fs.createReadStream(path)
+    const csvStream = Papa.parse(Papa.NODE_STREAM_INPUT, {
+      ...(options as ParseConfig),
+    })
+
+    const parsedContent: TParserResult[] = []
+    fileStream.pipe(csvStream)
+    for await (const chunk of csvStream) {
+      parsedContent.push(chunk)
+    }
+
+    return parsedContent
+  }
+
+  async validateSchema(
+    line: TParserResult,
+    context: LineContext
+  ): Promise<TOutputResult> {
+    const outputTuple = {} as TOutputResult
+
+    const tupleKeys = Object.keys(line)
+
+    for (const tupleKey of tupleKeys) {
+      const column = this.$$schema.columns.find((column) => {
+        return column.name === tupleKey
+      })
+
+      if (!column) {
+        throw new Error(
+          `Unable to to treat column ${tupleKey} from the csv file. No target column found in the provided schema`
+        )
+      }
+
+      if (column.validator) {
+        await column.validator.validate(line[tupleKey], line, {
+          line: context.lineNumber,
+          column: tupleKey,
+        })
+      }
+
+      outputTuple[column.mapTo ?? tupleKey] = line[tupleKey]
+    }
+
+    return outputTuple
+  }
+}
+
+export default CsvParser
