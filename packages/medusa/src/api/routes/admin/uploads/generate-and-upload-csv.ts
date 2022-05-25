8c93ea66fe0dd0c4d46c2a05caab21b146732920
Philip Korsholm <philip.korsholm@hotmail.com>
2022-05-25 17:51:41 +0200

    cleanup generate example

--- packages/medusa/src/api/routes/admin/uploads/generate-and-upload-csv.ts
+++ packages/medusa/src/api/routes/admin/uploads/generate-and-upload-csv.ts
@@ -16,9 +16,9 @@ export default async (req, res) => {
 
     const result: string[] = []
 
-    const writeStream = new stream.PassThrough()
-
-    const promise = fileService.uploadStream({ name: "test" }, writeStream)
+    const { writeStream, promise, url } = await fileService.getUploadStream({
+      name: "test",
+    })
 
     for (const product of products) {
       logger.info(`Uploading product ${product.id}`)
@@ -33,45 +33,9 @@ export default async (req, res) => {
 
     const resultUrl = await promise
 
-    // const result = await Promise.all(
-    //   req.files.map(async (f) => {
-    //     // return fileService.upload(f).then((result) => {
-    //     //   return result
-    //     // })
-    //   })
-    // )
-
-    res.status(200).json({ result, resultUrl })
+    res.status(200).json({ result, resultUrl, url })
   } catch (err) {
     console.log(err)
     throw err
   }
 }
-
-// logger.info("Uploading file")
-//     logger.info(typeof fileService)
-//     logger.info(fileService.constructor.name)
-
-//     const products = await productsService.list()
-
-//     const result: string[] = []
-
-//     const writeStream1 = new stream.PassThrough()
-
-//     const { writeStream, promise } = fileService.uploadStream(
-//       { name: "test" },
-//       writeStream1
-//     )
-
-//     for (const product of products) {
-//       logger.info(`Uploading product ${product.id}`)
-//       await new Promise((resolve) => setTimeout(resolve, 5000))
-//       writeStream.write(Object.values(product).join(";"))
-//       writeStream.write("\n")
-//       result.push(Object.values(product).join(";"))
-//       logger.info(`Uploaded product ${product.id}`)
-//     }
-
-//     writeStream.end()
-
-//     const resultUrl = await promise
