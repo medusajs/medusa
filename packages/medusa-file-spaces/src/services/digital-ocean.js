4c24e98a7511eea5e3aec6366ba0cb729fa9f912
Philip Korsholm <philip.korsholm@hotmail.com>
2022-05-25 17:23:26 +0200

    re-rename digital ocean fileservice to javascript

--- packages/medusa-file-spaces/src/services/digital-ocean.js
+++ packages/medusa-file-spaces/src/services/digital-ocean.js
@@ -1,16 +1,17 @@
 import fs from "fs"
 import aws from "aws-sdk"
 import { parse } from "path"
-import { AbstractFileService } from '@medusajs/medusa'
+import { AbstractFileService, FileServiceUploadResult } from '@medusajs/medusa'
 import { EntityManager } from "typeorm"
+import stream from "stream"
 
 class DigitalOceanService extends AbstractFileService {
-  protected bucket_: string
-  protected spacesUrl_: string
-  protected accessKeyId_: string
-  protected secretAccessKey_: string
-  protected region_: string
-  protected endpoint_: string
+  bucket_
+  spacesUrl_
+  accessKeyId_
+  secretAccessKey_
+  region_
+  endpoint_
 
   constructor({}, options) {
     super()
@@ -82,6 +83,78 @@ class DigitalOceanService extends AbstractFileService {
         resolve(data)
       })
     })
+  }
+
+  // uploadStream(
+  //   fileData,
+  //   fileStream
+  // ) {
+  //   aws.config.setPromisesDependency(null)
+  //   aws.config.update({
+  //     accessKeyId: this.accessKeyId_,
+  //     secretAccessKey: this.secretAccessKey_,
+  //     region: this.region_,
+  //     endpoint: this.endpoint_,
+  //   }, true)
+
+  //   const parsedFilename = fileData.name
+  //   const fileKey = `${parsedFilename}-${Date.now()}.csv`
+  //   const s3 = new aws.S3()
+  //   var params = {
+  //     ACL: "bucket-owner-full-control",
+  //     Bucket: this.bucket_,
+  //     Body: fileStream,
+  //     Key: fileKey,
+  //   }
+
+  //   return new Promise((resolve, reject) => {
+  //     s3.upload(params, (err, data) => {
+  //       if (err) {
+  //         reject(err)
+  //         return
+  //       }
+
+  //       if (this.spacesUrl_) {
+  //         resolve({ url: `${this.spacesUrl_}/${data.Key}` })
+  //       }
+
+  //       resolve({ url: data.Location })
+  //     })
+  //   })
+  // }
+
+  async uploadStream(fileData) {
+    aws.config.setPromisesDependency(null)
+    aws.config.update(
+      {
+        accessKeyId: this.accessKeyId_,
+        secretAccessKey: this.secretAccessKey_,
+        region: this.region_,
+        endpoint: this.endpoint_,
+      },
+      true
+    )
+
+    var pass = new stream.PassThrough()
+
+    const parsedFilename = fileData.name
+    const fileKey = `${parsedFilename}-${Date.now()}.csv`
+    const s3 = new aws.S3()
+
+    var params = {
+      ACL: "bucket-owner-full-control",
+      Bucket: this.bucket_,
+      Body: pass,
+      Key: fileKey,
+    }
+
+    return {
+      writeStream: pass,
+      promise: s3
+        .upload(params)
+        .promise(),
+      url: `${this.spacesUrl_}/${fileKey}`,
+    }
   }
 }
 
