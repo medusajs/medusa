22eeefba53e1c4a440634c5a798f7836cbca1259
Shahed Nasser <shahednasser@gmail.com>
2022-05-27 16:59:25 +0300

    added events reference

--- www/docs/sidebars.js
+++ www/docs/sidebars.js
@@ -153,6 +153,11 @@ module.exports = {
                   id: "advanced/backend/subscribers/create-subscriber",
                   label: "Create a Subscriber"
                 },
+                {
+                  type: "doc",
+                  id: "advanced/backend/subscribers/events-list",
+                  label: "List of Events"
+                },
               ]
             },
             {
