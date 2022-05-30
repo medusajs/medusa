f3ba304ce49ce4856f56d6af985ea9497bddf6f5
Zakaria El Asri <zakaria.elas@gmail.com>
2022-05-30 18:43:09 +0100

    add: test case for mapTo

--- packages/medusa/src/services/__tests__/csv-parser.js
+++ packages/medusa/src/services/__tests__/csv-parser.js
@@ -34,47 +34,70 @@ describe("CsvParser", () => {
   })
 
   describe("buildData", () => {
-    const csvParser = new CsvParser(createContainer(), {
-      columns: [
-        {
-          name: "title",
-          validator: {
-            validate: async (value) => {
-              if (/\d/.test(value)) {
-                throw new Error("title should not contain a number")
-              }
+    describe("schema validation", () => {
+      const csvParser = new CsvParser(createContainer(), {
+        columns: [
+          {
+            name: "title",
+            validator: {
+              validate: async (value) => {
+                if (/\d/.test(value)) {
+                  throw new Error("title should not contain a number")
+                }
+              },
             },
           },
-        },
-      ],
+        ],
+      })
+
+      it("given a line containing a column which is not defined in the schema, then validation should fail", async () => {
+        try {
+          await csvParser.buildData([{ first_name: "lebron " }])
+        } catch (err) {
+          expect(err.message).toEqual(
+            "Unable to to treat column first_name from the csv file. No target column found in the provided schema"
+          )
+        }
+      })
+
+      it("given a line containing a column which does not pass a validation constraint, then validation should fail", async () => {
+        try {
+          await csvParser.buildData([{ title: "contains a number 1" }])
+        } catch (err) {
+          expect(err.message).toEqual("title should not contain a number")
+        }
+      })
+
+      it("given a line which passes all validation constraints, then should returned validated content", async () => {
+        const content = await csvParser.buildData([{ title: "great product" }])
+
+        expect(content).toEqual([
+          {
+            title: "great product",
+          },
+        ])
+      })
     })
 
-    it("given a line containing a column which is not defined in the schema, then validation should fail", async () => {
-      try {
-        await csvParser.buildData([{ first_name: "lebron " }])
-      } catch (err) {
-        expect(err.message).toEqual(
-          "Unable to to treat column first_name from the csv file. No target column found in the provided schema"
-        )
-      }
-    })
+    describe("mapTo", () => {
+      const csvParser = new CsvParser(createContainer(), {
+        columns: [
+          {
+            name: "title",
+            mapTo: "product_title",
+          },
+        ],
+      })
 
-    it("given a line containing a column which does not pass a validation constraint, then validation should fail", async () => {
-      try {
-        await csvParser.buildData([{ title: "contains a number 1" }])
-      } catch (err) {
-        expect(err.message).toEqual("title should not contain a number")
-      }
-    })
+      it("given a mapTo field for a column, when building data including that column, should rename the column name to what mapTo refers to", async () => {
+        const content = await csvParser.buildData([{ title: "a product" }])
 
-    it("given a line which passes all validation constraints, then should returned validated content", async () => {
-      const content = await csvParser.buildData([{ title: "great product" }])
-
-      expect(content).toEqual([
-        {
-          title: "great product",
-        },
-      ])
+        expect(content).toEqual([
+          {
+            product_title: "a product",
+          },
+        ])
+      })
     })
   })
 })
