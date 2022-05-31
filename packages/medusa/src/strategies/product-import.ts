84289d0cc9725bf27a325b4c1ff40b6850519471
fPolic <frane@medusajs.com>
2022-05-31 20:24:42 +0200

    feat: create initial strategy and implement the Batch Job abstract class

---
+++ packages/medusa/src/strategies/product-import.ts
@@ -0,0 +1,41 @@
+import { EntityManager } from "typeorm"
+
+import { AbstractBatchJobStrategy } from "../interfaces"
+import { BatchJob } from "../models"
+
+class ProductImportStrategy extends AbstractBatchJobStrategy<ProductImportStrategy> {
+  buildTemplate(): Promise<string> {
+    return Promise.resolve("")
+  }
+
+  completeJob(batchJobId: string): Promise<BatchJob> {
+    return Promise.resolve(undefined)
+  }
+
+  protected manager_: EntityManager
+
+  prepareBatchJobForProcessing(
+    batchJobId: string,
+    req: Express.Request
+  ): Promise<BatchJob> {
+    return Promise.resolve(undefined)
+  }
+
+  processJob(batchJobId: string): Promise<BatchJob> {
+    return Promise.resolve(undefined)
+  }
+
+  protected transactionManager_: EntityManager | undefined
+
+  validateContext(
+    context: Record<string, unknown>
+  ): Promise<Record<string, unknown>> {
+    return Promise.resolve(undefined)
+  }
+
+  validateFile(fileLocation: string): Promise<boolean> {
+    return Promise.resolve(false)
+  }
+}
+
+export default ProductImportStrategy
--- packages/medusa/src/subscribers/batch-job.ts
+++ packages/medusa/src/subscribers/batch-job.ts
@@ -52,7 +52,7 @@ class BatchJobSubscriber {
 
     const batchJobStrategy = this.getBatchJobStrategy(batchJob.type)
 
-    batchJobStrategy.completeJob(batchJob.id)
+    await batchJobStrategy.completeJob(batchJob.id)
   }
 
   getBatchJobStrategy(type: string): AbstractBatchJobStrategy<any> {
