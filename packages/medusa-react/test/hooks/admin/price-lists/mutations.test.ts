3247496197576e6c403a2724389cbb8f4bb77426
Philip Korsholm <philip.korsholm@hotmail.com>
2022-05-31 10:02:45 +0200

    revert changes

--- packages/medusa-react/test/hooks/admin/price-lists/mutations.test.ts
+++ packages/medusa-react/test/hooks/admin/price-lists/mutations.test.ts
@@ -143,11 +143,10 @@ describe("useAdminDeletePriceList hook", () => {
 describe("useAdminDeletePriceListProductPrices hook", () => {
   test("should delete prices from a price list for all the variants related to the specified product", async () => {
     const { result, waitFor } = renderHook(
-      () =>
-        useAdminDeletePriceListProductPrices(
-          fixtures.get("price_list").id,
-          fixtures.get("product").id
-        ),
+      () => useAdminDeletePriceListProductPrices(
+        fixtures.get("price_list").id,
+        fixtures.get("product").id
+      ),
       {
         wrapper: createWrapper(),
       }
@@ -158,24 +157,21 @@ describe("useAdminDeletePriceListProductPrices hook", () => {
     await waitFor(() => result.current.isSuccess)
 
     expect(result.current.data.response.status).toEqual(200)
-    expect(result.current.data).toEqual(
-      expect.objectContaining({
-        ids: [],
-        object: "money-amount",
-        deleted: true,
-      })
-    )
+    expect(result.current.data).toEqual(expect.objectContaining({
+      ids: [],
+      object: "money-amount",
+      deleted: true
+    }))
   })
 })
 
 describe("useAdminDeletePriceListVariantPrices hook", () => {
   test("should delete prices from a price list for the specified variant", async () => {
     const { result, waitFor } = renderHook(
-      () =>
-        useAdminDeletePriceListVariantPrices(
-          fixtures.get("price_list").id,
-          fixtures.get("product_variant").id
-        ),
+      () => useAdminDeletePriceListVariantPrices(
+        fixtures.get("price_list").id,
+        fixtures.get("product_variant").id
+      ),
       {
         wrapper: createWrapper(),
       }
@@ -186,12 +182,10 @@ describe("useAdminDeletePriceListVariantPrices hook", () => {
     await waitFor(() => result.current.isSuccess)
 
     expect(result.current.data.response.status).toEqual(200)
-    expect(result.current.data).toEqual(
-      expect.objectContaining({
-        ids: [],
-        object: "money-amount",
-        deleted: true,
-      })
-    )
+    expect(result.current.data).toEqual(expect.objectContaining({
+      ids: [],
+      object: "money-amount",
+      deleted: true
+    }))
   })
 })
