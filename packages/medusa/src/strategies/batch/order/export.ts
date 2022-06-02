8450e6d9be430f2d32728b85adc7bd1cf4759f91
Philip Korsholm <philip.korsholm@hotmail.com>
2022-06-02 10:22:24 +0200

    make fields filterable in export file

--- packages/medusa/src/strategies/batch/order/export.ts
+++ packages/medusa/src/strategies/batch/order/export.ts
@@ -1,4 +1,4 @@
-import { Dictionary, omit, pickBy } from "lodash"
+import { Dictionary, indexOf, omit, pickBy } from "lodash"
 import { EntityManager } from "typeorm"
 import { AdminGetOrdersParams } from "../../../api"
 import { IFileService } from "../../../interfaces"
@@ -20,8 +20,122 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
   private NEWLINE = "\r\n"
   private DELIMITER = ";"
 
+  protected readonly propertiesDescriptorArray = new Map([
+    ["id", { title: "Order_ID", accessor: (order: Order): string => order.id }],
+    [
+      "display_id",
+      {
+        title: "Display_ID",
+        accessor: (order: Order): string => order.display_id.toString(),
+      },
+    ],
+    [
+      "created_at",
+      {
+        title: "Date",
+        accessor: (order: Order): string => order.created_at.toUTCString(),
+      },
+    ],
+    [
+      "customer",
+      {
+        title: [
+          "Customer First name",
+          "Customer Last name",
+          "Customer Email",
+          "Customer ID",
+        ].join(";"),
+        accessor: (order: Order): string =>
+          [
+            order.customer.first_name,
+            order.customer.last_name,
+            order.customer.email,
+            order.customer.id,
+          ].join(";"),
+      },
+    ],
+    [
+      "shipping_address",
+      {
+        title: [
+          "Shipping Address 1",
+          "Shipping Address 2",
+          "Shipping Country Code",
+          "Shipping City",
+          "Shipping Postal Code",
+          "Shipping Region ID",
+        ].join(";"),
+        accessor: (order: Order): string =>
+          [
+            order.shipping_address.address_1,
+            order.shipping_address.address_2,
+            order.shipping_address.country_code,
+            order.shipping_address.city,
+            order.shipping_address.postal_code,
+            order.region_id,
+          ].join(";"),
+      },
+    ],
+    [
+      "fulfillment_status",
+      {
+        title: "Fulfillment Status",
+        accessor: (order: Order): string => order.fulfillment_status,
+      },
+    ],
+    [
+      "payment_status",
+      {
+        title: "Payment Status",
+        accessor: (order: Order): string => order.payment_status,
+      },
+    ],
+    [
+      "subtotal",
+      {
+        title: "Subtotal",
+        accessor: (order: Order): string => order.subtotal.toString(),
+      },
+    ],
+    [
+      "shipping_total",
+      {
+        title: "Shipping Total",
+        accessor: (order: Order): string => order.shipping_total.toString(),
+      },
+    ],
+    [
+      "discount_total",
+      {
+        title: "Discount Total",
+        accessor: (order: Order): string => order.discount_total.toString(),
+      },
+    ],
+    [
+      "gift_card_total",
+      {
+        title: "Gift Card Total",
+        accessor: (order: Order): string => order.gift_card_total.toString(),
+      },
+    ],
+    [
+      "total",
+      {
+        title: "Total",
+        accessor: (order: Order): string => order.total.toString(),
+      },
+    ],
+    [
+      "currency_code",
+      {
+        title: "Currency Code",
+        accessor: (order: Order): string => order.currency_code,
+      },
+    ],
+  ])
+
   private readonly relations = ["customer", "shipping_address"]
-  private readonly selectConfig = [
+  private readonly select = [
     "id",
     "status",
     "fulfillment_status",
@@ -38,9 +152,6 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
     "created_at",
     "updated_at",
     "metadata",
-    "items.refundable",
-    "swaps.additional_items.refundable",
-    "claims.additional_items.refundable",
     "shipping_total",
     "discount_total",
     "tax_total",
@@ -59,6 +170,7 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
   protected readonly batchJobService_: BatchJobService
   protected readonly orderService_: OrderService
   protected readonly totalsService_: TotalsService
+  protected readonly logger_: Logger
 
   constructor({
     fileService,
@@ -66,10 +178,12 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
     orderService,
     totalsService,
     manager,
+    logger,
   }) {
     // eslint-disable-next-line prefer-rest-params
     super(arguments[0])
 
+    this.logger_ = logger
     this.manager_ = manager
     this.fileService_ = fileService
     this.batchJobService_ = batchJobService
@@ -101,6 +215,11 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
 
     const [filter, listConfig] = this.getListParameters(params)
 
+    const lineDescriptor = this.getLineDescriptor(
+      listConfig.select,
+      listConfig.relations
+    )
+
     const {
       writeStream,
       url: key,
@@ -110,7 +229,7 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
       ext: "csv",
     })
 
-    const header = this.buildHeader()
+    const header = this.buildHeader(lineDescriptor)
     writeStream.write(header)
 
     let offset = 0
@@ -132,7 +251,7 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
       })
 
       orders.forEach(async (order) => {
-        writeStream.write(await this.buildCSVLine(order))
+        writeStream.write(await this.buildCSVLine(order, lineDescriptor))
       })
 
       offset += this.BATCH_SIZE
@@ -177,62 +296,80 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
   }
 
   public async buildTemplate(): Promise<string> {
-    return this.buildHeader()
+    return this.buildHeader(this.getLineDescriptor(this.select, this.relations))
   }
 
-  private buildHeader(): string {
+  private buildHeader(lineDescriptor): string {
     return (
-      [
-        "Order_ID",
-        "Display_ID",
-        "Date",
-        "Customer First name",
-        "Customer Last name",
-        "Customer Email",
-        "Customer ID",
-        "Shipping Address 1",
-        "Shipping Address 2",
-        "Shipping Country Code",
-        "Shipping City",
-        "Shipping Postal Code",
-        "Shipping Region ID",
-        "Fulfillment Status",
-        "Payment Status",
-        "Subtotal",
-        "Shipping Total",
-        "Discount Total",
-        "Gift Card Total",
-        "Total",
-        "Currency Code",
-      ].join(this.DELIMITER) + this.NEWLINE
+      lineDescriptor.map(([key, { title }]) => title).join(this.DELIMITER) +
+      this.NEWLINE
     )
+    // return (
+    //   [
+    //     "Order_ID",
+    //     "Display_ID",
+    //     "Date",
+    //     "Customer First name",
+    //     "Customer Last name",
+    //     "Customer Email",
+    //     "Customer ID",
+    //     "Shipping Address 1",
+    //     "Shipping Address 2",
+    //     "Shipping Country Code",
+    //     "Shipping City",
+    //     "Shipping Postal Code",
+    //     "Shipping Region ID",
+    //     "Fulfillment Status",
+    //     "Payment Status",
+    //     "Subtotal",
+    //     "Shipping Total",
+    //     "Discount Total",
+    //     "Gift Card Total",
+    //     "Total",
+    //     "Currency Code",
+    //   ].join(this.DELIMITER) + this.NEWLINE
+    // )
   }
 
-  private async buildCSVLine(line: Order): Promise<string> {
+  private async buildCSVLine(order: Order, lineDescriptor): Promise<string> {
     return (
-      [
-        line.id,
-        line.display_id,
-        line.created_at,
-        line.customer.first_name,
-        line.customer.last_name,
-        line.customer.email,
-        line.customer.id,
-        line.shipping_address.address_1,
-        line.shipping_address.address_2,
-        line.shipping_address.country_code,
-        line.shipping_address.city,
-        line.shipping_address.postal_code,
-        line.region_id,
-        line.fulfillment_status,
-        line.payment_status,
-        line.subtotal,
-        line.shipping_total,
-        line.discount_total,
-        line.gift_card_total,
-        line.total,
-        line.currency_code,
-      ].join(this.DELIMITER) + this.NEWLINE
+      lineDescriptor
+        .map(([, { accessor }]) => accessor(order))
+        .join(this.DELIMITER) + this.NEWLINE
+    )
+    // return (
+    //   [
+    //     order.id,
+    //     order.display_id,
+    //     order.created_at.toUTCString(),
+    //     order.customer.first_name,
+    //     order.customer.last_name,
+    //     order.customer.email,
+    //     order.customer.id,
+    //     order.shipping_address.address_1,
+    //     order.shipping_address.address_2,
+    //     order.shipping_address.country_code,
+    //     order.shipping_address.city,
+    //     order.shipping_address.postal_code,
+    //     order.region_id,
+    //     order.fulfillment_status,
+    //     order.payment_status,
+    //     order.subtotal,
+    //     order.shipping_total,
+    //     order.discount_total,
+    //     order.gift_card_total,
+    //     order.total,
+    //     order.currency_code,
+    //   ].join(this.DELIMITER) + this.NEWLINE
+    // )
+  }
+
+  private getLineDescriptor(
+    fields: string[],
+    relations: string[]
+  ): [string, { title: string; accessor: (order: Order) => string }][] {
+    return [...this.propertiesDescriptorArray].filter(
+      ([key, _]) => fields.indexOf(key) !== -1 || relations.indexOf(key) !== -1
     )
   }
 
@@ -253,7 +390,7 @@ class OrderExportStrategy extends AbstractBatchJobStrategy<OrderExportStrategy> 
     }
 
     const listConfig = {
-      select: includeFields.length ? includeFields : this.selectConfig,
+      select: includeFields.length ? includeFields : this.select,
       relations: expandFields.length ? expandFields : this.relations,
       order: { created_at: "DESC" },
     }
