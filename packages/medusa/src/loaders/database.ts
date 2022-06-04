36dfb4e202931d4aa7bb1c34eeecc5cdccabf8a5
SGF <SGF@sourcegoodfood.com>
2022-06-04 12:30:01 +0000

    <feat> added changes to support IAM_Authentication of medusa postgres

--- packages/medusa/src/loaders/database.ts
+++ packages/medusa/src/loaders/database.ts
@@ -16,7 +16,18 @@ export default async ({ container, configModule }: Options): Promise<Connection>
 
   const connection = await createConnection({
     type: configModule.projectConfig.database_type,
-    url: configModule.projectConfig.database_url,
+    //url: configModule.projectConfig.database_url,
+    url:configModule.projectConfig.database_url?configModule.projectConfig.database_url:undefined,
+    ...{
+        host:configModule.projectConfig.host,
+        port:configModule.projectConfig.port,
+        database:configModule.projectConfig.database_database,
+        ssl:configModule.projectConfig.ssl,
+        //host:process.env.RDS_HOSTNAME,
+        //port:process.env.RDS_PORT,
+        username:configModule.projectConfig.username,
+        password: await configModule.projectConfig.password,
+    },
     database: configModule.projectConfig.database_database,
     extra: configModule.projectConfig.database_extra || {},
     entities,
