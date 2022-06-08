ae94b45548b2a5b25d9e6bb1de522a561a2bfda0
adrien2p <adrien.deperetti@gmail.com>
2022-06-08 14:39:09 +0200

    feat(medusa): Add description on normalized-query middleware

--- packages/medusa/src/api/middlewares/normalized-query.ts
+++ packages/medusa/src/api/middlewares/normalized-query.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express"
 
+/**
+ * Normalize an input query, especially from array like query params to an array type
+ * e.g: /admin/orders/?fields[]=id,status,cart_id becomes { fields: ["id", "status", "cart_id"] }
+ */
 export default (): ((
   req: Request,
   res: Response,
