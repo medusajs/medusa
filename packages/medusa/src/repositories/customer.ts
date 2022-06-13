211eb92b5d4dae0eb3947a58e226d5fd7fab6cb6
Philip Korsholm <philip.korsholm@hotmail.com>
2022-06-13 17:40:15 +0200

    centralize common knowledge and checks for list-customers in repository

--- packages/medusa/src/repositories/customer.ts
+++ packages/medusa/src/repositories/customer.ts
@@ -1,26 +1,48 @@
-import { EntityRepository, Repository } from "typeorm"
+import { Brackets, EntityRepository, ILike, Repository } from "typeorm"
 import { Customer } from "../models/customer"
 
 @EntityRepository(Customer)
 export class CustomerRepository extends Repository<Customer> {
-  async listAndCount(query, groups): Promise<[Customer[], number]> {
-    let qb = this.createQueryBuilder("customer")
-      .where(query.where)
+  async listAndCount(
+    query,
+    q: string | undefined = undefined
+  ): Promise<[Customer[], number]> {
+    const groups = query.where.groups
+    delete query.where.groups
+
+    const qb = this.createQueryBuilder("customer")
       .skip(query.skip)
       .take(query.take)
 
+    if (q) {
+      delete query.where.email
+      delete query.where.first_name
+      delete query.where.last_name
+
+      qb.where(
+        new Brackets((qb) => {
+          qb.where({ email: ILike(`%${q}%`) })
+            .orWhere({ first_name: ILike(`%${q}%`) })
+            .orWhere({ last_name: ILike(`%${q}%`) })
+        })
+      )
+    }
+
+    qb.andWhere(query.where)
+
     if (groups) {
-      qb = qb
-        .leftJoinAndSelect("customer.groups", "group")
-        .andWhere(`group.id IN (:...ids)`, { ids: groups.value })
+      qb.leftJoinAndSelect("customer.groups", "group").andWhere(
+        `group.id IN (:...ids)`,
+        { ids: groups.value }
+      )
     }
 
     if (query.relations?.length) {
       query.relations.forEach((rel) => {
-        qb = qb.leftJoinAndSelect(`customer.${rel}`, rel)
+        qb.leftJoinAndSelect(`customer.${rel}`, rel)
       })
     }
 
-    return await qb.getManyAndCount()
+    return qb.getManyAndCount()
   }
 }
