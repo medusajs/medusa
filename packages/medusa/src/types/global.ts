dfcd917bae97f89b7ab1b34f80d2855c66eb0bb8
SGF <SGF@sourcegoodfood.com>
2022-06-18 00:53:04 +0000

    feat:refactored to have single point of configuration promise resolution

--- packages/medusa/src/types/global.ts
+++ packages/medusa/src/types/global.ts
@@ -38,7 +38,7 @@ export type TlsOptions = {
   rejectUnauthorized: boolean | undefined
 }
 
-export type HostConfig =
+export type DatabaseHostConfig =
   | {
       database?: string
       password?: string | (() => string) | (() => Promise<string>)
