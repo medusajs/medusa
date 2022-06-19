56992d1f786749f3378c78333da3caead47d5043
fPolic <frane@medusajs.com>
2022-06-19 20:34:08 +0200

    feat: add subscribers, refactor strategies folder

--- packages/medusa/src/strategies/__tests__/product-import.ts
+++ packages/medusa/src/strategies/__tests__/product-import.ts
@@ -5,7 +5,7 @@ import { IdMap, MockManager, MockRepository } from "medusa-test-utils"
 
 import { User } from "../../models"
 import { BatchJobStatus } from "../../types/batch-job"
-import ProductImportStrategy from "../product-import"
+import ProductImportStrategy from "../product/import"
 
 let fakeJob = {
   id: IdMap.getId("product-import-job"),
