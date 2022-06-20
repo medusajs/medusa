17d3108b3f77b5dac3c5d1d25864e41c8173a300
fPolic <frane@medusajs.com>
2022-06-20 10:33:10 +0200

    fix: naming collision, medusa config

--- integration-tests/strategies/medusa-config.js
+++ integration-tests/strategies/medusa-config.js
@@ -3,6 +3,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD || ""
 const workerId = parseInt(process.env.JEST_WORKER_ID || "1")
 
 module.exports = {
+  plugins: [],
   projectConfig: {
     // redis_url: REDIS_URL,
     database_url: `postgres://${DB_USERNAME}:${DB_PASSWORD}@localhost/medusa-integration-${workerId}`,
