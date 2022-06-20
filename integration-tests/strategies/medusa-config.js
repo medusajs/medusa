3f70a5df3915a54deb59610b9155163f90dc5dd3
fPolic <frane@medusajs.com>
2022-06-20 15:08:23 +0200

    feat: make redis container run in integration tests

--- integration-tests/strategies/medusa-config.js
+++ integration-tests/strategies/medusa-config.js
@@ -5,10 +5,14 @@ const workerId = parseInt(process.env.JEST_WORKER_ID || "1")
 module.exports = {
   plugins: [],
   projectConfig: {
-    // redis_url: REDIS_URL,
     database_url: `postgres://${DB_USERNAME}:${DB_PASSWORD}@localhost/medusa-integration-${workerId}`,
     database_type: "postgres",
     jwt_secret: "test",
     cookie_secret: "test",
+    // redis_url: {
+    //   port: 6379,
+    //   host: "127.0.0.1",
+    //   db: workerId,
+    // },
   },
 }
