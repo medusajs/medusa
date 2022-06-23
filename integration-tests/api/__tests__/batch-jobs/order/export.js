f7f13e6d7ba66e690d6d1410be52b2e6e416f1f4
Philip Korsholm <philip.korsholm@hotmail.com>
2022-06-23 18:47:44 +0200

    remove verbose flag

--- integration-tests/api/__tests__/batch-jobs/order/export.js
+++ integration-tests/api/__tests__/batch-jobs/order/export.js
@@ -31,7 +31,7 @@ describe("Batchjob with type order-export", () => {
       cwd,
       redisUrl: "redis://127.0.0.1:6379",
       uploadDir: __dirname,
-      verbose: true,
+      verbose: false,
     })
   })
 
