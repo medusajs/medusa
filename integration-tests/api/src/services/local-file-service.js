82da194353a1961c158ca415e29cfa35ce2205f5
Philip Korsholm <philip.korsholm@hotmail.com>
2022-06-23 12:59:23 +0200

    linting of integration test files

--- integration-tests/api/src/services/local-file-service.js
+++ integration-tests/api/src/services/local-file-service.js
@@ -4,39 +4,41 @@ import { resolve } from "path"
 import * as fs from "fs"
 
 export default class LocalFileService extends AbstractFileService {
+  // eslint-disable-next-line no-empty-pattern
   constructor({}, options) {
-    super({});
-    this.upload_dir_ = process.env.UPLOAD_DIR ?? options.upload_dir ?? "uploads/images";
+    super({})
+    this.upload_dir_ =
+      process.env.UPLOAD_DIR ?? options.upload_dir ?? "uploads/images"
 
     if (!fs.existsSync(this.upload_dir_)) {
-      fs.mkdirSync(this.upload_dir_);
+      fs.mkdirSync(this.upload_dir_)
     }
   }
 
   upload(file) {
     return new Promise((resolve, reject) => {
       const path = resolve(this.upload_dir_, file.originalname)
-      fs.writeFile(path, "", err => {
+      fs.writeFile(path, "", (err) => {
         if (err) {
-          reject(err);
+          reject(err)
         }
 
-        resolve({ url: path });
-      });
-    });
+        resolve({ url: path })
+      })
+    })
   }
 
   delete({ name }) {
     return new Promise((resolve, _) => {
       const path = resolve(this.upload_dir_, name)
-      fs.unlink(path, err => {
+      fs.unlink(path, (err) => {
         if (err) {
-          throw err;
+          throw err
         }
 
-        resolve("file unlinked");
-      });
-    });
+        resolve("file unlinked")
+      })
+    })
   }
 
   async getUploadStreamDescriptor({ name, ext }) {
