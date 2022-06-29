8cc890bee492ec72a671c6b1196f55e02570d04f
fPolic <frane@medusajs.com>
2022-06-29 21:32:28 +0200

    fix: try reverting setup server

--- integration-tests/helpers/test-server.js
+++ integration-tests/helpers/test-server.js
@@ -1,25 +1,7 @@
-const redisLoader = require("@medusajs/medusa/dist/loaders/redis").default
-const path = require("path")
-
 const { bootstrapApp } = require("./bootstrap-app")
 
 const setup = async () => {
-  const cwd = path.resolve(path.join(__dirname, "..", "./api"))
-
-  const configPath = path.resolve(path.join(cwd, `medusa-config.js`))
-  const config = require(configPath)
-
-  const { app, container, port } = await bootstrapApp()
-
-  if (process.env.REDIS_URL) {
-    // after we know where the redis container is running, try connecting once more
-    config.projectConfig.redis_url = process.env.REDIS_URL
-    await redisLoader({
-      container,
-      configModule: config,
-      logger: {},
-    })
-  }
+  const { app, port } = await bootstrapApp()
 
   app.listen(port, (err) => {
     process.send(port)
