36acf092e0eb7d8964a29e3acb71f9661b6a269c
Zakaria El Asri <zakaria.elas@gmail.com>
2022-07-01 18:35:38 +0100

    make medusa-dev-cli compatible with yarn 2/3

--- packages/medusa-dev-cli/src/utils/get-monorepo-package-json-path.js
+++ packages/medusa-dev-cli/src/utils/get-monorepo-package-json-path.js
@@ -1,11 +1,4 @@
-const path = require(`path`);
+const path = require(`path`)
 
-exports.getMonorepoPackageJsonPath = ({ packageName, root }) => {
-  let dirName = packageName;
-  if (packageName.startsWith("@medusajs")) {
-    const [, directory] = packageName.split("/");
-    dirName = directory;
-  }
-
-  return path.join(root, `packages`, dirName, `package.json`);
-};
+exports.getMonorepoPackageJsonPath = ({ packageName, packageNameToPath }) =>
+  path.join(packageNameToPath.get(packageName), `package.json`)
