72a0b19fbfda0664b4ffc83feb2b33922688ceaa
fPolic <frane@medusajs.com>
2022-07-04 18:26:12 +0200

    fix: remove comment and import

--- packages/medusa/src/models/sales-channel.ts
+++ packages/medusa/src/models/sales-channel.ts
@@ -1,11 +1,10 @@
-import { BeforeInsert, Column, Entity } from "typeorm"
+import { BeforeInsert, Column } from "typeorm"
 
 import { SoftDeletableEntity } from "../interfaces"
 import { FeatureFlagEntity } from "../utils/feature-flag-decorators"
 import { resolveDbType } from "../utils/db-aware-column"
 import { generateEntityId } from "../utils"
 
-// @Entity()
 @FeatureFlagEntity("sales-channels")
 export class SalesChannel extends SoftDeletableEntity {
   @Column()
