4b06c657a8f55ed3f0021b0d086f537ee1c56c9e
adrien2p <adrien.deperetti@gmail.com>
2022-07-05 17:32:03 +0200

    feat(medusa): SalesChannel retrieve end point OAS

--- packages/medusa/src/api/routes/admin/sales-channels/get-sales-channels.ts
+++ packages/medusa/src/api/routes/admin/sales-channels/get-sales-channels.ts
@@ -1,6 +1,26 @@
 import { Response } from "express"
 import { SalesChannelRequest } from "../../../../types/sales-channels"
 
+/**
+ * @oas [get] /sales-channels/{id}
+ * operationId: "GetSalesChannelsSalesChannel"
+ * summary: "Retrieve a sales channel"
+ * description: "Retrieves the sales channel."
+ * x-authenticated: true
+ * parameters:
+ *   - (path) id=* {string} The id of the Sales channel.
+ * tags:
+ *   - Sales channel
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             sales_channel:
+ *               $ref: "#/components/schemas/sales-channel"
+ */
 export default async (
   req: SalesChannelRequest,
   res: Response
