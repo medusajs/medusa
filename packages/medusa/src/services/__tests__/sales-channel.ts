09ba15ac58a38da82ba63083e6723f97857eebda
adrien2p <adrien.deperetti@gmail.com>
2022-07-06 16:32:31 +0200

    test(medusa): fix

--- packages/medusa/src/services/__tests__/sales-channel.ts
+++ packages/medusa/src/services/__tests__/sales-channel.ts
@@ -40,7 +40,7 @@ describe("SalesChannelService", () => {
       salesChannelRepository: salesChannelRepositoryMock,
     })
 
-    afterEach(() => {
+    beforeEach(() => {
       jest.clearAllMocks()
     })
 
@@ -103,7 +103,7 @@ describe("SalesChannelService", () => {
       salesChannelRepository: salesChannelRepositoryMock
     })
 
-    afterEach(() => {
+    beforeEach(() => {
       jest.clearAllMocks()
     })
 
