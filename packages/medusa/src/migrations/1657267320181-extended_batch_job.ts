ba52b4f9df18a77f4b7e4d8f884c915eca54a74b
olivermrbl <oliver@mrbltech.com>
2022-07-11 10:19:22 +0200

    fix: New take on the migrations

--- packages/medusa/src/migrations/1657267320181-extended_batch_job.ts
+++ packages/medusa/src/migrations/1657267320181-extended_batch_job.ts
@@ -1,36 +1,37 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class extendedBatchJob1657267320181 implements MigrationInterface {
-    name = 'extendedBatchJob1657267320181'
+  name = "extendedBatchJob1657267320181"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "status"`);
-        await queryRunner.query(`DROP TYPE "public"."batch_job_status_enum"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "dry_run" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "pre_processed_at" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "processing_at" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "confirmed_at" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "completed_at" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "canceled_at" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "failed_at" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "created_by"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "created_by" character varying`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD CONSTRAINT "FK_cdf30493ba1c9ef207e1e80c10a" FOREIGN KEY ("created_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // check if batch job table exists and status column exists
+    // if that's the case, the previous migration has already been run
+    const batchJobTableExists = await queryRunner.query(`SELECT exists (
+            SELECT FROM information_schema.columns
+              WHERE  table_name = 'batch_job'
+              AND    column_name   = 'status'
+              )`)
+
+    // if the table exists, we alter the table to add the new columns
+    if (batchJobTableExists[0].exists) {
+      await queryRunner.query(`
+        ALTER TABLE "batch_job" DROP COLUMN "status";
+        DROP TYPE "public"."batch_job_status_enum";
+        ALTER TABLE "batch_job" ADD "dry_run" boolean NOT NULL DEFAULT false;
+        ALTER TABLE "batch_job" ADD "pre_processed_at" TIMESTAMP WITH TIME ZONE;
+        ALTER TABLE "batch_job" ADD "processing_at" TIMESTAMP WITH TIME ZONE;
+        ALTER TABLE "batch_job" ADD "confirmed_at" TIMESTAMP WITH TIME ZONE;
+        ALTER TABLE "batch_job" ADD "completed_at" TIMESTAMP WITH TIME ZONE;
+        ALTER TABLE "batch_job" ADD "canceled_at" TIMESTAMP WITH TIME ZONE;
+        ALTER TABLE "batch_job" ADD "failed_at" TIMESTAMP WITH TIME ZONE;
+        ALTER TABLE "batch_job" DROP COLUMN "created_by";
+        ALTER TABLE "batch_job" ADD "created_by" character varying;
+        ALTER TABLE "batch_job" ADD CONSTRAINT "FK_cdf30493ba1c9ef207e1e80c10a" FOREIGN KEY ("created_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+        `)
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP CONSTRAINT "FK_cdf30493ba1c9ef207e1e80c10a"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "created_by"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "created_by" text`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "failed_at"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "canceled_at"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "completed_at"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "confirmed_at"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "processing_at"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "pre_processed_at"`);
-        await queryRunner.query(`ALTER TABLE "batch_job" DROP COLUMN "dry_run"`);
-        await queryRunner.query(`CREATE TYPE "public"."batch_job_status_enum" AS ENUM('created', 'processing', 'awaiting_confirmation', 'completed')`);
-        await queryRunner.query(`ALTER TABLE "batch_job" ADD "status" "public"."batch_job_status_enum" NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    void 0
+  }
 }
