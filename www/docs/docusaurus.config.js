1a3895e80683ef9331d4a16bc4b5f3dbcb49352f
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-07-26 20:14:02 +0530

    removed docs link from the footer

--- www/docs/docusaurus.config.js
+++ www/docs/docusaurus.config.js
@@ -92,15 +92,6 @@ module.exports = {
     footer: {
       links: [
         {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/tutorial/set-up-your-development-environment",
-            },
-          ],
-        },
-        {
           title: "Community",
           items: [
             {
