b18ee6cd0437ce719e1f7d8d1aabcaccb94405d5
olivermrbl <oliver@mrbltech.com>
2022-07-28 20:11:32 +0200

    fix(medusa-js): Use correct customer payment methods path

--- packages/medusa-js/src/resources/payment-methods.ts
+++ packages/medusa-js/src/resources/payment-methods.ts
@@ -1,4 +1,4 @@
-import { StoreCustomersListPaymentMethodsRes } from '@medusajs/medusa'
+import { StoreCustomersListPaymentMethodsRes } from "@medusajs/medusa"
 import { ResponsePromise } from "../typings"
 import BaseResource from "./base"
 
@@ -9,8 +9,10 @@ class PaymentMethodsResource extends BaseResource {
    * @param customHeaders
    * @return {StoreCustomersListPaymentMethodsRes}
    */
-  list(id: string, customHeaders: Record<string, any> = {}): ResponsePromise<StoreCustomersListPaymentMethodsRes> {
-    const path = `/store/carts/${id}/payment-methods`
+  list(
+    customHeaders: Record<string, any> = {}
+  ): ResponsePromise<StoreCustomersListPaymentMethodsRes> {
+    const path = `/store/customers/me/payment-methods`
     return this.client.request("GET", path, undefined, {}, customHeaders)
   }
 }
