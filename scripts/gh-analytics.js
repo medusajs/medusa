639e9d39ff589dee8a729cbc409eb2466a0c1870
Shahed Nasser <shahednasser@gmail.com>
2022-07-29 15:16:34 +0300

    added script to retrieve analytics

---
+++ scripts/gh-analytics.js
@@ -0,0 +1,55 @@
+const { Octokit } = require("@octokit/core");
+const Analytics = require('analytics-node');
+const uniqid = require('uniqid'); 
+
+async function retrieveAnalytics () {
+  const octokit = new Octokit({
+    auth: process.env.GH_TOKEN
+  })
+  const analytics = new Analytics(process.env.SEGMENT_API_KEY);
+  const date = new Date();
+  
+  console.log("Retrieving referrals...")
+  const referrersResponse = await octokit.request('GET /repos/{owner}/{repo}/traffic/popular/referrers', {
+    owner: 'medusajs',
+    repo: 'medusa'
+  })
+  console.log(`Pushing ${referrersResponse.data.length} referrers...`);
+  referrersResponse.data.forEach((ref) => {
+    if (!process.env.DEBUG) {
+      analytics.page({
+        anonymousId: uniqid(),
+        category: 'referrals',
+        properties: {
+          title: ref.referrer,
+          count: ref.uniques
+        },
+        timestamp: date
+      })
+    }
+  })
+  
+  console.log("Retrieving popular paths...")
+  const pathsResponse = await octokit.request('GET /repos/{owner}/{repo}/traffic/popular/paths', {
+    owner: 'medusajs',
+    repo: 'medusa'
+  })
+  console.log(`Pushing ${pathsResponse.data.length} paths...`);
+  
+  pathsResponse.data.forEach((path) => {
+    if (!process.env.DEBUG) {
+      analytics.page({
+        anonymousId: uniqid(),
+        category: 'popular_paths',
+        properties: {
+          title: path.title,
+          path: path.path,
+          count: path.uniques
+        },
+        timestamp: date
+      })
+    }
+  })
+}
+
+retrieveAnalytics();
