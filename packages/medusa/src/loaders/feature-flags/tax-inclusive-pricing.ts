873767a3e2c3ffaafcf87de37c8117e7beaa78a4
adrien2p <adrien.deperetti@gmail.com>
2022-08-04 13:36:35 +0200

    feat(medusa): revert flag

--- packages/medusa/src/loaders/feature-flags/tax-inclusive-pricing.ts
+++ packages/medusa/src/loaders/feature-flags/tax-inclusive-pricing.ts
@@ -3,7 +3,7 @@ import { FlagSettings } from "../../types/feature-flags"
 const TaxInclusivePricingFeatureFlag: FlagSettings = {
   key: "tax_inclusive_pricing",
   default_val: false,
-  env_key: "MEDUSA_FF_TAX_INCLUSIVE_PRICING_PRICING",
+  env_key: "MEDUSA_FF_TAX_INCLUSIVE_PRICING",
   description: "[WIP] Enable tax inclusive pricing",
 }
 
