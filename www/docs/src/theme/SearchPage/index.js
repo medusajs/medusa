b126ab4decddfa29c780f37db2d3f84bb8043c8c
Shahed Nasser <shahednasser@gmail.com>
2022-08-04 19:09:36 +0300

    docs: update doocusaurus to 2.0.1 (#1997)

--- www/docs/src/theme/SearchPage/index.js
+++ www/docs/src/theme/SearchPage/index.js
@@ -1,19 +1,16 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+import {
+  HtmlClassNameProvider,
+  isRegexpStringMatch,
+  useEvent,
+  usePluralForm,
+} from '@docusaurus/theme-common';
 /* eslint-disable jsx-a11y/no-autofocus */
 import React, {useEffect, useReducer, useRef, useState} from 'react';
 import Translate, {translate} from '@docusaurus/Translate';
 import {
-  isRegexpStringMatch,
-  useDynamicCallback,
-  usePluralForm,
   useSearchPage,
   useTitleFormatter,
-} from '@docusaurus/theme-common';
+} from '@docusaurus/theme-common/internal';
 
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import Head from '@docusaurus/Head';
@@ -25,7 +22,6 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 import {useAllDocsData} from '@docusaurus/plugin-content-docs/client';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-
 // Very simple pluralization: probably good enough for now
 function useDocumentsFoundPlural() {
   const {selectMessage} = usePluralForm();
@@ -111,7 +107,7 @@ function SearchVersionSelectList({docsSearchVersionsHelpers}) {
     </div>
   );
 }
-export default function SearchPage() {
+function SearchPageContent() {
   const {
     siteConfig: {themeConfig},
     i18n: {currentLocale},
@@ -175,7 +171,7 @@ export default function SearchPage() {
   algoliaHelper.on(
     'result',
     ({results: {query, hits, page, nbHits, nbPages}}) => {
-      if (query === '' || !(hits instanceof Array)) {
+      if (query === '' || !Array.isArray(hits)) {
         searchResultStateDispatcher({type: 'reset'});
         return;
       }
@@ -223,7 +219,7 @@ export default function SearchPage() {
   const [loaderRef, setLoaderRef] = useState(null);
   const prevY = useRef(0);
   const observer = useRef(
-    ExecutionEnvironment.canUseDOM &&
+    ExecutionEnvironment.canUseIntersectionObserver &&
       new IntersectionObserver(
         (entries) => {
           const {
@@ -255,7 +251,7 @@ export default function SearchPage() {
           message: 'Search the documentation',
           description: 'The search page title for empty query',
         });
-  const makeSearch = useDynamicCallback((page = 0) => {
+  const makeSearch = useEvent((page = 0) => {
     algoliaHelper.setQuery(searchQuery).setPage(page).search();
   });
   useEffect(() => {
@@ -285,7 +281,7 @@ export default function SearchPage() {
     makeSearch(searchResultState.lastPage);
   }, [makeSearch, searchResultState.lastPage]);
   return (
-    <Layout wrapperClassName="search-page-wrapper">
+    <Layout>
       <Head>
         <title>{useTitleFormatter(getTitle())}</title>
         {/*
@@ -443,5 +439,12 @@ export default function SearchPage() {
         )}
       </div>
     </Layout>
+  );
+}
+export default function SearchPage() {
+  return (
+    <HtmlClassNameProvider className="search-page-wrapper">
+      <SearchPageContent />
+    </HtmlClassNameProvider>
   );
 }
