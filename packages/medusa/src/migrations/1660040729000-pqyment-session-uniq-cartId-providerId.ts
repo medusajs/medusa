1bb2eebccb88798f6e5e0670560095f86d98b303
adrien2p <adrien.deperetti@gmail.com>
2022-08-09 15:14:21 +0200

    fix(medusa): migration

--- packages/medusa/src/migrations/1660040729000-pqyment-session-uniq-cartId-providerId.ts
+++ packages/medusa/src/migrations/1660040729000-pqyment-session-uniq-cartId-providerId.ts
@@ -4,9 +4,7 @@ export class paymentSessionUniqCartIdProviderId1660040729000 implements Migratio
   name = "paymentSessionUniqCartIdProviderId1660040729000"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      await queryRunner.query(`CREATE UNIQUE INDEX "UniqPaymentSessionCartIdProviderId" ON "payment_session" ("cart_id", "provider_id")`)
-    )
+    await queryRunner.query(`CREATE UNIQUE INDEX "UniqPaymentSessionCartIdProviderId" ON "payment_session" ("cart_id", "provider_id")`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
