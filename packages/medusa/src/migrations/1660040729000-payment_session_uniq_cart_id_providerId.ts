fef5bfbe277e1a7227eea5e1f54cc5b08f527a99
olivermrbl <oliver@mrbltech.com>
2022-08-16 13:43:12 +0200

    fix: Migrations

--- packages/medusa/src/migrations/1660040729000-payment_session_uniq_cart_id_providerId.ts
+++ packages/medusa/src/migrations/1660040729000-payment_session_uniq_cart_id_providerId.ts
@@ -9,13 +9,9 @@ export class paymentSessionUniqCartIdProviderId1660040729000
     await queryRunner.query(
       `CREATE UNIQUE INDEX "UniqPaymentSessionCartIdProviderId" ON "payment_session" ("cart_id", "provider_id")`
     )
-    await queryRunner.query(
-      `CREATE UNIQUE INDEX "UniquePaymentActive" ON "payment" ("cart_id") WHERE "canceled_at" IS NULL`
-    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX "UniqPaymentSessionCartIdProviderId"`)
-    await queryRunner.query(`DROP INDEX "UniquePaymentActive"`)
   }
 }
