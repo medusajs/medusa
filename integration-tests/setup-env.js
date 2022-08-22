082db80436caeebe3fa0f40debfc26ff5a20b5e2
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-08-22 18:54:53 -0300

    fix(integration): temp db name

--- integration-tests/setup-env.js
+++ integration-tests/setup-env.js
@@ -3,6 +3,10 @@ const path = require("path")
 require("dotenv").config({ path: path.join(__dirname, ".env.test") })
 
 if (typeof process.env.DB_TEMP_NAME === "undefined") {
-  const tempName = parseInt(process.env.JEST_WORKER_ID || "1")
+  const tempName = [
+    Date.now(),
+    Math.floor(Math.random() * 9e2),
+    parseInt(process.env.JEST_WORKER_ID || "1"),
+  ].join("-")
   process.env.DB_TEMP_NAME = `medusa-integration-${tempName}`
 }
