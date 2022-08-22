644727e9f6a3e21b2a14f9848dbde3c2c6018490
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-08-22 18:27:12 -0300

    chore(integration): return jest_worker_id as db name

--- integration-tests/setup-env.js
+++ integration-tests/setup-env.js
@@ -3,10 +3,6 @@ const path = require("path")
 require("dotenv").config({ path: path.join(__dirname, ".env.test") })
 
 if (typeof process.env.DB_TEMP_NAME === "undefined") {
-  const tempName = [
-    Date.now(),
-    Math.floor(Math.random() * 9e2),
-    parseInt(process.env.JEST_WORKER_ID || "1"),
-  ].join("-")
+  const tempName = parseInt(process.env.JEST_WORKER_ID || "1")
   process.env.DB_TEMP_NAME = `medusa-integration-${tempName}`
 }
