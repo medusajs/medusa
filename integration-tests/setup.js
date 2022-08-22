719a94ef974f3f7917f7dbfb4c1607c0e4cab197
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-08-22 14:23:59 -0300

    fix(integration): setup env

--- integration-tests/setup.js
+++ integration-tests/setup.js
@@ -1,16 +1,9 @@
-const path = require("path")
 const { dropDatabase } = require("pg-god")
-
-require("dotenv").config({ path: path.join(__dirname, ".env.test") })
-
-if(typeof process.env.DB_TEMP_NAME === "undefined") {
-  const tempName = [Date.now(), Math.floor(Math.random() * 9e2), parseInt(process.env.JEST_WORKER_ID || "1")].join("-")
-  process.env.DB_TEMP_NAME = `medusa-integration-${tempName}`
-}
 
 const DB_HOST = process.env.DB_HOST
 const DB_USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_TEMP_NAME
 
 const pgGodCredentials = {
   user: DB_USERNAME,
@@ -20,7 +13,7 @@ const pgGodCredentials = {
 
 afterAll(async () => {
   await dropDatabase(
-    { databaseName: process.env.DB_TEMP_NAME },
+    { databaseName: DB_NAME },
     pgGodCredentials
   )
 })
