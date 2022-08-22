cb4861953dbe902fd9bc872212d4bd03b0413199
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-08-22 13:55:43 -0300

    chore(integration): normalize DB config

--- integration-tests/setup.js
+++ integration-tests/setup.js
@@ -1,20 +1,26 @@
 const path = require("path")
 const { dropDatabase } = require("pg-god")
 
-require("dotenv").config({ path: path.join(__dirname, ".env") })
+require("dotenv").config({ path: path.join(__dirname, ".env.test") })
 
-const DB_USERNAME = process.env.DB_USERNAME || "postgres"
-const DB_PASSWORD = process.env.DB_PASSWORD || ""
+if(typeof process.env.DB_TEMP_NAME === "undefined") {
+  const tempName = [Date.now(), Math.floor(Math.random() * 9e2), parseInt(process.env.JEST_WORKER_ID || "1")].join("-")
+  process.env.DB_TEMP_NAME = `medusa-integration-${tempName}`
+}
+
+const DB_HOST = process.env.DB_HOST
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
 
 const pgGodCredentials = {
   user: DB_USERNAME,
   password: DB_PASSWORD,
+  host: DB_HOST,
 }
 
 afterAll(async () => {
-  const workerId = parseInt(process.env.JEST_WORKER_ID || "1")
   await dropDatabase(
-    { databaseName: `medusa-integration-${workerId}` },
+    { databaseName: process.env.DB_TEMP_NAME },
     pgGodCredentials
   )
 })
