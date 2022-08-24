85d2993dde33d9188448198c23f306a083f9c76b
fPolic <frane@medusajs.com>
2022-08-24 14:36:13 +0200

    fix: remove unused input

--- packages/medusa/src/api/routes/admin/swaps/index.ts
+++ packages/medusa/src/api/routes/admin/swaps/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { Swap } from "../../../.."
-import { FindConfig, PaginatedResponse } from "../../../../types/common"
+import { PaginatedResponse } from "../../../../types/common"
 import middlewares from "../../../middlewares"
 
 const route = Router()
