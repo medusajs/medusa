45bf31c450d6e315f8d85a6972ebdb3c1984734f
fPolic <frane@medusajs.com>
2022-08-25 12:17:21 +0200

    fix: remove redis from integration test

--- integration-tests/api/__tests__/batch-jobs/product/import.js
+++ integration-tests/api/__tests__/batch-jobs/product/import.js
@@ -27,7 +27,6 @@ describe("Product import batch job", () => {
 
     medusaProcess = await setupServer({
       cwd,
-      redisUrl: "redis://127.0.0.1:6379",
       uploadDir: __dirname,
       verbose: false,
     })
