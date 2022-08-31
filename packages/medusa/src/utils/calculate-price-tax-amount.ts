dc249c35b99cd03abfce5759fb052c713db7f04c
adrien2p <adrien.deperetti@gmail.com>
2022-08-31 10:07:45 +0200

    comment

--- packages/medusa/src/utils/calculate-price-tax-amount.ts
+++ packages/medusa/src/utils/calculate-price-tax-amount.ts
@@ -1,6 +1,14 @@
 import { featureFlagRouter } from "../loaders/feature-flags";
 import TaxInclusivePricingFeatureFlag from "../loaders/feature-flags/tax-inclusive-pricing";
 
+/**
+ * Return the tax amount that
+ * - is includes in the price if it is tax inclusive
+ * - will be applied on to the price if it is tax exclusive
+ * @param price
+ * @param includesTax
+ * @param taxRate
+ */
 export function calculatePriceTaxAmount({
   price,
   includesTax,
