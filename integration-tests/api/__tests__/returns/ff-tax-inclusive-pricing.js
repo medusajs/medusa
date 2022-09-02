7886e68043d1ec4953410fbe8a6ca4a0574466de
adrien2p <adrien.deperetti@gmail.com>
2022-09-02 15:22:44 +0200

    test(integration): Add tax exclusive tests with new/old tax system

--- integration-tests/api/__tests__/returns/ff-tax-inclusive-pricing.js
+++ integration-tests/api/__tests__/returns/ff-tax-inclusive-pricing.js
@@ -28,6 +28,11 @@ const createReturnableOrder = async (dbConnection, options) => {
     ]
   }
 
+  let unitPrice = options.includes_tax ? 1200 : 1000
+  if (options.oldTaxes) {
+    unitPrice = options.includes_tax ? 1125 : 1000
+  }
+
   return await simpleOrderFactory(dbConnection, {
     email: "test@testson.com",
     tax_rate: options?.oldTaxes ? undefined : null,
@@ -44,8 +49,8 @@ const createReturnableOrder = async (dbConnection, options) => {
         quantity: 2,
         fulfilled_quantity: options?.shipped ? 2 : undefined,
         shipped_quantity: options?.shipped ? 2 : undefined,
-        unit_price: options?.oldTaxes ? 1125 : 1200,
-        includes_tax: true,
+        unit_price: unitPrice,
+        includes_tax: options?.includes_tax,
         tax_lines: [
           {
             name: "default",
@@ -87,10 +92,13 @@ describe("[MEDUSA_FF_TAX_INCLUSIVE_PRICING] /store/carts", () => {
     return await db.teardown()
   })
 
-  it("creates a return with the old tax system", async () => {
+  it("creates a return with the old tax system and tax inclusive line", async () => {
     await adminSeeder(dbConnection)
 
-    const order = await createReturnableOrder(dbConnection, { oldTaxes: true })
+    const order = await createReturnableOrder(dbConnection, {
+      oldTaxes: true,
+      includes_tax: true,
+    })
     const api = useApi()
 
     const response = await api.post(
@@ -130,10 +138,103 @@ describe("[MEDUSA_FF_TAX_INCLUSIVE_PRICING] /store/carts", () => {
     )
   })
 
-  it("creates a return", async () => {
+  it("creates a return with the old tax system and tax exclusive line", async () => {
     await adminSeeder(dbConnection)
 
-    const order = await createReturnableOrder(dbConnection)
+    const order = await createReturnableOrder(dbConnection, {
+      oldTaxes: true,
+      includes_tax: false,
+    })
+    const api = useApi()
+
+    const response = await api.post(
+      `/admin/orders/${order.id}/return`,
+      {
+        items: [
+          {
+            item_id: "test-item",
+            quantity: 1,
+            note: "TOO SMALL",
+          },
+        ],
+      },
+      {
+        headers: {
+          authorization: "Bearer test_token",
+        },
+      }
+    )
+
+    expect(response.status).toEqual(200)
+
+    /*
+     * Region has default tax rate 12.5 therefore refund amount should be
+     * 1000 * 1.125 = 1125
+     */
+    expect(response.data.order.returns[0].refund_amount).toEqual(1125)
+    expect(response.data.order.returns[0].items).toHaveLength(1)
+    expect(response.data.order.returns[0].items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          item_id: "test-item",
+          quantity: 1,
+          note: "TOO SMALL",
+        }),
+      ])
+    )
+  })
+
+  it("creates a return with tax inclusive line", async () => {
+    await adminSeeder(dbConnection)
+
+    const order = await createReturnableOrder(dbConnection, {
+      includes_tax: true,
+    })
+    const api = useApi()
+
+    const response = await api.post(
+      `/admin/orders/${order.id}/return`,
+      {
+        items: [
+          {
+            item_id: "test-item",
+            quantity: 1,
+            note: "TOO SMALL",
+          },
+        ],
+      },
+      {
+        headers: {
+          authorization: "Bearer test_token",
+        },
+      }
+    )
+
+    expect(response.status).toEqual(200)
+
+    /*
+     * Region has a tax rate of 20% therefore refund amount should be
+     * 1000 * 1.2 = 1200
+     */
+    expect(response.data.order.returns[0].refund_amount).toEqual(1200)
+    expect(response.data.order.returns[0].items).toHaveLength(1)
+    expect(response.data.order.returns[0].items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          item_id: "test-item",
+          quantity: 1,
+          note: "TOO SMALL",
+        }),
+      ])
+    )
+  })
+
+  it("creates a return with tax exclusive line", async () => {
+    await adminSeeder(dbConnection)
+
+    const order = await createReturnableOrder(dbConnection, {
+      includes_tax: false,
+    })
     const api = useApi()
 
     const response = await api.post(
