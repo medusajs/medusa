12bef0216dd1ca0614f99314d7bee1395135f1d3
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-09-07 13:48:38 -0300

    chore: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,14 +12,13 @@ module.exports = {
     "google",
   ],
   rules: {
-    curly: [2, "all"],
+    curly: [1, "all"],
     "new-cap": "off",
     "require-jsdoc": "off",
     "no-unused-expressions": "off",
     "no-unused-vars": "off",
     camelcase: "off",
     "no-invalid-this": "off",
-
     "max-len": ["error", {
       code: 80,
       ignoreStrings: true,
@@ -31,11 +30,26 @@ module.exports = {
     }],
     indent: ["error", 2],
     semi: ["error", "never"],
-    quotes: ["error", "double"],
-    "comma-dangle": ["error", "never"],
+    quotes: ["error", "double", {
+      "allowTemplateLiterals": true
+    }],
+    "comma-dangle": ["error", {
+      arrays: "never",
+      objects: "always-multiline",
+      imports: "always-multiline",
+      exports: "always-multiline",
+      functions: "never"
+    }],
     "object-curly-spacing": ["error", "always"],
     "arrow-parens": ["error", "always"],
     "linebreak-style": 0,
+    "no-confusing-arrow":  ["error", {
+      allowParens: false
+    }],
+    "no-mixed-operators": ["error"],
+    "no-use-before-define": "error",
+    "space-before-function-paren": ["error", "never"],
+    "space-infix-ops": "error",
   },
   env: {
     es6: true,
@@ -46,9 +60,7 @@ module.exports = {
   ],
   overrides: [
     {
-      files: [
-        "packages/medusa/**/*.ts"
-      ],
+      files: ["*.ts"],
       plugins: ["@typescript-eslint/eslint-plugin"],
       extends: ["plugin:@typescript-eslint/recommended"],
       parser: "@typescript-eslint/parser",
@@ -56,12 +68,31 @@ module.exports = {
         project: './packages/medusa/tsconfig.json'
       },
       rules: {
-        "valid-jsdoc": ["off"],
-        "@typescript-eslint/explicit-function-return-type": ["error"],
-        "@typescript-eslint/no-non-null-assertion": ["off"],
-        "@typescript-eslint/no-unused-vars": ["off"],
-        "@typescript-eslint/no-explicit-any": ["off"],
-        "@typescript-eslint/no-floating-promises": ["error"]
+        "valid-jsdoc": "off",
+        "brace-style": "off",
+        "keyword-spacing": "off",
+        "no-use-before-define": "off",
+        "space-before-function-paren": "off",
+        "space-infix-ops": "off",
+
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/await-thenable": "error",
+        "@typescript-eslint/promise-function-async": "error",
+        "@typescript-eslint/brace-style": ["error", "1tbs", {
+          "allowSingleLine": false
+        }],
+        "@typescript-eslint/keyword-spacing": "error",
+        "@typescript-eslint/no-use-before-define": "error",
+        "@typescript-eslint/space-before-function-paren": ["error", "never"],
+        "@typescript-eslint/space-infix-ops": "error",
+
+        // --- Rules to be fixed
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/ban-types": "off",
       },
     },
   ],
