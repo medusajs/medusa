142fea1e57a65e22bb84637fc2e3ecd5e4f5787b
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-09-08 10:23:22 -0300

    chore: lint error message

--- scripts/on-lint-error.js
+++ scripts/on-lint-error.js
@@ -1,8 +1,7 @@
 console.log(`
 
-Oops! Medusa noticed some lint or style warnings in the code for this
-commit. Your changes have been committed, but you should fix the warnings before
-creating a pull request.
+Oops! Medusa noticed some lint or style issues in the code for this
+commit. You need to fix the issues before pushing the changes.
 Use 'yarn run lint' to manually re-run these checks. You can also disable these
 checks:
 - for a single commit: git commit --no-verify
