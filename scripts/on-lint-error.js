b6b1c5d1e6003153cb72fb5896e382c77b6c0baa
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-09-08 09:22:35 -0300

    chore: husky pre commit message

--- scripts/on-lint-error.js
+++ scripts/on-lint-error.js
@@ -3,9 +3,9 @@ console.log(`
 Oops! Medusa noticed some lint or style warnings in the code for this
 commit. Your changes have been committed, but you should fix the warnings before
 creating a pull request.
-Use 'npm run lint' to manually re-run these checks. You can also disable these
+Use 'yarn run lint' to manually re-run these checks. You can also disable these
 checks:
 - for a single commit: git commit --no-verify
-- for all future commits: npm run hooks:uninstall
+- for all future commits: yarn run hooks:uninstall
 
 `)
