673ec00d4821def9847f73b95cdddc873f9c41ee
fPolic <frane@medusajs.com>
2022-09-13 13:18:37 +0200

    feat: order item change oas

--- packages/medusa/src/models/order-item-change.ts
+++ packages/medusa/src/models/order-item-change.ts
@@ -57,3 +57,46 @@ export class OrderItemChange extends SoftDeletableEntity {
     this.id = generateEntityId(this.id, "oic")
   }
 }
+
+/**
+ * @schema order_item_change
+ * title: "Order Item Change"
+ * description: "Represents an order edit item change"
+ * x-resourceId: order_item_change
+ * required:
+ *   - type
+ *   - order_edit_id
+ * properties:
+ *   id:
+ *     type: string
+ *     description: The order item change's ID
+ *     example: oic_01G8TJSYT9M6AVS5N4EMNFS1EK
+ *   type:
+ *     type: string
+ *     description: The order's status
+ *     enum:
+ *       - item_add
+ *       - item_remove
+ *       - item_update
+ *   order_edit_id:
+ *     type: string
+ *     description: The ID of the order edit
+ *     example: oe_01G2SG30J8C85S4A5CHM2S1NS2
+ *   order_edit:
+ *     description: Order edit object
+ *     $ref: "#/components/schemas/order_edit"
+ *   original_line_item_id:
+ *      type: string
+ *      description: The ID of the original line item in the order
+ *      example: item_01G8ZC9GWT6B2GP5FSXRXNFNGN
+ *   original_line_item:
+ *      description: Original line item object.
+ *      $ref: "#/components/schemas/line_item"
+ *   line_item_id:
+ *      type: string
+ *      description: The ID of the cloned line item.
+ *      example: item_01G8ZC9GWT6B2GP5FSXRXNFNGN
+ *   line_item:
+ *      description: Line item object.
+ *      $ref: "#/components/schemas/line_item"
+ */
