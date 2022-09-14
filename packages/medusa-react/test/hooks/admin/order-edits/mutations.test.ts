8371b08e0b11d8521615ab1facf11d7f3db68c44
Philip Korsholm <philip.korsholm@hotmail.com>
2022-09-14 16:29:21 +0700

    hook test update

--- packages/medusa-react/test/hooks/admin/order-edits/mutations.test.ts
+++ packages/medusa-react/test/hooks/admin/order-edits/mutations.test.ts
@@ -5,7 +5,7 @@ import { createWrapper } from "../../../utils"
 
 describe("useAdminDelete hook", () => {
   test("Deletes an order edit", async () => {
-    const id = "id"
+    const id = "id test"
     const { result, waitFor } = renderHook(() => useAdminDeleteOrderEdit(id), {
       wrapper: createWrapper(),
     })
@@ -15,10 +15,12 @@ describe("useAdminDelete hook", () => {
     await waitFor(() => result.current.isSuccess)
 
     expect(result.current.data.response.status).toEqual(200)
-    expect(result.current.data).toEqual({
-      id,
-      object: "order_edit",
-      deleted: true,
-    })
+    expect(result.current.data).toEqual(
+      expect.objectContaining({
+        id,
+        object: "order_edit",
+        deleted: true,
+      })
+    )
   })
 })
