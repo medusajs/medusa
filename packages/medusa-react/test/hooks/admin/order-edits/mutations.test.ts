c952a43eab6a643e916f1d759b2d7384b1da891f
Philip Korsholm <philip.korsholm@hotmail.com>
2022-09-14 16:22:24 +0700

    order edit deletion test medusa react

---
+++ packages/medusa-react/test/hooks/admin/order-edits/mutations.test.ts
@@ -0,0 +1,24 @@
+import { useAdminDeleteOrderEdit } from "../../../../src/"
+import { renderHook } from "@testing-library/react-hooks"
+import { fixtures } from "../../../../mocks/data"
+import { createWrapper } from "../../../utils"
+
+describe("useAdminDelete hook", () => {
+  test("Deletes an order edit", async () => {
+    const id = "id"
+    const { result, waitFor } = renderHook(() => useAdminDeleteOrderEdit(id), {
+      wrapper: createWrapper(),
+    })
+
+    result.current.mutate()
+
+    await waitFor(() => result.current.isSuccess)
+
+    expect(result.current.data.response.status).toEqual(200)
+    expect(result.current.data).toEqual({
+      id,
+      object: "order_edit",
+      deleted: true,
+    })
+  })
+})
