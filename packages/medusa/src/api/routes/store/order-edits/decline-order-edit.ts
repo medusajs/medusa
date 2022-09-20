554e05a8bee69af13d0a4ca59ef4a9857a8bee81
Philip Korsholm <philip.korsholm@hotmail.com>
2022-09-20 14:24:11 +0700

    decline order edit

---
+++ packages/medusa/src/api/routes/store/order-edits/decline-order-edit.ts
@@ -0,0 +1,78 @@
+import { IsOptional, IsString } from "class-validator"
+import { Request, Response } from "express"
+import { EntityManager } from "typeorm"
+import { OrderEditService } from "../../../../services"
+import { validator } from "../../../../utils/validator"
+
+/**
+ * @oas [get] /order-edits/{id}/cancel
+ * operationId: "PostOrderEditsOrderEditCancel"
+ * summary: "Cancel an OrderEdit"
+ * description: "Cancels a OrderEdit."
+ * parameters:
+ *   - (path) id=* {string} The ID of the OrderEdit.
+ *   - (body) declined_reason= {string} The reason for canceling the OrderEdit.
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       medusa.orderEdit.cancel(orderEditId)
+ *       .then(({ order_edit }) => {
+ *         console.log(order_edit.id);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl --location --request GET 'https://medusa-url.com/store/order-edits/{id}'
+ * tags:
+ *   - OrderEdit
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             order_edit:
+ *               $ref: "#/components/schemas/order_edit"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "401":
+ *     $ref: "#/components/responses/unauthorized"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req: Request, res: Response) => {
+  const { id } = req.params
+  const { validatedBody } = req as {
+    validatedBody: StorePostOrderEditsOrderEditCancel
+  }
+
+  const orderEditService: OrderEditService =
+    req.scope.resolve("orderEditService")
+
+  const manager: EntityManager = req.scope.resolve("manager")
+
+  const userId = req.user && req.user.customer_id
+
+  await manager.transaction(async (manager) => {
+    await orderEditService
+      .withTransaction(manager)
+      .decline(id, validatedBody.declined_reason, userId)
+  })
+  let orderEdit = await orderEditService.retrieve(id)
+
+  orderEdit = await orderEditService.decorateLineItemsAndTotals(orderEdit)
+
+  res.status(200).json({ order_edit: orderEdit })
+}
+
+export class StorePostOrderEditsOrderEditCancel {
+  @IsOptional()
+  @IsString()
+  declined_reason?: string
+}
