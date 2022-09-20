3b34b48636d76fe4782b47f8fa198ce68db64db2
fPolic <frane@medusajs.com>
2022-09-20 21:21:06 +0200

    wip: adjustments and tax lines

--- packages/medusa/src/services/order-edit.ts
+++ packages/medusa/src/services/order-edit.ts
@@ -10,7 +10,6 @@ import {
   OrderEdit,
   OrderEditItemChangeType,
   OrderEditStatus,
-  OrderItemChange,
 } from "../models"
 import { TransactionBaseService } from "../interfaces"
 import {
@@ -25,15 +24,20 @@ import { AddOrderEditLineItemInput, CreateOrderEditInput, UpdateOrderEditInput }
 import region from "./region"
 import LineItemAdjustmentService from "./line-item-adjustment"
 import { OrderItemChangeRepository } from "../repositories/order-item-change"
+import LineItemAdjustmentService from "./line-item-adjustment"
+import TaxProviderService from "./tax-provider"
 
 type InjectedDependencies = {
   manager: EntityManager
   orderEditRepository: typeof OrderEditRepository
   orderItemChangeRepository: typeof OrderItemChangeRepository
+
   orderService: OrderService
-  eventBusService: EventBusService
   totalsService: TotalsService
   lineItemService: LineItemService
+  eventBusService: EventBusService
+  taxProviderService: TaxProviderService
+  lineItemAdjustmentService: LineItemAdjustmentService
   orderEditItemChangeService: OrderEditItemChangeService
   lineItemAdjustmentService: LineItemAdjustmentService
   taxProviderService: TaxProviderService
@@ -55,8 +59,11 @@ export default class OrderEditService extends TransactionBaseService {
   protected readonly orderItemChangeRepository_: typeof OrderItemChangeRepository
 
   protected readonly orderService_: OrderService
+  protected readonly totalsService_: TotalsService
   protected readonly lineItemService_: LineItemService
   protected readonly eventBusService_: EventBusService
+  protected readonly taxProviderService_: TaxProviderService
+  protected readonly lineItemAdjustmentService_: LineItemAdjustmentService
   protected readonly totalsService_: TotalsService
   protected readonly orderEditItemChangeService_: OrderEditItemChangeService
   protected readonly lineItemAdjustmentService_: LineItemAdjustmentService
@@ -480,7 +487,7 @@ export default class OrderEditService extends TransactionBaseService {
       )
 
       const orderEdit = await this.retrieve(orderEditId, {
-        relations: ["order", "changes"],
+        relations: ["order", "order.cart", "changes"],
       })
 
       const regionId = orderEdit.order.region_id
@@ -498,7 +505,25 @@ export default class OrderEditService extends TransactionBaseService {
 
       const lineItem = await lineItemServiceTx.create(newItem)
 
-      // 2. generate change record (with new line item)
+      // TODO: check quantity with inventory service `confirmInventory` ?
+
+      // 2. generate line item adjustments
+
+      await this.lineItemAdjustmentService_
+        .withTransaction(manager)
+        .createAdjustments(orderEdit.order.cart, lineItem)
+
+      // 3. generate tax lines
+
+      const calcContext = await this.totalsService_.getCalculationContext(
+        orderEdit.order
+      )
+
+      await this.taxProviderService_
+        .withTransaction(manager)
+        .createTaxLines([lineItem], calcContext)
+
+      // 4. generate change record (with new line item)
 
       const change = orderEditItemChangeRepo.create({
         type: OrderEditItemChangeType.ITEM_ADD,
