e1c3535a79fb82f991277183f15b2215f35a57ec
Philip Korsholm <philip.korsholm@hotmail.com>
2022-09-21 17:58:28 +0700

    oas formatting

--- packages/medusa/src/api/routes/admin/order-edits/cancel-order-edit.ts
+++ packages/medusa/src/api/routes/admin/order-edits/cancel-order-edit.ts
@@ -17,9 +17,9 @@ import { EntityManager } from "typeorm"
  *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
  *       // must be previously logged in or use api token
  *       medusa.admin.orderEdit.cancel(orderEditId)
- *       .then(({ order_edit }) => {
- *         console.log(order_edit.id);
- *       });
+ *         .then(({ order_edit }) => {
+ *           console.log(order_edit.id);
+ *         })
  *   - lang: Shell
  *     label: cURL
  *     source: |
