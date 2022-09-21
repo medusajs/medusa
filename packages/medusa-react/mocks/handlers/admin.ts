c8abcd95f10d1a7d332e8f56ef0790102464a853
Philip Korsholm <philip.korsholm@hotmail.com>
2022-09-21 12:31:46 +0700

    add cancel order edit medusa-js and medusa-react

--- packages/medusa-react/mocks/handlers/admin.ts
+++ packages/medusa-react/mocks/handlers/admin.ts
@@ -1695,6 +1695,15 @@ export const adminHandlers = [
       })
     )
   }),
+  
+  rest.post("/admin/order-edits/:id/cancel", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        order_edit: { ...fixtures.get("order_edit"), canceled_at: new Date(), status: 'canceled' },
+      })
+    )
+  }),
 
   rest.post("/admin/order-edits/:id/request", (req, res, ctx) => {
     return res(
