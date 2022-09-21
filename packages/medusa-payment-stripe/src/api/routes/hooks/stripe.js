20905b1c3e0389985e6a2f474a927da90c89e618
adrien2p <adrien.deperetti@gmail.com>
2022-09-21 11:02:05 +0200

    revert stripe hook

--- packages/medusa-payment-stripe/src/api/routes/hooks/stripe.js
+++ packages/medusa-payment-stripe/src/api/routes/hooks/stripe.js
@@ -1,5 +1,3 @@
-import { IdempotencyKeyRepository } from "@medusajs/medusa/dist/repositories/idempotency-key"
-
 export default async (req, res) => {
   const signature = req.headers["stripe-signature"]
 
@@ -23,8 +21,6 @@ export default async (req, res) => {
     .retrieveByCartId(cartId)
     .catch(() => undefined)
 
-  let responseCode
-
   // handle payment intent events
   switch (event.type) {
     case "payment_intent.succeeded":
@@ -36,45 +32,12 @@ export default async (req, res) => {
       break
     case "payment_intent.amount_capturable_updated":
       if (!order) {
-        // Find and use the same idempotency that has been created and used for the cart that is used for that payment
-        let idempotencyKey
-        try {
-          idempotencyKey = await manager.transaction(
-            async (transactionManager) => {
-              const idempotencyKeyRepository =
-                transactionManager.getCustomRepository(IdempotencyKeyRepository)
-              return await idempotencyKeyRepository
-                .createQueryBuilder("idKey")
-                .where("idKey.request_params->>'id' = :cart_id", {
-                  cart_id: cartId,
-                })
-                .andWhere("idKey.request_method = :method", { method: "POST" })
-                .andWhere("idKey.request_path = :path", {
-                  path: `/${cartId}/complete`,
-                })
-                .getOneOrFail()
-            }
-          )
-        } catch (error) {
-          console.log(error)
-          res.status(409).send("Failed to retrieve or create idempotency key")
-          return
-        }
-
-        const { response_code } = await manager.transaction(async (manager) => {
-          const completionStrategy = req.scope.resolve("cartCompletionStrategy")
-
-          const { context } = await cartService
-            .withTransaction(manager)
-            .retrieve(cartId, { select: ["context"] })
-
-          return await completionStrategy.complete(
-            cartId,
-            idempotencyKey.idempotency_key,
-            context
-          )
+        await manager.transaction(async (manager) => {
+          const cartServiceTx = cartService.withTransaction(manager)
+          await cartServiceTx.setPaymentSession(cartId, "stripe")
+          await cartServiceTx.authorizePayment(cartId)
+          await orderService.withTransaction(manager).createFromCart(cartId)
         })
-        responseCode = response_code
       }
       break
     default:
@@ -82,5 +45,5 @@ export default async (req, res) => {
       return
   }
 
-  res.sendStatus(responseCode ?? 200)
+  res.sendStatus(200)
 }
