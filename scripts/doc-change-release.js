b8fba72e73e78d3359a2e0305e729aac191712b6
Shahed Nasser <shahednasser@gmail.com>
2022-09-30 11:51:24 +0300

    fix: fixed remove announcement bar action

--- scripts/doc-change-release.js
+++ scripts/doc-change-release.js
@@ -15,13 +15,19 @@ async function main () {
   if (shouldExpire) {
     //check if the file was last updated 6 days ago
     try {
-      const fileStat = fs.statSync(path.join(__dirname, '..', 'www', 'docs', 'announcement.json'));
-      if (dateDiffInDays(fileStat.mtime, new Date()) < 6) {
+      const commitResponse = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+        owner: 'medusajs',
+        repo: 'medusa',
+        path: path.join('www', 'docs', 'announcement.json'),
+        per_page: 1
+      })
+      
+      if (commitResponse.data.length && dateDiffInDays(commitResponse.data[0].commit.committer.date, new Date()) < 6) {
         console.log("File was edited less than 6 days ago. Expiry canceled.");
         return;
       }
     } catch (e) {
-      //file doesn't exist, continue
+      //continue as if file doesn't exist
     }
   } else {
     //retrieve the latest release
