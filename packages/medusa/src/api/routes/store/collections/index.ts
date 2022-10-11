21b9c4142d472a72381f486165709c5c450cfb5e
adrien2p <adrien.deperetti@gmail.com>
2022-10-11 10:32:51 +0200

    feat(medusa): Allow to filter collections by discount condition id

--- packages/medusa/src/api/routes/store/collections/index.ts
+++ packages/medusa/src/api/routes/store/collections/index.ts
@@ -1,14 +1,23 @@
 import { Router } from "express"
-import { PaginatedResponse } from "./../../../../types/common"
+import { PaginatedResponse } from "../../../../types/common"
 import { ProductCollection } from "../../../../"
-import middlewares from "../../../middlewares"
+import middlewares, { transformQuery } from "../../../middlewares"
+import { StoreGetCollectionsParams } from "./list-collections"
 
 const route = Router()
 
 export default (app) => {
   app.use("/collections", route)
 
-  route.get("/", middlewares.wrap(require("./list-collections").default))
+  route.get(
+    "/",
+    transformQuery(StoreGetCollectionsParams, {
+      defaultRelations: defaultStoreCollectionRelations,
+      defaultFields: defaultStoreCollectionFields,
+      isList: true,
+    }),
+    middlewares.wrap(require("./list-collections").default)
+  )
   route.get("/:id", middlewares.wrap(require("./get-collection").default))
 
   return app
--- packages/medusa/src/repositories/product-collection.ts
+++ packages/medusa/src/repositories/product-collection.ts
@@ -1,6 +1,36 @@
 import { EntityRepository, Repository } from "typeorm"
-import { ProductCollection } from "../models/product-collection"
+import { ProductCollection } from "../models"
+import { ExtendedFindConfig } from "../types/common"
 
 @EntityRepository(ProductCollection)
 // eslint-disable-next-line max-len
-export class ProductCollectionRepository extends Repository<ProductCollection> {}
+export class ProductCollectionRepository extends Repository<ProductCollection> {
+  async findAndCountByDiscountConditionId(
+    conditionId: string,
+    query: ExtendedFindConfig<ProductCollection, Partial<ProductCollection>>
+  ): Promise<[ProductCollection[], number]> {
+    const qb = this.createQueryBuilder("pc")
+
+    if (query?.select) {
+      qb.select(query.select.map((select) => `pc.${select}`))
+    }
+
+    if (query.skip) {
+      qb.skip(query.skip)
+    }
+
+    if (query.take) {
+      qb.take(query.take)
+    }
+
+    return await qb
+      .where(query.where)
+      .innerJoin(
+        "discount_condition_product_collection",
+        "dc_pc",
+        `dc_pc.product_collection_id = pc.id AND dc_pc.condition_id = :dcId`,
+        { dcId: conditionId }
+      )
+      .getManyAndCount()
+  }
+}
