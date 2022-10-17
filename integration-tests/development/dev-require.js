faacc5e82a1571b6fa80e5fbdf58294e7dc9197b
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-10-17 13:43:19 -0300

    fix: medusa interfaces

--- integration-tests/development/dev-require.js
+++ integration-tests/development/dev-require.js
@@ -7,17 +7,26 @@ const path = require("path")
 const Module = require("module")
 const originalRequire = Module.prototype.require
 const medusaCore = path.resolve(path.join(__dirname, "../../packages"))
-const currentPath = path.resolve(__dirname, "..")
 
 Module.prototype.require = function (...args) {
+  const interfaces = "medusa-interfaces"
   const base = "@medusajs/"
 
-  if (args[0].indexOf(base) > -1) {
+  if (args[0].includes(base)) {
     const idx = args[0].indexOf(base)
 
     const packPath = args[0].substring(idx + base.length)
     args[0] =
       medusaCore + "/" + packPath.replace("/dist/", "/src/").replace(".js", "")
+  } else if (args[0].includes(interfaces)) {
+    const idx = args[0].indexOf(interfaces)
+    const packPath = args[0].substring(idx + interfaces.length)
+    args[0] =
+      medusaCore +
+      "/" +
+      interfaces +
+      "/" +
+      packPath.replace("/dist/", "/src/").replace(".js", "")
   }
 
   if (args[0] === "glob") {
@@ -25,11 +34,9 @@ Module.prototype.require = function (...args) {
     const originalGlobSync = glob.sync
     glob.GlobSync = glob.sync = (pattern, options) => {
       if (pattern.endsWith(".js") || pattern.endsWith(".ts")) {
-        pattern = pattern
-          .replace(currentPath, medusaCore)
-          .replace(".js", ".{j,t}s")
-          .replace("/dist/", "/src/")
+        pattern = pattern.replace(".js", ".{j,t}s").replace("/dist/", "/src/")
       }
+
       return originalGlobSync.apply(this, [pattern, options])
     }
     return glob
