e6f53ddff57a2fde98ee49975ec14789d35acf20
Sebastian Rindom <skrindom@gmail.com>
2022-10-20 14:45:06 +0200

    fix: integration test of inventory

--- packages/medusa/src/commands/migrate.js
+++ packages/medusa/src/commands/migrate.js
@@ -1,29 +1,13 @@
+import { getConfigFile } from "medusa-core-utils"
 import { createConnection } from "typeorm"
+import configModuleLoader, { handleConfigError } from "../loaders/config"
 import featureFlagLoader from "../loaders/feature-flags"
-<<<<<<< HEAD
-import { handleConfigError } from "../loaders/config"
-=======
-import configModuleLoader from "../loaders/config"
->>>>>>> 5c51afb50 (wip)
 import Logger from "../loaders/logger"
 
 import getMigrations from "./utils/get-migrations"
 
-const t = async function ({ directory }) {
-  const args = process.argv
-  args.shift()
-  args.shift()
-  args.shift()
-
-<<<<<<< HEAD
-  const { configModule, error } = getConfigFile(directory, `medusa-config`)
-
-  if (error) {
-    handleConfigError(error)
-  }
-=======
+export const migrateUp = async (directory) => {
   const configModule = configModuleLoader(directory)
->>>>>>> 5c51afb50 (wip)
 
   const featureFlagRouter = featureFlagLoader(configModule)
 
@@ -37,31 +21,78 @@ const t = async function ({ directory }) {
     logging: true,
   })
 
+  let pluginMigrations = []
+  for (const moduleResolution of Object.values(
+    configModule.moduleResolutions
+  )) {
+    const loadedModule = await import(moduleResolution.resolutionPath)
+    const migrations = loadedModule.migrations || []
+    pluginMigrations = pluginMigrations.concat(migrations)
+  }
+
+  for (const migration of pluginMigrations) {
+    console.log("Running migration", migration)
+    await migration.up({ configModule })
+  }
+
+  await connection.runMigrations()
+  await connection.close()
+  Logger.info("Migrations completed.")
+}
+
+export const migrateDown = async (directory) => {
+  const configModule = configModuleLoader(directory)
+
+  const featureFlagRouter = featureFlagLoader(configModule)
+
+  const { coreMigrations } = await getMigrations(directory, featureFlagRouter)
+
+  const connection = await createConnection({
+    type: configModule.projectConfig.database_type,
+    url: configModule.projectConfig.database_url,
+    extra: configModule.projectConfig.database_extra || {},
+    migrations: coreMigrations,
+    logging: true,
+  })
+
+  await connection.undoLastMigration({ transaction: "all" })
+  await connection.close()
+  Logger.info("Migrations reverted.")
+}
+
+const t = async function ({ directory }) {
+  const args = process.argv
+  args.shift()
+  args.shift()
+  args.shift()
+
+  const { configModule, error } = getConfigFile(directory, `medusa-config`)
+
+  if (error) {
+    handleConfigError(error)
+  }
+
   if (args[0] === "run") {
-    let pluginMigrations = []
-    for (const moduleResolution of Object.values(
-      configModule.moduleResolutions
-    )) {
-      const loadedModule = await import(moduleResolution.resolutionPath)
-      const migrations = loadedModule.migrations || []
-      pluginMigrations = pluginMigrations.concat(migrations)
-    }
-
-    for (const migration of pluginMigrations) {
-      console.log("Running migration", migration)
-      await migration.up({ configModule })
-    }
-
-    await connection.runMigrations()
-    await connection.close()
-    Logger.info("Migrations completed.")
+    await migrateUp(directory)
     process.exit()
   } else if (args[0] === "revert") {
-    await connection.undoLastMigration({ transaction: "all" })
-    await connection.close()
-    Logger.info("Migrations reverted.")
+    await migrateDown(directory)
     process.exit()
   } else if (args[0] === "show") {
+    const configModule = configModuleLoader(directory)
+
+    const featureFlagRouter = featureFlagLoader(configModule)
+
+    const { coreMigrations } = await getMigrations(directory, featureFlagRouter)
+
+    const connection = await createConnection({
+      type: configModule.projectConfig.database_type,
+      url: configModule.projectConfig.database_url,
+      extra: configModule.projectConfig.database_extra || {},
+      migrations: coreMigrations,
+      logging: true,
+    })
+
     const unapplied = await connection.showMigrations()
     await connection.close()
     process.exit(unapplied ? 1 : 0)
