b3fa6247bb36fb98b4e1ca7d9841e79761e0e2ce
olivermrbl <oliver@mrbltech.com>
2022-10-30 16:37:07 +0100

    Add name as required prop

--- packages/medusa/src/loaders/custom-api.ts
+++ packages/medusa/src/loaders/custom-api.ts
@@ -14,18 +14,12 @@ type Handler = (
 ) => Promise<void>
 
 export type MedusaRoute = {
+  name: string
   method: AllowedMethods
   path: string
   handler: Handler | Handler[]
   routes: MedusaRoute[]
   options: {}
-}
-
-const validateRouteConfig = async (customRoute) => {
-  const { handler, path, method } = customRoute
-  if (!method || !path || !handler) {
-    console.warn("Invalid route config")
-  }
 }
 
 export default ({
@@ -36,9 +30,11 @@ export default ({
   configModule: ConfigModule
 }) => {
   configModule.routes.map(async (customRoute) => {
-    await validateRouteConfig(customRoute)
+    let { handler, path, method, routes, name } = customRoute
 
-    let { handler, path, method, routes } = customRoute
+    if (!method || !path || !handler || !name) {
+      console.warn(`Invalid config for custom route: ${name}`)
+    }
 
     if (routes) {
       const subRouter = Router({ mergeParams: true })
