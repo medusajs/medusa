23bc7c162cba6f0b1cf8221da5ac06a8f46d373e
Shahed Nasser <shahednasser@gmail.com>
2022-11-02 15:41:10 +0200

    docs: redesigned survey and content footer (#2522)
    * fixes to inline code
    
    * redesigned doc footer

--- www/docs/src/theme/CopyButton/index.js
+++ www/docs/src/theme/CopyButton/index.js
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import copy from 'copy-text-to-clipboard';
 import Tooltip from '../Tooltip';
 
-export default function CopyButton ({ children, buttonClassName, text }) {
+export default function CopyButton ({ children, buttonClassName, text, tooltipClassName }) {
   const [isCopied, setIsCopied] = useState(false);
   const copyTimeout = useRef(undefined);
 
@@ -17,7 +17,7 @@ export default function CopyButton ({ children, buttonClassName, text }) {
   useEffect(() => () => window.clearTimeout(copyTimeout.current), []);
 
   return (
-    <Tooltip text={isCopied ? `Copied!` : `Copy to Clipboard`}>
+    <Tooltip text={isCopied ? `Copied!` : `Copy to Clipboard`} tooltipClassName={tooltipClassName}>
       <button className={`copy-action ${buttonClassName}`} onClick={handleCopy}>
         {children}
       </button>
