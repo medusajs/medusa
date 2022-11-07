a5b62379e3ad591796d296718f0fd78aabc3713d
fPolic <frane@medusajs.com>
2022-11-07 11:00:07 +0100

    feat: remove user relation, regenerate migrations, change id identifier

--- packages/medusa/src/migrations/1667815005070-publishable_api_key.ts
+++ packages/medusa/src/migrations/1667815005070-publishable_api_key.ts
@@ -4,8 +4,8 @@ import PublishableAPIKeysFeatureFlag from "../loaders/feature-flags/publishable-
 
 export const featureFlag = PublishableAPIKeysFeatureFlag.key
 
-export class publishableApiKey1667552298472 implements MigrationInterface {
-  name = "publishableApiKey1667552298472"
+export class publishableApiKey1667815005070 implements MigrationInterface {
+  name = "publishableApiKey1667815005070"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
@@ -14,23 +14,9 @@ export class publishableApiKey1667552298472 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "publishable_api_key" ("id" character varying NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, "created_by" character varying, "revoked_by" character varying, "revoked_at" TIMESTAMP WITH TIME ZONE, CONSTRAINT "PK_9e613278673a87de92c606b4494" PRIMARY KEY ("id"))`
     )
-
-    await queryRunner.query(
-      `ALTER TABLE "publishable_api_key" ADD CONSTRAINT "FK_ff1b82944b1d321d88c6f17252d" FOREIGN KEY ("created_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
-    )
-    await queryRunner.query(
-      `ALTER TABLE "publishable_api_key" ADD CONSTRAINT "FK_264380a0a99740fd4a0c6642d14" FOREIGN KEY ("revoked_by") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
-    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "publishable_api_key" DROP CONSTRAINT "FK_264380a0a99740fd4a0c6642d14"`
-    )
-    await queryRunner.query(
-      `ALTER TABLE "publishable_api_key" DROP CONSTRAINT "FK_ff1b82944b1d321d88c6f17252d"`
-    )
-
     await queryRunner.query(`DROP TABLE "publishable_api_key"`)
     await queryRunner.query(`DROP TABLE "publishable_api_key_sales_channel"`)
   }
--- packages/medusa/src/models/publishable-api-key.ts
+++ packages/medusa/src/models/publishable-api-key.ts
@@ -1,34 +1,25 @@
-import { BeforeInsert, Column, JoinColumn, ManyToOne } from "typeorm"
+import { BeforeInsert, Column } from "typeorm"
 
 import { SoftDeletableEntity } from "../interfaces"
 import { resolveDbType } from "../utils/db-aware-column"
 import { generateEntityId } from "../utils"
-import { User } from "./user"
 import { FeatureFlagEntity } from "../utils/feature-flag-decorators"
 import PublishableAPIKeysFeatureFlag from "../loaders/feature-flags/publishable-api-keys"
 
 @FeatureFlagEntity(PublishableAPIKeysFeatureFlag.key)
 export class PublishableApiKey extends SoftDeletableEntity {
-  @Column({ nullable: true })
+  @Column({ type: "varchar", nullable: true })
   created_by: string | null
 
-  @ManyToOne(() => User)
-  @JoinColumn({ name: "created_by" })
-  created_by_user: User
-
-  @Column({ nullable: true })
+  @Column({ type: "varchar", nullable: true })
   revoked_by: string | null
-
-  @ManyToOne(() => User)
-  @JoinColumn({ name: "revoked_by" })
-  revoked_by_user?: User
 
   @Column({ type: resolveDbType("timestamptz"), nullable: true })
   revoked_at?: Date
 
   @BeforeInsert()
   private beforeInsert(): void {
-    this.id = generateEntityId(this.id, "pak")
+    this.id = generateEntityId(this.id, "pubkey")
   }
 }
 
