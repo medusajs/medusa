7370735b276bd66ac7346eed3c3247581d09c507
olivermrbl <oliver@mrbltech.com>
2022-11-11 15:17:03 +0100

    wuo

--- packages/event-bus-redis/src/loaders/index.ts
+++ packages/event-bus-redis/src/loaders/index.ts
@@ -3,32 +3,28 @@ import { asValue } from "awilix"
 import RealRedis from "ioredis"
 
 type LoaderOptions = {
-    container: MedusaContainer
-    configModule: ConfigModule
-    logger: Logger
-  }
-
+  container: MedusaContainer
+  configModule: ConfigModule
+  logger: Logger
+}
 
 export default async ({
   container,
   configModule,
-  logger
+  logger,
 }: LoaderOptions): Promise<void> => {
-    if (configModule.projectConfig.redis_url) {
-        // Economical way of dealing with redis clients
-        const client = new RealRedis(configModule.projectConfig.redis_url)
-        const subscriber = new RealRedis(configModule.projectConfig.redis_url)
-    
-        container.register({
-          redisClient: asValue(client),
-          redisSubscriber: asValue(subscriber),
-        })
-      } else {
-        if (process.env.NODE_ENV === "production") {
-          logger.warn(
-            `No Redis url was provided - using Medusa in production without a proper Redis instance is not recommended`
-          )
-        }
-      }
-  
+  if (configModule.projectConfig.redis_url) {
+    // Economical way of dealing with redis clients
+    const client = new RealRedis(configModule.projectConfig.redis_url)
+    const subscriber = new RealRedis(configModule.projectConfig.redis_url)
+
+    container.register({
+      redisClient: asValue(client),
+      redisSubscriber: asValue(subscriber),
+    })
+  } else {
+    logger.warn(
+      "No `redis_url` provided in project config. Event bus will not be available."
+    )
+  }
 }
