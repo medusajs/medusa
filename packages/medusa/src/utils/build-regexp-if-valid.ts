2757d99b9926d3b09a23d21e2c985877e2333cd4
adrien2p <adrien.deperetti@gmail.com>
2022-11-16 22:49:56 +0100

    cleanup

--- packages/medusa/src/utils/build-regexp-if-valid.ts
+++ packages/medusa/src/utils/build-regexp-if-valid.ts
@@ -1,6 +1,6 @@
 export function buildRegexpIfValid(str: string): RegExp | undefined {
   try {
-    const m = str.match(/^([/~@;%#'])(.*?)\1([gimsuy]*)$/)
+    const m = str.match(/^([\/~@;%#'])(.*?)\1([gimsuy]*)$/)
     if (m) {
       const regexp = new RegExp(m[2], m[3])
       return regexp
