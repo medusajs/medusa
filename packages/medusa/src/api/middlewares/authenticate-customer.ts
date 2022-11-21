58e1221673d5dd4f8a98c91004e089e46959ac80
Philip Korsholm <philip.korsholm@hotmail.com>
2022-11-21 20:40:46 +0100

    remove unused import

--- packages/medusa/src/api/middlewares/authenticate-customer.ts
+++ packages/medusa/src/api/middlewares/authenticate-customer.ts
@@ -1,6 +1,5 @@
 import passport from "passport"
 import { Request, Response, NextFunction, RequestHandler } from "express"
-import logger from "../../loaders/logger"
 
 export default (): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction): void => {
