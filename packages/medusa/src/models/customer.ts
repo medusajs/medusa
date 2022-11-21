d7a4160139f2cc45efe9ca69d0855aa7c7f5fda9
Philip Korsholm <philip.korsholm@hotmail.com>
2022-11-21 15:42:48 +0100

    typo fix

--- packages/medusa/src/models/customer.ts
+++ packages/medusa/src/models/customer.ts
@@ -19,7 +19,7 @@ import { generateEntityId } from "../utils/generate-entity-id"
 
 @Entity()
 export class Customer extends SoftDeletableEntity {
-  @Index({ unique: true, where: "has_acccount = true" })
+  @Index({ unique: true, where: "has_account = true" })
   @Column()
   email: string
 
