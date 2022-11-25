b07eb9e9dbe36c5f713572a4fcf2de0ecfb5bb7d
adrien2p <adrien.deperetti@gmail.com>
2022-11-25 20:22:05 +0100

    Interface augmentation

--- packages/medusa-core-utils/src/validator.ts
+++ packages/medusa-core-utils/src/validator.ts
@@ -115,4 +115,14 @@ Object.assign(Joi, {
   },
 })
 
+declare module "Joi" {
+  interface Root {
+    objectId: Joi.StringSchema
+    address: <T>() => Joi.AlternativesSchema
+    dateFilter: <T>() => Joi.ObjectSchema<T>
+    orderFilter: <T>() => Joi.ObjectSchema<T>
+    productFilter: <T>() => Joi.ObjectSchema<T>
+  }
+}
+
 export default Joi
