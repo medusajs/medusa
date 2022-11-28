b79d89bd5e2991a12bc83a71b6ec4bb3b0439aef
Patrick <patrick@medusajs.com>
2022-11-28 17:02:59 -0500

    feat: build client from OAS

---
+++ packages/medusa/src/scripts/oas/build-client.ts
@@ -0,0 +1,79 @@
+import path from "path"
+import { generate, HttpClient, Indent } from "openapi-typescript-codegen"
+import { upperFirst } from "lodash"
+import fs from "fs"
+
+type ApiType = "store" | "admin"
+
+const run = async () => {
+  console.debug("Generate Client from OAS.")
+
+  const targetPackageSrcDir = path.resolve(
+    __dirname,
+    "../../../../",
+    "medusa-client/src"
+  )
+  fs.mkdirSync(targetPackageSrcDir, { recursive: true })
+
+  for (const apiType of ["store", "admin"]) {
+    console.debug(`Building Client for ${apiType} api.`)
+    const oas = await getOASFromFile(apiType as ApiType)
+    await generateClientSDK(oas, apiType as ApiType, targetPackageSrcDir)
+  }
+
+  console.log("Client successfully generated.")
+}
+
+const getOASFromFile = (apiType: ApiType) => {
+  const jsonFile = path.resolve(
+    __dirname,
+    "../../../",
+    `dist/oas/${apiType}.oas.json`
+  )
+  const jsonString = fs.readFileSync(jsonFile, "utf8")
+  return JSON.parse(jsonString)
+}
+
+const generateClientSDK = async (
+  oas,
+  apiType: ApiType,
+  targetPackageSrcDir: string
+) => {
+  const input = oas
+  const output = path.resolve(targetPackageSrcDir, apiType)
+  const httpClient = HttpClient.AXIOS
+  const useOptions = true
+  const useUnionTypes = true
+  const exportCore = true
+  const exportServices = true
+  const exportModels = true
+  const exportHooks = true
+  const exportSchemas = true
+  const indent = Indent.SPACE_2
+  const postfixServices = "Service"
+  const postfixModels = ""
+  const clientName = `Medusa${upperFirst(apiType)}`
+  const request = undefined
+
+  await generate({
+    input,
+    output,
+    httpClient,
+    useOptions,
+    useUnionTypes,
+    exportCore,
+    exportServices,
+    exportModels,
+    exportHooks,
+    exportSchemas,
+    indent,
+    postfixServices,
+    postfixModels,
+    clientName,
+    request,
+  })
+}
+
+void (async () => {
+  await run()
+})()
