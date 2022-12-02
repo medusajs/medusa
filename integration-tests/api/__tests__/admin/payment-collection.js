b44192199767d21df1db8b650b5a657d4c736242
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2022-12-02 16:49:21 -0300

    fix: admin payment collection tests

--- integration-tests/api/__tests__/admin/payment-collection.js
+++ integration-tests/api/__tests__/admin/payment-collection.js
@@ -28,7 +28,7 @@ describe("[MEDUSA_FF_ORDER_EDITING] /admin/payment-collections", () => {
     const [process, connection] = await startServerWithEnvironment({
       cwd,
       env: { MEDUSA_FF_ORDER_EDITING: true },
-      verbose: true,
+      verbose: false,
     })
     dbConnection = connection
     medusaProcess = process
@@ -145,7 +145,7 @@ describe("[MEDUSA_FF_ORDER_EDITING] /admin/payment-collections", () => {
       const api = useApi()
 
       const response = await api.post(
-        `/admin/payment-collections/${payCol.id}/batch/sessions/authorize`,
+        `/admin/payment-collections/${payCol.id}/authorize`,
         undefined,
         adminHeaders
       )
@@ -201,7 +201,7 @@ describe("[MEDUSA_FF_ORDER_EDITING] /admin/payment-collections", () => {
       const api = useApi()
 
       await api.post(
-        `/admin/payment-collections/${payCol.id}/batch/sessions/authorize`,
+        `/admin/payment-collections/${payCol.id}/authorize`,
         undefined,
         adminHeaders
       )
