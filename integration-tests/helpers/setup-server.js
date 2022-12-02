d0f1fc2e733cd2c0b1b5e5e06b3006d3f476b154
adrien2p <adrien.deperetti@gmail.com>
2022-12-02 16:04:33 +0100

    feat: Allow to set the cache ttl through env

--- integration-tests/helpers/setup-server.js
+++ integration-tests/helpers/setup-server.js
@@ -21,6 +21,7 @@ module.exports = ({ cwd, redisUrl, uploadDir, verbose, env }) => {
         COOKIE_SECRET: "test",
         REDIS_URL: redisUrl ? redisUrlWithDatabase : undefined, // If provided, will use a real instance, otherwise a fake instance
         UPLOAD_DIR: uploadDir, // If provided, will be used for the fake local file service
+        CACHE_TTL: 0,
         ...env,
       },
       stdio: verbose
