3a6d4a74536396588e10be260013ee178b2ef6d6
Patrick <patrick@medusajs.com>
2022-12-16 17:10:58 -0500

    fix: pr feedback
    - typed OAS object
    - remove unnecessary assignment

--- packages/medusa/src/scripts/oas/build-client.ts
+++ packages/medusa/src/scripts/oas/build-client.ts
@@ -5,6 +5,7 @@ import { mkdir, readFile } from "fs/promises"
 import logger from "../../loaders/logger"
 
 type ApiType = "store" | "admin"
+type OAS = Record<string, unknown>
 
 const cliParams = process.argv.slice(2)
 const isVerbose = cliParams.includes("--verbose") || cliParams.includes("-V")
@@ -32,49 +33,33 @@ const run = async () => {
   debug("Client successfully generated.")
 }
 
-const getOASFromFile = async (apiType: ApiType) => {
+const getOASFromFile = async (apiType: ApiType): Promise<OAS> => {
   const jsonFile = path.resolve(packagePath, `oas/${apiType}.oas.json`)
   const jsonString = await readFile(jsonFile, "utf8")
   return JSON.parse(jsonString)
 }
 
 const generateClientSDK = async (
-  oas,
+  oas: OAS,
   apiType: ApiType,
   targetPackageSrcDir: string
 ) => {
-  const input = oas
-  const output = path.resolve(targetPackageSrcDir, apiType)
-  const httpClient = HttpClient.AXIOS
-  const useOptions = true
-  const useUnionTypes = true
-  const exportCore = true
-  const exportServices = true
-  const exportModels = true
-  const exportHooks = true
-  const exportSchemas = true
-  const indent = Indent.SPACE_2
-  const postfixServices = "Service"
-  const postfixModels = ""
-  const clientName = `Medusa${upperFirst(apiType)}`
-  const request = undefined
-
   await generate({
-    input,
-    output,
-    httpClient,
-    useOptions,
-    useUnionTypes,
-    exportCore,
-    exportServices,
-    exportModels,
-    exportHooks,
-    exportSchemas,
-    indent,
-    postfixServices,
-    postfixModels,
-    clientName,
-    request,
+    input: oas,
+    output: path.resolve(targetPackageSrcDir, apiType),
+    httpClient: HttpClient.AXIOS,
+    useOptions: true,
+    useUnionTypes: true,
+    exportCore: true,
+    exportServices: true,
+    exportModels: true,
+    exportHooks: true,
+    exportSchemas: true,
+    indent: Indent.SPACE_2,
+    postfixServices: "Service",
+    postfixModels: "",
+    clientName: `Medusa${upperFirst(apiType)}`,
+    request: undefined,
   })
 }
 
