c16522d6ce806aa6289d626b868818409f41c66e
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-12-16 12:16:34 +0100

    feat(medusa): Add has_account to list-customers query params (#2811)

--- packages/medusa/src/api/routes/admin/orders/create-fulfillment.ts
+++ packages/medusa/src/api/routes/admin/orders/create-fulfillment.ts
@@ -14,6 +14,7 @@ import { defaultAdminOrdersFields, defaultAdminOrdersRelations } from "."
 import { EntityManager } from "typeorm"
 import { OrderService } from "../../../../services"
 import { validator } from "../../../../utils/validator"
+import { optionalBooleanMapper } from "../../../../utils/validators/is-boolean"
 
 /**
  * @oas [post] /orders/{id}/fulfillment
@@ -147,7 +148,7 @@ export class AdminPostOrdersOrderFulfillmentsReq {
 
   @IsBoolean()
   @IsOptional()
-  @Transform(({ value }) => value === "true")
+  @Transform(({ value }) => optionalBooleanMapper.get(value))
   no_notification?: boolean
 
   @IsObject()
