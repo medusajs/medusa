49020c6499319dcbb8732f4a10d581bd122458cf
Philip Korsholm <philip.korsholm@hotmail.com>
2022-12-20 19:06:30 +0100

    add module definitions for inventory service and stock location service

--- packages/medusa/src/loaders/module-definitions/definitions.ts
+++ packages/medusa/src/loaders/module-definitions/definitions.ts
@@ -1,5 +1,22 @@
 import { ModuleDefinition } from "../../types/global"
 
-export const MODULE_DEFINITIONS: ModuleDefinition[] = []
+export const MODULE_DEFINITIONS: ModuleDefinition[] = [
+  {
+    key: "stockLocationService",
+    registrationName: "stockLocationService",
+    defaultPackage: "@medusajs/stock-locations",
+    label: "StockLocationService",
+    isRequired: false,
+    canOverride: true,
+  },
+  {
+    key: "inventoryService",
+    registrationName: "inventoryService",
+    defaultPackage: "@medusajs/inventory",
+    label: "InventoryService",
+    isRequired: false,
+    canOverride: true,
+  },
+]
 
 export default MODULE_DEFINITIONS
