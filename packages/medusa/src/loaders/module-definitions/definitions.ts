b45f5fd30a7e8f72c30358bd73a3dbc4d973829c
Philip Korsholm <philip.korsholm@hotmail.com>
2022-12-21 16:45:26 +0100

    remove commented out definitions

--- packages/medusa/src/loaders/module-definitions/definitions.ts
+++ packages/medusa/src/loaders/module-definitions/definitions.ts
@@ -1,22 +1,5 @@
 import { ModuleDefinition } from "../../types/global"
 
-export const MODULE_DEFINITIONS: ModuleDefinition[] = [
-  // {
-  //   key: "stockLocationService",
-  //   registrationName: "stockLocationService",
-  //   defaultPackage: "@medusajs/stock-locations",
-  //   label: "StockLocationService",
-  //   isRequired: false,
-  //   canOverride: true,
-  // },
-  // {
-  //   key: "inventoryService",
-  //   registrationName: "inventoryService",
-  //   defaultPackage: "@medusajs/inventory",
-  //   label: "InventoryService",
-  //   isRequired: false,
-  //   canOverride: true,
-  // },
-]
+export const MODULE_DEFINITIONS: ModuleDefinition[] = []
 
 export default MODULE_DEFINITIONS
