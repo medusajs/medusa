57840eec970ff0811d63bf6d8f991587fd14dd48
Patrick <patrick@medusajs.com>
2022-12-21 08:44:30 -0500

    fix(oas): update refreshPaymentSession codeSample

--- packages/medusa/src/api/routes/store/payment-collections/refresh-payment-session.ts
+++ packages/medusa/src/api/routes/store/payment-collections/refresh-payment-session.ts
@@ -16,7 +16,7 @@ import { PaymentCollectionService } from "../../../../services"
  *     source: |
  *       import Medusa from "@medusajs/medusa-js"
  *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
- *       medusa.paymentCollections.refreshPaymentSession(payment_collection_id, session_id, payload)
+ *       medusa.paymentCollections.refreshPaymentSession(payment_collection_id, session_id)
  *       .then(({ payment_session }) => {
  *         console.log(payment_session.id);
  *       });
