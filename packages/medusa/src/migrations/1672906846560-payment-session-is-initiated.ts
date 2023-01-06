e2c53daa0223db7fa7837661a8ad58faac18c1ce
adrien2p <adrien.deperetti@gmail.com>
2023-01-06 15:01:35 +0100

    fix migration name

--- packages/medusa/src/migrations/1672906846560-payment-session-is-initiated.ts
+++ packages/medusa/src/migrations/1672906846560-payment-session-is-initiated.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class PaymentSessionIsInitiated1672906846560 implements MigrationInterface {
-  name = "1672906846560PaymentSessionIsInitiated"
+  name = "paymentSessionIsInitiated1672906846560"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
