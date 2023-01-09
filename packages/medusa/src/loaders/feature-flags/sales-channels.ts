8b4792ff9cb2dc37e450ae75c51d69763f99f21a
olivermrbl <oliver@mrbltech.com>
2023-01-09 14:26:17 +0100

    chore(feature-flags): Sales Channels enabled by default

--- packages/medusa/src/loaders/feature-flags/sales-channels.ts
+++ packages/medusa/src/loaders/feature-flags/sales-channels.ts
@@ -2,7 +2,7 @@ import { FlagSettings } from "../../types/feature-flags"
 
 const SalesChannelFeatureFlag: FlagSettings = {
   key: "sales_channels",
-  default_val: false,
+  default_val: true,
   env_key: "MEDUSA_FF_SALES_CHANNELS",
   description: "[WIP] Enable the sales channels feature",
 }
