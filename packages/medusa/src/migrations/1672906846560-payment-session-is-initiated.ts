aa53d49d217a916e688b08f71f24902ef435205b
adrien2p <adrien.deperetti@gmail.com>
2023-01-09 14:57:03 +0100

    update migration

--- packages/medusa/src/migrations/1672906846560-payment-session-is-initiated.ts
+++ packages/medusa/src/migrations/1672906846560-payment-session-is-initiated.ts
@@ -7,6 +7,15 @@ export class PaymentSessionIsInitiated1672906846560 implements MigrationInterfac
     await queryRunner.query(`
       ALTER TABLE payment_session ADD COLUMN is_initiated BOOLEAN NOT NULL DEFAULT false
     `)
+
+    // Set is_initiated to true if there is more that 0 key in the data. We assume that if data contains any key
+    // A payment has been initiated to the payment provider
+    await queryRunner.query(`
+      UPDATE payment_session SET is_initiated = true WHERE (
+          SELECT coalesce(json_array_length(json_agg(keys)), 0)
+          FROM jsonb_object_keys(data) AS keys (keys)
+      ) > 0
+    `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
