17c3d2aaf18f47a4145ebbce26c582b33764f1da
Philip Korsholm <philip.korsholm@hotmail.com>
2023-01-10 14:28:25 +0100

    add endpoint to medusa-js

--- packages/medusa-js/src/resources/admin/variants.ts
+++ packages/medusa-js/src/resources/admin/variants.ts
@@ -1,4 +1,8 @@
-import { AdminGetVariantsParams, AdminVariantsListRes } from "@medusajs/medusa"
+import {
+  AdminGetVariantsVariantInventoryRes,
+  AdminGetVariantsParams,
+  AdminVariantsListRes,
+} from "@medusajs/medusa"
 import qs from "qs"
 import { ResponsePromise } from "../.."
 import BaseResource from "../base"
@@ -14,6 +18,21 @@ class AdminVariantsResource extends BaseResource {
       const queryString = qs.stringify(query)
       path = `/admin/variants?${queryString}`
     }
+
+    return this.client.request("GET", path, undefined, {}, customHeaders)
+  }
+
+  /**
+   *
+   * @param variantId id of the variant to fetch inventory for
+   * @param customHeaders custom headers
+   * @returns
+   */
+  getInventory(
+    variantId: string,
+    customHeaders: Record<string, any> = {}
+  ): ResponsePromise<AdminGetVariantsVariantInventoryRes> {
+    const path = `/admin/variants/${variantId}/inventory`
 
     return this.client.request("GET", path, undefined, {}, customHeaders)
   }
