f3ced106ad24fe21f099e10bee5666e1f65a9fc7
Riqwan Thamir <rmthamir@gmail.com>
2023-01-10 12:52:31 +0100

    feat(medusa): Nested Categories Admin List Endpoint (#2973)
    * chore: added get route for admin categories API
    
    * chore: add tree method to mock repository
    
    * chore: added changeset to the PR
    
    * chore: rename id to productCategoryId in service
    
    * chore: switch cli option to string
    
    * chore: lint fixes, tests for parent category
    
    * chore: move Nested Categories behind feature flag
    
    * chore: use transformQuery hook in api
    
    * chore: add feature flag in migrations
    
    * chore: remove migration FF, fix FF name
    
    * chore: add free text search + count repo function
    
    * chore: added list endpoint for admin
    
    * chore: added changeset for feature
    
    * chore: address pr review comments
    
    * chore: change oas comment
    
    * chore: add nullable parent category filter + test

--- packages/medusa/src/repositories/product-category.ts
+++ packages/medusa/src/repositories/product-category.ts
@@ -1,5 +1,48 @@
-import { EntityRepository, TreeRepository } from "typeorm"
+import { EntityRepository, TreeRepository, Brackets, ILike } from "typeorm"
 import { ProductCategory } from "../models/product-category"
+import { ExtendedFindConfig, Selector } from "../types/common"
 
 @EntityRepository(ProductCategory)
-export class ProductCategoryRepository extends TreeRepository<ProductCategory> {}
+export class ProductCategoryRepository extends TreeRepository<ProductCategory> {
+  public async getFreeTextSearchResultsAndCount(
+    options: ExtendedFindConfig<ProductCategory, Selector<ProductCategory>> = {
+      where: {},
+    },
+    q: string | undefined
+  ): Promise<[ProductCategory[], number]> {
+    const options_ = { ...options }
+    const entityName = "product_category"
+
+    const queryBuilder = this.createQueryBuilder(entityName)
+      .select()
+      .skip(options_.skip)
+      .take(options_.take)
+
+    if (q) {
+      delete options_.where?.name
+      delete options_.where?.handle
+
+      queryBuilder.where(
+        new Brackets((bracket) => {
+          bracket
+            .where({ name: ILike(`%${q}%`) })
+            .orWhere({ handle: ILike(`%${q}%`) })
+        })
+      )
+    }
+
+    queryBuilder.andWhere(options_.where)
+
+    if (options_.withDeleted) {
+      queryBuilder.withDeleted()
+    }
+
+    if (options_.relations?.length) {
+      options_.relations.forEach((rel) => {
+        queryBuilder.leftJoinAndSelect(`${entityName}.${rel}`, rel)
+      })
+    }
+
+    return await queryBuilder.getManyAndCount()
+  }
+}
--- packages/medusa/src/services/__tests__/product-category.ts
+++ packages/medusa/src/services/__tests__/product-category.ts
@@ -2,14 +2,17 @@ import { IdMap, MockRepository, MockManager } from "medusa-test-utils"
 import ProductCategoryService from "../product-category"
 
 describe("ProductCategoryService", () => {
+  const validProdCategoryId = "skinny-jeans"
+  const invalidProdCategoryId = "not-found"
+
   describe("retrieve", () => {
     const productCategoryRepository = MockRepository({
       findOne: query => {
-        if (query.where.id === "not-found") {
+        if (query.where.id === invalidProdCategoryId) {
           return Promise.resolve(undefined)
         }
 
-        return Promise.resolve({ id: IdMap.getId("skinny-jeans") })
+        return Promise.resolve({ id: IdMap.getId(validProdCategoryId) })
       },
       findDescendantsTree: productCategory => {
         return Promise.resolve(productCategory)
@@ -25,25 +28,72 @@ describe("ProductCategoryService", () => {
 
     it("successfully retrieves a product category", async () => {
       const result = await productCategoryService.retrieve(
-        IdMap.getId("skinny-jeans")
+        IdMap.getId(validProdCategoryId)
       )
 
-      expect(result.id).toEqual(IdMap.getId("skinny-jeans"))
+      expect(result.id).toEqual(IdMap.getId(validProdCategoryId))
       expect(productCategoryRepository.findOne).toHaveBeenCalledTimes(1)
       expect(productCategoryRepository.findDescendantsTree).toHaveBeenCalledTimes(1)
       expect(productCategoryRepository.findOne).toHaveBeenCalledWith({
-        where: { id: IdMap.getId("skinny-jeans") },
+        where: { id: IdMap.getId(validProdCategoryId) },
       })
     })
 
     it("fails on not-found product category id", async () => {
       const categoryResponse = await productCategoryService
-        .retrieve("not-found")
+        .retrieve(invalidProdCategoryId)
         .catch((e) => e)
 
       expect(categoryResponse.message).toBe(
         `ProductCategory with id: not-found was not found`
       )
+    })
+  })
+
+  describe("listAndCount", () => {
+    const productCategoryRepository = {
+      ...MockRepository({}),
+      getFreeTextSearchResultsAndCount: jest.fn().mockImplementation((query, q) => {
+        if (q == "not-found") {
+          return Promise.resolve([[], 0])
+        }
+
+        return Promise.resolve([[{ id: IdMap.getId(validProdCategoryId) }], 1])
+      })
+    }
+
+    const productCategoryService = new ProductCategoryService({
+      manager: MockManager,
+      productCategoryRepository,
+    })
+
+    beforeEach(async () => { jest.clearAllMocks() })
+
+    it("successfully retrieves an array of product category", async () => {
+      const [result, count] = await productCategoryService
+        .listAndCount({ q: validProdCategoryId })
+
+      expect(count).toEqual(1)
+      expect(result.length).toEqual(1)
+      expect(result[0].id).toEqual(IdMap.getId(validProdCategoryId))
+      expect(productCategoryRepository.getFreeTextSearchResultsAndCount).toHaveBeenCalledTimes(1)
+      expect(productCategoryRepository.getFreeTextSearchResultsAndCount).toHaveBeenCalledWith({
+        order: {
+          created_at: "DESC",
+        },
+        skip: 0,
+        take: 100,
+        where: {},
+      }, validProdCategoryId)
+    })
+
+    it("returns empty array if query doesn't match database results", async () => {
+      const [result, count] = await productCategoryService
+        .listAndCount({ q: "not-found" })
+
+      expect(productCategoryRepository.getFreeTextSearchResultsAndCount).toHaveBeenCalledTimes(1)
+      expect(result).toEqual([])
+      expect(count).toEqual(0)
     })
   })
 })
