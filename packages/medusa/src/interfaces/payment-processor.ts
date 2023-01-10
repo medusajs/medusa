a7f60fcc6fde864a8e7e4472d860aa4badc719b9
adrien2p <adrien.deperetti@gmail.com>
2023-01-10 11:36:07 +0100

    chore: Cleanup payment processor api

--- packages/medusa/src/interfaces/payment-processor.ts
+++ packages/medusa/src/interfaces/payment-processor.ts
@@ -9,6 +9,7 @@ export type PaymentProcessorContext = {
   resource_id?: string
   customer?: Customer
   context: Record<string, unknown>
+  paymentSessionData: Record<string, unknown>
 }
 
 export type PaymentProcessorSessionResponse = {
@@ -46,53 +47,43 @@ export interface PaymentProcessor {
 
   /**
    * Update an existing payment session
-   * @param paymentId
    * @param context
    */
   updatePayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 
   /**
    * Refund an existing session
-   * @param paymentId
    * @param context
    */
   refundPayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 
   /**
    * Authorize an existing session if it is not already authorized
-   * @param paymentId
    * @param context
    */
   authorizePayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 
   /**
    * Capture an existing session
-   * @param paymentId
    * @param context
    */
   capturePayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 
   /**
    * Delete an existing session
-   * @param paymentId
    */
   deletePayment(paymentId: string): Promise<PaymentProcessorError | void>
 
   /**
    * Retrieve an existing session
-   * @param paymentId
    */
   retrievePayment(
     paymentId: string
@@ -102,40 +93,13 @@ export interface PaymentProcessor {
 
   /**
    * Cancel an existing session
-   * @param paymentId
    */
   cancelPayment(paymentId: string): Promise<PaymentProcessorError | void>
 
   /**
    * Return the status of the session
-   * @param paymentId
    */
   getPaymentStatus(paymentId: string): Promise<PaymentSessionStatus>
-
-  /**
-   * An optional method to implement in order to retrieve the methods saved for a customer in case the provider
-   * provide such feature. It is based on the collected data that have been given by the plugin
-   * and saved on the customer
-   *
-   * @example
-   * getSavedMethods(
-   *   customer: Customer
-   * ): Promise<Record<string, unknown>[]> {
-   *  if (customer.metadata?.stripe_id) {
-   *    const methods = await this.stripe_.paymentMethods.list({
-   *      customer: customer.metadata.stripe_id
-   *      type: "card",
-   *    })
-   *
-   *    return methods.data
-   *  }
-   *
-   *  return []
-   * }
-   *
-   * @param customer
-   */
-  getSavedMethods(customer: Customer): Promise<Record<string, unknown>[]>
 }
 
 /**
@@ -162,12 +126,10 @@ export abstract class AbstractPaymentProcessor implements PaymentProcessor {
   abstract init(): Promise<void>
 
   abstract capturePayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 
   abstract authorizePayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 
@@ -183,14 +145,9 @@ export abstract class AbstractPaymentProcessor implements PaymentProcessor {
     paymentId: string
   ): Promise<PaymentProcessorError | void>
 
-  abstract getSavedMethods(
-    customer: Customer
-  ): Promise<Record<string, unknown>[]>
-
   abstract getPaymentStatus(paymentId: string): Promise<PaymentSessionStatus>
 
   abstract refundPayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 
@@ -201,7 +158,6 @@ export abstract class AbstractPaymentProcessor implements PaymentProcessor {
   >
 
   abstract updatePayment(
-    paymentId: string,
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | void>
 }
