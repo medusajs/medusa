73e20a1ee9fd9f33968b48d8e2d09438dae92f59
Himanshu Walimbe <himanshuwalimbe@gmail.com>
2023-01-12 22:05:10 +0530

    attach customer addresses to the response object during authentication

--- packages/medusa/src/api/routes/store/auth/create-session.ts
+++ packages/medusa/src/api/routes/store/auth/create-session.ts
@@ -4,6 +4,7 @@ import { EntityManager } from "typeorm"
 import AuthService from "../../../../services/auth"
 import CustomerService from "../../../../services/customer"
 import { validator } from "../../../../utils/validator"
+import { defaultStoreCustomersRelations } from "../customers"
 
 /**
  * @oas [post] /auth
@@ -90,7 +91,7 @@ export default async (req, res) => {
 
   const customerService: CustomerService = req.scope.resolve("customerService")
   const customer = await customerService.retrieve(result.customer?.id || "", {
-    relations: ["orders", "orders.items"],
+    relations: ["orders", "orders.items", ...defaultStoreCustomersRelations],
   })
 
   res.json({ customer })
