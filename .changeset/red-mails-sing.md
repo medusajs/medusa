27a29ef24e5ea1ba2bc0be8ecb7dd747d4c7c65b
Patrick <116003638+patrick-medusajs@users.noreply.github.com>
2023-01-15 14:04:26 -0500

    chore(oas): replace response with $ref class JSDoc (Admin SHI-V) (#3031)
    ### Scope
    
    Admin routes directories SHI to V.
    
    ### What
    
    Move inline OAS response schema declaration under their respective class declarations in order to expose them through  `#/components/schemas`. Replace inline OAS response schema with a `$ref` reference pointing to the newly declared schema.
    
    ### Why
    
    Having response declared as its own "named" schema will allow OAS code generators to output typed entities/DTO that can be consumed without having to reference the route/operation.
    
    ### How
    
    Declare a new @schema JSDoc for each "Res" class used to parse and validate request body. Move the current inline requestBody to the new @schema.
    
    ### Test
    
    - Ran OAS validator.
    - Ran docs build script.
    
    Expect no visible changes to the documentation.
    
    Co-authored-by: Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>

---
+++ .changeset/red-mails-sing.md
@@ -0,0 +1,5 @@
+---
+"@medusajs/medusa": patch
+---
+
+chore(oas): replace response with $ref class JSDoc (Admin SHI-V)
--- packages/medusa/src/api/routes/admin/variants/list-variants.ts
+++ packages/medusa/src/api/routes/admin/variants/list-variants.ts
@@ -91,21 +91,7 @@ import { IsType } from "../../../../utils/validators/is-type"
  *     content:
  *       application/json:
  *         schema:
- *           type: object
- *           properties:
- *             variants:
- *               type: array
- *               items:
- *                 $ref: "#/components/schemas/ProductVariant"
- *             count:
- *               type: integer
- *               description: The total number of items available
- *             offset:
- *               type: integer
- *               description: The number of items skipped before these items
- *             limit:
- *               type: integer
- *               description: The number of items per page
+ *           $ref: "#/components/schemas/AdminVariantsListRes"
  *   "400":
  *     $ref: "#/components/responses/400_error"
  *   "401":
