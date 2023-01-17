1609f945ce2818bc6af0d68ad6dfa2f8b873b6d7
Kasper <kasper@medusajs.com>
2023-01-17 15:02:56 +0100

    add endpoint to fetch product tags from the store API

---
+++ packages/medusa/src/api/routes/store/product-tags/index.ts
@@ -0,0 +1,45 @@
+import { Router } from "express"
+import { ProductTag } from "../../../../models"
+import { PaginatedResponse } from "../../../../types/common"
+import middlewares, { transformQuery } from "../../../middlewares"
+import { StoreGetProductTagsParams } from "./list-product-tags"
+
+const route = Router()
+
+export default (app: Router) => {
+  app.use("/product-tags", route)
+
+  route.get(
+    "/",
+    transformQuery(StoreGetProductTagsParams, {
+      defaultFields: defaultStoreProductTagFields,
+      defaultRelations: defaultStoreProductTagRelations,
+      allowedFields: allowedStoreProductTagFields,
+      isList: true,
+    }),
+    middlewares.wrap(require("./list-product-tags").default)
+  )
+
+  return app
+}
+
+export const allowedStoreProductTagFields = [
+  "id",
+  "value",
+  "created_at",
+  "updated_at",
+]
+
+export const defaultStoreProductTagFields = [
+  "id",
+  "value",
+  "created_at",
+  "updated_at",
+]
+export const defaultStoreProductTagRelations = []
+
+export type StoreProductTagsListRes = PaginatedResponse & {
+  product_tags: ProductTag[]
+}
+
+export * from "./list-product-tags"
