8395879eacf10da72d0ea507ca262fbf529c0721
Patrick <patrick@medusajs.com>
2023-01-19 15:28:12 -0500

    fix: updated index strategy in generated clients

---
+++ packages/medusa-client-store/src/lib/services/index.ts
@@ -0,0 +1,21 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthService';
+export * from './CartService';
+export * from './CollectionService';
+export * from './CustomerService';
+export * from './GiftCardService';
+export * from './InviteService';
+export * from './OrderService';
+export * from './OrderEditService';
+export * from './PaymentCollectionService';
+export * from './ProductService';
+export * from './ProductCategoryService';
+export * from './ProductTypeService';
+export * from './ProductVariantService';
+export * from './RegionService';
+export * from './ReturnService';
+export * from './ReturnReasonService';
+export * from './ShippingOptionService';
+export * from './SwapService';
--- packages/medusa-react-store/src/lib/index.ts
+++ packages/medusa-react-store/src/lib/index.ts
@@ -9,23 +9,8 @@ export { BaseHttpRequest } from './core/BaseHttpRequest';
 export { CancelablePromise, CancelError } from './core/CancelablePromise';
 export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
+
 export * from './models';
 
-export { AuthService } from './services/AuthService';
-export { CartService } from './services/CartService';
-export { CollectionService } from './services/CollectionService';
-export { CustomerService } from './services/CustomerService';
-export { GiftCardService } from './services/GiftCardService';
-export { InviteService } from './services/InviteService';
-export { OrderService } from './services/OrderService';
-export { OrderEditService } from './services/OrderEditService';
-export { PaymentCollectionService } from './services/PaymentCollectionService';
-export { ProductService } from './services/ProductService';
-export { ProductCategoryService } from './services/ProductCategoryService';
-export { ProductTypeService } from './services/ProductTypeService';
-export { ProductVariantService } from './services/ProductVariantService';
-export { RegionService } from './services/RegionService';
-export { ReturnService } from './services/ReturnService';
-export { ReturnReasonService } from './services/ReturnReasonService';
-export { ShippingOptionService } from './services/ShippingOptionService';
-export { SwapService } from './services/SwapService';
+
+export * from './services';
