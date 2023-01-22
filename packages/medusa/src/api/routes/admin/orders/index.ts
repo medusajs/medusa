e4148773786e273b8dccd9e762a3c931706b7a3f
Riqwan Thamir <rmthamir@gmail.com>
2023-01-22 13:50:12 +0530

    chore: fix claims specs

--- packages/medusa/src/api/routes/admin/orders/index.ts
+++ packages/medusa/src/api/routes/admin/orders/index.ts
@@ -319,6 +319,7 @@ export const defaultAdminOrdersRelations = [
   "claims.shipping_methods",
   "claims.shipping_address",
   "claims.additional_items",
+  "claims.additional_items.variant",
   "claims.fulfillments",
   "claims.fulfillments.tracking_links",
   "claims.claim_items",
