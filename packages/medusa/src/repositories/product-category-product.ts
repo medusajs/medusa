550f8b10d182f454fdd0802c480821f1bcaad39e
Riqwan Thamir <rmthamir@gmail.com>
2023-01-23 15:03:26 +0530

    chore: remove join table model + repo

--- packages/medusa/src/repositories/product-category-product.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from "typeorm"
-import { ProductCategoryProduct } from "../models"
-
-@EntityRepository(ProductCategoryProduct)
-// eslint-disable-next-line max-len
-export class ProductCategoryProductRepository extends Repository<ProductCategoryProduct> {}
