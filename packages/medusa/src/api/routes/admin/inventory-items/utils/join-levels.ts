f65f590a2771d6e526d7dfc7ca721be74c8f79a9
Carlos R. L. Rodrigues <37986729+carlos-r-l-rodrigues@users.noreply.github.com>
2023-01-23 09:06:23 -0300

    feat: inventory items api (#2971)
    What:
    Admin endpoints to handle inventory items and their stock levels per location
    
    FIXES: CORE-975
    
    Co-authored-by: Sebastian Rindom <7554214+srindom@users.noreply.github.com>

---
+++ packages/medusa/src/api/routes/admin/inventory-items/utils/join-levels.ts
@@ -0,0 +1,65 @@
+import { IInventoryService } from "../../../../../interfaces"
+import {
+  InventoryItemDTO,
+  InventoryLevelDTO,
+} from "../../../../../types/inventory"
+
+type LevelWithAvailability = InventoryLevelDTO & {
+  available_quantity: number
+}
+
+export const buildLevelsByInventoryItemId = (
+  inventoryLevels: InventoryLevelDTO[],
+  locationIds: string[]
+) => {
+  const filteredLevels = inventoryLevels.filter((level) =>
+    locationIds?.includes(level.location_id)
+  )
+
+  return filteredLevels.reduce((acc, level) => {
+    acc[level.inventory_item_id] = acc[level.inventory_item_id] ?? []
+    acc[level.inventory_item_id].push(level)
+    return acc
+  }, {})
+}
+
+export const getLevelsByInventoryItemId = async (
+  items: InventoryItemDTO[],
+  locationIds: string[],
+  inventoryService: IInventoryService
+) => {
+  const [levels] = await inventoryService.listInventoryLevels({
+    inventory_item_id: items.map((inventoryItem) => inventoryItem.id),
+  })
+
+  const levelsWithAvailability: LevelWithAvailability[] = await Promise.all(
+    levels.map(async (level) => {
+      const availability = await inventoryService.retrieveAvailableQuantity(
+        level.inventory_item_id,
+        [level.location_id]
+      )
+      return {
+        ...level,
+        available_quantity: availability,
+      }
+    })
+  )
+
+  return buildLevelsByInventoryItemId(levelsWithAvailability, locationIds)
+}
+
+export const joinLevels = async (
+  inventoryItems: InventoryItemDTO[],
+  locationIds: string[],
+  inventoryService: IInventoryService
+) => {
+  const levelsByItemId = await getLevelsByInventoryItemId(
+    inventoryItems,
+    locationIds,
+    inventoryService
+  )
+  return inventoryItems.map((inventoryItem) => ({
+    ...inventoryItem,
+    location_levels: levelsByItemId[inventoryItem.id] || [],
+  }))
+}
