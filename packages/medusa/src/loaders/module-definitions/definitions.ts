c86500c441ec23768bb9840e896519d275b98ac3
Philip Korsholm <philip.korsholm@hotmail.com>
2023-01-30 11:45:01 +0100

    initial

--- packages/medusa/src/loaders/module-definitions/definitions.ts
+++ packages/medusa/src/loaders/module-definitions/definitions.ts
@@ -4,31 +4,35 @@ import {
   MODULE_SCOPE,
 } from "../../types/global"
 
+export const inventoryModuleDefinition: ModuleDefinition = {
+  key: "inventoryService",
+  registrationName: "inventoryService",
+  defaultPackage: false,
+  label: "InventoryService",
+  isRequired: false,
+  canOverride: true,
+  defaultModuleDeclaration: {
+    scope: MODULE_SCOPE.INTERNAL,
+    resources: MODULE_RESOURCE_TYPE.SHARED,
+  },
+}
+
+export const stockLocationModuleDefinition: ModuleDefinition = {
+  key: "stockLocationService",
+  registrationName: "stockLocationService",
+  defaultPackage: false,
+  label: "StockLocationService",
+  isRequired: false,
+  canOverride: true,
+  defaultModuleDeclaration: {
+    scope: MODULE_SCOPE.INTERNAL,
+    resources: MODULE_RESOURCE_TYPE.SHARED,
+  },
+}
+
 export const MODULE_DEFINITIONS: ModuleDefinition[] = [
-  {
-    key: "stockLocationService",
-    registrationName: "stockLocationService",
-    defaultPackage: false,
-    label: "StockLocationService",
-    isRequired: false,
-    canOverride: true,
-    defaultModuleDeclaration: {
-      scope: MODULE_SCOPE.INTERNAL,
-      resources: MODULE_RESOURCE_TYPE.SHARED,
-    },
-  },
-  {
-    key: "inventoryService",
-    registrationName: "inventoryService",
-    defaultPackage: false,
-    label: "InventoryService",
-    isRequired: false,
-    canOverride: true,
-    defaultModuleDeclaration: {
-      scope: MODULE_SCOPE.INTERNAL,
-      resources: MODULE_RESOURCE_TYPE.SHARED,
-    },
-  },
+  stockLocationModuleDefinition,
+  inventoryModuleDefinition,
 ]
 
 export default MODULE_DEFINITIONS
