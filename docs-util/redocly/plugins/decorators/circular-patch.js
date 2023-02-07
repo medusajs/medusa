09de163d101423b9e436c658de8cac5a154a5f5d
Patrick <patrick@medusajs.com>
2023-02-07 11:50:49 -0500

    feat(redocly): custom plugin to patch specific circular references

---
+++ docs-util/redocly/plugins/decorators/circular-patch.js
@@ -0,0 +1,82 @@
+module.exports = CircularPatch
+
+/**
+ * Since ref() is triggered upon reaching a $ref OAS line (leaf),
+ * we want to inverse `schemas` instructions in order to optimize iterators.
+ */
+function preparePatches(schemas) {
+  const patches = []
+  const patchesObj = {}
+  for (const schemaToPatch in schemas) {
+    for (const schemaName of schemas[schemaToPatch]) {
+      if (!patchesObj[schemaName]) {
+        patchesObj[schemaName] = {
+          schemaName,
+          schemaPointer: `#/components/schemas/${schemaName}`,
+          schemaToPatchPointers: [],
+        }
+      }
+      const schemaToPatchPointer = `#/components/schemas/${schemaToPatch}`
+      if (
+        !patchesObj[schemaName].schemaToPatchPointers.includes(
+          schemaToPatchPointer
+        )
+      ) {
+        patchesObj[schemaName].schemaToPatchPointers.push(schemaToPatchPointer)
+      }
+    }
+  }
+  for (const key in patchesObj) {
+    patches.push(patchesObj[key])
+  }
+  return patches
+}
+
+function applyPatch(node, schemaName) {
+  delete node["$ref"]
+  node.type = "object"
+  if (!node.description && schemaName) {
+    node.description = `${schemaName} object.`
+  }
+}
+
+function CircularPatch({ schemas = {}, verbose = false }) {
+  const logs = []
+  const patches = preparePatches(schemas)
+
+  return {
+    ref(ref, ctx, resolved) {
+      if (ctx.type.name.toLowerCase() === "schema") {
+        if (resolved?.location?.pointer) {
+          for (const patch of patches) {
+            if (resolved.location.pointer === patch.schemaPointer) {
+              const ctxSchemaPointer = ctx.location.pointer.match(
+                /#\/components\/schemas\/\w+/
+              )[0]
+              if (patch.schemaToPatchPointers.includes(ctxSchemaPointer)) {
+                applyPatch(ref)
+                if (verbose) {
+                  logs.push(
+                    `${ctxSchemaPointer.substring(
+                      "#/components/schemas/".length
+                    )} patch $ref to ${patch.schemaName}`
+                  )
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    Root: {
+      leave() {
+        if (verbose) {
+          logs.sort()
+          for (const log of logs) {
+            console.debug(log)
+          }
+        }
+      },
+    },
+  }
+}
