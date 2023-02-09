507ad00bec74bb63b17eae8a4a3313eb6e0d2503
Patrick <116003638+patrick-medusajs@users.noreply.github.com>
2023-02-09 10:03:11 -0500

    feat(oas) - accurate model OAS representation - F to O (#3210)
    ### Scope
    
    Models F to O
    
    ### What
    
    Refactor OAS for models to accurately represent their shape in API responses.
    
    ### Why
    
    About 33% of model fields are not accurately represented in the OAS. Most of the issues are:
    - fields that can not be omitted in the response are not declared as `required`
    - fields that could return `null` as their value are not declared as `nullable: true`
    
    When using a code generator, these OAS issues would lead to inaccurate response shapes in the generated client.
    
    ### How
    
    #### nullable
    Fields meeting at least one of the following condition will be represented as `nullable: true` in OAS:
    * The field is decorated with `@Column({ nullable: true })`
    * The field is decorated with `@OneToOne`, `@ManyToOne`
    * The field is decorated with `@DeleteDateColumn`
    
    #### optional
    Fields meeting at least one of the following conditions will never be listed as `required` in OAS and will be considered optional and could be omitted in the response:
    * The field is decorated with `@OneToOne`, `@ManyToOne`, `@OneToMany`, `@ManyToMany`
    * The field is decorated with `@FeatureFlagColumn`
    * The field is decorated with `@Column({select: false})`
    * The field is representing dynamic values not persisted in the database
    
    Fields not meeting any of the conditions above will be declared as `required` and are expected to be present in the response.
    
    ### Test
    * Ran OAS validator.
    * Ran docs build script.
    
    Expect OAS changes to be reflected in the API documentation.

--- packages/medusa/src/models/idempotency-key.ts
+++ packages/medusa/src/models/idempotency-key.ts
@@ -55,14 +55,23 @@ export class IdempotencyKey {
  * description: "Idempotency Key is used to continue a process in case of any failure that might occur."
  * type: object
  * required:
+ *   - created_at
+ *   - id
  *   - idempotency_key
+ *   - locked_at
+ *   - recovery_point
+ *   - response_code
+ *   - response_body
+ *   - request_method
+ *   - request_params
+ *   - request_path
  * properties:
  *   id:
- *     type: string
  *     description: The idempotency key's ID
+ *     type: string
  *     example: ikey_01G8X9A7ESKAJXG2H0E6F1MW7A
  *   idempotency_key:
- *     description: "The unique randomly generated key used to determine the state of a process."
+ *     description: The unique randomly generated key used to determine the state of a process.
  *     type: string
  *     externalDocs:
  *       url: https://docs.medusajs.com/advanced/backend/payment/overview#idempotency-key
@@ -73,32 +82,38 @@ export class IdempotencyKey {
  *     format: date-time
  *   locked_at:
  *     description: Date which the idempotency key was locked.
+ *     nullable: true
  *     type: string
  *     format: date-time
  *   request_method:
- *     description: "The method of the request"
+ *     description: The method of the request
+ *     nullable: true
  *     type: string
  *     example: POST
  *   request_params:
+ *     description: The parameters passed to the request
+ *     nullable: true
  *     type: object
- *     description: "The parameters passed to the request"
  *     example:
  *       id: cart_01G8ZH853Y6TFXWPG5EYE81X63
  *   request_path:
- *     description: "The request's path"
+ *     description: The request's path
+ *     nullable: true
  *     type: string
  *     example: /store/carts/cart_01G8ZH853Y6TFXWPG5EYE81X63/complete
  *   response_code:
+ *     description: The response's code.
+ *     nullable: true
  *     type: string
- *     description: "The response's code."
  *     example: 200
  *   response_body:
+ *     description: The response's body
+ *     nullable: true
  *     type: object
- *     description: "The response's body"
  *     example:
  *       id: cart_01G8ZH853Y6TFXWPG5EYE81X63
  *   recovery_point:
+ *     description: Where to continue from.
  *     type: string
- *     description: "Where to continue from."
  *     default: started
  */
