ced1932042f0864f3c59108bcc730e45cbecae8c
Patrick <patrick@medusajs.com>
2023-02-13 09:45:57 -0500

    feat(oas) - accurate model OAS representation - R to U

--- packages/medusa/src/models/sales-channel-location.ts
+++ packages/medusa/src/models/sales-channel-location.ts
@@ -1,4 +1,4 @@
-import { BeforeInsert, Index, Column, ManyToOne, JoinColumn } from "typeorm"
+import { BeforeInsert, Column, Index, JoinColumn, ManyToOne } from "typeorm"
 
 import { FeatureFlagEntity } from "../utils/feature-flag-decorators"
 import { SoftDeletableEntity } from "../interfaces"
@@ -30,27 +30,35 @@ export class SalesChannelLocation extends SoftDeletableEntity {
  * title: "Sales Channel Stock Location"
  * description: "Sales Channel Stock Location link sales channels with stock locations."
  * type: object
+ * required:
+ *   - created_at
+ *   - id
+ *   - location_id
+ *   - sales_channel_id
+ *   - updated_at
  * properties:
  *   id:
- *     type: string
  *     description: The Sales Channel Stock Location's ID
+ *     type: string
  *     example: scloc_01G8X9A7ESKAJXG2H0E6F1MW7A
  *   sales_channel_id:
  *     description: "The id of the Sales Channel"
  *     type: string
+ *     example: sc_01G8X9A7ESKAJXG2H0E6F1MW7A
  *   location_id:
  *     description: "The id of the Location Stock."
  *     type: string
  *   created_at:
- *     type: string
  *     description: "The date with timezone at which the resource was created."
+ *     type: string
  *     format: date-time
  *   updated_at:
- *     type: string
  *     description: "The date with timezone at which the resource was updated."
+ *     type: string
  *     format: date-time
  *   deleted_at:
- *     type: string
  *     description: "The date with timezone at which the resource was deleted."
+ *     nullable: true
+ *     type: string
  *     format: date-time
  */
