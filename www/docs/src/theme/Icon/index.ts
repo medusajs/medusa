3a5800d7c3158a656f7077ba902b5a90bbf3f67b
Shahed Nasser <shahednasser@gmail.com>
2023-02-21 12:29:17 +0200

    docs: changed icons

--- www/docs/src/theme/Icon/index.ts
+++ www/docs/src/theme/Icon/index.ts
@@ -2,18 +2,27 @@ import IconAlert from './Alert';
 import IconBackArrow from './BackArrow';
 import IconBell from './Bell';
 import IconBolt from './Bolt';
+import IconBoltSolid from './BoltSolid';
 import IconBookOpen from './BookOpen';
+import IconBug from './Bug';
 import IconBugAntSolid from './BugAntSolid';
 import IconBuildingTax from './BuildingTax';
+import IconCashSolid from './CashSolid';
 import IconChannels from './Channels';
+import IconChannelsSolid from './ChannelsSolid';
+import IconCheckCircleSolid from './CheckCircleSolid';
 import IconClose from './Close';
 import IconCloudArrowUp from './CloudArrowUp';
 import IconCogSixTooth from './CogSixTooth';
+import IconCommandLine from './CommandLine';
 import IconCommandLineSolid from './CommandLineSolid';
+import IconComputerDesktop from './ComputerDesktop';
 import IconComputerDesktopSolid from './ComputerDesktopSolid';
 import IconCopy from './Copy';
+import IconCreditCardSolid from './CreditCardSolid';
 import IconCubeSolid from './CubeSolid';
 import IconCurrencyDollar from './CurrencyDollar';
+import IconCurrencyDollarSolid from './CurrencyDollarSolid';
 import IconDarkMode from './DarkMode';
 import IconDiscord from './Discord';
 import IconDocumentText from './DocumentText';
@@ -24,18 +33,27 @@ import IconGatsby from './Gatsby';
 import IconGiftSolid from './GiftSolid';
 import IconGitHub from './GitHub';
 import IconGlobeEurope from './GlobeEurope';
+import IconGlobeEuropeSolid from './GlobeEuropeSolid';
 import IconJavaScript from './JavaScript';
+import IconLightBulb from './LightBulb';
 import IconLightBulbSolid from './LightBulbSolid';
 import IconLightMode from './LightMode';
 import IconLinkedIn from './LinkedIn';
 import IconNextjs from './Nextjs';
+import IconPencilSquareSolid from './PencilSquareSolid';
 import IconReact from './React';
 import IconReport from './Report';
 import IconRocketLaunch from './RocketLaunch';
 import IconServerStack from './ServerStack';
 import IconShoppingCart from './ShoppingCart';
+import IconShoppingCartSolid from './ShoppingCartSolid';
 import IconSparkles from './Sparkles';
+import IconSparklesSolid from './SparklesSolid';
+import IconSquaresPlus from './SquaresPlus';
+import IconSquaresPlusSolid from './SquaresPlusSolid';
+import IconStripe from './Stripe';
 import IconTagSolid from './TagSolid';
+import IconTools from './Tools';
 import IconToolsSolid from './ToolsSolid';
 import IconTwitter from './Twitter';
 import IconUser from './User';
@@ -46,18 +64,27 @@ export default {
   'back-arrow': IconBackArrow,
   'bell': IconBell,
   'bolt': IconBolt,
+  'bolt-solid': IconBoltSolid,
   'book-open': IconBookOpen,
+  'bug': IconBug,
   'bug-ant-solid': IconBugAntSolid,
   'building-tax': IconBuildingTax,
+  'cash-solid': IconCashSolid,
+  'channels-solid': IconChannelsSolid,
   'channels': IconChannels,
+  'check-circle-solid': IconCheckCircleSolid,
   'close': IconClose,
   'cloud-arrow-up': IconCloudArrowUp,
   'cog-six-tooth': IconCogSixTooth,
+  'command-line': IconCommandLine,
   'command-line-solid': IconCommandLineSolid,
+  'computer-desktop': IconComputerDesktop,
   'computer-desktop-solid': IconComputerDesktopSolid,
   'copy': IconCopy,
+  'credit-card-solid': IconCreditCardSolid,
   'cube-solid': IconCubeSolid,
   'currency-dollar': IconCurrencyDollar,
+  'currency-dollar-solid': IconCurrencyDollarSolid,
   'dark-mode': IconDarkMode,
   'discord': IconDiscord,
   'document-text': IconDocumentText,
@@ -68,18 +95,27 @@ export default {
   'gift-solid': IconGiftSolid,
   'github': IconGitHub,
   'globe-europe': IconGlobeEurope,
+  'globe-europe-solid': IconGlobeEuropeSolid,
   'javascript': IconJavaScript,
-  'light-solid-bulb': IconLightBulbSolid,
+  'light-bulb': IconLightBulb,
+  'light-bulb-solid': IconLightBulbSolid,
   'light-mode': IconLightMode,
   'linkedin': IconLinkedIn,
   'nextjs': IconNextjs,
+  'pencil-square-solid': IconPencilSquareSolid,
   'react': IconReact,
   'report': IconReport,
   'rocket-launch': IconRocketLaunch,
   'server-stack': IconServerStack,
   'shopping-cart': IconShoppingCart,
+  'shopping-cart-solid': IconShoppingCartSolid,
   'sparkles': IconSparkles,
+  'sparkles-solid': IconSparklesSolid,
+  'squares-plus': IconSquaresPlus,
+  'squares-plus-solid': IconSquaresPlusSolid,
+  'stripe': IconStripe,
   'tag-solid': IconTagSolid,
+  'tools': IconTools,
   'tools-solid': IconToolsSolid,
   'twitter': IconTwitter,
   'user': IconUser,
