b4eba69911784a5cfd63bf101fbef57e71c3b75c
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2023-03-07 13:12:13 +0100

    fix(admin-ui): Resolve `tailwindcss/nesting` correctly (#3404)
    * attempt resolving tailwind nesting
    
    * fix import of tailwindcss/nesting plugin
    
    * trigger pipeline
    
    * trigger pipeline
    
    ---------
    
    Co-authored-by: Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>

--- packages/admin-ui/ui/postcss.config.js
+++ packages/admin-ui/ui/postcss.config.js
@@ -1,9 +1,11 @@
 const path = require("path")
 
 module.exports = {
-  plugins: {
-    "tailwindcss/nesting": {},
-    tailwindcss: { config: path.join(__dirname, "tailwind.config.js") },
-    autoprefixer: {},
-  },
+  plugins: [
+    require("tailwindcss")({
+      config: path.join(__dirname, "tailwind.config.js"),
+    }),
+    require("autoprefixer"),
+    require("tailwindcss/nesting"),
+  ],
 }
