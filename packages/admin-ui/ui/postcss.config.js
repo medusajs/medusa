c86d7eff5c6873cfbccd76c993123b392825a76d
Kasper <kasper@medusajs.com>
2023-03-07 14:13:12 +0100

    add missing dependencies

--- packages/admin-ui/ui/postcss.config.js
+++ packages/admin-ui/ui/postcss.config.js
@@ -2,10 +2,10 @@ const path = require("path")
 
 module.exports = {
   plugins: [
+    require("tailwindcss/nesting"),
     require("tailwindcss")({
       config: path.join(__dirname, "tailwind.config.js"),
     }),
     require("autoprefixer"),
-    require("tailwindcss/nesting"),
   ],
 }
