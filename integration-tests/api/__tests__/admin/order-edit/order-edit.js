59db355d364191b70a9b79de1e52cf216c60b208
olivermrbl <oliver@mrbltech.com>
2023-03-08 16:34:29 +0100

    fix jest test

--- integration-tests/api/__tests__/admin/order-edit/order-edit.js
+++ integration-tests/api/__tests__/admin/order-edit/order-edit.js
@@ -37,7 +37,7 @@ describe("/admin/order-edits", () => {
   beforeAll(async () => {
     const cwd = path.resolve(path.join(__dirname, "..", "..", ".."))
     dbConnection = await initDb({ cwd })
-    medusaProcess = await setupServer({ cwd, verbose: true })
+    medusaProcess = await setupServer({ cwd })
   })
 
   afterAll(async () => {
--- packages/medusa-react/jest.config.js
+++ packages/medusa-react/jest.config.js
@@ -1,19 +1,14 @@
 module.exports = {
-  // [...]
-  globals: {
-    "ts-jest": {
-      diagnostics: false,
-      isolatedModules: true,
-    },
-  },
   transform: {
-    ".(ts|tsx)$": require.resolve("ts-jest/dist/"),
+    ".(ts|tsx|ts)$": ["ts-jest", { isolatedModules: true, diagnostics: false }],
   },
   setupFilesAfterEnv: ["./jest.setup.js"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
-    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    // Force module uuid and msgpackr to resolve with the CJS entry point, because Jest does not support package.json.exports.
+    // See https://github.com/uuidjs/uuid/issues/451
     // Issue is fixed in newer versions of UUID, but since it's a transitive dependency, we need to force it here.
     "^uuid$": "uuid",
+    "^msgpackr": "msgpackr",
   },
 }
