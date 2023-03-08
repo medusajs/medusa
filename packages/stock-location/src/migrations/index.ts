761bdbd25a3b231dccfca2233a780fd7ad89c1fa
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2023-03-08 13:28:59 -0300

    export migrations fix

--- packages/stock-location/src/migrations/index.ts
+++ packages/stock-location/src/migrations/index.ts
@@ -1,8 +1,13 @@
 import { getFilesFromPath } from "@medusajs/modules-sdk"
 
 const migrations: object[] = []
-getFilesFromPath("./**/*.{js|ts}", ["index.js", "index.ts"], (file: string) => {
-  migrations.push(require(file))
-})
+
+getFilesFromPath(
+  __dirname + "/**/*.{ts,js}",
+  ["index.js", "index.ts"],
+  (file: string) => {
+    migrations.push(require(file))
+  }
+)
 
 export default migrations
