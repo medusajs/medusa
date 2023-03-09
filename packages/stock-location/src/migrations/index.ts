0633a159a900adafacb100d87cff94f6fcfc8c80
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2023-03-09 09:57:19 -0300

    ignore .d.ts files

--- packages/stock-location/src/migrations/index.ts
+++ packages/stock-location/src/migrations/index.ts
@@ -2,12 +2,8 @@ import { getFilesFromPath } from "@medusajs/modules-sdk"
 
 const migrations: object[] = []
 
-getFilesFromPath(
-  __dirname + "/**/*.{ts,js}",
-  ["index.js", "index.ts"],
-  (file: string) => {
-    migrations.push(require(file))
-  }
-)
+getFilesFromPath(__dirname + "/**/*.{ts,js}", ["**/*.d.ts"], (file: string) => {
+  migrations.push(require(file))
+})
 
 export default migrations
