41ca529c86abff4c876f7cf6cb3543c939b97f63
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2023-03-15 17:00:53 -0300

    cache-redis

--- packages/cache-redis/src/index.ts
+++ packages/cache-redis/src/index.ts
@@ -3,11 +3,11 @@ import { ModuleExports } from "@medusajs/modules-sdk"
 import Loader from "./loaders"
 import { RedisCacheService } from "./services"
 
-const services = [RedisCacheService]
+const service = RedisCacheService
 const loaders = [Loader]
 
 const moduleDefinition: ModuleExports = {
-  services,
+  service,
   loaders,
 }
 
