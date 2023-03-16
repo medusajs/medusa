afa64136f6fdafac81eea4372c3d0e0dfec765d9
olivermrbl <oliver@mrbltech.com>
2023-03-16 15:44:00 +0100

    remove unused methods

--- packages/medusa/src/services/staged-job.ts
+++ packages/medusa/src/services/staged-job.ts
@@ -24,35 +24,12 @@ class StagedJobService extends TransactionBaseService {
     this.stagedJobRepository_ = stagedJobRepository
   }
 
-  async create(data: Partial<StagedJob>) {
-    return this.atomicPhase_(async (manager) => {
-      const stagedJobRepo = manager.withRepository(this.stagedJobRepository_)
-
-      const stagedJob = stagedJobRepo.create(data)
-      return await stagedJobRepo.save(stagedJob)
-    })
-  }
-
   async list(config: FindConfig<StagedJob>) {
     const stagedJobRepo = this.activeManager_.withRepository(
       this.stagedJobRepository_
     )
 
     return await stagedJobRepo.find(config)
-  }
-
-  async remove(stagedJob: StagedJob) {
-    return this.atomicPhase_(async (manager) => {
-      const stagedJobRepo = manager.withRepository(this.stagedJobRepository_)
-
-      const [job] = await stagedJobRepo.find({ where: { id: stagedJob.id } })
-
-      if (!job) {
-        return
-      }
-
-      return await stagedJobRepo.remove(job)
-    })
   }
 
   async deleteBulk(stagedJobIds: string[]): Promise<void> {
