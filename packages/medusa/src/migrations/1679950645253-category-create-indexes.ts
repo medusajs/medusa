53ac9e5117024ce1a03124fc7eb3ead11d8dd5fb
Riqwan Thamir <rmthamir@gmail.com>
2023-03-27 23:01:39 +0200

    chore: redraw indexes

--- packages/medusa/src/migrations/1679950645253-category-create-indexes.ts
+++ packages/medusa/src/migrations/1679950645253-category-create-indexes.ts
@@ -4,13 +4,13 @@ export class categoryCreateIndexes1679950645253 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       CREATE INDEX "IDX_product_category_active_public" ON "product_category" (
-        "is_internal",
+        "parent_category_id",
         "is_active",
-        "parent_category_id"
+        "is_internal"
       ) WHERE (
-        ("is_internal" IS FALSE) AND
         ("is_active" IS TRUE) AND
-      )
+        ("is_internal" IS FALSE)
+      );
     `)
   }
 
