1ce3cc5ae4e4cb16eb03be49c9287503f759fc60
Frane Polić <16856471+fPolic@users.noreply.github.com>
2023-03-28 11:18:13 +0200

    feat(medusa): invalidate price selection caching within update request (#3553)
    * feat: invalidate price selection caching on update
    
    * feat: add `onVariantsPricesUpdate` to PriceSelectionStrategy
    
    * fix: update units
    
    * fix: import
    
    * Create .changeset/tame-pillows-heal.md
    
    * fix: address feedback
    
    * refactor: make `onVariantsPricesUpdate` optional
    
    ---------
    
    Co-authored-by: fPolic <frane@medusajs.com>
    Co-authored-by: Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>

--- integration-tests/api/medusa-config.js
+++ integration-tests/api/medusa-config.js
@@ -4,6 +4,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_NAME = process.env.DB_TEMP_NAME
 
 const redisUrl = process.env.REDIS_URL || "redis://localhost:6379"
+const cacheTTL = process.env.CACHE_TTL || 15
 
 module.exports = {
   plugins: [],
@@ -17,11 +18,7 @@ module.exports = {
   modules: {
     cacheService: {
       resolve: "@medusajs/cache-inmemory",
-      // don't set cache since this is shared between tests
-      // and since we have "test-product" / "test-variant" as ids
-      // in a bunch of tests, this could cause that incorrect data is returned
-      // (e.g. price selection caches calculations under `ps:${variantId}`)
-      options: { ttl: 0 },
+      options: { ttl: cacheTTL },
     },
   },
 }
