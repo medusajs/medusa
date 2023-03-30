49a407997b7a7ef51372cf60373646fcad2e4387
Kasper <kasper@medusajs.com>
2023-03-30 16:43:12 +0200

    fix failing test

--- integration-tests/api/__tests__/admin/order/order.js
+++ integration-tests/api/__tests__/admin/order/order.js
@@ -47,7 +47,7 @@ describe("/admin/orders", () => {
   beforeAll(async () => {
     const cwd = path.resolve(path.join(__dirname, "..", "..", ".."))
     dbConnection = await initDb({ cwd })
-    medusaProcess = await setupServer({ cwd })
+    medusaProcess = await setupServer({ cwd, verbose: true })
   })
 
   afterAll(async () => {
--- packages/medusa/src/services/order.ts
+++ packages/medusa/src/services/order.ts
@@ -957,11 +957,15 @@ class OrderService extends TransactionBaseService {
         where: { id: order.billing_address_id },
       })
 
-      address.metadata = setMetadata(addr, address.metadata)
+      if (address.metadata) {
+        address.metadata = setMetadata(addr, address.metadata)
+      }
 
       await addrRepo.save({ ...addr, ...address })
     } else {
-      address.metadata = setMetadata(null, address.metadata)
+      if (address.metadata) {
+        address.metadata = setMetadata(null, address.metadata)
+      }
 
       order.billing_address = addrRepo.create({ ...address })
     }
