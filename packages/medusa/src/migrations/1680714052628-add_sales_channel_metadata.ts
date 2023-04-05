28777597b75e503de78bff3ee11eef81f6e1cc02
Philip Korsholm <philip.korsholm@hotmail.com>
2023-04-05 19:05:24 +0200

    fix migration issue

--- packages/medusa/src/migrations/1680714052628-add_sales_channel_metadata.ts
+++ packages/medusa/src/migrations/1680714052628-add_sales_channel_metadata.ts
@@ -1,10 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class scMetadataAndReservationTypes1675694226918
+export const featureFlag = "sales_channels"
+
+export class addSalesChannelMetadata1680714052628
   implements MigrationInterface
 {
+  name = "addSalesChannelMetadata1680714052628"
+
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "sales_channel" ADD "metadata" jsonb`)
+    await queryRunner.query(
+      `ALTER TABLE "sales_channel" ADD COLUMN "metadata" jsonb NULL;`
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
