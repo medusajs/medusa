ca9a1ed812cff073476cc62fb6c6c2174562c978
adrien2p <adrien.deperetti@gmail.com>
2023-04-05 09:50:48 +0200

    chore(medusa): Migrate product tag repository

--- packages/medusa/src/repositories/product-tag.ts
+++ packages/medusa/src/repositories/product-tag.ts
@@ -2,6 +2,8 @@ import { In } from "typeorm"
 import { ProductTag } from "../models/product-tag"
 import { ExtendedFindConfig } from "../types/common"
 import { dataSource } from "../loaders/database"
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity"
+import { Image } from "../models"
 
 type UpsertTagsInput = (Partial<ProductTag> & {
   value: string
@@ -26,18 +28,33 @@ export type FindWithoutRelationsOptions = DefaultWithoutRelations & {
 export const ProductTagRepository = dataSource
   .getRepository(ProductTag)
   .extend({
-    async listTagsByUsage(count = 10): Promise<ProductTag[]> {
-      return await this.query(
-        `
-          SELECT id, COUNT(pts.product_tag_id) as usage_count, pt.value
-          FROM product_tag pt
-                   LEFT JOIN product_tags pts ON pt.id = pts.product_tag_id
-          GROUP BY id
-          ORDER BY usage_count DESC
-              LIMIT $1
-      `,
-        [count]
-      )
+    async insertBulk(
+      data: QueryDeepPartialEntity<ProductTag>[]
+    ): Promise<ProductTag[]> {
+      const queryBuilder = this.createQueryBuilder()
+        .insert()
+        .into(ProductTag)
+        .values(data)
+
+      if (!queryBuilder.connection.driver.isReturningSqlSupported("insert")) {
+        const rawImages = await queryBuilder.execute()
+        return rawImages.generatedMaps.map((d) =>
+          this.create(d)
+        ) as ProductTag[]
+      }
+
+      const rawImages = await queryBuilder.returning("*").execute()
+      return rawImages.generatedMaps.map((d) => this.create(d))
+    },
+
+    async listTagsByUsage(take = 10): Promise<ProductTag[]> {
+      return await this.createQueryBuilder("pt")
+        .leftJoin("product_tags", "pts", "pt.id = pts.product_tag_id")
+        .select(["id", "COUNT(pts.product_tag_id) as usage_count", "value"])
+        .groupBy("id")
+        .orderBy("usage_count", "DESC")
+        .limit(take)
+        .getMany()
     },
 
     async upsertTags(tags: UpsertTagsInput): Promise<ProductTag[]> {
@@ -52,6 +69,7 @@ export const ProductTagRepository = dataSource
       )
 
       const upsertedTags: ProductTag[] = []
+      const tagsToCreate: QueryDeepPartialEntity<Image>[] = []
 
       for (const tag of tags) {
         const aTag = existingTagsMap.get(tag.value)
@@ -59,9 +77,13 @@ export const ProductTagRepository = dataSource
           upsertedTags.push(aTag)
         } else {
           const newTag = this.create(tag)
-          const savedTag = await this.save(newTag)
-          upsertedTags.push(savedTag)
+          tagsToCreate.push(newTag as QueryDeepPartialEntity<Image>)
         }
+      }
+
+      if (tagsToCreate.length) {
+        const newTags = await this.insertBulk(tagsToCreate)
+        upsertedTags.push(...newTags)
       }
 
       return upsertedTags
