0a2e362eb4b2a84c507a70c2486fa70b83a85e66
adrien2p <adrien.deperetti@gmail.com>
2023-04-05 09:39:19 +0200

    chore(medusa): Migrate price list repository

--- packages/medusa/src/repositories/price-list.ts
+++ packages/medusa/src/repositories/price-list.ts
@@ -1,12 +1,7 @@
-import { FindOptionsWhere, ILike } from "typeorm"
+import { FindOperator, FindOptionsWhere, ILike, In } from "typeorm"
 import { PriceList } from "../models"
-import { CustomFindOptions, ExtendedFindConfig } from "../types/common"
+import { ExtendedFindConfig } from "../types/common"
 import { dataSource } from "../loaders/database"
-
-export type PriceListFindOptions = CustomFindOptions<
-  PriceList,
-  "status" | "type"
->
 
 export const PriceListRepository = dataSource.getRepository(PriceList).extend({
   async listAndCount(
@@ -14,26 +9,52 @@ export const PriceListRepository = dataSource.getRepository(PriceList).extend({
     q?: string
   ): Promise<[PriceList[], number]> {
     const query_ = { ...query }
+    query_.relationLoadStrategy = "query"
+    query_.where = query.where as FindOptionsWhere<PriceList>
 
-    if (q) {
-      query_.where = query_.where as FindOptionsWhere<PriceList>
-      delete query_.where.description
-      delete query_.where.name
+    const groups = query_.where.customer_groups as unknown as FindOperator<
+      string[]
+    >
+    delete query_.where.customer_groups
 
-      query_.where.description = ILike(`%${q}%`)
-      query_.where.name = ILike(`%${q}%`)
+    if (groups) {
+      query_.relations = query.relations ?? {}
+      query_.relations.customer_groups =
+        query_.relations.customer_groups ?? true
+
       query_.where.customer_groups = {
-        name: ILike(`%${q}%`),
+        ...(query_.where.customer_groups ?? {}),
+        id: In(groups.value),
       }
     }
 
-    return await this.findAndCount({
-      ...query_,
-      relations: {
-        customer_groups: true,
-      },
-      relationLoadStrategy: "query",
-    })
+    if (q) {
+      if (!groups) {
+        query.relations = query.relations ?? {}
+        query.relations.customer_groups =
+          query.relations.customer_groups ?? true
+      }
+
+      query_.where = [
+        {
+          ...query_.where,
+          name: ILike(`%${q}%`),
+        },
+        {
+          ...query_.where,
+          description: ILike(`%${q}%`),
+        },
+        {
+          ...query_.where,
+          customer_groups: {
+            ...query_.where.customer_groups,
+            name: ILike(`%${q}%`),
+          },
+        },
+      ]
+    }
+
+    return await this.findAndCount(query_)
   },
 })
 
