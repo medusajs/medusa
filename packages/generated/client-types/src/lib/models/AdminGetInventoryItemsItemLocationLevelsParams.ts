1a41c7f968fa7c4c2f5666f033a97862615dca72
Shahed Nasser <shahednasser@gmail.com>
2023-04-06 18:04:57 +0300

    generated changes

--- packages/generated/client-types/src/lib/models/AdminGetInventoryItemsItemLocationLevelsParams.ts
+++ packages/generated/client-types/src/lib/models/AdminGetInventoryItemsItemLocationLevelsParams.ts
@@ -5,14 +5,6 @@ import { SetRelation, Merge } from "../core/ModelUtils"
 
 export interface AdminGetInventoryItemsItemLocationLevelsParams {
   /**
-   * How many stock locations levels to skip in the result.
-   */
-  offset?: number
-  /**
-   * Limit the number of stock locations levels returned.
-   */
-  limit?: number
-  /**
    * Comma separated list of relations to include in the results.
    */
   expand?: string
