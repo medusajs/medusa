fe6607211a90991a2573fafb2838d0d99d75c739
Shahed Nasser <shahednasser@gmail.com>
2023-04-14 14:24:09 +0300

    docs(refactoring): set up and configured Tailwind Css (#3841)
    * docs: added tailwind config
    
    * docs: added more tailwind configurations
    
    * add includes option
    
    * added more tailwind configurations
    
    * fix to configurations

--- www/docs/docusaurus.config.js
+++ www/docs/docusaurus.config.js
@@ -29,6 +29,17 @@ const config = {
       },
     ],
     require.resolve("docusaurus-plugin-image-zoom"),
+    async function tailwindPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"))
+          postcssOptions.plugins.push(require("autoprefixer"))
+          return postcssOptions
+        },
+      }
+    },
   ],
   themeConfig: {
     image: "img/docs-banner.jpg",
