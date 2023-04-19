2c9a8f8adfa1465305acb0c926e24968f5532246
olivermrbl <oliver@mrbltech.com>
2023-04-19 14:27:54 +0200

    fix: Webshipper fulfillment service

--- packages/medusa-fulfillment-webshipper/src/services/webshipper-fulfillment.js
+++ packages/medusa-fulfillment-webshipper/src/services/webshipper-fulfillment.js
@@ -173,32 +173,14 @@ class WebshipperFulfillmentService extends FulfillmentService {
             },
             customs_lines: await Promise.all(
               returnOrder.items.map(async ({ item, quantity }) => {
-                const totals = await this.totalsService_.getLineItemTotals(
+                const customLine = await this.buildWebshipperItem(
                   item,
-                  fromOrder,
-                  {
-                    include_tax: true,
-                    use_tax_lines: true,
-                  }
+                  quantity,
+                  fromOrder
                 )
+
                 return {
-                  ext_ref: item.id,
-                  sku: item.variant.sku,
-                  description: item.title,
-                  quantity: quantity,
-                  country_of_origin:
-                    item.variant.origin_country ||
-                    item.variant.product.origin_country,
-                  tarif_number:
-                    item.variant.hs_code || item.variant.product.hs_code,
-                  unit_price: humanizeAmount(
-                    totals.unit_price,
-                    fromOrder.currency_code
-                  ),
-                  vat_percent: totals.tax_lines.reduce(
-                    (acc, next) => acc + next.rate,
-                    0
-                  ),
+                  ...customLine,
                   currency: fromOrder.currency_code.toUpperCase(),
                 }
               })
@@ -350,34 +332,13 @@ class WebshipperFulfillmentService extends FulfillmentService {
           visible_ref,
           order_lines: await Promise.all(
             fulfillmentItems.map(async (item) => {
-              const totals = await this.totalsService_.getLineItemTotals(
+              const orderLine = await this.buildWebshipperItem(
                 item,
-                fromOrder,
-                {
-                  include_tax: true,
-                  use_tax_lines: true,
-                }
+                item.quantity,
+                fromOrder
               )
 
-              return {
-                ext_ref: item.id,
-                sku: item.variant.sku,
-                description: item.title,
-                quantity: item.quantity,
-                country_of_origin:
-                  item.variant.origin_country ||
-                  item.variant.product.origin_country,
-                tarif_number:
-                  item.variant.hs_code || item.variant.product.hs_code,
-                unit_price: humanizeAmount(
-                  totals.unit_price,
-                  fromOrder.currency_code
-                ),
-                vat_percent: totals.tax_lines.reduce(
-                  (acc, next) => acc + next.rate,
-                  0
-                ),
-              }
+              return orderLine
             })
           ),
           delivery_address: {
@@ -636,6 +597,27 @@ class WebshipperFulfillmentService extends FulfillmentService {
         status: "cancelled",
       },
     })
+  }
+
+  async buildWebshipperItem(item, quantity, order) {
+    const totals = await this.totalsService_.getLineItemTotals(item, order, {
+      include_tax: true,
+      use_tax_lines: true,
+    })
+    return {
+      ext_ref: item.id,
+      sku: item?.variant?.sku || "N/A",
+      description: item.title,
+      quantity: quantity,
+      country_of_origin:
+        item?.variant?.origin_country ||
+        item?.variant?.product?.origin_country ||
+        "N/A",
+      tarif_number:
+        item?.variant?.hs_code || item?.variant?.product?.hs_code || "N/A",
+      unit_price: humanizeAmount(totals.unit_price, order.currency_code),
+      vat_percent: totals.tax_lines.reduce((acc, next) => acc + next.rate, 0),
+    }
   }
 }
 
