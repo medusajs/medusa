b7ccb192993c5a48dfb1e18823970ff8ad448e47
Riqwan Thamir <rmthamir@gmail.com>
2023-05-09 16:10:35 +0200

    chore: remove old test utils

--- packages/product/src/test-utils/config.ts
+++
@@ -1,7 +0,0 @@
-import { ProductServiceInitializeOptions } from "../types"
-
-export const options: ProductServiceInitializeOptions = {
-  database: {
-    clientUrl: "products-module-test",
-  },
-}
