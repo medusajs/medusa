3875a19f59d691c57aa05dd73c3e07748693e575
adrien2p <adrien.deperetti@gmail.com>
2023-05-09 16:25:53 +0200

    WIP migrations variant

--- packages/product/src/models/product-variant.ts
+++ packages/product/src/models/product-variant.ts
@@ -1,64 +1,110 @@
-import { BeforeCreate, Entity, PrimaryKey, Property } from "@mikro-orm/core"
+import {
+  BeforeCreate,
+  Entity,
+  Index,
+  PrimaryKey,
+  Property,
+} from "@mikro-orm/core"
 import { generateEntityId } from "@medusajs/utils"
 
-@Entity()
+@Entity({ tableName: "product_variant" })
 class ProductVariant {
-  @PrimaryKey()
+  @PrimaryKey({ columnType: "text" })
   id!: string
 
-  @Property()
+  @Property({ columnType: "text" })
   title: string
 
-  @Property({ nullable: true })
-  sku?: string
+  @Property({ columnType: "text", nullable: true })
+  @Index({
+    name: "IDX_product_variant_sku_unique",
+    properties: ["sku"],
+    expression:
+      "CREATE UNIQUE INDEX IF NOT EXISTS IDX_product_variant_sku_unique ON product_variant (sku) WHERE deleted_at IS NULL",
+  })
+  sku?: string | null
 
-  @Property({ nullable: true })
-  barcode?: string
+  @Property({ columnType: "text", nullable: true })
+  @Index({
+    name: "IDX_product_variant_sku_unique",
+    properties: ["barcode"],
+    expression:
+      "CREATE UNIQUE INDEX IF NOT EXISTS IDX_product_variant_barcode_unique ON product_variant (barcode) WHERE deleted_at IS NULL",
+  })
+  barcode?: string | null
 
-  @Property({ nullable: true })
-  ean?: string
+  @Property({ columnType: "text", nullable: true })
+  @Index({
+    name: "IDX_product_variant_sku_unique",
+    properties: ["ean"],
+    expression:
+      "CREATE UNIQUE INDEX IF NOT EXISTS IDX_product_variant_ean_unique ON product_variant (ean) WHERE deleted_at IS NULL",
+  })
+  ean?: string | null
 
-  @Property({ nullable: true })
-  upc?: string
+  @Property({ columnType: "text", nullable: true })
+  @Index({
+    name: "IDX_product_variant_sku_unique",
+    properties: ["upc"],
+    expression:
+      "CREATE UNIQUE INDEX IF NOT EXISTS IDX_product_variant_upc_unique ON product_variant (upc) WHERE deleted_at IS NULL",
+  })
+  upc?: string | null
 
-  @Property()
+  @Property({ columnType: "numeric" })
   inventory_quantity: number
 
-  @Property({ default: false })
+  @Property({ columnType: "boolean", default: false })
   allow_backorder: boolean
 
-  @Property({ default: true })
+  @Property({ columnType: "boolean", default: true })
   manage_inventory: boolean
 
-  @Property({ nullable: true })
-  hs_code?: string
+  @Property({ columnType: "text", nullable: true })
+  hs_code?: string | null
 
-  @Property({ nullable: true })
-  origin_country?: string
+  @Property({ columnType: "text", nullable: true })
+  origin_country?: string | null
 
-  @Property({ nullable: true })
-  mid_code?: string
+  @Property({ columnType: "text", nullable: true })
+  mid_code?: string | null
 
-  @Property({ nullable: true })
-  material?: string
+  @Property({ columnType: "text", nullable: true })
+  material?: string | null
 
-  @Property({ nullable: true })
-  weight?: number
+  @Property({ columnType: "numeric", nullable: true })
+  weight?: number | null
 
-  @Property({ nullable: true })
-  length?: number
+  @Property({ columnType: "numeric", nullable: true })
+  length?: number | null
 
-  @Property({ nullable: true })
-  height?: number
+  @Property({ columnType: "numeric", nullable: true })
+  height?: number | null
 
-  @Property({ nullable: true })
-  width?: number
+  @Property({ columnType: "numeric", nullable: true })
+  width?: number | null
 
-  @Property({ nullable: true })
-  metadata?: Record<string, unknown>
+  @Property({ columnType: "jsonb", nullable: true })
+  metadata?: Record<string, unknown> | null
 
-  @Property({ nullable: true })
-  variant_rank?: number
+  @Property({ columnType: "numeric", nullable: true })
+  variant_rank?: number | null
+
+  @Property({ onCreate: () => new Date(), columnType: "timestamptz" })
+  created_at: Date
+
+  @Property({
+    onCreate: () => new Date(),
+    onUpdate: () => new Date(),
+    columnType: "timestamptz",
+  })
+  updated_at: Date
+
+  /**
+   * Soft deleted will be an update of the record which set the deleted_at to new Date()
+   */
+  @Property({ columnType: "timestamptz", nullable: true })
+  deleted_at: Date
 
   // TODO: Bring relations back slowly one by one
   // @ManyToOne(() => Product, { onDelete: "cascade" })
