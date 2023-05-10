ca0125fce6e46c433f1e2e1f31aea46539199fb4
fPolic <frane@medusajs.com>
2023-05-10 11:27:15 +0200

    wip: product list

--- packages/product/src/services/product.ts
+++ packages/product/src/services/product.ts
@@ -1,9 +1,27 @@
 import { ProductVariantService } from "@services"
+import { Product } from "@models"
+import { SqlEntityManager } from "@mikro-orm/postgresql"
+
+type InjectedDependencies = {
+  manager: SqlEntityManager
+  productVariantService: ProductVariantService
+}
 
 export default class ProductService {
+  protected readonly manager: SqlEntityManager
+
   protected readonly productVariantService: ProductVariantService
-  constructor({ productVariantService }) {
+
+  constructor({ manager, productVariantService }: InjectedDependencies) {
+    this.manager = manager
     this.productVariantService = productVariantService
+  }
+
+  async list(
+    selector: Record<any, any> = {},
+    config: Record<any, any> = {}
+  ): Promise<Product[]> {
+    return await this.manager.find(Product, {})
   }
 
   async listVariants() {
