e40f5b55f5b87253dd083a89e6f9bc699ae50a49
Kasper <kasper@medusajs.com>
2023-05-12 16:24:55 +0200

    rm unused pkg

--- packages/plugins-sdk/src/cli/utils/get-package-dev-dependencies.ts
+++
@@ -1,9 +0,0 @@
-import fse from "fs-extra"
-
-export async function getPackageDevDependencies(
-  packagePath: string
-): Promise<string[]> {
-  const packageJson = await fse.readJSON(packagePath)
-
-  return Object.keys(packageJson.devDependencies || {})
-}
