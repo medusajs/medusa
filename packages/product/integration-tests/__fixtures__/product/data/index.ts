7e52b0f8b43a32a1de1ee0c11e4d3cead963f03a
Riqwan Thamir <rmthamir@gmail.com>
2023-05-16 17:13:30 +0200

    chore: modularize spec data file

---
+++ packages/product/integration-tests/__fixtures__/product/data/index.ts
@@ -0,0 +1,2 @@
+export * from './categories'
+export * from './products'
--- packages/product/integration-tests/__tests__/services/product/index.ts
+++ packages/product/integration-tests/__tests__/services/product/index.ts
@@ -5,10 +5,10 @@ import {
   ProductVariantService,
 } from "@services"
 import { ProductRepository } from "@repositories"
-import { Product } from "@models"
+import { Product, ProductCategory } from "@models"
 import { SqlEntityManager } from "@mikro-orm/postgresql"
-import { createProductAndTags } from "../../../__fixtures__/product"
-import { productsData } from "../../../__fixtures__/product/data"
+import { createProductAndTags, createCategories } from "../../../__fixtures__/product"
+import { productsData, categoriesData } from "../../../__fixtures__/product/data"
 
 const productVariantService = {
   list: jest.fn(),
@@ -22,6 +22,7 @@ describe("Product Service", () => {
   let testManager: SqlEntityManager
   let repositoryManager: SqlEntityManager
   let products!: Product[]
+  let categories!: ProductCategory[]
 
   beforeEach(async () => {
     await TestDatabase.setupDatabase()
@@ -48,6 +49,63 @@ describe("Product Service", () => {
         testManager = await TestDatabase.forkManager()
 
         products = await createProductAndTags(testManager, productsData)
+      })
+
+      it("filter by id and including relations", async () => {
+        const productsResult = await service.list(
+          {
+            id: products[0].id,
+          },
+          {
+            relations: ["tags"],
+          }
+        )
+
+        productsResult.forEach((product, index) => {
+          const tags = product.tags.toArray()
+
+          expect(product).toEqual(
+            expect.objectContaining({
+              id: productsData[index].id,
+              title: productsData[index].title,
+            })
+          )
+
+          tags.forEach((tag, tagIndex) => {
+            expect(tag).toEqual(
+              expect.objectContaining({
+                ...productsData[index].tags[tagIndex],
+              })
+            )
+          })
+        })
+      })
+
+      it("filter by id and without relations", async () => {
+        const productsResult = await service.list({
+          id: products[0].id,
+        })
+
+        productsResult.forEach((product, index) => {
+          const tags = product.tags.getItems(false)
+
+          expect(product).toEqual(
+            expect.objectContaining({
+              id: productsData[index].id,
+              title: productsData[index].title,
+            })
+          )
+
+          expect(tags.length).toBe(0)
+        })
+      })
+    })
+
+    describe("relation: categories", () => {
+      beforeEach(async () => {
+        testManager = await TestDatabase.forkManager()
+
+        categories = await createCategories(testManager, categoriesData)
       })
 
       it("filter by id and including relations", async () => {
