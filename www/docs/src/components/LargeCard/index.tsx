b571cfcb1e662c86cecd75400f0ed776e8adb9c2
Shahed Nasser <shahednasser@gmail.com>
2023-05-19 10:53:24 +0300

    fixes to large card

--- www/docs/src/components/LargeCard/index.tsx
+++ www/docs/src/components/LargeCard/index.tsx
@@ -32,15 +32,17 @@ const LargeCard: React.FC<LargeCardProps> = ({
         "tw-border tw-border-solid tw-border-medusa-border-base dark:tw-border-medusa-border-base-dark tw-rounded",
         "tw-pt-6 tw-pb-1 tw-px-1 tw-mb-2",
         "tw-flex tw-flex-col tw-justify-between tw-relative",
-        "before:tw-content-[''] before:tw-absolute before:tw-left-0 before:tw-w-full before:tw-rounded before:tw-z-[1] before:tw-h-[128px] before:tw-top-0",
+        "before:tw-content-[''] before:tw-absolute before:tw-left-0 before:tw-w-full before:tw-rounded before:tw-z-[1] before:tw-h-8 before:tw-top-0",
         "before:tw-bg-large-card dark:before:tw-bg-large-card-dark before:tw-bg-docs-bg-surface dark:before:tw-bg-docs-bg-surface-dark",
         "[&:nth-child(3n+1):before]:tw-bg-[2%_52%] [&:nth-child(3n+2):before]:tw-bg-[19%_16%] [&:nth-child(3n+3):before]:tw-bg-[17%_50%]",
         "after:tw-content-[''] after:tw-absolute after:tw-left-0 after:tw-w-full after:tw-rounded",
-        "after:tw-z-[2] after:tw-left-0 after:tw-top-[64px] after:tw-height-[64px] after:tw-bg-large-card-fade dark:after:tw-bg-large-card-fade-dark",
+        "after:tw-z-[2] after:tw-left-0 after:tw-top-4 after:tw-height-4 after:tw-bg-large-card-fade dark:after:tw-bg-large-card-fade-dark",
         !isSoon &&
-          "group-hover:after:tw-bg-large-card-fade-hover dark:group-hover:after:tw-bg-large-card-fade-hover",
+          "hover:after:tw-bg-large-card-fade-hover dark:hover:after:tw-bg-large-card-fade-hover",
         !isSoon &&
-          "group-hover:tw-bg-medusa-bg-subtle-hover dark:group-hover:tw-bg-medusa-bg-base-hover-dark"
+          "hover:tw-bg-medusa-bg-subtle-hover dark:hover:tw-bg-medusa-bg-base-hover-dark",
+        !isSoon &&
+          "hover:before:tw-bg-medusa-bg-subtle-hover dark:hover:before:tw-bg-medusa-bg-base-hover-dark"
       )}
     >
       <div className={clsx("tw-z-[3]")}>
@@ -61,7 +63,8 @@ const LargeCard: React.FC<LargeCardProps> = ({
         <div className="tw-mb-0.5">
           <span
             className={clsx(
-              "group-hover:tw-text-medusa-text-disabled dark:group-hover:tw-text-medusa-text-disabled-dark",
+              isSoon &&
+                "group-hover:tw-text-medusa-text-disabled dark:group-hover:tw-text-medusa-text-disabled-dark",
               "tw-text-medusa-text-base dark:tw-text-medusa-text-base-dark tw-text-label-regular-plus",
               "tw-transition-all tw-duration-200 tw-ease-ease"
             )}
@@ -71,8 +74,9 @@ const LargeCard: React.FC<LargeCardProps> = ({
         </div>
         <div
           className={clsx(
-            "group-hover:tw-text-medusa-text-disabled dark:group-hover:tw-text-medusa-text-disabled-dark",
-            "tw-transition-all tw-duration-200 tw-ease-ease"
+            isSoon &&
+              "group-hover:tw-text-medusa-text-disabled dark:group-hover:tw-text-medusa-text-disabled-dark",
+            "tw-transition-all tw-duration-200 tw-ease-ease tw-text-body-regular"
           )}
         >
           {children}
@@ -82,7 +86,6 @@ const LargeCard: React.FC<LargeCardProps> = ({
         href={href}
         className={clsx(
           "tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-full tw-z-[4] tw-rounded",
-          "tw-z-[3]",
           isSoon && "group-hover:tw-pointer-events-none"
         )}
       ></a>
