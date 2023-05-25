2fd7716cc891dca53b708caf8fc3e77b9f615a53
Mj.Mehdizadeh <mj.mehdizadeh93@gmail.com>
2023-05-25 17:35:02 +0330

    translate inventory

--- packages/admin-ui/ui/src/domain/inventory/locations/components/general-form/index.tsx
+++ packages/admin-ui/ui/src/domain/inventory/locations/components/general-form/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next"
 import FormValidator from "../../../../../utils/form-validator"
 import InputField from "../../../../../components/molecules/input"
 import { NestedForm } from "../../../../../utils/nested-form"
@@ -11,6 +12,7 @@ type Props = {
 }
 
 const GeneralForm = ({ form }: Props) => {
+  const { t } = useTranslation()
   const {
     register,
     path,
@@ -20,11 +22,11 @@ const GeneralForm = ({ form }: Props) => {
     <div>
       <div className="gap-x-large mb-small grid grid-cols-2">
         <InputField
-          label="Location name"
-          placeholder="Flagship store, warehouse"
+          label={t("Location name")}
+          placeholder={t("Flagship store, warehouse")}
           required
           {...register(path("name"), {
-            required: "Name is required",
+            required: t("Name is required"),
             pattern: FormValidator.whiteSpaceRule("Location name"),
           })}
           errors={errors}
