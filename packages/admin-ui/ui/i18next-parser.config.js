8c688279fc6eb1bfc9ac563b8b656d3246c744cc
Mj.Mehdizadeh <mj.mehdizadeh93@gmail.com>
2023-05-27 21:44:57 +0330

    update fa-IR, en-US translate.json

--- packages/admin-ui/ui/i18next-parser.config.js
+++ packages/admin-ui/ui/i18next-parser.config.js
@@ -42,7 +42,7 @@ module.exports = {
   lineEnding: "auto",
   // Control the line ending. See options at https://github.com/ryanve/eol
 
-  locales: ["en", "fa-IR"],
+  locales: ["en", "en-US", "fa-IR"],
   // An array of the locales in your applications
 
   namespaceSeparator: false,
