ba2ef51cd03afa4ddfa3e5b1ff6260df05b759df
Mj.Mehdizadeh <mj.mehdizadeh93@gmail.com>
2023-05-27 21:32:04 +0330

    translate sales-channels

--- packages/admin-ui/ui/src/domain/sales-channels/tables/placeholder.tsx
+++ packages/admin-ui/ui/src/domain/sales-channels/tables/placeholder.tsx
@@ -1,7 +1,9 @@
+import { useTranslation } from "react-i18next"
 import Button from "../../../components/fundamentals/button"
 import SidedMouthFaceIcon from "../../../components/fundamentals/icons/sided-mouth-face"
 
 function Placeholder({ showAddModal }) {
+  const { t } = useTranslation()
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <span className="text-grey-50">
@@ -9,15 +11,16 @@ function Placeholder({ showAddModal }) {
       </span>
 
       <h3 className="text-large text-gray-90 mt-6 font-semibold">
-        Start building your channels setup...
+        {t("Start building your channels setup...")}
       </h3>
       <p className="text-grey-50 mt-2 mb-8 w-[358px] text-center">
-        You haven’t added any products to this channels yet, but once you do
-        they will live here.
+        {t(
+          "You haven’t added any products to this channels yet, but once you do they will live here."
+        )}
       </p>
 
       <Button onClick={showAddModal} variant="primary" size="small">
-        Add products
+        {t("Add products")}
       </Button>
     </div>
   )
