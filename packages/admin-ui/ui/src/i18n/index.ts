09ad2410d1ca75b3778458789f98f6bdc06160a2
Mj.Mehdizadeh <mj.mehdizadeh93@gmail.com>
2023-05-27 12:26:59 +0330

    translate fa, fa-IR and language detector

--- packages/admin-ui/ui/src/i18n/index.ts
+++ packages/admin-ui/ui/src/i18n/index.ts
@@ -1,10 +1,12 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
-import en from "./locales/en/translation.json"
-import fa from "./locales/fa-IR/translation.json"
+import Backend from "i18next-http-backend"
+import LanguageDetector from "i18next-browser-languagedetector"
 
 void i18n
+  .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
@@ -13,10 +15,6 @@ void i18n
     lng: "fa-IR",
     fallbackLng: "en",
     debug: true,
-    resources: {
-      en: { translation: en },
-      "fa-IR": { translation: fa },
-    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
