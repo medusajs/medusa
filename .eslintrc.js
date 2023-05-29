1f843e44535a3fd90ef96e3684d4c3c32e922bd4
Kasper <kasper@medusajs.com>
2023-05-29 12:17:02 +0200

    add tsup

--- .eslintrc.js
+++ .eslintrc.js
@@ -94,6 +94,7 @@ module.exports = {
           "./packages/stock-location/tsconfig.spec.json",
           "./packages/cache-redis/tsconfig.spec.json",
           "./packages/cache-inmemory/tsconfig.spec.json",
+          "./packages/admin-ui/tsconfig.json",
         ],
       },
       rules: {
