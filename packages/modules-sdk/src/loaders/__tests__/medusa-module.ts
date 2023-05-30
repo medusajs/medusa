00640548a7e087ee71259d4ae781b7b3dbd15cdd
adrien2p <adrien.deperetti@gmail.com>
2023-05-30 10:21:22 +0200

    fix module tests with singleton module

--- packages/modules-sdk/src/loaders/__tests__/medusa-module.ts
+++ packages/modules-sdk/src/loaders/__tests__/medusa-module.ts
@@ -19,6 +19,8 @@ jest.mock("./../../loaders", () => ({
 
 describe("Medusa Module", () => {
   beforeEach(() => {
+    ;(MedusaModule as any).instances_ = new Map()
+
     jest.resetModules()
     jest.clearAllMocks()
   })
