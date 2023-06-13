81033f56e1f9c54730b9ac5061397323629c92c1
adrien2p <adrien.deperetti@gmail.com>
2023-06-13 15:49:01 +0200

    fix(product): The bin scripts should include a shebang and import in the body

--- packages/product/src/scripts/bin/run-migration-up.ts
+++ packages/product/src/scripts/bin/run-migration-up.ts
@@ -1,6 +1,7 @@
-import { runMigrations } from "../migration-up"
+#!/usr/bin/env node
 
 export default (async () => {
+  const { runMigrations } = await import("../migration-up")
   const { config } = await import("dotenv")
   config()
   await runMigrations()
