dfabc55f18de244e857a2aab548c22d54b7ac9ea
Kasper <kasper@medusajs.com>
2023-06-17 16:51:35 +0200

    delete ghost files

--- packages/ui/src/extensions.ts
+++
@@ -1,5 +0,0 @@
-import ext0 from "../../../../clean/my-medusa-store/dist/admin/_virtual/_virtual_entry.js"
-
-export const extensions = [ext0]
-
-export default extensions
--- packages/ui/tailwind.content.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: [
-    "../../../clean/my-medusa-store/dist/admin//**/*.{js,ts,jsx,tsx}",
-  ],
-}
