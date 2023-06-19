1baec44d57b26c24cebc98761cc2f796d3e9c477
Shahed Nasser <shahednasser@gmail.com>
2023-06-19 15:44:28 +0300

    changed code theme

--- www/docs/src/themes/medusaDocs.js
+++ www/docs/src/themes/medusaDocs.js
@@ -1,58 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const palenightTheme = require("prism-react-renderer/themes/palenight")
+const vsCodeTheme = require("prism-react-renderer/themes/vsDark")
 
 const theme = {
-  ...palenightTheme,
+  ...vsCodeTheme,
   plain: {
-    color: "#ECEDEE",
+    ...vsCodeTheme.plain,
     backgroundColor: "#151718",
   },
-  styles: [
-    ...palenightTheme.styles,
-    {
-      types: ["string", "inserted"],
-      style: {
-        color: "#4CC38A",
-      },
-    },
-    {
-      types: ["punctuation", "selector"],
-      style: {
-        color: "#9E8CFC",
-      },
-    },
-    {
-      types: ["doctype"],
-      style: {
-        color: "#9E8CFC",
-        fontStyle: "italic",
-      },
-    },
-    {
-      types: ["builtin", "char", "constant", "function"],
-      style: {
-        color: "#52A9FF",
-      },
-    },
-    {
-      types: ["class-name", "attr-name"],
-      style: {
-        color: "#F1A10D",
-      },
-    },
-    {
-      types: ["tag", "deleted"],
-      style: {
-        color: "#FF6369",
-      },
-    },
-    {
-      types: ["boolean"],
-      style: {
-        color: "#FF6369",
-      },
-    },
-  ],
 }
 
 module.exports = theme
