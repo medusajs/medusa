858bf94d7857bedf73b3ac8eb83f4df8e65b8eef
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-06-27 22:34:54 -0500

    sidebar toggle

--- packages/admin-ui/ui/src/components/organisms/sidebar/index.tsx
+++ packages/admin-ui/ui/src/components/organisms/sidebar/index.tsx
@@ -11,12 +11,13 @@ import SaleIcon from "../../fundamentals/icons/sale-icon"
 import TagIcon from "../../fundamentals/icons/tag-icon"
 import SwatchIcon from "../../fundamentals/icons/swatch-icon"
 import UsersIcon from "../../fundamentals/icons/users-icon"
+import ArrowLeftIcon from "../../fundamentals/icons/arrow-left-icon"
 import SidebarMenuItem from "../../molecules/sidebar-menu-item"
 import UserMenu from "../../molecules/user-menu"
 
 const ICON_SIZE = 20
 
-const Sidebar: React.FC = () => {
+const Sidebar: any = ({toggleSidebar}) => {
   const [currentlyOpen, setCurrentlyOpen] = useState(-1)
 
   const { isFeatureEnabled } = useFeatureFlag()
@@ -44,6 +45,7 @@ const Sidebar: React.FC = () => {
           <div className="rounded-circle flex h-8 w-8 items-center justify-center border border-solid border-gray-300">
             <UserMenu />
           </div>
+          <div onClick={() => toggleSidebar((prev) => !prev)}><ArrowLeftIcon /></div>
         </div>
         <div className="my-base flex flex-col px-2">
           <span className="text-grey-50 text-small font-medium">Store</span>
--- packages/admin-ui/ui/src/components/templates/layout.tsx
+++ packages/admin-ui/ui/src/components/templates/layout.tsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { Toaster } from "react-hot-toast"
 import Sidebar from "../organisms/sidebar"
 import Topbar from "../organisms/topbar"
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [isSidebarOpen, toggleSidebar] = useState(true)
   return (
     <div className="inter-base-regular text-grey-90 flex h-screen w-full">
       <Toaster
@@ -14,9 +15,11 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
           right: 24,
         }}
       />
-      <Sidebar />
+      {isSidebarOpen && (
+        <Sidebar toggleSidebar={toggleSidebar} />
+      )}
       <div className="flex flex-1 flex-col">
-        <Topbar />
+      <Topbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
         <div className="large:px-xlarge py-xlarge bg-grey-5 min-h-content overflow-y-auto">
           <main className="xsmall:mx-base small:mx-xlarge medium:mx-4xlarge large:mx-auto large:max-w-7xl large:w-full h-full">
             {children}
