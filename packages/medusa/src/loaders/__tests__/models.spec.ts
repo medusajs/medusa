04f380eebe36fcb5b8dcf39fd3b00e9f1de23a9a
Riqwan Thamir <rmthamir@gmail.com>
2023-06-28 12:51:11 +0200

    chore: test cleanup

--- packages/medusa/src/loaders/__tests__/models.spec.ts
+++ packages/medusa/src/loaders/__tests__/models.spec.ts
@@ -1,10 +1,6 @@
-import { asValue } from "awilix"
 import { createMedusaContainer } from "medusa-core-utils"
-import path, { resolve } from "path"
-import { DataSource, EntityManager } from "typeorm"
+import path from "path"
 
-import Logger from "../logger"
-import { MEDUSA_PROJECT_NAME, registerServices, registerStrategies } from "../plugins"
 import modelsLoader from "../models"
 
 describe("models loader", () => {
@@ -14,45 +10,35 @@ describe("models loader", () => {
   let models
   let error
 
-  container.register("logger", asValue(Logger))
-  container.register("manager", asValue(new EntityManager({} as DataSource)))
-  container.register("db_entities", asValue([]))
-
-  afterAll(() => {
-    jest.clearAllMocks()
+  beforeAll(async () => {
+    try {
+      models = await modelsLoader({
+        container,
+        isTest: true,
+        coreTestPathGlob: "../models/{product,product-variant}.ts",
+        rootDirectory,
+        extensionPathGlob,
+      })
+    } catch (e) {
+      error = e
+    }
   })
 
-  describe("modelsLoader", function () {
-    beforeAll(async () => {
-      try {
-        models = await modelsLoader({
-          container,
-          isTest: true,
-          coreTestPathGlob: "../models/{product,product-variant}.ts",
-          rootDirectory,
-          extensionPathGlob,
-        })
-      } catch (e) {
-        error = e
-      }
-    })
+  it("error should be falsy", () => {
+    expect(error).toBeFalsy()
+  })
 
-    it("error should be falsy", () => {
-      expect(error).toBeFalsy()
-    })
+  it("ensure that the product model is an extended model", () => {
+    expect(models).toHaveLength(2)
+    const productModel = models.find(model => model.name === 'Product')
 
-    it("ensure that the product model is an extended model", () => {
-      expect(models).toHaveLength(2)
-      const productModel = models.find(model => model.name === 'Product')
+    expect(new productModel().custom_attribute).toEqual("test")
+  })
 
-      expect(new productModel().custom_attribute).toEqual("test")
-    })
+  it("ensure that the product variant model is a core model", () => {
+    expect(models).toHaveLength(2)
+    const productVariantModel = models.find(model => model.name === 'ProductVariant')
 
-    it("ensure that the product variant model is a core model", () => {
-      expect(models).toHaveLength(2)
-      const productVariantModel = models.find(model => model.name === 'ProductVariant')
-
-      expect(productVariantModel.isExtendedModel).not.toBeDefined()
-    })
+    expect(productVariantModel.isExtendedModel).not.toBeDefined()
   })
 })
