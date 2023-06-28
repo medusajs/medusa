103242d1bf9efbb77dcda16dcb8307735b5e2976
Riqwan Thamir <rmthamir@gmail.com>
2023-06-28 14:49:29 +0200

    chore: ignore customization file on jest

--- packages/medusa/jest.config.js
+++ packages/medusa/jest.config.js
@@ -18,6 +18,7 @@ module.exports = {
   transform: {
     "^.+\\.[jt]s?$": "ts-jest",
   },
+  testPathIgnorePatterns: ['customizations', 'flags'],
   testEnvironment: `node`,
   moduleFileExtensions: [`js`, `jsx`, `ts`, `tsx`, `json`],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
