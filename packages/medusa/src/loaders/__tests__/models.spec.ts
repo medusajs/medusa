af762c969554f6326a7cf8310f7e7dac6fb5b8b5
adrien2p <adrien.deperetti@gmail.com>
2023-06-28 16:08:52 +0200

    fix glob

--- packages/medusa/src/loaders/__tests__/models.spec.ts
+++ packages/medusa/src/loaders/__tests__/models.spec.ts
@@ -16,7 +16,7 @@ describe("models loader", () => {
         container,
         isTest: true,
         coreTestPathGlob: "../models/{product,product-variant}.ts",
-        rootDirectory: path.join(__dirname, 'customizations'),
+        rootDirectory: path.join(__dirname, "__fixtures__/customizations"),
         extensionPathGlob: "models/{product,product-variant}.ts",
       })
     } catch (e) {
@@ -30,30 +30,36 @@ describe("models loader", () => {
   })
 
   it("ensure that the product model is an extended model", () => {
-    const productModel = models.find(model => model.name === 'Product')
+    const productModel = models.find((model) => model.name === "Product")
 
     expect(productModel.isExtendedModel).toBe(true)
     expect(new productModel().custom_attribute).toEqual("test")
   })
 
   it("ensure that the product variant model is a core model", () => {
-    const productVariantModel = models.find(model => model.name === 'ProductVariant')
+    const productVariantModel = models.find(
+      (model) => model.name === "ProductVariant"
+    )
 
     expect(productVariantModel.isExtendedModel).not.toBeDefined()
   })
 
   it("ensure that the extended product model is registered in db_entities", () => {
     const entities = container.resolve("db_entities_STORE")
-    const productModelResolver = entities.find(entity => entity.resolve().name === 'Product')
+    const productModelResolver = entities.find(
+      (entity) => entity.resolve().name === "Product"
+    )
     const productModel = productModelResolver.resolve()
 
     expect(productModel.isExtendedModel).toBe(true)
-    expect((new productModel()).custom_attribute).toEqual("test")
+    expect(new productModel().custom_attribute).toEqual("test")
   })
 
   it("ensure that the core product variant model is registered in db_entities", () => {
     const entities = container.resolve("db_entities_STORE")
-    const productVariantModelResolver = entities.find(entity => entity.resolve().name === 'ProductVariant')
+    const productVariantModelResolver = entities.find(
+      (entity) => entity.resolve().name === "ProductVariant"
+    )
     const productVariantModel = productVariantModelResolver.resolve()
 
     expect(productVariantModel.isExtendedModel).not.toBeDefined()
