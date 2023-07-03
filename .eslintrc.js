7d37b90c4a672d7db9969af66f0d8c6b0518274e
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2023-07-03 10:19:57 +0200

    feat(medusa-payment-stripe): Stripe Widget (#4309)

--- .eslintrc.js
+++ .eslintrc.js
@@ -181,5 +181,47 @@ module.exports = {
         project: "./packages/admin/tsconfig.json",
       },
     },
+    {
+      files: [
+        "packages/medusa-payment-stripe/src/admin/**/*.ts",
+        "packages/medusa-payment-stripe/src/admin/**/*.tsx",
+      ],
+      plugins: ["unused-imports"],
+      extends: [
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: "module", // Allows for the use of imports
+        project: "./packages/medusa-payment-stripe/tsconfig.admin.json",
+      },
+      env: {
+        browser: true,
+      },
+      rules: {
+        "prettier/prettier": "error",
+        "react/prop-types": "off",
+        "new-cap": "off",
+        "require-jsdoc": "off",
+        "valid-jsdoc": "off",
+        "no-unused-expressions": "off",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+          "warn",
+          {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+          },
+        ],
+      },
+    },
   ],
 }
