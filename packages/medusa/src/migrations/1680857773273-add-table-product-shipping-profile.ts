f28d600404d6985126cfbc50ae458f65b8783c43
adrien2p <adrien.deperetti@gmail.com>
2023-07-07 12:13:58 +0200

    improve migration rebusteness

--- packages/medusa/src/migrations/1680857773273-add-table-product-shipping-profile.ts
+++ packages/medusa/src/migrations/1680857773273-add-table-product-shipping-profile.ts
@@ -6,7 +6,7 @@ export class addTableProductShippingProfile1680857773273
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `
-        CREATE TABLE "product_shipping_profile"
+        CREATE TABLE IF NOT EXISTS "product_shipping_profile"
         (
             "profile_id" text NOT NULL,
             "product_id" text NOT NULL
@@ -15,11 +15,11 @@ export class addTableProductShippingProfile1680857773273
         INSERT INTO "product_shipping_profile" ("profile_id", "product_id")
         SELECT "profile_id", "id" FROM "product";
 
-        ALTER TABLE "product" DROP COLUMN "profile_id";
+        ALTER TABLE "product" DROP COLUMN IF EXISTS "profile_id";
         ALTER TABLE "product_shipping_profile" ADD CONSTRAINT "pk_product_shipping_profile" PRIMARY KEY ("id");
-        CREATE INDEX "idx_product_shipping_profile_deleted_at" ON "product_shipping_profile" ("deleted_at");
-        CREATE UNIQUE INDEX "idx_product_shipping_profile_profile_id_product_id_unique" ON "product_shipping_profile" ("profile_id", "product_id");
-        DROP INDEX "IDX_80823b7ae866dc5acae2dac6d2";
+        CREATE INDEX IF NOT EXISTS "idx_product_shipping_profile_deleted_at" ON "product_shipping_profile" ("deleted_at");
+        CREATE UNIQUE INDEX IF NOT EXISTS "idx_product_shipping_profile_profile_id_product_id_unique" ON "product_shipping_profile" ("profile_id", "product_id");
+        DROP INDEX IF EXISTS "IDX_80823b7ae866dc5acae2dac6d2";
       `
     )
   }
@@ -30,14 +30,14 @@ export class addTableProductShippingProfile1680857773273
         FROM "product_shipping_profile"
         WHERE "product"."id" = "product_shipping_profile"."product_id";
 
-        DROP INDEX "idx_product_shipping_profile_deleted_at";
-        DROP INDEX "idx_product_shipping_profile_profile_id_product_id_unique";
+        DROP INDEX IF EXISTS "idx_product_shipping_profile_deleted_at";
+        DROP INDEX IF EXISTS "idx_product_shipping_profile_profile_id_product_id_unique";
 
-        DROP TABLE "product_shipping_profile";
+        DROP TABLE IF EXISTS "product_shipping_profile";
 
-        ALTER TABLE "product" ADD COLUMN "profile_id" text NOT NULL;
+        ALTER TABLE "product" ADD COLUMN IF NOT EXISTS "profile_id" text NOT NULL;
 
-        CREATE INDEX "IDX_80823b7ae866dc5acae2dac6d2" ON "product" ("profile_id");
+        CREATE INDEX IF NOT EXISTS "IDX_80823b7ae866dc5acae2dac6d2" ON "product" ("profile_id");
         ALTER TABLE "product" ADD CONSTRAINT "FK_80823b7ae866dc5acae2dac6d2c" FOREIGN KEY ("profile_id") REFERENCES "shipping_profile"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
     `)
   }
