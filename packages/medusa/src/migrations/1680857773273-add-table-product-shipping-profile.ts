a8483d21f61e376fa72dc583a50dc32bfde548da
adrien2p <adrien.deperetti@gmail.com>
2023-07-19 12:09:15 +0200

    update migrations

--- packages/medusa/src/migrations/1680857773273-add-table-product-shipping-profile.ts
+++ packages/medusa/src/migrations/1680857773273-add-table-product-shipping-profile.ts
@@ -32,11 +32,13 @@ export class addTableProductShippingProfile1680857773273
 
         DROP TABLE IF EXISTS "product_shipping_profile";
 
-        ALTER TABLE "product" ADD COLUMN IF NOT EXISTS "profile_id" text NOT NULL;
+        ALTER TABLE "product" ADD COLUMN IF NOT EXISTS "profile_id";
 
         UPDATE "product" SET "profile_id" = "product_shipping_profile"."profile_id"
             FROM "product_shipping_profile"
         WHERE "product"."id" = "product_shipping_profile"."product_id";
+
+        ALTER TABLE "product" ALTER COLUMN profile_id SET NOT NULL;
 
         CREATE INDEX IF NOT EXISTS "IDX_80823b7ae866dc5acae2dac6d2" ON "product" ("profile_id");
         ALTER TABLE "product" ADD CONSTRAINT "FK_80823b7ae866dc5acae2dac6d2c" FOREIGN KEY ("profile_id") REFERENCES "shipping_profile"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
