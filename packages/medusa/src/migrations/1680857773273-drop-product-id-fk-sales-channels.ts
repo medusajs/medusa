2165ac6db69e8e2e1f27d288cba69610aeee74b4
adrien2p <adrien.deperetti@gmail.com>
2023-07-20 11:50:08 +0200

    rm create Cindexes since they already exists

--- packages/medusa/src/migrations/1680857773273-drop-product-id-fk-sales-channels.ts
+++ packages/medusa/src/migrations/1680857773273-drop-product-id-fk-sales-channels.ts
@@ -8,16 +8,12 @@ export class dropProductIdFkSalesChannels1680857773273
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         alter table if exists "product_sales_channel" drop constraint if exists "FK_5a4d5e1e60f97633547821ec8d6";
-        create index if not exists "IDX_product_sales_channel_product_id" on "product_sales_channel" ("product_id");
-        create index if not exists "IDX_product_sales_channel_sales_channel_id" on "product_sales_channel" ("sales_channel_id");
     `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         ALTER TABLE if exists "product_sales_channel" ADD CONSTRAINT if not exists "FK_5a4d5e1e60f97633547821ec8d6" FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE cascade ON UPDATE NO ACTION;
-        DROP INDEX if exists "IDX_product_sales_channel_product_id";
-        DROP INDEX if exists "IDX_product_sales_channel_sales_channel_id";
     `)
   }
 }
--- packages/medusa/src/migrations/1680857773273-drop-variant-id-fk-money-amount.ts
+++ packages/medusa/src/migrations/1680857773273-drop-variant-id-fk-money-amount.ts
@@ -9,7 +9,6 @@ export class dropVariantIdFkMoneyAmount1680857773273
     await queryRunner.query(
       `
         alter table if exists "money_amount" drop constraint if exists "FK_17a06d728e4cfbc5bd2ddb70af0";
-        create index if not exists "IDX_money_amount_variant_id" on "money_amount" ("variant_id");
       `
     )
   }
@@ -17,7 +16,6 @@ export class dropVariantIdFkMoneyAmount1680857773273
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         ALTER TABLE if exists "money_amount" ADD CONSTRAINT if not exists "FK_17a06d728e4cfbc5bd2ddb70af0" FOREIGN KEY ("variant_id") REFERENCES "product_variant"("id") ON DELETE cascade ON UPDATE NO ACTION;
-        DROP INDEX if exists "IDX_money_amount_variant_id";
     `)
   }
 }
