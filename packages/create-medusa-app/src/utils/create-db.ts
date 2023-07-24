15e87a8100c9fe66f6d120423ef0351f4c657e7e
Shahed Nasser <shahednasser@gmail.com>
2023-07-24 13:33:14 +0300

    feat(create-medusa-app): add stable option + add URI encoding to database string (#4567)
    * feat(create-medusa-app): added stable option
    
    * add new function
    
    * changed open url
    
    * switch condition
    
    * fix open url for stable
    
    * add URI encoding
    
    * modified db error message

--- packages/create-medusa-app/src/utils/create-db.ts
+++ packages/create-medusa-app/src/utils/create-db.ts
@@ -82,7 +82,7 @@ export async function getDbClientAndCredentials(dbName: string): Promise<{
       })
     } catch (e) {
       logMessage({
-        message: `Couldn't connect to PostgreSQL. Make sure you have PostgreSQL installed and the credentials you provided are correct.${EOL}${EOL}You can learn how to install PostgreSQL here: https://docs.medusajs.com/development/backend/prepare-environment?os=${getCurrentOs()}#postgresql`,
+        message: `Couldn't connect to PostgreSQL. Make sure you have PostgreSQL installed and the credentials you provided are correct.${EOL}${EOL}You can learn how to install PostgreSQL here: https://docs.medusajs.com/development/backend/prepare-environment?os=${getCurrentOs()}#postgresql${EOL}${EOL}If you keep running into this issue despite having PostgreSQL installed, please check out our troubleshooting guidelines: https://docs.medusajs.com/troubleshooting/database-error`,
         type: "error",
       })
     }
