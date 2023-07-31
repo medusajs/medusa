25a9edc19c77240e75a1a7cfbde73acdb54ed1de
carlos-r-l-rodrigues <rodrigolr@gmail.com>
2023-07-30 21:59:55 -0300

    jest config

--- packages/link-modules/jest.config.js
+++ packages/link-modules/jest.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  moduleNameMapper: {
-    "^@models": "<rootDir>/src/models",
-    "^@services": "<rootDir>/src/services",
-    "^@repositories": "<rootDir>/src/repositories",
-  },
   globals: {
     "ts-jest": {
-      tsConfig: "tsconfig.spec.json",
+      tsConfig: "tsconfig.json",
       isolatedModules: false,
     },
   },
@@ -15,7 +10,4 @@ module.exports = {
   },
   testEnvironment: `node`,
   moduleFileExtensions: [`js`, `ts`],
-  modulePathIgnorePatterns: ["dist/"],
-  setupFiles: ["<rootDir>/integration-tests/setup-env.js"],
-  setupFilesAfterEnv: ["<rootDir>/integration-tests/setup.js"],
 }
