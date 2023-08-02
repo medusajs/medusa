e78c47b66fe99f5d242ca119dea580804a31768c
Adrien de Peretti <adrien.deperetti@gmail.com>
2023-08-02 13:50:06 +0200

    fix(workflows): Workflow types (#4673)
    * fix(workflow): types and handlers
    
    * add middleware pipe example
    
    * Create nine-gorillas-collect.md

--- packages/workflows/src/handlers/remove-inventory-items.ts
+++ packages/workflows/src/handlers/remove-inventory-items.ts
@@ -1,31 +1,26 @@
-import { InventoryItemDTO, MedusaContainer } from "@medusajs/types"
+import { InventoryItemDTO } from "@medusajs/types"
 
 import { InputAlias } from "../definitions"
-import { WorkflowArguments } from "../helper"
+import { PipelineHandlerResult, WorkflowArguments } from "../helper"
 
-export async function removeInventoryItems({
+export async function removeInventoryItems<T = void[]>({
   container,
   data,
 }: WorkflowArguments & {
   data: {
     [InputAlias.InventoryItems]: InventoryItemDTO
   }[]
-}) {
+}): Promise<PipelineHandlerResult<T>> {
   const manager = container.resolve("manager")
   const inventoryService = container.resolve("inventoryService")
   const context = { transactionManager: manager }
 
-  const value = await Promise.all(
+  return (await Promise.all(
     data.map(async ({ [InputAlias.InventoryItems]: inventoryItem }) => {
       return await inventoryService!.deleteInventoryItem(
         inventoryItem.id,
         context
       )
     })
-  )
-
-  return {
-    alias: InputAlias.RemovedInventoryItems,
-    value,
-  }
+  )) as PipelineHandlerResult<T>
 }
