1268cedcce452fc82951d513489cf864fe1114d1
adrien2p <adrien.deperetti@gmail.com>
2023-08-02 14:22:45 +0200

    improve tests

--- packages/workflows/src/helper/__tests__/pipe.spec.ts
+++ packages/workflows/src/helper/__tests__/pipe.spec.ts
@@ -4,30 +4,40 @@ describe("Pipe", function () {
   it("should evaluate the input object and append the values to the data object and return the result from the handler", async function () {
     const payload = { input: "input" }
     const output = { test: "test" }
+    const invoke = {
+      input: payload,
+      step1: { ...payload, step1Data: { test: "test" } },
+      step2: { ...payload, step2Data: { test: "test" } },
+    }
 
     const handler = jest.fn().mockImplementation(async () => output)
     const input = {
-      inputAlias: "input",
+      inputAlias: "payload",
       invoke: [
         {
-          from: "input",
+          from: "payload",
           alias: "input",
         },
         {
-          from: "input",
-          alias: "inputData",
+          from: "step1",
+          alias: "previousDataStep1",
+        },
+        {
+          from: "step2",
+          alias: "previousDataStep2",
         },
       ],
     }
 
-    const result = await pipe(input, handler)({ payload } as any)
+    const result = await pipe(input, handler)({ invoke, payload } as any)
 
     expect(handler).toHaveBeenCalled()
     expect(handler).toHaveBeenCalledWith(
       expect.objectContaining({
         data: {
           input: payload,
-          inputData: payload,
+          previousDataStep1: invoke.step1,
+          previousDataStep2: invoke.step2,
         },
       })
     )
