1260b1a806b035b7223cf873a3dc5c1b6b6da0c2
Riqwan Thamir <rmthamir@gmail.com>
2023-08-04 14:38:07 +0200

    chore: fix module test (#4693)

--- packages/product/integration-tests/__tests__/module.ts
+++ packages/product/integration-tests/__tests__/module.ts
@@ -5,7 +5,10 @@ import { ProductRepository } from "../__fixtures__/module"
 import { createProductAndTags } from "../__fixtures__/product"
 import { productsData } from "../__fixtures__/product/data"
 import { DB_URL, TestDatabase } from "../utils"
+import { buildProductAndRelationsData } from "../__fixtures__/product/data/create-product"
+
 import { IProductModuleService } from "@medusajs/types"
+import { kebabCase } from "@medusajs/utils"
 
 const beforeEach_ = async () => {
   await TestDatabase.setupDatabase()
