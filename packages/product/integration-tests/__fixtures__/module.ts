3f3a84262ce9cbd911923278a54e301fbe9a4634
Adrien de Peretti <adrien.deperetti@gmail.com>
2023-08-08 17:10:34 +0200

    feat(types, product, utils, medusa): Include shared connection for modules (#4626)

--- packages/product/integration-tests/__fixtures__/module.ts
+++ packages/product/integration-tests/__fixtures__/module.ts
@@ -1,3 +1,4 @@
+import { Context } from "@medusajs/types"
 import { DALUtils } from "@medusajs/utils"
 
 class CustomRepository extends DALUtils.MikroOrmBaseRepository {
@@ -5,12 +6,44 @@ class CustomRepository extends DALUtils.MikroOrmBaseRepository {
     // @ts-ignore
     super(...arguments)
   }
-}
 
-CustomRepository.prototype.find = jest.fn().mockImplementation(async () => [])
-CustomRepository.prototype.findAndCount = jest
-  .fn()
-  .mockImplementation(async () => [])
+  find = jest.fn().mockImplementation(async () => [])
+  findAndCount = jest.fn().mockImplementation(async () => [])
+  create = jest.fn()
+  update = jest.fn()
+  delete = jest.fn()
+  softDelete = jest.fn()
+  restore = jest.fn()
+
+  async transaction<TManager = unknown>(
+    task: (transactionManager: TManager) => Promise<any>,
+    options: {
+      isolationLevel?: string
+      enableNestedTransactions?: boolean
+      transaction?: TManager
+    } = {}
+  ): Promise<any> {
+    return super.transaction(task, options)
+  }
+
+  getActiveManager<TManager = unknown>({
+    transactionManager,
+    manager,
+  }: Context = {}): TManager {
+    return super.getActiveManager({ transactionManager, manager })
+  }
+
+  getFreshManager<TManager = unknown>(): TManager {
+    return super.getFreshManager()
+  }
+
+  async serialize<TOutput extends object | object[]>(
+    data: any,
+    options?: any
+  ): Promise<TOutput> {
+    return super.serialize(data, options)
+  }
+}
 
 export class ProductRepository extends CustomRepository {}
 export class ProductTagRepository extends CustomRepository {}
