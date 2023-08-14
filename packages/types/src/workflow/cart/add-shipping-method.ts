1e7dbf8367dcd6bf890f1d3b316d0e11e03cfca8
olivermrbl <oliver@mrbltech.com>
2023-08-14 13:08:22 +0200

    remove unused types

--- packages/types/src/workflow/cart/add-shipping-method.ts
+++ packages/types/src/workflow/cart/add-shipping-method.ts
@@ -3,38 +3,3 @@ export interface AddShippingMethodToCartWorkflowDTO {
   option_id: string
   data?: Record<string, unknown>
 }
-
-export interface ValidateShippingOptionForCartDTO {
-  option: any // shipping option
-  cart: any // cart
-  data: Record<string, unknown>
-}
-
-export type EnsureCorrectLineItemShippingDTO = {
-  items: any // line items
-  methods: any // shipping methods to validate against
-}
-
-export type GetShippingOptionPriceDTO = {
-  shippingOption: any // Shipping Option
-  shippingOptionData: any // Validated shipping option data
-  cart: any // cart
-}
-
-export type CreateShippingMethodsDTO = {
-  option: any // Shipping Option
-  config: any // Shipping method config
-
-  cart?: any // cart
-  order?: any // order
-
-  data?: any
-  shipping_option_id?: string
-  price?: number
-  return_id?: string
-  swap_id?: string
-  cart_id?: string
-  order_id?: string
-  draft_order_id?: string
-  claim_order_id?: string
-}
