7d7376d7121c73a1a79fb705edb02d9cb520bfec
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-08-13 20:32:51 -0500

    add cache-control option, fix delete function, update to sdk v3

--- packages/medusa-file-s3/src/services/s3.ts
+++ packages/medusa-file-s3/src/services/s3.ts
@@ -1,52 +1,61 @@
 import fs from "fs"
-import aws from "aws-sdk"
+import stream from "stream"
 import { parse } from "path"
+import type { S3ClientConfigType, GetObjectCommandOutput } from "@aws-sdk/client-s3"
+import { Upload } from "@aws-sdk/lib-storage"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import { 
+  S3Client, 
+  PutObjectCommand, 
+  DeleteObjectCommand, 
+  GetObjectCommand 
+} from "@aws-sdk/client-s3"
 import {
   AbstractFileService,
   DeleteFileType,
   FileServiceUploadResult,
   GetUploadedFileType,
   IFileService,
+  Logger,
   UploadStreamDescriptorType,
 } from "@medusajs/medusa"
-import stream from "stream"
-import { PutObjectRequest } from "aws-sdk/clients/s3"
-import { ClientConfiguration } from "aws-sdk/clients/s3"
 
 class S3Service extends AbstractFileService implements IFileService {
-  protected bucket_: string
   protected s3Url_: string
+  protected region_: string
+  protected bucket_: string
   protected accessKeyId_: string
   protected secretAccessKey_: string
-  protected region_: string
-  protected endpoint_: string
   protected awsConfigObject_: any
-  protected downloadFileDuration_: string
+  protected cacheControl_: string
+  protected downloadFileDuration_: number
+  protected client: S3Client
+  protected logger_: Logger
 
-  constructor({}, options) {
+  constructor({logger}, options) {
     super({}, options)
 
-    this.bucket_ = options.bucket
     this.s3Url_ = options.s3_url
+    this.region_ = options.region
+    this.bucket_ = options.bucket
     this.accessKeyId_ = options.access_key_id
     this.secretAccessKey_ = options.secret_access_key
-    this.region_ = options.region
-    this.endpoint_ = options.endpoint
-    this.downloadFileDuration_ = options.download_file_duration
     this.awsConfigObject_ = options.aws_config_object ?? {}
-  }
+    this.cacheControl_ = options.cache_control ?? "max-age=31536000"
+    this.downloadFileDuration_ = options.download_file_duration ?? 3600
+    this.logger_ = logger
 
-  protected getClient(overwriteConfig: Partial<ClientConfiguration> = {}) {
-    const config: ClientConfiguration = {
-      accessKeyId: this.accessKeyId_,
-      secretAccessKey: this.secretAccessKey_,
+    const config: S3ClientConfigType = {
+      credentials: {
+        accessKeyId: this.accessKeyId_,
+        secretAccessKey: this.secretAccessKey_,
+      },
       region: this.region_,
-      endpoint: this.endpoint_,
       ...this.awsConfigObject_,
-      ...overwriteConfig,
+      signatureVersion: 'v4'
     }
-
-    return new aws.S3(config)
+  
+    this.client = new S3Client(config)
   }
 
   async upload(file: Express.Multer.File): Promise<FileServiceUploadResult> {
@@ -64,93 +73,85 @@ class S3Service extends AbstractFileService implements IFileService {
       acl: undefined,
     }
   ) {
-    const client = this.getClient()
-
     const parsedFilename = parse(file.originalname)
-
     const fileKey = `${parsedFilename.name}-${Date.now()}${parsedFilename.ext}`
 
-    const params = {
+    const command = new PutObjectCommand({
       ACL: options.acl ?? (options.isProtected ? "private" : "public-read"),
       Bucket: this.bucket_,
-      Body: fs.createReadStream(file.path),
       Key: fileKey,
+      Body: fs.createReadStream(file.path),
       ContentType: file.mimetype,
-    }
+      CacheControl: this.cacheControl_
+    })
 
-    const result = await client.upload(params).promise()
-
-    return {
-      url: result.Location,
-      key: result.Key,
+    try {
+      await this.client.send(command)
+      return {
+        url: `${this.s3Url_}/${fileKey}`,
+        key: fileKey,
+      }
+    } catch (e) {
+      this.logger_.error(e)
+      throw(e) // rejects the promise
     }
   }
 
   async delete(file: DeleteFileType): Promise<void> {
-    const client = this.getClient()
-
-    const params = {
+    const command = new DeleteObjectCommand({
       Bucket: this.bucket_,
-      Key: `${file}`,
-    }
-
-    return new Promise((resolve, reject) => {
-      client.deleteObject(params, (err, data) => {
-        if (err) {
-          reject(err)
-          return
-        }
-        resolve()
-      })
+      Key: `${file.file_key}`,
     })
+
+    try {
+      const response = await this.client.send(command)
+      console.log(response)
+    } catch (e) {
+      this.logger_.error(e)
+    }
   }
 
   async getUploadStreamDescriptor(fileData: UploadStreamDescriptorType) {
-    const client = this.getClient()
     const pass = new stream.PassThrough()
-
     const fileKey = `${fileData.name}.${fileData.ext}`
-    const params: PutObjectRequest = {
-      ACL: fileData.acl ?? "private",
-      Bucket: this.bucket_,
-      Body: pass,
-      Key: fileKey,
-      ContentType: fileData.contentType as string,
-    }
+
+    const uploadJob = new Upload({
+      client: this.client,
+      params: {
+        ACL: fileData.acl ?? "private",
+        Bucket: this.bucket_,
+        Key: fileKey,
+        Body: pass,
+        ContentType: fileData.contentType as string
+      }
+    })
 
     return {
       writeStream: pass,
-      promise: client.upload(params).promise(),
+      promise: uploadJob.done(),
       url: `${this.s3Url_}/${fileKey}`,
-      fileKey,
+      fileKey
     }
   }
 
-  async getDownloadStream(
-    fileData: GetUploadedFileType
-  ): Promise<NodeJS.ReadableStream> {
-    const client = this.getClient()
-
-    const params = {
+  async getDownloadStream(fileData: GetUploadedFileType): Promise<NodeJS.ReadableStream> {
+    const command = new GetObjectCommand({
       Bucket: this.bucket_,
       Key: `${fileData.fileKey}`,
-    }
+    })
 
-    return await client.getObject(params).createReadStream()
+    const response: GetObjectCommandOutput = await this.client.send(command)
+
+    return response.Body as NodeJS.ReadableStream
   }
 
-  async getPresignedDownloadUrl(
-    fileData: GetUploadedFileType
-  ): Promise<string> {
-    const client = this.getClient({ signatureVersion: "v4" })
-
-    const params = {
+  async getPresignedDownloadUrl(fileData: GetUploadedFileType): Promise<string> {
+    const command = new GetObjectCommand({
       Bucket: this.bucket_,
-      Key: `${fileData.fileKey}`,
-      Expires: this.downloadFileDuration_,
-    }
+      Key: `${fileData.fileKey}`
+    })
 
-    return await client.getSignedUrlPromise("getObject", params)
+    return await getSignedUrl(this.client, command, { expiresIn: this.downloadFileDuration_ })
   }
 }
 
