a984fc154b0219414edf5b4579590b74953098d3
Riqwan Thamir <rmthamir@gmail.com>
2023-08-28 10:37:40 +0200

    chore: extract common utils

--- packages/pricing/src/loaders/container.ts
+++ packages/pricing/src/loaders/container.ts
@@ -1,10 +1,10 @@
-import { Constructor, DAL, ModulesSdkTypes } from "@medusajs/types"
-import * as DefaultRepositories from "@repositories"
+import { ModulesSdkTypes } from "@medusajs/types"
+import * as defaultRepositories from "@repositories"
 import { BaseRepository, CurrencyRepository } from "@repositories"
 import { CurrencyService } from "@services"
 
 import { LoaderOptions } from "@medusajs/modules-sdk"
-import { lowerCaseFirst } from "@medusajs/utils"
+import { loadCustomRepositories } from "@medusajs/utils"
 import { asClass } from "awilix"
 
 export default async ({
@@ -23,7 +23,11 @@ export default async ({
   })
 
   if (customRepositories) {
-    loadCustomRepositories({ customRepositories, container })
+    loadCustomRepositories({
+      defaultRepositories,
+      customRepositories,
+      container,
+    })
   } else {
     loadDefaultRepositories({ container })
   }
@@ -33,33 +37,5 @@ function loadDefaultRepositories({ container }) {
   container.register({
     baseRepository: asClass(BaseRepository).singleton(),
     currencyRepository: asClass(CurrencyRepository).singleton(),
-  })
-}
-
-/**
- * Load the repositories from the custom repositories object. If a repository is not
- * present in the custom repositories object, the default repository will be used.
- *
- * @param customRepositories
- * @param container
- */
-function loadCustomRepositories({ customRepositories, container }) {
-  const customRepositoriesMap = new Map(Object.entries(customRepositories))
-
-  Object.entries(DefaultRepositories).forEach(([key, DefaultRepository]) => {
-    let finalRepository = customRepositoriesMap.get(key)
-
-    if (
-      !finalRepository ||
-      !(finalRepository as Constructor<DAL.RepositoryService>).prototype.find
-    ) {
-      finalRepository = DefaultRepository
-    }
-
-    container.register({
-      [lowerCaseFirst(key)]: asClass(
-        finalRepository as Constructor<DAL.RepositoryService>
-      ).singleton(),
-    })
   })
 }
