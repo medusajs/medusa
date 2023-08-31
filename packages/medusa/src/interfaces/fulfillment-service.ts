96f31d3116d0e05ca996808c6012fab62c7e9508
olivermrbl <oliver@mrbltech.com>
2023-08-31 12:37:19 +0200

    add data types

--- packages/medusa/src/interfaces/fulfillment-service.ts
+++ packages/medusa/src/interfaces/fulfillment-service.ts
@@ -2,6 +2,14 @@ import { MedusaContainer } from "@medusajs/types"
 import { Cart, Fulfillment, LineItem, Order } from "../models"
 import { CreateReturnType } from "../types/fulfillment-provider"
 
+type FulfillmentProviderData = Record<string, unknown>
+type ShippingOptionData = Record<string, unknown>
+type ShippingMethodData = Record<string, unknown>
+
+/**
+ * Fulfillment Provider interface
+ * Fullfillment provider plugin services should extend the AbstractFulfillmentService from this file
+ */
 export interface FulfillmentService {
   /**
    * Return a unique identifier to retrieve the fulfillment plugin provider
@@ -14,8 +22,7 @@ export interface FulfillmentService {
    * and it is here the distinction between different shipping options are
    * enforced. For example, a fulfillment provider may offer Standard Shipping
    * and Express Shipping as fulfillment options, it is up to the store operator
-   * to create shipping options in Medusa that can be chosen between by the
-   * customer.
+   * to create shipping options in Medusa that are offered to the customer.
    */
   getFulfillmentOptions(): Promise<any[]>
 
@@ -25,15 +32,12 @@ export interface FulfillmentService {
    * data about the shipment such as an id of a drop point. It is up to the
    * fulfillment provider to enforce that the correct data is being sent
    * through.
-   * @param {object} optionData - the data to validate
-   * @param {object} data - the data to validate
-   * @param {object | undefined} cart - the cart to which the shipping method will be applied
-   * @return {object} the data to populate `cart.shipping_methods.$.data` this
+   * @return the data to populate `cart.shipping_methods.$.data` this
    *    is usually important for future actions like generating shipping labels
    */
   validateFulfillmentData(
-    optionData: Record<string, unknown>,
-    data: Record<string, unknown>,
+    optionData: ShippingOptionData,
+    data: FulfillmentProviderData,
     cart: Cart
   ): Promise<Record<string, unknown>>
 
@@ -41,29 +45,36 @@ export interface FulfillmentService {
    * Called before a shipping option is created in Admin. Use this to ensure
    * that a fulfillment option does in fact exist.
    */
-  validateOption(data: Record<string, unknown>): Promise<boolean>
+  validateOption(data: ShippingOptionData): Promise<boolean>
 
-  canCalculate(data): Promise<boolean>
+  /**
+   * Used to determine if a shipping option can have a calculated price
+   */
+  canCalculate(data: ShippingOptionData): Promise<boolean>
 
   /**
    * Used to calculate a price for a given shipping option.
    */
-  calculatePrice(optionData, data, cart): Promise<number>
-
-  createFulfillment(
-    data: Record<string, unknown>,
-    items: LineItem,
-    order: Order,
-    fulfillment: Omit<Fulfillment, "beforeInsert">
-  )
-
-  cancelFulfillment(fulfillment: Record<string, unknown>): Promise<any>
+  calculatePrice(
+    optionData: ShippingOptionData,
+    data: FulfillmentProviderData,
+    cart: Cart
+  ): Promise<number>
 
   /**
-   * Used to retrieve documents associated with a fulfillment.
-   * Will default to returning no documents.
+   * Create a fulfillment using data from shipping method, line items, and fulfillment. All from the order.
    */
-  getFulfillmentDocuments(data: Record<string, unknown>): Promise<any>
+  createFulfillment(
+    data: ShippingMethodData,
+    items: LineItem,
+    order: Order,
+    fulfillment: Fulfillment
+  ): Promise<any>
+
+  /**
+   * Cancel a fulfillment using data from the fulfillment
+   */
+  cancelFulfillment(fulfillmentData: FulfillmentProviderData): Promise<any>
 
   /**
    * Used to create a return order. Should return the data necessary for future
@@ -71,6 +82,11 @@ export interface FulfillmentService {
    * documents attached to the return.
    */
   createReturn(returnOrder: CreateReturnType): Promise<Record<string, unknown>>
+
+  /**
+   * Used to retrieve documents associated with a fulfillment.
+   */
+  getFulfillmentDocuments(data: FulfillmentProviderData): Promise<any>
 
   /**
    * Used to retrieve documents related to a return order.
@@ -83,17 +99,11 @@ export interface FulfillmentService {
   getShipmentDocuments(data: Record<string, unknown>): Promise<any>
 
   retrieveDocuments(
-    fulfillmentData: Record<string, unknown>,
+    fulfillmentData: FulfillmentProviderData,
     documentType: "invoice" | "label"
   ): Promise<any>
 }
 
-/**
- * The interface that all fulfillment services must inherit from. The intercace
- * provides the necessary methods for creating, authorizing and managing
- * fulfillment orders.
- * @interface
- */
 export abstract class AbstractFulfillmentService implements FulfillmentService {
   protected constructor(
     protected readonly container: MedusaContainer,
@@ -115,34 +125,35 @@ export abstract class AbstractFulfillmentService implements FulfillmentService {
   abstract getFulfillmentOptions(): Promise<any[]>
 
   abstract validateFulfillmentData(
-    optionData: Record<string, unknown>,
-    data: Record<string, unknown>,
+    optionData: ShippingOptionData,
+    data: FulfillmentProviderData,
     cart: Cart
   ): Promise<Record<string, unknown>>
 
-  abstract validateOption(data: Record<string, unknown>): Promise<boolean>
+  abstract validateOption(data: ShippingOptionData): Promise<boolean>
 
-  abstract canCalculate(data): Promise<boolean>
+  abstract canCalculate(data: ShippingOptionData): Promise<boolean>
 
-  /**
-   * Used to calculate a price for a given shipping option.
-   */
-  abstract calculatePrice(optionData, data, cart): Promise<number>
+  abstract calculatePrice(
+    optionData: ShippingOptionData,
+    data: FulfillmentProviderData,
+    cart: Cart
+  ): Promise<number>
 
   abstract createFulfillment(
-    data: Record<string, unknown>,
+    data: ShippingMethodData,
     items: LineItem,
     order: Order,
-    fulfillment: Omit<Fulfillment, "beforeInsert">
+    fulfillment: Fulfillment
   )
 
-  abstract cancelFulfillment(fulfillment: Record<string, unknown>): Promise<any>
-
-  abstract getFulfillmentDocuments(data: Record<string, unknown>): Promise<any>
+  abstract cancelFulfillment(fulfillment: FulfillmentProviderData): Promise<any>
 
   abstract createReturn(
     returnOrder: CreateReturnType
   ): Promise<Record<string, unknown>>
+
+  abstract getFulfillmentDocuments(data: FulfillmentProviderData): Promise<any>
 
   abstract getReturnDocuments(data: Record<string, unknown>): Promise<any>
 
