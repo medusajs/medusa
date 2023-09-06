c5965f796d7c2af071d47adc9753649288821096
medbenzekri <medbenzekri219@gmail.com>
2023-09-06 02:34:36 +0100

    arabic translation added

--- packages/admin-ui/ui/i18next-parser.config.js
+++ packages/admin-ui/ui/i18next-parser.config.js
@@ -42,7 +42,7 @@ module.exports = {
   lineEnding: "auto",
   // Control the line ending. See options at https://github.com/ryanve/eol
 
-  locales: ["en", "en-US", "fa-IR"],
+  locales: ["en", "en-US", "fa-IR","ar"],
   // An array of the locales in your applications
 
   namespaceSeparator: false,
--- packages/admin-ui/ui/src/i18n/index.ts
+++ packages/admin-ui/ui/src/i18n/index.ts
@@ -4,6 +4,8 @@ import { initReactI18next } from "react-i18next"
 import Backend from "i18next-http-backend"
 import LanguageDetector from "i18next-browser-languagedetector"
 
+
+
 void i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -12,7 +14,7 @@ void i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     returnNull: false,
-    fallbackLng: ["fa-IR", "en-US"],
+    fallbackLng: ["fa-IR", "en-US","ar"],
     debug: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
