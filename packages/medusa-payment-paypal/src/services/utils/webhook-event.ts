d9c6cf409f604702a6fb390611c2d2f8fe53e628
Martin Ghiotti <martinghiotti@yahoo.com.ar>
2023-09-06 11:39:42 -0300

    feat: add support for PAYMENT.AUTHORIZATION.CREATED event

--- packages/medusa-payment-paypal/src/services/utils/webhook-event.ts
+++ packages/medusa-payment-paypal/src/services/utils/webhook-event.ts
@@ -5,97 +5,32 @@ import { PaypalWebhookEvents } from "../../core";
 class WebhookEventHandler {
   handle(event_type, req, res) {
     switch(event_type) {
+      case PaypalWebhookEvents.PAYMENT_AUTHORIZATION_CREATED:
+        return this.handlePaymentAuthorizationCreated(req, res);
       case PaypalWebhookEvents.PAYMENT_CAPTURE_COMPLETED:
         return this.handlePaymentCaptureCompleted(req, res);
       default:
-        res.sendStatus(501)
         console.log(`Unhandled event type: ${event_type}`);
+        res.sendStatus(501)
+        return res
     }
-    return res
+  }
+
+  async handlePaymentAuthorizationCreated(req, res) {
+    return this.handlePayment(req, res, false)
   }
 
   async handlePaymentCaptureCompleted(req, res) {
-    const { resource_type, resource } = req.body
-    const { final_capture, status, custom_id } = resource
-    const shouldCapturePayment =
-      final_capture &&
-      resource_type === "capture" &&
-      status === "COMPLETED" &&
-      custom_id
-    if (!shouldCapturePayment) {
-      res.sendStatus(304)
-      return res
-    }
-  
-    const manager = req.scope.resolve("manager")
+    return this.handlePayment(req, res, true)
+  }
 
-    async function authorizeCart(req, res, cartId) {
-      const cartService: CartService = req.scope.resolve("cartService")
-      const swapService: SwapService = req.scope.resolve("swapService")
-      const orderService: OrderService = req.scope.resolve("orderService")
-  
-      await manager.transaction(async (m) => {
-        const cart = await cartService.withTransaction(m).retrieve(cartId)
-  
-        switch (cart.type) {
-          case "swap": {
-            const swap = await swapService
-              .withTransaction(m)
-              .retrieveByCartId(cartId)
-              .catch((_) => undefined)
-  
-            if (swap && swap.confirmed_at === null) {
-              await cartService
-                .withTransaction(m)
-                .setPaymentSession(cartId, "paypal")
-              await cartService.withTransaction(m).authorizePayment(cartId)
-              await swapService.withTransaction(m).registerCartCompletion(swap.id)
-            }
-            break
-          }
-  
-          default: {
-            let order = await orderService
-              .withTransaction(m)
-              .retrieveByCartId(cartId)
-              .catch((_) => undefined)
-            if (!order) {
-              await cartService.withTransaction(m).authorizePayment(cartId)
-              await orderService.withTransaction(m).createFromCart(cartId)
-              order = await orderService
-                .withTransaction(m)
-                .retrieveByCartId(cartId)
-                .catch((_) => undefined)
-            }
-            if (!order?.id) {
-              res.sendStatus(404)
-            } else {
-              await cartService
-                .withTransaction(m)
-                .setPaymentSession(cartId, "paypal")
-              await orderService.withTransaction(m).capturePayment(order.id)
-            }
-            break
-          }
-        }
-      })
-    }
-  
-    async function authorizePaymentCollection(req, id) {
-      const paymentCollectionService = req.scope.resolve(
-        "paymentCollectionService"
-      )
-  
-      await manager.transaction(async (manager) => {
-        await paymentCollectionService.withTransaction(manager).authorize(id)
-      })
-    }
-  
+  async handlePayment(req, res, capture) {
+    const { custom_id } = req.body.resource  
     try {
       if (custom_id.startsWith("paycol")) {
-        await authorizePaymentCollection(req, custom_id)
+        await this.authorizePaymentCollection(req, custom_id)
       } else {
-        await authorizeCart(req, res, custom_id)
+        await this.authorizeCart(req, res, custom_id, capture)
       }
   
       res.sendStatus(200)
@@ -105,6 +40,73 @@ class WebhookEventHandler {
     }
     return res
   }
+
+  async authorizeCart(req, res, cartId, capture) {
+    const manager = req.scope.resolve("manager")
+    const cartService: CartService = req.scope.resolve("cartService")
+    const swapService: SwapService = req.scope.resolve("swapService")
+    const orderService: OrderService = req.scope.resolve("orderService")
+
+    await manager.transaction(async (m) => {
+      const cart = await cartService.withTransaction(m).retrieve(cartId)
+
+      switch (cart.type) {
+        case "swap": {
+          const swap = await swapService
+            .withTransaction(m)
+            .retrieveByCartId(cartId)
+            .catch((_) => undefined)
+
+          if (swap && swap.confirmed_at === null) {
+            await cartService
+              .withTransaction(m)
+              .setPaymentSession(cartId, "paypal")
+            await cartService.withTransaction(m).authorizePayment(cartId)
+            await swapService.withTransaction(m).registerCartCompletion(swap.id)
+          }
+          break
+        }
+
+        default: {
+          let order = await orderService
+            .withTransaction(m)
+            .retrieveByCartId(cartId)
+            .catch((_) => undefined)
+          if (!order) {
+            await orderService.withTransaction(m).createFromCart(cartId)
+            order = await orderService
+              .withTransaction(m)
+              .retrieveByCartId(cartId)
+              .catch((_) => undefined)
+          }
+          if (!order?.id) {
+            res.sendStatus(404)
+            return res
+          } 
+          await cartService
+            .withTransaction(m)
+            .setPaymentSession(cartId, "paypal")
+          await cartService.withTransaction(m).authorizePayment(cartId)
+          if (capture) {
+            await orderService.withTransaction(m).capturePayment(order.id)
+          }
+          break
+        }
+      }
+    })
+  }
+
+  async authorizePaymentCollection(req, id) {
+    const manager = req.scope.resolve("manager")
+    const paymentCollectionService = req.scope.resolve(
+      "paymentCollectionService"
+    )
+
+    await manager.transaction(async (manager) => {
+      await paymentCollectionService.withTransaction(manager).authorize(id)
+    })
+  }
+
 }
 
 export const webhookEventHandler = new WebhookEventHandler();
