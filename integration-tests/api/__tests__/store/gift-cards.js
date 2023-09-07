3755e954f9674472782abdf474f6eef68cf53f67
Morten Engel <me@alpha-solutions.dk>
2023-09-07 11:18:58 +0200

    fix/4892 added unit tests

--- integration-tests/api/__tests__/store/gift-cards.js
+++ integration-tests/api/__tests__/store/gift-cards.js
@@ -4,6 +4,12 @@ const { Region, GiftCard } = require("@medusajs/medusa")
 const setupServer = require("../../../environment-helpers/setup-server")
 const { useApi } = require("../../../environment-helpers/use-api")
 const { initDb, useDb } = require("../../../environment-helpers/use-db")
+const {
+  simpleProductFactory,
+  simpleCartFactory,
+  simpleRegionFactory,
+  simpleGiftCardFactory,
+} = require("../../../factories")
 
 jest.setTimeout(30000)
 
@@ -58,6 +64,140 @@ describe("/store/gift-cards", () => {
         balance: 120,
         region: expect.any(Object),
       })
+    })
+  })
+
+  describe("gift card transactions", () => {
+    afterEach(async () => {
+      const db = useDb()
+      await db.teardown()
+    })
+
+    it("only creates transactions for gift cards used", async () => {
+      const api = useApi()
+
+      const region = await simpleRegionFactory(dbConnection, {
+        currency_code: "usd",
+      })
+      const cart = await simpleCartFactory(dbConnection, {
+        region: region.id,
+      })
+      const product = await simpleProductFactory(dbConnection)
+      const giftCard1 = await simpleGiftCardFactory(dbConnection, {
+        code: "GC_1",
+        region_id: region.id,
+        balance: 50000,
+        value: 50000,
+      })
+      const giftCard2 = await simpleGiftCardFactory(dbConnection, {
+        code: "GC_2",
+        region_id: region.id,
+        balance: 50000,
+        value: 50000,
+      })
+
+      await api.post(`/store/carts/${cart.id}/line-items`, {
+        variant_id: product.variants[0].id,
+        quantity: 1,
+      })
+
+      await api.post(`/store/carts/${cart.id}`, {
+        email: "some@customer.com",
+        gift_cards: [{ code: "GC_1" }, { code: "GC_2" }],
+      })
+
+      const response = await api.post(`/store/carts/${cart.id}/complete`)
+      expect(response.status).toEqual(200)
+      const orderId = response.data.data.id
+
+      const transaction1 = await dbConnection.manager.find(
+        GiftCardTransaction,
+        {
+          order_id: orderId,
+        }
+      )
+
+      expect(transaction1.length).toEqual(1)
+      expect(transaction1[0].amount).toEqual(100)
+    })
+  })
+
+  describe("gift card transaction selections", () => {
+    afterEach(async () => {
+      const db = useDb()
+      await db.teardown()
+    })
+
+    it("apply gift cards in ordered fashion, first by end_date, then by remaining balance", async () => {
+      const api = useApi()
+
+      const region = await simpleRegionFactory(dbConnection, {
+        currency_code: "usd",
+      })
+      const cart = await simpleCartFactory(dbConnection, {
+        region: region.id,
+      })
+      const product = await simpleProductFactory(dbConnection)
+      const ends_first = new Date(2050, 1, 1)
+      const ends_later = new Date(2050, 1, 2)
+      const giftCard1 = await simpleGiftCardFactory(dbConnection, {
+        id: "GC_1",
+        code: "GC_1",
+        region_id: region.id,
+        balance: 100,
+        value: 100,
+        ends_at: ends_later
+      })
+      const giftCard2 = await simpleGiftCardFactory(dbConnection, {
+        id: "GC_2",
+        code: "GC_2",
+        region_id: region.id,
+        balance: 200,
+        value: 200,
+        ends_at: ends_first
+      })
+      const giftCard3 = await simpleGiftCardFactory(dbConnection, {
+        id: "GC_3",
+        code: "GC_3",
+        region_id: region.id,
+        balance: 100,
+        value: 100,
+        ends_at: ends_first
+      })
+      const giftCard4 = await simpleGiftCardFactory(dbConnection, {
+        id: "GC_4",
+        code: "GC_4",
+        region_id: region.id,
+        balance: 100,
+        value: 100,
+      })
+
+      await api.post(`/store/carts/${cart.id}/line-items`, {
+        variant_id: product.variants[0].id,
+        quantity: 5,
+      })
+
+      await api.post(`/store/carts/${cart.id}`, {
+        email: "some@customer.com",
+        gift_cards: [{ code: "GC_1" }, { code: "GC_2" }, { code: "GC_3" }, { code: "GC_4" }],
+      })
+
+      const response = await api.post(`/store/carts/${cart.id}/complete`)
+      expect(response.status).toEqual(200)
+      const orderId = response.data.data.id
+
+      const transaction1 = await dbConnection.manager.find(
+        GiftCardTransaction,
+        {
+          order_id: orderId,
+        }
+      )
+
+      expect(transaction1.length).toEqual(4)
+      expect(transaction1[0].gift_card_id).toEqual(giftCard3.id)
+      expect(transaction1[0].gift_card_id).toEqual(giftCard2.id)
+      expect(transaction1[0].gift_card_id).toEqual(giftCard1.id)
+      expect(transaction1[0].gift_card_id).toEqual(giftCard4.id)
     })
   })
 })
