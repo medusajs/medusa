4b6f64a7b4b7a6481fe48c7f593f81dec0b127c7
Philip Korsholm <philip.korsholm@hotmail.com>
2023-09-08 15:21:09 +0200

    add default clause for price list

--- packages/pricing/src/migrations/Migration20230831085443.ts
+++ packages/pricing/src/migrations/Migration20230831085443.ts
@@ -5,7 +5,7 @@ export class Migration20230831085443 extends Migration {
   async up(): Promise<void> {
     this.addSql('create table "currency" ("code" text not null, "symbol" text not null, "symbol_native" text not null, "name" text not null, constraint "currency_pkey" primary key ("code"));');
 
-    this.addSql('create table "price_list" ("id" text not null, "name" text not null, "description" text not null, "type" text check ("type" in (\'sale\', \'override\')) not null default \'sale\', "status" text check ("status" in (\'active\', \'draft\')) not null default \'draft\', "starts_at" timestamptz null, "ends_at" timestamptz null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "deleted_at" timestamptz null, constraint "price_list_pkey" primary key ("id"));');
+    this.addSql('create table "price_list" ("id" text not null, "name" text not null, "description" text not null, "type" text check ("type" in (\'sale\', \'override\')) not null default \'sale\', "status" text check ("status" in (\'active\', \'draft\')) not null default \'draft\', "starts_at" timestamptz null, "ends_at" timestamptz null, "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, constraint "price_list_pkey" primary key ("id"));');
     this.addSql('create index "IDX_product_deleted_at" on "price_list" ("deleted_at");');
 
     this.addSql('create table "money_amount" ("id" text not null, "currency_code" text null, "amount" numeric null, "min_quantity" numeric null, "max_quantity" numeric null, "price_list_id" text null, constraint "money_amount_pkey" primary key ("id"));');
