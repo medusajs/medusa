f9bb529180432cd5dd7ac07636a7d216ce5a42b5
Riqwan Thamir <rmthamir@gmail.com>
2023-09-08 11:01:23 +0200

    chore: change price set link definition file name

--- packages/link-modules/src/definitions/index.ts
+++ packages/link-modules/src/definitions/index.ts
@@ -1,3 +1,3 @@
 export * from "./inventory-level-stock-location"
 export * from "./product-variant-inventory-item"
-export * from "./product-variant-money-amount"
+export * from "./product-variant-price-set"
