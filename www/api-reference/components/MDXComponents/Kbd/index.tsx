0174acbe8beab3f9f1052c660563ab0bc3b8eedc
Shahed nasser <shahednasser@gmail.com>
2023-09-08 19:04:36 +0300

    additional fixes

--- www/api-reference/components/MDXComponents/Kbd/index.tsx
+++ www/api-reference/components/MDXComponents/Kbd/index.tsx
@@ -6,12 +6,12 @@ const Kbd = ({ children, className, ...props }: KbdProps) => {
   return (
     <kbd
       className={clsx(
-        "h-[22px] w-[22px] rounded-sm p-0",
+        "rounded-sm border-solid py-0 px-[6px]",
         "inline-flex items-center justify-center",
         "border-medusa-tag-neutral-border dark:border-medusa-tag-neutral-border-dark border",
         "bg-medusa-tag-neutral-bg dark:bg-medusa-tag-neutral-bg-dark",
         "text-medusa-tag-neutral-text dark:text-medusa-tag-neutral-text-dark",
-        "text-compact-x-small-plus",
+        "text-compact-x-small-plus font-monospace shadow-none",
         className
       )}
       {...props}
