c2651ad0fe307dfba79aabc524f8c8a76cbddab4
Geoffroy Empain <geoffroy@charlie-bravo.be>
2023-09-11 11:12:07 +0000

    chore: disable translation backup on sync

--- packages/admin-ui/ui/i18next-parser.config.js
+++ packages/admin-ui/ui/i18next-parser.config.js
@@ -2,7 +2,7 @@ module.exports = {
   contextSeparator: "_",
   // Key separator used in your translation keys
 
-  createOldCatalogs: true,
+  createOldCatalogs: false,
   // Save the \_old files
 
   defaultNamespace: "translation",
