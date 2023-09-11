e1d11e656ee5d0e5421615c8395700f8c845227d
adrien2p <adrien.deperetti@gmail.com>
2023-09-11 12:04:44 +0200

    fix typo

--- packages/types/src/dal/entity.ts
+++ packages/types/src/dal/entity.ts
@@ -1,2 +1,2 @@
-export type EntityDateColumns = "create_at" | "update_at"
-export type SoftDeletableEntityDateColumns = "delete_at" | EntityDateColumns
+export type EntityDateColumns = "created_at" | "updated_at"
+export type SoftDeletableEntityDateColumns = "deleted_at" | EntityDateColumns
