776a85b54ec05e4c93f2edc4eb3bd896a84ddc64
Stan Bondar <bondar.stan@gmail.com>
2023-09-12 16:27:44 +0300

    test

--- packages/admin/src/lib/index.ts
+++ packages/admin/src/lib/index.ts
@@ -1,4 +1,4 @@
-import { withCustomWebpackConfig } from "@medusajs/admin-ui"
+import { withCustomWebpackConfig } from "uc-custon-medusajs-admin-ui"
 export { withCustomWebpackConfig }
 
 export * from "../types"
--- packages/admin/src/types/routes.ts
+++ packages/admin/src/types/routes.ts
@@ -1,3 +1,3 @@
-import type { RouteConfig, RouteProps } from "@medusajs/admin-ui"
+import type { RouteConfig, RouteProps } from "uc-custon-medusajs-admin-ui"
 
 export { RouteConfig, RouteProps }
