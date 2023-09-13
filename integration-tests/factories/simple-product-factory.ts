36ec8ee8e833df141f125f4fcdbc21e07162c793
fPolic <mainacc.polic@gmail.com>
2023-09-13 16:42:31 +0200

    fix: handle creation in factory

--- integration-tests/factories/simple-product-factory.ts
+++ integration-tests/factories/simple-product-factory.ts
@@ -100,7 +100,7 @@ export const simpleProductFactory = async (
     metadata: data.metadata || null,
   } as unknown as Product
 
-  productToCreate.handle = data.handle || kebabCase(data.title)
+  productToCreate.handle = data.handle || kebabCase(productToCreate.title)
 
   if (typeof data.tags !== "undefined") {
     for (let i = 0; i < data.tags.length; i++) {
