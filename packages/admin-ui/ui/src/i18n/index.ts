96f8f1aed446200b4029f6bdc7aa1f6ded8c7cc9
olivermrbl <oliver@mrbltech.com>
2023-09-15 13:22:14 +0200

    chore(admin-ui): Add pt-BR to supported languages

--- packages/admin-ui/ui/src/i18n/index.ts
+++ packages/admin-ui/ui/src/i18n/index.ts
@@ -12,6 +12,10 @@ export const supportedLanguages = [
     locale: "fr",
     name: "Français",
   },
+  {
+    locale: "pt-BR",
+    name: "Português Brasileiro",
+  },
 ]
 
 const backendUrl = window.location.origin
