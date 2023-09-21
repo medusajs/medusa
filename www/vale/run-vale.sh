3c2e1c679d2812f654ddc8f8b91e501f2b6986a0
Shahed nasser <shahednasser@gmail.com>
2023-09-21 18:45:44 +0300

    added vale for all docs MD and MDX

--- www/vale/run-vale.sh
+++ www/vale/run-vale.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 list=""
-prefix="www/docs/"
-alertLevel=$1
+app="www/"
+path="../apps/$1/$2"
+alertLevel=$3
+exception="$path/$4"
 
 if [ ${#alertLevel} -eq 0 ]; then
   alertLevel="suggestion"
 fi
 
 # get directories in content other than reference
-for i in `find ../content -type d -maxdepth 1 -not -path '../content/references' -not -path '../content'`
+for i in `find $path -type d -maxdepth 1 -not -path $exception -not -path $path`
 do
   if [ ${#list} -gt 0 ]; then
     list+=' '
   fi
-  list+="$prefix${i#../}"
+  list+="$app${i#../}"
 done
 #get files in content (not nested)
-for i in `find ../content -type f -maxdepth 1 -not -path '../content/references'`
+for i in `find $path -type f -maxdepth 1 -not -path $exception`
 do
   if [ ${#list} -gt 0 ]; then
     list+=' '
   fi
-  list+="$prefix${i#../}"
+  list+="$app${i#../}"
 done
 
 # echo $list
-cd ../../..
+cd ../..
 exec vale $list --minAlertLevel $alertLevel
