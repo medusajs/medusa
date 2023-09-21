27d74cb399a1e317b4ddf039f5b8faf10ccc3cac
Shahed Nasser <shahednasser@gmail.com>
2023-09-21 21:41:18 +0300

    docs: fix error related to Key type (#5178)
    * docs: fix error related to Key type
    
    * fix styling issue

--- www/apps/ui/src/components/tabs.tsx
+++ www/apps/ui/src/components/tabs.tsx
@@ -3,6 +3,7 @@
 import { clx } from "@medusajs/ui"
 import * as Primitives from "@radix-ui/react-tabs"
 import * as React from "react"
+import { Key } from "@/types/props"
 
 const Tabs = Primitives.Root
 
@@ -10,13 +11,14 @@ const TabsList = React.forwardRef<
   React.ElementRef<typeof Primitives.List>,
   React.ComponentPropsWithoutRef<typeof Primitives.List>
   // eslint-disable-next-line react/prop-types
->(({ className, ...props }, ref) => (
+>(({ className, key, ...props }, ref) => (
   <Primitives.List
     ref={ref}
     className={clx(
       "inline-flex h-10 w-full items-end justify-start rounded-none bg-transparent p-0",
       className
     )}
+    key={key as Key}
     {...props}
   />
 ))
@@ -26,7 +28,7 @@ const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof Primitives.Trigger>,
   React.ComponentPropsWithoutRef<typeof Primitives.Trigger>
   // eslint-disable-next-line react/prop-types
->(({ className, ...props }, ref) => (
+>(({ className, key, ...props }, ref) => (
   <Primitives.Trigger
     ref={ref}
     className={clx(
@@ -35,6 +37,7 @@ const TabsTrigger = React.forwardRef<
       "dark:text-medusa-fg-subtle-dark dark:data-[state=active]:text-medusa-fg-base-dark",
       className
     )}
+    key={key as Key}
     {...props}
   />
 ))
@@ -44,13 +47,14 @@ const TabsContent = React.forwardRef<
   React.ElementRef<typeof Primitives.Content>,
   React.ComponentPropsWithoutRef<typeof Primitives.Content>
   // eslint-disable-next-line react/prop-types
->(({ className, ...props }, ref) => (
+>(({ className, key, ...props }, ref) => (
   <Primitives.Content
     ref={ref}
     className={clx(
       "w-full rounded-lg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 data-[state=active]:mt-4",
       className
     )}
+    key={key as Key}
     {...props}
   />
 ))
