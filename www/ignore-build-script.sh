4439c9772bee90eeab49bd375b5447e3ada4f804
Shahed nasser <shahednasser@gmail.com>
2023-09-22 14:59:04 +0300

    fixed ignore script

--- www/ignore-build-script.sh
+++ www/ignore-build-script.sh
@@ -4,11 +4,19 @@ echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_ENV" == "production" || "$VERCEL_GIT_COMMIT_REF" = "docs/"* ]] ; then
+  $(git diff HEAD^ HEAD --quiet .)
+  diffResult=$?
+  if [[ diffResult -eq 0 ]] ; then
+    # Don't build
+    echo "🛑 - Build cancelled: No diff"
+    exit 0;
+  fi
+
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
 else
   # Don't build
-  echo "🛑 - Build cancelled"
+  echo "🛑 - Build cancelled: Conditions don't match"
   exit 0;
 fi
