123b3787d5a8c80bf6e9e86faf28881dad15a469
Shahed Nasser <shahednasser@gmail.com>
2023-09-22 12:25:48 +0300

    docs: general fixes for docs workspace (#5184)

---
+++ www/ignore-build-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_ENV" == "production" || "$VERCEL_GIT_COMMIT_REF" = "docs/"* ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
