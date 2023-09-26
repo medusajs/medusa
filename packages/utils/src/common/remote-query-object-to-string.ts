4ac975057026277d4a345cedd2d66957a337fcc6
adrien2p <adrien.deperetti@gmail.com>
2023-09-26 15:17:58 +0200

    finalise

--- packages/utils/src/common/remote-query-object-to-string.ts
+++ packages/utils/src/common/remote-query-object-to-string.ts
@@ -1,115 +1,30 @@
-const defaultStoreProductRemoteQueryObject = {
-  fields: [
-    "id",
-    "title",
-    "subtitle",
-    "status",
-    "external_id",
-    "description",
-    "handle",
-    "is_giftcard",
-    "discountable",
-    "thumbnail",
-    "collection_id",
-    "type_id",
-    "weight",
-    "length",
-    "height",
-    "width",
-    "hs_code",
-    "origin_country",
-    "mid_code",
-    "material",
-    "created_at",
-    "updated_at",
-    "deleted_at",
-    "metadata",
-  ],
-  images: {
-    fields: ["id", "created_at", "updated_at", "deleted_at", "url", "metadata"],
-  },
-  tags: {
-    fields: ["id", "created_at", "updated_at", "deleted_at", "value"],
-  },
-
-  type: {
-    fields: ["id", "created_at", "updated_at", "deleted_at", "value"],
-  },
-
-  collection: {
-    fields: ["title", "handle", "id", "created_at", "updated_at", "deleted_at"],
-  },
-
-  options: {
-    fields: [
-      "id",
-      "created_at",
-      "updated_at",
-      "deleted_at",
-      "title",
-      "product_id",
-      "metadata",
-    ],
-    values: {
-      fields: [
-        "id",
-        "created_at",
-        "updated_at",
-        "deleted_at",
-        "value",
-        "option_id",
-        "variant_id",
-        "metadata",
-      ],
-    },
-  },
-
-  variants: {
-    fields: [
-      "id",
-      "created_at",
-      "updated_at",
-      "deleted_at",
-      "title",
-      "product_id",
-      "sku",
-      "barcode",
-      "ean",
-      "upc",
-      "variant_rank",
-      "inventory_quantity",
-      "allow_backorder",
-      "manage_inventory",
-      "hs_code",
-      "origin_country",
-      "mid_code",
-      "material",
-      "weight",
-      "length",
-      "height",
-      "width",
-      "metadata",
-    ],
-
-    options: {
-      fields: [
-        "id",
-        "created_at",
-        "updated_at",
-        "deleted_at",
-        "value",
-        "option_id",
-        "variant_id",
-        "metadata",
-      ],
-    },
-  },
-  profile: {
-    fields: ["id", "created_at", "updated_at", "deleted_at", "name", "type"],
-  },
-}
-
-function fromObjectToStringPath(fields, parent) {
+/**
+ * Transform a remote query object to a string array containing the chain of fields and relations
+ *
+ * @param fields
+ * @param parent
+ *
+ * @example
+ *
+ * const remoteQueryObject = = {
+ *   fields: [
+ *     "id",
+ *     "title",
+ *   ],
+ *   images: {
+ *     fields: ["id", "created_at", "updated_at", "deleted_at", "url", "metadata"],
+ *   },
+ * }
+ *
+ * const fields = remoteQueryObjectToString(remoteQueryObject)
+ *
+ * console.log(fields)
+ * // ["id", "title", "images.id", "images.created_at", "images.updated_at", "images.deleted_at", "images.url", "images.metadata"]
+ */
+export function remoteQueryObjectToString(
+  fields: object,
+  parent?: string
+): string[] {
   return Object.keys(fields).reduce((acc, key) => {
     if (key === "fields") {
       if (parent) {
@@ -123,7 +38,10 @@ function fromObjectToStringPath(fields, parent) {
 
     if (typeof fields[key] === "object") {
       acc = acc.concat(
-        fromObjectToStringPath(fields[key], parent ? `${parent}.${key}` : key)
+        remoteQueryObjectToString(
+          fields[key],
+          parent ? `${parent}.${key}` : key
+        )
       )
       return acc
     }
