fb33e4e44a0bb5e2007ec77c541ece58b8832318
shahednasser <shahednasser@users.noreply.github.com>
2023-09-29 20:10:51 +0000

    chore(docs): Generated API Reference

--- www/apps/api-reference/specs/admin/code_samples/Shell/admin_inventory-items_{id}/get.sh
+++ www/apps/api-reference/specs/admin/code_samples/Shell/admin_inventory-items_{id}/get.sh
@@ -1,2 +1,2 @@
 curl '{backend_url}/admin/inventory-items/{id}' \
--H 'Authorization: Bearer {api_token}'
+-H 'x-medusa-access-token: {api_token}'
--- www/apps/api-reference/specs/admin/code_samples/Shell/admin_publishable-api-keys_{id}_revoke/post.sh
+++ www/apps/api-reference/specs/admin/code_samples/Shell/admin_publishable-api-keys_{id}_revoke/post.sh
@@ -1,2 +1,2 @@
 curl -X POST '{backend_url}/admin/publishable-api-keys/{id}/revoke' \
--H 'Authorization: Bearer {api_token}'
+-H 'x-medusa-access-token: {api_token}'
