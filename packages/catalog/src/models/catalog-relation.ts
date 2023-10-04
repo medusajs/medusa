60b28c417eac00b356529541e6dc992a1ca3da3b
adrien2p <adrien.deperetti@gmail.com>
2023-10-04 17:11:08 +0200

    WIP

--- packages/catalog/src/models/catalog-relation.ts
+++ packages/catalog/src/models/catalog-relation.ts
@@ -1,23 +1,26 @@
-import { BeforeCreate, Entity, PrimaryKey, Property } from "@mikro-orm/core"
+import {
+  BeforeCreate,
+  Entity,
+  Index,
+  PrimaryKey,
+  Property,
+} from "@mikro-orm/core"
 
 import { generateEntityId } from "@medusajs/utils"
 
 @Entity({ tableName: "catalog" })
-export class Catalog {
-  // composite primary key id - name
-  // the id should be composed of {entityName}_{id}
-
-  // Create a catalog relation table that link the catalog row to the related parent rows
-
+export class CatalogRelation {
+  // composite primary key id - entity
+  // the id and therefore the children ids, should be composed of {entityName}_{id}
   @PrimaryKey({ columnType: "text" })
   id!: string
 
-  /*@Property({ columnType: "json", nullable: true })
+  @Property({ columnType: "json", nullable: true })
   @Index({ name: "IDX_catalog_children_ids" })
-  children_ids: string[] | null*/
+  children_ids: string[] | null
 
   @Property({ columnType: "text", nullable: false })
-  name: string
+  entity: string
 
   @Property({ columnType: "jsonb", default: "{}" })
   data: Record<string, unknown>
