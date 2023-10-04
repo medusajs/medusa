0d4abe19af84ea15ba4702891af489aec3936793
Philip Korsholm <philip.korsholm@hotmail.com>
2023-10-04 10:42:46 +0200

    update env key

--- packages/medusa/src/loaders/feature-flags/pricing-integration.ts
+++ packages/medusa/src/loaders/feature-flags/pricing-integration.ts
@@ -3,7 +3,7 @@ import { FlagSettings } from "../../types/feature-flags"
 const PricingIntegrationFeatureFlag: FlagSettings = {
   key: "pricing_integration",
   default_val: false,
-  env_key: "MEDUSA_FF_PRICING_INTEGRATION",
+  env_key: "MEDUSA_FF_PRICE_MODULE",
   description: "[WIP] use price module integration for pricing",
 }
 
