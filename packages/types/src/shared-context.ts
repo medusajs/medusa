5a1530c65d77257414acb46f5b0f7506b2e97882
Shahed nasser <shahednasser@gmail.com>
2023-10-05 10:32:21 +0300

    revert type changes

--- packages/types/src/shared-context.ts
+++ packages/types/src/shared-context.ts
@@ -1,13 +1,5 @@
 import { EntityManager } from "typeorm"
 
-/**
- * {@label SharedContext}
- * 
- * The context shared between modules.
- * 
- * @param transactionManager - the Entity manager used for transactions.
- * @param manager - the Entity manager used to resolve dependencies from the container.
- */
 export type SharedContext = {
   transactionManager?: EntityManager
   manager?: EntityManager
