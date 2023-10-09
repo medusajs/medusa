098d04acca96faae9a4f1d295e4f3b98503dca29
Sebastian Rindom <skrindom@gmail.com>
2023-10-09 12:41:28 -0700

    fix: clarify comments

--- packages/workflows/src/definition/product/prepare-create-inventory-items.ts
+++ packages/workflows/src/definition/product/prepare-create-inventory-items.ts
@@ -13,7 +13,11 @@ type VariantIndexAndInventoryItems = {
 type InventoryItemAssociation = {
   /** The variant id that the inventory item should be associated with. */
   _associationTag: string
+
+  /** The inventory item creation input. */
   creationInput?: Partial<InventoryItemDTO>
+
+  /** The inventory item that should be associated with the variant. */
   existingItem?: WorkflowTypes.ProductWorkflow.CreateVariantInventoryInputDTO
 }
 
@@ -30,64 +34,75 @@ export async function prepareCreateInventoryItems({
 }>) {
   const taggedVariants = data.products.reduce<InventoryItemAssociation[]>(
     (acc, product: ProductTypes.ProductDTO) => {
+      /*
+       * An array of VariantIndexAndInventoryItems objects.
+       *
+       *   {
+       *     index: number
+       *     inventoryItems: {
+       *       inventory_item_id: string
+       *       required_quantity: number
+       *     }[]
+       *   }
+       *
+       * We can use this to find the inventory items that should be
+       * associated with a variant.
+       */
       const inventoryVariants =
         data.prepare.productsHandleVariantsIndexInventoryItemsMap.get(
           product.handle!
         )
 
+      /*
+       * It is not given that all variants will have inventory items to
+       * associate with them. Here we loop through the product's variants and
+       * check whether we should create new inventory items or associate
+       * existing ones. If there are no inventory items to create/associate
+       * we ignore the variant.
+       */
       const cleanVariants = product.variants.reduce<InventoryItemAssociation[]>(
         (acc, variant: AssociationTaggedVariant, index: number) => {
-          // We need to create and/or attach inventory items to variants in two
-          // cases:
-          //  1. The consuming party has specified inventory items that should
-          //     be associated with the variant.
-          //  2. The consuming party has specified that inventory should be
-          //     managed for the variant and has not provided any existing
-          //     inventory items to associate the variant with.
-          //
-          // We want to carry over inventory item ids and required quantities
-          // in the case of 1. and we want to create new inventory items in The
-          // case of 2.
+          /*
+           * We need to create and/or attach inventory items to variants in two
+           * cases:
+           *  1. Client specified inventory items to associate with the variant
+           *  2. Client specified inventory should managed for the variant.
+           *
+           * In the case of 1. we want to associate existing inventory items.
+           * In the case of 2. we want to create new inventory items.
+           */
 
-          // Case 1.
-          // We check if there are inventory items to associate with the variant
-          // and if so, we create an association for each inventory item.
+          /*
+           * Case 1.
+           * Check if there are inventory items to associate with variant.
+           */
           const itemsToAssociate = inventoryVariants?.find(
             (iv) => iv.index === index
           )
 
           if (itemsToAssociate && itemsToAssociate.inventoryItems?.length) {
-            const toAssociate = itemsToAssociate.inventoryItems.map((item) => {
-              return {
-                existingItem: item,
-                _associationTag: variant.id,
-              }
-            })
-
+            const toAssociate = prepareExistingItemsForVariant(
+              itemsToAssociate,
+              variant
+            )
             return acc.concat(toAssociate)
           }
 
-          // Case 2.
-          // We check if the variant has inventory management enabled and if so,
-          // we pass inventory item creation input and the variant id.
+          /*
+           * Case 2.
+           * We know this variant doesn't have inventory items to associate, but
+           * we still need to check if we should create new inventory items.
+           *
+           * Check if the variant has inventory management enabled and if so,
+           * we pass inventory item creation input and the variant id.
+           */
           if (!variant.manage_inventory) {
             return acc
           }
 
-          acc.push({
-            _associationTag: variant.id,
-            creationInput: {
-              sku: variant.sku,
-              origin_country: variant.origin_country,
-              hs_code: variant.hs_code,
-              mid_code: variant.mid_code,
-              material: variant.material,
-              weight: variant.weight,
-              length: variant.length,
-              height: variant.height,
-              width: variant.width,
-            },
-          })
+          const preparedData = prepareCreationInputForVariant(variant)
+          acc.push(preparedData)
+
           return acc
         },
         []
@@ -102,6 +117,37 @@ export async function prepareCreateInventoryItems({
     alias: prepareCreateInventoryItems.aliases.output,
     value: {
       inventoryItems: taggedVariants,
+    },
+  }
+}
+
+function prepareExistingItemsForVariant(
+  itemsToAssociate: VariantIndexAndInventoryItems,
+  variant: ProductTypes.ProductVariantDTO
+) {
+  return itemsToAssociate.inventoryItems.map((item) => {
+    return {
+      existingItem: item,
+      _associationTag: variant.id,
+    }
+  })
+}
+
+function prepareCreationInputForVariant(
+  variant: ProductTypes.ProductVariantDTO
+) {
+  return {
+    _associationTag: variant.id,
+    creationInput: {
+      sku: variant.sku,
+      origin_country: variant.origin_country,
+      hs_code: variant.hs_code,
+      mid_code: variant.mid_code,
+      material: variant.material,
+      weight: variant.weight,
+      length: variant.length,
+      height: variant.height,
+      width: variant.width,
     },
   }
 }
