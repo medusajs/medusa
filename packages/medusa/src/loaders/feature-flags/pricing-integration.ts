c26c6518cd70ea82c0f869a8db64796c0b0337e7
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2023-10-12 09:17:47 +0200

    Update packages/medusa/src/loaders/feature-flags/pricing-integration.ts
    Co-authored-by: Oli Juhl <59018053+olivermrbl@users.noreply.github.com>

--- packages/medusa/src/loaders/feature-flags/pricing-integration.ts
+++ packages/medusa/src/loaders/feature-flags/pricing-integration.ts
@@ -1,9 +1,9 @@
 import { FlagSettings } from "../../types/feature-flags"
 
 const PricingIntegrationFeatureFlag: FlagSettings = {
-  key: "pricing_integration",
+  key: "isolate_pricing_domain",
   default_val: false,
-  env_key: "MEDUSA_FF_PRICE_MODULE",
+  env_key: "MEDUSA_FF_ISOLATE_PRICING_DOMAIN",
   description: "[WIP] use price module integration for pricing",
 }
 
