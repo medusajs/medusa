1c4727376d3c03c5edd2c031ef36a44708a63bad
Philip Korsholm <philip.korsholm@hotmail.com>
2023-10-18 16:55:47 +0200

    ensure vairant creation in product endpoint works + integration test

---
+++ integration-tests/api/__tests__/admin/services/product.ts
@@ -0,0 +1,55 @@
+import path from "path"
+import { initDb, useDb } from "../../../../environment-helpers/use-db"
+import { bootstrapApp } from "../../../../environment-helpers/bootstrap-app"
+
+jest.setTimeout(30000)
+
+describe("/admin/products", () => {
+  let medusaProcess
+  let dbConnection
+  let medusaContainer
+  let productService
+
+  beforeAll(async () => {
+    const cwd = path.resolve(path.join(__dirname, "..", "..", ".."))
+    dbConnection = await initDb({ cwd } as any)
+    const { container } = await bootstrapApp({ cwd })
+    medusaContainer = container
+    productService = medusaContainer.resolve("productService")
+  })
+
+  afterAll(async () => {
+    const db = useDb()
+    await db.shutdown()
+
+    medusaProcess.kill()
+  })
+
+  it("should create variant prices correctly when propagating variants in service creation", async () => {
+    const payload = {
+      title: "test-product",
+      handle: "test-product",
+      options: [{ title: "test-option" }],
+      variants: [
+        {
+          title: "test-variant",
+          inventory_quantity: 10,
+          sku: "test",
+          options: [{ value: "sm", title: "test-option" }],
+          prices: [{ amount: "100", currency_code: "usd" }],
+        },
+      ],
+    }
+
+    const { id } = await productService.create(payload)
+
+    const result = await productService.retrieve(id, {
+      relations: ["variants", "variants.prices", "variants.options"],
+    })
+    console.warn(JSON.stringify(result, null, 2))
+
+    // const prices = result.variants
+
+    // expect(prices.length).toBeTruthy()
+  })
+})
