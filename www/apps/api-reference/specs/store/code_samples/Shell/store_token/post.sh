c67d490db3d97e6b325f9511a5b00a52e68ecf0d
Shahed Nasser <shahednasser@gmail.com>
2023-10-23 11:48:02 +0300

    chore(oas): Fix OAS related to JWT authentication (#5448)
    * chore(oas): Fix OAS related to JWT authentication
    
    * generate openapi spec files
    
    * remove L option in curl examples

--- www/apps/api-reference/specs/store/code_samples/Shell/store_token/post.sh
+++
@@ -1,6 +0,0 @@
-curl --location --request POST 'https://medusa-url.com/store/auth/token' \
---header 'Content-Type: application/json' \
---data-raw '{
-  "email": "user@example.com",
-  "password": "supersecret"
-}'
