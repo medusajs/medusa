efd0262687570b38eb37ce4c521b230914f59d32
Shahed nasser <shahednasser@gmail.com>
2023-10-23 11:06:32 +0300

    remove L option in curl examples

--- packages/medusa/src/api/routes/admin/auth/get-token.ts
+++ packages/medusa/src/api/routes/admin/auth/get-token.ts
@@ -34,7 +34,7 @@ import { AdminPostAuthReq } from "./create-session"
  *   - lang: Shell
  *     label: cURL
  *     source: |
- *       curl -L -X POST '{backend_url}/admin/auth/token' \
+ *       curl -X POST '{backend_url}/admin/auth/token' \
  *       -H 'Content-Type: application/json' \
  *       --data-raw '{
  *         "email": "user@example.com",
--- packages/medusa/src/api/routes/store/auth/get-token.ts
+++ packages/medusa/src/api/routes/store/auth/get-token.ts
@@ -34,7 +34,7 @@ import { StorePostAuthReq } from "./create-session"
  *   - lang: Shell
  *     label: cURL
  *     source: |
- *       curl -L -X POST '{backend_url}/store/auth/token' \
+ *       curl -X POST '{backend_url}/store/auth/token' \
  *       -H 'Content-Type: application/json' \
  *       --data-raw '{
  *         "email": "user@example.com",
