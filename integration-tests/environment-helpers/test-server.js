d3658508b266cd5658d63ff584db56eeb4c5689b
Riqwan Thamir <rmthamir@gmail.com>
2023-10-24 11:19:08 +0200

    feat(medusa,workflows,types): update prices on product and variant update (#5412)
    * wip
    
    * chore: update product prices through workflow
    
    * chore: cleanup
    
    * chore: update product handler updates prices for variants
    
    * chore: handle reverts
    
    * chore: address pr comments

--- integration-tests/environment-helpers/test-server.js
+++ integration-tests/environment-helpers/test-server.js
@@ -1,7 +1,10 @@
 const { bootstrapApp } = require("./bootstrap-app")
+const { setContainer } = require("./use-container")
 
 const setup = async () => {
-  const { app, port } = await bootstrapApp()
+  const { app, port, container } = await bootstrapApp()
+
+  setContainer(container)
 
   app.listen(port, (err) => {
     process.send(port)
