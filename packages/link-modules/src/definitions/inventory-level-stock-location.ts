d1451a8d9eff2642272952edba862f55fa46ecf0
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2023-10-26 14:42:34 -0400

    chore: read only inventory stock

--- packages/link-modules/src/definitions/inventory-level-stock-location.ts
+++ packages/link-modules/src/definitions/inventory-level-stock-location.ts
@@ -1,46 +1,9 @@
-import { ModuleJoinerConfig } from "@medusajs/types"
 import { Modules } from "@medusajs/modules-sdk"
+import { ModuleJoinerConfig } from "@medusajs/types"
 
 export const InventoryLevelStockLocation: ModuleJoinerConfig = {
   isLink: true,
   isReadOnlyLink: true,
-  /* databaseConfig: {
-    tableName: "inventory_level",
-    idPrefix: "pvitem",
-    extraFields: {
-      required_quantity: {
-        type: "integer",
-        defaultValue: "1",
-      },
-    },
-  },
-  alias: [
-    {
-      name: "inventory_level_stock_location",
-    },
-    {
-      name: "inventory_level_stock_locations",
-    },
-  ],
-  primaryKeys: ["id", "inventory_item_id", "stock_location_id"],
-  relationships: [
-    {
-      serviceName: Modules.INVENTORY,
-      primaryKey: "id",
-      foreignKey: "inventory_level_id",
-      alias: "inventory_level",
-      args: {
-        methodSuffix: "InventoryLevel",
-      },
-    },
-    {
-      serviceName: Modules.STOCK_LOCATION,
-      primaryKey: "id",
-      foreignKey: "stock_location_id",
-      alias: "stock_location",
-      deleteCascade: true,
-    },
-  ],*/
   extends: [
     {
       serviceName: Modules.INVENTORY,
@@ -52,15 +15,5 @@ export const InventoryLevelStockLocation: ModuleJoinerConfig = {
         isList: true,
       },
     },
-    /*{
-      serviceName: Modules.STOCK_LOCATION,
-      relationship: {
-        serviceName: Modules.INVENTORY,
-        primaryKey: "id",
-        foreignKey: "inventory_level_id",
-        alias: "inventory_level_link",
-        isList: true,
-      },
-    },*/
   ],
 }
