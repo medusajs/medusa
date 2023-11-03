022769d16b59f692e1cf311724674d7897e3f10d
adrien2p <adrien.deperetti@gmail.com>
2023-11-03 15:58:37 +0100

    aggregate error option

--- packages/utils/src/common/__tests__/promise-all.spec.ts
+++ packages/utils/src/common/__tests__/promise-all.spec.ts
@@ -1,4 +1,5 @@
 import { promiseAll } from "../promise-all"
+import { EOL } from "os"
 
 describe("promiseAll", function () {
   it("should throw an error if any of the promises throw", async function () {
@@ -9,6 +10,22 @@ describe("promiseAll", function () {
     ]).catch((e) => e)
 
     expect(res.message).toBe("error")
+  })
+
+  it("should throw errors if any of the promises throw and aggregate them", async function () {
+    const res = await promiseAll(
+      [
+        Promise.resolve(1),
+        Promise.reject("error"),
+        Promise.reject("error2"),
+        Promise.resolve(3),
+      ],
+      {
+        aggregateErrors: true,
+      }
+    ).catch((e) => e)
+
+    expect(res.message).toBe(["error", "error2"].join(EOL))
   })
 
   it("should return all values if all promises are fulfilled", async function () {
--- packages/utils/src/common/promise-all.ts
+++ packages/utils/src/common/promise-all.ts
@@ -1,14 +1,29 @@
+import { EOL } from "os"
+
 /**
  * Promise.allSettled with error handling, safe alternative to Promise.all
  * @param promises
+ * @param aggregateErrors
  */
 export async function promiseAll<T = unknown>(
-  promises: Promise<T>[]
+  promises: Promise<T>[],
+  { aggregateErrors } = { aggregateErrors: false }
 ): Promise<T[]> {
   const states = await Promise.allSettled(promises)
-  const rejected = states.filter((state) => state.status === "rejected")
+
+  const rejected = states.filter(
+    (state) => state.status === "rejected"
+  ) as PromiseRejectedResult[]
+
   if (rejected.length) {
-    throw new Error((rejected[0] as PromiseRejectedResult).reason)
+    let aggregatedErrors = (rejected[0] as PromiseRejectedResult).reason
+    if (aggregateErrors) {
+      aggregatedErrors = rejected.reduce((errors, state) => {
+        errors.push(state.reason)
+        return errors
+      }, [] as string[])
+    }
+    throw new Error(aggregatedErrors.join(EOL))
   }
 
   return states.map((state) => (state as PromiseFulfilledResult<T>).value)
