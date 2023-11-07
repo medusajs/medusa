5ed17a544a2341a520bb17afc89b694d8159a694
adrien2p <adrien.deperetti@gmail.com>
2023-11-07 12:08:12 +0100

    rm module helper

--- packages/medusa/src/helpers/test-request.js
+++ packages/medusa/src/helpers/test-request.js
@@ -1,5 +1,4 @@
 import {
-  moduleHelper,
   moduleLoader,
   ModulesDefinition,
   registerMedusaModule,
@@ -79,7 +78,6 @@ container.registerAdd = function (name, registration) {
 }.bind(container)
 
 container.register("featureFlagRouter", asValue(featureFlagRouter))
-container.register("modulesHelper", asValue(moduleHelper))
 container.register("configModule", asValue(config))
 container.register({
   logger: asValue({
