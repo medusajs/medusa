c0451f9d21e0752c99f44d82c76059830f953fc3
adrien2p <adrien.deperetti@gmail.com>
2023-11-09 16:03:28 +0100

    Add structured tests

--- integration-tests/plugins/__tests__/workflows/utils/composer/compose.ts
+++ integration-tests/plugins/__tests__/workflows/utils/composer/compose.ts
@@ -1,5 +1,5 @@
-import { isString } from "@medusajs/utils"
 import { createStep, createWorkflow } from "@medusajs/workflows"
+import { promiseAll } from "@medusajs/utils"
 
 jest.setTimeout(30000)
 
@@ -8,7 +8,7 @@ describe("Workflow composer", function () {
 
   afterAll(async () => {})
 
-  it.only("should compose a new workflow and execute it", async () => {
+  it("should compose a new workflow and execute it", async () => {
     const step1 = createStep("step1", (input) => {
       const ret = "return from 1 + input = " + JSON.stringify(input)
       console.log(ret)
@@ -39,83 +39,135 @@ describe("Workflow composer", function () {
       return step3(returnStep1, ret2)
     })
 
-    await mainFlow({ test: "payload" })
+    await mainFlow({ test: "payload1" })
   })
 
-  it("should compose a new workflow and execute it with async steps", async () => {
-    class WorkflowContext {
-      steps = {}
-      ctx = ((that) => {
-        return {
-          addStep: function ({ name, invoke }) {
-            that.steps[name] = invoke
-            return (invoke.prototype._stepDetails = { name, invoke })
-          },
-        }
-      })(this)
-
-      step(nameOrFn, fn) {
-        const name = isString(nameOrFn)
-          ? nameOrFn
-          : nameOrFn.prototype._stepDetails.name
-        fn ??= nameOrFn.prototype._stepDetails.invoke
-
-        const stepDetails = {
-          name,
-          invoke: fn,
-        }
-
-        return this.ctx.addStep(stepDetails)
-      }
-    }
-
-    class CreateWorkflow {
-      private static context() {
-        return new WorkflowContext()
-      }
-
-      static init(): {
-        step: any
-        createWorkflow: (name, composer) => { run: (...input) => Promise<any> }
-      } {
-        const context = CreateWorkflow.context()
-
-        return {
-          step: context.step.bind(context),
-          createWorkflow(name, composer): { run: (...input) => Promise<any> } {
-            return {
-              async run(...input) {
-                return await composer.apply(context, input)
-              },
-            }
-          },
-        }
-      }
-    }
-
-    const { step: step1 } = CreateWorkflow.init()
-
-    const prepareProductDataStep = step1("a1", (input) => {
-      console.log("prepareProductDataStep invoke", input)
-      return input
+  it("should compose two new workflows sequentially and execute them sequentially", async () => {
+    const step1 = createStep("step1", (input) => {
+      const ret = "return from 1 + input = " + JSON.stringify(input)
+      console.log(ret)
+      return ret
     })
 
-    const action1 = step1("a2", (input) => {
-      console.log("action1 invoke", input)
-      return {
-        ...input,
-        prop: "test",
+    const step2 = createStep("step2", (param) => {
+      const ret = {
+        complex: "return from 2",
+        arg: param,
       }
+      console.log(ret)
+      return ret
     })
 
-    const { createWorkflow } = CreateWorkflow.init()
-
-    const workflow = createWorkflow("test", function (this: any, input) {
-      const preparedResponse = this.step(prepareProductDataStep(input))
-      return this.step(action1(preparedResponse))
+    const step3 = createStep("step3", (param, other) => {
+      const ret = {
+        obj: "return from 3",
+        args: [param, other],
+      }
+      console.log(ret)
+      return ret
     })
 
-    const result = await workflow.run({ test: "payload" })
-    console.log(result)
+    const mainFlow = createWorkflow("test", function (this: any, input) {
+      const returnStep1 = step1(input)
+      const ret2 = step2(returnStep1)
+      return step3(returnStep1, ret2)
+    })
+
+    const mainFlow2 = createWorkflow("test2", function (this: any, input) {
+      const ret2 = step2(input)
+      const returnStep1 = step1(ret2)
+      return step3(ret2, returnStep1)
+    })
+
+    await mainFlow({ test: "payload3" })
+    await mainFlow2({ test: "payload2" })
+  })
+
+  it("should compose two new workflows concurrently and execute them sequentially", async () => {
+    const step1 = createStep("step1", (input) => {
+      const ret = "return from 1 + input = " + JSON.stringify(input)
+      console.log(ret)
+      return ret
+    })
+
+    const step2 = createStep("step2", (param) => {
+      const ret = {
+        complex: "return from 2",
+        arg: param,
+      }
+      console.log(ret)
+      return ret
+    })
+
+    const step3 = createStep("step3", (param, other) => {
+      const ret = {
+        obj: "return from 3",
+        args: [param, other],
+      }
+      console.log(ret)
+      return ret
+    })
+
+    const [mainFlow, mainFlow2] = await promiseAll([
+      createWorkflow("test", function (this: any, input) {
+        const returnStep1 = step1(input)
+        const ret2 = step2(returnStep1)
+        return step3(returnStep1, ret2)
+      }),
+
+      createWorkflow("test2", function (this: any, input) {
+        const ret2 = step2(input)
+        const returnStep1 = step1(ret2)
+        return step3(ret2, returnStep1)
+      }),
+    ])
+
+    await mainFlow({ test: "payload3" })
+    await mainFlow2({ test: "payload2" })
+  })
+
+  it("should compose two new workflows concurrently and execute them concurrently", async () => {
+    const step1 = createStep("step1", (input) => {
+      const ret = "return from 1 + input = " + JSON.stringify(input)
+      console.log(ret)
+      return ret
+    })
+
+    const step2 = createStep("step2", (param) => {
+      const ret = {
+        complex: "return from 2",
+        arg: param,
+      }
+      console.log(ret)
+      return ret
+    })
+
+    const step3 = createStep("step3", (param, other) => {
+      const ret = {
+        obj: "return from 3",
+        args: [param, other],
+      }
+      console.log(ret)
+      return ret
+    })
+
+    const [mainFlow, mainFlow2] = await promiseAll([
+      createWorkflow("test", function (this: any, input) {
+        const returnStep1 = step1(input)
+        const ret2 = step2(returnStep1)
+        return step3(returnStep1, ret2)
+      }),
+
+      createWorkflow("test2", function (this: any, input) {
+        const ret2 = step2(input)
+        const returnStep1 = step1(ret2)
+        return step3(ret2, returnStep1)
+      }),
+    ])
+
+    await promiseAll([
+      mainFlow({ test: "payload3" }),
+      mainFlow2({ test: "payload2" }),
+    ])
   })
 })
