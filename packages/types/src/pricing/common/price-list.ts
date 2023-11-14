fd539b7e32bd671c6e64f2d804341365ddc92085
Philip Korsholm <philip.korsholm@hotmail.com>
2023-11-14 15:29:53 +0000

    remove comment

--- packages/types/src/pricing/common/price-list.ts
+++ packages/types/src/pricing/common/price-list.ts
@@ -73,44 +73,6 @@ export interface FilterablePriceListRuleValueProps
   price_list_rule_id?: string[]
 }
 
-// export interface PriceListRuleDTO {
-//   id: string
-//   value: string
-//   rule_type: RuleTypeDTO
-//   price_list: PriceListDTO
-//   price_list_rule_values?: PriceListRuleValueDTO[]
-// }
-
-// export interface CreatePriceListRuleDTO {
-//   rule_type_id?: string
-//   rule_type?: string | RuleTypeDTO
-//   price_list_id?: string
-//   price_list?: string | PriceListDTO
-// }
-
-// export interface UpdatePriceListRuleDTO {
-//   id: string
-//   price_list_id?: string
-//   rule_type_id?: string
-// }
-
-// export interface PriceListRuleValueDTO {
-//   id: string
-//   value: string
-//   price_list_rule: PriceListRuleDTO
-// }
-
-// export interface CreatePriceListRuleValueDTO {
-//   value: string
-//   price_list_rule_id?: string
-//   price_list_rule?: PriceListRuleDTO | string
-// }
-
-// export interface UpdatePriceListRuleValueDTO {
-//   id: string
-//   value: string
-//   price_list_rule_id: string
-// }
 export interface PriceListRuleValueDTO {
   id: string
   value: string
