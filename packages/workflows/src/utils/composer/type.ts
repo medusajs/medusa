30c80efa11b85b35128c80090d259d2e9de9980d
adrien2p <adrien.deperetti@gmail.com>
2023-11-14 18:15:20 +0100

    types

--- packages/workflows/src/utils/composer/type.ts
+++ packages/workflows/src/utils/composer/type.ts
@@ -1,6 +1,7 @@
 import {
   OrchestratorBuilder,
   TransactionContext as OriginalWorkflowTransactionContext,
+  TransactionPayload,
   WorkflowHandler,
 } from "@medusajs/orchestration"
 import { Context, MedusaContainer } from "@medusajs/types"
@@ -49,12 +50,9 @@ export type CreateWorkflowComposerContext = {
   ) => TOutput
 }
 
-// TODO: Create a type for the context in the orchestration package
-// TODO: The below is a subset of the original transaction context
-
 export interface StepExecutionContext {
   container: MedusaContainer
-  metadata: any
+  metadata: TransactionPayload["metadata"]
   context: Context
 }
 
