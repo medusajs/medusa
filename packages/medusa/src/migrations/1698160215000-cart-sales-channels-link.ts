28b2eef2a551b9bc3b4d808e5f787b586be8d31b
fPolic <mainacc.polic@gmail.com>
2023-11-17 11:26:43 +0100

    fix: migrations

--- packages/medusa/src/migrations/1698160215000-cart-sales-channels-link.ts
+++ packages/medusa/src/migrations/1698160215000-cart-sales-channels-link.ts
@@ -11,23 +11,22 @@ export class CartSalesChannelsLink1698160215000 implements MigrationInterface {
             "created_at"        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
             "updated_at"        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
             "deleted_at"        TIMESTAMP WITH TIME ZONE,
-            CONSTRAINT "cart_sales_channel_pk"              PRIMARY KEY ("id"),
+                                              CONSTRAINT "cart_sales_channel_pk"              PRIMARY KEY ("id"),
             CONSTRAINT "cart_sales_channel_cart_id_unique"  UNIQUE ("cart_id"),
             CONSTRAINT "cart_sales_channel_cart_id_sales_channel_id_unique"  UNIQUE ("cart_id", "sales_channel_id")
             );
 
         insert into "cart_sales_channel" (id, cart_id, sales_channel_id)
-            (select 'cartsc_' || substr(md5(random()::text), 0, 27), id, sales_channel_id from "cart");
+            (select 'cartsc_' || substr(md5(random()::text), 0, 27), id, sales_channel_id from "cart" WHERE sales_channel_id IS NOT NULL);
 
         ALTER TABLE "cart" DROP COLUMN IF EXISTS "sales_channel_id";
-
         ALTER TABLE "cart" DROP CONSTRAINT IF EXISTS "FK_a2bd3c26f42e754b9249ba78fd6";
     `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        ALTER TABLE "cart" ADD COLUMN IF NOT EXISTS "sales_channel_id";
+        ALTER TABLE "cart" ADD COLUMN IF NOT EXISTS "sales_channel_id" CHARACTER VARYING;
 
         UPDATE "cart" SET "sales_channel_id" = "cart_sales_channel"."sales_channel_id"
             FROM "cart_sales_channel"
