88a61de636762a5fae202405364355aec8a90ecb
Philip Korsholm <philip.korsholm@hotmail.com>
2023-11-21 13:28:37 +0000

    rm tsdocs for internal interface

--- packages/product/src/types/services/product-option-value.ts
+++ packages/product/src/types/services/product-option-value.ts
@@ -1,52 +1,14 @@
-/**
- * @interface
- *
- * The product option value's data.
- */
 export interface UpdateProductOptionValueDTO {
-  /**
-   * The ID of the product option value.
-   */
   id: string
-  /**
-   * The value of the product option value.
-   */
   value: string
-  /**
-   * The associated product option.
-   *
-   * @expandable
-   */
   option: string
-  /**
-   * Holds custom data in key-value pairs.
-   */
   metadata?: Record<string, unknown> | null
 }
 
 export interface CreateProductOptionValueDTO {
-  /**
-   * The ID of the product option value.
-   */
   id?: string
-  /**
-   * The value of the product option value.
-   */
   value: string
-  /**
-   * The associated product option.
-   *
-   * @expandable
-   */
   option: string
-  /**
-   * The associated product option.
-   *
-   * @expandable
-   */
   variant: string
-  /**
-   * Holds custom data in key-value pairs.
-   */
   metadata?: Record<string, unknown> | null
 }
