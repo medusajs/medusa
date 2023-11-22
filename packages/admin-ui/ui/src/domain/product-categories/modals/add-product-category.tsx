8cc45915f3a3ca2efa4a08709c0ca8afb145f031
lulu <lucjan.grzesik@gmail.com>
2023-11-22 21:37:19 +0000

    lint

--- packages/admin-ui/ui/src/domain/product-categories/modals/add-product-category.tsx
+++ packages/admin-ui/ui/src/domain/product-categories/modals/add-product-category.tsx
@@ -1,72 +1,76 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import { ProductCategory } from "@medusajs/medusa"
+import { ProductCategory } from "@medusajs/medusa";
 import {
   adminProductCategoryKeys,
   useAdminCreateProductCategory,
-} from "medusa-react"
-import { useTranslation } from "react-i18next"
+} from "medusa-react";
+import { useTranslation } from "react-i18next";
 
-import { useQueryClient } from "@tanstack/react-query"
-import Button from "../../../components/fundamentals/button"
-import CrossIcon from "../../../components/fundamentals/icons/cross-icon"
-import InputField from "../../../components/molecules/input"
-import TextArea from "../../../components/molecules/textarea"
-import FocusModal from "../../../components/molecules/modal/focus-modal"
-import { NextSelect } from "../../../components/molecules/select/next-select"
-import useNotification from "../../../hooks/use-notification"
-import { getErrorMessage } from "../../../utils/error-messages"
-import TreeCrumbs from "../components/tree-crumbs"
+import { useQueryClient } from "@tanstack/react-query";
+import Button from "../../../components/fundamentals/button";
+import CrossIcon from "../../../components/fundamentals/icons/cross-icon";
+import InputField from "../../../components/molecules/input";
+import TextArea from "../../../components/molecules/textarea";
+import FocusModal from "../../../components/molecules/modal/focus-modal";
+import { NextSelect } from "../../../components/molecules/select/next-select";
+import useNotification from "../../../hooks/use-notification";
+import { getErrorMessage } from "../../../utils/error-messages";
+import TreeCrumbs from "../components/tree-crumbs";
 import MetadataForm, {
   getSubmittableMetadata,
   MetadataFormType,
-} from "../../../components/forms/general/metadata-form"
-import { Controller, useForm } from "react-hook-form"
-import { nestedForm } from '../../../utils/nested-form';
-import { TFunction } from "i18next"
+} from "../../../components/forms/general/metadata-form";
+import { Controller, useForm } from "react-hook-form";
+import { nestedForm } from "../../../utils/nested-form";
+import { TFunction } from "i18next";
 
-const visibilityOptions = (t: TFunction<"translation", undefined, "translation">) => [
+const visibilityOptions = (
+  t: TFunction<"translation", undefined, "translation">
+) => [
   {
     label: t("modals-public", "Public"),
     value: "public",
   },
   { label: t("modals-private", "Private"), value: "private" },
-]
+];
 
-const statusOptions = (t: TFunction<"translation", undefined, "translation">) => [
+const statusOptions = (
+  t: TFunction<"translation", undefined, "translation">
+) => [
   { label: t("modals-active", "Active"), value: "active" },
   { label: t("modals-inactive", "Inactive"), value: "inactive" },
-]
+];
 
 type CreateProductCategoryProps = {
-  closeModal: () => void
-  parentCategory?: ProductCategory
-  categories: ProductCategory[]
-}
+  closeModal: () => void;
+  parentCategory?: ProductCategory;
+  categories: ProductCategory[];
+};
 
 export type CategoryFormData = {
-  name: string
-  handle: string | undefined
-  description: string | undefined
-  metadata: MetadataFormType
+  name: string;
+  handle: string | undefined;
+  description: string | undefined;
+  metadata: MetadataFormType;
   is_active: {
-    value: "active" | "inactive"
-    label: string
-  }
+    value: "active" | "inactive";
+    label: string;
+  };
   is_public: {
-    value: "public" | "private"
-    label: string
-  }
-}
+    value: "public" | "private";
+    label: string;
+  };
+};
 
 /**
  * Focus modal container for creating Publishable Keys.
  */
 function CreateProductCategory(props: CreateProductCategoryProps) {
-  const { t } = useTranslation()
-  const { closeModal, parentCategory, categories } = props
-  const notification = useNotification()
-  const queryClient = useQueryClient()
+  const { t } = useTranslation();
+  const { closeModal, parentCategory, categories } = props;
+  const notification = useNotification();
+  const queryClient = useQueryClient();
 
   const form = useForm<CategoryFormData>({
     defaultValues: {
@@ -86,17 +90,24 @@ function CreateProductCategory(props: CreateProductCategoryProps) {
       },
     },
     mode: "onChange",
-  })
-  const { register, handleSubmit, watch, reset, control, formState: { errors, isDirty, isValid, isSubmitting }} = form
+  });
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    control,
+    formState: { errors, isDirty, isValid, isSubmitting },
+  } = form;
   const name = watch("name", "");
 
   useEffect(() => {
     console.log(errors);
     console.log(isSubmitting);
-  }
-  , [errors,isSubmitting]);
+  }, [errors, isSubmitting]);
 
-  const { mutateAsync: createProductCategory } = useAdminCreateProductCategory()
+  const { mutateAsync: createProductCategory } =
+    useAdminCreateProductCategory();
 
   const submit = async (data: CategoryFormData) => {
     console.log(data);
@@ -109,10 +120,10 @@ function CreateProductCategory(props: CreateProductCategoryProps) {
         is_internal: data.is_public.value === "private",
         parent_category_id: parentCategory?.id ?? null,
         metadata: getSubmittableMetadata(data.metadata),
-      })
+      });
       // TODO: temporary here, investigate why `useAdminCreateProductCategory` doesn't invalidate this
-      await queryClient.invalidateQueries(adminProductCategoryKeys.lists())
-      closeModal()
+      await queryClient.invalidateQueries(adminProductCategoryKeys.lists());
+      closeModal();
       notification(
         t("modals-success", "Success"),
         t(
@@ -120,17 +131,17 @@ function CreateProductCategory(props: CreateProductCategoryProps) {
           "Successfully created a category"
         ),
         "success"
-      )
+      );
     } catch (e) {
       const errorMessage =
         getErrorMessage(e) ||
         t(
           "modals-failed-to-create-a-new-category",
           "Failed to create a new category"
-        )
-      notification(t("modals-error", "Error"), errorMessage, "error")
+        );
+      notification(t("modals-error", "Error"), errorMessage, "error");
     }
-  }
+  };
 
   return (
     <FocusModal>
@@ -183,10 +194,12 @@ function CreateProductCategory(props: CreateProductCategoryProps) {
               label={t("modals-name", "Name") || "Name"}
               type="string"
               className="w-[338px]"
-              placeholder={t(
-                "modals-give-this-category-a-name",
-                "Give this category a name"
-              ) || "Give this category a name"}
+              placeholder={
+                t(
+                  "modals-give-this-category-a-name",
+                  "Give this category a name"
+                ) || "Give this category a name"
+              }
               {...register("name", { required: true })}
             />
 
@@ -194,7 +207,9 @@ function CreateProductCategory(props: CreateProductCategoryProps) {
               label={t("modals-handle", "Handle") || "Handle"}
               type="string"
               className="w-[338px]"
-              placeholder={t("modals-custom-handle", "Custom handle") || "Custom handle"}
+              placeholder={
+                t("modals-custom-handle", "Custom handle") || "Custom handle"
+              }
               {...register("handle")}
             />
           </div>
@@ -202,63 +217,75 @@ function CreateProductCategory(props: CreateProductCategoryProps) {
           <div className="mb-8">
             <TextArea
               label={t("modals-description", "Description")}
-              placeholder={t(
-                "modals-give-this-category-a-description",
-                "Give this category a description"
-              ) || "Give this category a description"}
+              placeholder={
+                t(
+                  "modals-give-this-category-a-description",
+                  "Give this category a description"
+                ) || "Give this category a description"
+              }
               {...register("description")}
             />
           </div>
 
           <div className="mb-8 flex justify-between gap-6">
             <div className="flex-1">
-            <Controller
-              name={'is_active'}
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => {
-                return (
-                  <NextSelect
-                    {...field}
-                    label={t("modals-status", "Status") || "Status"}
-                    placeholder="Choose a country"
-                    options={statusOptions(t)}
-                    value={statusOptions(t)[field.value?.value === 'active' ? 0 : 1]}
-                  />
-                )
-              }}
+              <Controller
+                name={"is_active"}
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => {
+                  return (
+                    <NextSelect
+                      {...field}
+                      label={t("modals-status", "Status") || "Status"}
+                      placeholder="Choose a country"
+                      options={statusOptions(t)}
+                      value={
+                        statusOptions(t)[
+                          field.value?.value === "active" ? 0 : 1
+                        ]
+                      }
+                    />
+                  );
+                }}
               />
             </div>
 
             <div className="flex-1">
-            <Controller
-              name={'is_public'}
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => {
-                return (
-                  <NextSelect
-                    {...field}
-                    label={t("modals-visibility", "Visibility") || "Visibility"}
-                    placeholder="Choose a country"
-                    options={visibilityOptions(t)}
-                    value={visibilityOptions(t)[field.value.value === 'public' ? 0 : 1]}
-                  />
-                )
-              }}
+              <Controller
+                name={"is_public"}
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => {
+                  return (
+                    <NextSelect
+                      {...field}
+                      label={
+                        t("modals-visibility", "Visibility") || "Visibility"
+                      }
+                      placeholder="Choose a country"
+                      options={visibilityOptions(t)}
+                      value={
+                        visibilityOptions(t)[
+                          field.value.value === "public" ? 0 : 1
+                        ]
+                      }
+                    />
+                  );
+                }}
               />
             </div>
           </div>
           <div className="mt-xlarge">
-              <h2 className="inter-base-semibold mb-base">
-                {t("collection-modal-metadata", "Metadata")}
-              </h2>
-              <MetadataForm form={nestedForm(form, "metadata")} />
-            </div>
+            <h2 className="inter-base-semibold mb-base">
+              {t("collection-modal-metadata", "Metadata")}
+            </h2>
+            <MetadataForm form={nestedForm(form, "metadata")} />
+          </div>
         </div>
       </FocusModal.Main>
     </FocusModal>
-  )
+  );
 }
 
-export default CreateProductCategory
+export default CreateProductCategory;
