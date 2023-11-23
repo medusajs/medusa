53bd841d4a05baee49f2d0cb92dd11a30b0ebaf0
adrien2p <adrien.deperetti@gmail.com>
2023-11-23 12:00:40 +0100

    WIP

--- packages/medusa/src/api/routes/store/carts/create-line-item/utils/handler-steps.ts
+++ packages/medusa/src/api/routes/store/carts/create-line-item/utils/handler-steps.ts
@@ -1,10 +1,17 @@
 import { FlagRouter } from "@medusajs/utils"
 import { AwilixContainer } from "awilix"
 import { EntityManager } from "typeorm"
-import { Cart, LineItem } from "../../../../../../models"
-import { CartService } from "../../../../../../services"
+import { Cart } from "../../../../../../models"
+import {
+  CartService,
+  LineItemService,
+  ProductVariantInventoryService,
+} from "../../../../../../services"
 import { WithRequiredProperty } from "../../../../../../types/common"
 import { IdempotencyCallbackResult } from "../../../../../../types/idempotency-key"
+import { defaultStoreCartFields, defaultStoreCartRelations } from "../../index"
+import SalesChannelFeatureFlag from "../../../../../../loaders/feature-flags/sales-channels"
+import { MedusaError } from "medusa-core-utils"
 
 export const CreateLineItemSteps = {
   STARTED: "started",
@@ -13,23 +20,61 @@ export const CreateLineItemSteps = {
 
 export async function handleAddOrUpdateLineItem(
   cartId: string,
-  {
-    cart,
-    line,
-  }: {
-    cart: Cart
-    line: LineItem
+  data: {
+    metadata?: Record<string, unknown>
+    customer_id?: string
+    variant_id: string
+    quantity: number
   },
   { container, manager }: { container: AwilixContainer; manager: EntityManager }
 ): Promise<IdempotencyCallbackResult> {
   const cartService: CartService = container.resolve("cartService")
+  const lineItemService: LineItemService = container.resolve("lineItemService")
   const featureFlagRouter: FlagRouter = container.resolve("featureFlagRouter")
 
+  const productVariantInventoryService: ProductVariantInventoryService =
+    container.resolve("productVariantInventoryService")
+
   const txCartService = cartService.withTransaction(manager)
+
+  let cart = await txCartService.retrieve(cartId, {
+    select: ["id", "region_id", "customer_id"],
+  })
+
+  const line = await lineItemService
+    .withTransaction(manager)
+    .generate(data.variant_id, cart.region_id, data.quantity, {
+      customer_id: data.customer_id || cart.customer_id,
+      metadata: data.metadata,
+    })
 
   await txCartService.addOrUpdateLineItems(cart.id, line, {
     validateSalesChannels: featureFlagRouter.isFeatureEnabled("sales_channels"),
   })
+
+  const relations = [
+    ...defaultStoreCartRelations,
+    "billing_address",
+    "region.payment_providers",
+    "payment_sessions",
+    "customer",
+  ]
+
+  const shouldSetAvailability =
+    relations?.some((rel) => rel.includes("variant")) &&
+    featureFlagRouter.isFeatureEnabled(SalesChannelFeatureFlag.key)
+
+  cart = await txCartService.retrieveWithTotals(cart.id, {
+    select: defaultStoreCartFields,
+    relations,
+  })
+
+  if (shouldSetAvailability) {
+    await productVariantInventoryService.setVariantAvailability(
+      cart.items.map((i) => i.variant),
+      cart.sales_channel_id!
+    )
+  }
 
   if (cart.payment_sessions?.length) {
     await txCartService.setPaymentSessions(
