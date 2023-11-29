2fe8ae9c1dfeb7a5dca2316933869d6b40fe2b19
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2023-11-29 13:05:02 +0000

    type

--- packages/medusa/src/loaders/index.ts
+++ packages/medusa/src/loaders/index.ts
@@ -49,7 +49,7 @@ async function loadLegacyModulesEntities(configModules, container) {
       continue
     }
 
-    let modulePath = isString(moduleConfig)
+    const modulePath = isString(moduleConfig)
       ? moduleConfig
       : (moduleConfig as InternalModuleDeclaration).resolve ??
         (definition.defaultPackage as string)
@@ -67,7 +67,7 @@ async function loadLegacyModulesEntities(configModules, container) {
         continue
       }
 
-      const module = await import(modulePath)
+      const module = await import(modulePath as string)
 
       if (module.default?.models) {
         module.default.models.map((model) =>
