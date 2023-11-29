d85e9c7df9eea620b399fd429090ae701ddbd02f
Philip Korsholm <philip.korsholm@hotmail.com>
2023-11-29 17:53:08 +0000

    move type conversion to the datalayer

--- packages/pricing/src/services/pricing-module.ts
+++ packages/pricing/src/services/pricing-module.ts
@@ -1406,8 +1406,6 @@ export default class PricingModuleService<
       const {
         rules = {},
         prices = [],
-        starts_at,
-        ends_at,
         ...priceListOnlyData
       } = priceListData
 
@@ -1415,8 +1413,6 @@ export default class PricingModuleService<
         [
           {
             ...priceListOnlyData,
-            starts_at: starts_at ? starts_at.toISOString() : undefined,
-            ends_at: ends_at ? ends_at.toISOString() : undefined,
             number_rules: Object.keys(rules).length,
           },
         ],
@@ -1563,17 +1559,9 @@ export default class PricingModuleService<
     )
 
     for (const priceListData of data) {
-      const { rules, starts_at, ends_at, ...priceListOnlyData } = priceListData
-      const updatePriceListData: any = {
+      const { rules, ...priceListOnlyData } = priceListData
+      const updatePriceListData = {
         ...priceListOnlyData,
-      }
-
-      if (starts_at) {
-        updatePriceListData.starts_at = starts_at!.toISOString()
-      }
-
-      if (ends_at) {
-        updatePriceListData.ends_at = ends_at!.toISOString()
       }
 
       if (typeof rules === "object") {
--- packages/pricing/src/types/repositories/price-list.ts
+++ packages/pricing/src/types/repositories/price-list.ts
@@ -3,8 +3,8 @@ import { PriceListStatus, PriceListType } from "@medusajs/utils"
 export interface CreatePriceListDTO {
   title: string
   description: string
-  starts_at?: string
-  ends_at?: string
+  starts_at?: Date | null
+  ends_at?: Date | null
   status?: PriceListStatus
   type?: PriceListType
   number_rules?: number
