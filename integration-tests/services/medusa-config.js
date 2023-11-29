ca70fd8ffc17bd6d442162c84b0d0b4b35450886
Philip Korsholm <philip.korsholm@hotmail.com>
2023-11-29 11:31:25 +0000

    shave down medusa-config

--- integration-tests/services/medusa-config.js
+++ integration-tests/services/medusa-config.js
@@ -4,26 +4,9 @@ const DB_USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_NAME = process.env.DB_TEMP_NAME
 const DB_URL = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
-process.env.POSTGRES_URL = DB_URL
 
 module.exports = {
-  plugins: [
-    {
-      resolve: `medusa-fulfillment-webshipper`,
-      options: {
-        account: "test-account",
-        api_token: "something",
-        order_channel_id: "1",
-        webhook_secret: "1234",
-      },
-    },
-    {
-      resolve: `medusa-plugin-sendgrid`,
-      options: {
-        api_key: "SG.TEST",
-      },
-    },
-  ],
+  plugins: [],
   projectConfig: {
     // redis_url: REDIS_URL,
     database_url: DB_URL,
