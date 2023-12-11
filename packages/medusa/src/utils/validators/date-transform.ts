6741d7dc3de550ed0767fd0d04944d447b75631f
Philip Korsholm <philip.korsholm@hotmail.com>
2023-12-11 11:11:58 +0100

    fix build

--- packages/medusa/src/utils/validators/date-transform.ts
+++ packages/medusa/src/utils/validators/date-transform.ts
@@ -4,6 +4,6 @@ export const transformDate = ({ value }): Date => {
   return !isDate(value) ? new Date(value) : new Date(Number(value) * 1000)
 }
 
-export const transformOptionalDate = (input) => {
-  return !isDate(input) ? input.value : transformDate(input)
+export const transformOptionalDate = ({ value }) => {
+  return !isDate(value) ? value : transformDate({ value })
 }
