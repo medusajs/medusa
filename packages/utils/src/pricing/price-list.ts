e15ffd987cf252cf8c093c4f17b97b5742a4e996
Philip Korsholm <philip.korsholm@hotmail.com>
2023-12-11 08:59:57 +0100

    make check for value exists

--- packages/utils/src/pricing/price-list.ts
+++ packages/utils/src/pricing/price-list.ts
@@ -1,4 +1,4 @@
-import { isDate, isString, MedusaError } from "../common"
+import { isDate, MedusaError } from "../common"
 
 export enum PriceListStatus {
   ACTIVE = "active",
@@ -14,14 +14,14 @@ export const validatePriceListDates = (priceListData: {
   starts_at?: Date | string | null
   ends_at?: Date | string | null
 }) => {
-  if (isString(priceListData.starts_at) && !isDate(priceListData.starts_at)) {
+  if (!!priceListData.starts_at && !isDate(priceListData.starts_at)) {
     throw new MedusaError(
       MedusaError.Types.INVALID_DATA,
       `Cannot set price list starts at with with invalid date string: ${priceListData.starts_at}`
     )
   }
 
-  if (isString(priceListData.ends_at) && !isDate(priceListData.ends_at)) {
+  if (!!priceListData.ends_at && !isDate(priceListData.ends_at)) {
     throw new MedusaError(
       MedusaError.Types.INVALID_DATA,
       `Cannot set price list ends at with with invalid date string: ${priceListData.ends_at}`
