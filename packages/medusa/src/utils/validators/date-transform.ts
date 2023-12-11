14d47c7774d5b8669eb578c335aa241fc13e1584
Philip Korsholm <philip.korsholm@hotmail.com>
2023-12-11 11:27:51 +0100

    flip transform condition

--- packages/medusa/src/utils/validators/date-transform.ts
+++ packages/medusa/src/utils/validators/date-transform.ts
@@ -1,7 +1,7 @@
 import { isDate } from "@medusajs/utils"
 
 export const transformDate = ({ value }): Date => {
-  return !isDate(value) ? new Date(value) : new Date(Number(value) * 1000)
+  return isDate(value) ? new Date(value) : new Date(Number(value) * 1000)
 }
 
 export const transformOptionalDate = ({ value }) => {
