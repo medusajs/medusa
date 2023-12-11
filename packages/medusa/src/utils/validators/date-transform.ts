0e4fb82c013cfd87f1c5b404050bc48548b1f888
Philip Korsholm <philip.korsholm@hotmail.com>
2023-12-11 10:07:06 +0100

    change utils

--- packages/medusa/src/utils/validators/date-transform.ts
+++ packages/medusa/src/utils/validators/date-transform.ts
@@ -1,11 +1,9 @@
+import { isDate } from "@medusajs/utils"
+
 export const transformDate = ({ value }): Date => {
-  return !isNaN(Date.parse(value))
-    ? new Date(value)
-    : new Date(Number(value) * 1000)
+  return !isDate(value) ? new Date(value) : new Date(Number(value) * 1000)
 }
 
 export const transformOptionalDate = (input) => {
-  return !input.value && !Number.isInteger(input.value)
-    ? input.value
-    : transformDate(input)
+  return !isDate(input) ? input.value : transformDate(input)
 }
