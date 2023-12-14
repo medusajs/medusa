041edcbdc95bd567bbc599663cf37b39d57997ca
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2023-12-14 11:13:21 -0300

    wip

---
+++ packages/medusa/src/services/workflow-orchestrator.ts
@@ -0,0 +1,83 @@
+import { Context, MedusaContainer } from "@medusajs/types"
+import { MedusaWorkflow } from "@medusajs/workflows-sdk"
+import { ulid } from "ulid"
+
+class WorkflowOrchestrator {
+  constructor() {
+    //
+  }
+  private static subscribers: Record<string, Function[]> = {}
+
+  static async run({
+    workflowId,
+    transactionId,
+    input,
+    context,
+    container,
+  }: {
+    workflowId: string
+    transactionId?: string
+    input?: any
+    context?: Context
+    container?: MedusaContainer
+  }) {
+    if (!workflowId) {
+      throw new Error("Workflow ID is required")
+    }
+
+    context ??= {}
+    context.transactionId ??= transactionId ?? ulid()
+
+    const events = {
+      onFinish: (transaction) => {
+        //WorkflowOrchestrator.notify(workflowId, data)
+      },
+      onStepBegin: ({ step, transaction }) => {
+        //
+      },
+    }
+
+    const flow = MedusaWorkflow.getWorkflow(workflowId)(container)
+
+    return await flow.run({
+      input,
+      context,
+      events,
+    })
+  }
+
+  static setSuccess({ workflowId, idempotencyKey, stepResponse }) {
+    //
+  }
+
+  static setFailure({ workflowId, idempotencyKey, stepResponse }) {
+    //
+  }
+
+  static subscribe(workflowId, handler) {
+    WorkflowOrchestrator.subscribers[workflowId] ??= []
+
+    WorkflowOrchestrator.subscribers[workflowId].push(handler)
+  }
+
+  static unsubscribe(workflowId, handler) {
+    if (!WorkflowOrchestrator.subscribers[workflowId]) {
+      return
+    }
+
+    WorkflowOrchestrator.subscribers[workflowId] =
+      WorkflowOrchestrator.subscribers[workflowId].filter((h) => h !== handler)
+  }
+
+  private static notify(workflowId, data) {
+    if (!WorkflowOrchestrator.subscribers[workflowId]) {
+      return
+    }
+
+    WorkflowOrchestrator.subscribers[workflowId].forEach((handler) => {
+      handler(data)
+    })
+  }
+}
+
+export default WorkflowOrchestrator
