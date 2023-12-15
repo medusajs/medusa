7c9e4ad023068bec7bc8f487d4fb96a9e9116f93
adrien2p <adrien.deperetti@gmail.com>
2023-12-15 16:29:01 +0100

    wip workflow orchestrator subrscribe

--- packages/medusa/src/services/workflow-orchestrator.ts
+++ packages/medusa/src/services/workflow-orchestrator.ts
@@ -1,26 +1,43 @@
-import { Context, MedusaContainer } from "@medusajs/types"
-import { MedusaWorkflow } from "@medusajs/workflows-sdk"
+import { FlowRunOptions, MedusaWorkflow } from "@medusajs/workflows-sdk"
+import {
+  DistributedTransaction,
+  DistributedTransactionEvents,
+} from "@medusajs/orchestration"
 import { ulid } from "ulid"
 
+type WorkflowOrchestratorRunOptions<T> = FlowRunOptions<T> & {
+  transactionId?: string
+  container?: { resolve<T = unknown>(key: string): T } // TODO: use ContainerLike once it is merged
+}
+
+type SubscribeOptions = {
+  workflowId: string
+  transactionId?: string
+  handler: (input: {
+    eventType: keyof DistributedTransactionEvents
+    result: unknown
+  }) => void
+}
+
+type NotifyOptions = {
+  workflowId: string
+  transactionId?: string
+  data: unknown
+}
+
 class WorkflowOrchestrator {
+  private static subscribers: Record<string, Function[]> = {}
+
   constructor() {
     //
   }
-  private static subscribers: Record<string, Function[]> = {}
 
-  static async run({
-    workflowId,
-    transactionId,
-    input,
-    context,
-    container,
-  }: {
-    workflowId: string
-    transactionId?: string
-    input?: any
-    context?: Context
-    container?: MedusaContainer
-  }) {
+  static async run<T = unknown>(
+    workflowId: string,
+    options?: WorkflowOrchestratorRunOptions<T>
+  ) {
+    let { input, context, transactionId, events: eventHandlers } = options ?? {}
+
     if (!workflowId) {
       throw new Error("Workflow ID is required")
     }
@@ -28,8 +45,28 @@ class WorkflowOrchestrator {
     context ??= {}
     context.transactionId ??= transactionId ?? ulid()
 
-    const events = {
+    const notify = (
+      eventType: keyof DistributedTransactionEvents,
+      data: unknown
+    ) => {
+      WorkflowOrchestrator.notify({ workflowId, transactionId, data })
+    }
+
+    const events: FlowRunOptions["events"] = {
+      onBegin: (transaction) => {},
+      onResume: (transaction) => {},
+      onCompensate: (transaction) => {},
+      onFinish: (transaction: DistributedTransaction, result: unknown) => {
+        WorkflowOrchestrator.notify(transaction.modelId, result)
+      },
+
       onStepBegin: ({ step, transaction }) => {
+        //
+      },
+      onStepSuccess: ({ step, transaction, result }) => {
+        //
+      },
+      onStepFailure: ({ step, transaction, error }) => {
         //
       },
     }
@@ -57,13 +94,13 @@ class WorkflowOrchestrator {
     //
   }
 
-  static subscribe(workflowId, handler) {
+  static subscribe({ workflowId, transactionId, handler }: SubscribeOptions) {
     WorkflowOrchestrator.subscribers[workflowId] ??= []
 
     WorkflowOrchestrator.subscribers[workflowId].push(handler)
   }
 
-  static unsubscribe(workflowId, handler) {
+  static unsubscribe({ workflowId, transactionId, handler }: SubscribeOptions) {
     if (!WorkflowOrchestrator.subscribers[workflowId]) {
       return
     }
@@ -72,7 +109,7 @@ class WorkflowOrchestrator {
       WorkflowOrchestrator.subscribers[workflowId].filter((h) => h !== handler)
   }
 
-  private static notify(workflowId, data) {
+  private static notify({ workflowId, transactionId, data }: NotifyOptions) {
     if (!WorkflowOrchestrator.subscribers[workflowId]) {
       return
     }
