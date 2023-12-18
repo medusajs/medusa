bda905a172d3149baee743ad652775507ff631a4
fPolic <mainacc.polic@gmail.com>
2023-12-18 11:58:02 +0100

    refactor: FF missing key

--- packages/medusa/src/models/cart-sales-channel.ts
+++ packages/medusa/src/models/cart-sales-channel.ts
@@ -1,11 +1,11 @@
 import { BeforeInsert, Column, Index } from "typeorm"
-import { MedusaV2Flag } from "@medusajs/utils"
+import { MedusaV2Flag, SalesChannelFeatureFlag } from "@medusajs/utils"
 
 import { generateEntityId } from "../utils"
 import { SoftDeletableEntity } from "../interfaces"
 import { FeatureFlagEntity } from "../utils/feature-flag-decorators"
 
-@FeatureFlagEntity([MedusaV2Flag.key, "sales_channels"])
+@FeatureFlagEntity([MedusaV2Flag.key, SalesChannelFeatureFlag.key])
 export class CartSalesChannel extends SoftDeletableEntity {
   @Index("cart_sales_channel_cart_id_unique", {
     unique: true,
