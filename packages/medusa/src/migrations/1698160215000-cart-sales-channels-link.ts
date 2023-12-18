01bcaa321c3d0893b51b9772f99861779761fa0e
fPolic <mainacc.polic@gmail.com>
2023-12-18 11:35:47 +0100

    fix: merge migrations

--- packages/medusa/src/migrations/1698160215000-cart-sales-channels-link.ts
+++ packages/medusa/src/migrations/1698160215000-cart-sales-channels-link.ts
@@ -25,6 +25,8 @@ export class CartSalesChannelsLink1698160215000 implements MigrationInterface {
             (select 'cartsc_' || substr(md5(random()::text), 0, 27), id, sales_channel_id from "cart" WHERE sales_channel_id IS NOT NULL);
 
         ALTER TABLE "cart" DROP CONSTRAINT IF EXISTS "FK_a2bd3c26f42e754b9249ba78fd6";
+
+        ALTER TABLE "store" DROP CONSTRAINT IF EXISTS "FK_61b0f48cccbb5f41c750bac7286";
     `)
   }
 
@@ -37,6 +39,8 @@ export class CartSalesChannelsLink1698160215000 implements MigrationInterface {
         DROP TABLE IF EXISTS "cart_sales_channel";
 
         ALTER TABLE "cart" ADD CONSTRAINT "FK_a2bd3c26f42e754b9249ba78fd6" FOREIGN KEY ("sales_channel_id") REFERENCES "sales_channel"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+        ALTER TABLE "store" ADD CONSTRAINT "FK_61b0f48cccbb5f41c750bac7286" FOREIGN KEY ("default_sales_channel_id") REFERENCES "sales_channel"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
     `)
   }
 }
