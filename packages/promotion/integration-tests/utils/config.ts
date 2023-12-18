eeda83bae5471b58954b8fb9a14a0e5df2c510fc
Riqwan Thamir <rmthamir@gmail.com>
2023-12-18 15:31:06 +0100

    chore: singularize module name

--- packages/promotion/integration-tests/utils/config.ts
+++ packages/promotion/integration-tests/utils/config.ts
@@ -2,5 +2,5 @@ import { ModuleServiceInitializeOptions } from "@medusajs/types"
 
 export const databaseOptions: ModuleServiceInitializeOptions["database"] = {
   schema: "public",
-  clientUrl: "medusa-pricing-test",
+  clientUrl: "medusa-promotion-test",
 }
