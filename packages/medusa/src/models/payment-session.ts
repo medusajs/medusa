e22c8f42383a00bd9aa5a4f47bc31bd1922bdd4f
Shahed nasser <shahednasser@gmail.com>
2023-12-18 10:55:43 +0200

    added tsdocs for payment processor

--- packages/medusa/src/models/payment-session.ts
+++ packages/medusa/src/models/payment-session.ts
@@ -13,11 +13,29 @@ import { generateEntityId } from "../utils"
 import { DbAwareColumn, resolveDbType } from "../utils/db-aware-column"
 import { Cart } from "./cart"
 
+/**
+ * The status of a payment session.
+ */
 export enum PaymentSessionStatus {
+  /**
+   * The payment is authorized.
+   */
   AUTHORIZED = "authorized",
+  /**
+   * The payment is pending.
+   */
   PENDING = "pending",
+  /**
+   * The payment requires an action.
+   */
   REQUIRES_MORE = "requires_more",
+  /**
+   * An error occurred while processing the payment.
+   */
   ERROR = "error",
+  /**
+   * The payment is canceled.
+   */
   CANCELED = "canceled",
 }
 
