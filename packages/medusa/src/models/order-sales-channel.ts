ea3cad74ed3bffeec5cb00e2d00a66cffaab5ac7
fPolic <mainacc.polic@gmail.com>
2024-01-02 13:03:14 +0100

    fix: address some comments

--- packages/medusa/src/models/order-sales-channel.ts
+++ packages/medusa/src/models/order-sales-channel.ts
@@ -1,11 +1,11 @@
 import { BeforeInsert, Column, Index } from "typeorm"
-import { MedusaV2Flag } from "@medusajs/utils"
+import { MedusaV2Flag, SalesChannelFeatureFlag } from "@medusajs/utils"
 
 import { generateEntityId } from "../utils"
 import { SoftDeletableEntity } from "../interfaces"
 import { FeatureFlagEntity } from "../utils/feature-flag-decorators"
 
-@FeatureFlagEntity([MedusaV2Flag.key, "sales_channels"])
+@FeatureFlagEntity([MedusaV2Flag.key, SalesChannelFeatureFlag.key])
 export class OrderSalesChannel extends SoftDeletableEntity {
   @Index("order_sales_channel_order_id_unique", {
     unique: true,
@@ -19,7 +19,6 @@ export class OrderSalesChannel extends SoftDeletableEntity {
   /**
    * @apiIgnore
    */
-  // NOTE: not working
   @BeforeInsert()
   private beforeInsert(): void {
     this.id = generateEntityId(this.id, "ordersc")
