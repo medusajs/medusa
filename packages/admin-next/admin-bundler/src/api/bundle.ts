737337bce2f0f79fb634d7cece062e2db67928ac
Kasper <kasper@medusajs.com>
2024-01-03 15:40:03 +0100

    use glob.sync

--- packages/admin-next/admin-bundler/src/api/bundle.ts
+++ packages/admin-next/admin-bundler/src/api/bundle.ts
@@ -1,29 +1,29 @@
-import { readFileSync } from "fs";
-import { glob } from "glob";
-import { relative, resolve } from "path";
-import { build as command } from "vite";
+import { readFileSync } from "fs"
+import glob from "glob"
+import { relative, resolve } from "path"
+import { build as command } from "vite"
 
 type BundleArgs = {
-  root?: string | undefined;
-  watch?: boolean | undefined;
-};
+  root?: string | undefined
+  watch?: boolean | undefined
+}
 
 export async function bundle({ watch, root }: BundleArgs) {
   const resolvedRoot = root
     ? resolve(process.cwd(), root)
-    : resolve(process.cwd(), "src", "admin");
+    : resolve(process.cwd(), "src", "admin")
 
-  const files = await glob(`${resolvedRoot}/**/*.{ts,tsx,js,jsx}`);
+  const files = glob.sync(`${resolvedRoot}/**/*.{ts,tsx,js,jsx}`)
 
-  const input: Record<string, string> = {};
+  const input: Record<string, string> = {}
   for (const file of files) {
-    const relativePath = relative(resolvedRoot, file);
-    input[relativePath] = file;
+    const relativePath = relative(resolvedRoot, file)
+    input[relativePath] = file
   }
 
   const packageJson = JSON.parse(
     readFileSync(resolve(process.cwd(), "package.json"), "utf-8")
-  );
+  )
   const external = [
     ...Object.keys(packageJson.dependencies),
     "@medusajs/ui",
@@ -32,7 +32,7 @@ export async function bundle({ watch, root }: BundleArgs) {
     "react-dom",
     "react-router-dom",
     "react-hook-form",
-  ];
+  ]
 
   await command({
     build: {
@@ -42,5 +42,5 @@ export async function bundle({ watch, root }: BundleArgs) {
         external: external,
       },
     },
-  });
+  })
 }
