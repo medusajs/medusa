757e752bd3436997076693a6577f3333875e51aa
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-01-05 09:59:46 -0300

    index

--- packages/workflow-orchestrator-inmemory/src/migrations/Migration20231228143900.ts
+++ packages/workflow-orchestrator-inmemory/src/migrations/Migration20231228143900.ts
@@ -15,10 +15,10 @@ export class Migration20231221104256 extends Migration {
             created_at timestamp WITHOUT time zone NOT NULL DEFAULT Now(),
             updated_at timestamp WITHOUT time zone NOT NULL DEFAULT Now(),
             deleted_at timestamp WITHOUT time zone NULL,
-            CONSTRAINT "PK_workflow_execution_id" PRIMARY KEY ("workflow_id", "transaction_id")
+            CONSTRAINT "PK_workflow_execution_workflow_id_transaction_id" PRIMARY KEY ("workflow_id", "transaction_id")
         );
         
-        CREATE INDEX IF NOT EXISTS "IDX_workflow_execution_id" ON "workflow_execution" ("id") WHERE deleted_at IS NULL;
+        CREATE UNIQUE INDEX IF NOT EXISTS "IDX_workflow_execution_id" ON "workflow_execution" ("id");
         CREATE INDEX IF NOT EXISTS "IDX_workflow_execution_workflow_id" ON "workflow_execution" ("workflow_id") WHERE deleted_at IS NULL;
         CREATE INDEX IF NOT EXISTS "IDX_workflow_execution_transaction_id" ON "workflow_execution" ("transaction_id") WHERE deleted_at IS NULL;
         CREATE INDEX IF NOT EXISTS "IDX_workflow_execution_state" ON "workflow_execution" ("state") WHERE deleted_at IS NULL;
