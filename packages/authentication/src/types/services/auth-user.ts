ab4aec21815819b97a650fe37c720efa7173d017
Philip Korsholm <philip.korsholm@hotmail.com>
2024-01-05 08:30:59 +0100

    fix build

--- packages/authentication/src/types/services/auth-user.ts
+++ packages/authentication/src/types/services/auth-user.ts
@@ -1,6 +1,9 @@
+import { AuthProviderDTO } from "./auth-provider"
+
 export type AuthUserDTO = {
   id: string
   provider_id: string
+  provider: AuthProviderDTO
   provider_metadata?: Record<string, unknown>
   user_metadata: Record<string, unknown>
   app_metadata: Record<string, unknown>
