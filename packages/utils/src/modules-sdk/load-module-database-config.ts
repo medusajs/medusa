860135aaa0822a7d5c44981a808e3ffce05cf515
adrien2p <adrien.deperetti@gmail.com>
2024-01-05 15:33:06 +0100

    turn db debug to false by default

--- packages/utils/src/modules-sdk/load-module-database-config.ts
+++ packages/utils/src/modules-sdk/load-module-database-config.ts
@@ -66,7 +66,7 @@ export function loadDatabaseConfig(
   "clientUrl" | "schema" | "driverOptions" | "debug"
 > {
   const clientUrl = getEnv("POSTGRES_URL", moduleName)
-  
+
   const database = {
     clientUrl,
     schema: getEnv("POSTGRES_SCHEMA", moduleName) ?? "public",
@@ -74,7 +74,7 @@ export function loadDatabaseConfig(
       getEnv("POSTGRES_DRIVER_OPTIONS", moduleName) ||
         JSON.stringify(getDefaultDriverOptions(clientUrl))
     ),
-    debug: process.env.NODE_ENV?.startsWith("dev") ?? false,
+    debug: false,
   }
 
   if (isModuleServiceInitializeOptions(options)) {
