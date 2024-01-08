bdae29a4d6674c6b9ed9042235a6b3305a7d98b1
olivermrbl <oliver@mrbltech.com>
2024-01-08 14:26:04 +0100

    fix admin command

--- packages/medusa/src/commands/utils/resolve-admin-cli.ts
+++ packages/medusa/src/commands/utils/resolve-admin-cli.ts
@@ -1,19 +1,13 @@
 import { resolveExecutable } from "./resolve-executable"
 
 export function resolveAdminCLI(directory) {
-  try {
-    const adminExecutable = resolveExecutable(
-      directory,
-      "@medusajs/admin",
-      "../../",
-      "bin",
-      "medusa-admin.js"
-    )
+  const adminExecutable = resolveExecutable(
+    directory,
+    "@medusajs/admin",
+    "../../",
+    "bin",
+    "medusa-admin.js"
+  )
 
-    console.log("Admin executable: ", adminExecutable)
-
-    return { cli: adminExecutable, binExists: true }
-  } catch (e) {
-    return { cli: null, binExists: false }
-  }
+  return { cli: adminExecutable, binExists: adminExecutable ? true : false }
 }
