3896f71adedc5436d5c23bebdb986f79d7d4970b
Kasper <kasper@medusajs.com>
2024-01-08 15:06:25 +0100

    fix w/h of sidebar btn

--- packages/admin-next/dashboard/src/components/layout/app-layout/topbar.tsx
+++ packages/admin-next/dashboard/src/components/layout/app-layout/topbar.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "@medusajs/icons"
+import { IconButton } from "@medusajs/ui"
 import { Breadcrumbs } from "./breadcrumbs"
 import { Notifications } from "./notifications"
 import { SearchToggle } from "./search-toggle"
@@ -7,7 +8,9 @@ export const Topbar = () => {
   return (
     <div className="hidden items-center justify-between px-3 py-1 md:flex">
       <div className="flex items-center gap-x-1.5">
-        <Sidebar className="text-ui-fg-muted" />
+        <IconButton variant="transparent" className="h-7 w-7 p-0">
+          <Sidebar className="text-ui-fg-muted" />
+        </IconButton>
         <Breadcrumbs />
       </div>
       <div className="text-ui-fg-muted flex items-center gap-x-1">
