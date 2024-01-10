ef5024980d08031d9f2415a6550b10f33cdfdc0c
Oli Juhl <59018053+olivermrbl@users.noreply.github.com>
2024-01-10 13:27:58 +0100

    feat(cart): Public-facing DTOs + (partial) module interface (#6000)

---
+++ packages/types/src/cart/mutations.ts
@@ -0,0 +1,133 @@
+export interface UpsertAddressDTO {
+  customer_id?: string
+  company?: string
+  first_name?: string
+  last_name?: string
+  address_1?: string
+  address_2?: string
+  city?: string
+  country_code?: string
+  province?: string
+  postal_code?: string
+  phone?: string
+  metadata?: Record<string, unknown>
+}
+
+export interface UpdateAddressDTO extends UpsertAddressDTO {
+  id: string
+}
+
+export interface CreateAddressDTO extends UpsertAddressDTO {}
+
+export interface CreateCartDTO {
+  region_id?: string
+  customer_id?: string
+  sales_channel_id?: string
+
+  email?: string
+  currency_code: string
+
+  shipping_address?: CreateAddressDTO | UpdateAddressDTO
+  billing_address?: CreateAddressDTO | UpdateAddressDTO
+
+  metadata?: Record<string, unknown>
+}
+
+export interface UpdateCartDTO {
+  id: string
+  region_id?: string
+  customer_id?: string
+  sales_channel_id?: string
+
+  email?: string
+  currency_code?: string
+
+  billing_address?: CreateAddressDTO | UpdateAddressDTO
+  shipping_address?: CreateAddressDTO | UpdateAddressDTO
+
+  metadata?: Record<string, unknown>
+}
+
+export interface CreateLineItemTaxLineDTO {
+  description?: string
+  tax_rate_id?: string
+  code: string
+  rate: number
+  provider_id?: string
+}
+
+export interface CreateLineItemAdjustmentDTO {
+  code: string
+  amount: number
+  description?: string
+  promotion_id?: string
+  provider_id?: string
+}
+
+export interface UpdateLineItemTaxLineDTO {
+  id: string
+  description?: string
+  tax_rate_id?: string
+  code?: string
+  rate?: number
+  provider_id?: string
+}
+
+export interface UpdateLineItemAdjustmentDTO {
+  id: string
+  code?: string
+  amount?: number
+  description?: string
+  promotion_id?: string
+  provider_id?: string
+}
+
+export interface CreateLineItemDTO {
+  title: string
+  subtitle?: string
+  thumbnail?: string
+
+  quantity: number
+
+  product_id?: string
+  product_title?: string
+  product_description?: string
+  product_subtitle?: string
+  product_type?: string
+  product_collection?: string
+  product_handle?: string
+
+  variant_id?: string
+  variant_sku?: string
+  variant_barcode?: string
+  variant_title?: string
+  variant_option_values?: Record<string, unknown>
+
+  requires_shipping?: boolean
+  is_discountable?: boolean
+  is_tax_inclusive?: boolean
+
+  compare_at_unit_price?: number
+  unit_price: number
+
+  tax_lines: CreateLineItemTaxLineDTO[]
+  adjustments: CreateLineItemAdjustmentDTO[]
+}
+
+export interface UpdateLineItemDTO
+  extends Omit<CreateLineItemDTO, "tax_lines" | "adjustments"> {
+  id: string
+
+  tax_lines: UpdateLineItemTaxLineDTO[] | CreateLineItemTaxLineDTO[]
+  adjustments: UpdateLineItemAdjustmentDTO[] | CreateLineItemAdjustmentDTO[]
+}
+
+export interface AddLineItemsDTO {
+  cart_id: string
+  items: CreateLineItemDTO[]
+}
+
+export interface UpdateLineItemsDTO {
+  cart_id: string
+  items: UpdateLineItemDTO[]
+}
