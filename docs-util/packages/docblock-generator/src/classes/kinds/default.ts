febc20f370ac34b01164cb0cb0ea6be4dfb6cc79
Shahed nasser <shahednasser@gmail.com>
2024-01-11 18:55:59 +0200

    added check for decorators

--- docs-util/packages/docblock-generator/src/classes/kinds/default.ts
+++ docs-util/packages/docblock-generator/src/classes/kinds/default.ts
@@ -55,7 +55,8 @@ class DefaultKind implements KindDocGenerator {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  getDocBlock(node: ts.Node): string {
+  getDocBlock(node: ts.Node, options = { addEnd: true }): string {
+    // const { addEnd = true } = options
     let str = this.getDocBlockStart(node)
 
     switch (node.kind) {
@@ -69,7 +70,7 @@ class DefaultKind implements KindDocGenerator {
         str += `{summary}`
     }
 
-    return `${str}${DOCKBLOCK_END_LINE}`
+    return `${str}${options.addEnd ? DOCKBLOCK_END_LINE : ""}`
   }
 
   getSymbolTypeDocBlock(symbolType: ts.Type): string {
@@ -114,13 +115,57 @@ class DefaultKind implements KindDocGenerator {
     return ts.displayPartsToString(commentDisplayParts)
   }
 
+  getDecoratorDocs(symbol: ts.Symbol): string {
+    let str = ""
+
+    symbol.declarations?.forEach((declaration) => {
+      const modifiers =
+        "modifiers" in declaration && declaration.modifiers
+          ? (declaration.modifiers as ts.NodeArray<ts.Modifier>)
+          : []
+
+      modifiers.forEach((modifier) => {
+        if (!ts.isDecorator(modifier)) {
+          return
+        }
+
+        // check for decorator text
+        ;(modifier as ts.Decorator).forEachChild((childNode) => {
+          if (ts.isCallExpression(childNode)) {
+            const childNodeExpression = (childNode as ts.CallExpression)
+              .expression
+            if (ts.isIdentifier(childNodeExpression)) {
+              switch (childNodeExpression.escapedText) {
+                case "FeatureFlagEntity":
+                  // add the `@featureFlag` tag.
+                  str += `${DOCBLOCK_NEW_LINE}${DOCBLOCK_NEW_LINE}@featureFlag [flag_name]`
+                  break
+                case "BeforeInsert":
+                case "BeforeLoad":
+                case "AfterLoad":
+                  // add `@apiIgnore` tag
+                  str += `${DOCBLOCK_NEW_LINE}${DOCBLOCK_NEW_LINE}@apiIgnore`
+              }
+            }
+          }
+        })
+      })
+    })
+
+    return str
+  }
+
   getCommonDocs(node: ts.Node): string {
     const tags = new Set<string>()
 
     const symbol = getSymbol(node, this.checker)
 
+    if (!symbol) {
+      return ""
+    }
+
     // check for private or protected modifiers
-    symbol?.declarations?.some((declaration) => {
+    symbol.declarations?.some((declaration) => {
       if (!("modifiers" in declaration) || !declaration.modifiers) {
         return false
       }
@@ -140,8 +185,34 @@ class DefaultKind implements KindDocGenerator {
       return true
     })
 
-    if (symbol?.getName().startsWith("_")) {
+    // if a symbol's name starts with `_` then we
+    // should add the `@ignore` tag
+    if (symbol.getName().startsWith("_")) {
       tags.add("@ignore")
+    }
+
+    // check if any docs can be added for the symbol's
+    // decorators
+    this.getDecoratorDocs(symbol)
+      .split(`${DOCBLOCK_NEW_LINE}${DOCBLOCK_NEW_LINE}`)
+      .filter((docItem) => docItem.length > 0)
+      .forEach((docItem) => tags.add(docItem))
+
+    // add `@expandable` tag if the resource is
+    if (ts.isPropertyDeclaration(node)) {
+      const symbolType = this.checker.getTypeOfSymbol(symbol)
+      if (
+        symbolType.symbol?.declarations?.length &&
+        ts.isClassDeclaration(symbolType.symbol?.declarations[0]) &&
+        this.isEntity({
+          heritageClauses: (
+            symbolType.symbol?.declarations[0] as ts.ClassDeclaration
+          ).heritageClauses,
+          node: symbolType.symbol?.declarations[0],
+        })
+      ) {
+        tags.add(`@expandable`)
+      }
     }
 
     let str = ""
@@ -152,6 +223,45 @@ class DefaultKind implements KindDocGenerator {
       str += `${tag}`
     })
     return str
+  }
+
+  isEntity({
+    heritageClauses,
+    node,
+  }: {
+    heritageClauses?: ts.NodeArray<ts.HeritageClause>
+    node?: ts.Node
+  }): boolean {
+    return (
+      heritageClauses?.some((heritageClause) => {
+        return heritageClause.types.some((heritageClauseType) => {
+          const symbolType = this.checker.getTypeAtLocation(
+            heritageClauseType.expression
+          )
+
+          if (
+            this.checker
+              .typeToString(symbolType, node, undefined)
+              .includes("BaseEntity")
+          ) {
+            return true
+          }
+
+          if (
+            symbolType.symbol.valueDeclaration &&
+            "heritageClauses" in symbolType.symbol.valueDeclaration
+          ) {
+            return this.isEntity({
+              heritageClauses: symbolType.symbol.valueDeclaration
+                .heritageClauses as ts.NodeArray<ts.HeritageClause>,
+              node,
+            })
+          }
+
+          return false
+        })
+      }) || false
+    )
   }
 }
 
--- docs-util/packages/docblock-generator/src/interface/KindDocGenerator.ts
+++ docs-util/packages/docblock-generator/src/interface/KindDocGenerator.ts
@@ -1,11 +1,15 @@
 import ts from "typescript"
 
+type GetDocBlockOptions = {
+  addEnd?: boolean
+}
+
 interface KindDocGenerator {
   getAllowedKinds(): ts.SyntaxKind[]
 
   isAllowed(node: ts.Node): boolean
 
-  getDocBlock(node: ts.Node): string
+  getDocBlock(node: ts.Node, options?: GetDocBlockOptions): string
 }
 
 export default KindDocGenerator
