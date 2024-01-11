824bcce3dd9916ede1cd83875b747bac60ca1fa8
fPolic <mainacc.polic@gmail.com>
2024-01-11 11:26:41 +0100

    feat: Payment module package setup

---
+++ packages/payment/src/scripts/seed-utils.ts
@@ -0,0 +1,8 @@
+import { SqlEntityManager } from "@mikro-orm/postgresql"
+
+export async function createPayments(
+  manager: SqlEntityManager,
+  paymentsData: any[]
+): Promise<any[]> {
+  return []
+}
