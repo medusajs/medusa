fce304ee1399ca9d381e3e330b915602b2d798e2
adrien2p <adrien.deperetti@gmail.com>
2024-01-15 13:33:08 +0100

    migrate migration scripts

--- packages/workflow-orchestrator-inmemory/src/index.ts
+++ packages/workflow-orchestrator-inmemory/src/index.ts
@@ -1,7 +1,22 @@
 import { moduleDefinition } from "./module-definition"
+import { Modules } from "@medusajs/modules-sdk"
+import * as models from "@models"
+import { ModulesSdkUtils } from "@medusajs/utils"
 
 export default moduleDefinition
 
+const migrationScriptOptions = {
+  moduleName: Modules.WORKFLOW_ORCHESTRATOR,
+  models: models,
+  pathToMigrations: __dirname + "/migrations",
+}
+
+export const runMigrations = ModulesSdkUtils.buildMigrationScript(
+  migrationScriptOptions
+)
+export const revertMigration = ModulesSdkUtils.buildRevertMigrationScript(
+  migrationScriptOptions
+)
+
 export * from "./initialize"
 export * from "./loaders"
-export * from "./scripts"
