56752039cfb8df126c69434d90907e89d0a186c6
fPolic <mainacc.polic@gmail.com>
2024-01-15 15:18:02 +0100

    wip: interface and types

--- packages/types/src/payment/service.ts
+++ packages/types/src/payment/service.ts
@@ -1,3 +1,74 @@
 import { IModuleService } from "../modules-sdk"
+import { Context } from "../shared-context"
+import {
+  CreatePaymentCollectionDTO,
+  UpdatePaymentCollectionDTO,
+} from "./mutations"
+import {
+  FilterablePaymentCollectionProps,
+  PaymentCollectionDTO,
+  PaymentDTO,
+} from "./common"
+import { FindConfig } from "../common"
 
-export interface IPaymentModuleService extends IModuleService {}
+export interface IPaymentModuleService extends IModuleService {
+  createPaymentCollection(
+    data: CreatePaymentCollectionDTO[],
+    sharedContext?: Context
+  ): Promise<PaymentCollectionDTO[]>
+  createPaymentCollection(
+    data: CreatePaymentCollectionDTO,
+    sharedContext?: Context
+  ): Promise<PaymentCollectionDTO>
+
+  retrievePaymentCollection(
+    paymentCollectionId: string,
+    config?: FindConfig<PaymentCollectionDTO>,
+    sharedContext?: Context
+  ): Promise<PaymentCollectionDTO>
+
+  listPaymentCollections(
+    filters?: FilterablePaymentCollectionProps,
+    config?: FindConfig<PaymentCollectionDTO>,
+    sharedContext?: Context
+  ): Promise<[PaymentCollectionDTO[], number]>
+
+  listAndCountPaymentCollections(
+    filters?: FilterablePaymentCollectionProps,
+    config?: FindConfig<PaymentCollectionDTO>,
+    sharedContext?: Context
+  ): Promise<[PaymentCollectionDTO[], number]>
+
+  updatePaymentCollection(
+    data: UpdatePaymentCollectionDTO[],
+    sharedContext?: Context
+  ): Promise<PaymentCollectionDTO[]>
+  updatePaymentCollection(
+    data: UpdatePaymentCollectionDTO,
+    sharedContext?: Context
+  ): Promise<PaymentCollectionDTO>
+
+  deletePaymentCollection(
+    paymentCollectionId: string[],
+    sharedContext?: Context
+  ): Promise<void>
+  deletePaymentCollection(
+    paymentCollectionId: string,
+    sharedContext?: Context
+  ): Promise<void>
+
+  authorizePaymentCollection(
+    paymentCollectionId: string,
+    sharedContext?: Context
+  ): Promise<PaymentCollectionDTO>
+
+  completePaymentCollection(
+    paymentCollectionId: string,
+    sharedContext?: Context
+  ): Promise<PaymentCollectionDTO>
+
+  capturePayment(paymentId: string): Promise<PaymentDTO>
+  refundPayment(paymentId: string): Promise<PaymentDTO>
+
+  createPayment()
+}
