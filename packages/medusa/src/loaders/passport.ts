79a801f3d662d19eadfb6586fdb554fe3b6e32da
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2024-01-17 07:25:24 +0100

    Update passport.ts

--- packages/medusa/src/loaders/passport.ts
+++ packages/medusa/src/loaders/passport.ts
@@ -7,13 +7,13 @@ import { AuthService } from "../services"
 import { ConfigModule } from "../types/global"
 import { MedusaRequest } from "../types/routing"
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
 
 export default async ({
   app,
