9e916dc1c8c47cced9c693a99a8e69de33b8b12c
Philip Korsholm <philip.korsholm@hotmail.com>
2024-01-17 14:00:23 +0100

    refactor google provider

--- packages/authentication/src/providers/google.ts
+++ packages/authentication/src/providers/google.ts
@@ -8,6 +8,7 @@ import { OAuth2 } from "oauth"
 import url, { UrlWithParsedQuery } from "url"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { MedusaError } from "@medusajs/utils"
+import { TreeLevelColumn } from "typeorm"
 
 type InjectedDependencies = {
   authUserService: AuthUserService
@@ -19,14 +20,6 @@ type GoogleProviderConfig = {
   clientID: string
   clientSecret: string
 }
-const nullStore = {
-  store: function (req, cb) {
-    return cb()
-  },
-  verify: function (req, state, cb) {
-    return cb(null, true)
-  },
-}
 
 class GoogleProvider extends AbstractAuthenticationModuleProvider {
   public static PROVIDER = "google"
@@ -37,7 +30,6 @@ class GoogleProvider extends AbstractAuthenticationModuleProvider {
 
   protected readonly authUserSerivce_: AuthUserService
   protected readonly authProviderService_: AuthProviderService
-  protected readonly _stateStore = nullStore
 
   constructor({ authUserService, authProviderService }: InjectedDependencies) {
     super()
@@ -60,69 +52,68 @@ class GoogleProvider extends AbstractAuthenticationModuleProvider {
     }
   }
 
-  async authenticate(
-    userData: Record<string, any>
-  ): Promise<AuthenticationResponse> {
-    const originalURL = (req) => {
-      var tls = req.connection.encrypted,
-        host = req.headers.host,
-        protocol = tls ? "https" : "http",
-        path = req.url || ""
-      return protocol + "://" + host + path
-    }
+  private originalURL(req) {
+    var tls = req.connection.encrypted,
+      host = req.headers.host,
+      protocol = tls ? "https" : "http",
+      path = req.url || ""
+    return protocol + "://" + host + path
+  }
 
-    const verify_ = async (request, accessToken, refreshToken, profile, done) => {
-      // decode email from jwt
-      const jwtData = await jwt.decode(refreshToken.id_token, { complete: true }) as JwtPayload | null
-      // const email = jwtData!.email
-      const entity_id = jwtData!.payload.email
+  async verify_(request, accessToken, refreshToken) {
+    // decode email from jwt
+    const jwtData = (await jwt.decode(refreshToken.id_token, {
+      complete: true,
+    })) as JwtPayload | null
+    // const email = jwtData!.email
+    const entity_id = jwtData!.payload.email
 
-      let authUser 
-      
-      try { 
-        authUser = await this.authUserSerivce_.retrieveByProviderAndEntityId(
-          entity_id,
-          GoogleProvider.PROVIDER
-        )
-      } catch (error) {
-        if(error.type === MedusaError.Types.NOT_FOUND) { 
-          authUser = await this.authUserSerivce_.create([{
+    let authUser
+
+    try {
+      authUser = await this.authUserSerivce_.retrieveByProviderAndEntityId(
+        entity_id,
+        GoogleProvider.PROVIDER
+      )
+    } catch (error) {
+      if (error.type === MedusaError.Types.NOT_FOUND) {
+        authUser = await this.authUserSerivce_.create([
+          {
             entity_id,
             provider_id: GoogleProvider.PROVIDER,
-            user_metadata: { 
-              email: jwtData!.payload.email,
-              email_verified: jwtData!.payload.email_verified,
-              name: jwtData!.payload.name,
-              picture: jwtData!.payload.picture,
-              locale: jwtData!.payload.locale,
-              family_name: jwtData!.payload.family_name,
-              given_name: jwtData!.payload.given_name,
-              hd: jwtData!.payload.hd,
-            },
-          }])
-        }
-        else { 
-          return done(error.message)
-        }
+            user_metadata: jwtData!.payload,
+          },
+        ])
+      } else {
+        return { success: false, error: error.message }
       }
-
-      return done(null, authUser)
     }
 
-    const req = userData
+    return { success: true, authUser }
+  }
 
-    const provider = await this.authProviderService_.retrieve(
+  async getProviderConfig(
+    req: Record<string, unknown>
+  ): Promise<GoogleProviderConfig> {
+    const { config } = await this.authProviderService_.retrieve(
       GoogleProvider.PROVIDER
     )
 
-    try {
-      this.validateConfig(provider.config || {})
-    } catch (error) {
-      return { success: false, error: error.message }
-    }
+    this.validateConfig(config || {})
 
-    let { callbackURL, clientID, clientSecret } =
-      provider.config as GoogleProviderConfig
+    const { callbackURL } = config as GoogleProviderConfig
+
+    const cbUrl = !url.parse(callbackURL).protocol
+      ? url.resolve(this.originalURL(req), callbackURL)
+      : callbackURL
+
+    return {...config, callbackURL: cbUrl } as GoogleProviderConfig
+  }
+
+  async authenticate(
+    userData: Record<string, any>
+  ): Promise<AuthenticationResponse> {
+    const req = userData
 
     if (req.query && req.query.error) {
       return {
@@ -131,141 +122,150 @@ class GoogleProvider extends AbstractAuthenticationModuleProvider {
       }
     }
 
-    // var callbackURL = provider./callbackURL;
-    if (callbackURL) {
-      var parsed = url.parse(callbackURL)
-      if (!parsed.protocol) {
-        // The callback URL is relative, resolve a fully qualified URL from the
-        // URL of the originating request.
-        callbackURL = url.resolve(originalURL(req), callbackURL)
-      }
+    let config
+
+    try {
+      config = await this.getProviderConfig(req)
+    } catch (error) {
+      return { success: false, error: error.message }
     }
+
+    let { callbackURL, clientID, clientSecret } = config
 
     var meta = {
       authorizationURL: this.authorizationUrl_,
       tokenURL: this.tokenUrl_,
       clientID,
-      callbackURL: callbackURL,
+      callbackURL,
+      clientSecret,
     }
 
+    const code = (req.query && req.query.code) || (req.body && req.body.code)
+
+    // Redirect to google
+    if (!code) {
+      return this.getRedirect(meta)
+    }
+
+    return await this.validateCallback(code, meta)
+  }
+
+  // abstractable
+  private async validateCallback(code: string, {
+    authorizationURL,
+    tokenURL,
+    clientID,
+    callbackURL,
+    clientSecret,
+  }: {
+    authorizationURL: string
+    tokenURL: string
+    clientID: string
+    callbackURL: string
+    clientSecret: string
+  }) {
     const oauth2 = new OAuth2(
       clientID,
       clientSecret,
       "",
-      meta.authorizationURL,
-      meta.tokenURL
+      authorizationURL,
+      tokenURL
     )
 
-    if ((req.query && req.query.code) || (req.body && req.body.code)) {
-      const stateStore = {
-        state: null,
+    let state = null
+    const setState = (newState) => {
+      state = newState
+    }
+   
+    try {
+      oauth2.getOAuthAccessToken(
+        code,
+        { grant_type: "authorization_code", redirect_uri: callbackURL },
+        this.getOAuthAccessTokenCallback(setState)
+      )
+    } catch (ex) {
+      return { success: false, error: ex }
+    }
 
-        setState: function (state) {
-          this.state = state
-        },
+    // wait for callback to resolve
+    while (state === null) {
+      await new Promise((resolve) => setTimeout(resolve, 50))
+    }
+
+    return state
+  }
+
+  private getOAuthAccessTokenCallback(setResult) {
+    return async (err, accessToken, refreshToken, params) => {
+      const result = await this.oathAccessTokenCallback(
+        err,
+        accessToken,
+        refreshToken,
+        params
+      )
+      setResult(result)
+    }
+  }
+
+  // abstractable
+  private async oathAccessTokenCallback(
+    err,
+    accessToken,
+    refreshToken,
+    params
+  ) {
+    if (err || !accessToken) {
+      return {
+        success: false,
+        error: "Failed to obtain access token",
+      }
+    }
+
+    try {
+      return await this.verify_(accessToken, refreshToken, params)
+    } catch (ex) {
+      return { error: ex.message, success: false }
+    }
+  }
+
+  // Abstractable
+  private getRedirect({
+    authorizationURL,
+    clientID,
+    callbackURL
+  }: {
+    authorizationURL: string
+    clientID: string
+    callbackURL: string
+  }) {
+    const params: {
+      response_type: string
+      redirect_uri: string
+      scope?: string
+    } = {
+      response_type: "code",
+      scope: "email profile",
+      redirect_uri: callbackURL,
+    }
+
+    try {
+      const parsed: Omit<UrlWithParsedQuery, "search"> & {
+        search?: string | null
+      } = url.parse(authorizationURL, true)
+
+      for (const key in params) {
+        parsed.query[key] = params[key]
       }
 
-      function loaded(err, ok) {
-          if (err) {
-            stateStore.setState({
-              success: false,
-              error: err,
-            })
-          }
-          if (!ok) {
-            // return self.fail(state, 403);
-            // TODO: fail with status
-            return stateStore.setState({
-              success: false,
-              error: "fail with status, 403",
-            })
-          }
+      parsed.query["client_id"] = clientID
 
-          var code = (req.query && req.query.code) || (req.body && req.body.code)
+      delete parsed.search
 
-          var params: {
-            grant_type: string
-            redirect_uri?: string
-            code_verifier?: string
-          } = { grant_type: "authorization_code" }
-          if (callbackURL) {
-            params.redirect_uri = callbackURL
-          }
+      var location = url.format(parsed)
 
-          oauth2.getOAuthAccessToken(
-            code,
-            params,
-            async (err, accessToken, refreshToken, params) => {
-              console.warn("testing")
-              if (err || !accessToken) {
-                return stateStore.setState({
-                  success: false,
-                  error: "Failed to obtain access token",
-                })
-              }
-
-              function verified(err, user, info) {
-                if (err) {
-                  stateStore.setState({ success: false, error: err })
-                } else if (!user) {
-                  stateStore.setState({ success: false, error: info })
-                } else {
-                  stateStore.setState({ success: true, authUser: user })
-                }
-              }
-
-              try {
-                await verify_(accessToken, refreshToken, params, {}, verified)
-              } catch (ex) {
-                stateStore.setState({ error: ex.message, success: false })
-              }
-            }
-          )
-      }
-
-      var state = (req.query && req.query.state) || (req.body && req.body.state)
-      try {
-        this._stateStore.verify(req, state, loaded) // TODO: call loaded
-      } catch (ex) {
-        return { success: false, error: ex }
-      }
-
-      while(stateStore.state === null) {
-        await new Promise((resolve) => setTimeout(resolve, 100))
-      }
-
-      return stateStore.state
-
-    } else {
-      var params: {
-        response_type: string
-        redirect_uri?: string
-        scope?: string
-      } = { response_type: "code", scope: "email profile" }
-      if (callbackURL) {
-        params.redirect_uri = callbackURL
-      }
-
-      function stored() {
-        var parsed: Omit<UrlWithParsedQuery, "search"> & {
-          search?: string | null
-        } = url.parse(oauth2._authorizeUrl, true)
-        for (const key in params) {
-          parsed.query[key] = params[key]
-        }
-
-        parsed.query["client_id"] = meta.clientID
-
-        delete parsed.search
-        var location = url.format(parsed)
-        return { success: true, location } // TODO: redirect
-      }
-
-      try {
-        return this._stateStore.store(req, stored)
-      } catch (ex) {
-        return { success: false, error: ex }
-      }
+      return { success: true, location }
+    } catch (ex) {
+      return { success: false, error: ex.message }
     }
   }
 }
