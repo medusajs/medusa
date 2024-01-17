1267b63f5f1aa078fc48db0b27217ea40ff3d6b5
Riqwan Thamir <rmthamir@gmail.com>
2024-01-18 00:10:31 +0100

    chore: remove api-v2

--- packages/medusa/src/api/routes/admin/promotions/[id]/route.ts
+++ packages/medusa/src/api/routes/admin/promotions/[id]/route.ts
@@ -1,7 +1,6 @@
 import { ModuleRegistrationName } from "@medusajs/modules-sdk"
 import { IPromotionModuleService } from "@medusajs/types"
-import { FindParams } from "../../../../types/common"
-import { MedusaRequest, MedusaResponse } from "../../../../types/routing"
+import { MedusaRequest, MedusaResponse } from "../../../../../types/routing"
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const promotionModuleService: IPromotionModuleService = req.scope.resolve(
@@ -15,5 +14,3 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
 
   res.status(200).json({ promotion })
 }
-
-export class AdminGetPromotionsPromotionParams extends FindParams {}
--- packages/medusa/src/api/routes/admin/promotions/route.ts
+++ packages/medusa/src/api/routes/admin/promotions/route.ts
@@ -1,8 +1,6 @@
 import { ModuleRegistrationName } from "@medusajs/modules-sdk"
 import { IPromotionModuleService } from "@medusajs/types"
-import { IsOptional, IsString } from "class-validator"
-import { extendedFindParamsMixin } from "../../../types/common"
-import { MedusaRequest, MedusaResponse } from "../../../types/routing"
+import { MedusaRequest, MedusaResponse } from "../../../../types/routing"
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const promotionModuleService: IPromotionModuleService = req.scope.resolve(
@@ -22,13 +20,4 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
     offset,
     limit,
   })
-}
-
-export class AdminGetPromotionsParams extends extendedFindParamsMixin({
-  limit: 100,
-  offset: 0,
-}) {
-  @IsString()
-  @IsOptional()
-  code?: string
 }
