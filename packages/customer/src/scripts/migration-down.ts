7640ab0a42a1255bbb6118049f75cf329f0f5814
Sebastian Rindom <skrindom@gmail.com>
2024-01-18 14:14:17 +0100

    fix: use script factory

--- packages/customer/src/scripts/migration-down.ts
+++ packages/customer/src/scripts/migration-down.ts
@@ -1,43 +1,13 @@
 import { Modules } from "@medusajs/modules-sdk"
-import { LoaderOptions, Logger, ModulesSdkTypes } from "@medusajs/types"
-import { DALUtils, ModulesSdkUtils } from "@medusajs/utils"
-import { EntitySchema } from "@mikro-orm/core"
+import { ModulesSdkUtils } from "@medusajs/utils"
 import * as Models from "@models"
 
-/**
- * This script is only valid for mikro orm managers. If a user provide a custom manager
- * he is in charge of reverting the migrations.
- * @param options
- * @param logger
- * @param moduleDeclaration
- */
-export async function revertMigration({
-  options,
-  logger,
-}: Pick<
-  LoaderOptions<ModulesSdkTypes.ModuleServiceInitializeOptions>,
-  "options" | "logger"
-> = {}) {
-  logger ??= console as unknown as Logger
-
-  const dbData = ModulesSdkUtils.loadDatabaseConfig(Modules.CUSTOMER, options)!
-  const entities = Object.values(Models) as unknown as EntitySchema[]
-  const pathToMigrations = __dirname + "/../migrations"
-
-  const orm = await DALUtils.mikroOrmCreateConnection(
-    dbData,
-    entities,
-    pathToMigrations
-  )
-
-  try {
-    const migrator = orm.getMigrator()
-    await migrator.down()
-
-    logger?.info("Customer module migration executed")
-  } catch (error) {
-    logger?.error(`Customer module migration failed to run - Error: ${error}`)
-  }
-
-  await orm.close()
+const migrationScriptOptions = {
+  moduleName: Modules.CUSTOMER,
+  models: Models,
+  pathToMigrations: __dirname + "/../migrations",
 }
+
+export const revertMigration = ModulesSdkUtils.buildRevertMigrationScript(
+  migrationScriptOptions
+)
