17d06b14aa43b9edfd09ad82641bf2b1f28338a2
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-01-18 10:49:58 -0300

    module rename

--- .eslintrc.js
+++ .eslintrc.js
@@ -99,7 +99,7 @@ module.exports = {
           "./packages/orchestration/tsconfig.json",
           "./packages/workflows-sdk/tsconfig.spec.json",
           "./packages/core-flows/tsconfig.spec.json",
-          "./packages/workflows-inmemory/tsconfig.spec.json",
+          "./packages/workflows-engine-inmemory/tsconfig.spec.json",
         ],
       },
       rules: {
