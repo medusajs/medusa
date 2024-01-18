c10d4f3fbe4bc9788732f66ae9147a830c49cb7d
fPolic <mainacc.polic@gmail.com>
2024-01-18 17:34:26 +0100

    refactor: use abstract service

--- packages/sales-channel/src/services/sales-channel.ts
+++ packages/sales-channel/src/services/sales-channel.ts
@@ -1,23 +1,8 @@
-import {
-  Context,
-  DAL,
-  FilterableSalesChannelProps,
-  FindConfig,
-  SalesChannelDTO,
-} from "@medusajs/types"
-import {
-  doNotForceTransaction,
-  InjectManager,
-  InjectTransactionManager,
-  MedusaContext,
-  ModulesSdkUtils,
-  retrieveEntity,
-  shouldForceTransaction,
-} from "@medusajs/utils"
+import { DAL } from "@medusajs/types"
+import { ModulesSdkUtils } from "@medusajs/utils"
 import { CreateSalesChannelDTO, UpdateSalesChannelDTO } from "@medusajs/types"
 
 import { SalesChannel } from "@models"
-import { SalesChannelRepository } from "@repositories"
 
 type InjectedDependencies = {
   salesChannelRepository: DAL.RepositoryService
@@ -25,114 +10,15 @@ type InjectedDependencies = {
 
 export default class SalesChannelService<
   TEntity extends SalesChannel = SalesChannel
-> {
-  protected readonly salesChannelRepository_: DAL.RepositoryService
-
-  constructor({ salesChannelRepository }: InjectedDependencies) {
-    this.salesChannelRepository_ = salesChannelRepository
+> extends ModulesSdkUtils.abstractServiceFactory<
+  InjectedDependencies,
+  {
+    create: CreateSalesChannelDTO
+    update: UpdateSalesChannelDTO
   }
-
-  @InjectTransactionManager(shouldForceTransaction, "salesChannelRepository_")
-  async create(
-    data: CreateSalesChannelDTO[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<SalesChannelDTO[]> {
-    return (await (
-      this.salesChannelRepository_ as SalesChannelRepository
-    ).create(data, sharedContext)) as unknown as SalesChannelDTO[]
-  }
-
-  @InjectTransactionManager(doNotForceTransaction, "salesChannelRepository_")
-  async delete(
-    ids: string[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<void> {
-    await (this.salesChannelRepository_ as SalesChannelRepository).delete(
-      ids,
-      sharedContext
-    )
-  }
-
-  @InjectTransactionManager("salesChannelRepository_")
-  async softDelete(
-    salesChannelIds: string[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<[TEntity[], Record<string, unknown[]>]> {
-    return await this.salesChannelRepository_.softDelete(
-      salesChannelIds,
-      sharedContext
-    )
-  }
-
-  @InjectTransactionManager("salesChannelRepository_")
-  async restore(
-    salesChannelIds: string[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<[TEntity[], Record<string, unknown[]>]> {
-    return await this.salesChannelRepository_.restore(
-      salesChannelIds,
-      sharedContext
-    )
-  }
-
-  @InjectTransactionManager(shouldForceTransaction, "salesChannelRepository_")
-  async update(
-    data: UpdateSalesChannelDTO[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<SalesChannelDTO[]> {
-    return (await (
-      this.salesChannelRepository_ as SalesChannelRepository
-    ).update(data, sharedContext)) as unknown as SalesChannelDTO[]
-  }
-
-  @InjectManager("salesChannelRepository_")
-  async retrieve(
-    salesChannelId: string,
-    config: FindConfig<SalesChannelDTO> = {},
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<SalesChannelDTO> {
-    return (await retrieveEntity<SalesChannel, SalesChannelDTO>({
-      id: salesChannelId,
-      entityName: SalesChannel.name,
-      repository: this.salesChannelRepository_,
-      config,
-      sharedContext,
-    })) as unknown as SalesChannelDTO
-  }
-
-  @InjectManager("salesChannelRepository_")
-  async list(
-    filters: {} = {},
-    config: FindConfig<SalesChannelDTO> = {},
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<SalesChannelDTO[]> {
-    const queryOptions = ModulesSdkUtils.buildQuery<SalesChannel>(
-      filters,
-      config
-    )
-
-    return (await this.salesChannelRepository_.find(
-      queryOptions,
-      sharedContext
-    )) as unknown as SalesChannelDTO[]
-  }
-
-  @InjectManager("salesChannelRepository_")
-  async listAndCount(
-    filters: FilterableSalesChannelProps = {},
-    config: FindConfig<SalesChannelDTO> = {},
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<[SalesChannelDTO[], number]> {
-    const queryOptions = ModulesSdkUtils.buildQuery<SalesChannel>(
-      filters,
-      config
-    )
-
-    return (await (
-      this.salesChannelRepository_ as SalesChannelRepository
-    ).findAndCount(queryOptions, sharedContext)) as unknown as [
-      SalesChannelDTO[],
-      number
-    ]
+>(SalesChannel)<TEntity> {
+  constructor(container: InjectedDependencies) {
+    // @ts-ignore
+    super(...arguments)
   }
 }
