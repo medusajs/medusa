a637b2965d9812b05b2dc6aa488d4d18bb4c9de7
olivermrbl <oliver@mrbltech.com>
2024-01-18 11:52:25 +0100

    fix test

--- packages/cart/src/models/shipping-method.ts
+++ packages/cart/src/models/shipping-method.ts
@@ -29,7 +29,7 @@ export default class ShippingMethod {
     index: "IDX_shipping_method_cart_id",
     nullable: true,
   })
-  cart: Cart | null = null
+  cart?: Cart | null
 
   @Property({ columnType: "text" })
   name: string
