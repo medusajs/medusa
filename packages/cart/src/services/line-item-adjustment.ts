b939be5aa03a0cac3453d7a8e85bf79f747cee92
olivermrbl <oliver@mrbltech.com>
2024-01-18 10:38:09 +0100

    use abstract service factory

--- packages/cart/src/services/line-item-adjustment.ts
+++ packages/cart/src/services/line-item-adjustment.ts
@@ -1,118 +1,21 @@
-import {
-  Context,
-  DAL,
-  FindConfig,
-  LineItemAdjustmentLineDTO,
-} from "@medusajs/types"
-import {
-  InjectManager,
-  InjectTransactionManager,
-  MedusaContext,
-  ModulesSdkUtils,
-  retrieveEntity,
-} from "@medusajs/utils"
-import { Address, LineItemAdjustmentLine } from "@models"
-import { LineItemAdjustmentRepository } from "@repositories"
+import { DAL } from "@medusajs/types"
+import { ModulesSdkUtils } from "@medusajs/utils"
+import { LineItemAdjustmentLine } from "@models"
 import {
   CreateLineItemAdjustmentDTO,
   UpdateLineItemAdjustmentDTO,
 } from "src/types/line-item-adjustment"
-import CartService from "./cart"
 
 type InjectedDependencies = {
   lineItemAdjustmentRepository: DAL.RepositoryService
-  cartService: CartService
 }
 
 export default class LineItemAdjustmentService<
   TEntity extends LineItemAdjustmentLine = LineItemAdjustmentLine
-> {
-  protected readonly lineItemAdjustmentRepository_: DAL.RepositoryService
-  protected readonly cartService_: CartService
-
-  constructor({
-    lineItemAdjustmentRepository,
-    cartService,
-  }: InjectedDependencies) {
-    this.lineItemAdjustmentRepository_ = lineItemAdjustmentRepository
-    this.cartService_ = cartService
+> extends ModulesSdkUtils.abstractServiceFactory<
+  InjectedDependencies,
+  {
+    create: CreateLineItemAdjustmentDTO
+    update: UpdateLineItemAdjustmentDTO
   }
-
-  @InjectManager("lineItemAdjustmentRepository_")
-  async retrieve(
-    id: string,
-    config: FindConfig<LineItemAdjustmentLineDTO> = {},
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<TEntity> {
-    return (await retrieveEntity<
-      LineItemAdjustmentLine,
-      LineItemAdjustmentLineDTO
-    >({
-      id: id,
-      entityName: Address.name,
-      repository: this.lineItemAdjustmentRepository_,
-      config,
-      sharedContext,
-    })) as TEntity
-  }
-
-  @InjectManager("lineItemAdjustmentRepository_")
-  async list(
-    filters: any = {},
-    config: FindConfig<LineItemAdjustmentLineDTO> = {},
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<TEntity[]> {
-    const queryOptions = ModulesSdkUtils.buildQuery<LineItemAdjustmentLine>(
-      filters,
-      config
-    )
-
-    return (await this.lineItemAdjustmentRepository_.find(
-      queryOptions,
-      sharedContext
-    )) as TEntity[]
-  }
-
-  @InjectManager("lineItemAdjustmentRepository_")
-  async listAndCount(
-    filters: any = {},
-    config: FindConfig<LineItemAdjustmentLineDTO> = {},
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<[TEntity[], number]> {
-    const queryOptions = ModulesSdkUtils.buildQuery<LineItemAdjustmentLine>(
-      filters,
-      config
-    )
-
-    return (await this.lineItemAdjustmentRepository_.findAndCount(
-      queryOptions,
-      sharedContext
-    )) as [TEntity[], number]
-  }
-
-  @InjectTransactionManager("lineItemAdjustmentRepository_")
-  async create(
-    data: CreateLineItemAdjustmentDTO[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<TEntity[]> {
-    return (await (
-      this.lineItemAdjustmentRepository_ as LineItemAdjustmentRepository
-    ).create(data, sharedContext)) as TEntity[]
-  }
-
-  @InjectTransactionManager("lineItemAdjustmentRepository_")
-  async delete(
-    ids: string[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<void> {
-    await this.lineItemAdjustmentRepository_.delete(ids, sharedContext)
-  }
-
-  @InjectTransactionManager("lineItemAdjustmentRepository_")
-  async update(
-    data: UpdateLineItemAdjustmentDTO[],
-    @MedusaContext() sharedContext: Context = {}
-  ): Promise<void> {
-    await this.lineItemAdjustmentRepository_.update(data, sharedContext)
-  }
-}
+>(LineItemAdjustmentLine)<TEntity> {}
