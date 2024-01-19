049edb62f6388db93089fcd0157b8d60b1df2880
adrien2p <adrien.deperetti@gmail.com>
2024-01-19 13:05:33 +0100

    migrate pricing module

--- packages/pricing/src/index.ts
+++ packages/pricing/src/index.ts
@@ -1,8 +1,23 @@
 import { moduleDefinition } from "./module-definition"
+import { Modules } from "@medusajs/modules-sdk"
+import * as Models from "@models"
+import { ModulesSdkUtils } from "@medusajs/utils"
 
 export default moduleDefinition
 
-export * from "./scripts"
+const migrationScriptOptions = {
+  moduleName: Modules.PRICING,
+  models: Models,
+  pathToMigrations: __dirname + "/migrations",
+}
+
+export const runMigrations = ModulesSdkUtils.buildMigrationScript(
+  migrationScriptOptions
+)
+export const revertMigration = ModulesSdkUtils.buildRevertMigrationScript(
+  migrationScriptOptions
+)
+
 export * from "./initialize"
 export * from "./types"
 export * from "./loaders"
