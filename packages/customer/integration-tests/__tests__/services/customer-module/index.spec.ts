ea84b70be658c6bef4f4757c68d3b60a13fdc117
Sebastian Rindom <skrindom@gmail.com>
2024-01-19 16:31:38 +0100

    feat: add customer group methods

--- packages/customer/integration-tests/__tests__/services/customer-module/index.spec.ts
+++ packages/customer/integration-tests/__tests__/services/customer-module/index.spec.ts
@@ -37,5 +37,48 @@ describe("Customer Module Service", () => {
         })
       )
     })
+
+    it.only("playground", async () => {
+      const cus1 = await service.create({
+        first_name: "John",
+        last_name: "Doe",
+      })
+
+      const cus2 = await service.create({
+        first_name: "Jane",
+        last_name: "Doe",
+      })
+
+      const group1 = await service.createCustomerGroup({ name: "Group 1" })
+      const group2 = await service.createCustomerGroup({ name: "Group 2" })
+
+      await service.addCustomerToGroup({
+        customer_id: cus2.id,
+        customer_group_id: group2.id,
+      })
+
+      await service.addCustomerToGroup([
+        { customer_id: cus1.id, customer_group_id: group1.id },
+      ])
+
+      const groups = await service.listCustomerGroups(
+        { customers: cus1.id },
+        {
+          relations: ["customers"],
+        }
+      )
+
+      const customers = await service.list(
+        {},
+        {
+          relations: ["groups"],
+        }
+      )
+
+      console.log(JSON.stringify(groups, null, 2))
+      console.log(JSON.stringify(customers, null, 2))
+
+      expect(true).toEqual(true)
+    })
   })
 })
--- packages/types/src/customer/common.ts
+++ packages/types/src/customer/common.ts
@@ -1,4 +1,42 @@
+import { BaseFilterable } from "../dal"
+import { OperatorMap } from "../dal/utils"
 import { AddressDTO } from "../address"
+
+export interface CustomerGroupDTO {
+  id: string
+  name: string
+  customers?: Partial<CustomerDTO>[]
+  metadata?: Record<string, unknown>
+  created_by?: string | null
+  deleted_at?: Date | string | null
+  created_at?: Date | string
+  updated_at?: Date | string
+}
+
+export interface FilterableCustomerGroupProps
+  extends BaseFilterable<FilterableCustomerGroupProps> {
+  id?: string | string[]
+  name?: OperatorMap<string>
+  customers?: FilterableCustomerProps | string | string[]
+  created_by?: string | string[] | null
+  created_at?: OperatorMap<string>
+  updated_at?: OperatorMap<string>
+}
+
+export interface FilterableCustomerProps
+  extends BaseFilterable<FilterableCustomerProps> {
+  id?: string | string[]
+  email?: string | string[] | OperatorMap<string>
+  groups?: FilterableCustomerGroupProps | string | string[]
+  default_billing_address_id?: string | string[] | null
+  default_shipping_address_id?: string | string[] | null
+  company_name?: string | string[] | OperatorMap<string> | null
+  first_name?: string | string[] | OperatorMap<string> | null
+  last_name?: string | string[] | OperatorMap<string> | null
+  created_by?: string | string[] | null
+  created_at?: OperatorMap<string>
+  updated_at?: OperatorMap<string>
+}
 
 export interface CustomerDTO {
   id: string
@@ -14,7 +52,7 @@ export interface CustomerDTO {
   phone?: string | null
   groups?: { id: string }[]
   metadata?: Record<string, unknown>
-  deleted_at?: Date | string
+  deleted_at?: Date | string | null
   created_at?: Date | string
   updated_at?: Date | string
 }
