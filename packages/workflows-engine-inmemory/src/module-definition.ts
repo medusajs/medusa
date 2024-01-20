d6bb9f61ac4c88756d7c962df3ecb06bc74a655c
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-01-20 10:32:16 -0300

    loaders

--- packages/workflows-engine-inmemory/src/module-definition.ts
+++ packages/workflows-engine-inmemory/src/module-definition.ts
@@ -2,9 +2,10 @@ import { ModuleExports } from "@medusajs/types"
 import { WorkflowsModuleService } from "@services"
 import loadConnection from "./loaders/connection"
 import loadContainer from "./loaders/container"
+import loadUtils from "./loaders/utils"
 
 const service = WorkflowsModuleService
-const loaders = [loadContainer, loadConnection] as any
+const loaders = [loadContainer, loadConnection, loadUtils] as any
 
 export const moduleDefinition: ModuleExports = {
   service,
--- packages/workflows-engine-redis/src/module-definition.ts
+++ packages/workflows-engine-redis/src/module-definition.ts
@@ -3,9 +3,15 @@ import { WorkflowsModuleService } from "@services"
 import loadConnection from "./loaders/connection"
 import loadContainer from "./loaders/container"
 import redisConnection from "./loaders/redis"
+import loadUtils from "./loaders/utils"
 
 const service = WorkflowsModuleService
-const loaders = [loadContainer, loadConnection, redisConnection] as any
+const loaders = [
+  loadContainer,
+  loadConnection,
+  loadUtils,
+  redisConnection,
+] as any
 
 export const moduleDefinition: ModuleExports = {
   service,
