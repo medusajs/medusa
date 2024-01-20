b1a24c2a5efba3d7f383241749201e4c2318bb59
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-01-20 09:09:43 -0300

    default container loader

--- packages/workflows-engine-inmemory/src/loaders/container.ts
+++ packages/workflows-engine-inmemory/src/loaders/container.ts
@@ -1,57 +1,9 @@
-import * as defaultRepositories from "@repositories"
+import { MikroOrmBaseRepository, ModulesSdkUtils } from "@medusajs/utils"
+import * as ModuleModels from "@models"
+import * as ModuleServices from "@services"
 
-import { LoaderOptions } from "@medusajs/modules-sdk"
-import { ModulesSdkTypes } from "@medusajs/types"
-import { loadCustomRepositories } from "@medusajs/utils"
-import * as defaultServices from "@services"
-import { asClass } from "awilix"
-import { InMemoryDistributedTransactionStorage } from "../utils"
-
-export default async ({
-  container,
-  options,
-  logger,
-}: LoaderOptions<
-  | ModulesSdkTypes.ModuleServiceInitializeOptions
-  | ModulesSdkTypes.ModuleServiceInitializeCustomDataLayerOptions
->): Promise<void> => {
-  logger?.warn(
-    "Workflows Engine In Memory installed. This is not recommended for production."
-  )
-
-  const customRepositories = (
-    options as ModulesSdkTypes.ModuleServiceInitializeCustomDataLayerOptions
-  )?.repositories
-
-  container.register({
-    workflowExecutionService: asClass(
-      defaultServices.WorkflowExecutionService
-    ).singleton(),
-    workflowOrchestratorService: asClass(
-      defaultServices.WorkflowOrchestratorService
-    ).singleton(),
-
-    inMemoryDistributedTransactionStorage: asClass(
-      InMemoryDistributedTransactionStorage
-    ).singleton(),
-  })
-
-  if (customRepositories) {
-    loadCustomRepositories({
-      defaultRepositories,
-      customRepositories,
-      container,
-    })
-  } else {
-    loadDefaultRepositories({ container })
-  }
-}
-
-function loadDefaultRepositories({ container }) {
-  container.register({
-    baseRepository: asClass(defaultRepositories.BaseRepository).singleton(),
-    workflowExecutionRepository: asClass(
-      defaultRepositories.WorkflowExecutionRepository
-    ).singleton(),
-  })
-}
+export default ModulesSdkUtils.moduleContainerLoaderFactory({
+  moduleModels: ModuleModels,
+  moduleServices: ModuleServices,
+  moduleRepositories: { BaseRepository: MikroOrmBaseRepository },
+})
--- packages/workflows-engine-redis/src/loaders/container.ts
+++ packages/workflows-engine-redis/src/loaders/container.ts
@@ -1,54 +1,9 @@
-import * as defaultRepositories from "@repositories"
+import { MikroOrmBaseRepository, ModulesSdkUtils } from "@medusajs/utils"
+import * as ModuleModels from "@models"
+import * as ModuleServices from "@services"
 
-import { LoaderOptions } from "@medusajs/modules-sdk"
-import { ModulesSdkTypes } from "@medusajs/types"
-import { loadCustomRepositories } from "@medusajs/utils"
-import * as defaultServices from "@services"
-import { asClass } from "awilix"
-import { RedisDistributedTransactionStorage } from "../utils"
-
-export default async ({
-  container,
-  options,
-  logger,
-}: LoaderOptions<
-  | ModulesSdkTypes.ModuleServiceInitializeOptions
-  | ModulesSdkTypes.ModuleServiceInitializeCustomDataLayerOptions
->): Promise<void> => {
-  const customRepositories = (
-    options as ModulesSdkTypes.ModuleServiceInitializeCustomDataLayerOptions
-  )?.repositories
-
-  container.register({
-    workflowExecutionService: asClass(
-      defaultServices.WorkflowExecutionService
-    ).singleton(),
-
-    workflowOrchestratorService: asClass(
-      defaultServices.WorkflowOrchestratorService
-    ).singleton(),
-
-    redisDistributedTransactionStorage: asClass(
-      RedisDistributedTransactionStorage
-    ).singleton(),
-  })
-
-  if (customRepositories) {
-    loadCustomRepositories({
-      defaultRepositories,
-      customRepositories,
-      container,
-    })
-  } else {
-    loadDefaultRepositories({ container })
-  }
-}
-
-function loadDefaultRepositories({ container }) {
-  container.register({
-    baseRepository: asClass(defaultRepositories.BaseRepository).singleton(),
-    workflowExecutionRepository: asClass(
-      defaultRepositories.WorkflowExecutionRepository
-    ).singleton(),
-  })
-}
+export default ModulesSdkUtils.moduleContainerLoaderFactory({
+  moduleModels: ModuleModels,
+  moduleServices: ModuleServices,
+  moduleRepositories: { BaseRepository: MikroOrmBaseRepository },
+})
