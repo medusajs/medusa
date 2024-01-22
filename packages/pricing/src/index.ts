6528072a861b44483cece200c8aae345d5b1326d
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-01-22 14:20:30 -0300

    update modules definition

--- packages/pricing/src/index.ts
+++ packages/pricing/src/index.ts
@@ -1,25 +1,9 @@
 import { moduleDefinition } from "./module-definition"
-import { Modules } from "@medusajs/modules-sdk"
-import * as Models from "@models"
-import { ModulesSdkUtils } from "@medusajs/utils"
 
 export default moduleDefinition
 
-const migrationScriptOptions = {
-  moduleName: Modules.PRICING,
-  models: Models,
-  pathToMigrations: __dirname + "/migrations",
-}
-
-export const runMigrations = ModulesSdkUtils.buildMigrationScript(
-  migrationScriptOptions
-)
-export const revertMigration = ModulesSdkUtils.buildRevertMigrationScript(
-  migrationScriptOptions
-)
-
 export * from "./initialize"
-export * from "./types"
 export * from "./loaders"
 export * from "./models"
 export * from "./services"
+export * from "./types"
--- packages/product/src/index.ts
+++ packages/product/src/index.ts
@@ -1,25 +1,9 @@
 import { moduleDefinition } from "./module-definition"
-import { ModulesSdkUtils } from "@medusajs/utils"
-import { Modules } from "@medusajs/modules-sdk"
-import * as ProductModels from "@models"
 
 export default moduleDefinition
 
-const migrationScriptOptions = {
-  moduleName: Modules.PRODUCT,
-  models: ProductModels,
-  pathToMigrations: __dirname + "/migrations",
-}
-
-export const runMigrations = ModulesSdkUtils.buildMigrationScript(
-  migrationScriptOptions
-)
-export const revertMigration = ModulesSdkUtils.buildRevertMigrationScript(
-  migrationScriptOptions
-)
-
 export * from "./initialize"
-export * from "./types"
 export * from "./loaders"
 export * from "./models"
 export * from "./services"
+export * from "./types"
