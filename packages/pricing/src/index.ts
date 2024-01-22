a8fab05a141f07befadac356c310ff7e4a35c22e
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-01-22 16:59:48 -0300

    pricing exporting models

--- packages/pricing/src/index.ts
+++ packages/pricing/src/index.ts
@@ -8,4 +8,6 @@ export default moduleDefinition
 export { revertMigration, runMigrations }
 
 export * from "./initialize"
+// TODO: remove models exported from here
+export * from "./models"
 export * from "./types"
