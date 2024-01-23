720177fdb91ea6a9fb436772b88b06d8b85ecfc7
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-01-22 21:03:25 -0300

    export service

--- packages/pricing/src/index.ts
+++ packages/pricing/src/index.ts
@@ -8,6 +8,7 @@ export default moduleDefinition
 export { revertMigration, runMigrations }
 
 export * from "./initialize"
-// TODO: remove models exported from here
+// TODO: remove export from models and services
 export * from "./models"
+export * from "./services"
 export * from "./types"
--- packages/product/src/index.ts
+++ packages/product/src/index.ts
@@ -8,4 +8,7 @@ export default moduleDefinition
 export { revertMigration, runMigrations }
 
 export * from "./initialize"
+// TODO: remove export from models and services
+export * from "./models"
+export * from "./services"
 export * from "./types"
