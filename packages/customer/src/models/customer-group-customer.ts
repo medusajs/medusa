a5403b4e132ff8f181132b540380a722d60d09ec
Sebastian Rindom <skrindom@gmail.com>
2024-01-24 11:14:33 +0100

    fix: revert comment change

--- packages/customer/src/models/customer-group-customer.ts
+++ packages/customer/src/models/customer-group-customer.ts
@@ -13,7 +13,7 @@ import {
 import Customer from "./customer"
 import CustomerGroup from "./customer-group"
 
-type OptionalGroupProps = "customer_group" | "customer" | DAL.EntityDateColumns
+type OptionalGroupProps = "customer_group" | "customer" | DAL.EntityDateColumns // TODO: To be revisited when more clear
 
 @Entity({ tableName: "customer_group_customer" })
 export default class CustomerGroupCustomer {
