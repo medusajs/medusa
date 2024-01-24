118d5cd8e64ed6830cd94d231efdda5306607a87
adrien2p <adrien.deperetti@gmail.com>
2024-01-24 13:51:50 +0100

    fix tests

--- integration-tests/plugins/__tests__/workflows/product/create-product.ts
+++ integration-tests/plugins/__tests__/workflows/product/create-product.ts
@@ -11,7 +11,7 @@ import { getContainer } from "../../../../environment-helpers/use-container"
 import { initDb, useDb } from "../../../../environment-helpers/use-db"
 import { pipe } from "@medusajs/workflows-sdk"
 
-jest.setTimeout(30000)
+jest.setTimeout(50000)
 
 describe("CreateProduct workflow", function () {
   let medusaContainer
@@ -131,7 +131,7 @@ describe("CreateProduct workflow", function () {
 
     expect(product).toEqual(
       expect.objectContaining({
-        deleted_at: expect.any(String),
+        deleted_at: expect.any(Date),
       })
     )
   })
