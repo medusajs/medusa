9d49a5d43333eca2f7f26ba659f4c18f4ea4f66b
Philip Korsholm <philip.korsholm@hotmail.com>
2024-01-26 17:18:26 +0800

    rm auth endpoints from branch

--- packages/medusa/src/api-v2/auth/middlewares.ts
+++
@@ -1,8 +0,0 @@
-import { MiddlewareRoute } from "../../loaders/helpers/routing/types"
-
-export const adminCampaignRoutesMiddlewares: MiddlewareRoute[] = [
-  {
-    matcher: "/auth/:scope/:auth-provider",
-    middlewares: [],
-  },
-]
