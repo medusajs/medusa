44dbffd618bd36803548a6355f8b72a5c9553486
Philip Korsholm <philip.korsholm@hotmail.com>
2024-01-26 17:43:48 +0800

    cleanup for pr

--- packages/medusa/src/api-v2/store/customers/route.ts
+++ packages/medusa/src/api-v2/store/customers/route.ts
@@ -1,16 +1,11 @@
-import { MedusaRequest, MedusaResponse } from "../../../types/routing";
-import { createStoreCustomersWorkflow } from "@medusajs/core-flows";
-import { CreateCustomerDTO } from "@medusajs/types";
-
-// export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
-//
-// }
+import { MedusaRequest, MedusaResponse } from "../../../types/routing"
+import { createStoreCustomersWorkflow } from "@medusajs/core-flows"
+import { CreateCustomerDTO } from "@medusajs/types"
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   const createCustomers = createStoreCustomersWorkflow(req.scope)
   const customersData = [req.validatedBody as CreateCustomerDTO]
 
-  console.warn(customersData)
   const { result } = await createCustomers.run({
     input: { customersData },
   })
