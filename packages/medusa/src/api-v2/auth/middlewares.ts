58144bbc9fb29bb4784731c96fc70b1454be6745
Philip Korsholm <philip.korsholm@hotmail.com>
2024-01-29 09:54:48 +0800

    add middlewares

--- packages/medusa/src/api-v2/auth/middlewares.ts
+++ packages/medusa/src/api-v2/auth/middlewares.ts
@@ -1,8 +1,83 @@
 import { MiddlewareRoute } from "../../loaders/helpers/routing/types"
+import passport from "passport"
+import { ModuleRegistrationName } from "@medusajs/modules-sdk"
+import passportCustom from "passport-custom"
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.use(
+  "custom",
+  new passportCustom.Strategy(async function (req: any, cb) {
+    if (req.user || req.customer) {
+      return cb(null, req.user)
+    }
+
+    const { scope, auth_provider } = req.params
+
+    if (!auth_provider || !scope) {
+      return this.fail()
+    }
+
+    const service = req.scope.resolve(ModuleRegistrationName.AUTHENTICATION)
+
+    const res = await service.authenticate(auth_provider, req)
+
+    const { success, error, authUser, location } = res
+    if (location) {
+      return this.redirect(location)
+    }
+
+    if (!success) {
+      return cb(error, null)
+    } else {
+      return cb(null, authUser)
+    }
+  })
+)
+
+passport.use(
+  "customCallback",
+  new passportCustom.Strategy(async function (req: any, cb) {
+    if (req.user) {
+      return cb(null, req.user)
+    }
+
+    const { scope, auth_provider } = req.params
+
+    if (!auth_provider || !scope) {
+      return this.fail()
+    }
+
+    const service = req.scope.resolve(ModuleRegistrationName.AUTHENTICATION)
+
+    const res = await service.validateCallback(auth_provider, req)
+
+    const { success, error, authUser, location } = res
+    if (location) {
+      return this.redirect(location)
+    }
+
+    if (!success) {
+      return cb(error, null)
+    } else {
+      return cb(null, authUser)
+    }
+  })
+)
 
 export const adminCampaignRoutesMiddlewares: MiddlewareRoute[] = [
   {
     matcher: "/auth/:scope/:auth-provider",
+    middlewares: [],
+  },
+  {
+    matcher: "/auth/:scope/:auth-provider/callback",
     middlewares: [],
   },
 ]
