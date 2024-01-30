2dc12b8568fb05129d0ae154d426602f0ef37af1
olivermrbl <oliver@mrbltech.com>
2024-01-30 19:10:54 +0100

    feat: Add payment provider support

---
+++ packages/payment/src/loaders/providers.ts
@@ -0,0 +1,54 @@
+import {
+  AbstractPaymentProcessor,
+  moduleProviderLoader,
+} from "@medusajs/medusa"
+
+import { LoaderOptions, ModulesSdkTypes } from "@medusajs/types"
+import { Lifetime, asFunction } from "awilix"
+
+type PaymentModuleProviders = {
+  providers: {
+    resolve: string
+    provider_id: string // e.g. stripe-usd
+    options: Record<string, unknown>
+  }[]
+}
+
+const loadAuthProviderService = async (klass, container, pluginOptions) => {
+  if (!AbstractPaymentProcessor.isPaymentProcessor(klass.prototype)) {
+    return
+  }
+
+  container.register({
+    [`payment_provider_${klass.prototype}`]: asFunction(
+      (cradle) => new klass(cradle, pluginOptions),
+      {
+        lifetime: klass.LIFE_TIME || Lifetime.SINGLETON,
+      }
+    ),
+  })
+
+  container.registerAdd(
+    "payment_providers",
+    asFunction((cradle) => new klass(cradle, pluginOptions), {
+      lifetime: klass.LIFE_TIME || Lifetime.SINGLETON,
+    })
+  )
+}
+
+export default async ({
+  container,
+  options,
+}: LoaderOptions<
+  (
+    | ModulesSdkTypes.ModuleServiceInitializeOptions
+    | ModulesSdkTypes.ModuleServiceInitializeCustomDataLayerOptions
+  ) &
+    PaymentModuleProviders
+>): Promise<void> => {
+  await moduleProviderLoader({
+    container,
+    providers: options!.providers,
+    registerServiceFn: loadAuthProviderService,
+  })
+}
