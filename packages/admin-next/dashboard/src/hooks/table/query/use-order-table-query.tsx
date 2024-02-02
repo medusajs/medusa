8cbf6c60fec7fe8ddf59dcf420b9339f84b8636c
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-02-02 21:56:55 +0100

    feat(dashboard): DataTable component (#6297)

---
+++ packages/admin-next/dashboard/src/hooks/table/query/use-order-table-query.tsx
@@ -0,0 +1,58 @@
+import { AdminGetOrdersParams } from "@medusajs/medusa"
+import { useQueryParams } from "../../use-query-params"
+
+type UseOrderTableQueryProps = {
+  prefix?: string
+  pageSize?: number
+}
+
+/**
+ * TODO: Enable `order` query param when staging is updated
+ */
+
+export const useOrderTableQuery = ({
+  prefix,
+  pageSize = 50,
+}: UseOrderTableQueryProps) => {
+  const queryObject = useQueryParams(
+    [
+      "offset",
+      "q",
+      "created_at",
+      "updated_at",
+      "region_id",
+      "sales_channel_id",
+      "payment_status",
+      "fulfillment_status",
+    ],
+    prefix
+  )
+
+  const {
+    offset,
+    sales_channel_id,
+    created_at,
+    updated_at,
+    fulfillment_status,
+    payment_status,
+    region_id,
+    q,
+  } = queryObject
+
+  const searchParams: AdminGetOrdersParams = {
+    limit: pageSize,
+    offset: offset ? Number(offset) : 0,
+    sales_channel_id: sales_channel_id?.split(","),
+    fulfillment_status: fulfillment_status?.split(","),
+    payment_status: payment_status?.split(","),
+    created_at: created_at ? JSON.parse(created_at) : undefined,
+    updated_at: updated_at ? JSON.parse(updated_at) : undefined,
+    region_id: region_id?.split(","),
+    q,
+  }
+
+  return {
+    searchParams,
+    raw: queryObject,
+  }
+}
--- packages/admin-next/dashboard/src/routes/orders/order-list/components/order-list-table/order-list-table.tsx
+++ packages/admin-next/dashboard/src/routes/orders/order-list/components/order-list-table/order-list-table.tsx
@@ -1,33 +1,25 @@
-import { Customer } from "@medusajs/medusa"
-import { Button, Container, Heading } from "@medusajs/ui"
+import { Container, Heading } from "@medusajs/ui"
 import { useAdminOrders } from "medusa-react"
 import { useTranslation } from "react-i18next"
-import { DataTable } from "../../../../../components/table/data-table"
+import { DataTable } from "../../../../../components/table/data-table/data-table"
 import { useOrderTableColumns } from "../../../../../hooks/table/columns/use-order-table-columns"
 import { useOrderTableFilters } from "../../../../../hooks/table/filters/use-order-table-filters"
 import { useOrderTableQuery } from "../../../../../hooks/table/query/use-order-table-query"
 import { useDataTable } from "../../../../../hooks/use-data-table"
 
-type CustomerGeneralSectionProps = {
-  customer: Customer
-}
-
-const PAGE_SIZE = 10
+const PAGE_SIZE = 50
 const DEFAULT_RELATIONS = "customer,items,sales_channel"
 const DEFAULT_FIELDS =
   "id,status,display_id,created_at,email,fulfillment_status,payment_status,total,currency_code"
 
-export const CustomerOrderSection = ({
-  customer,
-}: CustomerGeneralSectionProps) => {
+export const OrderListTable = () => {
   const { t } = useTranslation()
-
   const { searchParams, raw } = useOrderTableQuery({
     pageSize: PAGE_SIZE,
   })
-  const { orders, count, isLoading, isError, error } = useAdminOrders(
+
+  const { orders, count, isError, error, isLoading } = useAdminOrders(
     {
-      customer_id: customer.id,
       expand: DEFAULT_RELATIONS,
       fields: DEFAULT_FIELDS,
       ...searchParams,
@@ -37,10 +29,8 @@ export const CustomerOrderSection = ({
     }
   )
 
-  const columns = useOrderTableColumns({
-    exclude: ["customer"],
-  })
   const filters = useOrderTableFilters()
+  const columns = useOrderTableColumns({})
 
   const { table } = useDataTable({
     data: orders ?? [],
@@ -57,12 +47,7 @@ export const CustomerOrderSection = ({
   return (
     <Container className="divide-y p-0">
       <div className="flex items-center justify-between px-6 py-4">
-        <Heading level="h2">{t("orders.domain")}</Heading>
-        <div className="flex items-center gap-x-2">
-          <Button size="small" variant="secondary">
-            {t("general.create")}
-          </Button>
-        </div>
+        <Heading>{t("orders.domain")}</Heading>
       </div>
       <DataTable
         columns={columns}
@@ -71,10 +56,10 @@ export const CustomerOrderSection = ({
         navigateTo={(row) => `/orders/${row.original.id}`}
         filters={filters}
         count={count}
+        search
         isLoading={isLoading}
         rowCount={PAGE_SIZE}
         orderBy={["display_id", "created_at", "updated_at"]}
-        search={true}
         queryObject={raw}
       />
     </Container>
