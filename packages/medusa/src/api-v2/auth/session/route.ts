73d3aaebe408262eca839d650dcb254a1117320a
Philip Korsholm <philip.korsholm@hotmail.com>
2024-02-02 11:22:45 +0800

    remove console.logs

--- packages/medusa/src/api-v2/auth/session/route.ts
+++ packages/medusa/src/api-v2/auth/session/route.ts
@@ -1,8 +1,6 @@
 import { MedusaRequest, MedusaResponse } from "../../../types/routing"
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
-  // req.session.auth_user = authUser
-  // req.session.scope = authUser.scope
   req.session.auth_user = req.auth_user
 
   res.status(200).json({ user: req.auth_user })
