14af85b71079df580bd726511043f28791498adc
Philip Korsholm <philip.korsholm@hotmail.com>
2024-02-06 17:20:40 +0800

    add api-v2 skeleton for users routes

---
+++ packages/medusa/src/api-v2/admin/users/[id]/route.ts
@@ -0,0 +1,16 @@
+import { MedusaRequest, MedusaResponse } from "../../../../types/routing"
+
+// Get user
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  res.status(200).json({})
+}
+
+// update user
+export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
+  res.status(200).json({})
+}
+
+// delete user
+export const DELETE = async (req: MedusaRequest, res: MedusaResponse) => {
+  res.status(200).json({})
+}
---
+++ packages/medusa/src/api-v2/admin/users/route.ts
@@ -0,0 +1,11 @@
+import { MedusaRequest, MedusaResponse } from "../../../types/routing"
+
+// List users
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  res.status(200).json({})
+}
+
+// Create user
+export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
+  res.status(200).json({})
+}
