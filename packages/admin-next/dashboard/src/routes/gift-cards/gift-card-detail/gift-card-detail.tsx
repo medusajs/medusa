84c78e5762c153c2f300ce54a8dcbb04b1a16cb3
Kasper <kasper@medusajs.com>
2024-02-08 15:18:14 +0100

    add gc details page

--- packages/admin-next/dashboard/src/routes/gift-cards/gift-card-detail/gift-card-detail.tsx
+++ packages/admin-next/dashboard/src/routes/gift-cards/gift-card-detail/gift-card-detail.tsx
@@ -1,3 +1,25 @@
+import { useAdminGiftCard } from "medusa-react"
+import { useParams } from "react-router-dom"
+import { JsonViewSection } from "../../../components/common/json-view-section"
+import { GiftCardGeneralSection } from "./components/gift-card-general-section"
+
 export const GiftCardDetail = () => {
-  return <div>Gift Card Details</div>
+  const { id } = useParams()
+
+  const { gift_card, isLoading, isError, error } = useAdminGiftCard(id!)
+
+  if (isLoading || !gift_card) {
+    return null
+  }
+
+  if (isError) {
+    throw error
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <GiftCardGeneralSection giftCard={gift_card} />
+      <JsonViewSection data={gift_card} />
+    </div>
+  )
 }
