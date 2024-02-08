857adbb47ddd2de521ec53826eb610fce2b9d514
shahednasser <shahednasser@users.noreply.github.com>
2024-02-08 17:16:45 +0000

    [create-pull-request] automated change

--- packages/types/src/fulfillment/common.ts
+++ packages/types/src/fulfillment/common.ts
@@ -1,6 +1,21 @@
+/**
+ * @interface
+ *
+ * {summary}
+ */
 export type GeoZoneType = "country" | "province" | "city" | "zip"
 
+/**
+ * The fulfillment details.
+ */
 export interface FulfillmentDTO {
+  /**
+   * The ID of the fulfillment.
+   */
   id: string
+
+  /**
+   * The name of the fulfillment.
+   */
   name: string
 }
