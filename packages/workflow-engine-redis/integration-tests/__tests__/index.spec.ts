1d9d2d83319c96521e6e79bb97a89d8021bb9404
adrien2p <adrien.deperetti@gmail.com>
2024-02-16 13:25:39 +0100

    cleanup

--- packages/workflow-engine-redis/integration-tests/__tests__/index.spec.ts
+++ packages/workflow-engine-redis/integration-tests/__tests__/index.spec.ts
@@ -3,10 +3,10 @@ import {
   TransactionStepTimeoutError,
   TransactionTimeoutError,
 } from "@medusajs/orchestration"
-import { RemoteJoinerQuery } from "@medusajs/types"
+import { RemoteQueryFunction } from "@medusajs/types"
 import { TransactionHandlerType } from "@medusajs/utils"
 import { IWorkflowEngineService } from "@medusajs/workflows-sdk"
-import { knex } from "@mikro-orm/knex"
+import knex from "knex"
 import { setTimeout } from "timers/promises"
 import "../__fixtures__"
 import { DB_URL, TestDatabase } from "../utils"
@@ -27,10 +27,7 @@ const afterEach_ = async () => {
 describe("Workflow Orchestrator module", function () {
   describe("Testing basic workflow", function () {
     let workflowOrcModule: IWorkflowEngineService
-    let query: (
-      query: string | RemoteJoinerQuery | object,
-      variables?: Record<string, unknown>
-    ) => Promise<any>
+    let query: RemoteQueryFunction
 
     afterEach(afterEach_)
 
