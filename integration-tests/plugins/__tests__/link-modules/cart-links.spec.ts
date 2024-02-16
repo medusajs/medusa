1ee3d3b6714424904babbbdac7b870c26552e61c
olivermrbl <oliver@mrbltech.com>
2024-02-16 15:14:52 +0100

    consolidate tests

--- integration-tests/plugins/__tests__/link-modules/cart-links.spec.ts
+++ integration-tests/plugins/__tests__/link-modules/cart-links.spec.ts
@@ -1,22 +1,26 @@
 import { ModuleRegistrationName } from "@medusajs/modules-sdk"
-import { ICartModuleService, ISalesChannelModuleService } from "@medusajs/types"
+import {
+  ICartModuleService,
+  IRegionModuleService,
+  ISalesChannelModuleService,
+} from "@medusajs/types"
 import path from "path"
 import { startBootstrapApp } from "../../../environment-helpers/bootstrap-app"
 import { getContainer } from "../../../environment-helpers/use-container"
 import { initDb, useDb } from "../../../environment-helpers/use-db"
-import adminSeeder from "../../../helpers/admin-seeder"
 
 jest.setTimeout(50000)
 
 const env = { MEDUSA_FF_MEDUSA_V2: true }
 
-describe("Link: Cart Sales Channel", () => {
+describe("Cart links", () => {
   let dbConnection
   let appContainer
   let shutdownServer
   let cartModuleService: ICartModuleService
   let scModuleService: ISalesChannelModuleService
   let remoteQuery
+  let regionModule: IRegionModuleService
 
   beforeAll(async () => {
     const cwd = path.resolve(path.join(__dirname, "..", ".."))
@@ -25,6 +29,7 @@ describe("Link: Cart Sales Channel", () => {
     appContainer = getContainer()
     cartModuleService = appContainer.resolve(ModuleRegistrationName.CART)
     scModuleService = appContainer.resolve(ModuleRegistrationName.SALES_CHANNEL)
+    regionModule = appContainer.resolve(ModuleRegistrationName.REGION)
     remoteQuery = appContainer.resolve("remoteQuery")
   })
 
@@ -35,7 +40,8 @@ describe("Link: Cart Sales Channel", () => {
   })
 
   beforeEach(async () => {
-    await adminSeeder(dbConnection)
+    // @ts-ignore
+    await regionModule.createDefaultCountriesAndCurrencies()
   })
 
   afterEach(async () => {
@@ -43,7 +49,12 @@ describe("Link: Cart Sales Channel", () => {
     await db.teardown()
   })
 
-  it("should query carts and sales channels with remote query", async () => {
+  it("should query carts, sales channels, regions with remote query", async () => {
+    const region = await regionModule.create({
+      name: "Region",
+      currency_code: "usd",
+    })
+
     const salesChannel = await scModuleService.create({
       name: "Webshop",
     })
@@ -51,6 +62,7 @@ describe("Link: Cart Sales Channel", () => {
     const cart = await cartModuleService.create({
       email: "tony@stark.com",
       currency_code: "usd",
+      region_id: region.id,
       sales_channel_id: salesChannel.id,
     })
 
@@ -58,6 +70,9 @@ describe("Link: Cart Sales Channel", () => {
       cart: {
         fields: ["id"],
         sales_channel: {
+          fields: ["id"],
+        },
+        region: {
           fields: ["id"],
         },
       },
@@ -72,11 +87,21 @@ describe("Link: Cart Sales Channel", () => {
       },
     })
 
+    const regions = await remoteQuery({
+      region: {
+        fields: ["id"],
+        carts: {
+          fields: ["id"],
+        },
+      },
+    })
+
     expect(carts).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
           id: cart.id,
           sales_channel: expect.objectContaining({ id: salesChannel.id }),
+          region: expect.objectContaining({ id: region.id }),
         }),
       ])
     )
@@ -85,6 +110,17 @@ describe("Link: Cart Sales Channel", () => {
       expect.arrayContaining([
         expect.objectContaining({
           id: salesChannel.id,
+          carts: expect.arrayContaining([
+            expect.objectContaining({ id: cart.id }),
+          ]),
+        }),
+      ])
+    )
+
+    expect(regions).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: region.id,
           carts: expect.arrayContaining([
             expect.objectContaining({ id: cart.id }),
           ]),
