b7731250928ee2b83a99e273fd6561f7959b007e
Philip Korsholm <philip.korsholm@hotmail.com>
2024-02-19 18:12:38 +0800

    refactor utils

--- packages/utils/src/modules-sdk/decorators/inject-into-context.ts
+++ packages/utils/src/modules-sdk/decorators/inject-into-context.ts
@@ -30,26 +30,3 @@ export function InjectIntoContext(
     }
   }
 }
-
-export function EmitEvents() {
-  return function (
-    target: any,
-    propertyKey: string | symbol,
-    descriptor: any
-  ): void {
-    const aggregator = new MessageAggregator()
-    InjectIntoContext({
-      messageAggregator: () => aggregator,
-    })(target, propertyKey, descriptor)
-
-    const original = descriptor.value
-    descriptor.value = async function (...args: any[]) {
-      const result = await original.apply(this, args)
-
-      await target.emitEvents_.apply(this, [aggregator.getMessages()])
-
-      aggregator.clearMessages()
-      return result
-    }
-  }
-}
