5db3ec09e25875de089c31e356a8c1f51deb6a06
Oli Juhl <59018053+olivermrbl@users.noreply.github.com>
2024-02-19 08:28:44 +0100

    feat: Cart Customer link + create cart with customer (#6426)

--- packages/core-flows/src/definition/cart/workflows/create-carts.ts
+++ packages/core-flows/src/definition/cart/workflows/create-carts.ts
@@ -4,28 +4,44 @@ import {
   createWorkflow,
   transform,
 } from "@medusajs/workflows-sdk"
-import { createCartsStep, findOneOrAnyRegionStep } from "../steps"
+import {
+  createCartsStep,
+  findOneOrAnyRegionStep,
+  findOrCreateCustomerStep,
+} from "../steps"
 
 export const createCartWorkflowId = "create-cart"
 export const createCartWorkflow = createWorkflow(
   createCartWorkflowId,
-  (
-    input: WorkflowData<CreateCartWorkflowInputDTO>
-  ): WorkflowData<CartDTO[]> => {
+  (input: WorkflowData<CreateCartWorkflowInputDTO>): WorkflowData<CartDTO> => {
     const region = findOneOrAnyRegionStep({
       regionId: input.region_id,
     })
 
-    const cartInput = transform({ input, region }, (data) => {
-      return {
+    const customerData = findOrCreateCustomerStep({
+      customerId: input.customer_id,
+      email: input.email,
+    })
+
+    const cartInput = transform({ input, region, customerData }, (data) => {
+      const data_ = {
         ...data.input,
-        currency_code: data?.input.currency_code || data.region.currency_code,
+        currency_code: data.input.currency_code ?? data.region.currency_code,
         region_id: data.region.id,
       }
+
+      if (data.customerData.customer?.id) {
+        data_.customer_id = data.customerData.customer.id
+        data_.email = data.input?.email ?? data.customerData.customer.email
+      }
+
+      return data_
     })
+
+    // TODO: Add line items
 
     const cart = createCartsStep([cartInput])
 
-    return cart
+    return cart[0]
   }
 )
