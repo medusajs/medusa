5008d0ef75d5776808c6b7fe30831334cd9b8f28
Kasper <kasper@medusajs.com>
2024-02-21 12:54:55 +0100

    update final domains

--- packages/admin-next/dashboard/src/routes/products/product-create/product-create.tsx
+++ packages/admin-next/dashboard/src/routes/products/product-create/product-create.tsx
@@ -1,15 +1,10 @@
-import { FocusModal } from "@medusajs/ui"
-import { useRouteModalState } from "../../../hooks/use-route-modal-state"
+import { RouteFocusModal } from "../../../components/route-modal"
 import { CreateProductForm } from "./components/create-product-form"
 
 export const ProductCreate = () => {
-  const [open, onOpenChange, subscribe] = useRouteModalState()
-
   return (
-    <FocusModal open={open} onOpenChange={onOpenChange}>
-      <FocusModal.Content>
-        <CreateProductForm subscribe={subscribe} />
-      </FocusModal.Content>
-    </FocusModal>
+    <RouteFocusModal>
+      <CreateProductForm />
+    </RouteFocusModal>
   )
 }
