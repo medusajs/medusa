3c91fc1bb5e4b799280c8523b42abf03f6ff8d01
Philip Korsholm <philip.korsholm@hotmail.com>
2024-02-22 11:28:49 +0800

    cleanup resend step

--- packages/core-flows/src/invite/steps/resend-invites.ts
+++ packages/core-flows/src/invite/steps/resend-invites.ts
@@ -11,64 +11,8 @@ export const resendInvitesStep = createStep(
       ModuleRegistrationName.USER
     )
 
-    const createdInvites = await service.resendInvites(input)
+    const resendInvites = await service.resendInvites(input)
 
-    return new StepResponse(
-      createdInvites,
-      createdInvites.map((inv) => inv.id)
-    )
-  },
-  async (createdInvitesIds, { container }) => {
-    if (!createdInvitesIds?.length) {
-      return
-    }
-
-    const service: IUserModuleService = container.resolve(
-      ModuleRegistrationName.USER
-    )
-
-    await service.deleteInvites(createdInvitesIds)
+    return new StepResponse(resendInvites)
   }
 )
-// export const updateCustomersStepId = "update-customer"
-// export const updateCustomersStep = createStep(
-//   updateCustomersStepId,
-//   async (data: UpdateCustomersStepInput, { container }) => {
-//     const service = container.resolve<ICustomerModuleService>(
-//       ModuleRegistrationName.CUSTOMER
-//     )
-
-//     const { selects, relations } = getSelectsAndRelationsFromObjectArray([
-//       data.update,
-//     ])
-//     const prevCustomers = await service.list(data.selector, {
-//       select: selects,
-//       relations,
-//     })
-
-//     const customers = await service.update(data.selector, data.update)
-
-//     return new StepResponse(customers, prevCustomers)
-//   },
-//   async (prevCustomers, { container }) => {
-//     if (!prevCustomers?.length) {
-//       return
-//     }
-
-//     const service = container.resolve<ICustomerModuleService>(
-//       ModuleRegistrationName.CUSTOMER
-//     )
-
-//     await promiseAll(
-//       prevCustomers.map((c) =>
-//         service.update(c.id, {
-//           first_name: c.first_name,
-//           last_name: c.last_name,
-//           email: c.email,
-//           phone: c.phone,
-//           metadata: c.metadata,
-//         })
-//       )
-//     )
-//   }
-// )
