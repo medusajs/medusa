06755156f6ab6434886bac660dfa0f9968c89b90
Kasper <kasper@medusajs.com>
2024-02-23 12:05:09 +0100

    add filters

--- packages/admin-next/dashboard/src/routes/api-key-management/api-key-management-list/components/api-key-management-list-table/use-api-key-management-table-filters.tsx
+++ packages/admin-next/dashboard/src/routes/api-key-management/api-key-management-list/components/api-key-management-list-table/use-api-key-management-table-filters.tsx
@@ -9,6 +9,7 @@ export const useApiKeyManagementTableFilters = () => {
   const dateFilters: Filter[] = [
     { label: t("fields.createdAt"), key: "created_at" },
     { label: t("fields.updatedAt"), key: "updated_at" },
+    { label: t("fields.revokedAt"), key: "revoked_at" },
   ].map((f) => ({
     key: f.key,
     label: f.label,
