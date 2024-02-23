06e25bb45aafd674467cf58e1a3185d954e06079
adrien2p <adrien.deperetti@gmail.com>
2024-02-23 11:18:05 +0100

    revert mistake file

--- packages/medusa/src/types/routing.ts
+++ packages/medusa/src/types/routing.ts
@@ -1,30 +1,8 @@
-import type { Request } from "express"
+import type { NextFunction, Request, Response } from "express"
 
 import type { Customer, User } from "../models"
-import { FindConfig, RequestQueryFields } from "./common"
+import { MedusaContainer } from "@medusajs/types"
 
-export interface MedusaRequest<Body = unknown> extends Request {
-  validatedBody: Body
-  validatedQuery: RequestQueryFields & Record<string, unknown>
-  listConfig: FindConfig<unknown>
-  retrieveConfig: FindConfig<unknown>
-  filterableFields: Record<string, unknown>
-  allowedProperties: string[]
-  includes?: Record<string, boolean>
-  errors: string[]
-}
-
-export interface AuthenticatedMedusaRequest<Body = never>
-  extends MedusaRequest<Body> {
-  user: (User | Customer) & { customer_id?: string; userId?: string }
-  auth: {
-    actor_id: string
-    auth_user_id: string
-    app_metadata: Record<string, any>
-    scope: string
-  }
-}
-/*
 export interface MedusaRequest extends Request {
   user?: (User | Customer) & { customer_id?: string; userId?: string }
   scope: MedusaContainer
@@ -41,4 +19,4 @@ export type MedusaRequestHandler = (
   req: MedusaRequest,
   res: MedusaResponse,
   next: MedusaNextFunction
-) => Promise<void> | void*/
+) => Promise<void> | void
