c86e27bd0dd06a25a227fe65097bc9cc335b334e
Shahed Nasser <shahednasser@gmail.com>
2024-02-23 17:21:16 +0200

    chore: generated tsdocs (#6352)
    Generated TSDocs for the past release.
    
    Note: I haven't updated examples as the examples are for modules without a public reference yet, so the examples can wait.

--- packages/types/src/region/mutations.ts
+++ packages/types/src/region/mutations.ts
@@ -1,21 +1,71 @@
+import { RegionCurrencyDTO } from "./common"
+
+/**
+ * The region to be created.
+ */
 export interface CreateRegionDTO {
+  /**
+   * The name of the region.
+   */
   name: string
+  /**
+   * The currency code of the region.
+   */
   currency_code: string
+  /**
+   * The region's countries.
+   */
   countries?: string[]
+  /**
+   * Holds custom data in key-value pairs.
+   */
   metadata?: Record<string, unknown>
 }
 
+/**
+ * The attributes to update in the region.
+ */
 export interface UpdateRegionDTO {
+  /**
+   * The ID of the region.
+   */
   id: string
+  /**
+   * The name of the region.
+   */
   name?: string
+  /**
+   * The currency code of the region.
+   */
   currency_code?: string
+  /**
+   * The region's countries.
+   */
   countries?: string[]
+  /**
+   * Holds custom data in key-value pairs.
+   */
   metadata?: Record<string, unknown>
 }
 
+/**
+ * The updatable fields of a region.
+ */
 export interface UpdatableRegionFields {
+  /**
+   * The name of the region.
+   */
   name?: string
+  /**
+   * The currency code of the region.
+   */
   currency_code?: string
+  /**
+   * The region's countries.
+   */
   countries?: string[]
+  /**
+   * Holds custom data in key-value pairs.
+   */
   metadata?: Record<string, unknown>
 }
