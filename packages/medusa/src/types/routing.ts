cda61368a8d84ef5506f833c8d2f6c7f82829ca5
Philip Korsholm <philip.korsholm@hotmail.com>
2024-02-26 11:08:51 +0800

    update authenticatedRequest type

--- packages/medusa/src/types/routing.ts
+++ packages/medusa/src/types/routing.ts
@@ -2,13 +2,23 @@ import type { Customer, User } from "../models"
 import type { NextFunction, Request, Response } from "express"
 
 import type { MedusaContainer } from "./global"
+import { RequestQueryFields } from "@medusajs/types"
 
-export interface MedusaRequest extends Request {
-  user?: (User | Customer) & { customer_id?: string; userId?: string }
+export interface MedusaRequest<Body = unknown> extends Request {
+  validatedBody: Body
+  validatedQuery: RequestQueryFields & Record<string, unknown>
+  allowedProperties: string[]
+  includes?: Record<string, boolean>
+  errors: string[]
   scope: MedusaContainer
   session?: any
   requestId?: string
-  auth?: {
+}
+
+export interface AuthenticatedMedusaRequest<Body = never>
+  extends MedusaRequest<Body> {
+  user: (User | Customer) & { customer_id?: string; userId?: string } // TODO: Remove this property when v2 is released
+  auth: {
     actor_id: string
     auth_user_id: string
     app_metadata: Record<string, any>
@@ -21,7 +31,7 @@ export type MedusaResponse = Response
 export type MedusaNextFunction = NextFunction
 
 export type MedusaRequestHandler = (
-  req: MedusaRequest,
+  req: MedusaRequest<unknown>,
   res: MedusaResponse,
   next: MedusaNextFunction
 ) => Promise<void> | void
