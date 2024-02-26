fec98d445b32bcd9c575c8a453cf53e4fe0e0fef
olivermrbl <oliver@mrbltech.com>
2024-02-26 20:58:29 +0100

    fix error message

--- packages/payment/integration-tests/__tests__/services/payment-module/index.spec.ts
+++ packages/payment/integration-tests/__tests__/services/payment-module/index.spec.ts
@@ -141,8 +141,9 @@ moduleIntegrationTestRunner({
               ])
               .catch((e) => e)
 
+            // TODO: Change error thrown by Mikro for BigNumber fields
             expect(error.message).toContain(
-              "Value for PaymentCollection.amount is required, 'undefined' found"
+              "Value for PaymentCollection.amount_ is required, 'undefined' found"
             )
 
             error = await service
