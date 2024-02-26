7a1c5cb613b5c2a3290a7783fefef3bcfcfcda16
fPolic <mainacc.polic@gmail.com>
2024-02-26 11:11:37 +0100

    fix: redemptions alignment

--- packages/admin-next/dashboard/src/components/table/table-cells/discount/redemption-cell/redemption-cell.tsx
+++ packages/admin-next/dashboard/src/components/table/table-cells/discount/redemption-cell/redemption-cell.tsx
@@ -6,7 +6,7 @@ type DiscountCellProps = {
 
 export const RedemptionCell = ({ redemptions }: DiscountCellProps) => {
   return (
-    <div className="flex h-full w-full items-center justify-end gap-x-3">
+    <div className="flex h-full w-full items-center justify-end gap-x-3 text-right">
       <span>{redemptions}</span>
     </div>
   )
@@ -16,8 +16,8 @@ export const RedemptionHeader = () => {
   const { t } = useTranslation()
 
   return (
-    <div className="flex h-full w-full items-center justify-end">
-      <span>{t("fields.totalRedemptions")}</span>
+    <div className="flex h-full w-full items-center justify-end text-right">
+      <span className="truncate">{t("fields.totalRedemptions")}</span>
     </div>
   )
 }
