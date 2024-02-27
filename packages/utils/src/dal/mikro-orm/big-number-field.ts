b3d826497b3dae5e1b26b7924706c24fd5e87ca5
Adrien de Peretti <adrien.deperetti@gmail.com>
2024-02-27 16:48:45 +0100

    chore(utils): Improve big number decorator (#6525)
    **What**
    - Re add the ability to use the original column name as part of a select in both select in and joined strategy
    - The undefined error message will now display the original column name instead of the underscored one

--- packages/utils/src/dal/mikro-orm/big-number-field.ts
+++ packages/utils/src/dal/mikro-orm/big-number-field.ts
@@ -9,16 +9,15 @@ export function MikroOrmBigNumberProperty(
   } = {}
 ) {
   return function (target: any, columnName: string) {
-    const targetColumn = columnName + "_"
     const rawColumnName = options.rawColumnName ?? `raw_${columnName}`
 
     Object.defineProperty(target, columnName, {
       get() {
-        return this[targetColumn]
+        return this.__helper.__data[columnName]
       },
       set(value: BigNumberInput) {
         if (options?.nullable && !isPresent(value)) {
-          this[targetColumn] = null
+          this.__helper.__data[columnName] = null
           this[rawColumnName] = null
 
           return
@@ -42,30 +41,22 @@ export function MikroOrmBigNumberProperty(
           bigNumber = new BigNumber(value)
         }
 
-        this[targetColumn] = bigNumber.numeric
+        this.__helper.__data[columnName] = bigNumber.numeric
 
         const raw = bigNumber.raw!
         raw.value = trimZeros(raw.value as string)
 
         this[rawColumnName] = raw
+
+        this.__helper.__touched = !this.__helper.hydrator.isRunning()
       },
     })
 
     Property({
       type: "number",
       columnType: "numeric",
-      fieldName: columnName,
-      serializer: () => {
-        return undefined
-      },
+      trackChanges: false,
       ...options,
-    })(target, targetColumn)
-
-    Property({
-      type: "number",
-      persist: false,
-      getter: true,
-      setter: true,
     })(target, columnName)
   }
 }
