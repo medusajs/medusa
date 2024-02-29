6279fb3c67fb70cdb3b08a90b115546e586ebe8d
Sebastian Rindom <skrindom@gmail.com>
2024-02-29 12:03:18 +0100

    feat(tax): add support for updating tax rates (#6537)

--- packages/medusa/src/api-v2/admin/tax-rates/[id]/route.ts
+++ packages/medusa/src/api-v2/admin/tax-rates/[id]/route.ts
@@ -5,6 +5,37 @@ import {
 
 import { defaultAdminTaxRateFields } from "../query-config"
 import { remoteQueryObjectFromString } from "@medusajs/utils"
+import { AdminPostTaxRatesTaxRateReq } from "../../../../api/routes/admin/tax-rates"
+import { updateTaxRatesWorkflow } from "@medusajs/core-flows"
+
+export const POST = async (
+  req: AuthenticatedMedusaRequest<AdminPostTaxRatesTaxRateReq>,
+  res: MedusaResponse
+) => {
+  const { errors } = await updateTaxRatesWorkflow(req.scope).run({
+    input: {
+      selector: { id: req.params.id },
+      update: req.validatedBody,
+    },
+    throwOnError: false,
+  })
+
+  if (Array.isArray(errors) && errors[0]) {
+    throw errors[0].error
+  }
+
+  const remoteQuery = req.scope.resolve("remoteQuery")
+
+  const queryObject = remoteQueryObjectFromString({
+    entryPoint: "tax_rate",
+    variables: { id: req.params.id },
+    fields: defaultAdminTaxRateFields,
+  })
+
+  const [taxRate] = await remoteQuery(queryObject)
+
+  res.status(200).json({ tax_rate: taxRate })
+}
 
 export const GET = async (
   req: AuthenticatedMedusaRequest,
