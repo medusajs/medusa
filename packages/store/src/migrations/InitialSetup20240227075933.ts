e0e56487ac768d3f20270604caa17514858060d5
Stevche Radevski <sradevski@live.com>
2024-03-06 11:41:19 +0100

    fix: Store module improvements to get it closer to the v1 model

--- packages/store/src/migrations/InitialSetup20240227075933.ts
+++ packages/store/src/migrations/InitialSetup20240227075933.ts
@@ -14,13 +14,21 @@ export class InitialSetup20240226130829 extends Migration {
         `alter table "store" alter column "name" SET DEFAULT 'Medusa Store';`
       )
       this.addSql(
+        `alter table "store" alter column "default_currency_code" TYPE text;`
+      )
+      this.addSql(
+        `alter table "store" alter column "default_currency_code" drop not null;`
+      )
+      this.addSql(
         `alter table "store" alter column "default_sales_channel_id" TYPE text;`
       )
       this.addSql(
         `alter table "store" alter column "default_location_id" TYPE text;`
       )
 
-      this.addSql(`alter table "store" add column "default_region_id" text;`)
+      this.addSql(
+        `alter table "store" add column "default_region_id" text null;`
+      )
       this.addSql(
         `alter table "store" add column "deleted_at" timestamptz null;`
       )
@@ -32,6 +40,10 @@ export class InitialSetup20240226130829 extends Migration {
         'create index if not exists "IDX_store_deleted_at" on "store" (deleted_at) where deleted_at is not null;'
       )
 
+      this.addSql(
+        `alter table "store" drop constraint if exists "FK_61b0f48cccbb5f41c750bac7286";`
+      )
+
       // this.addSql(`alter table "store" drop column "default_currency_code";`)
       // this.addSql(`alter table "store" drop column "swap_link_template";`)
       // this.addSql(`alter table "store" drop column "payment_link_template";`)
@@ -39,7 +51,7 @@ export class InitialSetup20240226130829 extends Migration {
     } else {
       this.addSql(`create table if not exists "store" 
       ("id" text not null, "name" text not null default \'Medusa Store\', "supported_currency_codes" text[] not null default \'{}\',
-      "default_sales_channel_id" text null, "default_region_id" text null, "default_location_id" text null, 
+      "default_currency_code" text null, "default_sales_channel_id" text null, "default_region_id" text null, "default_location_id" text null, 
       "metadata" jsonb null, "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, 
       constraint "store_pkey" primary key ("id"));`)
 
