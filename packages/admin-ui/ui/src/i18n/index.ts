a64a4c822e4db20ac3140d9d383a1510263c99c7
Felix Bogen <88351462+felixb1010@users.noreply.github.com>
2024-03-06 18:56:17 +0100

    Norwegian Language Support

--- packages/admin-ui/ui/src/i18n/index.ts
+++ packages/admin-ui/ui/src/i18n/index.ts
@@ -89,6 +89,10 @@ export const supportedLanguages = [
   {
     locale: "zh",
     name: "简体中文",
+  },
+  {
+    locale: "nb",
+    name: "Norsk",
   }
 ].sort((a, b) => a.locale.localeCompare(b.locale))
 
