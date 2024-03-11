444d05ec523438ae17248b79a2b83177c5fafca8
Kasper <kasper@medusajs.com>
2024-03-11 09:11:30 +0100

    stop propegation on inline links

--- packages/admin-next/dashboard/src/components/common/inline-link/inline-link.tsx
+++ packages/admin-next/dashboard/src/components/common/inline-link/inline-link.tsx
@@ -8,6 +8,9 @@ export const InlineLink = ({
 }: ComponentPropsWithoutRef<typeof Link>) => {
   return (
     <Link
+      onClick={(e) => {
+        e.stopPropagation()
+      }}
       className={clx(
         "text-ui-fg-interactive transition-fg hover:text-ui-fg-interactive-hover focus-visible:text-ui-fg-interactive-hover rounded-md outline-none",
         className
