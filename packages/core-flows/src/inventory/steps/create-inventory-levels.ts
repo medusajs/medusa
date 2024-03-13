ddf130f95419bd6642e668b8bebe8db8858580da
Philip Korsholm <philip.korsholm@hotmail.com>
2024-03-13 17:24:44 +0100

    initialize create location levels

---
+++ packages/core-flows/src/inventory/steps/create-inventory-levels.ts
@@ -0,0 +1,34 @@
+import {
+  CreateInventoryLevelInput,
+  IInventoryServiceNext,
+} from "@medusajs/types"
+import { StepResponse, createStep } from "@medusajs/workflows-sdk"
+
+import { ModuleRegistrationName } from "@medusajs/modules-sdk"
+
+export const createInventoryLevelsStepId = "create-inventory-levels"
+export const createInventoryLevelsStep = createStep(
+  createInventoryLevelsStepId,
+  async (data: CreateInventoryLevelInput[], { container }) => {
+    const service = container.resolve<IInventoryServiceNext>(
+      ModuleRegistrationName.INVENTORY
+    )
+
+    const inventoryLevels = await service.createInventoryLevels(data)
+    return new StepResponse(
+      inventoryLevels,
+      inventoryLevels.map((level) => level.id)
+    )
+  },
+  async (ids, { container }) => {
+    if (!ids?.length) {
+      return
+    }
+
+    const service = container.resolve<IInventoryServiceNext>(
+      ModuleRegistrationName.INVENTORY
+    )
+
+    await service.deleteInventoryLevels(ids)
+  }
+)
---
+++ packages/medusa/src/api-v2/admin/inventory-items/middlewares.ts
@@ -0,0 +1,40 @@
+import * as QueryConfig from "./query-config"
+
+import {
+  AdminGetInventoryItemsItemParams,
+  AdminGetInventoryItemsParams,
+  AdminPostInventoryItemsItemLocationLevelsReq,
+  AdminPostInventoryItemsReq,
+} from "./validators"
+import { transformBody, transformQuery } from "../../../api/middlewares"
+
+import { MiddlewareRoute } from "../../../types/middlewares"
+import { authenticate } from "../../../utils/authenticate-middleware"
+
+export const adminInventoryRoutesMiddlewares: MiddlewareRoute[] = [
+  {
+    method: "ALL",
+    matcher: "/admin/inventory-items*",
+    middlewares: [authenticate("admin", ["session", "bearer"])],
+  },
+  {
+    method: ["GET"],
+    matcher: "/admin/inventory-items",
+    middlewares: [
+      transformQuery(
+        AdminGetInventoryItemsParams,
+        QueryConfig.listTransformQueryConfig
+      ),
+    ],
+  },
+  {
+    method: ["POST"],
+    matcher: "/admin/inventory-items/:id/location-levels",
+    middlewares: [transformBody(AdminPostInventoryItemsItemLocationLevelsReq)],
+  },
+  {
+    method: ["POST"],
+    matcher: "/admin/inventory-items",
+    middlewares: [transformBody(AdminPostInventoryItemsReq)],
+  },
+]
