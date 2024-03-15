96f2369e9a7b04297e96abc9fe88bf9589e00ef0
Shahed Nasser <shahednasser@gmail.com>
2024-03-15 15:12:14 +0200

    fixed lint error

--- www/apps/docs/src/theme/CodeBlock/Content/String.tsx
+++ www/apps/docs/src/theme/CodeBlock/Content/String.tsx
@@ -59,8 +59,6 @@ export default function CodeBlockString({
   })
   const showLineNumbers = showLineNumbersProp ?? containsLineNumbers(metastring)
 
-  console.log("here1")
-
   return (
     <Container
       as="div"
