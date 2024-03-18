5b308bdb0019078e176017e4cca5fc0f7b6b8bf7
Philip Korsholm <philip.korsholm@hotmail.com>
2024-03-18 15:07:52 +0100

    use links

--- packages/core-flows/src/inventory/steps/deatach-inventory-items.ts
+++ packages/core-flows/src/inventory/steps/deatach-inventory-items.ts
@@ -5,24 +5,25 @@ import {
 import { ModuleRegistrationName, Modules } from "@medusajs/modules-sdk"
 import { StepResponse, createStep } from "@medusajs/workflows-sdk"
 
+import { ILinkModule } from "@medusajs/types"
+
 export const deatachInventoryItemStepId = "deattach-inventory-items-step"
 export const deatachInventoryItemStep = createStep(
   deatachInventoryItemStepId,
   async (ids: string[], { container }) => {
     const remoteLink = container.resolve(ContainerRegistrationKeys.REMOTE_LINK)
-    const remoteQuery = container.resolve(
-      ContainerRegistrationKeys.REMOTE_QUERY
+
+    const linkModule: ILinkModule = remoteLink.getLinkModule(
+      Modules.PRODUCT,
+      "variant_id",
+      Modules.INVENTORY,
+      "inventory_item_id"
     )
 
-    const links = await remoteQuery(
-      remoteQueryObjectFromString({
-        entryPoint: "product_variant_inventory_item",
-        variables: {
-          filter: { inventory_item_id: ids },
-        },
-        fields: ["variant_id", "inventory_item_id"],
-      })
-    )
+    const links = (await linkModule.list(
+      { inventory_item_id: ids },
+      { select: ["variant_id", "inventory_item_id"] }
+    )) as { inventory_item_id: string; variant_id: string }[]
 
     await remoteLink.dismiss(
       links.map(({ inventory_item_id, variant_id }) => ({
