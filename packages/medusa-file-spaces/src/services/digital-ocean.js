b16cfae5126e40932451b66dce4159a28f104998
Benjamin Storrier <benjamin.storrier@gmail.com>
2024-03-19 16:21:02 +1100

    add optional file key

--- packages/medusa-file-spaces/src/services/digital-ocean.js
+++ packages/medusa-file-spaces/src/services/digital-ocean.js
@@ -15,6 +15,7 @@ class DigitalOceanService extends AbstractFileService {
     this.region_ = options.region
     this.endpoint_ = options.endpoint
     this.downloadUrlDuration = options.download_url_duration ?? 60 // 60 seconds
+    this.prefix = options.prefix ?? '' // optional prefix for the file key
   }
 
   upload(file) {
@@ -31,7 +32,7 @@ class DigitalOceanService extends AbstractFileService {
 
   uploadFile(file, options = { isProtected: false, acl: undefined }) {
     const parsedFilename = parse(file.originalname)
-    const fileKey = `${parsedFilename.name}-${Date.now()}${parsedFilename.ext}`
+    const fileKey = `${this.prefix}${parsedFilename.name}-${Date.now()}${parsedFilename.ext}`
 
     const s3 = new aws.S3()
     const params = {
