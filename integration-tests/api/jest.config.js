d06273f19c47496c1e33bf32f58c35f3dd2b8937
Riqwan Thamir <rmthamir@gmail.com>
2024-03-22 00:18:59 +0100

    revert to jest transformer for api

--- integration-tests/api/jest.config.js
+++ integration-tests/api/jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     `.cache`,
   ],
   transformIgnorePatterns: [`/dist`],
-  transform: { "^.+\\.[jt]s$": ["@swc/jest"] },
+  transform: { "^.+\\.[jt]s$": `../../jest-transformer.js` },
   setupFiles: ["../setup-env.js"],
   setupFilesAfterEnv: ["../setup.js"],
   globalSetup: "../globalSetup.js",
