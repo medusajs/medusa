cf8b5ce85bcc44f3bab8a75b1f4021787ef861e1
Riqwan Thamir <rmthamir@gmail.com>
2024-03-21 11:01:52 +0100

    chore: pricing models uses standardized relationships attributes (#6767)
    what:
    
    - all relationships under all models are standardized

--- packages/pricing/integration-tests/__fixtures__/price-set-money-amount/data.ts
+++ packages/pricing/integration-tests/__fixtures__/price-set-money-amount/data.ts
@@ -2,22 +2,22 @@ export const defaultPriceSetMoneyAmountsData = [
   {
     id: "price-set-money-amount-USD",
     title: "price set money amount USD",
-    price_set: "price-set-1",
-    money_amount: "money-amount-USD",
+    price_set_id: "price-set-1",
+    money_amount_id: "money-amount-USD",
     rules_count: 1,
   },
   {
     id: "price-set-money-amount-EUR",
     title: "price set money amount EUR",
-    price_set: "price-set-2",
-    money_amount: "money-amount-EUR",
+    price_set_id: "price-set-2",
+    money_amount_id: "money-amount-EUR",
     rules_count: 1,
   },
   {
     id: "price-set-money-amount-CAD",
     title: "price set money amount CAD",
-    price_set: "price-set-3",
-    money_amount: "money-amount-CAD",
+    price_set_id: "price-set-3",
+    money_amount_id: "money-amount-CAD",
     rules_count: 1,
   },
 ]
