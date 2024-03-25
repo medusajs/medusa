20132d7cea13b7c7ae77b33684d01a9ab40f7ed3
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2024-03-25 16:58:23 +0100

    feat(medusa, core-flows): add retrieve stock location endpoint to api-v2 (#6791)
    * initial create
    
    * add changeset
    
    * redo changes for stock locatino module'
    
    * add get endpoint
    
    * add changeset
    
    * add exception if location is not found
    
    * remove only
    
    * initial delete stock location
    
    * add changeset
    
    * add changeset
    
    * pr prep
    
    * remote duplicate changeset
    
    * propagate deletion with common step
    
    * move integration tests
    
    ---------
    
    Co-authored-by: Riqwan Thamir <rmthamir@gmail.com>

--- packages/medusa/src/api-v2/admin/stock-locations/[id]/route.ts
+++ packages/medusa/src/api-v2/admin/stock-locations/[id]/route.ts
@@ -1,6 +1,35 @@
+import {
+  ContainerRegistrationKeys,
+  remoteQueryObjectFromString,
+} from "@medusajs/utils"
 import { MedusaRequest, MedusaResponse } from "../../../../types/routing"
 
+import { MedusaError } from "@medusajs/utils"
 import { deleteStockLocationsWorkflow } from "@medusajs/core-flows"
+
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  const { id } = req.params
+  const remoteQuery = req.scope.resolve(ContainerRegistrationKeys.REMOTE_QUERY)
+
+  const [stock_location] = await remoteQuery(
+    remoteQueryObjectFromString({
+      entryPoint: "stock_locations",
+      variables: {
+        id,
+      },
+      fields: req.remoteQueryConfig.fields,
+    })
+  )
+
+  if (!stock_location) {
+    throw new MedusaError(
+      MedusaError.Types.NOT_FOUND,
+      `Stock location with id: ${id} was not found`
+    )
+  }
+
+  res.status(200).json({ stock_location })
+}
 
 export const DELETE = async (req: MedusaRequest, res: MedusaResponse) => {
   const { id } = req.params
