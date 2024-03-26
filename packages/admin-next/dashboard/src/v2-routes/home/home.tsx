448a30d0300612b72550e99fda2b13d56c5ec152
Riqwan Thamir <rmthamir@gmail.com>
2024-03-26 15:29:06 +0100

    chore(dashboard): render main layout as home page for v2 (#6823)
    * chore(dashboard): render main layout as home page for v2
    
    * chore: throw an error on store not existing

--- packages/admin-next/dashboard/src/v2-routes/home/home.tsx
+++ packages/admin-next/dashboard/src/v2-routes/home/home.tsx
@@ -4,9 +4,8 @@ import { useNavigate } from "react-router-dom"
 export const Home = () => {
   const navigate = useNavigate()
 
-  // Currently, the home page simply redirects to the settings page
   useEffect(() => {
-    navigate("/settings", { replace: true })
+    navigate("/orders", { replace: true })
   }, [navigate])
 
   return <div />
