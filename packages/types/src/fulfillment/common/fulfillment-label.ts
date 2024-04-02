ed83a1251ba23206591d9ba8faf24f8005a635b8
Shahed Nasser <shahednasser@gmail.com>
2024-04-02 18:48:00 +0300

    chore: Added TSDocs for the Fulfillment Module

--- packages/types/src/fulfillment/common/fulfillment-label.ts
+++ packages/types/src/fulfillment/common/fulfillment-label.ts
@@ -1,13 +1,51 @@
 import { FulfillmentDTO } from "./fulfillment"
 
+/**
+ * The fulfillment label details.
+ */
 export interface FulfillmentLabelDTO {
+  /**
+   * The ID of the fulfillment label.
+   */
   id: string
+
+  /**
+   * The tracking number of the fulfillment label.
+   */
   tracking_number: string
+
+  /**
+   * The tracking URL of the fulfillment label.
+   */
   tracking_url: string
+
+  /**
+   * The label's URL.
+   */
   label_url: string
+
+  /**
+   * The associated fulfillment's ID.
+   */
   fulfillment_id: string
+
+  /**
+   * The associated fulfillment.
+   */
   fulfillment: FulfillmentDTO
+
+  /**
+   * The creation date of the fulfillment label.
+   */
   created_at: Date
+
+  /**
+   * The update date of the fulfillment label.
+   */
   updated_at: Date
+
+  /**
+   * The deletion date of the fulfillment label.
+   */
   deleted_at: Date | null
 }
--- packages/types/src/fulfillment/mutations/shipping-option-rule.ts
+++ packages/types/src/fulfillment/mutations/shipping-option-rule.ts
@@ -1,3 +1,6 @@
+/**
+ * The accepted values for the shipping rule option's operator.
+ */
 export type ShippingOptionRuleOperatorType =
   | "in"
   | "eq"
@@ -8,14 +11,38 @@ export type ShippingOptionRuleOperatorType =
   | "lte"
   | "nin"
 
+/**
+ * The shipping option rule to be created.
+ */
 export interface CreateShippingOptionRuleDTO {
+  /**
+   * The attribute of the shipping option rule.
+   */
   attribute: string
+
+  /**
+   * The operator of the shipping option rule.
+   */
   operator: ShippingOptionRuleOperatorType
+
+  /**
+   * The value(s) of the shipping option rule.
+   */
   value: string | string[]
+
+  /**
+   * The associated shipping option's ID.
+   */
   shipping_option_id: string
 }
 
+/**
+ * The attributes to update in the shipping option rule.
+ */
 export interface UpdateShippingOptionRuleDTO
   extends Partial<CreateShippingOptionRuleDTO> {
+  /**
+   * The ID of the shipping option rule.
+   */
   id: string
 }
