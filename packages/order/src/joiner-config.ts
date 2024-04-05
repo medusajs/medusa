2245fae7e569b0b861ece41ae55db3b586266a0c
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-04-05 14:29:49 +0200

    joiner config

--- packages/order/src/joiner-config.ts
+++ packages/order/src/joiner-config.ts
@@ -1,10 +1,12 @@
 import { Modules } from "@medusajs/modules-sdk"
 import { ModuleJoinerConfig } from "@medusajs/types"
 import { MapToConfig } from "@medusajs/utils"
+import { LineItem } from "@models"
+import Order from "./models/order"
 
 export const LinkableKeys: Record<string, string> = {
-  order_id: "Order",
-  order_item_id: "OrderLineItem",
+  order_id: Order.name,
+  order_item_id: LineItem.name,
 }
 
 const entityLinkableKeysMap: MapToConfig = {}
@@ -26,7 +28,7 @@ export const joinerConfig: ModuleJoinerConfig = {
     {
       name: ["order", "orders"],
       args: {
-        entity: "Order",
+        entity: Order.name,
       },
     },
   ],
