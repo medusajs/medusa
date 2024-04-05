36567c9574a49026e4fa70badbf249c6870f000b
adrien2p <adrien.deperetti@gmail.com>
2024-04-05 14:56:32 +0200

    continue http types

--- packages/types/src/http/fulfillment/admin/shipping-option.ts
+++ packages/types/src/http/fulfillment/admin/shipping-option.ts
@@ -1,6 +1,15 @@
 import { ShippingOptionPriceType } from "../../../fulfillment"
 import { RuleOperatorType } from "../../../common"
+import {
+  ServiceZoneDTO,
+  ShippingOptionRuleDTO,
+  ShippingOptionTypeDTO,
+  ShippingProfileDTO,
+} from "../dtos"
 
+/**
+ * @experimental
+ */
 export interface AdminPostCreateShippingOptionType {
   label: string
   description: string
@@ -8,12 +17,18 @@ export interface AdminPostCreateShippingOptionType {
   shipping_option_id: string
 }
 
+/**
+ * @experimental
+ */
 export interface AdminPostCreateShippingOptionRule {
   attribute: string
   operator: RuleOperatorType
   value: string | string[]
 }
 
+/**
+ * @experimental
+ */
 export interface AdminPostCreateShippingOption {
   name: string
   price_type: ShippingOptionPriceType
@@ -23,4 +38,26 @@ export interface AdminPostCreateShippingOption {
   type: AdminPostCreateShippingOptionType
   data?: Record<string, unknown> | null
   rules?: AdminPostCreateShippingOptionRule[]
+}
+
+/**
+ * @experimental
+ */
+export interface AdminPostCreateShippingOptionResponse {
+  id: string
+  name: string
+  price_type: ShippingOptionPriceType
+  service_zone_id: string
+  shipping_profile_id: string
+  provider_id: string
+  shipping_option_type_id: string | null
+  data: Record<string, unknown> | null
+  metadata: Record<string, unknown> | null
+  service_zone: ServiceZoneDTO
+  shipping_profile: ShippingProfileDTO
+  type: ShippingOptionTypeDTO
+  rules: ShippingOptionRuleDTO[]
+  created_at: Date
+  updated_at: Date
+  deleted_at: Date | null
 }
