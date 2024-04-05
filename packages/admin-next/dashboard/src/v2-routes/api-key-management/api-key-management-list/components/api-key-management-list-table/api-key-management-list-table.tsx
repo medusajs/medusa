627d011a4553a09a6969d01b031994276f284479
olivermrbl <oliver@mrbltech.com>
2024-04-05 09:30:10 +0200

    remove v1 translations

--- packages/admin-next/dashboard/src/v2-routes/api-key-management/api-key-management-list/components/api-key-management-list-table/api-key-management-list-table.tsx
+++ packages/admin-next/dashboard/src/v2-routes/api-key-management/api-key-management-list/components/api-key-management-list-table/api-key-management-list-table.tsx
@@ -60,7 +60,7 @@ export const ApiKeyManagementListTable = ({
     <Container className="divide-y p-0">
       <div className="flex items-center justify-between px-6 py-4">
         <Heading level="h2">
-          {t(`apiKeyManagementV2.domain`, { keyType: upperCaseFirst(keyType) })}
+          {t(`apiKeyManagement.domain`, { keyType: upperCaseFirst(keyType) })}
         </Heading>
         <Link to="create">
           <Button variant="secondary" size="small">
