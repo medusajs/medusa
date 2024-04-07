ddde227f98e0538ba216d1eee0c78c778f8ab7ef
adrien2p <adrien.deperetti@gmail.com>
2024-04-07 16:26:25 +0200

    feat(*): Delete shipping options API

---
+++ packages/core-flows/src/fulfillment/workflows/delete-shipping-options.ts
@@ -0,0 +1,16 @@
+import { FulfillmentWorkflow } from "@medusajs/types"
+import { createWorkflow, WorkflowData } from "@medusajs/workflows-sdk"
+import { deleteShippingOptionsStep } from "../steps"
+
+export const deleteShippingOptionsWorkflowId =
+  "delete-shipping-options-workflow"
+export const deleteShippingOptionsWorkflow = createWorkflow(
+  deleteShippingOptionsWorkflowId,
+  (
+    input: WorkflowData<FulfillmentWorkflow.DeleteShippingOptionsWorkflowInput>
+  ): WorkflowData => {
+    const serviceZones = deleteShippingOptionsStep(input.data)
+
+    return serviceZones
+  }
+)
--- packages/types/src/workflow/fulfillment/index.ts
+++ packages/types/src/workflow/fulfillment/index.ts
@@ -1,3 +1,4 @@
 export * from "./create-shipping-options"
 export * from "./update-shipping-options"
 export * from "./service-zones"
+export * from "./delete-shipping-options"
