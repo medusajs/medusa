b500192adaa89ebe1f2335e44e44190fff5ea706
Shahed Nasser <shahednasser@gmail.com>
2024-04-09 18:09:34 +0300

    generate oas for v2

---
+++ www/apps/api-reference/specs-v2/admin/code_samples/Shell/admin_customers_{id}_addresses/post.sh
@@ -0,0 +1,2 @@
+curl -X POST '{backend_url}/admin/customers/{id}/addresses' \
+-H 'x-medusa-access-token: {api_token}'
---
+++ www/apps/api-reference/specs-v2/admin/code_samples/Shell/admin_tax-rates_{id}/post.sh
@@ -0,0 +1,2 @@
+curl -X POST '{backend_url}/admin/tax-rates/{id}' \
+-H 'x-medusa-access-token: {api_token}'
