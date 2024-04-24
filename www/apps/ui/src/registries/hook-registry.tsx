325a9a3a3d70abba8c3dceed5438a7cd12be001b
Shahed Nasser <shahednasser@gmail.com>
2024-04-24 11:35:14 +0300

    remove unused file

--- www/apps/ui/src/registries/hook-registry.tsx
+++ www/apps/ui/src/registries/hook-registry.tsx
@@ -2,9 +2,6 @@ import { HookRegistryItem } from "@/types/hooks"
 import * as React from "react"
 
 export const HookRegistry: Record<string, HookRegistryItem> = {
-  ToasterToast: {
-    table: React.lazy(async () => import("../props/hooks/ToasterToast")),
-  },
   usePrompt: {
     table: React.lazy(async () => import("../props/hooks/usePrompt")),
   },
