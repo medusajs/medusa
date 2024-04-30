0f8a375018ef4e07f31160f29e27657029ba8236
Riqwan Thamir <rmthamir@gmail.com>
2024-04-30 14:04:01 +0200

    chore: add middleware filters + scope products

--- packages/medusa/src/api-v2/utils/middlewares/common/apply-default-filters.ts
+++ packages/medusa/src/api-v2/utils/middlewares/common/apply-default-filters.ts
@@ -1,26 +1,37 @@
-import { isObject } from "@medusajs/utils"
+import { isObject, isPresent } from "@medusajs/utils"
 import { NextFunction } from "express"
 import { MedusaRequest } from "../../../../types/routing"
 
-export function applyDefaultFilters<TFilter extends object>(filters: TFilter) {
+export function applyDefaultFilters<TFilter extends object>(
+  filtersToApply: TFilter
+) {
   return async (req: MedusaRequest, _, next: NextFunction) => {
-    const filterableFields = req.filterableFields || {}
+    for (const [filter, filterValue] of Object.entries(filtersToApply)) {
+      let valueToApply = filterValue
 
-    for (const [filter, filterValue] of Object.entries(filters)) {
-      let existingFilter = filterableFields[filter]
+      // If certain manipulations need to be done on a middleware level, we can provide a simple
+      // function that mutates the data based on any custom requirement
+      if (typeof filterValue === "function") {
+        // pass the actual filterable fields so that the function can mutate the original object.
+        // Currently we only need it to delete filter keys from the request filter object, but this could
+        // be used for other purposes. If we can't find other purposes, we can refactor to accept an array
+        // of strings to delete after filters have been applied.
+        valueToApply = filterValue(
+          req.filterableFields,
+          req.remoteQueryConfig.fields
+        )
+      }
 
-      if (existingFilter && isObject(existingFilter)) {
-        // If an existing filter is found, append to it
-        filterableFields[filter] = {
-          ...existingFilter,
-          [filter]: filterValue,
+      // If the value to apply is an object, we add it to any existing filters thats already applied
+      if (isObject(valueToApply)) {
+        req.filterableFields[filter] = {
+          ...(req.filterableFields[filter] || {}),
+          ...valueToApply,
         }
-      } else {
-        filterableFields[filter] = filterValue
+      } else if (isPresent(valueToApply)) {
+        req.filterableFields[filter] = valueToApply
       }
     }
-
-    req.filterableFields = filterableFields
 
     return next()
   }
