b5aea3f11d3687f4b261f602f2d704a467fa4434
olivermrbl <oliver@mrbltech.com>
2024-05-01 14:26:23 +0200

    chore: Remove deprecated plugins

--- packages/medusa-plugin-economic/src/api/routes/book-invoice.js
+++
@@ -1,5 +0,0 @@
-export default async (req, res) => {
-  const economicService = req.scope.resolve("economicService")
-  await economicService.bookEconomicInvoice(req.body.orderId)
-  res.sendStatus(200)
-}
