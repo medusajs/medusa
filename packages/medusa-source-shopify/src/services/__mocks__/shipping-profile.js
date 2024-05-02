f430ed7040f766dd7bbfd6d9911cf3fb90ef1603
Oli Juhl <59018053+olivermrbl@users.noreply.github.com>
2024-05-02 09:55:50 +0200

    chore: Remove deprecated plugins (#7199)

--- packages/medusa-source-shopify/src/services/__mocks__/shipping-profile.js
+++
@@ -1,8 +0,0 @@
-export const ShippingProfileServiceMock = {
-  retrieveGiftCardDefault: jest.fn().mockImplementation((_data) => {
-    return Promise.resolve({ id: "gift_card_profile" })
-  }),
-  retrieveDefault: jest.fn().mockImplementation((_data) => {
-    return Promise.resolve({ id: "default_shipping_profile" })
-  }),
-}
