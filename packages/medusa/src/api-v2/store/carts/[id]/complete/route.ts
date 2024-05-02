f1294156505f94bfdcbe8c621f39d1655ed5314d
Riqwan Thamir <rmthamir@gmail.com>
2024-05-03 00:24:50 +0200

    feat(medusa,core-flows): complete cart [part-1] (#7201)
    what:
    
    - adds a very basic complete cart endpoint that creates an order
    - the complete cart workflow currently does the following:
      - create tax lines
      - create order

---
+++ packages/medusa/src/api-v2/store/carts/[id]/complete/route.ts
@@ -0,0 +1,34 @@
+import { completeCartWorkflow } from "@medusajs/core-flows"
+import { MedusaRequest, MedusaResponse } from "../../../../../types/routing"
+import { refetchOrder } from "../../../orders/helpers"
+import { StoreCompleteCartType } from "../../validators"
+
+export const POST = async (
+  req: MedusaRequest<StoreCompleteCartType>,
+  res: MedusaResponse
+) => {
+  const { idempotency_key: idempotencyKey } = req.validatedBody
+
+  // If the idempotencyKey is present:
+  //  - is workflow is running?
+  //    = throw error
+  //  - else
+  //   - re-run the workflow at the failed step
+
+  const { errors, result } = await completeCartWorkflow(req.scope).run({
+    input: { id: req.params.id },
+    throwOnError: false,
+  })
+
+  if (Array.isArray(errors) && errors[0]) {
+    throw errors[0].error
+  }
+
+  const order = await refetchOrder(
+    result.id,
+    req.scope,
+    req.remoteQueryConfig.fields
+  )
+
+  res.status(200).json({ order })
+}
