55c828007e5818e1e2ef25f5736e8a2e2a97537c
adrien2p <adrien.deperetti@gmail.com>
2024-05-03 10:14:59 +0200

    rm plugins

--- integration-tests/plugins/medusa-config.js
+++ integration-tests/plugins/medusa-config.js
@@ -11,23 +11,7 @@ process.env.LOG_LEVEL = "error"
 const enableMedusaV2 = process.env.MEDUSA_FF_MEDUSA_V2 == "true"
 
 module.exports = {
-  plugins: [
-    {
-      resolve: `medusa-fulfillment-webshipper`,
-      options: {
-        account: "test-account",
-        api_token: "something",
-        order_channel_id: "1",
-        webhook_secret: "1234",
-      },
-    },
-    {
-      resolve: `medusa-plugin-sendgrid`,
-      options: {
-        api_key: "SG.TEST",
-      },
-    },
-  ],
+  plugins: [],
   projectConfig: {
     // redis_url: REDIS_URL,
     database_url: DB_URL,
