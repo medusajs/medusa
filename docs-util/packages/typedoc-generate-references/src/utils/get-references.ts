4b530b82c2408d3d158ad1d5b987ef94f1f4f048
Shahed Nasser <shahednasser@gmail.com>
2024-05-03 17:48:36 +0300

    remove remaining docs-util

--- docs-util/packages/typedoc-generate-references/src/utils/get-references.ts
+++
@@ -1,5 +0,0 @@
-import allReferences from "../constants/references.js"
-
-export default function getReferences(names: string[]): string[] {
-  return names.includes("all") ? allReferences : names
-}
