35f2c872f1757b6db74d0883268a1ef953d0e99a
Riqwan Thamir <rmthamir@gmail.com>
2024-05-08 20:43:19 +0200

    chore: split workflows step into 2

--- packages/core/core-flows/src/promotion/workflows/add-or-remove-campaign-promotions.ts
+++ packages/core/core-flows/src/promotion/workflows/add-or-remove-campaign-promotions.ts
@@ -1,12 +1,16 @@
 import { LinkWorkflowInput } from "@medusajs/types"
 import { WorkflowData, createWorkflow } from "@medusajs/workflows-sdk"
-import { addOrRemoveCampaignPromotionsStep } from "../steps"
+import {
+  addCampaignPromotionsStep,
+  removeCampaignPromotionsStep,
+} from "../steps"
 
 export const addOrRemoveCampaignPromotionsWorkflowId =
   "add-or-remove-campaign-promotions"
 export const addOrRemoveCampaignPromotionsWorkflow = createWorkflow(
   addOrRemoveCampaignPromotionsWorkflowId,
   (input: WorkflowData<LinkWorkflowInput>): WorkflowData<void> => {
-    addOrRemoveCampaignPromotionsStep(input)
+    addCampaignPromotionsStep(input)
+    removeCampaignPromotionsStep(input)
   }
 )
