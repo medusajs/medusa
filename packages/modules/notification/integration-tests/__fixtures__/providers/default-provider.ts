144e09e852a5be625d1e325b70f9255aa36369a5
Stevche Radevski <sradevski@live.com>
2024-05-10 11:22:03 +0200

    feat: Add the basic implementation of notification module (#7282)
    * feat: Add the basic implementation of notification module
    
    * fix: Minor fixes and introduction of idempotency key
    
    * fix: Changes based on PR review

---
+++ packages/modules/notification/integration-tests/__fixtures__/providers/default-provider.ts
@@ -0,0 +1,14 @@
+import { NotificationTypes } from "@medusajs/types"
+import { AbstractNotificationProviderService } from "@medusajs/utils/src"
+
+export class NotificationProviderServiceFixtures extends AbstractNotificationProviderService {
+  static identifier = "fixtures-notification-provider"
+
+  async send(
+    notification: NotificationTypes.ProviderSendNotificationDTO
+  ): Promise<NotificationTypes.ProviderSendNotificationResultsDTO> {
+    return { id: "external_id" }
+  }
+}
+
+export const services = [NotificationProviderServiceFixtures]
