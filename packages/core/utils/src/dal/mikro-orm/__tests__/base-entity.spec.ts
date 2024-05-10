9b743f6dfd36458aa3631b4fe68f1d8d07b3c73a
adrien2p <adrien.deperetti@gmail.com>
2024-05-10 09:50:33 +0200

    chore(): Provide a mikro orm base entity

---
+++ packages/core/utils/src/dal/mikro-orm/__tests__/base-entity.spec.ts
@@ -0,0 +1,73 @@
+import { Entity, MikroORM } from "@mikro-orm/core"
+import { MikroOrmBaseEntity } from "../base-entity"
+
+describe("BaseEntity", () => {
+  it("should handle the id generation using the provided prefix", async () => {
+    @Entity()
+    class Entity1 extends MikroOrmBaseEntity {
+      constructor() {
+        super({ prefix_id: "ent" })
+      }
+    }
+
+    const orm = await MikroORM.init({
+      entities: [Entity1],
+      dbName: "test",
+      type: "postgresql",
+    })
+
+    const manager = orm.em.fork()
+    const entity1 = manager.create(Entity1, {})
+
+    expect(entity1.id).toMatch(/ent_[0-9]/)
+
+    await orm.close()
+  })
+
+  it("should handle the id generation without a provided prefix using the first three letter of the entity lower cased", async () => {
+    @Entity()
+    class Entity1 extends MikroOrmBaseEntity {}
+
+    const orm = await MikroORM.init({
+      entities: [Entity1],
+      dbName: "test",
+      type: "postgresql",
+    })
+
+    const manager = orm.em.fork()
+    const entity1 = manager.create(Entity1, {})
+
+    expect(entity1.id).toMatch(/ent_[0-9]/)
+
+    await orm.close()
+  })
+
+  it("should handle the id generation without a provided prefix inferring it based on the words composing the entity name excluding model and entity as part of the name", async () => {
+    @Entity()
+    class ProductModel extends MikroOrmBaseEntity {}
+
+    @Entity()
+    class ProductCategoryEntity extends MikroOrmBaseEntity {}
+
+    @Entity()
+    class ProductOptionValue extends MikroOrmBaseEntity {}
+
+    const orm = await MikroORM.init({
+      entities: [ProductModel, ProductCategoryEntity, ProductOptionValue],
+      dbName: "test",
+      type: "postgresql",
+    })
+
+    const manager = orm.em.fork()
+
+    const product = manager.create(ProductModel, {})
+    const productCategory = manager.create(ProductCategoryEntity, {})
+    const productOptionValue = manager.create(ProductOptionValue, {})
+
+    expect(product.id).toMatch(/pro_[0-9]/)
+    expect(productCategory.id).toMatch(/prc_[0-9]/)
+    expect(productOptionValue.id).toMatch(/pov_[0-9]/)
+
+    await orm.close()
+  })
+})
