95fee321279d86ba089ae66946486a5796767bb1
adrien2p <adrien.deperetti@gmail.com>
2024-05-10 17:49:56 +0200

    cast

--- packages/modules/workflow-engine-redis/src/module-definition.ts
+++ packages/modules/workflow-engine-redis/src/module-definition.ts
@@ -4,7 +4,7 @@ import redisConnection from "./loaders/redis"
 import loadUtils from "./loaders/utils"
 
 const service = WorkflowsModuleService
-const loaders = [loadUtils, redisConnection]
+const loaders = [loadUtils, redisConnection] as any
 
 export const moduleDefinition: ModuleExports = {
   service,
