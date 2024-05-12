58d15f5bd397620af365b773b88f26f6baaf3aba
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-05-12 09:59:03 -0300

    endpoint

--- packages/medusa/src/api-v2/store/return/route.ts
+++ packages/medusa/src/api-v2/store/return/route.ts
@@ -4,14 +4,7 @@ import { MedusaRequest, MedusaResponse } from "../../../types/routing"
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   const input = [req.validatedBody as CreateOrderReturnDTO]
 
-  const { result, errors } = await createReturnsWorkflow(req.scope).run({
-    input: { products: input },
-    throwOnError: false,
-  })
+  // TODO: create return workflow
 
-  if (Array.isArray(errors) && errors[0]) {
-    throw errors[0].error
-  }
-
-  res.status(200).json({ return: result })
+  res.status(200).json({ return: {} })
 }
