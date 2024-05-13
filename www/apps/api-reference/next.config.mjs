6be69814b09f3691354a40cca29772f79d7789e4
Shahed Nasser <shahednasser@gmail.com>
2024-05-13 11:05:07 +0300

    fix condition

--- www/apps/api-reference/next.config.mjs
+++ www/apps/api-reference/next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
           source: "/:path*",
           destination: `${process.env.NEXT_PUBLIC_DOCS_URL}/:path*`,
         },
-        process.env.NEXT_PUBLIC_VERSIONING ? {
+        process.env.NEXT_PUBLIC_VERSIONING === "true" ? {
           source: "/v2/:path*",
           destination: `${process.env.NEXT_PUBLIC_DOCS_V2_URL}/v2/:path*`,
         } : {}
