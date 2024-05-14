f5fe1f08a9743a1687f0078e980ea459942d0159
adrien2p <adrien.deperetti@gmail.com>
2024-05-14 09:55:45 +0200

    Add few validation

--- packages/core/core-flows/src/order/workflows/create-return.ts
+++ packages/core/core-flows/src/order/workflows/create-return.ts
@@ -1,10 +1,57 @@
-import { CreateOrderDTO, OrderDTO } from "@medusajs/types"
-import { createWorkflow, WorkflowData } from "@medusajs/workflows-sdk"
+import { CreateOrderReturnDTO, OrderDTO } from "@medusajs/types"
+import {
+  createWorkflow,
+  transform,
+  WorkflowData,
+} from "@medusajs/workflows-sdk"
+import { useRemoteQueryStep } from "../../common"
+import { arrayDifference, MedusaError } from "@medusajs/utils"
+
+function throwIfOrderIsCancelled(order: OrderDTO) {
+  return transform({ order }, (data) => {
+    if (false /*order.cancelled_at*/) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        `Order with id ${order.id} has been cancelled.`
+      )
+    }
+  })
+}
+
+function throwIfItemsDoesNotExistsInOrder(
+  order: OrderDTO,
+  inputItems: CreateOrderReturnDTO["items"]
+) {
+  return transform({ order, inputItems }, (data) => {
+    const orderItemIds = data.order.items?.map((i) => i.id) ?? []
+    const inputItemIds = data.inputItems.map((i) => i.item_id)
+    const diff = arrayDifference(inputItemIds, orderItemIds)
+
+    if (diff.length) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        `Items with ids ${diff.join(", ")} does not exist in order with id ${
+          order.id
+        }.`
+      )
+    }
+  })
+}
 
 export const createReturnsWorkflowId = "create-returns"
 export const createReturnsWorkflow = createWorkflow(
   createReturnsWorkflowId,
-  (input: WorkflowData<CreateOrderDTO>): WorkflowData<OrderDTO> => {
+  (input: WorkflowData<CreateOrderReturnDTO>): WorkflowData<OrderDTO> => {
+    const order = useRemoteQueryStep({
+      entry_point: "orders",
+      fields: ["*"],
+      variables: { id: input.order_id },
+      list: false,
+    })
+
+    throwIfOrderIsCancelled(order)
+    throwIfItemsDoesNotExistsInOrder(order, input.items)
+
     /*const variantIds = transform({ input }, (data) => {
       return (data.input.items ?? [])
         .map((item) => item.variant_id)
