bb6e2b414060e3bdf61e83c09c477749c48307e4
Riqwan Thamir <rmthamir@gmail.com>
2024-05-15 15:53:31 +0200

    chore: fkix cart spec

--- integration-tests/modules/__tests__/cart/store/carts.spec.ts
+++ integration-tests/modules/__tests__/cart/store/carts.spec.ts
@@ -535,6 +535,7 @@ medusaIntegrationTestRunner({
               target_type: "items",
               allocation: "each",
               value: 300,
+              currency_code: "usd",
               apply_to_quantity: 1,
               max_quantity: 1,
               target_rules: targetRules,
@@ -548,6 +549,7 @@ medusaIntegrationTestRunner({
               type: "fixed",
               target_type: "items",
               allocation: "across",
+              currency_code: "usd",
               value: 1000,
               apply_to_quantity: 1,
               target_rules: targetRules,
@@ -1233,6 +1235,7 @@ medusaIntegrationTestRunner({
               allocation: "across",
               value: 300,
               apply_to_quantity: 2,
+              currency_code: "usd",
               target_rules: [
                 {
                   attribute: "product_id",
