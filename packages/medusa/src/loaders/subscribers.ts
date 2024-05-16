ee924b1b28bc5759347901c393ce18d8004d5b86
Stevche Radevski <sradevski@live.com>
2024-05-16 13:36:09 +0200

    feat: Add a simple configurable notifications subscriber (#7331)
    * feat: Add a simple configurable notifications subscriber that is configurable
    
    * Proposal on awaiting all subscribers to run
    
    * fix: Clean up wait subscribers util and notifications test
    
    ---------
    
    Co-authored-by: adrien2p <adrien.deperetti@gmail.com>

--- packages/medusa/src/loaders/subscribers.ts
+++ packages/medusa/src/loaders/subscribers.ts
@@ -2,21 +2,24 @@ import glob from "glob"
 import path from "path"
 import { asFunction } from "awilix"
 import { MedusaContainer } from "../types/global"
+import { MedusaError } from "@medusajs/utils"
 
 /**
  * Registers all subscribers in the subscribers directory
  */
 export default ({ container }: { container: MedusaContainer }) => {
-  const isTest = process.env.NODE_ENV === "test"
-
-  const corePath = isTest
-    ? "../subscribers/__mocks__/*.js"
-    : "../subscribers/*.js"
+  const corePath = "../subscribers/*.js"
   const coreFull = path.join(__dirname, corePath)
 
   const core = glob.sync(coreFull, { cwd: __dirname })
   core.forEach((fn) => {
     const loaded = require(fn).default
+    if (!loaded) {
+      throw new MedusaError(
+        MedusaError.Types.UNEXPECTED_STATE,
+        `Subscriber ${fn} does not have a default export`
+      )
+    }
 
     container.build(asFunction((cradle) => new loaded(cradle)).singleton())
   })
