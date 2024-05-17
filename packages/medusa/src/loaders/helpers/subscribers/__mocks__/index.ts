05e42bc6fc0d88e8e1558af3b63b0b83c05d01f3
Stevche Radevski <sradevski@live.com>
2024-05-17 17:37:56 +0200

    feat: Add back the subscribers API and load v2 subscribers (#7358)

---
+++ packages/medusa/src/loaders/helpers/subscribers/__mocks__/index.ts
@@ -0,0 +1,18 @@
+import { ModuleRegistrationName } from "@medusajs/modules-sdk"
+
+export const eventBusServiceMock = {
+  subscribe: jest.fn().mockImplementation((...args) => {
+    return Promise.resolve(args)
+  }),
+}
+
+export const containerMock = {
+  // mock .resolve method so if its called with "eventBusService" it returns the mock
+  resolve: jest.fn().mockImplementation((name: string) => {
+    if (name === ModuleRegistrationName.EVENT_BUS) {
+      return eventBusServiceMock
+    } else {
+      return {}
+    }
+  }),
+}
