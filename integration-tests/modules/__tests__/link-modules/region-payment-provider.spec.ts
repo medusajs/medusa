cba04f49c393ad3bd1fa570907e566cd7db2dc92
Riqwan Thamir <rmthamir@gmail.com>
2024-05-20 14:13:33 +0200

    chore: fix test

--- integration-tests/modules/__tests__/link-modules/region-payment-provider.spec.ts
+++ integration-tests/modules/__tests__/link-modules/region-payment-provider.spec.ts
@@ -1,7 +1,7 @@
 import { ModuleRegistrationName, Modules } from "@medusajs/modules-sdk"
 import { IPaymentModuleService, IRegionModuleService } from "@medusajs/types"
+import { ContainerRegistrationKeys } from "@medusajs/utils"
 import { medusaIntegrationTestRunner } from "medusa-test-utils"
-import {ContainerRegistrationKeys} from "@medusajs/utils";
 
 jest.setTimeout(50000)
 
@@ -21,7 +21,9 @@ medusaIntegrationTestRunner({
         appContainer = getContainer()
         regionModule = appContainer.resolve(ModuleRegistrationName.REGION)
         paymentModule = appContainer.resolve(ModuleRegistrationName.PAYMENT)
-        remoteQuery = appContainer.resolve(ContainerRegistrationKeys.REMOTE_QUERY)
+        remoteQuery = appContainer.resolve(
+          ContainerRegistrationKeys.REMOTE_QUERY
+        )
         remoteLink = appContainer.resolve(ContainerRegistrationKeys.REMOTE_LINK)
       })
 
@@ -60,7 +62,7 @@ medusaIntegrationTestRunner({
           },
         })
 
-        expect(links).toHaveLength(1)
+        expect(links).toHaveLength(2)
         expect(links).toEqual(
           expect.arrayContaining([
             expect.objectContaining({
@@ -70,6 +72,10 @@ medusaIntegrationTestRunner({
                   id: "pp_system_default",
                 }),
               ]),
+            }),
+            expect.objectContaining({
+              id: expect.any(String),
+              payment_providers: [],
             }),
           ])
         )
@@ -87,7 +93,7 @@ medusaIntegrationTestRunner({
             }),
             expect.objectContaining({
               id: "pp_system_default_2",
-              regions: []
+              regions: [],
             }),
           ])
         )
