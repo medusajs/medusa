1de52319b129a69f81654bed695c2c6e6bee03f8
Stevche Radevski <sradevski@live.com>
2024-05-23 10:44:12 +0200

    feat: Add support for per-actor auth methods

--- packages/medusa/src/api/auth/[actor_type]/[auth_provider]/callback/route.ts
+++ packages/medusa/src/api/auth/[actor_type]/[auth_provider]/callback/route.ts
@@ -1,5 +1,9 @@
 import { ModuleRegistrationName } from "@medusajs/modules-sdk"
-import { AuthenticationInput, IAuthModuleService } from "@medusajs/types"
+import {
+  AuthenticationInput,
+  IAuthModuleService,
+  ConfigModule,
+} from "@medusajs/types"
 import {
   ContainerRegistrationKeys,
   MedusaError,
@@ -11,6 +15,21 @@ import { generateJwtToken } from "../../../../utils/auth/token"
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const { actor_type, auth_provider } = req.params
   const remoteQuery = req.scope.resolve(ContainerRegistrationKeys.REMOTE_QUERY)
+  const config: ConfigModule = req.scope.resolve(
+    ContainerRegistrationKeys.CONFIG_MODULE
+  )
+
+  const authMethods = (config.projectConfig?.http as any)?.authMethods ?? {}
+  // Not having the config defined would allow for all auth providers for the particular actor.
+  if (authMethods[actor_type]) {
+    if (!authMethods[actor_type].includes(auth_provider)) {
+      throw new MedusaError(
+        MedusaError.Types.NOT_ALLOWED,
+        `The actor type ${actor_type} is not allowed to use the auth provider ${auth_provider}`
+      )
+    }
+  }
+
   const service: IAuthModuleService = req.scope.resolve(
     ModuleRegistrationName.AUTH
   )
