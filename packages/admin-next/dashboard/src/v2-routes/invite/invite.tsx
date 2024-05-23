7a43429ae857b9ed7d3e79e176bd3734ca8fb67c
adrien2p <adrien.deperetti@gmail.com>
2024-05-23 10:24:09 +0200

    fix(admin-next, utils, medusa): Handle db errors properly and fix form

--- packages/admin-next/dashboard/src/v2-routes/invite/invite.tsx
+++ packages/admin-next/dashboard/src/v2-routes/invite/invite.tsx
@@ -233,7 +233,7 @@ const CreateView = ({
         dismissLabel: t("actions.close"),
       })
     } catch (error) {
-      if (isAxiosError(error) && error.response?.status === 400) {
+      if ("type" in error && error.type === "invalid_data") {
         form.setError("root", {
           type: "manual",
           message: t("invite.invalidInvite"),
