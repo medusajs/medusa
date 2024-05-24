1996da129bba30e9a74f762321bec47e9775df54
adrien2p <adrien.deperetti@gmail.com>
2024-05-24 13:42:32 +0200

    cleanup

--- packages/core/workflows-sdk/src/helper/workflow-error.ts
+++
@@ -1,8 +0,0 @@
-export class WorkflowError extends Error {
-  extra_message: string[]
-
-  constructor(message: string, extraMessage?: string[]) {
-    super(message)
-    this.extra_message = extraMessage ?? []
-  }
-}
