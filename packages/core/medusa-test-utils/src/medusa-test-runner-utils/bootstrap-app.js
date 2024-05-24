b63906053560460881a163058117894f559deb69
adrien2p <adrien.deperetti@gmail.com>
2024-05-24 18:24:04 +0200

    fixes

--- packages/core/medusa-test-utils/src/medusa-test-runner-utils/bootstrap-app.js
+++ packages/core/medusa-test-utils/src/medusa-test-runner-utils/bootstrap-app.js
@@ -2,7 +2,9 @@ const path = require("path")
 const express = require("express")
 const getPort = require("get-port")
 const { isObject, promiseAll } = require("@medusajs/utils")
-const { GracefulShutdownServer } = require("@medusajs/medusa")
+const {
+  GracefulShutdownServer,
+} = require("@medusajs/medusa/dist/utils/graceful-shutdown-server")
 
 async function bootstrapApp({ cwd, env = {} } = {}) {
   const app = express()
@@ -16,7 +18,6 @@ async function bootstrapApp({ cwd, env = {} } = {}) {
   const { container, shutdown } = await loaders({
     directory: path.resolve(cwd || process.cwd()),
     expressApp: app,
-    isTest: false,
   })
 
   const PORT = await getPort()
