43b26f05cc507ad8a78038764ca65a004e5b2a04
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-05-24 15:19:18 +0200

    add more injection zones

--- packages/admin-next/dashboard/src/routes/collections/collection-detail/collection-detail.tsx
+++ packages/admin-next/dashboard/src/routes/collections/collection-detail/collection-detail.tsx
@@ -1,9 +1,13 @@
 import { Outlet, json, useLoaderData, useParams } from "react-router-dom"
+
 import { JsonViewSection } from "../../../components/common/json-view-section"
 import { useCollection } from "../../../hooks/api/collections"
 import { CollectionGeneralSection } from "./components/collection-general-section"
 import { CollectionProductSection } from "./components/collection-product-section"
 import { collectionLoader } from "./loader"
+
+import after from "virtual:medusa/widgets/product_collection/details/after"
+import before from "virtual:medusa/widgets/product_collection/details/before"
 
 export const CollectionDetail = () => {
   const initialData = useLoaderData() as Awaited<
@@ -29,9 +33,23 @@ export const CollectionDetail = () => {
 
   return (
     <div className="flex flex-col gap-y-2">
+      {before.widgets.map((w, i) => {
+        return (
+          <div key={i}>
+            <w.Component />
+          </div>
+        )
+      })}
       <CollectionGeneralSection collection={collection} />
       <CollectionProductSection collection={collection} />
       <JsonViewSection data={collection} />
+      {after.widgets.map((w, i) => {
+        return (
+          <div key={i}>
+            <w.Component />
+          </div>
+        )
+      })}
       <Outlet />
     </div>
   )
