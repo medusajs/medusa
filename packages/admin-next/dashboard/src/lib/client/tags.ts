76b494aaa9b9bd597c85ddf25f3f7f9bf6f8badc
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-05-28 14:42:11 +0200

    cleanup

--- packages/admin-next/dashboard/src/lib/client/tags.ts
+++ packages/admin-next/dashboard/src/lib/client/tags.ts
@@ -1,8 +1,7 @@
-import { ProductCollectionListRes } from "../../types/api-responses"
 import { getRequest } from "./common"
 
 async function listProductTags(query?: Record<string, any>) {
-  return getRequest<ProductCollectionListRes>(`/admin/tags`, query)
+  return getRequest<any>(`/admin/tags`, query)
 }
 
 async function retrieveProductTag(id: string, query?: Record<string, any>) {
