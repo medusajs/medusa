41a1ca1458fc3e1d285d1164f3066912b1fd6152
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-05-28 10:34:27 +0200

    add campaign zones

--- packages/admin-next/dashboard/src/routes/campaigns/campaign-list/campaign-list.tsx
+++ packages/admin-next/dashboard/src/routes/campaigns/campaign-list/campaign-list.tsx
@@ -1,10 +1,28 @@
 import { Outlet } from "react-router-dom"
+
 import { CampaignListTable } from "./components/campaign-list-table"
+
+import after from "virtual:medusa/widgets/campaign/list/after"
+import before from "virtual:medusa/widgets/campaign/list/before"
 
 export const CampaignList = () => {
   return (
     <div className="flex flex-col gap-y-2">
+      {before.widgets.map((w, i) => {
+        return (
+          <div key={i}>
+            <w.Component />
+          </div>
+        )
+      })}
       <CampaignListTable />
+      {after.widgets.map((w, i) => {
+        return (
+          <div key={i}>
+            <w.Component />
+          </div>
+        )
+      })}
       <Outlet />
     </div>
   )
