e2f7fde19d9955fb093d5c023f955ac8e122783b
Harminder Virk <virk.officials@gmail.com>
2024-05-30 18:16:41 +0530

    WIP

--- packages/core/types/src/common/medusa-container.ts
+++ packages/core/types/src/common/medusa-container.ts
@@ -1,19 +1,32 @@
-import { AwilixContainer } from "awilix"
+import { AwilixContainer, ResolveOptions } from "awilix"
 
 /**
- * The Medusa Container extends [Awilix](https://github.com/jeffijoe/awilix) to 
+ * The following interface acts as a bucket that other modules or the
+ * utils package can fill using declaration merging
+ */
+export interface ModuleImplementations {}
+
+/**
+ * The Medusa Container extends [Awilix](https://github.com/jeffijoe/awilix) to
  * provide dependency injection functionalities.
  */
-export type MedusaContainer = AwilixContainer & {
-  /**
-   * @ignore
-   */
-  registerAdd: <T>(name: string, registration: T) => MedusaContainer
-  /**
-   * @ignore
-   */
-  createScope: () => MedusaContainer
-}
+export type MedusaContainer<Cradle extends object = ModuleImplementations> =
+  Omit<AwilixContainer, "resolve"> & {
+    resolve<K extends keyof Cradle>(
+      key: K,
+      resolveOptions?: ResolveOptions
+    ): Cradle[K]
+    resolve<T = unknown>(key: string, resolveOptions?: ResolveOptions): T
+
+    /**
+     * @ignore
+     */
+    registerAdd: <T>(name: string, registration: T) => MedusaContainer
+    /**
+     * @ignore
+     */
+    createScope: () => MedusaContainer
+  }
 
 export type ContainerLike = {
   resolve<T = unknown>(key: string): T
