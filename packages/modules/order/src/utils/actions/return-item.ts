520d24ef1a95e0d4e888b83268d258fc25be214d
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-06-01 12:12:22 -0300

    error message

--- packages/modules/order/src/utils/actions/return-item.ts
+++ packages/modules/order/src/utils/actions/return-item.ts
@@ -42,7 +42,7 @@ OrderChangeProcessing.registerActionType(ChangeActionType.RETURN_ITEM, {
     if (!existing) {
       throw new MedusaError(
         MedusaError.Types.INVALID_DATA,
-        `Reference ID "${refId}" not found.`
+        `Item ID "${refId}" not found.`
       )
     }
 
