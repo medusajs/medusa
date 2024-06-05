a1b7c668601c82f8c381a025a7963411c6822ad9
olivermrbl <oliver@mrbltech.com>
2024-06-05 08:56:36 +0200

    chore: Split up admin domain in js-sdk

---
+++ packages/core/js-sdk/src/admin/customer.ts
@@ -0,0 +1,80 @@
+import {
+  DeleteResponse,
+  FindParams,
+  HttpTypes,
+  PaginatedResponse,
+  SelectParams,
+} from "@medusajs/types"
+import { Client } from "../client"
+import { ClientHeaders } from "../types"
+
+export class Customer {
+  private client: Client
+  constructor(client: Client) {
+    this.client = client
+  }
+
+  async create(
+    body: HttpTypes.AdminCreateCustomer,
+    query?: SelectParams,
+    headers?: ClientHeaders
+  ) {
+    return this.client.fetch<{
+      customer: HttpTypes.AdminCustomer
+    }>(`/admin/customers`, {
+      method: "POST",
+      headers,
+      body,
+      query,
+    })
+  }
+
+  async update(
+    id: string,
+    body: HttpTypes.AdminUpdateCustomer,
+    query?: SelectParams,
+    headers?: ClientHeaders
+  ) {
+    return this.client.fetch<{ customer: HttpTypes.AdminCustomer }>(
+      `/admin/customers/${id}`,
+      {
+        method: "POST",
+        headers,
+        body,
+        query,
+      }
+    )
+  }
+
+  async list(
+    queryParams?: FindParams & HttpTypes.AdminCollectionFilters,
+    headers?: ClientHeaders
+  ) {
+    return this.client.fetch<
+      PaginatedResponse<{ customers: HttpTypes.AdminCustomer[] }>
+    >(`/admin/customers`, {
+      headers,
+      query: queryParams,
+    })
+  }
+
+  async retrieve(id: string, query?: SelectParams, headers?: ClientHeaders) {
+    return this.client.fetch<{ customer: HttpTypes.AdminCustomer }>(
+      `/admin/customers/${id}`,
+      {
+        query,
+        headers,
+      }
+    )
+  }
+
+  async delete(id: string, headers?: ClientHeaders) {
+    return this.client.fetch<DeleteResponse<"customer">>(
+      `/admin/customers/${id}`,
+      {
+        method: "DELETE",
+        headers,
+      }
+    )
+  }
+}
