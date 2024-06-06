478674e150dc1a147ef420cf580319584d564deb
Shahed Nasser <shahednasser@gmail.com>
2024-06-06 19:48:01 +0300

    change to use refetchEntity

--- packages/medusa/src/api/store/product-categories/[id]/route.ts
+++ packages/medusa/src/api/store/product-categories/[id]/route.ts
@@ -4,26 +4,25 @@ import {
   AuthenticatedMedusaRequest,
   MedusaResponse,
 } from "../../../../types/routing"
-import { refetchEntities } from "../../../utils/refetch-entity"
+import { refetchEntity } from "../../../utils/refetch-entity"
 import { StoreProductCategoryParamsType } from "../validators"
 
 export const GET = async (
   req: AuthenticatedMedusaRequest<StoreProductCategoryParamsType>,
   res: MedusaResponse<StoreProductCategoryResponse>
 ) => {
-  const category = await refetchEntities(
+  const category = await refetchEntity(
     "product_category",
-    { id: req.params.id, ...req.filterableFields },
+    req.params.id,
     req.scope,
-    req.remoteQueryConfig.fields,
-    req.remoteQueryConfig.pagination
+    req.remoteQueryConfig.fields
   )
 
-  if (!category?.length) {
+  if (!category) {
     throw new MedusaError(
       MedusaError.Types.NOT_FOUND,
       `Product category with id: ${req.params.id} was not found`
     )
   }
-  res.json({ product_category: category[0] })
+  res.json({ product_category: category })
 }
