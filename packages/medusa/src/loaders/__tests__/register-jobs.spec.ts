bef5941714f111f4198e7c8ddf5d5ab1cbaf958e
Stevche Radevski <sradevski@live.com>
2024-06-11 13:35:56 +0200

    feat: Add support for scheduled jobs (#7674)

---
+++ packages/medusa/src/loaders/__tests__/register-jobs.spec.ts
@@ -0,0 +1,19 @@
+import path from "path"
+import { registerJobs } from "../helpers/register-jobs"
+import { WorkflowManager, WorkflowScheduler } from "@medusajs/orchestration"
+import { MockSchedulerStorage } from "../__fixtures__/mock-scheduler-storage"
+
+describe("register jobs", () => {
+  WorkflowScheduler.setStorage(new MockSchedulerStorage())
+  it("registers jobs from plugins", async () => {
+    await registerJobs([
+      { resolve: path.join(__dirname, "../__fixtures__/plugin") },
+    ])
+    const workflow = WorkflowManager.getWorkflow("summarize-orders-job")
+    expect(workflow).toBeDefined()
+    expect(workflow?.options.schedule).toEqual({
+      cron: "* * * * * *",
+      numberOfExecutions: 2,
+    })
+  })
+})
