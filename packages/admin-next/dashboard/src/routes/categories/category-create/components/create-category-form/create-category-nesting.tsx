7efce44484d23f97f2d8d5c6ac4b44476896b10f
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-06-17 15:43:25 +0200

    fix flickering in create form

--- packages/admin-next/dashboard/src/routes/categories/category-create/components/create-category-form/create-category-nesting.tsx
+++ packages/admin-next/dashboard/src/routes/categories/category-create/components/create-category-form/create-category-nesting.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { UseFormReturn, useWatch } from "react-hook-form"
 import { useProductCategories } from "../../../../../hooks/api/categories"
 import { CategoryTree } from "../../../common/components/category-tree"
@@ -8,11 +8,17 @@ import { CreateCategorySchema } from "./schema"
 
 type CreateCategoryNestingProps = {
   form: UseFormReturn<CreateCategorySchema>
+  shouldFreeze?: boolean
 }
 
 const ID = "new-item"
 
-export const CreateCategoryNesting = ({ form }: CreateCategoryNestingProps) => {
+export const CreateCategoryNesting = ({
+  form,
+  shouldFreeze,
+}: CreateCategoryNestingProps) => {
+  const [snapshot, setSnapshot] = useState<CategoryTreeItem[]>([])
+
   const { product_categories, isPending, isError, error } =
     useProductCategories(
       {
@@ -53,7 +59,10 @@ export const CreateCategoryNesting = ({ form }: CreateCategoryNestingProps) => {
     return insertCategoryTreeItem(product_categories ?? [], temp)
   }, [product_categories, watchedName, parentCategoryId, watchedRank])
 
-  const handleChange = ({ parent_category_id, rank }: CategoryTreeItem) => {
+  const handleChange = (
+    { parent_category_id, rank }: CategoryTreeItem,
+    list: CategoryTreeItem[]
+  ) => {
     form.setValue("parent_category_id", parent_category_id, {
       shouldDirty: true,
       shouldTouch: true,
@@ -63,6 +72,8 @@ export const CreateCategoryNesting = ({ form }: CreateCategoryNestingProps) => {
       shouldDirty: true,
       shouldTouch: true,
     })
+
+    setSnapshot(list)
   }
 
   if (isError) {
@@ -71,7 +82,8 @@ export const CreateCategoryNesting = ({ form }: CreateCategoryNestingProps) => {
 
   return (
     <CategoryTree
-      value={value}
+      // When we submit the form we want to freeze the rendered tree to prevent flickering during the exit animation
+      value={shouldFreeze ? snapshot : value}
       onChange={handleChange}
       enableDrag={(i) => i.id === ID}
       showBadge={(i) => i.id === ID}
