2a7606382b77b14f9e6e9b76684b943bef076dc1
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-06-17 09:46:05 -0300

    chore(payment-stripe): smallest unit

---
+++ packages/modules/providers/payment-stripe/src/utils/get-smallest-unit.ts
@@ -0,0 +1,67 @@
+import { BigNumberInput } from "@medusajs/types"
+import { BigNumber, MathBN } from "@medusajs/utils"
+
+function getCurrencyMultiplier(currency) {
+  const currencyMultipliers = {
+    0: [
+      "BIF",
+      "CLP",
+      "DJF",
+      "GNF",
+      "JPY",
+      "KMF",
+      "KRW",
+      "MGA",
+      "PYG",
+      "RWF",
+      "UGX",
+      "VND",
+      "VUV",
+      "XAF",
+      "XOF",
+      "XPF",
+    ],
+    3: ["BHD", "IQD", "JOD", "KWD", "OMR", "TND"],
+  }
+
+  currency = currency.toUpperCase()
+  let power = 2
+  for (const [key, value] of Object.entries(currencyMultipliers)) {
+    if (value.includes(currency)) {
+      power = parseInt(key, 10)
+      break
+    }
+  }
+  return Math.pow(10, power)
+}
+
+/**
+ * Converts an amount to the format required by Stripe based on currency.
+ * https://docs.stripe.com/currencies
+ * @param {BigNumberInput} amount - The amount to be converted.
+ * @param {string} currency - The currency code (e.g., 'USD', 'JOD').
+ * @returns {number} - The converted amount in the smallest currency unit.
+ */
+export function getSmallestUnit(
+  amount: BigNumberInput,
+  currency: string
+): number {
+  const multiplier = getCurrencyMultiplier(currency)
+  const smallestAmount = new BigNumber(MathBN.mult(amount, multiplier))
+  return smallestAmount.numeric
+}
+
+/**
+ * Converts an amount from the smallest currency unit to the standard unit based on currency.
+ * @param {BigNumberInput} amount - The amount in the smallest currency unit.
+ * @param {string} currency - The currency code (e.g., 'USD', 'JOD').
+ * @returns {number} - The converted amount in the standard currency unit.
+ */
+export function getAmountFromSmallestUnit(
+  amount: BigNumberInput,
+  currency: string
+): number {
+  const multiplier = getCurrencyMultiplier(currency)
+  const standardAmount = new BigNumber(MathBN.div(amount, multiplier))
+  return standardAmount.numeric
+}
