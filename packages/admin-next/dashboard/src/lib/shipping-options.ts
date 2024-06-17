2e8e7b27b65a9fa8d03a6456331caa9c7ce14409
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-06-17 16:10:39 +0200

    fix(dashboard,types,js-sdk): Locations & Shipping fixes and cleanup (#7715)

--- packages/admin-next/dashboard/src/lib/shipping-options.ts
+++ packages/admin-next/dashboard/src/lib/shipping-options.ts
@@ -1,13 +1,15 @@
-import { ShippingOptionDTO } from "@medusajs/types"
+import { HttpTypes } from "@medusajs/types"
 
-export function isReturnOption(shippingOption: ShippingOptionDTO) {
+export function isReturnOption(shippingOption: HttpTypes.AdminShippingOption) {
   return !!shippingOption.rules?.find(
     (r) =>
       r.attribute === "is_return" && r.value === "true" && r.operator === "eq"
   )
 }
 
-export function isOptionEnabledInStore(shippingOption: ShippingOptionDTO) {
+export function isOptionEnabledInStore(
+  shippingOption: HttpTypes.AdminShippingOption
+) {
   return !!shippingOption.rules?.find(
     (r) =>
       r.attribute === "enabled_in_store" &&
---
+++ packages/admin-next/dashboard/src/routes/locations/location-service-zone-shipping-option-edit/location-service-zone-shipping-option-edit.tsx
@@ -0,0 +1,44 @@
+import { Heading } from "@medusajs/ui"
+import { useTranslation } from "react-i18next"
+import { json, useParams } from "react-router-dom"
+
+import { RouteDrawer } from "../../../components/route-modal"
+import { useShippingOptions } from "../../../hooks/api/shipping-options"
+import { EditShippingOptionForm } from "./components/edit-region-form"
+
+export const LocationServiceZoneShippingOptionEdit = () => {
+  const { t } = useTranslation()
+
+  const { location_id, so_id } = useParams()
+
+  const { shipping_options, isPending, isError, error } = useShippingOptions({
+    id: so_id,
+  })
+
+  const shippingOption = shipping_options?.find((so) => so.id === so_id)
+
+  if (isError) {
+    throw error
+  }
+
+  if (!isPending && !shippingOption) {
+    throw json(
+      { message: `Shipping option with ID ${so_id} was not found` },
+      404
+    )
+  }
+
+  return (
+    <RouteDrawer>
+      <RouteDrawer.Header>
+        <Heading>{t("stockLocations.shippingOptions.edit.header")}</Heading>
+      </RouteDrawer.Header>
+      {!isPending && shippingOption && (
+        <EditShippingOptionForm
+          shippingOption={shippingOption}
+          locationId={location_id!}
+        />
+      )}
+    </RouteDrawer>
+  )
+}
