57c098645709c6f0ac9ce1dbb37efe90a65819af
fPolic <mainacc.polic@gmail.com>
2024-06-18 17:00:44 +0200

    feat: finish prices section pagination

--- packages/admin-next/dashboard/src/routes/product-variants/product-variant-detail/components/variant-prices-section/variant-prices-section.tsx
+++ packages/admin-next/dashboard/src/routes/product-variants/product-variant-detail/components/variant-prices-section/variant-prices-section.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from "react-i18next"
+import { useState } from "react"
 
-import { Container, Heading } from "@medusajs/ui"
 import { CurrencyDollar } from "@medusajs/icons"
+import { Button, Container, Heading } from "@medusajs/ui"
 import { MoneyAmountDTO, ProductVariantDTO } from "@medusajs/types"
 
 import { ActionMenu } from "../../../../../components/common/action-menu"
 import { getLocaleAmount } from "../../../../../lib/money-amount-helpers"
+import { NoRecords } from "../../../../../components/common/empty-table-content"
 
 type VariantPricesSectionProps = {
   variant: ProductVariantDTO & { prices: MoneyAmountDTO[] }
@@ -14,7 +16,19 @@ type VariantPricesSectionProps = {
 export function VariantPricesSection({ variant }: VariantPricesSectionProps) {
   const { t } = useTranslation()
 
-  const prices = variant.prices.filter((p) => !p.rules?.length) // dispaly just currency prices
+  const prices = variant.prices
+    .filter((p) => !p.rules?.length)
+    .sort((p1, p2) => p1.currency_code?.localeCompare(p2.currency_code)) // display just currency prices
+
+  const [current, setCurrent] = useState(Math.min(prices.length, 3))
+
+  const hasPrices = !!prices.length
+
+  const displayPrices = prices.slice(0, current)
+
+  const onShowMore = () => {
+    setCurrent(Math.min(current + 3, prices.length))
+  }
 
   return (
     <Container className="flex flex-col divide-y p-0">
@@ -34,7 +48,8 @@ export function VariantPricesSection({ variant }: VariantPricesSectionProps) {
           ]}
         />
       </div>
-      {prices.map((price) => {
+      {!hasPrices && <NoRecords className="h-60" />}
+      {displayPrices.map((price) => {
         return (
           <div className="txt-small text-ui-fg-subtle flex justify-between px-6 py-4">
             <span className="font-medium">
@@ -44,6 +59,24 @@ export function VariantPricesSection({ variant }: VariantPricesSectionProps) {
           </div>
         )
       })}
+      {hasPrices && (
+        <div className="txt-small text-ui-fg-subtle flex items-center justify-between px-6 py-4">
+          <span className="font-medium">
+            {t("products.variant.pricesPagination", {
+              total: prices.length,
+              current,
+            })}
+          </span>
+          <Button
+            onClick={onShowMore}
+            disabled={current >= prices.length}
+            className="-mr-3 text-blue-500"
+            variant="transparent"
+          >
+            {t("actions.showMore")}
+          </Button>
+        </div>
+      )}
     </Container>
   )
 }
--- packages/admin-next/dashboard/src/routes/product-variants/product-variant-detail/constants.ts
+++ packages/admin-next/dashboard/src/routes/product-variants/product-variant-detail/constants.ts
@@ -1,2 +1,2 @@
 export const VARIANT_DETAIL_FIELDS =
-  "*inventory_items,*inventory_items.inventory,*inventory_items.inventory.location_levels,*options,*options.option,*prices"
+  "*inventory_items,*inventory_items.inventory,*inventory_items.inventory.location_levels,*options,*options.option,*prices,*prices.price_rules"
