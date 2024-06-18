288e41856b804d72b12df4e68a66ac10b9a60f20
Riqwan Thamir <rmthamir@gmail.com>
2024-06-18 13:51:20 +0200

    fix(core-flows): delete existing payment session before creating new (#7751)
    what:
    
    Multiple active sessions are used for split payments. We don't currently support split payments. Until we have that implemented, this change is to ensure that whenever we create a new payment session, we delete an existing session if present. 
    
    RESOLVES CORE-2284

---
+++ packages/core/core-flows/src/payment-collection/workflows/delete-payment-sessions.ts
@@ -0,0 +1,24 @@
+import { WorkflowData, createWorkflow } from "@medusajs/workflows-sdk"
+import {
+  deletePaymentSessionsStep,
+  validateDeletedPaymentSessionsStep,
+} from "../steps"
+
+interface WorkflowInput {
+  ids: string[]
+}
+
+export const deletePaymentSessionsWorkflowId = "delete-payment-sessions"
+export const deletePaymentSessionsWorkflow = createWorkflow(
+  deletePaymentSessionsWorkflowId,
+  (input: WorkflowData<WorkflowInput>) => {
+    const idsDeleted = deletePaymentSessionsStep({ ids: input.ids })
+
+    validateDeletedPaymentSessionsStep({
+      idsToDelete: input.ids,
+      idsDeleted,
+    })
+
+    return idsDeleted
+  }
+)
