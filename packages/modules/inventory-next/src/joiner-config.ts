3a0c0fe6b9786e02ff599fcc725d16ec2842c2aa
adrien2p <adrien.deperetti@gmail.com>
2024-06-19 11:14:33 +0200

    fix invetory joiner config custom aliases

--- packages/modules/inventory-next/src/joiner-config.ts
+++ packages/modules/inventory-next/src/joiner-config.ts
@@ -5,7 +5,36 @@ import {
 } from "@medusajs/utils"
 import { Modules } from "@medusajs/modules-sdk"
 
-export const joinerConfig = defineJoinerConfig(Modules.INVENTORY)
+export const joinerConfig = defineJoinerConfig(Modules.INVENTORY, {
+  alias: [
+    {
+      name: ["inventory_items", "inventory_item", "inventory"],
+      args: {
+        entity: "InventoryItem",
+        methodSuffix: "InventoryItems",
+      },
+    },
+    {
+      name: ["inventory_level", "inventory_levels"],
+      args: {
+        entity: "InventoryLevel",
+        methodSuffix: "InventoryLevels",
+      },
+    },
+    {
+      name: [
+        "reservation",
+        "reservations",
+        "reservation_item",
+        "reservation_items",
+      ],
+      args: {
+        entity: "ReservationItem",
+        methodSuffix: "ReservationItems",
+      },
+    },
+  ],
+})
 
 export const entityNameToLinkableKeysMap: MapToConfig =
   buildEntitiesNameToLinkableKeysMap(joinerConfig.linkableKeys)
