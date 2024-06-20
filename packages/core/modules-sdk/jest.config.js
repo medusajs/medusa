415691f7f2272ebdf2e4ad405529d69ec1c55e80
adrien2p <adrien.deperetti@gmail.com>
2024-06-20 14:07:40 +0200

    fix modules-sdk jest

--- packages/core/modules-sdk/jest.config.js
+++ packages/core/modules-sdk/jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  transform: { "^.+\\.[jt]s?$": "@swc/jest" },
+  transform: {
+    "^.+\\.[jt]s?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: { syntax: "typescript", decorators: true },
+          transform: { decoratorMetadata: true },
+        },
+      },
+    ],
+  },
   testEnvironment: `node`,
   moduleFileExtensions: [`js`, `ts`],
 }
