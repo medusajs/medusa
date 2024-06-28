3f2db61801243f46f3ffc35f94bf43c22e72dfb6
Stevche Radevski <sradevski@live.com>
2024-06-28 09:54:34 +0200

    chore: Migrate notification module to DML

--- packages/modules/notification/src/loaders/providers.ts
+++ packages/modules/notification/src/loaders/providers.ts
@@ -1,5 +1,11 @@
 import { moduleProviderLoader } from "@medusajs/modules-sdk"
-import { LoaderOptions, ModuleProvider, ModulesSdkTypes } from "@medusajs/types"
+import {
+  DAL,
+  InferEntityType,
+  LoaderOptions,
+  ModuleProvider,
+  ModulesSdkTypes,
+} from "@medusajs/types"
 import {
   ContainerRegistrationKeys,
   lowerCaseFirst,
@@ -62,8 +68,9 @@ async function syncDatabaseProviders({
   const providerServiceRegistrationKey = lowerCaseFirst(
     NotificationProviderService.name
   )
-  const providerService: ModulesSdkTypes.IMedusaInternalService<NotificationProvider> =
-    container.resolve(providerServiceRegistrationKey)
+  const providerService: ModulesSdkTypes.IMedusaInternalService<
+    typeof NotificationProvider
+  > = container.resolve(providerServiceRegistrationKey)
 
   const logger = container.resolve(ContainerRegistrationKeys.LOGGER) ?? console
   const normalizedProviders = providers.map((provider) => {
@@ -100,13 +107,16 @@ async function syncDatabaseProviders({
     const promises: Promise<any>[] = []
 
     if (normalizedProviders.length) {
-      promises.push(providerService.upsert(normalizedProviders))
+      promises.push(providerService.upsertWithReplace(normalizedProviders))
     }
 
     if (providersToDisable.length) {
       promises.push(
         providerService.update(
-          providersToDisable.map((p) => ({ id: p.id, is_enabled: false }))
+          providersToDisable.map((p) => ({
+            entity: p,
+            update: { is_enabled: false },
+          }))
         )
       )
     }
