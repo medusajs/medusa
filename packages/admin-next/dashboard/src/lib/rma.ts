82ac21a57829a1004a5400bb567f9b799cd91905
fPolic <mainacc.polic@gmail.com>
2024-06-28 16:05:57 +0200

    fix: use shipped quantity to determine returnable quantity

--- packages/admin-next/dashboard/src/lib/rma.ts
+++ packages/admin-next/dashboard/src/lib/rma.ts
@@ -2,11 +2,14 @@ import { AdminOrderLineItem } from "@medusajs/types"
 
 export function getReturnableQuantity(item: AdminOrderLineItem): number {
   const {
-    quantity, // TODO: do we use shipped_quantity / fulfilled_quantity here?
+    // TODO: this should be `fulfilled_quantity`? now there is check on the BD that we can't return more quantity than we shipped but some items don't require shipping
+    shipped_quantity,
     return_received_quantity,
     return_dismissed_quantity, // TODO: check this
     return_requested_quantity,
   } = item.detail
 
-  return quantity - (return_received_quantity + return_requested_quantity)
+  return (
+    shipped_quantity - (return_received_quantity + return_requested_quantity)
+  )
 }
