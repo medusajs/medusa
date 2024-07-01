85752371a8a00668c9484caf7b29240fddc66b82
fPolic <mainacc.polic@gmail.com>
2024-07-01 20:41:22 +0200

    feat: placeholder stripes

--- packages/admin-next/dashboard/src/routes/orders/order-create-return/components/return-create-form/return-create-form.tsx
+++ packages/admin-next/dashboard/src/routes/orders/order-create-return/components/return-create-form/return-create-form.tsx
@@ -321,7 +321,13 @@ export const ReturnCreateForm = ({ order }: ReturnCreateFormProps) => {
               </StackedFocusModal>
             </div>
             {showPlaceholder && (
-              <div className="bg-ui-bg-field mt-4 block h-[56px] w-full rounded-lg border border-dashed" />
+              <div
+                style={{
+                  background:
+                    "repeating-linear-gradient(-45deg, rgb(212, 212, 216, 0.15), rgb(212, 212, 216,.15) 10px, transparent 10px, transparent 20px)",
+                }}
+                className="bg-ui-bg-field mt-4 block h-[56px] w-full rounded-lg border border-dashed"
+              />
             )}
             {items.map((item, index) => (
               <ReturnItem
