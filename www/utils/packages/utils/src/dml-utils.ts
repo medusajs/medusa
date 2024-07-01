dd864da4e087df85527470e1d543669cd495fda0
Shahed Nasser <shahednasser@gmail.com>
2024-07-01 16:02:36 +0300

    generate references (#7882)

--- www/utils/packages/utils/src/dml-utils.ts
+++ www/utils/packages/utils/src/dml-utils.ts
@@ -8,9 +8,29 @@ export function isDmlEntity(reflection: DeclarationReflection) {
   return reflection.type.name === "DmlEntity"
 }
 
+export const RELATION_NAMES = ["HasOne", "HasMany", "BelongsTo", "ManyToMany"]
+
+export function isDmlRelation(reflection: DeclarationReflection) {
+  if (reflection.type?.type !== "reference") {
+    return false
+  }
+
+  const reflectionType = reflection.type
+
+  return RELATION_NAMES.some((relatioName) =>
+    reflectionType.name.startsWith(relatioName)
+  )
+}
+
+const MAX_LEVEL = 2
+
 export function getDmlProperties(
-  reflectionType: ReferenceType
+  reflectionType: ReferenceType,
+  level = 1
 ): DeclarationReflection[] {
+  if (level > MAX_LEVEL) {
+    return []
+  }
   if (
     !reflectionType.typeArguments?.length ||
     reflectionType.typeArguments[0].type !== "intersection"
@@ -21,4 +41,22 @@ export function getDmlProperties(
   const schemaType = reflectionType.typeArguments[0].types[0] as ReflectionType
 
   return schemaType.declaration.children || []
+}
+
+export function getDmlRelationProperties(
+  reflectionType: ReferenceType
+): DeclarationReflection[] {
+  if (
+    !reflectionType.typeArguments?.length ||
+    reflectionType.typeArguments[0].type !== "reference" ||
+    !(
+      reflectionType.typeArguments[0].reflection instanceof
+      DeclarationReflection
+    ) ||
+    reflectionType.typeArguments[0].reflection.type?.type !== "reference"
+  ) {
+    return []
+  }
+
+  return getDmlProperties(reflectionType.typeArguments[0].reflection.type)
 }
