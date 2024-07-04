aa0390af4b9533ba71494251858982fdb980eba1
Harminder Virk <virk.officials@gmail.com>
2024-07-04 12:24:07 +0530

    Prettify DML properties when inferred (#7937)

--- packages/core/types/src/dml/index.ts
+++ packages/core/types/src/dml/index.ts
@@ -1,4 +1,4 @@
-import { CamelCase } from "../common"
+import { CamelCase, Prettify } from "../common"
 
 /**
  * Symbol to identify a DML entity from an object
@@ -187,7 +187,7 @@ export type InferManyToManyFields<Relation> = InferHasManyFields<Relation>
  * Inferring the types of the schema fields from the DML
  * entity
  */
-export type InferSchemaFields<Schema extends DMLSchema> = {
+export type InferSchemaFields<Schema extends DMLSchema> = Prettify<{
   [K in keyof Schema]: Schema[K] extends RelationshipType<any>
     ? Schema[K]["type"] extends "belongsTo"
       ? InferBelongsToFields<Schema[K]["$dataType"]>
@@ -199,7 +199,7 @@ export type InferSchemaFields<Schema extends DMLSchema> = {
       ? InferManyToManyFields<Schema[K]["$dataType"]>
       : never
     : Schema[K]["$dataType"]
-}
+}>
 
 /**
  * Helper to infer the schema type of a DmlEntity
