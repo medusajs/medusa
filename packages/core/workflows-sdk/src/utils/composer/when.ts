cdf3c3ba8908cd62d4bf45cbb8d64f72d139cfa6
adrien2p <adrien.deperetti@gmail.com>
2024-07-05 14:47:33 +0200

    feat: Add types to the workflow conditional

--- packages/core/workflows-sdk/src/utils/composer/when.ts
+++ packages/core/workflows-sdk/src/utils/composer/when.ts
@@ -1,4 +1,27 @@
 import { OrchestrationUtils } from "@medusajs/utils"
+import { StepExecutionContext, WorkflowData } from "./type"
+
+type ConditionFunction<T extends object | WorkflowData> = (
+  input: T extends WorkflowData<infer U>
+    ? U
+    : T extends object
+    ? { [K in keyof T]: T[K] extends WorkflowData<infer U> ? U : T[K] }
+    : {},
+  context: StepExecutionContext
+) => boolean
+
+type ThenFunc = <ThenResolver extends () => any>(
+  resolver: ThenResolver
+) => ReturnType<ThenResolver> extends WorkflowData<infer ReturnedWorkflowData>
+  ? Partial<WorkflowData<ReturnedWorkflowData>>
+  : never
+
+export function when<T extends object | WorkflowData, Then extends Function>(
+  values: T,
+  condition: ConditionFunction<T>
+): {
+  then: ThenFunc
+}
 
 export function when(input, condition) {
   global[OrchestrationUtils.SymbolMedusaWorkflowComposerCondition] = {
