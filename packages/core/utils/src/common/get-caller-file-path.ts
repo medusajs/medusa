5ecd28f26d907442ce59560a840f522d7b7ba9ab
adrien2p <adrien.deperetti@gmail.com>
2024-07-05 12:34:35 +0200

    improve compatibility

--- packages/core/utils/src/common/get-caller-file-path.ts
+++ packages/core/utils/src/common/get-caller-file-path.ts
@@ -8,14 +8,22 @@ export function getCallerFilePath(position = 2) {
   }
 
   const oldPrepareStackTrace = Error.prepareStackTrace
-  Error.prepareStackTrace = (_, stack) => stack
-  const stack = new Error().stack
+
+  let result!: string
+
+  Error.prepareStackTrace = (_, stack) => {
+    if (stack !== null && typeof stack === "object") {
+      // stack[0] holds this file
+      // stack[1] holds where this function was called
+      // stack[2] holds the file we're interested in
+      result = stack[position]
+        ? (stack[position] as any).getFileName()
+        : undefined
+    }
+    return stack
+  }
+  new Error().stack
   Error.prepareStackTrace = oldPrepareStackTrace
 
-  if (stack !== null && typeof stack === "object") {
-    // stack[0] holds this file
-    // stack[1] holds where this function was called
-    // stack[2] holds the file we're interested in
-    return stack[position] ? (stack[position] as any).getFileName() : undefined
-  }
+  return result
 }
