a16c2c0f2c5517f4c2f65b32d112c87de07ca39b
adrien2p <adrien.deperetti@gmail.com>
2024-07-05 12:47:42 +0200

    chore: Apply defineMikroOrmCliConfig

--- packages/modules/fulfillment/mikro-orm.config.dev.ts
+++ packages/modules/fulfillment/mikro-orm.config.dev.ts
@@ -1,6 +1,6 @@
 import * as entities from "./src/models"
 import { defineMikroOrmCliConfig, Modules } from "@medusajs/utils"
 
-module.exports = defineMikroOrmCliConfig(Modules.FULFILLMENT, {
+export default defineMikroOrmCliConfig(Modules.FULFILLMENT, {
   entities: Object.values(entities),
 })
