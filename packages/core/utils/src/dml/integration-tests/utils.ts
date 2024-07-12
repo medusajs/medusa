34678aec4bc2332cb93fd07bdf7317f0da015539
adrien2p <adrien.deperetti@gmail.com>
2024-07-12 14:53:15 +0200

    fix

--- packages/core/utils/src/dml/integration-tests/utils.ts
+++ packages/core/utils/src/dml/integration-tests/utils.ts
@@ -1,5 +1,5 @@
-const DB_HOST = process.env.DB_HOST
-const DB_USERNAME = process.env.DB_USERNAME
+const DB_HOST = process.env.DB_HOST ?? "localhost"
+const DB_USERNAME = process.env.DB_USERNAME ?? ""
 const DB_PASSWORD = process.env.DB_PASSWORD
 
 export const pgGodCredentials = {
