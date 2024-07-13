50f435f12150892e3c0b533638ceae9223ba1337
kodiakhq <kodiakhq@users.noreply.github.com>
2024-07-13 00:13:45 +0000

    chore(docs): Generated API Reference (v2)

--- www/apps/api-reference/specs/admin/code_samples/Shell/admin_orders_{id}_fulfillments_{fulfillment_id}_shipments/post.sh
+++ www/apps/api-reference/specs/admin/code_samples/Shell/admin_orders_{id}_fulfillments_{fulfillment_id}_shipments/post.sh
@@ -4,8 +4,8 @@ curl -X POST '{backend_url}/admin/orders/{id}/fulfillments/{fulfillment_id}/ship
 --data-raw '{
   "items": [
     {
-      "id": "id_rnSpxwfxEfbXVCA7",
-      "quantity": 3643591194836992
+      "id": "id_Cw4XVkx06Ph5R9cO",
+      "quantity": 4875752895938560
     }
   ],
   "metadata": {}
