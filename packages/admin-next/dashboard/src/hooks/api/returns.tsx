424bbf4147968962bfaad56960dbaac69fa8ce69
fPolic <mainacc.polic@gmail.com>
2024-07-15 10:24:24 +0200

    feat: add rest of the hooks

--- packages/admin-next/dashboard/src/hooks/api/returns.tsx
+++ packages/admin-next/dashboard/src/hooks/api/returns.tsx
@@ -27,3 +27,75 @@ export const useInitiateReturn = (
     ...options,
   })
 }
+
+export const useAddReturnItem = (
+  id: string,
+  options?: UseMutationOptions<
+    HttpTypes.AdminReturnResponse,
+    Error,
+    HttpTypes.AdminAddReturnItems
+  >
+) => {
+  return useMutation({
+    mutationFn: (payload: HttpTypes.AdminAddReturnItems) =>
+      sdk.admin.return.addReturnItem(id, payload),
+    onSuccess: (data: any, variables: any, context: any) => {
+      queryClient.invalidateQueries({
+        queryKey: ordersQueryKeys.details(),
+      })
+      queryClient.invalidateQueries({
+        queryKey: ordersQueryKeys.lists(),
+      })
+      options?.onSuccess?.(data, variables, context)
+    },
+    ...options,
+  })
+}
+
+export const useAddReturnShipping = (
+  id: string,
+  options?: UseMutationOptions<
+    HttpTypes.AdminReturnResponse,
+    Error,
+    HttpTypes.AdminAddReturnShipping
+  >
+) => {
+  return useMutation({
+    mutationFn: (payload: HttpTypes.AdminAddReturnShipping) =>
+      sdk.admin.return.addReturnShipping(id, payload),
+    onSuccess: (data: any, variables: any, context: any) => {
+      queryClient.invalidateQueries({
+        queryKey: ordersQueryKeys.details(),
+      })
+      queryClient.invalidateQueries({
+        queryKey: ordersQueryKeys.lists(),
+      })
+      options?.onSuccess?.(data, variables, context)
+    },
+    ...options,
+  })
+}
+
+export const useAConfirmReturnRequest = (
+  id: string,
+  options?: UseMutationOptions<
+    HttpTypes.AdminReturnResponse,
+    Error,
+    HttpTypes.AdminConfirmReturnRequest
+  >
+) => {
+  return useMutation({
+    mutationFn: (payload: HttpTypes.AdminConfirmReturnRequest) =>
+      sdk.admin.return.confirmRequest(id, payload),
+    onSuccess: (data: any, variables: any, context: any) => {
+      queryClient.invalidateQueries({
+        queryKey: ordersQueryKeys.details(),
+      })
+      queryClient.invalidateQueries({
+        queryKey: ordersQueryKeys.lists(),
+      })
+      options?.onSuccess?.(data, variables, context)
+    },
+    ...options,
+  })
+}
