2d39f58761b5b74ffbafa2069d78f589900254d7
fPolic <mainacc.polic@gmail.com>
2024-07-21 20:09:54 +0200

    fix: breaking page when there is no initiated return

--- packages/admin-next/dashboard/src/routes/orders/order-create-return/return-create.tsx
+++ packages/admin-next/dashboard/src/routes/orders/order-create-return/return-create.tsx
@@ -36,7 +36,7 @@ export const ReturnCreate = () => {
       /**
        * Active return already exists
        */
-      if (preview.order_change.change_type === "return") {
+      if (preview?.order_change.change_type === "return") {
         setActiveReturnId(preview.order_change.return.id)
         return
       }
