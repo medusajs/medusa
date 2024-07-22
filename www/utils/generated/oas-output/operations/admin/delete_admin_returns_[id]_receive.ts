b93953482189522ba7d7e9350cb51f26f703f671
kodiakhq <kodiakhq@users.noreply.github.com>
2024-07-22 00:14:37 +0000

    chore(docs): Generated API Reference (v2)

--- www/utils/generated/oas-output/operations/admin/delete_admin_returns_[id]_receive.ts
+++ www/utils/generated/oas-output/operations/admin/delete_admin_returns_[id]_receive.ts
@@ -1,8 +1,9 @@
 /**
- * @oas [post] /admin/returns/{id}/cancel
- * operationId: PostReturnsIdCancel
- * summary: Add Cancels to Return
- * description: Add a list of cancels to a return.
+ * @oas [delete] /admin/returns/{id}/receive
+ * operationId: DeleteReturnsIdReceive
+ * summary: Remove Receives from Return
+ * description: Remove a list of receives from a return. This doesn't delete the
+ *   Receive, only the association between the Receive and the return.
  * x-authenticated: true
  * parameters:
  *   - name: id
@@ -21,18 +22,18 @@
  *       description: Comma-separated relations that should be expanded in the returned data.
  *   - name: fields
  *     in: query
- *     description: Comma-separated fields that should be included in the returned
- *       data. if a field is prefixed with `+` it will be added to the default
- *       fields, using `-` will remove it from the default fields. without prefix
- *       it will replace the entire default fields.
+ *     description: >-
+ *       Comma-separated fields that should be included in the returned data.
+ *        * if a field is prefixed with `+` it will be added to the default fields, using `-` will remove it from the default fields.
+ *        * without prefix it will replace the entire default fields.
  *     required: false
  *     schema:
  *       type: string
  *       title: fields
- *       description: Comma-separated fields that should be included in the returned
- *         data. if a field is prefixed with `+` it will be added to the default
- *         fields, using `-` will remove it from the default fields. without prefix
- *         it will replace the entire default fields.
+ *       description: >-
+ *         Comma-separated fields that should be included in the returned data.
+ *          * if a field is prefixed with `+` it will be added to the default fields, using `-` will remove it from the default fields.
+ *          * without prefix it will replace the entire default fields.
  *   - name: offset
  *     in: query
  *     description: The number of items to skip when retrieving a list.
@@ -69,11 +70,13 @@
  *   - lang: Shell
  *     label: cURL
  *     source: |-
- *       curl -X POST '{backend_url}/admin/returns/{id}/cancel' \
+ *       curl -X DELETE '{backend_url}/admin/returns/{id}/receive' \
  *       -H 'x-medusa-access-token: {api_token}'
  * tags:
  *   - Returns
  * responses:
+ *   "200":
+ *     description: OK
  *   "400":
  *     $ref: "#/components/responses/400_error"
  *   "401":
@@ -86,11 +89,6 @@
  *     $ref: "#/components/responses/invalid_request_error"
  *   "500":
  *     $ref: "#/components/responses/500_error"
- * requestBody:
- *   content:
- *     application/json:
- *       schema:
- *         $ref: "#/components/schemas/AdminPostCancelReturnReqSchema"
  * 
 */
 
