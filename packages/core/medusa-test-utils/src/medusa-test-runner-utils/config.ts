329279770aece5b79b5d342e2ed90c56252211ec
adrien2p <adrien.deperetti@gmail.com>
2024-07-23 12:36:39 +0200

    fix

--- packages/core/medusa-test-utils/src/medusa-test-runner-utils/config.ts
+++ packages/core/medusa-test-utils/src/medusa-test-runner-utils/config.ts
@@ -4,6 +4,8 @@ export async function configLoaderOverride(
   entryDirectory: string,
   override: { clientUrl: string; debug?: boolean }
 ) {
+  // in case it is not install as it is optional and required only when using this util
+  // ts-ignore
   const { configManager } = await import("@medusajs/framework/config")
   const { configModule, error } = getConfigFile<
     ReturnType<typeof configManager.loadConfig>
