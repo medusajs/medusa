7a0941e9971b0c9e0ccc820c8e526ffe0d550ea2
Shahed Nasser <shahednasser@gmail.com>
2024-07-24 17:06:54 +0300

    docs: fix rewrites in sub-projects to prevent loop on 404 page (#8255)

--- www/apps/ui/next.config.js
+++ www/apps/ui/next.config.js
@@ -3,19 +3,6 @@ const { withContentlayer } = require("next-contentlayer")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/ui",
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/:path*",
-          destination: `${
-            process.env.NEXT_PUBLIC_DOCS_URL || "https://localhost:3001"
-          }/:path*`,
-          basePath: false,
-        },
-      ],
-    }
-  },
   reactStrictMode: true,
 }
 
