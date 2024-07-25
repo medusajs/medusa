516e78c97abd13d17bb3349cfab359b7308d38df
Harminder Virk <virk.officials@gmail.com>
2024-07-25 11:16:53 +0530

    refactor: remove loading middlewares.ts file in favor of routes-config.ts file
    Breaking change: The `middlewares.ts` file is no longer loaded.
    Fixes: FRMW-2622

--- packages/medusa/src/loaders/helpers/routing/__fixtures__/routers-middleware/routes-config.ts
+++ packages/medusa/src/loaders/helpers/routing/__fixtures__/routers-middleware/routes-config.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response, raw } from "express"
-import { MiddlewaresConfig } from "../../types"
 import {
   customersCreateMiddlewareMock,
   customersGlobalMiddlewareMock,
   storeGlobalMiddlewareMock,
 } from "../mocks"
+import { defineRoutesConfig } from "../../../../../utils/define-routes-config"
 
 const customersGlobalMiddleware = (
   req: Request,
@@ -29,26 +29,24 @@ const storeGlobal = (req: Request, res: Response, next: NextFunction) => {
   next()
 }
 
-export const config: MiddlewaresConfig = {
-  routes: [
-    {
-      matcher: "/customers",
-      middlewares: [customersGlobalMiddleware],
-    },
-    {
-      method: "POST",
-      matcher: "/customers",
-      middlewares: [customersCreateMiddleware],
-    },
-    {
-      matcher: "/store/*",
-      middlewares: [storeGlobal],
-    },
-    {
-      matcher: "/webhooks/*",
-      method: "POST",
-      bodyParser: false,
-      middlewares: [raw({ type: "application/json" })],
-    },
-  ],
-}
+export default defineRoutesConfig([
+  {
+    matcher: "/customers",
+    middlewares: [customersGlobalMiddleware],
+  },
+  {
+    method: "POST",
+    matcher: "/customers",
+    middlewares: [customersCreateMiddleware],
+  },
+  {
+    matcher: "/store/*",
+    middlewares: [storeGlobal],
+  },
+  {
+    matcher: "/webhooks/*",
+    method: "POST",
+    bodyParser: false,
+    middlewares: [raw({ type: "application/json" })],
+  },
+])
