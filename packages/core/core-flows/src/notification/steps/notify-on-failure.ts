29fd4ffabcfcdb879e8665b7fae5b0de0fd15727
Stevche Radevski <sradevski@live.com>
2024-07-29 21:05:34 +0300

    fix: Added more tests and fixed a couple of issues with product import

---
+++ packages/core/core-flows/src/notification/steps/notify-on-failure.ts
@@ -0,0 +1,36 @@
+import { INotificationModuleService } from "@medusajs/types"
+import { ModuleRegistrationName } from "@medusajs/utils"
+import { StepResponse, createStep } from "@medusajs/workflows-sdk"
+
+type NotifyOnFailureStepInput = {
+  to: string
+  channel: string
+  template: string
+  data?: Record<string, unknown> | null
+  trigger_type?: string | null
+  resource_id?: string | null
+  resource_type?: string | null
+  receiver_id?: string | null
+  original_notification_id?: string | null
+  idempotency_key?: string | null
+}[]
+
+export const notifyOnFailureStepId = "notify-on-failure"
+export const notifyOnFailureStep = createStep(
+  notifyOnFailureStepId,
+  async (data: NotifyOnFailureStepInput) => {
+    return new StepResponse(void 0, data)
+  },
+
+  async (data, { container }) => {
+    console.log("HERE!!!")
+    if (!data) {
+      return
+    }
+
+    const service = container.resolve<INotificationModuleService>(
+      ModuleRegistrationName.NOTIFICATION
+    )
+    await service.createNotifications(data)
+  }
+)
