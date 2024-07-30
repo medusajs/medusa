e36c6f76a1053c78bfb6ea23493637f96fe2c642
Shahed Nasser <shahednasser@gmail.com>
2024-07-30 17:40:37 +0300

    chore(core-flows,modules-sdk): add TSDocs to helper steps

--- packages/core/core-flows/src/common/steps/use-remote-query.ts
+++ packages/core/core-flows/src/common/steps/use-remote-query.ts
@@ -4,23 +4,141 @@ import {
 } from "@medusajs/utils"
 import { createStep, StepResponse } from "@medusajs/workflows-sdk"
 
-interface StepInput {
+/**
+ * The remote query's details.
+ */
+export interface RemoteStepInput {
+  /**
+   * The fields to retrieve in the records.
+   */
   fields: string[]
+  /**
+   * Filters, context variables, or pagination fields to apply when retrieving the records.
+   */
   variables?: Record<string, any>
+  /**
+   * Throw an error if a record isn't found matching an ID specified in the filters.
+   */
   throw_if_key_not_found?: boolean
+  /**
+   * Throw an error if a specified relation isn't found.
+   */
   throw_if_relation_not_found?: boolean | string[]
+  /**
+   * Whether to retrieve the records as an array. If disabled, only one record is retrieved as an object.
+   * 
+   * @defaultValue true
+   */
   list?: boolean
 }
 
-interface EntryStepInput extends StepInput {
+export interface EntryStepInput extends RemoteStepInput {
+  /**
+   * The name of the data model to retrieve its records.
+   */
   entry_point: string
 }
 
-interface ServiceStepInput extends StepInput {
+export interface ServiceStepInput extends RemoteStepInput {
+  /**
+   * The name of the module's service.
+   */
   service: string
 }
 
 export const useRemoteQueryStepId = "use-remote-query"
+/**
+ * This step fetches data across modules using the remote query.
+ * 
+ * Learn more in the [Remote Query documentation](https://docs.medusajs.com/v2/advanced-development/modules/remote-query).
+ * 
+ * @example
+ * 
+ * To retrieve a list of records of a data model:
+ * 
+ * ```ts
+ * import { 
+ *   createWorkflow
+ * } from "@medusajs/workflows-sdk"
+ * import {
+ *   useRemoteQueryStep
+ * } from "@medusajs/core-flows"
+ * 
+ * const helloWorldWorkflow = createWorkflow(
+ *   "hello-world",
+ *   () => {
+ *     const products = useRemoteQueryStep({
+ *       entry_point: "product",
+ *       fields: [
+ *         "*",
+ *         "variants.*"
+ *       ]
+ *     })
+ *   }
+ * )
+ * ```
+ * 
+ * To retrieve a single item instead of a an array:
+ * 
+ * ```ts
+ * import { 
+ *   createWorkflow
+ * } from "@medusajs/workflows-sdk"
+ * import {
+ *   useRemoteQueryStep
+ * } from "@medusajs/core-flows"
+ * 
+ * const helloWorldWorkflow = createWorkflow(
+ *   "hello-world",
+ *   () => {
+ *     const product = useRemoteQueryStep({
+ *       entry_point: "product",
+ *       fields: [
+ *         "*",
+ *         "variants.*"
+ *       ],
+ *       variables: {
+ *         filters: {
+ *           id: "123"
+ *         }
+ *       },
+ *       list: false
+ *     })
+ *   }
+ * )
+ * ```
+ * 
+ * To throw an error if a record isn't found matching the specified ID:
+ * 
+ * ```ts
+ * import { 
+ *   createWorkflow
+ * } from "@medusajs/workflows-sdk"
+ * import {
+ *   useRemoteQueryStep
+ * } from "@medusajs/core-flows"
+ * 
+ * const helloWorldWorkflow = createWorkflow(
+ *   "hello-world",
+ *   () => {
+ *     const product = useRemoteQueryStep({
+ *       entry_point: "product",
+ *       fields: [
+ *         "*",
+ *         "variants.*"
+ *       ],
+ *       variables: {
+ *         filters: {
+ *           id: "123"
+ *         }
+ *       },
+ *       list: false,
+ *       throw_if_key_not_found: true
+ *     })
+ *   }
+ * )
+ * ```
+ */
 export const useRemoteQueryStep = createStep(
   useRemoteQueryStepId,
   async (data: EntryStepInput | ServiceStepInput, { container }) => {
