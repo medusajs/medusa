8f0dd0d344df5c652b9c47b5151e7e3ed47c2160
Adil <abasri@webexpr.dev>
2024-07-30 23:03:58 +0200

    fix(admin-ui): Fixed DropdownMenu inside a Modal zIndex issues
    - This is a common issue with RadixUI the solution is to use a Portal to make sure the DropdownMenu is rendered on the `document.body` instead of the same container
    - Updated the zIndex from `z-30` to `z-50`

--- packages/admin-ui/ui/src/components/molecules/actionables.tsx
+++ packages/admin-ui/ui/src/components/molecules/actionables.tsx
@@ -41,32 +41,34 @@ const Actionables: React.FC<ActionablesProps> = ({
             )}
           </DropdownMenu.Trigger>
 
-          <DropdownMenu.Content
-            sideOffset={5}
-            className="bg-grey-0 border-grey-20 rounded-rounded shadow-dropdown p-xsmall z-30 min-w-[200px] border"
-          >
-            {actions.map((action, i) => {
-              return (
-                <DropdownMenu.Item className="mb-1 last:mb-0" key={i}>
-                  {
-                    <Button
-                      variant="ghost"
-                      size="small"
-                      className={clsx("flex w-full justify-start", {
-                        "text-rose-50": action?.variant === "danger",
-                        "pointer-events-none select-none opacity-50":
-                          action?.disabled,
-                      })}
-                      onClick={action?.onClick}
-                    >
-                      {action.icon}
-                      {action.label}
-                    </Button>
-                  }
-                </DropdownMenu.Item>
-              )
-            })}
-          </DropdownMenu.Content>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              sideOffset={5}
+              className="bg-grey-0 border-grey-20 rounded-rounded shadow-dropdown p-xsmall z-50 min-w-[200px] border"
+            >
+              {actions.map((action, i) => {
+                return (
+                  <DropdownMenu.Item className="mb-1 last:mb-0" key={i}>
+                    {
+                      <Button
+                        variant="ghost"
+                        size="small"
+                        className={clsx("flex w-full justify-start", {
+                          "text-rose-50": action?.variant === "danger",
+                          "pointer-events-none select-none opacity-50":
+                            action?.disabled,
+                        })}
+                        onClick={action?.onClick}
+                      >
+                        {action.icon}
+                        {action.label}
+                      </Button>
+                    }
+                  </DropdownMenu.Item>
+                )
+              })}
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
         </DropdownMenu.Root>
       </div>
     )
