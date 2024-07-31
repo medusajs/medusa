4a1c18c2e28b3a37b6c74bc9c00cd9be5d572093
kodiakhq <kodiakhq@users.noreply.github.com>
2024-07-31 00:14:41 +0000

    chore(docs): Generated API Reference (v2)

--- www/utils/generated/oas-output/operations/admin/post_admin_claims_[id]_cancel.ts
+++ www/utils/generated/oas-output/operations/admin/post_admin_claims_[id]_cancel.ts
@@ -1,8 +1,8 @@
 /**
- * @oas [post] /admin/claims/{id}/request
- * operationId: PostClaimsIdRequest
- * summary: Add Requests to Claim
- * description: Add a list of requests to a claim.
+ * @oas [post] /admin/claims/{id}/cancel
+ * operationId: PostClaimsIdCancel
+ * summary: Add Cancels to Claim
+ * description: Add a list of cancels to a claim.
  * x-authenticated: true
  * parameters:
  *   - name: id
@@ -21,18 +21,18 @@
  *       description: Comma-separated relations that should be expanded in the returned data.
  *   - name: fields
  *     in: query
- *     description: Comma-separated fields that should be included in the returned
- *       data. if a field is prefixed with `+` it will be added to the default
- *       fields, using `-` will remove it from the default fields. without prefix
- *       it will replace the entire default fields.
+ *     description: >-
+ *       Comma-separated fields that should be included in the returned data.
+ *        * if a field is prefixed with `+` it will be added to the default fields, using `-` will remove it from the default fields.
+ *        * without prefix it will replace the entire default fields.
  *     required: false
  *     schema:
  *       type: string
  *       title: fields
- *       description: Comma-separated fields that should be included in the returned
- *         data. if a field is prefixed with `+` it will be added to the default
- *         fields, using `-` will remove it from the default fields. without prefix
- *         it will replace the entire default fields.
+ *       description: >-
+ *         Comma-separated fields that should be included in the returned data.
+ *          * if a field is prefixed with `+` it will be added to the default fields, using `-` will remove it from the default fields.
+ *          * without prefix it will replace the entire default fields.
  *   - name: offset
  *     in: query
  *     description: The number of items to skip when retrieving a list.
@@ -65,15 +65,22 @@
  *   - api_token: []
  *   - cookie_auth: []
  *   - jwt_token: []
+ * requestBody:
+ *   content:
+ *     application/json:
+ *       schema:
+ *         $ref: "#/components/schemas/AdminPostCancelClaimReqSchema"
  * x-codeSamples:
  *   - lang: Shell
  *     label: cURL
  *     source: |-
- *       curl -X POST '{backend_url}/admin/claims/{id}/request' \
+ *       curl -X POST '{backend_url}/admin/claims/{id}/cancel' \
  *       -H 'x-medusa-access-token: {api_token}'
  * tags:
  *   - Claims
  * responses:
+ *   "200":
+ *     description: OK
  *   "400":
  *     $ref: "#/components/responses/400_error"
  *   "401":
@@ -86,10 +93,6 @@
  *     $ref: "#/components/responses/invalid_request_error"
  *   "500":
  *     $ref: "#/components/responses/500_error"
- * requestBody:
- *   content:
- *     application/json:
- *       schema: {}
  * 
 */
 
