5064e25ae62684c1b5f16db41801ee2d3d136aa4
Harminder Virk <virk.officials@gmail.com>
2024-08-02 17:42:09 +0530

    test: fix failing tests

--- packages/medusa/src/utils/__tests__/define-routes-config.spec.ts
+++ packages/medusa/src/utils/__tests__/define-routes-config.spec.ts
@@ -27,9 +27,9 @@ describe("defineMiddlewares", function () {
     } as MedusaRequest
     const res = {} as MedusaResponse
     const nextFn = jest.fn()
-    const schema = zod.object({
+    const schema = {
       brand_id: zod.string(),
-    })
+    }
 
     const config = defineMiddlewares([
       {
@@ -48,6 +48,10 @@ describe("defineMiddlewares", function () {
     })
 
     config.routes?.[0].middlewares?.[0](req, res, nextFn)
-    expect(req.additionalDataValidator).toMatchObject(schema)
+    expect(req.additionalDataValidator!.parse({ brand_id: "1" })).toMatchObject(
+      {
+        brand_id: "1",
+      }
+    )
   })
 })
