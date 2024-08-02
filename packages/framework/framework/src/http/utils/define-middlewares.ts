8feef414ce0e4e3fa071a438759a586e5710ff01
Harminder Virk <virk.officials@gmail.com>
2024-08-02 14:24:33 +0530

    refactor: remove support of extendedValidators in query params

--- packages/framework/framework/src/http/utils/define-middlewares.ts
+++ packages/framework/framework/src/http/utils/define-middlewares.ts
@@ -19,10 +19,7 @@ export function defineMiddlewares<
     method?: MiddlewareVerb | MiddlewareVerb[]
     matcher: string | RegExp
     bodyParser?: ParserConfig
-    extendedValidators?: {
-      body?: ZodObject<any, any>
-      queryParams?: ZodObject<any, any>
-    }
+    additionalDataValidator?: ZodObject<any, any>
     // eslint-disable-next-line space-before-function-paren
     middlewares?: (<Req extends MedusaRequest>(
       req: Req,
@@ -41,17 +38,16 @@ export function defineMiddlewares<
   return {
     errorHandler,
     routes: routes.map((route) => {
-      const { middlewares, extendedValidators, ...rest } = route
+      const { middlewares, additionalDataValidator, ...rest } = route
       const customMiddleware: MedusaRequestHandler[] = []
 
       /**
-       * Define a custom validator when "extendedValidators.body" or
-       * "extendedValidators.queryParams" validation schema is
-       * provided.
+       * Define a custom validator when a zod schema is provided via
+       * "additionalDataValidator" property
        */
-      if (extendedValidators?.body || extendedValidators?.queryParams) {
+      if (additionalDataValidator) {
         customMiddleware.push((req, _, next) => {
-          req.extendedValidators = extendedValidators
+          req.additionalDataValidator = additionalDataValidator
           next()
         })
       }
