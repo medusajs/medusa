1bde67d5e2feadc6de27a96af74a49b89e88ebeb
Shahed Nasser <shahednasser@gmail.com>
2024-08-05 16:31:55 +0300

    add case for non objects

--- www/apps/api-reference/utils/merge-all-of-types.ts
+++ www/apps/api-reference/utils/merge-all-of-types.ts
@@ -9,6 +9,7 @@ export default function mergeAllOfTypes(
   }
   // merge objects' properties in this var
   let properties: PropertiesObject = {}
+  let foundObjects = false
 
   allOfSchema.allOf.forEach((item) => {
     if (item.type !== "object") {
@@ -16,7 +17,14 @@ export default function mergeAllOfTypes(
     }
 
     properties = Object.assign(properties, item.properties)
+    foundObjects = true
   })
+
+  if (!foundObjects && allOfSchema.allOf.length) {
+    // return the first item in the union.
+    // allOf should always include an object but just in case
+    return allOfSchema.allOf[0]
+  }
 
   return {
     type: "object",
