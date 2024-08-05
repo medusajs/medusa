06a7d0145894abed0e8461c35cc461f8a2e481ab
Harminder Virk <virk.officials@gmail.com>
2024-08-05 17:29:11 +0530

    feat: add hooks to campaigns

--- packages/core/core-flows/src/promotion/workflows/update-campaigns.ts
+++ packages/core/core-flows/src/promotion/workflows/update-campaigns.ts
@@ -1,17 +1,26 @@
-import { CampaignDTO, UpdateCampaignDTO } from "@medusajs/types"
+import { AdditionalData, CampaignDTO, UpdateCampaignDTO } from "@medusajs/types"
 import {
   WorkflowData,
   WorkflowResponse,
+  createHook,
   createWorkflow,
 } from "@medusajs/workflows-sdk"
 import { updateCampaignsStep } from "../steps"
 
-type WorkflowInput = { campaignsData: UpdateCampaignDTO[] }
+type WorkflowInput = { campaignsData: UpdateCampaignDTO[] } & AdditionalData
 
 export const updateCampaignsWorkflowId = "update-campaigns"
 export const updateCampaignsWorkflow = createWorkflow(
   updateCampaignsWorkflowId,
-  (input: WorkflowData<WorkflowInput>): WorkflowResponse<CampaignDTO[]> => {
-    return new WorkflowResponse(updateCampaignsStep(input.campaignsData))
+  (input: WorkflowData<WorkflowInput>) => {
+    const updatedCampaigns = updateCampaignsStep(input.campaignsData)
+    const campaignsUpdated = createHook("campaignsUpdated", {
+      campaigns: updatedCampaigns,
+      additional_data: input.additional_data,
+    })
+
+    return new WorkflowResponse(updatedCampaigns, {
+      hooks: [campaignsUpdated],
+    })
   }
 )
