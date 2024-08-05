0dab340013adf4284050e6b9b85e5224e39852b1
Harminder Virk <virk.officials@gmail.com>
2024-08-05 15:16:32 +0530

    feature: add hooks to customers modules workflows (#8442)

--- packages/core/core-flows/src/customer/workflows/create-customers.ts
+++ packages/core/core-flows/src/customer/workflows/create-customers.ts
@@ -1,17 +1,27 @@
-import { CustomerDTO, CreateCustomerDTO } from "@medusajs/types"
+import { CreateCustomerDTO, AdditionalData } from "@medusajs/types"
 import {
   WorkflowData,
   WorkflowResponse,
+  createHook,
   createWorkflow,
 } from "@medusajs/workflows-sdk"
 import { createCustomersStep } from "../steps"
 
-type WorkflowInput = { customersData: CreateCustomerDTO[] }
+type WorkflowInput = { customersData: CreateCustomerDTO[] } & AdditionalData
 
 export const createCustomersWorkflowId = "create-customers"
+
 export const createCustomersWorkflow = createWorkflow(
   createCustomersWorkflowId,
-  (input: WorkflowData<WorkflowInput>): WorkflowResponse<CustomerDTO[]> => {
-    return new WorkflowResponse(createCustomersStep(input.customersData))
+  (input: WorkflowData<WorkflowInput>) => {
+    const createdCustomers = createCustomersStep(input.customersData)
+    const customersCreated = createHook("customersCreated", {
+      customers: createdCustomers,
+      additional_data: input.additional_data,
+    })
+
+    return new WorkflowResponse(createdCustomers, {
+      hooks: [customersCreated],
+    })
   }
 )
