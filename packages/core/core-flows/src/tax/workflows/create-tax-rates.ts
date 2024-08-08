ad8a0400c5edc19f9bbb6a0caf82d103c7bca100
Shahed Nasser <shahednasser@gmail.com>
2024-08-08 19:36:09 +0300

    chore(core-flows): [18/18] export types and types, add basic TSDocs

--- packages/core/core-flows/src/tax/workflows/create-tax-rates.ts
+++ packages/core/core-flows/src/tax/workflows/create-tax-rates.ts
@@ -6,12 +6,13 @@ import {
 } from "@medusajs/workflows-sdk"
 import { createTaxRatesStep } from "../steps"
 
-type WorkflowInput = CreateTaxRateDTO[]
-
 export const createTaxRatesWorkflowId = "create-tax-rates"
+/**
+ * This workflow creates one or more tax rates.
+ */
 export const createTaxRatesWorkflow = createWorkflow(
   createTaxRatesWorkflowId,
-  (input: WorkflowData<WorkflowInput>): WorkflowResponse<TaxRateDTO[]> => {
+  (input: WorkflowData<CreateTaxRateDTO[]>): WorkflowResponse<TaxRateDTO[]> => {
     return new WorkflowResponse(createTaxRatesStep(input))
   }
 )
