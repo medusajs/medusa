85ed02570575a40ac796e66f5cd734d65da1d4c8
Riqwan Thamir <rmthamir@gmail.com>
2024-08-08 14:20:13 +0200

    feat(dashboard,js-sdk,medusa): add ability to add outbound items to claim (#8502)
    what:
    
    - user can add/remove/update outbound items to a claim
    - adds API to query variants
    
    Note: There are several paths that are not implemented yet correctly, but have some code lying around. Those will be tackled in the followup PRs
    
    https://github.com/user-attachments/assets/cadb3f7a-982f-44c7-8d7e-9f4f26949f4f
    
    RESOLVES CC-330
    RESOLVES CC-296

---
+++ packages/admin-next/dashboard/src/routes/orders/order-create-claim/components/add-claim-outbound-items-table/index.ts
@@ -0,0 +1,1 @@
+export * from "./add-claim-outbound-items-table"
--- packages/admin-next/dashboard/src/routes/orders/order-create-claim/components/claim-create-form/schema.ts
+++ packages/admin-next/dashboard/src/routes/orders/order-create-claim/components/claim-create-form/schema.ts
@@ -9,16 +9,15 @@ export const ClaimCreateSchema = z.object({
       note: z.string().nullish(),
     })
   ),
-  // TODO: Bring back when introducing outbound items
-  // outbound_items: z.array(
-  //   z.object({
-  //     item_id: z.string(), // TODO: variant id?
-  //     quantity: z.number(),
-  //   })
-  // ),
+  outbound_items: z.array(
+    z.object({
+      item_id: z.string(), // TODO: variant id?
+      quantity: z.number(),
+    })
+  ),
   location_id: z.string().optional(),
   inbound_option_id: z.string().nullish(),
   send_notification: z.boolean().optional(),
 })
 
-export type ReturnCreateSchemaType = z.infer<typeof ClaimCreateSchema>
+export type CreateClaimSchemaType = z.infer<typeof ClaimCreateSchema>
