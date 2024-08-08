a85e2746e92c052454bde2f7f8b28a148cde26f4
Shahed Nasser <shahednasser@gmail.com>
2024-08-08 14:19:43 +0300

    docs-util: added a namespaces plugin

--- www/utils/packages/utils/src/workflow-utils.ts
+++ www/utils/packages/utils/src/workflow-utils.ts
@@ -2,7 +2,7 @@ import { SignatureReflection } from "typedoc"
 
 export function isWorkflow(reflection: SignatureReflection): boolean {
   return (
-    reflection.parent.children?.some((child) => child.name === "runAsStep") ||
+    reflection.parent?.children?.some((child) => child.name === "runAsStep") ||
     false
   )
 }
