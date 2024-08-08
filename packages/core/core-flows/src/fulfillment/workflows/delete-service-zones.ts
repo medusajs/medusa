4927a2cbb32f46ee5e491d37cc66a9266cbd87e7
Shahed Nasser <shahednasser@gmail.com>
2024-08-08 16:55:25 +0300

    chore(core-flows): [4] export types and types, add basic TSDocs

--- packages/core/core-flows/src/fulfillment/workflows/delete-service-zones.ts
+++ packages/core/core-flows/src/fulfillment/workflows/delete-service-zones.ts
@@ -2,6 +2,9 @@ import { WorkflowData, createWorkflow } from "@medusajs/workflows-sdk"
 import { deleteServiceZonesStep } from "../steps"
 
 export const deleteServiceZonesWorkflowId = "delete-service-zones-workflow"
+/**
+ * This workflow deletes one or more service zones.
+ */
 export const deleteServiceZonesWorkflow = createWorkflow(
   deleteServiceZonesWorkflowId,
   (input: WorkflowData<{ ids: string[] }>) => {
