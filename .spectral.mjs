ef092475b1752b78e530603f79db02026db7254e
Jesse Teo <jessezhang@apifox.com>
2024-08-09 10:33:44 +0800

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NTAwNA/spectral.js?branch_slug=develop&token=9487687a-4ce1-44fe-9c77-798eb6a1b128";
+export default { extends: ruleset };
