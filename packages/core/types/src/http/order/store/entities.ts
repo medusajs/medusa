8f4b495fcb3517e3af94e2ce1b3632187eeaee10
Shahed Nasser <shahednasser@gmail.com>
2024-08-12 14:26:17 +0300

    chore(medusa,types): [3] Add request types to API routes

--- packages/core/types/src/http/order/store/entities.ts
+++ packages/core/types/src/http/order/store/entities.ts
@@ -1,14 +1,11 @@
 import {
   BaseOrder,
   BaseOrderAddress,
-  BaseOrderFilters,
   BaseOrderLineItem,
   BaseOrderShippingMethod,
-} from "./common"
+} from "../common"
 
 export interface StoreOrder extends BaseOrder {}
 export interface StoreOrderLineItem extends BaseOrderLineItem {}
 export interface StoreOrderAddress extends BaseOrderAddress {}
 export interface StoreOrderShippingMethod extends BaseOrderShippingMethod {}
-
-export interface StoreOrderFilters extends BaseOrderFilters {}
