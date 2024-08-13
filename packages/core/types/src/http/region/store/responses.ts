078c2df773f32c942ce15b7d749777ebadf01d91
Shahed Nasser <shahednasser@gmail.com>
2024-08-13 09:37:11 +0300

    chore(medusa,types): [4] Add request types to API routes (#8561)

---
+++ packages/core/types/src/http/region/store/responses.ts
@@ -0,0 +1,8 @@
+import { PaginatedResponse } from "../../common";
+import { StoreRegion } from "./entities";
+
+export type StoreRegionResponse = {
+  region: StoreRegion
+}
+
+export type StoreRegionListResponse = PaginatedResponse<{ regions: StoreRegion[] }>
