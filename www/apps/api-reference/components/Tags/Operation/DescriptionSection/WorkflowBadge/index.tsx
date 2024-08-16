47eb19ca5dd7ab8afc5464f7b7ad56b2bb52b244
Shahed Nasser <shahednasser@gmail.com>
2024-08-16 09:26:04 +0300

    docs: add associated workflow badge in api reference (#8613)

---
+++ www/apps/api-reference/components/Tags/Operation/DescriptionSection/WorkflowBadge/index.tsx
@@ -0,0 +1,35 @@
+import { Badge, DecisionProcessIcon } from "docs-ui"
+import { config } from "../../../../../config"
+import Link from "next/link"
+
+export type TagsOperationDescriptionSectionWorkflowBadgeProps = {
+  workflow: string
+}
+
+const TagsOperationDescriptionSectionWorkflowBadge = ({
+  workflow,
+}: TagsOperationDescriptionSectionWorkflowBadgeProps) => {
+  return (
+    <p className="my-1">
+      Workflow{" "}
+      <Link
+        href={`${config.baseUrl}/v2/resources/references/medusa-workflows/${workflow}`}
+        className="align-middle"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Badge
+          variant="neutral"
+          className="inline-flex hover:bg-medusa-tag-neutral-bg-hover cursor-pointer"
+          childrenWrapperClassName="inline-flex flex-row gap-[3px] items-center"
+        >
+          <DecisionProcessIcon />
+          <span>{workflow}</span>
+        </Badge>
+      </Link>{" "}
+      is used in this API route.
+    </p>
+  )
+}
+
+export default TagsOperationDescriptionSectionWorkflowBadge
