adcd25650ccfb87481d44bd9ea28c4d845bd2b3a
Riqwan Thamir <rmthamir@gmail.com>
2024-08-16 10:16:40 +0200

    feat(medusa,core-flows,types): API to create payment collections for order (#8617)
    what:
    
    - adds an API to create a payment collection for order

---
+++ packages/medusa/src/api/admin/payment-collections/middlewares.ts
@@ -0,0 +1,22 @@
+import { MiddlewareRoute } from "@medusajs/framework"
+import { validateAndTransformBody } from "../../utils/validate-body"
+import { validateAndTransformQuery } from "../../utils/validate-query"
+import * as queryConfig from "./query-config"
+import {
+  AdminCreatePaymentCollection,
+  AdminGetPaymentCollectionParams,
+} from "./validators"
+
+export const adminPaymentCollectionsMiddlewares: MiddlewareRoute[] = [
+  {
+    method: ["POST"],
+    matcher: "/admin/payment-collections",
+    middlewares: [
+      validateAndTransformBody(AdminCreatePaymentCollection),
+      validateAndTransformQuery(
+        AdminGetPaymentCollectionParams,
+        queryConfig.retrievePaymentCollectionTransformQueryConfig
+      ),
+    ],
+  },
+]
