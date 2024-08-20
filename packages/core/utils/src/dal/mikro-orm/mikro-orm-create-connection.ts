11347fd89271f66b91693a55782e2689eedfa9db
Harminder Virk <virk.officials@gmail.com>
2024-08-20 13:50:41 +0530

    fix: maintain connections pool and prevent eager MikroORM.init queries

--- packages/core/utils/src/dal/mikro-orm/mikro-orm-create-connection.ts
+++ packages/core/utils/src/dal/mikro-orm/mikro-orm-create-connection.ts
@@ -70,6 +70,12 @@ export async function mikroOrmCreateConnection(
 
   const { MikroORM } = await import("@mikro-orm/postgresql")
   return await MikroORM.init({
+    /**
+     * "connect=false" will not query the database as soon as a
+     * new instance is created.
+     * https://github.com/mikro-orm/mikro-orm/blob/master/packages/core/src/MikroORM.ts#L57C25-L59
+     */
+    connect: false,
     discovery: { disableDynamicFileAccess: true, warnWhenNoEntities: false },
     entities,
     debug: database.debug ?? process.env.NODE_ENV?.startsWith("dev") ?? false,
@@ -93,6 +99,9 @@ export async function mikroOrmCreateConnection(
     schemaGenerator: {
       disableForeignKeys: false,
     },
-    pool: database.pool as any,
+    pool: {
+      min: 2,
+      ...database.pool,
+    },
   })
 }
