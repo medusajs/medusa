c45878509b2cc634aec04efc5fcc223c0e318c2b
fPolic <mainacc.polic@gmail.com>
2024-08-21 15:26:20 +0200

    wip: order edit requested panel

---
+++ packages/admin-next/dashboard/src/routes/orders/order-detail/components/order-active-edit-section/order-active-edit-section.tsx
@@ -0,0 +1,62 @@
+import { Button, Container, Heading } from "@medusajs/ui"
+import { useTranslation } from "react-i18next"
+import { useOrderPreview } from "../../../../../hooks/api"
+import { ExclamationCircleSolid } from "@medusajs/icons"
+
+type OrderActiveEditSectionProps = {
+  orderId: string
+}
+
+export const OrderActiveEditSection = ({
+  orderId,
+}: OrderActiveEditSectionProps) => {
+  const { t } = useTranslation()
+
+  const { order: orderPreview } = useOrderPreview(orderId)
+
+  console.log(orderPreview)
+
+  if (!orderPreview || orderPreview.order_change.change_type !== "edit") {
+    return null
+  }
+
+  return (
+    <div
+      style={{
+        background:
+          "repeating-linear-gradient(-45deg, rgb(212, 212, 216, 0.15), rgb(212, 212, 216,.15) 10px, transparent 10px, transparent 20px)",
+      }}
+      className="-m-4 mb-1 border-b p-4"
+    >
+      <Container className="flex items-center justify-between p-0">
+        <div className="flex w-full flex-col divide-y divide-dashed">
+          <div className="flex items-center gap-2 px-6 py-4">
+            <ExclamationCircleSolid className="text-blue-500" />
+            <Heading level="h2">{t("orders.edits.panel.title")}</Heading>
+          </div>
+
+          {/*ADDED ITEMS*/}
+          <div className="txt-small text-ui-fg-subtle flex flex-row px-6 py-4">
+            <span className="flex-1 font-medium">{t("labels.added")}</span>
+            <div className="flex-1">TODO</div>
+          </div>
+
+          {/*REMOVED ITEMS*/}
+          <div className="txt-small text-ui-fg-subtle flex flex-row px-6 py-4">
+            <span className="flex-1 font-medium">{t("labels.removed")}</span>
+            <div className="flex-1">TODO</div>
+          </div>
+
+          <div className="bg-ui-bg-subtle flex items-center justify-end gap-x-2 rounded-b-xl px-4 py-4">
+            <Button size="small" variant="secondary" onClick={() => {}}>
+              {t("actions.forceConfirm")}
+            </Button>
+            <Button size="small" variant="secondary" onClick={() => {}}>
+              {t("actions.cancel")}
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
+}
