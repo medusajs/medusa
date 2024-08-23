c63a08fb032de772855a923a38ea05abb9710bd6
Shahed Nasser <shahednasser@gmail.com>
2024-08-23 10:42:23 +0300

    docs: second round of polishing (#8724)
    * docs: second round of polishing
    
    * fix overflowing width

--- www/packages/docs-ui/src/layouts/tight.tsx
+++ www/packages/docs-ui/src/layouts/tight.tsx
@@ -17,7 +17,8 @@ export const TightLayout = ({
           "w-full h-fit",
           "max-w-inner-content-xs sm:max-w-inner-content-sm md:max-w-inner-content-md",
           "lg:max-w-inner-content-lg xl:max-w-inner-content-xl xxl:max-w-inner-content-xxl",
-          "xxxl:max-w-inner-content-xxxl"
+          "xxxl:max-w-inner-content-xxxl",
+          "px-docs_1 md:px-docs_4 lg:px-0"
         )}
       >
         {children}
