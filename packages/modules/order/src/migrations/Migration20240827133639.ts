da61372bc8e58624303f6d72ea48e7a30eb994d0
fPolic <mainacc.polic@gmail.com>
2024-08-28 08:58:16 +0200

    fix: order migration raw damaged quantity

--- packages/modules/order/src/migrations/Migration20240827133639.ts
+++ packages/modules/order/src/migrations/Migration20240827133639.ts
@@ -3,7 +3,7 @@ import { Migration } from "@mikro-orm/migrations"
 export class Migration20240827133639 extends Migration {
   async up(): Promise<void> {
     this.addSql(
-      'alter table if exists "return_item" add column if not exists "damaged_quantity" numeric not null default 0, add column if not exists "raw_damaged_quantity" jsonb not null;'
+      'alter table if exists "return_item" add column if not exists "damaged_quantity" numeric not null default 0, add column if not exists "raw_damaged_quantity" jsonb not null default \'{"value": "0", "precision": 20}\'::jsonb;'
     )
   }
 
