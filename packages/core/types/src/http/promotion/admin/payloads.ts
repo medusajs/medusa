9dcde30db236259e7f90265ac81f8d805185595f
Shahed Nasser <shahednasser@gmail.com>
2024-08-29 17:34:49 +0300

    fix optional

--- packages/core/types/src/http/promotion/admin/payloads.ts
+++ packages/core/types/src/http/promotion/admin/payloads.ts
@@ -2,30 +2,30 @@ import { AdminCreateCampaign } from "../../campaign"
 
 export type AdminCreatePromotionRule = {
   operator: "gte" | "lte" | "gt" | "lt" | "eq" | "ne" | "in"
-  description: string | null
+  description?: string | null
   attribute: string
   value: string | string[]
 }
 
 export type AdminCreateApplication = {
-  description: string | null
+  description?: string | null
   value: number
-  currency_code: string | null
-  max_quantity: number | null
+  currency_code?: string | null
+  max_quantity?: number | null
   type: "fixed" | "percentage"
   target_type: "order" | "shipping_methods" | "items"
   allocation?: "each" | "across"
   target_rules?: AdminCreatePromotionRule[]
   buy_rules?: AdminCreatePromotionRule[]
-  apply_to_quantity: number | null
-  buy_rules_min_quantity: number | null
+  apply_to_quantity?: number | null
+  buy_rules_min_quantity?: number | null
 }
 
 export type AdminCreatePromotion = {
   code: string
   is_automatic?: boolean
   type: "standard" | "buyget"
-  campaign_id: string | null
+  campaign_id?: string | null
   campaign?: AdminCreateCampaign
   application_method: AdminCreateApplication
   rules?: AdminCreatePromotionRule[]
