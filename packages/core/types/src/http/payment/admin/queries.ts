3bb050e15e00630cfc84a201f7e05ee46664fd62
Shahed Nasser <shahednasser@gmail.com>
2024-08-30 10:04:07 +0300

    resolve PR comments

--- packages/core/types/src/http/payment/admin/queries.ts
+++ packages/core/types/src/http/payment/admin/queries.ts
@@ -16,7 +16,7 @@ export interface AdminPaymentCollectionFilters
   extends BasePaymentCollectionFilters {}
 export interface AdminPaymentSessionFilters extends BasePaymentSessionFilters {}
 
-export interface AdminPaymentFilters extends BasePaymentFilters {
+export interface AdminPaymentFilters extends FindParams, BasePaymentFilters {
   q?: string
   payment_session_id?: string | string[]
   created_at?: OperatorMap<string>
@@ -24,10 +24,7 @@ export interface AdminPaymentFilters extends BasePaymentFilters {
   deleted_at?: OperatorMap<string>
 }
 
-export interface RefundFilters extends BaseFilterable<AdminRefund> {
-  id?: string | string[]
-}
-export interface RefundReasonFilters extends BaseFilterable<AdminRefundReason> {
+export interface RefundReasonFilters extends FindParams, BaseFilterable<AdminRefundReason> {
   id?: string | string[]
   q?: string
 }
