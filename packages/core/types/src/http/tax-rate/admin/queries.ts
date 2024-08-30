812aafaf3ea767447a9b96ec653ed8252731f3cf
Shahed Nasser <shahednasser@gmail.com>
2024-08-30 09:53:11 +0300

    remove string union

--- packages/core/types/src/http/tax-rate/admin/queries.ts
+++ packages/core/types/src/http/tax-rate/admin/queries.ts
@@ -10,7 +10,7 @@ export interface AdminTaxRateListParams extends
     shipping_profile_id?: string
     provider_id?: string
     shipping_option_type_id?: string
-    created_at?: string | OperatorMap<string>
-    updated_at?: string | OperatorMap<string>
-    deleted_at?: string | OperatorMap<string>
+    created_at?: OperatorMap<string>
+    updated_at?: OperatorMap<string>
+    deleted_at?: OperatorMap<string>
   }
