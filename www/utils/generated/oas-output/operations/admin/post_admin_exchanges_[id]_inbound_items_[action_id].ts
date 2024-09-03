1a63209290e8af825cc3ce194c9759f4628ab040
Shahed Nasser <shahednasser@gmail.com>
2024-09-03 09:45:11 +0300

    oas: [8/n] improve admin OAS (#8945)
    Improve the OAS of admin API routes [8/n]

--- www/utils/generated/oas-output/operations/admin/post_admin_exchanges_[id]_inbound_items_[action_id].ts
+++ www/utils/generated/oas-output/operations/admin/post_admin_exchanges_[id]_inbound_items_[action_id].ts
@@ -1,8 +1,12 @@
 /**
  * @oas [post] /admin/exchanges/{id}/inbound/items/{action_id}
  * operationId: PostExchangesIdInboundItemsAction_id
- * summary: Add Items to Exchange
- * description: Add a list of items to a exchange.
+ * summary: Update an Inbount Item of an Exchange
+ * x-sidebar-summary: Update Inbound Item
+ * description: |
+ *   Update an inbound (or return) item from an exchange using the `ID` of the item's `RETURN_ITEM` action.
+ * 
+ *   Every item has an `actions` property, whose value is an array of actions. You can check the action's name using its `action` property, and use the value of the `id` property.
  * x-authenticated: true
  * parameters:
  *   - name: id
@@ -13,7 +17,7 @@
  *       type: string
  *   - name: action_id
  *     in: path
- *     description: The exchange's action id.
+ *     description: The ID of the return item's `RETURN_ITEM` action.
  *     required: true
  *     schema:
  *       type: string
