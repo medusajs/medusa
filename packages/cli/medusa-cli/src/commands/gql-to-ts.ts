904fb2917882ca149e48e0245eac37c6a3e415e0
adrien2p <adrien.deperetti@gmail.com>
2024-09-03 13:33:29 +0200

    WIP

--- packages/cli/medusa-cli/src/commands/gql-to-ts.ts
+++ packages/cli/medusa-cli/src/commands/gql-to-ts.ts
@@ -4,6 +4,7 @@ import { codegen } from "@graphql-codegen/core"
 import { logger, MedusaAppLoader } from "@medusajs/framework"
 import { initializeContainer } from "@medusajs/medusa/dist/loaders"
 import { FileSystem } from "@medusajs/utils"
+import { MedusaModule } from "@medusajs/modules-sdk"
 
 export async function graphqlToTs() {
   try {
@@ -31,8 +32,47 @@ export async function graphqlToTs() {
 
     const fileSystem = new FileSystem(directory)
 
-    const output = await codegen(config)
-    await fileSystem.create("src/__generated/remote-query-type.ts", output)
+    let output = await codegen(config)
+
+    // build map entry point to there type to be merged and used by the remote query
+
+    const joinerConfigs = MedusaModule.getAllJoinerConfigs()
+    const entryPoints = joinerConfigs.flatMap((config) => {
+      const aliases = Array.isArray(config.alias)
+        ? config.alias
+        : config.alias
+        ? [config.alias]
+        : []
+
+      return aliases.flatMap((alias) => {
+        const names = Array.isArray(alias.name) ? alias.name : [alias.name]
+        const entity = alias.args?.["entity"]
+        return names.map((aliasItem) => {
+          return {
+            entryPoint: aliasItem,
+            entityType: entity
+              ? output.includes(`export type ${entity} `)
+                ? alias.args?.["entity"]
+                : "any"
+              : "any",
+          }
+        })
+      })
+    })
+
+    const remoteQueryEntryPoints = `
+declare module '@medusajs/modules-sdk' {
+  interface RemoteQueryEntryPoints {
+    ${entryPoints
+      .map((entry) => `${entry.entryPoint}: ${entry.entityType}`)
+      .join("\n")}
+  }
+}
+    `
+
+    output += remoteQueryEntryPoints
+
+    await fileSystem.create("src/__generated/remote-query-types.ts", output)
 
     const barrelExists = await fileSystem.exists("src/__generated/index.ts")
 
