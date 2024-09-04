f7319a89d423591865c3440f75f8a73062f5a037
adrien2p <adrien.deperetti@gmail.com>
2024-09-04 09:59:06 +0200

    cleanup CLI func

--- packages/cli/medusa-cli/src/commands/gql-to-ts.ts
+++ packages/cli/medusa-cli/src/commands/gql-to-ts.ts
@@ -5,6 +5,84 @@ import { logger, MedusaAppLoader } from "@medusajs/framework"
 import { initializeContainer } from "@medusajs/medusa/dist/loaders"
 import { FileSystem } from "@medusajs/utils"
 import { MedusaModule } from "@medusajs/modules-sdk"
+import { performance } from "node:perf_hooks"
+
+const outputPath = "src/.medusa"
+
+function buildEntryPointsTypeMap(
+  schema: string
+): { entryPoint: string; entityType: any }[] {
+  // build map entry point to there type to be merged and used by the remote query
+
+  const joinerConfigs = MedusaModule.getAllJoinerConfigs()
+  return joinerConfigs
+    .flatMap((config) => {
+      const aliases = Array.isArray(config.alias)
+        ? config.alias
+        : config.alias
+        ? [config.alias]
+        : []
+
+      return aliases.flatMap((alias) => {
+        const names = Array.isArray(alias.name) ? alias.name : [alias.name]
+        const entity = alias.args?.["entity"]
+        return names.map((aliasItem) => {
+          return {
+            entryPoint: aliasItem,
+            entityType: entity
+              ? schema.includes(`export type ${entity} `)
+                ? alias.args?.["entity"]
+                : "any"
+              : "any",
+          }
+        })
+      })
+    })
+    .filter(Boolean)
+}
+
+async function generateTypes({
+  directory,
+  config,
+}: {
+  directory: string
+  config: Parameters<typeof codegen>[0]
+}) {
+  const fileSystem = new FileSystem(directory)
+
+  let output = await codegen(config)
+  const entryPoints = buildEntryPointsTypeMap(output)
+
+  const remoteQueryEntryPoints = `
+declare module '@medusajs/types' {
+  interface RemoteQueryEntryPoints {
+    ${entryPoints
+      .map((entry) => `${entry.entryPoint}: ${entry.entityType}`)
+      .join("\n")}
+  }
+}
+    `
+
+  output += remoteQueryEntryPoints
+
+  await fileSystem.create(
+    outputPath + "/generated/remote-query-types.ts",
+    output
+  )
+  await fileSystem.create(
+    outputPath + "/generated/remote-query-types.d.ts",
+    output
+  )
+
+  const barrelExists = await fileSystem.exists("src/generated/index.ts")
+
+  if (!barrelExists) {
+    await fileSystem.create(
+      outputPath + "index.ts",
+      "export * as RemoteQueryTypes from './generated/remote-query-types'"
+    )
+  }
+}
 
 export async function graphqlToTs() {
   try {
@@ -35,62 +113,22 @@ export async function graphqlToTs() {
       },
     }
 
-    const fileSystem = new FileSystem(directory)
+    performance.mark("[Medusa] Start generating types from GQL")
 
-    let output = await codegen(config)
+    await generateTypes({ directory, config })
 
-    // build map entry point to there type to be merged and used by the remote query
-
-    const joinerConfigs = MedusaModule.getAllJoinerConfigs()
-    const entryPoints = joinerConfigs.flatMap((config) => {
-      const aliases = Array.isArray(config.alias)
-        ? config.alias
-        : config.alias
-        ? [config.alias]
-        : []
-
-      return aliases.flatMap((alias) => {
-        const names = Array.isArray(alias.name) ? alias.name : [alias.name]
-        const entity = alias.args?.["entity"]
-        return names.map((aliasItem) => {
-          return {
-            entryPoint: aliasItem,
-            entityType: entity
-              ? output.includes(`export type ${entity} `)
-                ? alias.args?.["entity"]
-                : "any"
-              : "any",
-          }
-        })
-      })
-    })
-
-    const remoteQueryEntryPoints = `
-declare module '@medusajs/types' {
-  interface RemoteQueryEntryPoints {
-    ${entryPoints
-      .map((entry) => `${entry.entryPoint}: ${entry.entityType}`)
-      .join("\n")}
-  }
-}
-    `
-
-    output += remoteQueryEntryPoints
-
-    await fileSystem.create("src/__generated/remote-query-types.ts", output)
-
-    const barrelExists = await fileSystem.exists("src/__generated/index.ts")
-
-    if (!barrelExists) {
-      await fileSystem.create(
-        "src/__generated/index.ts",
-        "export * as RemoteQueryTypes from './remote-query-types'"
-      )
-    }
+    performance.mark("[Medusa] End generating types from GQL")
 
     process.exit()
   } catch (error) {
     logger.error(error)
     process.exit(1)
+  } finally {
+    const perf = performance.measure(
+      "[Medusa] Generating types from GQL",
+      "[Medusa] Start generating types from GQL",
+      "[Medusa] End generating types from GQL"
+    )
+    console.log(perf)
   }
 }
