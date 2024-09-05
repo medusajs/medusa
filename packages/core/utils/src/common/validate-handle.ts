e40b5f38af9fe965cababd62d18b2e54684f5516
Veak <51446230+v0eak@users.noreply.github.com>
2024-09-05 16:27:08 +0200

    fix(utils): add regex to isValidHandle allow encoded special character in front

--- packages/core/utils/src/common/validate-handle.ts
+++ packages/core/utils/src/common/validate-handle.ts
@@ -3,5 +3,5 @@
  * friendly.
  */
 export const isValidHandle = (value: string): boolean => {
-  return /^[a-z0-9]+(?:-[a-z0-9]+|(?:-%[0-9A-Fa-f]{2})*(?:%[0-9A-Fa-f]{2})*(?:-[a-z0-9])?)+(?:[a-z0-9])*$/.test(value)
+  return /^(?:%[0-9A-Fa-f]{2}|[a-z0-9]+)(?:-[a-z0-9]+|(?:-%[0-9A-Fa-f]{2})*(?:%[0-9A-Fa-f]{2})*(?:-[a-z0-9])?)+(?:[a-z0-9])*$/.test(value)
 }
