75dca7b6cf49d41efd037f21612c267886be5895
fPolic <mainacc.polic@gmail.com>
2024-09-13 18:01:23 +0200

    feat: inventory kit tab

--- packages/admin/dashboard/src/routes/products/product-create-variant/components/create-product-variant-form/constants.ts
+++ packages/admin/dashboard/src/routes/products/product-create-variant/components/create-product-variant-form/constants.ts
@@ -1,4 +1,6 @@
 import { z } from "zod"
+import * as zod from "zod"
+import { optionalInt } from "../../../../../lib/validation.ts"
 
 export const CreateProductVariantSchema = z.object({
   title: z.string().min(1),
@@ -7,4 +9,15 @@ export const CreateProductVariantSchema = z.object({
   allow_backorder: z.boolean(),
   inventory_kit: z.boolean(),
   options: z.record(z.string()),
+  prices: zod
+    .record(zod.string(), zod.string().or(zod.number()).optional())
+    .optional(),
+  inventory: z
+    .array(
+      z.object({
+        inventory_item_id: z.string(),
+        required_quantity: optionalInt,
+      })
+    )
+    .optional(),
 })
