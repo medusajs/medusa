3e97a64b21ca8b6fa8eff3eb85f4232e02b6b059
Riqwan Thamir <rmthamir@gmail.com>
2024-09-16 11:59:01 +0200

    feat(core-flows,medusa,utils,types): adds delivered_quantity to order (#9130)
    what:
    
    - adds delivered_quantity to order
    
    
    https://github.com/user-attachments/assets/709b1727-08ed-4a88-ae29-38f13540e301

---
+++ packages/core/core-flows/src/order/steps/register-delivery.ts
@@ -0,0 +1,31 @@
+import { IOrderModuleService, RegisterOrderDeliveryDTO } from "@medusajs/types"
+import { ModuleRegistrationName } from "@medusajs/utils"
+import { StepResponse, createStep } from "@medusajs/workflows-sdk"
+
+export const registerOrderDeliveryStepId = "register-order-delivery"
+/**
+ * This step registers a delivery for an order fulfillment.
+ */
+export const registerOrderDeliveryStep = createStep(
+  registerOrderDeliveryStepId,
+  async (data: RegisterOrderDeliveryDTO, { container }) => {
+    const service = container.resolve<IOrderModuleService>(
+      ModuleRegistrationName.ORDER
+    )
+
+    await service.registerDelivery(data)
+
+    return new StepResponse(void 0, data.order_id)
+  },
+  async (orderId, { container }) => {
+    if (!orderId) {
+      return
+    }
+
+    const service = container.resolve<IOrderModuleService>(
+      ModuleRegistrationName.ORDER
+    )
+
+    await service.revertLastVersion(orderId)
+  }
+)
