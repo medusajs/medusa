3649c081582b4c06a5496ff4bbe384294efb3a5e
adrien2p <adrien.deperetti@gmail.com>
2024-09-17 17:54:26 +0200

    simplify integration

--- integration-tests/modules/__tests__/modules/remote-query.spec.ts
+++ integration-tests/modules/__tests__/modules/remote-query.spec.ts
@@ -1,10 +1,4 @@
-import {
-  ICustomerModuleService,
-  IPricingModuleService,
-  IProductModuleService,
-  IRegionModuleService,
-  RemoteQueryFunction,
-} from "@medusajs/types"
+import { IRegionModuleService, RemoteQueryFunction } from "@medusajs/types"
 import { ContainerRegistrationKeys, Modules } from "@medusajs/utils"
 import { medusaIntegrationTestRunner } from "medusa-test-utils"
 import { createAdminUser } from "../../..//helpers/create-admin-user"
@@ -206,83 +200,13 @@ medusaIntegrationTestRunner({
       let appContainer
       let query: RemoteQueryFunction
 
-      let pricingModule: IPricingModuleService
-      let productModule: IProductModuleService
-      let customerModule: ICustomerModuleService
-      let regionModule: IRegionModuleService
-
-      let product
-      let variant
-      let variant2
-      let region
-      let customerGroup
-
       beforeAll(() => {
         appContainer = getContainer()
         query = appContainer.resolve(ContainerRegistrationKeys.QUERY)
-
-        pricingModule = appContainer.resolve(Modules.PRICING)
-        productModule = appContainer.resolve(Modules.PRODUCT)
-        customerModule = appContainer.resolve(Modules.CUSTOMER)
-        regionModule = appContainer.resolve(Modules.REGION)
       })
 
       beforeEach(async () => {
         await createAdminUser(dbConnection, adminHeaders, appContainer)
-
-        /* customerGroup = await customerModule.createCustomerGroups({
-          name: "VIP",
-        })
-        region = await regionModule.createRegions({
-          name: "US",
-          currency_code: "USD",
-        })
-        ;[product] = await productModule.createProducts([
-          {
-            title: "test product",
-            variants: [
-              {
-                title: "test product variant",
-              },
-              {
-                title: "test product variant 2",
-              },
-            ],
-          },
-        ])
-
-        variant = product.variants[0]
-        variant2 = product.variants[1]
-
-        const priceSet = await createVariantPriceSet({
-          container: appContainer,
-          variantId: variant.id,
-          prices: [],
-        })
-
-        await pricingModule.createPriceLists([
-          {
-            title: "test price list",
-            description: "test",
-            ends_at: new Date().toDateString(),
-            starts_at: new Date().toDateString(),
-            status: PriceListStatus.ACTIVE,
-            type: PriceListType.OVERRIDE,
-            prices: [
-              {
-                amount: 5000,
-                currency_code: "usd",
-                price_set_id: priceSet.id,
-                rules: {
-                  region_id: region.id,
-                },
-              },
-            ],
-            rules: {
-              customer_group_id: [customerGroup.id],
-            },
-          },
-        ])*/
 
         const payload = {
           title: "Test Giftcard",
