7f74462f564437c3fec4d16bea933c29ad9fb6bd
adrien2p <adrien.deperetti@gmail.com>
2024-09-18 17:56:47 +0200

    add playground tests

--- integration-tests/modules/__tests__/index/search.spec.ts
+++ integration-tests/modules/__tests__/index/search.spec.ts
@@ -5,8 +5,9 @@ import {
   adminHeaders,
   createAdminUser,
 } from "../../../helpers/create-admin-user"
+import { setTimeout } from "timers/promises"
 
-jest.setTimeout(10000)
+jest.setTimeout(120000)
 
 process.env.ENABLE_INDEX_MODULE = "true"
 
@@ -141,6 +142,63 @@ medusaIntegrationTestRunner({
           expect(variant.prices[0].amount).toBeGreaterThan(50)
           expect(variant.prices[0].currency_code).toBe("AUD")
         }
+      })
+
+      it.skip("should search through the indexed data and return the correct results ordered and filtered [3]", async () => {
+        const payloads = new Array(50).fill(0).map((_, a) => ({
+          title: "Test Giftcard-" + a,
+          is_giftcard: true,
+          description: "test-giftcard-description" + a,
+          options: [{ title: "Denominations", values: ["100"] }],
+          variants: new Array(10).fill(0).map((_, i) => ({
+            title: `Test variant ${i}`,
+            sku: `test-variant-${i}${a}`,
+            prices: new Array(10).fill(0).map((_, j) => ({
+              currency_code: Object.values(defaultCurrencies)[j].code,
+              amount: 10 * j,
+            })),
+            options: {
+              Denominations: "100",
+            },
+          })),
+        }))
+
+        let i = 0
+        for (const payload of payloads) {
+          ++i
+          await api.post("/admin/products", payload, adminHeaders).then(() => {
+            console.log(`Created ${i} products in ${payloads.length} payloads`)
+          })
+        }
+
+        await setTimeout(5000)
+
+        const queryArgs = [
+          {
+            select: {
+              product: {
+                variants: {
+                  prices: true,
+                },
+              },
+            },
+            where: {
+              "product.variants.prices.amount": { $gt: 50 },
+              "product.variants.prices.currency_code": { $eq: "AUD" },
+            },
+          },
+          {
+            orderBy: [{ "product.variants.prices.amount": "DESC" }],
+          },
+        ]
+
+        await indexEngine.queryAndCount(...queryArgs)
+
+        const [results, count, perf] = await indexEngine.queryAndCount(
+          ...queryArgs
+        )
+
+        console.log(perf)
       })
     })
   },
