722c25c4c817c8e4f19528e3da55d467bcf7d838
adrien2p <adrien.deperetti@gmail.com>
2024-09-19 14:07:16 +0200

    Add typings support over the API

--- packages/core/types/src/index/service.ts
+++ packages/core/types/src/index/service.ts
@@ -5,7 +5,7 @@ export interface IIndexService extends IModuleService {
   query<const TEntry extends string>(
     config: IndexQueryConfig<TEntry>
   ): Promise<any[]>
-  queryAndCount<const TEntry extends string>(
+  queryAndCount<const TEntry extends string, Test = IndexQueryConfig<TEntry>>(
     config: IndexQueryConfig<TEntry>
   ): Promise<[any[], number, PerformanceMeasure]>
 }
