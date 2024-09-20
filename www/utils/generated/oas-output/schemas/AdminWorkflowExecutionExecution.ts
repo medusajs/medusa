c5aafa7867159484078f007d62809f21f7ea8952
Shahed Nasser <shahednasser@gmail.com>
2024-09-20 16:15:38 +0300

    chore(oas): support additional props, fix circular references patch, and other fixes

--- www/utils/generated/oas-output/schemas/AdminWorkflowExecutionExecution.ts
+++ www/utils/generated/oas-output/schemas/AdminWorkflowExecutionExecution.ts
@@ -1,7 +1,7 @@
 /**
  * @schema AdminWorkflowExecutionExecution
  * type: object
- * description: The workflow execution's execution.
+ * description: The workflow execution's steps details.
  * x-schemaName: AdminWorkflowExecutionExecution
  * required:
  *   - steps
@@ -9,6 +9,40 @@
  *   steps:
  *     type: object
  *     description: The execution's steps.
+ *     required:
+ *       - id
+ *       - invoke
+ *       - definition
+ *       - compensate
+ *       - depth
+ *       - startedAt
+ *     additionalProperties:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           title: id
+ *           description: The step's ID.
+ *         invoke:
+ *           type: object
+ *           description: The step's invoke.
+ *           x-schemaName: WorkflowExecutionFn
+ *         definition:
+ *           type: object
+ *           description: The step's definition.
+ *           x-schemaName: WorkflowExecutionDefinition
+ *         compensate:
+ *           type: object
+ *           description: The step's compensate.
+ *           x-schemaName: WorkflowExecutionFn
+ *         depth:
+ *           type: number
+ *           title: depth
+ *           description: The step's depth.
+ *         startedAt:
+ *           type: number
+ *           title: startedAt
+ *           description: The step's startedat.
  * 
 */
 
