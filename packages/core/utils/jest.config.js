69f7750ea7ab7c2c29f2a591760f1becc6dfa56b
Harminder Virk <virk.officials@gmail.com>
2024-09-23 13:39:25 +0530

    tests: jest use tsconfig.json for running tests

--- packages/core/utils/jest.config.js
+++ packages/core/utils/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "^.+\\.[jt]s?$": [
       "ts-jest",
       {
-        tsconfig: "tsconfig.spec.json",
+        tsconfig: "tsconfig.json",
         isolatedModules: true,
       },
     ],
