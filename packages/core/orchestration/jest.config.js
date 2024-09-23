c3e3ff0558697061c172f4ff6de7eb2b2f93e123
adrien2p <adrien.deperetti@gmail.com>
2024-09-23 11:01:54 +0200

    fix configuration

--- packages/core/orchestration/jest.config.js
+++ packages/core/orchestration/jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  transform: { "^.+\\.[jt]s?$": "@swc/jest" },
+  transform: {
+    "^.+\\.[jt]s$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: { syntax: "typescript", decorators: true },
+          transform: { decoratorMetadata: true },
+          target: "ES2021",
+        },
+      },
+    ],
+  },
+  testPathIgnorePatterns: [`dist/`, `node_modules/`],
   testEnvironment: `node`,
   moduleFileExtensions: [`js`, `ts`],
 }
