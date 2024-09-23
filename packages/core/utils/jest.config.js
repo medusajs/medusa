7b25b0e9b76d8103773668ee4338dc2568276296
Harminder Virk <virk.officials@gmail.com>
2024-09-23 14:39:12 +0530

    chore: use swc for running tests

--- packages/core/utils/jest.config.js
+++ packages/core/utils/jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   transform: {
-    "^.+\\.[jt]s?$": [
-      "ts-jest",
+    "^.+\\.[jt]s$": [
+      "@swc/jest",
       {
-        tsconfig: "tsconfig.json",
-        isolatedModules: true,
+        jsc: {
+          parser: { syntax: "typescript", decorators: true },
+          transform: { decoratorMetadata: true },
+          target: "ES2021",
+        },
       },
     ],
   },
+  testPathIgnorePatterns: [`dist/`, `node_modules/`],
   testEnvironment: `node`,
   moduleFileExtensions: [`js`, `ts`],
 }
