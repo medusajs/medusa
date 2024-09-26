ebb1f33d66eec66337f1d906b6ab1c0158fc3a25
Shahed Nasser <shahednasser@gmail.com>
2024-09-26 18:04:38 +0300

    improve oas

--- www/utils/generated/oas-output/schemas/StorePaymentCollection.ts
+++ www/utils/generated/oas-output/schemas/StorePaymentCollection.ts
@@ -19,80 +19,45 @@
  *     type: string
  *     title: currency_code
  *     description: The payment collection's currency code.
+ *     example: usd
  *   region_id:
  *     type: string
  *     title: region_id
- *     description: The payment collection's region id.
+ *     description: The ID of the associated region.
  *   amount:
- *     oneOf:
- *       - type: string
- *         title: amount
- *         description: The payment collection's amount.
- *       - type: number
- *         title: amount
- *         description: The payment collection's amount.
- *       - type: string
- *         title: amount
- *         description: The payment collection's amount.
- *       - $ref: "#/components/schemas/IBigNumber"
+ *     type: number
+ *     title: amount
+ *     description: The payment collection's amount.
  *   authorized_amount:
- *     oneOf:
- *       - type: string
- *         title: authorized_amount
- *         description: The payment collection's authorized amount.
- *       - type: number
- *         title: authorized_amount
- *         description: The payment collection's authorized amount.
- *       - type: string
- *         title: authorized_amount
- *         description: The payment collection's authorized amount.
- *       - $ref: "#/components/schemas/IBigNumber"
+ *     type: number
+ *     title: authorized_amount
+ *     description: The payment collection's authorized amount.
  *   captured_amount:
- *     oneOf:
- *       - type: string
- *         title: captured_amount
- *         description: The payment collection's captured amount.
- *       - type: number
- *         title: captured_amount
- *         description: The payment collection's captured amount.
- *       - type: string
- *         title: captured_amount
- *         description: The payment collection's captured amount.
- *       - $ref: "#/components/schemas/IBigNumber"
+ *     type: number
+ *     title: captured_amount
+ *     description: The payment collection's captured amount.
  *   refunded_amount:
- *     oneOf:
- *       - type: string
- *         title: refunded_amount
- *         description: The payment collection's refunded amount.
- *       - type: number
- *         title: refunded_amount
- *         description: The payment collection's refunded amount.
- *       - type: string
- *         title: refunded_amount
- *         description: The payment collection's refunded amount.
- *       - $ref: "#/components/schemas/IBigNumber"
+ *     type: number
+ *     title: refunded_amount
+ *     description: The payment collection's refunded amount.
  *   completed_at:
- *     oneOf:
- *       - type: string
- *         title: completed_at
- *         description: The payment collection's completed at.
- *       - type: string
- *         title: completed_at
- *         description: The payment collection's completed at.
- *         format: date-time
+ *     type: string
+ *     title: completed_at
+ *     description: The date the payment collection was completed.
+ *     format: date-time
  *   created_at:
  *     type: string
  *     format: date-time
  *     title: created_at
- *     description: The payment collection's created at.
+ *     description: The date the payment collection was created.
  *   updated_at:
  *     type: string
  *     format: date-time
  *     title: updated_at
- *     description: The payment collection's updated at.
+ *     description: The date the payment collection was updated.
  *   metadata:
  *     type: object
- *     description: The payment collection's metadata.
+ *     description: The payment collection's metadata, can hold custom key-value pairs.
  *   status:
  *     type: string
  *     description: The payment collection's status.
