da6987dd454f6e36cf23511b89b1848fd5684906
Shahed Nasser <shahednasser@gmail.com>
2024-09-26 18:13:51 +0300

    more improvements

--- www/utils/generated/oas-output/schemas/WorkflowExecutionContext.ts
+++ www/utils/generated/oas-output/schemas/WorkflowExecutionContext.ts
@@ -10,6 +10,27 @@
  *   data:
  *     type: object
  *     description: The context's data.
+ *     properties:
+ *       invoke:
+ *         type: object
+ *         description: The datum's invoke.
+ *         required:
+ *           - output
+ *         additionalProperties:
+ *           type: object
+ *           properties:
+ *             output:
+ *               type: object
+ *               description: The invoke's output.
+ *               required:
+ *                 - output
+ *                 - compensateInput
+ *               properties:
+ *                 output: {}
+ *                 compensateInput: {}
+ *       payload: {}
+ *     required:
+ *       - invoke
  *   compensate:
  *     type: object
  *     description: The context's compensate.
@@ -18,7 +39,23 @@
  *     description: The context's errors.
  *     items:
  *       type: object
- *       description: The error's errors.
+ *       description: The error's details.
+ *       properties:
+ *         error:
+ *           type: object
+ *           description: The error's details.
+ *         action:
+ *           type: string
+ *           title: action
+ *           description: The error's action.
+ *         handlerType:
+ *           type: string
+ *           title: handlerType
+ *           description: The error's handler type.
+ *       required:
+ *         - error
+ *         - action
+ *         - handlerType
  * 
 */
 
