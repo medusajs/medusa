2769c7e7168f0fa0b1c598e4e69dc65c8d64b6d7
Shahed Nasser <shahednasser@gmail.com>
2024-09-26 15:35:21 +0300

    docs-util: fixes following packages reorg

--- www/utils/packages/docs-generator/src/classes/helpers/schema-factory.ts
+++ www/utils/packages/docs-generator/src/classes/helpers/schema-factory.ts
@@ -37,6 +37,34 @@ class SchemaFactory {
     BigNumberValue: {
       type: "number",
     },
+    expand: {
+      type: "string",
+      title: "expand",
+      description:
+        "Comma-separated relations that should be expanded in the returned data.",
+    },
+    fields: {
+      type: "string",
+      title: "fields",
+      description:
+        "Comma-separated fields that should be included in the returned data. if a field is prefixed with `+` it will be added to the default fields, using `-` will remove it from the default fields. without prefix it will replace the entire default fields.",
+    },
+    offset: {
+      type: "number",
+      title: "offset",
+      description: "The number of items to skip when retrieving a list.",
+    },
+    limit: {
+      type: "number",
+      title: "limit",
+      description: "Limit the number of items returned in the list.",
+    },
+    order: {
+      type: "string",
+      title: "order",
+      description:
+        "The field to sort the data by. By default, the sort order is ascending. To change the order to descending, prefix the field name with `-`.",
+    },
   }
   /**
    * Schemas used only for response types.
