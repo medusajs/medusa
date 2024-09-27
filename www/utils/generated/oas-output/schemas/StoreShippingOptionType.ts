ff36368ec7dc6ebc65da99b4077dda5e189d56a7
Shahed Nasser <shahednasser@gmail.com>
2024-09-27 17:07:18 +0300

    more fixes

---
+++ www/utils/generated/oas-output/schemas/StoreShippingOptionType.ts
@@ -0,0 +1,53 @@
+/**
+ * @schema StoreShippingOptionType
+ * type: object
+ * description: The shipping option type's details.
+ * x-schemaName: StoreShippingOptionType
+ * required:
+ *   - id
+ *   - label
+ *   - description
+ *   - code
+ *   - shipping_option_id
+ *   - created_at
+ *   - updated_at
+ *   - deleted_at
+ * properties:
+ *   id:
+ *     type: string
+ *     title: id
+ *     description: The type's ID.
+ *   label:
+ *     type: string
+ *     title: label
+ *     description: The type's label.
+ *   description:
+ *     type: string
+ *     title: description
+ *     description: The type's description.
+ *   code:
+ *     type: string
+ *     title: code
+ *     description: The type's code.
+ *   shipping_option_id:
+ *     type: string
+ *     title: shipping_option_id
+ *     description: The ID of the associated shipping option.
+ *   created_at:
+ *     type: string
+ *     format: date-time
+ *     title: created_at
+ *     description: The date the type was created.
+ *   updated_at:
+ *     type: string
+ *     format: date-time
+ *     title: updated_at
+ *     description: The date the type was updated.
+ *   deleted_at:
+ *     type: string
+ *     format: date-time
+ *     title: deleted_at
+ *     description: The date the type was deleted.
+ * 
+*/
+
