c90689a06265897c7b5eed47e0db3b32093eee16
adrien2p <adrien.deperetti@gmail.com>
2024-09-30 10:19:34 +0200

    fix build

--- packages/medusa/src/commands/__tests__/start.spec.ts
+++ packages/medusa/src/commands/__tests__/start.spec.ts
@@ -11,7 +11,7 @@ describe("start", () => {
 
   describe("registerInstrumentation", () => {
     it("should not throw when registering the instrumentation if the file is present but do not export a register function", async () => {
-      const fsSpy = jest.spyOn(utils.FileSystem.prototype, "exists", "" as any)
+      const fsSpy = jest.spyOn(utils.FileSystem.prototype, "exists", "" as never)
 
       await registerInstrumentation(__dirname)
 
@@ -22,7 +22,7 @@ describe("start", () => {
     })
 
     it("should register the instrumentation if the file is present and exports a register function", async () => {
-      const fsSpy = jest.spyOn(utils.FileSystem.prototype, "exists", "" as any)
+      const fsSpy = jest.spyOn(utils.FileSystem.prototype, "exists", "" as never)
 
       instrumentationFixture.registerMock.mockReturnValue(true)
 
@@ -37,7 +37,7 @@ describe("start", () => {
     })
 
     it("should throw if the instrumentation file exists but cannot be imported", async () => {
-      const fsSpy = jest.spyOn(utils.FileSystem.prototype, "exists", "" as any)
+      const fsSpy = jest.spyOn(utils.FileSystem.prototype, "exists", "" as never)
 
       const err = await registerInstrumentation(
         path.join(__dirname, "../__fixtures__/instrumentation-failure")
