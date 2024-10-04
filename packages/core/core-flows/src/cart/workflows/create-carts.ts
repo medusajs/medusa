9cf16534c23ca7f3bc1d54dbf2784e80004663f0
Oli Juhl <59018053+olivermrbl@users.noreply.github.com>
2024-10-04 11:36:27 +0200

    Update packages/core/core-flows/src/cart/workflows/create-carts.ts
    Co-authored-by: Carlos R. L. Rodrigues <37986729+carlos-r-l-rodrigues@users.noreply.github.com>

--- packages/core/core-flows/src/cart/workflows/create-carts.ts
+++ packages/core/core-flows/src/cart/workflows/create-carts.ts
@@ -117,11 +117,9 @@ export const createCartWorkflow = createWorkflow(
         }
 
         // If there is only one country in the region, we prepare a shipping address with that country's code.
-        if (!data.input.shipping_address) {
-          if (data.region.countries.length === 1) {
-            data_.shipping_address = {
-              country_code: data.region.countries[0].iso_2,
-            }
+        if (!data.input.shipping_address && data.region.countries.length === 1) {
+          data_.shipping_address = {
+            country_code: data.region.countries[0].iso_2,
           }
         }
 
