5ac61d60d4513ff208cb624c04ba21b91e84d472
Riqwan Thamir <rmthamir@gmail.com>
2024-10-07 10:55:18 +0200

    fix(medusa): use transform order for store order endpoints

--- packages/medusa/src/api/store/orders/[id]/route.ts
+++ packages/medusa/src/api/store/orders/[id]/route.ts
@@ -1,17 +1,19 @@
-import { HttpTypes } from "@medusajs/framework/types"
+import { getOrderDetailWorkflow } from "@medusajs/core-flows"
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
-import { refetchOrder } from "../helpers"
+import { HttpTypes } from "@medusajs/framework/types"
 
 // TODO: Do we want to apply some sort of authentication here? My suggestion is that we do
 export const GET = async (
   req: MedusaRequest,
   res: MedusaResponse<HttpTypes.StoreOrderResponse>
 ) => {
-  const order = await refetchOrder(
-    req.params.id,
-    req.scope,
-    req.remoteQueryConfig.fields
-  )
+  const workflow = getOrderDetailWorkflow(req.scope)
+  const { result } = await workflow.run({
+    input: {
+      fields: req.remoteQueryConfig.fields,
+      order_id: req.params.id,
+    },
+  })
 
-  res.json({ order })
+  res.status(200).json({ order: result as HttpTypes.StoreOrder })
 }
