a38e82e0f5f5885b7906b862a514fdb8a09e3734
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-10-09 13:26:03 +0200

    hide CustomFields API

--- packages/admin/admin-sdk/src/config/index.ts
+++ packages/admin/admin-sdk/src/config/index.ts
@@ -1,2 +1,3 @@
-export { CustomFieldConfig, RouteConfig, WidgetConfig } from "./types"
-export * from "./utils"
+// We don't export anything related to CustomFields for the time being
+export type { RouteConfig, WidgetConfig } from "./types"
+export { defineRouteConfig, defineWidgetConfig } from "./utils"
