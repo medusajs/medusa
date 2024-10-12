9b5a580a966be2167fb2df948f50b01eeef9f60a
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2024-10-12 18:55:00 -0300

    eslint

--- packages/medusa/src/modules/locking-postgres.ts
+++
@@ -1,6 +0,0 @@
-import PostgresLockingProvider from "@medusajs/locking-postgres"
-
-export * from "@medusajs/locking-postgres"
-
-export default PostgresLockingProvider
-export const discoveryPath = require.resolve("@medusajs/locking-postgres")
