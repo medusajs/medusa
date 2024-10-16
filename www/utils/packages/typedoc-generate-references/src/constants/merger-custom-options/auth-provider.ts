ea5bff9034d3a8fdd32b735f5d858c059aa375a5
Shahed Nasser <shahednasser@gmail.com>
2024-10-16 14:37:14 +0300

    docs: update providers to use ModuleProvider (#9579)
    Document change introduced by #9544
    
    > Note: fine to merge now as it requires generating references, which will only happen after the next release.

--- www/utils/packages/typedoc-generate-references/src/constants/merger-custom-options/auth-provider.ts
+++ www/utils/packages/typedoc-generate-references/src/constants/merger-custom-options/auth-provider.ts
@@ -39,10 +39,14 @@ Create the file \`src/modules/my-auth/index.ts\` with the following content:
 
 \`\`\`ts title="src/modules/my-auth/index.ts"
 import MyAuthProviderService from "./service"
+import { 
+  ModuleProvider, 
+  Modules
+} from "@medusajs/framework/utils"
 
-export default {
+export default ModuleProvider(Modules.AUTH, {
   services: [MyAuthProviderService],
-}
+})
 \`\`\`
 
 This exports the module's definition, indicating that the \`MyAuthProviderService\` is the module's service.`,
