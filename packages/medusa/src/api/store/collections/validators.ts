1b24c41f5f9e10a7773127c1719db8c48269c2ce
adrien2p <adrien.deperetti@gmail.com>
2024-10-21 15:29:53 +0200

    continue refactoring

--- packages/medusa/src/api/store/collections/validators.ts
+++ packages/medusa/src/api/store/collections/validators.ts
@@ -4,8 +4,18 @@ import {
   createOperatorMap,
   createSelectParams,
 } from "../../utils/validators"
+import { applyAndAndOrOperators } from "../../utils/common-validators"
 
 export const StoreGetCollectionParams = createSelectParams()
+
+export const StoreGetCollectionsParamsFields = z.object({
+  q: z.string().optional(),
+  title: z.union([z.string(), z.array(z.string())]).optional(),
+  handle: z.union([z.string(), z.array(z.string())]).optional(),
+  created_at: createOperatorMap().optional(),
+  updated_at: createOperatorMap().optional(),
+  deleted_at: createOperatorMap().optional(),
+})
 
 export type StoreGetCollectionsParamsType = z.infer<
   typeof StoreGetCollectionsParams
@@ -14,15 +24,6 @@ export const StoreGetCollectionsParams = createFindParams({
   offset: 0,
   limit: 10,
   order: "-created_at",
-}).merge(
-  z.object({
-    q: z.string().optional(),
-    title: z.union([z.string(), z.array(z.string())]).optional(),
-    handle: z.union([z.string(), z.array(z.string())]).optional(),
-    created_at: createOperatorMap().optional(),
-    updated_at: createOperatorMap().optional(),
-    deleted_at: createOperatorMap().optional(),
-    $and: z.lazy(() => StoreGetCollectionsParams.array()).optional(),
-    $or: z.lazy(() => StoreGetCollectionsParams.array()).optional(),
-  })
-)
+})
+  .merge(StoreGetCollectionsParamsFields)
+  .merge(applyAndAndOrOperators(StoreGetCollectionsParamsFields))
