653998eb33585249547afcb732369de4ff0780d4
docloulou <lucas.zerbib@gmail.com>
2024-10-24 17:57:16 +0200

    Allow customer metadata update
    See issue 9779
    https://github.com/medusajs/medusa/issues/9779

--- packages/medusa/src/api/store/customers/validators.ts
+++ packages/medusa/src/api/store/customers/validators.ts
@@ -10,6 +10,7 @@ export const StoreCreateCustomer = z.object({
   first_name: z.string().nullish(),
   last_name: z.string().nullish(),
   phone: z.string().nullish(),
+  metadata: z.record(z.unknown()).nullish(),
 })
 
 export const StoreUpdateCustomer = z.object({
@@ -17,6 +18,7 @@ export const StoreUpdateCustomer = z.object({
   first_name: z.string().nullish(),
   last_name: z.string().nullish(),
   phone: z.string().nullish(),
+  metadata: z.record(z.unknown()).nullish(),
 })
 
 export const StoreGetCustomerAddressParams = createSelectParams()
