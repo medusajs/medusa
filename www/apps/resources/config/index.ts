04cec64fa1c6f7b6171e83cc2b71957eec16e7bb
Shahed Nasser <shahednasser@gmail.com>
2024-10-24 14:03:34 +0300

    docs: show latest version in docs + update automatically (#9753)

--- www/apps/resources/config/index.ts
+++ www/apps/resources/config/index.ts
@@ -1,9 +1,11 @@
 import { DocsConfig, SidebarItem } from "types"
 import { generatedSidebar } from "../generated/sidebar.mjs"
+import { globalConfig } from "docs-ui"
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
 
 export const config: DocsConfig = {
+  ...globalConfig,
   titleSuffix: "Medusa Development Resources",
   baseUrl,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
