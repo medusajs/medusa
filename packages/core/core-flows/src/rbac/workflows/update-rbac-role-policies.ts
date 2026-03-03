6695f211633620d5c8c54c8f7a866e92d4ec6166
Shahed Nasser <shahednasser@gmail.com>
2026-03-03 16:25:45 +0200

    chore: add ignore tags for rbac

--- packages/core/core-flows/src/rbac/workflows/update-rbac-role-policies.ts
+++ packages/core/core-flows/src/rbac/workflows/update-rbac-role-policies.ts
@@ -6,13 +6,25 @@ import {
 import { UpdateRbacRolePolicyDTO } from "@medusajs/types"
 import { updateRbacRolePoliciesStep } from "../steps/update-rbac-role-policies"
 
+/**
+ * @ignore
+ * @featureFlag rbac
+ */
 export type UpdateRbacRolePoliciesWorkflowInput = {
   selector: Record<string, any>
   update: Omit<UpdateRbacRolePolicyDTO, "id">
 }
 
+/**
+ * @ignore
+ * @featureFlag rbac
+ */
 export const updateRbacRolePoliciesWorkflowId = "update-rbac-role-policies"
 
+/**
+ * @ignore
+ * @featureFlag rbac
+ */
 export const updateRbacRolePoliciesWorkflow = createWorkflow(
   updateRbacRolePoliciesWorkflowId,
   (input: WorkflowData<UpdateRbacRolePoliciesWorkflowInput>) => {
