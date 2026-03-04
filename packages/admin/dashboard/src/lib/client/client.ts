c4e37dc4a10576bf8bc83d5121607195c75bc627
fPolic <mainacc.polic@gmail.com>
2026-03-04 11:49:15 +0100

    fix: revert client

--- packages/admin/dashboard/src/lib/client/client.ts
+++ packages/admin/dashboard/src/lib/client/client.ts
@@ -1,8 +1,8 @@
 import Medusa from "@medusajs/js-sdk"
 
 export const backendUrl = __BACKEND_URL__ ?? "/"
-const authType = "__AUTH_TYPE__" ?? "session"
-const jwtTokenStorageKey = "__JWT_TOKEN_STORAGE_KEY__" || undefined
+const authType = __AUTH_TYPE__ ?? "session"
+const jwtTokenStorageKey = __JWT_TOKEN_STORAGE_KEY__ || undefined
 
 export const sdk = new Medusa({
   baseUrl: backendUrl,
