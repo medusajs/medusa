156f50d0407f06e301aac4716b158c08901b683e
Shahed Nasser <shahednasser@gmail.com>
2026-04-14 09:20:43 +0300

    fix build

--- packages/core/types/src/http/workflow-execution/admin/payloads.ts
+++ packages/core/types/src/http/workflow-execution/admin/payloads.ts
@@ -1,4 +1,4 @@
-import type { TransactionHandlerType } from "@medusajs/framework/utils"
+import { TransactionHandlerType } from "@medusajs/framework/utils"
 
 export interface AdminCreateWorkflowsRun {
   transaction_id?: string | undefined
