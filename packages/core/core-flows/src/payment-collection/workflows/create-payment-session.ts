6faec9be640971a6b00bc7127ed278fdb9599b3a
suh016 <afifsabrin25@gmail.com>
2026-05-01 22:44:28 +0100

    chore(core-flows): use non-optional input.provider_id in account holder find

--- packages/core/core-flows/src/payment-collection/workflows/create-payment-session.ts
+++ packages/core/core-flows/src/payment-collection/workflows/create-payment-session.ts
@@ -118,7 +118,7 @@ export const createPaymentSessionsWorkflow = createWorkflow(
 
       const existingAccountHolder = transform({ customer, input }, (data) => {
         return (data.customer.account_holders ?? []).find(
-          (ac) => ac?.provider_id === data.input?.provider_id
+          (ac) => ac?.provider_id === data.input.provider_id
         )
       })
 
