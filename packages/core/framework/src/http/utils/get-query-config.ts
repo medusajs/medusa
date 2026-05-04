2bb39c1f28f00a6fd88f3a9f5b487e534624899a
v0eak <github@v0eak.com>
2026-05-04 12:39:33 +0200

    fix starfields

--- packages/core/framework/src/http/utils/get-query-config.ts
+++ packages/core/framework/src/http/utils/get-query-config.ts
@@ -59,7 +59,12 @@ export async function prepareListQuery<T extends RequestQueryFields, TEntity>(
     with_deleted,
   } = validated
 
-  const parsedFields = FieldParser.parse(fields, defaults as string[])
+  const normalizedFields =
+    fields?.includes("*")
+      ? fields.replace("*", defaults.join(","))
+      : fields
+
+  const parsedFields = FieldParser.parse(normalizedFields, defaults as string[])
   const { fields: allFields, starFields } = parsedFields
 
   const rbacFilterFieldsFeatureFlag =
