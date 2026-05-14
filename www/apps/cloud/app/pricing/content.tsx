e65807e5c3437ac27e76c720aeaaf3c8fec4e96a
Shahed Nasser <shahednasser@gmail.com>
2026-05-14 12:23:41 +0300

    add check for cloudflare env

--- www/apps/cloud/app/pricing/content.tsx
+++ www/apps/cloud/app/pricing/content.tsx
@@ -9,7 +9,8 @@ export default async function PricingPage() {
   if (
     process.env.NEXT_PUBLIC_ENV === "CI" ||
     process.env.NEXT_PUBLIC_ENV === "preview" ||
-    process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+    process.env.CLOUDFLARE_ENV !== "production"
   ) {
     return (
       <div>Pricing page is not available in the CI / Preview environment.</div>
