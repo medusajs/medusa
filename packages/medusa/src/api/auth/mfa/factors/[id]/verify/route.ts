27b6175d6de0b999c5cb0eef4f09066364ae8699
Christian <christian.anese@gmail.com>
2026-05-18 08:43:04 +0200

    feat(auth): add MFA API routes

---
+++ packages/medusa/src/api/auth/mfa/factors/[id]/verify/route.ts
@@ -0,0 +1,27 @@
+import {
+  AuthenticatedMedusaRequest,
+  MedusaResponse,
+} from "@medusajs/framework/http"
+import { IAuthModuleService } from "@medusajs/framework/types"
+import { Modules } from "@medusajs/framework/utils"
+import { AuthMfaVerifyFactorRequestType } from "../../../../validators"
+
+export const POST = async (
+  req: AuthenticatedMedusaRequest<AuthMfaVerifyFactorRequestType>,
+  res: MedusaResponse
+) => {
+  const { id } = req.params
+  const authService = req.scope.resolve<IAuthModuleService>(Modules.AUTH)
+
+  await authService.retrieveAuthMfa({
+    id,
+    auth_identity_id: req.auth_context.auth_identity_id,
+  })
+
+  const verifiedFactor = await authService.verifyAuthMfa({
+    id,
+    code: req.validatedBody.code,
+  })
+
+  return res.status(200).json({ mfa_factor: verifiedFactor })
+}
