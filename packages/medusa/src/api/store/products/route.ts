3a0884303520515fd548dc9dbb03205f97be3fa1
Shahed Nasser <shahednasser@gmail.com>
2026-05-25 08:54:30 +0300

    remove unused

--- packages/medusa/src/api/store/products/route.ts
+++ packages/medusa/src/api/store/products/route.ts
@@ -14,13 +14,6 @@ export const GET = async (
   req: RequestWithContext<HttpTypes.StoreProductListParams>,
   res: MedusaResponse<HttpTypes.StoreProductListResponse>
 ) => {
-  const filterableFields: HttpTypes.StoreProductListParams & {
-    // these are available through the Zod transformation
-    tags?: string | string[]
-    categories?: string | string[]
-  } =
-    req.filterableFields
-
   if (FeatureFlag.isFeatureEnabled(IndexEngineFeatureFlag.key)) {
     // TODO: These filters are not supported by the index engine yet
     if (
