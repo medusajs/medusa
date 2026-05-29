9ed8cdb334c45195e0e9013f3e13abd7d0617d80
Nicolas Gorga <nicogorga11@gmail.com>
2026-05-29 00:39:06 -0300

    guard configurable data table

--- packages/admin/dashboard/src/routes/products/product-list/components/product-list-table/configurable-product-list-table.tsx
+++ packages/admin/dashboard/src/routes/products/product-list/components/product-list-table/configurable-product-list-table.tsx
@@ -2,23 +2,33 @@ import { useTranslation } from "react-i18next"
 import { Outlet, useLocation } from "react-router-dom"
 
 import { ConfigurableDataTable } from "../../../../../components/table/configurable-data-table"
+import { usePermissions } from "../../../../../providers/permissions-provider"
 import { useProductTableAdapter } from "./product-table-adapter"
 
 export const ConfigurableProductListTable = () => {
   const { t } = useTranslation()
   const location = useLocation()
   const adapter = useProductTableAdapter()
+  const { hasPermission } = usePermissions()
+
+  const canCreate = hasPermission("product:create")
+
+  const actions = [
+    { label: t("actions.export"), to: `export${location.search}` },
+    ...(canCreate
+      ? [
+          { label: t("actions.import"), to: `import${location.search}` },
+          { label: t("actions.create"), to: "create" },
+        ]
+      : []),
+  ]
 
   return (
     <>
       <ConfigurableDataTable
         adapter={adapter}
         heading={t("products.domain")}
-        actions={[
-          { label: t("actions.export"), to: `export${location.search}` },
-          { label: t("actions.import"), to: `import${location.search}` },
-          { label: t("actions.create"), to: "create" },
-        ]}
+        actions={actions}
       />
       <Outlet />
     </>
