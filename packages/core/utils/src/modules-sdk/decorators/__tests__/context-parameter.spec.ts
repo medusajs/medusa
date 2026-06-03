0312040be667a5c575dd724110284b6f92e22d96
Deepak kudi <deepakkudi23@adsl-172-10-9-116.dsl.sndg02.sbcglobal.net>
2026-06-03 18:37:49 +0530

    test(utils): cover inherited context indexes

--- packages/core/utils/src/modules-sdk/decorators/__tests__/context-parameter.spec.ts
+++ packages/core/utils/src/modules-sdk/decorators/__tests__/context-parameter.spec.ts
@@ -1,6 +1,21 @@
 import { MedusaContext } from "../context-parameter"
 
 describe("MedusaContext", () => {
+  it("keeps inherited context indexes when adding child metadata", () => {
+    const parentPrototype = {}
+    MedusaContext()(parentPrototype, "createProductOptions", 1)
+
+    const childPrototype = Object.create(parentPrototype)
+    MedusaContext()(childPrototype, "updateProductOptionValues", 2)
+
+    expect(MedusaContext.getIndex(childPrototype, "createProductOptions")).toBe(
+      1
+    )
+    expect(
+      MedusaContext.getIndex(childPrototype, "updateProductOptionValues")
+    ).toBe(2)
+  })
+
   it("keeps overridden context indexes on the child prototype", () => {
     const parentPrototype = {}
     MedusaContext()(parentPrototype, "updateProductOptionValues", 1)
