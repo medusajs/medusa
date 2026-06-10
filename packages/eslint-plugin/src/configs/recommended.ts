f3ad615c5d902931aae172ddf10374d5621f296d
Shahed Nasser <shahednasser@gmail.com>
2026-06-10 16:01:51 +0300

    added no variable mutation rule

--- packages/eslint-plugin/src/configs/recommended.ts
+++ packages/eslint-plugin/src/configs/recommended.ts
@@ -30,6 +30,7 @@ export function buildRecommended(plugin: unknown): Linter.Config[] {
         [ruleId("no-async-workflow-constructor")]: "error",
         [ruleId("no-conditional-expressions-in-workflow")]: "error",
         [ruleId("no-console-log-in-workflow")]: "warn",
+        [ruleId("no-direct-variable-mutation-in-workflow")]: "error",
         [ruleId("no-if-in-workflow-constructor")]: "error",
         [ruleId("no-loops-in-workflow")]: "error",
         [ruleId("no-spread-in-workflow")]: "error",
--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -2,6 +2,7 @@ import type { ESLint } from "eslint"
 import { rule as noAsyncWorkflowConstructor } from "./no-async-workflow-constructor"
 import { rule as noConditionalExpressionsInWorkflow } from "./no-conditional-expressions-in-workflow"
 import { rule as noConsoleLogInWorkflow } from "./no-console-log-in-workflow"
+import { rule as noDirectVariableMutationInWorkflow } from "./no-direct-variable-mutation-in-workflow"
 import { rule as noIfInWorkflowConstructor } from "./no-if-in-workflow-constructor"
 import { rule as noLoopsInWorkflow } from "./no-loops-in-workflow"
 import { rule as noSpreadInWorkflow } from "./no-spread-in-workflow"
@@ -11,6 +12,7 @@ export const rules = {
   "no-async-workflow-constructor": noAsyncWorkflowConstructor,
   "no-conditional-expressions-in-workflow": noConditionalExpressionsInWorkflow,
   "no-console-log-in-workflow": noConsoleLogInWorkflow,
+  "no-direct-variable-mutation-in-workflow": noDirectVariableMutationInWorkflow,
   "no-if-in-workflow-constructor": noIfInWorkflowConstructor,
   "no-loops-in-workflow": noLoopsInWorkflow,
   "no-spread-in-workflow": noSpreadInWorkflow,
