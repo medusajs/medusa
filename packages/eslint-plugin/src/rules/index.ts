4d4e010b35b30d0c8be7829af536972c3c2ce1be
Shahed Nasser <shahednasser@gmail.com>
2026-06-10 16:30:29 +0300

    feat(eslint-plugin): added rules for workflows (2/2)

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -5,6 +5,7 @@ import { rule as noConsoleLogInWorkflow } from "./no-console-log-in-workflow"
 import { rule as noDirectVariableMutationInWorkflow } from "./no-direct-variable-mutation-in-workflow"
 import { rule as noIfInWorkflowConstructor } from "./no-if-in-workflow-constructor"
 import { rule as noLoopsInWorkflow } from "./no-loops-in-workflow"
+import { rule as noNewDateInWorkflow } from "./no-new-date-in-workflow"
 import { rule as noSpreadInWorkflow } from "./no-spread-in-workflow"
 import { rule as noTryCatchInWorkflow } from "./no-try-catch-in-workflow"
 
@@ -15,6 +16,7 @@ export const rules = {
   "no-direct-variable-mutation-in-workflow": noDirectVariableMutationInWorkflow,
   "no-if-in-workflow-constructor": noIfInWorkflowConstructor,
   "no-loops-in-workflow": noLoopsInWorkflow,
+  "no-new-date-in-workflow": noNewDateInWorkflow,
   "no-spread-in-workflow": noSpreadInWorkflow,
   "no-try-catch-in-workflow": noTryCatchInWorkflow,
 } as unknown as NonNullable<ESLint.Plugin["rules"]>
