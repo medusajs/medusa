d6d2b3ee17b0cf023d4ca7281aef91d2f457eb34
Shahed Nasser <shahednasser@gmail.com>
2026-06-11 16:54:47 +0300

    Update version in config-module.ts documentation

--- packages/core/types/src/common/config-module.ts
+++ packages/core/types/src/common/config-module.ts
@@ -297,14 +297,14 @@ export type MedusaCloudOptions = {
    * The JWKS URI for the Medusa Cloud OAuth service. This is used to verify the JWT tokens issued by the Medusa Cloud OAuth service.
    * If not provided, an error is thrown when trying to use the Medusa Cloud OAuth service.
    *
-   * @since 2.15.6
+   * @since 2.16.0
    */
   oauthJwksUri?: string
   /**
    * The expected `aud` claim of id_tokens issued by the Medusa Cloud OAuth service.
    * If not provided, an error is thrown when trying to use the Medusa Cloud OAuth service.
    *
-   * @since 2.15.6
+   * @since 2.16.0
    */
   oauthAudience?: string
 }
