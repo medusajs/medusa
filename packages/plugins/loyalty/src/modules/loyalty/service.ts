f7ce3884415ba8f8bf036283af809f00b2cbd585
scherddel <orussek@gmail.com>
2026-06-30 19:03:21 +0200

    dont make options optional

--- packages/plugins/loyalty/src/modules/loyalty/service.ts
+++ packages/plugins/loyalty/src/modules/loyalty/service.ts
@@ -1,19 +1,22 @@
-import { MedusaService } from "@medusajs/framework/utils";
-import { LoyaltyPluginOptions } from "../../types";
-import GiftCard from "./models/gift-card";
+import { MedusaService } from "@medusajs/framework/utils"
+import { LoyaltyPluginOptions } from "../../types"
+import GiftCard from "./models/gift-card"
 
 class LoyaltyModuleService extends MedusaService({
   GiftCard,
 }) {
-  private options_?: LoyaltyPluginOptions
-  
-  constructor(dependencies: Record<string, unknown>, options: LoyaltyPluginOptions = {}) {
+  private options_: LoyaltyPluginOptions
+
+  constructor(
+    dependencies: Record<string, unknown>,
+    options: LoyaltyPluginOptions
+  ) {
     super(dependencies, options)
     this.options_ = options
   }
   getOptions(): LoyaltyPluginOptions {
-    return this.options_ ?? {}
+    return this.options_
   }
 }
 
-export default LoyaltyModuleService;
+export default LoyaltyModuleService
