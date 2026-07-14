7d6143404ea3b392d7f900c717341a23bb35b978
Nicolas Gorga <nicogorga11@gmail.com>
2026-07-14 12:04:52 -0300

    Add layout composer to property labels and widget zone

--- packages/admin/dashboard/src/routes/property-labels/property-labels.tsx
+++ packages/admin/dashboard/src/routes/property-labels/property-labels.tsx
@@ -1,16 +1,19 @@
-import { SingleColumnPage } from "../../components/layout/pages"
+import { CORE_LAYOUT_IDS } from "@medusajs/admin-shared"
+import { LayoutComposer } from "../../components/layout-composer"
 import { PropertyLabelsList } from "./components/property-labels-list"
 
 export const PropertyLabels = () => {
   return (
-    <SingleColumnPage
-      widgets={{
-        before: [],
-        after: [],
+    <LayoutComposer
+      widgetsZonePrefix="property_label.list"
+      preferredLayoutId={CORE_LAYOUT_IDS.SINGLE_COLUMN}
+      sections={{
+        main: (
+          <LayoutComposer.Entry id="PropertyLabelListTable">
+            <PropertyLabelsList />
+          </LayoutComposer.Entry>
+        ),
       }}
-      hasOutlet
-    >
-      <PropertyLabelsList />
-    </SingleColumnPage>
+    ></LayoutComposer>
   )
 }
