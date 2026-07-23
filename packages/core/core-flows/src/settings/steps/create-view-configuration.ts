7803e213b60d5cea6e324655ee18de32fad29f54
Shahed Nasser <shahednasser@gmail.com>
2026-07-23 16:23:22 +0300

    chore: update tsdocs of the settings module

--- packages/core/core-flows/src/settings/steps/create-view-configuration.ts
+++ packages/core/core-flows/src/settings/steps/create-view-configuration.ts
@@ -4,13 +4,28 @@ import {
 import { Modules } from "@medusajs/framework/utils"
 import { StepResponse, createStep } from "@medusajs/framework/workflows-sdk"
 
+/**
+ * The data to create a view configuration.
+ */
 export type CreateViewConfigurationStepInput = CreateViewConfigurationDTO
 
 export const createViewConfigurationStepId = "create-view-configuration"
 
 /**
+ * This step creates a view configuration.
+ *
  * @since 2.10.3
  * @featureFlag view_configurations
+ *
+ * @example
+ * const viewConfig = createViewConfigurationStep({
+ *   entity: "orders",
+ *   name: "My Orders View",
+ *   configuration: {
+ *     visible_columns: ["display_id", "status"],
+ *     column_order: ["display_id", "status"],
+ *   },
+ * })
  */
 export const createViewConfigurationStep = createStep(
   createViewConfigurationStepId,
