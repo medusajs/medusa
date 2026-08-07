96d40d295726c7585c43b61fedc7e253641496bd
Shahed Nasser <shahednasser@gmail.com>
2026-08-07 14:52:23 +0300

    docs: add docs on gating returns creation from storefront (#16360)

--- www/packages/tags/src/tags/order.ts
+++ www/packages/tags/src/tags/order.ts
@@ -68,6 +68,10 @@ export const order = [
     "path": "https://docs.medusajs.com/resources/commerce-modules/order/secure-order-retrieval"
   },
   {
+    "title": "Restrict Return Creation",
+    "path": "https://docs.medusajs.com/resources/commerce-modules/order/secure-return-creation"
+  },
+  {
     "title": "Implement Quote Management",
     "path": "https://docs.medusajs.com/resources/examples/guides/quote-management"
   },
