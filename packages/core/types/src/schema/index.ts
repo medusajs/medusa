ef62b138bc3b47bc2874378e5ec533dbd22a3f9c
Shahed Nasser <shahednasser@gmail.com>
2026-08-11 18:48:45 +0300

    docs: add TSDocs for 39fac16bfa77374e3158842f64aeff4031e4f37b (#16399)
    Co-authored-by: sradevski <sradevski@users.noreply.github.com>

--- packages/core/types/src/schema/index.ts
+++ packages/core/types/src/schema/index.ts
@@ -1,6 +1,8 @@
 /**
  * The meta-data returned by a shared schema property parse method.
  * Domain-specific metadata (DML indexes, search facets, etc.) extends this.
+ *
+ * @since 2.18.1
  */
 export type SchemaPropertyMetadata = {
   fieldName: string
@@ -17,6 +19,8 @@ export type SchemaPropertyMetadata = {
  * Definition of a schema property type. It should have a parse
  * method to get the metadata and a type-only property
  * to get its static type.
+ *
+ * @since 2.18.1
  */
 export type SchemaPropertyType<T> = {
   $dataType: T
