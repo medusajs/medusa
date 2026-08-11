39fac16bfa77374e3158842f64aeff4031e4f37b
Stevche Radevski <sradevski@live.com>
2026-08-11 17:13:11 +0200

    feat: Implement a DSL for search index (#16391)
    * feat: Implement a DSL for search index
    
    * fix: Correctly load search indexes in test setup

---
+++ packages/core/types/src/schema/index.ts
@@ -0,0 +1,24 @@
+/**
+ * The meta-data returned by a shared schema property parse method.
+ * Domain-specific metadata (DML indexes, search facets, etc.) extends this.
+ */
+export type SchemaPropertyMetadata = {
+  fieldName: string
+  defaultValue?: any
+  nullable: boolean
+  computed: boolean
+  dataType: {
+    name: string
+    options?: Record<string, any>
+  }
+}
+
+/**
+ * Definition of a schema property type. It should have a parse
+ * method to get the metadata and a type-only property
+ * to get its static type.
+ */
+export type SchemaPropertyType<T> = {
+  $dataType: T
+  parse(fieldName: string): SchemaPropertyMetadata
+}
