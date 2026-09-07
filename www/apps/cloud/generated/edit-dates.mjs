ea00419fcb2e88fcd8edd84a884236cb309ce4d6
Shahed Nasser <shahednasser@gmail.com>
2026-09-07 10:04:43 +0300

    update docs

--- www/apps/cloud/generated/edit-dates.mjs
+++ www/apps/cloud/generated/edit-dates.mjs
@@ -126,10 +126,10 @@ export const generatedEditDates = {
   "app/assistant/page.mdx": "2026-08-18T10:54:48.592Z",
   "app/projects/log-drains/page.mdx": "2026-08-21T12:38:31.597Z",
   "app/status/page.mdx": "2026-08-19T07:28:02.802Z",
-  "app/search/page.mdx": "2026-08-26T09:35:23.413Z",
-  "app/search/algolia/page.mdx": "2026-08-25T09:43:35.128Z",
-  "app/search/meilisearch/page.mdx": "2026-08-25T09:43:35.129Z",
-  "app/search/postgres/page.mdx": "2026-08-26T09:27:25.897Z",
-  "app/search/migrate-from-algolia/page.mdx": "2026-08-26T09:39:05.330Z",
-  "app/search/migrate-from-meilisearch/page.mdx": "2026-08-26T09:38:59.816Z"
+  "app/search/page.mdx": "2026-09-07T06:37:43.108Z",
+  "app/search/algolia/page.mdx": "2026-09-07T06:08:13.246Z",
+  "app/search/meilisearch/page.mdx": "2026-09-07T06:08:13.246Z",
+  "app/search/postgres/page.mdx": "2026-09-07T06:17:13.804Z",
+  "app/search/migrate-from-algolia/page.mdx": "2026-09-07T06:04:28.326Z",
+  "app/search/migrate-from-meilisearch/page.mdx": "2026-09-07T06:04:08.794Z"
 }
