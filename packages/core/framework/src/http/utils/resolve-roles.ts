f7bb422e96d6eee993fb7adc28a0e9e1e9fcf88c
Nicolas Gorga <nicogorga11@gmail.com>
2026-09-08 13:56:04 -0300

    Fix resolveRoles condition

--- packages/core/framework/src/http/utils/resolve-roles.ts
+++ packages/core/framework/src/http/utils/resolve-roles.ts
@@ -48,17 +48,13 @@ export const resolveRoles = async ({
           reference_id: g.id,
         })),
       },
-      ...(scope
-        ? [
-            {
-              $or: [
-                // Unscoped role assignments allow exercising privileges across all scopes.
-                { scope: null, scope_id: null },
-                { scope: scope.type, scope_id: scope.id },
-              ],
-            },
-          ]
-        : []),
+      {
+        $or: [
+          // Unscoped role assignments allow exercising privileges across all scopes.
+          { scope: null, scope_id: null },
+          ...(scope ? [{ scope: scope.type, scope_id: scope.id }] : []),
+        ],
+      },
     ],
   })
 
