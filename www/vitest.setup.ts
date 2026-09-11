46a8020d207b740d484a00fe67dcb5a63ac46cbc
Shahed Nasser <shahednasser@gmail.com>
2026-09-11 16:37:15 +0300

    fixes

--- www/vitest.setup.ts
+++ www/vitest.setup.ts
@@ -1,4 +1,9 @@
-import { expect } from "vitest"
+import { afterEach, expect } from "vitest"
 import * as matchers from "@testing-library/jest-dom/matchers"
+import { cleanup } from "@testing-library/react"
 
 expect.extend(matchers)
+
+afterEach(() => {
+  cleanup()
+})
