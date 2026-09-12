074c542265ef7760f8e280f9d679bbe5e847330b
Kim Wallgren <kim_wallgren@hotmail.com>
2026-09-12 15:00:49 +0200

    feat(admin): add Swedish translations

--- packages/admin/dashboard/src/i18n/translations/index.ts
+++ packages/admin/dashboard/src/i18n/translations/index.ts
@@ -31,6 +31,7 @@ import nl from "./nl.json"
 import bs from "./bs.json"
 import id from "./id.json"
 import zhTW from "./zhTW.json"
+import sv from "./sv.json"
 
 export default {
   bs: {
@@ -132,4 +133,7 @@ export default {
   zhTW: {
     translation: zhTW,
   },
+  sv: {
+    translation: sv,
+  }
 }
