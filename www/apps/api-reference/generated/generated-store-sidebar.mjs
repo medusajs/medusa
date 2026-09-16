b82755a56b08d8109cbc0ded9a50445c2fd1025b
Shahed Nasser <shahednasser@gmail.com>
2026-09-16 16:11:33 +0300

    docs: use incremental cache bucket

--- www/apps/api-reference/generated/generated-store-sidebar.mjs
+++ www/apps/api-reference/generated/generated-store-sidebar.mjs
@@ -1018,16 +1018,6 @@ const generatedgeneratedStoreSidebarSidebar = {
             "variant": "green",
             "text": "Get"
           }
-        },
-        {
-          "type": "link",
-          "path": "/store/products/list-products-2",
-          "title": "List Products",
-          "loaded": true,
-          "badge": {
-            "variant": "green",
-            "text": "Get"
-          }
         }
       ]
     },
